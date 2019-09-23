8b56ea48293762581c98653a44b1577787c27653
Winnie Hellmann <winnie@gitlab.com>
2019-09-23 14:04:25 +0200

    Make Jest transpile lodash-es

--- jest.config.js
+++ jest.config.js
@@ -56,7 +56,7 @@ module.exports = {
     '^.+\\.js$': 'babel-jest',
     '^.+\\.vue$': 'vue-jest',
   },
-  transformIgnorePatterns: ['node_modules/(?!(@gitlab/ui)/)'],
+  transformIgnorePatterns: ['node_modules/(?!(@gitlab/ui|lodash-es)/)'],
   timers: 'fake',
   testEnvironment: '<rootDir>/spec/frontend/environment.js',
   testEnvironmentOptions: {
