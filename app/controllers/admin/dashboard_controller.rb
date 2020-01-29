69d8351e363714dd96954605da56e657df70b0cd
Zack Cuddy <zcuddy@gitlab.com>
2020-01-29 10:58:18 -0600

    @zcuddy changes

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -12,7 +12,20 @@ class Admin::DashboardController < Admin::ApplicationController
     @projects = Project.order_id_desc.without_deleted.with_route.limit(10)
     @users = User.order_id_desc.limit(10)
     @groups = Group.order_id_desc.with_route.limit(10)
-    @notices = []
+    @notices = [
+    { 
+      type: 'info',
+      message: 'You are running Puma. This is experimental. Please go to puma upgrade document[https://docs.gitlab.com/ee/administration/operations/puma.html] to read more.'
+    },
+    { 
+      type: 'info',
+      message: 'You are running Puma with threads=2, please read puma upgrade document[https://docs.gitlab.com/ee/administration/operations/puma.html] of deprecations of running Puma.'
+    },
+    { 
+      type: 'warning',
+      message: 'We detected puma thread>1 and rugged service is enabled. This may decrease performance for some occasions. For details refer to puma upgrade document[https://docs.gitlab.com/ee/administration/operations/puma.html#performance-caveat-when-using-puma-with-rugged]'
+    }
+  ]
   end
   # rubocop: enable CodeReuse/ActiveRecord
 
