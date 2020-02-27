57dc4d92cf20506ec7b0cee6aac77bb3c9b5cec0
Alex Pooley <apooley@gitlab.com>
2020-02-27 14:44:15 +0800

    Protect against incorrectly named files
    There was an incorrect assumption that content_type_whitelist would
    parse the file contents to determine mime type. Unfortunately this only
    occurs in CarrierWave 2 and not 1.3.1 as we are currently using.
    
    Currently the mime type is determined by the file extension despite the
    method name of content_type_whitelist. This change will add the
    CarrierWave 2 behaviour of content_type_whitelist.

--- spec/uploaders/avatar_uploader_spec.rb
+++ spec/uploaders/avatar_uploader_spec.rb
@@ -48,30 +48,29 @@ describe AvatarUploader do
   end
 
   context 'accept whitelist file content type' do
+    include_context 'remove extension whitelist'
+
     # We need to feed through a valid path, but we force the parsed mime type
     # in a stub below so we can set any path.
-    let(:path) { File.join('spec', 'fixtures', 'video_sample.mp4') }
+    let_it_be(:path) { File.join('spec', 'fixtures', 'video_sample.mp4') }
 
     where(:mime_type) { described_class::MIME_WHITELIST }
 
     with_them do
-      before do
-        allow_any_instance_of(described_class).to receive(:extension_whitelist).and_return(nil)
-        allow_any_instance_of(MimeMagic).to receive(:type).and_return(mime_type)
-      end
+      include_context 'stubbed content type detection'
 
       it_behaves_like 'accepted carrierwave upload'
     end
   end
 
   context 'upload non-whitelisted file content type' do
-    let(:path) { File.join('spec', 'fixtures', 'sanitized.svg') }
+    let_it_be(:path) { File.join('spec', 'fixtures', 'sanitized.svg') }
 
     it_behaves_like 'denied carrierwave upload'
   end
 
   context 'upload misnamed non-whitelisted file content type' do
-    let(:path) { File.join('spec', 'fixtures', 'not_a_png.png') }
+    let_it_be(:path) { File.join('spec', 'fixtures', 'not_a_png.png') }
 
     it_behaves_like 'denied carrierwave upload'
   end
