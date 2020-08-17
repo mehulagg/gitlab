41c66f7b93926bc676fe962af26f6f19d33eb140
Drew Blessing <drew@blessing.io>
2020-08-17 17:05:55 -0500

    Add validator for IP address/inet columns
    When using column type `inet` Rails will silently return the value
    as `nil` when the value is not valid according to its type cast
    using `IpAddr`. It's not very user friendly to return an error
    "IP Address can't be blank" when a value was clearly given but
    was not the right format. This validator will look at the value
    before Rails type casts it when the value itself is `nil`.
    This enables the validator to return a specific and useful error
    message.

--- spec/models/audit_event_spec.rb
+++ spec/models/audit_event_spec.rb
@@ -6,6 +6,17 @@ RSpec.describe AuditEvent do
   let_it_be(:audit_event) { create(:project_audit_event) }
   subject { audit_event }
 
+  describe 'validations' do
+    include_examples 'validates IP address' do
+      let(:attribute) { :ip_address }
+      let(:object) { create(:audit_event) }
+
+      before do
+        object.details[:ip_address] = nil
+      end
+    end
+  end
+
   describe '#as_json' do
     context 'ip_address' do
       subject { build(:group_audit_event, ip_address: '192.168.1.1').as_json }
