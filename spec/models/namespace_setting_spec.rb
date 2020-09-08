3cf5ea007722420b2e4fe75c0982c79a16983512
Małgorzata Ksionek <mksionek@gitlab.com>
2020-09-08 16:48:27 +0200

    Add validations that respect new allow_mfa_for_subgroups setting
    Add specs for new validation
    
    Add validation in group
    
    Add specs for new requires_two_factor_validation
    
    WIP
    
    Update validations for namespace setting

--- spec/models/namespace_setting_spec.rb
+++ spec/models/namespace_setting_spec.rb
@@ -3,5 +3,27 @@
 require 'spec_helper'
 
 RSpec.describe NamespaceSetting, type: :model do
-  it { is_expected.to belong_to(:namespace) }
+  describe "Associations" do
+    it { is_expected.to belong_to(:namespace) }
+  end
+
+  describe "Validation" do
+    describe '#allow_mfa_for_group' do
+      context 'group is top-level group' do
+        let(:group) { create(:group) }
+
+        it 'is valid when updated' do
+          expect(group.namespace_settings.update(allow_mfa_for_subgroups: false)).to eq(true)
+        end
+      end
+
+      context 'group is a subgroup' do
+        let(:group) { create(:group, parent: create(:group)) }
+
+        it 'is invalid when updated' do
+          expect(group.namespace_settings.update(allow_mfa_for_subgroups: false)).to eq(false)
+        end
+      end
+    end
+  end
 end
