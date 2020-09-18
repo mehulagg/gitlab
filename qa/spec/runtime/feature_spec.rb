78266744428d1c4fe3578405c80bbf82c75f26c2
Mark Lapierre <mlapierre@gitlab.com>
2020-09-18 13:51:41 +1000

    Allow scoped feature flag toggling
    - Allows feature flags to be set for a user, project, group, or
      feature group

--- qa/spec/runtime/feature_spec.rb
+++ qa/spec/runtime/feature_spec.rb
@@ -4,30 +4,63 @@ describe QA::Runtime::Feature do
   let(:api_client) { double('QA::Runtime::API::Client') }
   let(:request) { Struct.new(:url).new('http://api') }
   let(:response_post) { Struct.new(:code).new(201) }
-  let(:response_get) { Struct.new(:code, :body).new(200, '[{ "name": "a-flag", "state": "on" }]') }
 
   before do
     allow(described_class).to receive(:api_client).and_return(api_client)
   end
 
-  describe '.enable' do
-    it 'enables a feature flag' do
-      expect(QA::Runtime::API::Request)
-        .to receive(:new)
-        .with(api_client, "/features/a-flag")
-        .and_return(request)
-      expect(described_class)
-        .to receive(:post)
-        .with(request.url, { value: true })
-        .and_return(response_post)
+  shared_examples 'enables a feature flag' do
+    it 'enables a feature flag for a scope' do
+      allow(described_class).to receive(:get)
+        .and_return(Struct.new(:code, :body).new(200, '[{ "name": "a-flag", "state": "on" }]'))
 
-      subject.enable('a-flag')
+      expect(QA::Runtime::API::Request).to receive(:new)
+        .with(api_client, "/features/a-flag").and_return(request)
+      expect(described_class).to receive(:post)
+        .with(request.url, { value: true, scope => actor_name }).and_return(response_post)
+      expect(QA::Runtime::API::Request).to receive(:new)
+        .with(api_client, "/features").and_return(request)
+
+      described_class.enable_and_verify('a-flag', scope => actor)
+    end
+  end
+
+  shared_examples 'disables a feature flag' do
+    it 'disables a feature flag for a scope' do
+      allow(described_class).to receive(:get)
+        .and_return(Struct.new(:code, :body).new(200, '[{ "name": "a-flag", "state": "off" }]'))
+
+      expect(QA::Runtime::API::Request).to receive(:new)
+        .with(api_client, "/features/a-flag").and_return(request)
+      expect(described_class).to receive(:post)
+        .with(request.url, { value: false, scope => actor_name }).and_return(response_post)
+      expect(QA::Runtime::API::Request).to receive(:new)
+        .with(api_client, "/features").and_return(request)
+
+      described_class.disable_and_verify('a-flag', scope => actor )
+    end
+  end
+
+  shared_examples 'checks a feature flag' do
+    context 'when the flag is enabled for a scope' do
+      it 'returns the feature flag state' do
+        expect(QA::Runtime::API::Request)
+          .to receive(:new)
+          .with(api_client, "/features")
+          .and_return(request)
+        expect(described_class)
+          .to receive(:get)
+          .and_return(Struct.new(:code, :body).new(200, %Q([{ "name": "a-flag", "state": "conditional", "gates": #{gates} }])))
+
+        expect(described_class.enabled?('a-flag', scope => actor)).to be_truthy
+      end
     end
   end
 
   describe '.enable_and_verify' do
     it 'enables a feature flag' do
-      allow(described_class).to receive(:get).and_return(response_get)
+      allow(described_class).to receive(:get)
+        .and_return(Struct.new(:code, :body).new(200, '[{ "name": "a-flag", "state": "on" }]'))
 
       expect(QA::Runtime::API::Request).to receive(:new)
         .with(api_client, "/features/a-flag").and_return(request)
@@ -36,22 +69,39 @@ describe QA::Runtime::Feature do
       expect(QA::Runtime::API::Request).to receive(:new)
         .with(api_client, "/features").and_return(request)
 
-      subject.enable_and_verify('a-flag')
+      described_class.enable_and_verify('a-flag')
     end
-  end
 
-  describe '.disable' do
-    it 'disables a feature flag' do
-      expect(QA::Runtime::API::Request)
-        .to receive(:new)
-        .with(api_client, "/features/a-flag")
-        .and_return(request)
-      expect(described_class)
-        .to receive(:post)
-        .with(request.url, { value: false })
-        .and_return(response_post)
+    context 'when a project scope is provided' do
+      it_behaves_like 'enables a feature flag' do
+        let(:scope) { :project }
+        let(:actor_name) { 'group-name/project-name' }
+        let(:actor) { Struct.new(:full_path).new(actor_name) }
+      end
+    end
 
-      subject.disable('a-flag')
+    context 'when a group scope is provided' do
+      it_behaves_like 'enables a feature flag' do
+        let(:scope) { :group }
+        let(:actor_name) { 'group-name' }
+        let(:actor) { Struct.new(:full_path).new(actor_name) }
+      end
+    end
+
+    context 'when a user scope is provided' do
+      it_behaves_like 'enables a feature flag' do
+        let(:scope) { :user }
+        let(:actor_name) { 'user-name' }
+        let(:actor) { Struct.new(:username).new(actor_name) }
+      end
+    end
+
+    context 'when a feature group scope is provided' do
+      it_behaves_like 'enables a feature flag' do
+        let(:scope) { :feature_group }
+        let(:actor_name) { 'foo' }
+        let(:actor) { "foo" }
+      end
     end
   end
 
@@ -67,7 +117,39 @@ describe QA::Runtime::Feature do
       expect(QA::Runtime::API::Request).to receive(:new)
         .with(api_client, "/features").and_return(request)
 
-      subject.disable_and_verify('a-flag')
+      described_class.disable_and_verify('a-flag')
+    end
+
+    context 'when a project scope is provided' do
+      it_behaves_like 'disables a feature flag' do
+        let(:scope) { :project }
+        let(:actor_name) { 'group-name/project-name' }
+        let(:actor) { Struct.new(:full_path).new(actor_name) }
+      end
+    end
+
+    context 'when a group scope is provided' do
+      it_behaves_like 'disables a feature flag' do
+        let(:scope) { :group }
+        let(:actor_name) { 'group-name' }
+        let(:actor) { Struct.new(:full_path).new(actor_name) }
+      end
+    end
+
+    context 'when a user scope is provided' do
+      it_behaves_like 'disables a feature flag' do
+        let(:scope) { :user }
+        let(:actor_name) { 'user-name' }
+        let(:actor) { Struct.new(:username).new(actor_name) }
+      end
+    end
+
+    context 'when a feature group scope is provided' do
+      it_behaves_like 'disables a feature flag' do
+        let(:scope) { :feature_group }
+        let(:actor_name) { 'foo' }
+        let(:actor) { "foo" }
+      end
     end
   end
 
@@ -79,9 +161,45 @@ describe QA::Runtime::Feature do
         .and_return(request)
       expect(described_class)
         .to receive(:get)
-        .and_return(response_get)
+        .and_return(Struct.new(:code, :body).new(200, '[{ "name": "a-flag", "state": "on" }]'))
 
-      expect(subject.enabled?('a-flag')).to be_truthy
+      expect(described_class.enabled?('a-flag')).to be_truthy
+    end
+
+    context 'when a project scope is provided' do
+      it_behaves_like 'checks a feature flag' do
+        let(:scope) { :project }
+        let(:actor_name) { 'group-name/project-name' }
+        let(:actor) { Struct.new(:full_path, :id).new(actor_name, 270) }
+        let(:gates) { %q([{"key": "actors", "value": ["Project:270"]}]) }
+      end
+    end
+
+    context 'when a group scope is provided' do
+      it_behaves_like 'checks a feature flag' do
+        let(:scope) { :group }
+        let(:actor_name) { 'group-name' }
+        let(:actor) { Struct.new(:full_path, :id).new(actor_name, 33) }
+        let(:gates) { %q([{"key": "actors", "value": ["Group:33"]}]) }
+      end
+    end
+
+    context 'when a user scope is provided' do
+      it_behaves_like 'checks a feature flag' do
+        let(:scope) { :user }
+        let(:actor_name) { 'user-name' }
+        let(:actor) { Struct.new(:full_path, :id).new(actor_name, 13) }
+        let(:gates) { %q([{"key": "actors", "value": ["User:13"]}]) }
+      end
+    end
+
+    context 'when a feature group scope is provided' do
+      it_behaves_like 'checks a feature flag' do
+        let(:scope) { :feature_group }
+        let(:actor_name) { 'foo' }
+        let(:actor) { "foo" }
+        let(:gates) { %q([{"key": "groups", "value": ["foo"]}]) }
+      end
     end
   end
 end
