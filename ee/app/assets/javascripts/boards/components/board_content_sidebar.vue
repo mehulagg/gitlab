fce25e8c3d52d2610d174e16b165e02e34e68c47
sstern <sstern@gitlab.com>
2020-09-21 16:04:50 -0700

    WIP

--- ee/app/assets/javascripts/boards/components/board_content_sidebar.vue
+++ ee/app/assets/javascripts/boards/components/board_content_sidebar.vue
@@ -1,11 +1,12 @@
 <script>
 import { mapState, mapActions, mapGetters } from 'vuex';
-import { GlDrawer } from '@gitlab/ui';
+import { GlDrawer, GlDropdown, GlSearchBoxByType } from '@gitlab/ui';
 import { ISSUABLE } from '~/boards/constants';
 import { contentTop } from '~/lib/utils/common_utils';
 import IssuableAssignees from '~/sidebar/components/assignees/issuable_assignees.vue';
 import IssuableTitle from '~/boards/components/issuable_title.vue';
 import BoardSidebarEpicSelect from './sidebar/board_sidebar_epic_select.vue';
+import BoardEditableItem from '~/boards/components/sidebar/board_editable_item.vue';
 
 export default {
   headerHeight: `${contentTop()}px`,
@@ -14,6 +15,9 @@ export default {
     GlDrawer,
     IssuableTitle,
     BoardSidebarEpicSelect,
+    BoardEditableItem,
+    GlDropdown,
+    GlSearchBoxByType,
   },
   computed: {
     ...mapGetters(['isSidebarOpen', 'getActiveIssue']),
@@ -41,6 +45,22 @@ export default {
 
     <template>
       <issuable-assignees :users="getActiveIssue.assignees" />
+      <board-editable-item :title="'Assignee'">
+        <template #collapsed>
+          <issuable-assignees :users="getActiveIssue.assignees" />
+        </template>
+
+        <template #default>
+          <gl-dropdown class="show w-100" text="Assignees" header-text="Assign to">
+            <b-dropdown-form class="w-100">
+              <gl-search-box-by-type />
+            </b-dropdown-form>
+          </gl-dropdown>
+        </template>
+      </board-editable-item>
+    </template>
+
+    <template>
       <board-sidebar-epic-select />
     </template>
   </gl-drawer>
