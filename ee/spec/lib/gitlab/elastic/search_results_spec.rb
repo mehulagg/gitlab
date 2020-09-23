62a13064d78dad0b7e64be89cf58a29c7a3b9b6c
Mark Chao <mchao@gitlab.com>
2020-09-23 02:17:59 +0000

    Apply 1 suggestion(s) to 1 file(s)

--- ee/spec/lib/gitlab/elastic/search_results_spec.rb
+++ ee/spec/lib/gitlab/elastic/search_results_spec.rb
@@ -181,6 +181,7 @@ RSpec.describe Gitlab::Elastic::SearchResults, :elastic, :sidekiq_might_not_need
 
         ensure_elasticsearch_index!
       end
+
       include_examples 'search results filtered by state'
       include_examples 'search results filtered by confidential'
     end
