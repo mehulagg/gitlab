mehulagg/gitlab_test

Adds package event tracking
Adds package event model
and extend package event
tracking to support
existing tracking calls.

spec/support/shared_examples/requests/api/nuget_packages_shared_examples.rb
@@ -26,7 +26,7 @@ RSpec.shared_examples 'process nuget service index request' do |user_type, statu
 
     it_behaves_like 'returning response status', status
 
-    it_behaves_like 'a gitlab tracking event', described_class.name, 'nuget_service_index'
+    it_behaves_like 'a package tracking event', described_class.name, 'list_package'
 
     it 'returns a valid json response' do
       subject
@@ -169,7 +169,7 @@ RSpec.shared_examples 'process nuget upload' do |user_type, status, add_member =
       context 'with correct params' do
         it_behaves_like 'package workhorse uploads'
         it_behaves_like 'creates nuget package files'
-        it_behaves_like 'a gitlab tracking event', described_class.name, 'push_package'
+        it_behaves_like 'a package tracking event', described_class.name, 'push_package'
       end
     end
 
@@ -286,7 +286,7 @@ RSpec.shared_examples 'process nuget download content request' do |user_type, st
 
     it_behaves_like 'returning response status', status
 
-    it_behaves_like 'a gitlab tracking event', described_class.name, 'pull_package'
+    it_behaves_like 'a package tracking event', described_class.name, 'pull_package'
 
     it 'returns a valid package archive' do
       subject
@@ -336,7 +336,7 @@ RSpec.shared_examples 'process nuget search request' do |user_type, status, add_
 
     it_behaves_like 'returns a valid json search response', status, 4, [1, 5, 5, 1]
 
-    it_behaves_like 'a gitlab tracking event', described_class.name, 'search_package'
+    it_behaves_like 'a package tracking event', described_class.name, 'search_package'
 
     context 'with skip set to 2' do
       let(:skip) { 2 }

mehulagg/gitlab

spec/support/shared_examples/requests/api/nuget_packages_shared_examples.rb
@@ -26,7 +26,7 @@ RSpec.shared_examples 'process nuget service index request' do |user_type, statu
 
     it_behaves_like 'returning response status', status
 
-    it_behaves_like 'a gitlab tracking event', described_class.name, 'nuget_service_index'
+    it_behaves_like 'a package tracking event', described_class.name, 'package_metadata'
 
     it 'returns a valid json response' do
       subject
@@ -169,7 +169,7 @@ RSpec.shared_examples 'process nuget upload' do |user_type, status, add_member =
       context 'with correct params' do
         it_behaves_like 'package workhorse uploads'
         it_behaves_like 'creates nuget package files'
-        it_behaves_like 'a gitlab tracking event', described_class.name, 'push_package'
+        it_behaves_like 'a package tracking event', described_class.name, 'push_package'
       end
     end
 
@@ -286,7 +286,7 @@ RSpec.shared_examples 'process nuget download content request' do |user_type, st
 
     it_behaves_like 'returning response status', status
 
-    it_behaves_like 'a gitlab tracking event', described_class.name, 'pull_package'
+    it_behaves_like 'a package tracking event', described_class.name, 'pull_package'
 
     it 'returns a valid package archive' do
       subject
@@ -336,7 +336,7 @@ RSpec.shared_examples 'process nuget search request' do |user_type, status, add_
 
     it_behaves_like 'returns a valid json search response', status, 4, [1, 5, 5, 1]
 
-    it_behaves_like 'a gitlab tracking event', described_class.name, 'search_package'
+    it_behaves_like 'a package tracking event', described_class.name, 'search_package'
 
     context 'with skip set to 2' do
       let(:skip) { 2 }