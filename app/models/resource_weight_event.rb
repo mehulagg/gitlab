2e11d4b531f9f70fa16d3874e6a05aa9af9f7d21
Mario de la Ossa <mariodelaossa@gmail.com>
2020-09-23 17:24:56 -0600

    Add more Issue usage metrics
    Get a unique MAU count for different metrics
    Tracks:
    - issue created
    - issue closed
    - issue reopened
    - label changed
    - milestone changed
    - iteration changed
    - weight changed

--- app/models/resource_weight_event.rb
+++ app/models/resource_weight_event.rb
@@ -3,5 +3,13 @@
 class ResourceWeightEvent < ResourceEvent
   validates :issue, presence: true
 
+  after_save :usage_metrics
+
   include IssueResourceEvent
+
+  private
+
+  def usage_metrics
+    Gitlab::UsageDataCounters::IssueActivityUniqueCounter.track_issue_weight_changed_action(author: user)
+  end
 end
