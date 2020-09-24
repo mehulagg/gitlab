eb685b34ac08dc6b3fe205c267d0a37482c8a6ed
Simon Knox <simon@gitlab.com>
2020-09-24 11:20:55 +1000

    Remove antiquated warning from pre-9.5 GitLab
    It is no longer likely to be relevant

--- ee/app/helpers/ee/timeboxes_helper.rb
+++ ee/app/helpers/ee/timeboxes_helper.rb
@@ -26,23 +26,6 @@ module EE
       warning.nil? && can?(current_user, :admin_milestone, milestone.resource_parent)
     end
 
-    def data_warning_for(burndown)
-      return unless burndown
-
-      message =
-        if burndown.empty?
-          "The burndown chart can’t be shown, as all issues assigned to this milestone were closed on an older GitLab version before data was recorded. "
-        elsif !burndown.accurate?
-          "Some issues can’t be shown in the burndown chart, as they were closed on an older GitLab version before data was recorded. "
-        end
-
-      if message
-        link = link_to "About burndown charts", help_page_path('user/project/milestones/burndown_charts.md'), class: 'burndown-docs-link'
-
-        content_tag(:div, (message + link).html_safe, id: "data-warning", class: "settings-message prepend-top-20")
-      end
-    end
-
     def milestone_weight_tooltip_text(weight)
       if weight == 0
         _("Weight")
