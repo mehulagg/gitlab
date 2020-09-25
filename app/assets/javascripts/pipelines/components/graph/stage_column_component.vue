264ed5a3d1e8dfa573e3395884841065583a3905
Sarah GP <sarah.groff.palermo@gmail.com>
2020-09-25 18:05:10 +0200

    Remove the absolute positioning, too

--- app/assets/javascripts/pipelines/components/graph/stage_column_component.vue
+++ app/assets/javascripts/pipelines/components/graph/stage_column_component.vue
@@ -71,7 +71,7 @@ export default {
         :action-icon="action.icon"
         :tooltip-text="action.title"
         :link="action.path"
-        class="js-stage-action stage-action position-absolute rounded"
+        class="js-stage-action stage-action rounded"
         @pipelineActionRequestComplete="pipelineActionRequestComplete"
       />
     </div>
