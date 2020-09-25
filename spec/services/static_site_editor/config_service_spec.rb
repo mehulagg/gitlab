mehulagg/gitlab_test

Fully implement SSE config file handling
* Third iteration towards static site editor config file.
* Replaces stub FileConfig with full implementation based on
  internal ::Gitlab::Config config-file API.

spec/services/static_site_editor/config_service_spec.rb
@@ -7,8 +7,8 @@ RSpec.describe StaticSiteEditor::ConfigService do
   let_it_be(:user) { create(:user) }
 
   # params
-  let(:ref) { double(:ref) }
-  let(:path) { double(:path) }
+  let(:ref) { 'master' }
+  let(:path) { 'README.md' }
   let(:return_url) { double(:return_url) }
 
   # stub data
@@ -42,22 +42,89 @@ RSpec.describe StaticSiteEditor::ConfigService do
         allow_next_instance_of(Gitlab::StaticSiteEditor::Config::GeneratedConfig) do |config|
           allow(config).to receive(:data) { generated_data }
         end
+      end
 
-        allow_next_instance_of(Gitlab::StaticSiteEditor::Config::FileConfig) do |config|
-          allow(config).to receive(:data) { file_data }
+      context 'when reading file from repo fails with an unexpected error' do
+        let(:unexpected_error) { RuntimeError.new('some unexpected error') }
+
+        before do
+          allow(project.repository).to receive(:blob_data_at).and_raise(unexpected_error)
+        end
+
+        it 'returns an error response' do
+          expect(Gitlab::ErrorTracking).to receive(:track_and_raise_exception).with(unexpected_error).and_call_original
+          expect { execute }.to raise_error(unexpected_error)
         end
       end
 
-      it 'returns merged generated data and config file data' do
-        expect(execute).to be_success
-        expect(execute.payload).to eq(generated: true, file: true)
+      context 'when file is missing' do
+        before do
+          allow(project.repository).to receive(:blob_data_at).and_raise(GRPC::NotFound)
+          expect_next_instance_of(Gitlab::StaticSiteEditor::Config::FileConfig, '{}') do |config|
+            allow(config).to receive(:valid?) { true }
+            allow(config).to receive(:to_hash) { file_data }
+          end
+        end
+
+        it 'returns default config' do
+          expect(execute).to be_success
+          expect(execute.payload).to eq(generated: true, file: true)
+        end
       end
 
-      it 'returns an error if any keys would be overwritten by the merge' do
-        generated_data[:duplicate_key] = true
-        file_data[:duplicate_key] = true
-        expect(execute).to be_error
-        expect(execute.message).to match(/duplicate key.*duplicate_key.*found/i)
+      context 'when file is present' do
+        before do
+          sha = 'abcde'
+          commit = instance_double(Commit, id: sha)
+          allow(project.repository).to receive(:commit).with(ref) do
+            commit
+          end
+          allow(project.repository).to receive(:blob_data_at).with(sha, anything) do
+            config_content
+          end
+        end
+
+        context 'and configuration is not valid' do
+          let(:config_content) { 'invalid content' }
+
+          before do
+            expect_next_instance_of(Gitlab::StaticSiteEditor::Config::FileConfig, config_content) do |config|
+              error = 'error'
+              allow(config).to receive_message_chain('errors.first') { error }
+              allow(config).to receive(:valid?) { false }
+            end
+          end
+
+          it 'returns an error' do
+            expect(execute).to be_error
+            expect(execute.message).to eq('Invalid configuration format')
+          end
+        end
+
+        context 'and configuration is valid' do
+          # NOTE: This has to be a valid config, even though it is mocked, because
+          #       `expect_next_instance_of` executes the constructor logic.
+          let(:config_content) { 'static_site_generator: middleman' }
+
+          before do
+            expect_next_instance_of(Gitlab::StaticSiteEditor::Config::FileConfig, config_content) do |config|
+              allow(config).to receive(:valid?) { true }
+              allow(config).to receive(:to_hash) { file_data }
+            end
+          end
+
+          it 'returns merged generated data and config file data' do
+            expect(execute).to be_success
+            expect(execute.payload).to eq(generated: true, file: true)
+          end
+
+          it 'returns an error if any keys would be overwritten by the merge' do
+            generated_data[:duplicate_key] = true
+            file_data[:duplicate_key] = true
+            expect(execute).to be_error
+            expect(execute.message).to match(/duplicate key.*duplicate_key.*found/i)
+          end
+        end
       end
     end
   end

mehulagg/gitlab

spec/services/static_site_editor/config_service_spec.rb
@@ -7,8 +7,8 @@ RSpec.describe StaticSiteEditor::ConfigService do
   let_it_be(:user) { create(:user) }
 
   # params
-  let(:ref) { double(:ref) }
-  let(:path) { double(:path) }
+  let(:ref) { 'master' }
+  let(:path) { 'README.md' }
   let(:return_url) { double(:return_url) }
 
   # stub data
@@ -42,22 +42,84 @@ RSpec.describe StaticSiteEditor::ConfigService do
         allow_next_instance_of(Gitlab::StaticSiteEditor::Config::GeneratedConfig) do |config|
           allow(config).to receive(:data) { generated_data }
         end
+      end
 
-        allow_next_instance_of(Gitlab::StaticSiteEditor::Config::FileConfig) do |config|
-          allow(config).to receive(:data) { file_data }
+      context 'when reading file from repo fails with an unexpected error' do
+        let(:unexpected_error) { RuntimeError.new('some unexpected error') }
+
+        before do
+          allow(project.repository).to receive(:blob_data_at).and_raise(unexpected_error)
+        end
+
+        it 'returns an error response' do
+          expect(Gitlab::ErrorTracking).to receive(:track_and_raise_exception).with(unexpected_error).and_call_original
+          expect { execute }.to raise_error(unexpected_error)
         end
       end
 
-      it 'returns merged generated data and config file data' do
-        expect(execute).to be_success
-        expect(execute.payload).to eq(generated: true, file: true)
+      context 'when file is missing' do
+        before do
+          allow(project.repository).to receive(:blob_data_at).and_raise(GRPC::NotFound)
+          expect_next_instance_of(Gitlab::StaticSiteEditor::Config::FileConfig, '{}') do |config|
+            allow(config).to receive(:valid?) { true }
+            allow(config).to receive(:to_hash) { file_data }
+          end
+        end
+
+        it 'returns default config' do
+          expect(execute).to be_success
+          expect(execute.payload).to eq(generated: true, file: true)
+        end
       end
 
-      it 'returns an error if any keys would be overwritten by the merge' do
-        generated_data[:duplicate_key] = true
-        file_data[:duplicate_key] = true
-        expect(execute).to be_error
-        expect(execute.message).to match(/duplicate key.*duplicate_key.*found/i)
+      context 'when file is present' do
+        before do
+          allow(project.repository).to receive(:blob_data_at).with(ref, anything) do
+            config_content
+          end
+        end
+
+        context 'and configuration is not valid' do
+          let(:config_content) { 'invalid content' }
+
+          before do
+            expect_next_instance_of(Gitlab::StaticSiteEditor::Config::FileConfig, config_content) do |config|
+              error = 'error'
+              allow(config).to receive_message_chain('errors.first') { error }
+              allow(config).to receive(:valid?) { false }
+            end
+          end
+
+          it 'returns an error' do
+            expect(execute).to be_error
+            expect(execute.message).to eq('Invalid configuration format')
+          end
+        end
+
+        context 'and configuration is valid' do
+          # NOTE: This has to be a valid config, even though it is mocked, because
+          #       `expect_next_instance_of` executes the constructor logic.
+          let(:config_content) { 'static_site_generator: middleman' }
+
+          before do
+            expect_next_instance_of(Gitlab::StaticSiteEditor::Config::FileConfig, config_content) do |config|
+              allow(config).to receive(:valid?) { true }
+              allow(config).to receive(:to_hash) { file_data }
+            end
+          end
+
+          it 'returns merged generated data and config file data' do
+            expect(execute).to be_success
+            expect(execute.payload).to eq(generated: true, file: true)
+          end
+
+          it 'returns an error if any keys would be overwritten by the merge' do
+            generated_data[:duplicate_key] = true
+            file_data[:duplicate_key] = true
+            expect(execute).to be_error
+            expect(execute.message).to match(/duplicate key.*duplicate_key.*found/i)
+          end
+        end
       end
     end
   end