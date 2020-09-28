46f9a32a60cd5596cbb8ea04e8ba1bc7da7a4fca
nmccorrison <nmccorrison@gitlab.com>
2020-09-27 23:53:55 -0600

    Updated QA tests

--- qa/qa/ee/page/component/license_management.rb
+++ qa/qa/ee/page/component/license_management.rb
@@ -21,8 +21,6 @@ module QA
 
               view 'ee/app/assets/javascripts/vue_shared/license_compliance/components/set_approval_status_modal.vue' do
                 element :license_management_modal
-                element :approve_license_button
-                element :deny_license_button
               end
 
               view 'ee/app/assets/javascripts/vue_shared/license_compliance/mr_widget_license_report.vue' do
@@ -31,40 +29,13 @@ module QA
             end
           end
 
-          def has_approved_license?(name)
-            within_element(:report_item_row, text: name) do
-              has_element?(:status_success_icon, wait: 1)
-            end
-          end
-
-          def has_denied_license?(name)
-            within_element(:report_item_row, text: name) do
-              has_element?(:status_failed_icon, wait: 1)
-            end
-          end
-
-          def click_license(name)
+           def click_license(name)
             within_element(:license_report_widget) do
               click_on name
             end
             wait_for_animated_element(:license_management_modal)
           end
 
-          def approve_license(name)
-            wait_until(reload: true) do
-              click_license(name)
-              has_element?(:approve_license_button, wait: 1)
-            end
-            click_element(:approve_license_button)
-          end
-
-          def deny_license(name)
-            wait_until(reload: true) do
-              click_license(name)
-              has_element?(:deny_license_button, wait: 1)
-            end
-            click_element(:deny_license_button)
-          end
         end
       end
     end
