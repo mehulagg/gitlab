9603a1a2ae7d43d568d2fccf25494b826eb511c7
Robert Marshall <rmarshall@gitlab.com>
2020-09-28 13:41:40 -0400

    Convert to using parameterized table syntax
    - Use parameterized table syntax per suggestion from Imre Farkas
    
    Signed-off-by: Robert Marshall <rmarshall@gitlab.com>

--- ee/spec/lib/system_check/geo/license_check_spec.rb
+++ ee/spec/lib/system_check/geo/license_check_spec.rb
@@ -7,123 +7,29 @@ RSpec.describe SystemCheck::Geo::LicenseCheck do
   describe '#check?' do
     subject { described_class.new }
 
-    context 'when in the primary Geo site' do
-      before do
-        allow(Gitlab::Geo).to receive(:primary?).and_return(true)
-      end
+    using RSpec::Parameterized::TableSyntax
 
-      context 'and Geo is enabled' do
-        before do
-          allow(Gitlab::Geo).to receive(:enabled?).and_return(true)
-        end
-
-        context 'and a supporting License exists' do
-          before do
-            allow(Gitlab::Geo).to receive(:license_allows?).and_return(true)
-          end
-
-          it 'passes the check' do
-            expect(subject.check?).to be_truthy
-          end
-
-          it 'returns an empty string via check_pass' do
-            expect(SystemCheck::Geo::LicenseCheck.check_pass).to eq('')
-          end
-        end
-
-        context 'and no License supporting Geo is found' do
-          before do
-            allow(Gitlab::Geo).to receive(:license_allows?).and_return(false)
-          end
-
-          it 'fails the check' do
-            expect(subject.check?).to be_falsey
-          end
-
-          it 'returns an empty string via check_pass' do
-            expect(SystemCheck::Geo::LicenseCheck.check_pass).to eq('')
-          end
-        end
-      end
-
-      context 'and Geo is not enabled' do
-        before do
-          allow(Gitlab::Geo).to receive(:enabled?).and_return(false)
-        end
-
-        context 'but a supporting License exists' do
-          before do
-            allow(Gitlab::Geo).to receive(:license_allows?).and_return(true)
-          end
-
-          it 'passes the check' do
-            expect(subject.check?).to be_truthy
-          end
-
-          it 'informs the user via check_pass that Geo can be enabled' do
-            expect(SystemCheck::Geo::LicenseCheck.check_pass).to eq('License supports Geo, but Geo is not enabled')
-          end
-        end
-
-        context 'and no License supporting Geo is found' do
-          before do
-            allow(Gitlab::Geo).to receive(:license_allows?).and_return(false)
-          end
-
-          it 'fails the check' do
-            expect(subject.check?).to be_truthy
-          end
-
-          it 'informs the user via check_pass that Geo is not supported' do
-            expect(SystemCheck::Geo::LicenseCheck.check_pass).to eq('License does not support Geo, and Geo is not enabled')
-          end
-        end
-      end
+    where(:primary, :geo_enabled, :license_allows, :check_result, :pass_message) do
+      true | true | true | true | ''
+      true | true | false | false | ''
+      true | false | true | true | 'License supports Geo, but Geo is not enabled'
+      true | false | false | true | 'License does not support Geo, and Geo is not enabled'
+      false | true | true | true | ''
+      false | true | false | true | 'License only required on a primary site'
+      false | false | true | true | ''
+      false | false | false | true | ''
     end
 
-    context 'when in a secondary Geo site' do
+    with_them do
       before do
-        allow(Gitlab::Geo).to receive(:primary?).and_return(false)
+        allow(Gitlab::Geo).to receive(:primary?).and_return(primary)
+        allow(Gitlab::Geo).to receive(:enabled?).and_return(geo_enabled)
+        allow(Gitlab::Geo).to receive(:license_allows?).and_return(license_allows)
       end
 
-      it 'passes the check' do
-        allow(Gitlab::Geo).to receive(:license_allows?).and_return(true)
-      end
-
-      context 'and Geo is enabled' do
-        before do
-          allow(Gitlab::Geo).to receive(:enabled?).and_return(true)
-        end
-
-        context 'and a supporting License exists' do
-          before do
-            allow(Gitlab::Geo).to receive(:license_allows?).and_return(true)
-          end
-
-          it 'returns an empty string via check_pass' do
-            expect(SystemCheck::Geo::LicenseCheck.check_pass).to eq('')
-          end
-        end
-
-        context 'and no License supporting Geo is found' do
-          before do
-            allow(Gitlab::Geo).to receive(:license_allows?).and_return(false)
-          end
-
-          it 'informs user that License is only required on a primary site' do
-            expect(SystemCheck::Geo::LicenseCheck.check_pass).to eq('License only required on a primary site')
-          end
-        end
-      end
-
-      context 'and Geo is not enabled' do
-        before do
-          allow(Gitlab::Geo).to receive(:enabled?).and_return(false)
-        end
-
-        it 'returns an empty string via check_pass' do
-          expect(SystemCheck::Geo::LicenseCheck.check_pass).to eq('')
-        end
+      it 'checks the license' do
+        expect(subject.check?).to eq(check_result)
+        expect(described_class.check_pass).to eq(pass_message) if check_result
       end
     end
   end
