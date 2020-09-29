25f0197de03b1f99b342f15b919e8c3947327c12
Martin Wortschack <mwortschack@gitlab.com>
2020-09-29 12:22:13 +0200

    Remove apollo provider from VSA

--- ee/app/assets/javascripts/analytics/cycle_analytics/index.js
+++ ee/app/assets/javascripts/analytics/cycle_analytics/index.js
@@ -1,18 +1,11 @@
 import Vue from 'vue';
-import VueApollo from 'vue-apollo';
 import { GlToast } from '@gitlab/ui';
 import CycleAnalytics from './components/base.vue';
 import createStore from './store';
-import createDefaultClient from '~/lib/graphql';
 import { buildCycleAnalyticsInitialData } from '../shared/utils';
 import { urlQueryToFilter } from '~/vue_shared/components/filtered_search_bar/filtered_search_utils';
 
 Vue.use(GlToast);
-Vue.use(VueApollo);
-
-const apolloProvider = new VueApollo({
-  defaultClient: createDefaultClient(),
-});
 
 export default () => {
   const el = document.querySelector('#js-cycle-analytics-app');
@@ -50,7 +43,6 @@ export default () => {
   return new Vue({
     el,
     name: 'CycleAnalyticsApp',
-    apolloProvider,
     store,
     render: createElement =>
       createElement(CycleAnalytics, {
