f8a4b6687e94e2e7256eeede1448c5b4cdff1ece
Sean McGivern <sean@gitlab.com>
2020-09-29 14:34:29 +0100

    Add feature categories to controllers beginning with B

--- ee/app/controllers/boards/milestones_controller.rb
+++ ee/app/controllers/boards/milestones_controller.rb
@@ -5,6 +5,7 @@ module Boards
     include BoardsResponses
 
     before_action :authorize_read_milestone, only: [:index]
+    feature_category :kanban_boards, only: [:index]
 
     def index
       milestones_finder = Boards::MilestonesFinder.new(board, current_user)
