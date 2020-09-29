d20480a6316cd8b6e975acc810afc01550b7b24d
Francisco Javier López <fjlopez@gitlab.com>
2020-09-29 18:10:31 +0200

    Fix spec

--- spec/lib/backup/repositories_spec.rb
+++ spec/lib/backup/repositories_spec.rb
@@ -220,7 +220,8 @@ RSpec.describe Backup::Repositories do
     end
 
     it 'cleans existing repositories' do
-      expect(Repository).to receive(:new).twice.and_wrap_original do |method, *original_args|
+      # 4 times = project repo + wiki repo + project_snippet repo + personal_snippet repo
+      expect(Repository).to receive(:new).exactly(4).times.and_wrap_original do |method, *original_args|
         repository = method.call(*original_args)
 
         expect(repository).to receive(:remove)
