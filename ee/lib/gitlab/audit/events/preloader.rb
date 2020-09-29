0fa7191b8999385d06c63a2ef1d27a7449c2b74b
Adam Hegyi <ahegyi@gitlab.com>
2020-09-29 17:00:14 +0200

    Improve preloading logic

--- ee/lib/gitlab/audit/events/preloader.rb
+++ ee/lib/gitlab/audit/events/preloader.rb
@@ -4,12 +4,16 @@ module Gitlab
   module Audit
     module Events
       class Preloader
-        def self.preload!(audit_events)
-          audit_events.tap do |audit_events|
-            audit_events.each do |audit_event|
-              audit_event.lazy_author
-              audit_event.lazy_entity
-            end
+        def initialize(audit_events)
+          @audit_events = audit_events
+        end
+
+        def find_each(&block)
+          @audit_events.find_each do |audit_event|
+            audit_event.lazy_author
+            audit_event.lazy_entity
+
+            block.call(audit_event)
           end
         end
       end
