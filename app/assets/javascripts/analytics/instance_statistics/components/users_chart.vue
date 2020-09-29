3e51ac2eb40a8f536cc86c38a1199920d2c0ed67
Ezekiel Kigbo <ekigbo@gitlab.com>
2020-09-30 01:32:32 +1000

    wip - handle fetching more users until our limit

--- app/assets/javascripts/analytics/instance_statistics/components/users_chart.vue
+++ app/assets/javascripts/analytics/instance_statistics/components/users_chart.vue
@@ -1,84 +1,108 @@
 <script>
-import * as Sentry from '@sentry/browser';
-import { deprecatedCreateFlash as createFlash } from '~/flash';
-import ChartSkeletonLoader from '~/vue_shared/components/resizable_chart/skeleton_loader.vue';
 import { GlLineChart } from '@gitlab/ui/dist/charts';
+import { reverse } from 'lodash';
+import produce from 'immer';
+import createFlash from '~/flash';
+import ChartSkeletonLoader from '~/vue_shared/components/resizable_chart/skeleton_loader.vue';
+import { __ } from '~/locale';
 // Interesting
 import ResizableChartContainer from '~/vue_shared/components/resizable_chart/resizable_chart_container.vue';
 // Interesting
-import dateFormat from 'dateformat';
-import { formatDate, getDatesInRange } from '~/lib/utils/datetime_utility';
+import { formatDate, getDatesInRange, getDateInPast } from '~/lib/utils/datetime_utility';
 import latestUsersQuery from '../graphql/queries/latest_users_count.query.graphql';
+
+// TODO: we want to fetch 1 year's worth
+// TODO: if there is no count for a day, do we get 0?
+// TODO: group chart data by month
+
+const TIME_PERIOD_IN_DAYS = 365;
+const ymd = d => formatDate(d, 'yyyy-mm-dd');
+const FETCH_LIMIT = TIME_PERIOD_IN_DAYS;
+// TODO: check if empty days are returned
+// Maybe we use a Map() to store the entries by their date
+// const TODAY = new Date();
+// const START_DATE = getDateInPast(TODAY, TIME_PERIOD_IN_DAYS);
+// const DATE_RANGE = getDatesInRange(START_DATE, TODAY).map(ymd);
 
 export default {
   name: 'UsersChart',
   components: { GlLineChart, ChartSkeletonLoader },
   data() {
     return {
-      usersCount: [],
+      usersDailyCount: [],
+      pageInfo: null,
     };
   },
-  updated() {
-    console.log('data', this.data);
-  },
   apollo: {
-    usersCount: {
+    usersDailyCount: {
       query: latestUsersQuery,
+      variables: {
+        first: FETCH_LIMIT,
+        after: null,
+      },
       update(data) {
-        console.log('apollo::update::data', data);
-        console.log('apollo::update::data.users.nodes', data.users.nodes);
-        // return Object.entries(data).map(([key, obj]) => {
-        //   const label = this.$options.i18n.labels[key];
-        //   const formatter = getFormatter(SUPPORTED_FORMATS.number);
-        //   const value = obj.nodes?.length ? formatter(obj.nodes[0].count, defaultPrecision) : null;
-
-        //   return {
-        //     key,
-        //     value,
-        //     label,
-        //   };
-        // });
         // TODO: might need to cache data, so that we can add 0 values
-        return data.users.nodes.map(({ count, recordedAt }) => [
-          formatDate(recordedAt, 'yyyy-mm-dd'),
-          count,
-        ]);
+        return data.users.nodes.map(({ count, recordedAt }) => [ymd(recordedAt), count]);
+      },
+      result({ data }) {
+        const {
+          users: { pageInfo },
+        } = data;
+        this.pageInfo = pageInfo;
+        this.fetchNextPage();
       },
       error(error) {
-        createFlash(this.$options.i18n.loadCountsError);
-        Sentry.captureException(error);
+        createFlash({ message: this.$options.i18n.loadUserChartError, captureError: true, error });
       },
     },
   },
   i18n: {
-    // labels: {
-    //   users: s__('InstanceStatistics|Users'),
-    //   projects: s__('InstanceStatistics|Projects'),
-    //   groups: s__('InstanceStatistics|Groups'),
-    //   issues: s__('InstanceStatistics|Issues'),
-    //   mergeRequests: s__('InstanceStatistics|Merge Requests'),
-    //   pipelines: s__('InstanceStatistics|Pipelines'),
-    // },
-    // loadCountsError: s__('Could not load instance counts. Please refresh the page to try again.'),
+    loadUserChartError: __('Could not load the user chart. Please refresh the page to try again.'),
   },
   computed: {
     isLoading() {
-      return this.$apollo.queries.usersCount.loading;
+      // Dont show the chart until all data is fetched
+      return (
+        this.$apollo.queries.usersDailyCount.loading || this.usersDailyCount.length < FETCH_LIMIT
+      );
     },
     range() {
-      return { min: this.usersCount[0][0], max: this.usersCount[this.usersCount.length - 1][0] };
+      return {
+        min: this.usersDailyCount[0][0],
+        max: this.usersDailyCount[this.usersDailyCount.length - 1][0],
+      };
     },
     options() {
       return {
         xAxis: {
           // ...this.range,
-          name: 'Date',
+          name: __('Date'),
           type: 'category',
         },
         yAxis: {
-          name: 'Users',
+          name: __('Users'),
         },
       };
+    },
+    chartUserData() {
+      // TODO: this is temporary until we can sort via the query
+      return reverse(this.usersDailyCount, 'recordedAt');
+    },
+  },
+  methods: {
+    fetchNextPage() {
+      if (this.pageInfo.hasNextPage) {
+        this.$apollo.queries.usersDailyCount.fetchMore({
+          variables: { first: FETCH_LIMIT, after: this.pageInfo.endCursor },
+          updateQuery: (previousResult, { fetchMoreResult }) => {
+            const results = produce(fetchMoreResult, newUsers => {
+              // eslint-disable-next-line no-param-reassign
+              newUsers.users.nodes = [...previousResult.users.nodes, ...newUsers.users.nodes];
+            });
+            return results;
+          },
+        });
+      }
     },
   },
 };
@@ -93,7 +117,7 @@ export default {
     :data="[
       {
         name: 'Users',
-        data: usersCount,
+        data: chartUserData,
       },
     ]"
   />
