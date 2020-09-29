87df5cf2fb1b4001fa18e72ff265fa9475544e29
Chad Woolley <cwoolley@gitlab.com>
2020-09-28 19:34:24 -0700

    [CI SKIP] WIP - controller spec for nested data attributes

--- spec/controllers/projects/static_site_editor_controller_spec.rb
+++ spec/controllers/projects/static_site_editor_controller_spec.rb
@@ -52,42 +52,57 @@ RSpec.describe Projects::StaticSiteEditorController do
         end
       end
 
-      %w[developer maintainer].each do |role|
-        context "as #{role}" do
-          before_all do
-            project.add_role(user, role)
+      context "as developer" do
+        before_all do
+          project.add_role(user, 'developer')
+        end
+
+        before do
+          sign_in(user)
+          get :show, params: default_params
+        end
+
+        it 'renders the edit page' do
+          expect(response).to render_template(:show)
+        end
+
+        it 'assigns a required variables' do
+          expect(assigns(:data)).to eq(data)
+          expect(assigns(:ref)).to eq('master')
+          expect(assigns(:path)).to eq('README.md')
+        end
+
+        context 'when combination of ref and path is incorrect' do
+          let(:default_params) { super().merge(id: 'unknown') }
+
+          it 'responds with 404 page' do
+            expect(response).to have_gitlab_http_status(:not_found)
+          end
+        end
+
+        context 'when invalid config file' do
+          let(:service_response) { ServiceResponse.error(message: 'invalid') }
+
+          it 'redirects to project page and flashes error message' do
+            expect(response).to redirect_to(project_path(project))
+            expect(response).to set_flash[:alert].to('invalid')
+          end
+        end
+
+        context 'with a deeply nested service response payload' do
+          let(:data) do
+            {
+              mounts: [
+                {
+                  source: 'source',
+                  target: 'target'
+                }
+              ]
+            }
           end
 
-          before do
-            sign_in(user)
-            get :show, params: default_params
-          end
-
-          it 'renders the edit page' do
-            expect(response).to render_template(:show)
-          end
-
-          it 'assigns a required variables' do
-            expect(assigns(:data)).to eq(data)
-            expect(assigns(:ref)).to eq('master')
-            expect(assigns(:path)).to eq('README.md')
-          end
-
-          context 'when combination of ref and path is incorrect' do
-            let(:default_params) { super().merge(id: 'unknown') }
-
-            it 'responds with 404 page' do
-              expect(response).to have_gitlab_http_status(:not_found)
-            end
-          end
-
-          context 'when invalid config file' do
-            let(:service_response) { ServiceResponse.error(message: 'invalid') }
-
-            it 'redirects to project page and flashes error message' do
-              expect(response).to redirect_to(project_path(project))
-              expect(response).to set_flash[:alert].to('invalid')
-            end
+          it 'properly converts to JSON in the view' do
+            puts response.body
           end
         end
       end
