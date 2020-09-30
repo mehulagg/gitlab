99d9c574f798dd8962bb02421c967e59620cfb3b
sstern <sstern@gitlab.com>
2020-09-30 14:53:37 -0700

    Add multiselect wrapper

---
+++ app/assets/javascripts/vue_shared/components/sidebar/assignees_dropdown.vue
@@ -0,0 +1,29 @@
+<script>
+import { BDropdownForm } from 'bootstrap-vue';
+import { GlDropdown } from '@gitlab/ui';
+
+export default {
+  components: {
+    BDropdownForm,
+    GlDropdown,
+  },
+  props: {
+    headerText: {
+      type: String,
+      required: true,
+    },
+    text: {
+      type: String,
+      required: true,
+    },
+  },
+};
+</script>
+
+<template>
+  <gl-dropdown class="show" :text="text" :header-text="headerText">
+    <b-dropdown-form>
+      <slot name="items"></slot>
+    </b-dropdown-form>
+  </gl-dropdown>
+</template>
