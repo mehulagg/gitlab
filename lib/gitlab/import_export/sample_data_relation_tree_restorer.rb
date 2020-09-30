133ba61a827adc6a3579f459f942a643ad86a63b
Grant Young <gyoung@gitlab.com>
2020-09-30 15:25:37 +0100

    Fix due date modification

--- lib/gitlab/import_export/sample_data_relation_tree_restorer.rb
+++ lib/gitlab/import_export/sample_data_relation_tree_restorer.rb
@@ -14,7 +14,7 @@ module Gitlab
       end
 
       def override_due_date_attributes!(relation_key, data_hash)
-        return unless RelationTreeRestorer::DUE_DATE_MODELS.include?(relation_key)
+        return unless DUE_DATE_MODELS.include?(relation_key.to_sym) && !data_hash['due_date'].nil?
 
         data_hash['due_date'] = due_date_calculator.calculate(data_hash['due_date'].to_time)
       end
@@ -24,10 +24,10 @@ module Gitlab
 
         unless relation_reader.legacy?
           due_dates += DUE_DATE_MODELS.map do |tag|
-            relation_reader.consume_relation(importable_path, tag).map{ |model| model.first['due_date'] }.tap do
+            relation_reader.consume_relation(@importable_path, tag).map { |model| model.first['due_date'] }.tap do
               # Hack to clear consumed relations, so we could read them again
               consumed_relations = relation_reader.instance_variable_get(:@consumed_relations)
-              consumed_relations.delete("#{importable_path}/#{tag}")
+              consumed_relations.delete("#{@importable_path}/#{tag}")
             end
           end
           due_dates.flatten!
