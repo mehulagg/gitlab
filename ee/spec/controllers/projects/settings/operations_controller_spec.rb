02b0432d648f470c697104c12e2b9cdf64bc9759
Sean Arnold <sarnold@gitlab.com>
2020-10-01 13:38:16 +1300

    Add EE changes to controller for SLA
    - Update specs

--- ee/spec/controllers/projects/settings/operations_controller_spec.rb
+++ ee/spec/controllers/projects/settings/operations_controller_spec.rb
@@ -115,7 +115,31 @@ RSpec.describe Projects::Settings::OperationsController do
 
     context 'with a license' do
       before do
-        stub_licensed_features(status_page: true)
+        stub_licensed_features(status_page: true, incident_sla: true)
+      end
+
+      shared_examples 'user with write access' do |project_visibility|
+        let(:project) { create(:project, project_visibility) }
+        let(:tracing_url) { 'https://gitlab.com' }
+
+        before do
+          project.add_maintainer(user)
+        end
+
+        it 'creates tracing setting' do
+          update_project(
+            project,
+            tracing_params: { external_url: tracing_url }
+          )
+
+          expect(project.tracing_setting.external_url).to eq(tracing_url)
+        end
+      end
+
+      context 'with maintainer role' do
+        it_behaves_like 'user with write access', :public
+        it_behaves_like 'user with write access', :private
+        it_behaves_like 'user with write access', :internal
       end
 
       context 'with non maintainer roles' do
@@ -181,16 +205,90 @@ RSpec.describe Projects::Settings::OperationsController do
           expect(project.status_page_setting).to be_nil
         end
       end
+
+      context 'indident management settings' do
+        let(:project) { create(:project) }
+
+        let(:params) { attributes_for(:project_incident_management_setting) }
+
+        subject(:incident_management_setting) do
+          update_project(project, incident_management_params: params)
+
+          project.incident_management_setting
+        end
+
+        before do
+          project.add_maintainer(user)
+        end
+
+        shared_examples 'can set the sla timer settings' do
+          let(:sla_settings) do
+            {
+              sla_timer: 'true',
+              sla_timer_minutes: 60
+            }
+          end
+
+          before do
+            params.merge!(sla_settings)
+          end
+
+          it 'updates the sla settings' do
+            setting = incident_management_setting
+
+            expect(setting.sla_timer).to eq(true)
+            expect(setting.sla_timer_minutes).to eq(60)
+          end
+
+          it_behaves_like 'an incident management gitlab tracking event', { sla_timer: '1' }, 'enabled_sla_timer'
+          it_behaves_like 'an incident management gitlab tracking event', { sla_timer: '0' }, 'disabled_sla_timer'
+        end
+
+        context 'without existing incident management setting' do
+          it { is_expected.to be_a(IncidentManagement::ProjectIncidentManagementSetting) }
+
+          it_behaves_like 'can set the sla timer settings'
+        end
+
+        context 'with existing incident management setting' do
+          before do
+            update_project(project, incident_management_params: params)
+          end
+
+          it { is_expected.to be_a(IncidentManagement::ProjectIncidentManagementSetting) }
+
+          it_behaves_like 'can set the sla timer settings'
+        end
+      end
     end
 
     context 'without a license' do
+      let(:project) { create(:project) }
+
       before do
-        stub_licensed_features(status_page: false)
+        project.add_maintainer(user)
+        stub_licensed_features(status_page: false, incident_sla: false)
       end
 
       it_behaves_like 'user without write access', :public, :maintainer
       it_behaves_like 'user without write access', :private, :maintainer
       it_behaves_like 'user without write access', :internal, :maintainer
+
+      it 'cannot update sla timer settings', :aggregate_failures do
+        default_attributes = attributes_for(:project_incident_management_setting)
+
+        sla_settings = {
+          sla_timer: 'true',
+          sla_timer_minutes: 60
+        }
+
+        update_project(project, incident_management_params: default_attributes.merge(sla_settings) )
+
+        setting = project.incident_management_setting
+
+        expect(setting.sla_timer).to eq(default_attributes[:sla_timer])
+        expect(setting.sla_timer_minutes).to eq(default_attributes[:sla_timer_minutes])
+      end
     end
 
     private
@@ -198,6 +296,11 @@ RSpec.describe Projects::Settings::OperationsController do
     def update_project(project, status_page_params: nil)
       patch :update, params: project_params(
         project,
+<<<<<<< HEAD
+=======
+        tracing_params: tracing_params,
+        incident_management_setting_attributes: incident_management_params,
+>>>>>>> 481c2a19df5... Add EE changes to controller for SLA
         status_page_params: status_page_params
       )
 
@@ -207,11 +310,20 @@ RSpec.describe Projects::Settings::OperationsController do
 
   private
 
+<<<<<<< HEAD
   def project_params(project, status_page_params: nil)
+=======
+  def project_params(project, tracing_params: nil, incident_management_setting_attributes: nil, status_page_params: nil)
+>>>>>>> 481c2a19df5... Add EE changes to controller for SLA
     {
       namespace_id: project.namespace,
       project_id: project,
       project: {
+<<<<<<< HEAD
+=======
+        tracing_setting_attributes: tracing_params,
+        incident_management_setting_attributes: incident_management_setting_attributes,
+>>>>>>> 481c2a19df5... Add EE changes to controller for SLA
         status_page_setting_attributes: status_page_params
       }
     }
