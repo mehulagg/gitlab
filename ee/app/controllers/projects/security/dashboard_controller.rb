260942dfeec3ded5c36abaaaba598046ec57db5f
Lindsay Kerr <lkerr@gitlab.com>
2020-10-02 20:44:26 +0000

    Remove unused code & yml file

--- ee/app/controllers/projects/security/dashboard_controller.rb
+++ ee/app/controllers/projects/security/dashboard_controller.rb
@@ -7,9 +7,6 @@ module Projects
 
       alias_method :vulnerable, :project
 
-      before_action only: [:index] do
-        push_frontend_feature_flag(:hide_dismissed_vulnerabilities)
-      end
     end
   end
 end
