58a54d1473e27d3de751633f0e28034b67208cb9
ggelatti <ggelatti@gitlab.com>
2020-10-04 20:36:56 +0000

    Use backticks for gp execution

--- config/initializers/rails_host_authorization_gitpod.rb
+++ config/initializers/rails_host_authorization_gitpod.rb
@@ -1,4 +1,4 @@
 if Rails.env.development? && ENV['GITPOD_WORKSPACE_ID'].present?
-  gitpod_host = URI(%x(gp url 3000).strip).host
+  gitpod_host = URI(`gp url 3000`.strip).host
   Rails.application.config.hosts += [gitpod_host]
 end
