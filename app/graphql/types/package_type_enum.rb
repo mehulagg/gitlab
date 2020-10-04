eba54a772aab2ceef61c65b7925473140ebc93ba
ggelatti <ggelatti@gitlab.com>
2020-10-04 18:38:19 +0000

    Add dict to package name in docs

--- app/graphql/types/package_type_enum.rb
+++ app/graphql/types/package_type_enum.rb
@@ -2,8 +2,14 @@
 
 module Types
   class PackageTypeEnum < BaseEnum
+    PACKAGE_TYPE_NAMES = {
+      pypi: 'PyPi',
+      npm: 'NPM'
+    }.freeze
+
     ::Packages::Package.package_types.keys.each do |package_type|
-      value package_type.to_s.upcase, "Packages from the #{package_type} package manager", value: package_type.to_s
+      type_name = PACKAGE_TYPE_NAMES[package_type.to_sym] || package_type.capitalize
+      value package_type.to_s.upcase, "Packages from the #{type_name} package manager", value: package_type.to_s
     end
   end
 end
