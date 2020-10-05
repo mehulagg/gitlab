be544b4e47f5540611cb8f7b2fc8630ca53d2299
Mehmet Emin INAC <minac@gitlab.com>
2020-10-05 19:58:25 +0300

    Add `by_build_ids` scope to Security::Finding model

--- ee/spec/models/security/finding_spec.rb
+++ ee/spec/models/security/finding_spec.rb
@@ -12,4 +12,13 @@ RSpec.describe Security::Finding do
     it { is_expected.to validate_presence_of(:project_fingerprint) }
     it { is_expected.to validate_length_of(:project_fingerprint).is_at_most(40) }
   end
+
+  describe '.by_build_ids' do
+    let!(:finding_1) { create(:security_finding) }
+    let!(:finding_2) { create(:security_finding) }
+
+    subject { described_class.by_build_ids(finding_1.scan.build_id) }
+
+    it { is_expected.to eq([finding_1]) }
+  end
 end
