8b99ac9fc1c6136a3ccc342c55b3faee6e23cb68
Tristan Read <tread@gitlab.com>
2020-10-06 15:51:17 +1300

    Add incident SLA to higlight bar
    - update components

--- app/assets/javascripts/issue_show/components/incidents/incident_tabs.vue
+++ app/assets/javascripts/issue_show/components/incidents/incident_tabs.vue
@@ -1,5 +1,6 @@
 <script>
 import { GlTab, GlTabs } from '@gitlab/ui';
+import IncidentSla from 'ee_else_ce/issue_show/components/incidents/incident_sla.vue';
 import DescriptionComponent from '../description.vue';
 import HighlightBar from './highlight_bar.vue';
 import createFlash from '~/flash';
@@ -15,6 +16,7 @@ export default {
     GlTab,
     GlTabs,
     HighlightBar,
+    IncidentSla,
   },
   inject: ['fullPath', 'iid'],
   apollo: {
@@ -53,7 +55,11 @@ export default {
   <div>
     <gl-tabs content-class="gl-reset-line-height" class="gl-mt-n3" data-testid="incident-tabs">
       <gl-tab :title="s__('Incident|Summary')">
-        <highlight-bar v-if="alert" :alert="alert" />
+        <highlight-bar v-if="alert" :alert="alert">
+          <template #incident-sla>
+            <incident-sla />
+          </template>
+        </highlight-bar>
         <description-component v-bind="$attrs" />
       </gl-tab>
       <gl-tab v-if="alert" class="alert-management-details" :title="s__('Incident|Alert details')">
