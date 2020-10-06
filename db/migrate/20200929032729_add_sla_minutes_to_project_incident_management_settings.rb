311d4a550b0c5200c263168ff7cdff422987029d
Sean Arnold <sarnold@gitlab.com>
2020-10-07 07:30:24 +1300

    Use change in migration, add new changelog

--- db/migrate/20200929032729_add_sla_minutes_to_project_incident_management_settings.rb
+++ db/migrate/20200929032729_add_sla_minutes_to_project_incident_management_settings.rb
@@ -3,13 +3,8 @@
 class AddSlaMinutesToProjectIncidentManagementSettings < ActiveRecord::Migration[6.0]
   DOWNTIME = false
 
-  def up
+  def change
     add_column :project_incident_management_settings, :sla_timer, :boolean, default: false
     add_column :project_incident_management_settings, :sla_timer_minutes, :integer
-  end
-
-  def down
-    remove_column :project_incident_management_settings, :sla_timer
-    remove_column :project_incident_management_settings, :sla_timer_minutes
   end
 end
