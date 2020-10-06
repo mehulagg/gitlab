991314a1ab2a04ed5fc69116fb756be89e01995b
Stan Hu <stanhu@gmail.com>
2020-10-06 15:07:17 -0700

    Update to Rack v2.1.4
    This is needed to address a few outstanding CVEs and fix cookie
    timestamp formats.
    
    Full list of changes:
    https://github.com/rack/rack/compare/2.0.9..2.1.4
    
    Relates to:
    * https://gitlab.com/gitlab-org/gitlab/-/issues/36362
    * https://gitlab.com/gitlab-org/gitlab/-/issues/228622

--- spec/lib/gitlab/middleware/go_spec.rb
+++ spec/lib/gitlab/middleware/go_spec.rb
@@ -176,10 +176,11 @@ RSpec.describe Gitlab::Middleware::Go do
 
           it 'returns 404' do
             response = go
+
             expect(response[0]).to eq(404)
             expect(response[1]['Content-Type']).to eq('text/html')
             expected_body = %{<html><body>go get #{Gitlab.config.gitlab.url}/#{project.full_path}</body></html>}
-            expect(response[2].body).to eq([expected_body])
+            expect(response[2]).to eq([expected_body])
           end
         end
 
@@ -251,7 +252,7 @@ RSpec.describe Gitlab::Middleware::Go do
       expect(response[0]).to eq(200)
       expect(response[1]['Content-Type']).to eq('text/html')
       expected_body = %{<html><head><meta name="go-import" content="#{Gitlab.config.gitlab.host}/#{path} git #{repository_url}" /><meta name="go-source" content="#{Gitlab.config.gitlab.host}/#{path} #{project_url} #{project_url}/-/tree/#{branch}{/dir} #{project_url}/-/blob/#{branch}{/dir}/{file}#L{line}" /></head><body>go get #{Gitlab.config.gitlab.url}/#{path}</body></html>}
-      expect(response[2].body).to eq([expected_body])
+      expect(response[2]).to eq([expected_body])
     end
   end
 end
