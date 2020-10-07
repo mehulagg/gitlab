b824815e130914efb928a322328ea300e3717398
Tetiana Chupryna <tchupryna@gitlab.com>
2020-10-07 14:10:15 +0300

    Create auto-fix MRs

--- ee/app/models/project_security_setting.rb
+++ ee/app/models/project_security_setting.rb
@@ -10,4 +10,9 @@ class ProjectSecuritySetting < ApplicationRecord
   rescue ActiveRecord::RecordNotUnique
     retry
   end
+
+  def auto_fix_enabled
+    auto_fix = [ :auto_fix_container_scanning, :auto_fix_dast, :auto_fix_dependency_scanning, :auto_fix_sast ]
+    auto_fix.filter { |setting| !!setting }
+  end
 end
