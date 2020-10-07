2cbfa10ac51faa211378d9afe1825892f7bcd8fe
Simon Knox <simon@gitlab.com>
2020-10-07 18:25:24 +1100

    Add another helper spec
    For show_burndown_placeholder

--- ee/spec/helpers/timeboxes_helper_spec.rb
+++ ee/spec/helpers/timeboxes_helper_spec.rb
@@ -3,6 +3,41 @@
 require 'spec_helper'
 
 RSpec.describe TimeboxesHelper do
+  describe '#show_burndown_placeholder?' do
+    let(:user) { build(:user) }
+    subject { helper.show_burndown_placeholder?(milestone) }
+
+    before do
+      allow(helper).to receive(:current_user).and_return(user)
+    end
+
+    describe('milestone does not support burndown charts') do
+      let(:milestone) { double('Milestone', supports_milestone_charts?: false) }
+
+      it { is_expected.to be false }
+    end
+
+    describe('user without permission') do
+      let(:milestone) { double('Milestone', supports_milestone_charts?: true, resource_parent: 'board') }
+
+      before do
+        expect(helper).to receive(:can?).with(user, :admin_milestone, milestone.resource_parent) { false }
+      end
+
+      it { is_expected.to be false }
+    end
+
+    describe('user with permission') do
+      let(:milestone) { double('Milestone', supports_milestone_charts?: true, resource_parent: 'board') }
+
+      before do
+        expect(helper).to receive(:can?).with(user, :admin_milestone, milestone.resource_parent) { true }
+      end
+
+      it { is_expected.to be true }
+    end
+  end
+
   describe '#legacy_milestone?' do
     subject { legacy_milestone?(milestone) }
 
