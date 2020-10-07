c75f0c06034d937039940517a067ff283fd816c7
Craig Smith <5344211-craigmsmith@users.noreply.gitlab.com>
2020-10-07 13:35:06 +1100

    Fix sast errors

--- ee/app/graphql/types/dast_site_validation_type.rb
+++ ee/app/graphql/types/dast_site_validation_type.rb
@@ -1,20 +1,18 @@
 # frozen_string_literal: true
 
 module Types
-    class DastSiteValidationType < BaseObject
-      graphql_name 'DastSiteValidation'
-      description 'Represents a DAST Site Validation'
-  
-      authorize :create_on_demand_dast_scan
-  
-      field :id, ::Types::GlobalIDType[::DastSiteValidation], null: false,
-            description: 'ID of the site validation'
-  
-    #   field :status, ::Types::DastSiteProfileValidationStatusEnum, null: false,
-      field :status, GraphQL::STRING_TYPE, null: false,
-            description: 'The status of the validation',
-            resolve: -> (obj, _args, _ctx) { obj.state }
-  
-    end
+  class DastSiteValidationType < BaseObject
+    graphql_name 'DastSiteValidation'
+    description 'Represents a DAST Site Validation'
+
+    authorize :create_on_demand_dast_scan
+
+    field :id, ::Types::GlobalIDType[::DastSiteValidation], null: false,
+          description: 'ID of the site validation'
+
+    # field :status, ::Types::DastSiteProfileValidationStatusEnum, null: false,
+    field :status, GraphQL::STRING_TYPE, null: false,
+          description: 'The status of the validation',
+          resolve: -> (obj, _args, _ctx) { obj.state }
   end
-  
+end
