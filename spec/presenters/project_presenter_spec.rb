4177619d374bef65b2cbaf839863e112c1e9aa69
Alex Kalderimis <akalderimis@gitlab.com>
2020-10-07 17:58:11 +0100

    Update presenter specs
    We need to pretend in some circumstances that the wiki exists.

--- spec/presenters/project_presenter_spec.rb
+++ spec/presenters/project_presenter_spec.rb
@@ -38,7 +38,8 @@ RSpec.describe ProjectPresenter do
       context 'when repository is empty' do
         let_it_be(:project) { create(:project_empty_repo, :public) }
 
-        it 'returns wiki if user has repository access and can read wiki' do
+        it 'returns wiki if user has repository access and can read wiki, which exists' do
+          allow(project).to receive(:wiki_repository_exists?).and_return(true)
           allow(presenter).to receive(:can?).with(nil, :download_code, project).and_return(true)
           allow(presenter).to receive(:can?).with(nil, :read_wiki, project).and_return(true)
           allow(presenter).to receive(:can?).with(nil, :read_issue, project).and_return(false)
@@ -46,10 +47,26 @@ RSpec.describe ProjectPresenter do
           expect(presenter.default_view).to eq('wiki')
         end
 
-        it 'returns wiki if user does not have repository access' do
-          allow(project).to receive(:can?).with(nil, :download_code, project).and_return(false)
+        it 'returns activity if user has repository access and can read wiki, which does not exist' do
+          allow(presenter).to receive(:can?).with(nil, :download_code, project).and_return(true)
+          allow(presenter).to receive(:can?).with(nil, :read_wiki, project).and_return(true)
+          allow(presenter).to receive(:can?).with(nil, :read_issue, project).and_return(false)
 
-          expect(presenter.default_view).to eq('wiki')
+          expect(presenter.default_view).to eq('activity')
+        end
+
+        it 'returns wiki if user does not have repository access, but can read issues' do
+          allow(presenter).to receive(:can?).with(nil, :download_code, project).and_return(false)
+          allow(presenter).to receive(:can?).with(nil, :read_issue, project).and_call_original
+
+          expect(presenter.default_view).to eq('projects/issues/issues')
+        end
+
+        it 'returns activity if user can read neither wiki nor issues' do
+          allow(presenter).to receive(:can?).with(nil, :download_code, project).and_return(false)
+          allow(presenter).to receive(:can?).with(nil, :read_issue, project).and_return(false)
+
+          expect(presenter.default_view).to eq('activity')
         end
       end
 
@@ -63,7 +80,8 @@ RSpec.describe ProjectPresenter do
           expect(presenter.default_view).to eq('files')
         end
 
-        it 'returns wiki if user does not have repository access and can read wiki' do
+        it 'returns wiki if user does not have repository access and can read wiki, which exists' do
+          allow(project).to receive(:wiki_repository_exists?).and_return(true)
           allow(presenter).to receive(:can?).with(nil, :download_code, project).and_return(false)
           allow(presenter).to receive(:can?).with(nil, :read_wiki, project).and_return(true)
 
@@ -107,17 +125,18 @@ RSpec.describe ProjectPresenter do
           allow(presenter).to receive(:can?).with(user, :download_code, project).and_return(false)
         end
 
-        it 'returns wiki if the user has the right policy' do
+        it 'returns wiki if the user has the right policy and the wiki exists' do
+          allow(project).to receive(:wiki_repository_exists?).and_return(true)
           allow(presenter).to receive(:can?).with(user, :read_wiki, project).and_return(true)
 
           expect(presenter.default_view).to eq('wiki')
         end
 
-        it 'returns customize_workflow if the user does not have the right policy' do
+        it 'returns activity if the user does not have the right policy' do
           allow(presenter).to receive(:can?).with(user, :read_wiki, project).and_return(false)
           allow(presenter).to receive(:can?).with(user, :read_issue, project).and_return(false)
 
-          expect(presenter.default_view).to eq('customize_workflow')
+          expect(presenter.default_view).to eq('activity')
         end
       end
 
