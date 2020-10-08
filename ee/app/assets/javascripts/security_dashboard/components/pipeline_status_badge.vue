420b103e646aa7a6e85ecc07fa55d25b2db34702
Savas Vedova <svedova@gitlab.com>
2020-10-08 14:17:27 +0300

    Prepare a pipeline status badge
    - The badge will be used in a later iteration.
    - Write tests

---
+++ ee/app/assets/javascripts/security_dashboard/components/pipeline_status_badge.vue
@@ -0,0 +1,24 @@
+<script>
+import { GlBadge, GlIcon } from '@gitlab/ui';
+
+export default {
+  components: { GlBadge, GlIcon },
+  inject: {
+    pipelineSecurityBuildsFailedCount: { type: Number, required: false, default: 0 },
+    pipelineSecurityBuildsFailedPath: { type: String, required: false, default: '' },
+  },
+};
+</script>
+
+<template>
+  <gl-badge
+    v-if="pipelineSecurityBuildsFailedCount > 0"
+    variant="danger"
+    :href="pipelineSecurityBuildsFailedPath"
+  >
+    <gl-icon name="status_failed" class="gl-mr-2" />
+    {{
+      n__('%d failed security job', '%d failed security jobs', pipelineSecurityBuildsFailedCount)
+    }}
+  </gl-badge>
+</template>
