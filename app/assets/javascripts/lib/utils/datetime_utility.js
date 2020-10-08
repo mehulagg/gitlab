704c47764684d48fece456f1b60c75f3002e482a
mlunoe <michael.lunoe@gmail.com>
2020-10-08 15:14:36 +0200

    Feat(datetime_utility): add formatDateAsMonth
    Add function to format a date as a month string
    to be used in charts displaying monthly data,
    specifically for the instance analytics, but this
    should be useble elsewhere.

--- app/assets/javascripts/lib/utils/datetime_utility.js
+++ app/assets/javascripts/lib/utils/datetime_utility.js
@@ -86,6 +86,16 @@ export const getDayName = date =>
   ][date.getDay()];
 
 /**
+ * Returns the i18n month name from a given date
+ * @param  {String} datetime where month is extracted from
+ * @return {String} the i18n month name
+ */
+export function formatDateAsMonth(datetime) {
+  const month = new Date(datetime).getMonth();
+  return getMonthNames(true)[month];
+}
+
+/**
  * @example
  * dateFormat('2017-12-05','mmm d, yyyy h:MMtt Z' ) -> "Dec 5, 2017 12:00am GMT+0000"
  * @param {date} datetime
