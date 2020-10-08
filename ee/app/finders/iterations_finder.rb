7c2df450020914ccaff04b5083c24ee63ef48f7a
Heinrich Lee Yu <heinrich@gitlab.com>
2020-10-08 12:31:42 +0800

    Add REST API for listing iterations
    This will be used for the filter bar autocomplete

--- ee/app/finders/iterations_finder.rb
+++ ee/app/finders/iterations_finder.rb
@@ -15,6 +15,24 @@ class IterationsFinder
 
   attr_reader :params, :current_user
 
+  class << self
+    def params_for_parent(parent, include_ancestors: false)
+      if parent.is_a?(Group)
+        if include_ancestors
+          { group_ids: parent.self_and_ancestors.select(:id) }
+        else
+          { group_ids: parent.id }
+        end
+      elsif parent.is_a?(Project)
+        if include_ancestors && parent.parent_id.present?
+          { group_ids: parent.parent.self_and_ancestors.select(:id), project_ids: parent.id }
+        else
+          { project_ids: parent.id }
+        end
+      end
+    end
+  end
+
   def initialize(current_user, params = {})
     @params = params
     @current_user = current_user
