1a4ca1c78a707ef48e8d58a8437d798cdd77469e
Nicolas Dular <ndular@gitlab.com>
2020-10-08 12:24:30 +0200

    Fix Snowplow spec helper
    With Ruby 2.6 using `**kwargs` here will not work because it would be an
    empty hash that we then expect to be submitted to
    `Gitlab::Tracking#event`.
    
    An example when using `expect_snowplow_event`:
    
    ```
    expect_snowplow_event(
      category: 'cluster:services:prometheus',
      action: 'enabled_manual_prometheus'
    )
    ```
    
    Error:
    ```
    Gitlab::Tracking received :event with unexpected arguments
     expected: ("cluster:services:prometheus", "enabled_manual_prometheus", {})
          got: ("cluster:services:prometheus", "disabled_manual_prometheus") (1 time)
    	   ("cluster:services:prometheus", "enabled_manual_prometheus") (1 time)
    ```
    
    This has not been an issue so far because the other specs we migrated
    always provided additional `kwargs`.
    
    To prevent this, we only expect the `**kwargs` parameter when it exists.
    However, this will get fixed with Ruby 2.7 again and we can remove the
    condition again.

--- spec/support/helpers/snowplow_helpers.rb
+++ spec/support/helpers/snowplow_helpers.rb
@@ -32,8 +32,13 @@ module SnowplowHelpers
   #     end
   #   end
   def expect_snowplow_event(category:, action:, **kwargs)
-    expect(Gitlab::Tracking).to have_received(:event)
-      .with(category, action, **kwargs).at_least(:once)
+    if kwargs.present?
+      expect(Gitlab::Tracking).to have_received(:event)
+        .with(category, action, **kwargs).at_least(:once)
+    else
+      expect(Gitlab::Tracking).to have_received(:event)
+        .with(category, action).at_least(:once)
+    end
   end
 
   # Asserts that no call to `Gitlab::Tracking#event` was made.
