89346b0cb3679bffa4565a150cc6496eba77f325
Vasilii Iakliushin <viakliushin@gitlab.com>
2020-10-10 17:17:00 +0200

    Add GraphQL mutation for Usage Ping events tracking

---
+++ spec/services/usage_ping/track_spec.rb
@@ -0,0 +1,35 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+RSpec.describe UsagePing::TrackService do
+  subject(:service) { described_class.new(container: event, current_user: user) }
+  let(:event) { 'static_site_editor_commit' }
+  let_it_be(:user) { create(:user) }
+
+  describe '#execute' do
+    subject { service.execute }
+
+    it 'returns a success status' do
+      expect(subject.status).to eq(:success)
+    end
+
+    it 'tracks a usage ping event' do
+      expect(Gitlab::UsageDataCounters::StaticSiteEditorCounter).to receive(:increment_commits_count)
+
+      subject
+    end
+
+    context 'when event is not supported' do
+      let(:event) { 'unknown' }
+
+      it 'returns an error status' do
+        expect(subject.status).to eq(:error)
+      end
+
+      it 'populates message' do
+        expect(subject.message).to eq('Unsupported event')
+      end
+    end
+  end
+end
