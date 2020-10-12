mehulagg/gitlab_test

Refactor ExportCsvWorker to IssuableExportCsvWorker
Amends ExportCsvWorker to allow it to use
any issuable type. Specifically MergeRequests for
exports.

app/mailers/emails/merge_requests.rb
@@ -99,6 +99,20 @@ module Emails
       mail_answer_thread(@merge_request, merge_request_thread_options(mwps_set_by_user_id, recipient_id, reason))
     end
 
+    def merge_requests_csv_email(user, project, csv_data, export_status)
+      @project = project
+      @merge_requests_count = export_status.fetch(:rows_expected)
+      @written_count = export_status.fetch(:rows_written)
+      @truncated = export_status.fetch(:truncated)
+
+      filename = "#{project.full_path.parameterize}_merge_requests_#{Date.today.iso8601}.csv"
+      attachments[filename] = { content: csv_data, mime_type: 'text/csv' }
+      mail(to: user.notification_email_for(@project.group), subject: subject("Exported merge requests")) do |format|
+        format.html { render layout: 'mailer' }
+        format.text { render layout: 'mailer' }
+      end
+    end
+
     private
 
     def setup_merge_request_mail(merge_request_id, recipient_id, present: false)

app/workers/issuable_export_csv_worker.rb
@@ -1,0 +1,53 @@
+# frozen_string_literal: true
+
+class IssuableExportCsvWorker # rubocop:disable Scalability/IdempotentWorker
+  include ApplicationWorker
+
+  feature_category :issue_tracking
+  worker_resource_boundary :cpu
+  loggable_arguments 2
+
+  PERMITTED_TYPES = [:merge_request, :issue].freeze
+
+  def perform(type, current_user_id, project_id, params)
+    @type = type.to_sym
+    check_permitted_type!
+    process_params!(params, project_id)
+
+    @current_user = User.find(current_user_id)
+    @project = Project.find(project_id)
+    @service = service(find_objects(params))
+
+    @service.email(@current_user)
+  end
+
+  private
+
+  def find_objects(params)
+    case @type
+    when :issue
+      IssuesFinder.new(@current_user, params).execute
+    when :merge_request
+      MergeRequestsFinder.new(@current_user, params).execute
+    end
+  end
+
+  def service(issues)
+    case @type
+    when :issue
+      Issues::ExportCsvService.new(issues, @project)
+    when :merge_request
+      MergeRequests::ExportCsvService.new(issues, @project)
+    end
+  end
+
+  def process_params!(params, project_id)
+    params.symbolize_keys!
+    params[:project_id] = project_id
+    params.delete(:sort)
+  end
+
+  def check_permitted_type!
+    raise ArgumentError, "type parameter must be :issue or :merge_request, it was #{@type}" unless PERMITTED_TYPES.include?(@type)
+  end
+end

spec/workers/issuable_export_csv_worker_spec.rb
@@ -1,0 +1,71 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+RSpec.describe IssuableExportCsvWorker do
+  let(:user) { create(:user) }
+  let(:project) { create(:project, creator: user) }
+  let(:params) { {} }
+
+  subject { described_class.new.perform(issuable_type, user.id, project.id, params) }
+
+  context 'when issuable type is Issue' do
+    let(:issuable_type) { :issue }
+
+    it 'emails a CSV' do
+      expect { subject }.to change(ActionMailer::Base.deliveries, :size).by(1)
+    end
+
+    it 'ensures that project_id is passed to issues_finder' do
+      expect(IssuesFinder).to receive(:new).with(anything, hash_including(project_id: project.id)).and_call_original
+
+      subject
+    end
+
+    it 'removes sort parameter' do
+      expect(IssuesFinder).to receive(:new).with(anything, hash_not_including(:sort)).and_call_original
+
+      subject
+    end
+
+    it 'calls the issue export service' do
+      allow(Issues::ExportCsvService).to receive(:new).once
+    end
+
+    context 'with params' do
+      let(:params) { { 'test_key' => true } }
+
+      it 'converts controller string keys to symbol keys for IssuesFinder' do
+        expect(IssuesFinder).to receive(:new).with(user, hash_including(test_key: true)).and_call_original
+
+        subject
+      end
+    end
+  end
+
+  context 'when issuable type is MergeRequest' do
+    let(:issuable_type) { :merge_request }
+
+    it 'emails a CSV' do
+      expect { subject }.to change(ActionMailer::Base.deliveries, :size).by(1)
+    end
+
+    it 'calls the MR export service' do
+      allow(MergeRequests::ExportCsvService).to receive(:new).with(anything, project).once.and_call_original
+
+      subject
+    end
+
+    it 'calls the MergeRequest finder' do
+      allow(MergeRequestsFinder).to receive(:new).once.and_call_original
+
+      subject
+    end
+  end
+
+  context 'when issuable type is User' do
+    let(:issuable_type) { :user }
+
+    it { expect { subject }.to raise_error(ArgumentError) }
+  end
+end

mehulagg/gitlab

app/mailers/emails/merge_requests.rb
@@ -110,6 +110,20 @@ module Emails
       mail_answer_thread(@merge_request, merge_request_thread_options(mwps_set_by_user_id, recipient_id, reason))
     end
 
+    def merge_requests_csv_email(user, project, csv_data, export_status)
+      @project = project
+      @merge_requests_count = export_status.fetch(:rows_expected)
+      @written_count = export_status.fetch(:rows_written)
+      @truncated = export_status.fetch(:truncated)
+
+      filename = "#{project.full_path.parameterize}_merge_requests_#{Date.current.iso8601}.csv"
+      attachments[filename] = { content: csv_data, mime_type: 'text/csv' }
+      mail(to: user.notification_email_for(@project.group), subject: subject("Exported merge requests")) do |format|
+        format.html { render layout: 'mailer' }
+        format.text { render layout: 'mailer' }
+      end
+    end
+
     private
 
     def setup_merge_request_mail(merge_request_id, recipient_id, present: false)

app/workers/issuable_export_csv_worker.rb
@@ -1,0 +1,53 @@
+# frozen_string_literal: true
+
+class IssuableExportCsvWorker # rubocop:disable Scalability/IdempotentWorker
+  include ApplicationWorker
+
+  feature_category :issue_tracking
+  worker_resource_boundary :cpu
+  loggable_arguments 2
+
+  PERMITTED_TYPES = [:merge_request, :issue].freeze
+
+  def perform(type, current_user_id, project_id, params)
+    @type = type.to_sym
+    check_permitted_type!
+    process_params!(params, project_id)
+
+    @current_user = User.find(current_user_id)
+    @project = Project.find(project_id)
+    @service = service(find_objects(params))
+
+    @service.email(@current_user)
+  end
+
+  private
+
+  def find_objects(params)
+    case @type
+    when :issue
+      IssuesFinder.new(@current_user, params).execute
+    when :merge_request
+      MergeRequestsFinder.new(@current_user, params).execute
+    end
+  end
+
+  def service(issuables)
+    case @type
+    when :issue
+      Issues::ExportCsvService.new(issuables, @project)
+    when :merge_request
+      MergeRequests::ExportCsvService.new(issuables, @project)
+    end
+  end
+
+  def process_params!(params, project_id)
+    params.symbolize_keys!
+    params[:project_id] = project_id
+    params.delete(:sort)
+  end
+
+  def check_permitted_type!
+    raise ArgumentError, "type parameter must be :issue or :merge_request, it was #{@type}" unless PERMITTED_TYPES.include?(@type)
+  end
+end

spec/workers/issuable_export_csv_worker_spec.rb
@@ -1,0 +1,73 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+RSpec.describe IssuableExportCsvWorker do
+  let(:user) { create(:user) }
+  let(:project) { create(:project, creator: user) }
+  let(:params) { {} }
+
+  subject { described_class.new.perform(issuable_type, user.id, project.id, params) }
+
+  context 'when issuable type is Issue' do
+    let(:issuable_type) { :issue }
+
+    it 'emails a CSV' do
+      expect { subject }.to change(ActionMailer::Base.deliveries, :size).by(1)
+    end
+
+    it 'ensures that project_id is passed to issues_finder' do
+      expect(IssuesFinder).to receive(:new).with(anything, hash_including(project_id: project.id)).and_call_original
+
+      subject
+    end
+
+    it 'removes sort parameter' do
+      expect(IssuesFinder).to receive(:new).with(anything, hash_not_including(:sort)).and_call_original
+
+      subject
+    end
+
+    it 'calls the issue export service' do
+      expect(Issues::ExportCsvService).to receive(:new).once.and_call_original
+
+      subject
+    end
+
+    context 'with params' do
+      let(:params) { { 'test_key' => true } }
+
+      it 'converts controller string keys to symbol keys for IssuesFinder' do
+        expect(IssuesFinder).to receive(:new).with(user, hash_including(test_key: true)).and_call_original
+
+        subject
+      end
+    end
+  end
+
+  context 'when issuable type is MergeRequest' do
+    let(:issuable_type) { :merge_request }
+
+    it 'emails a CSV' do
+      expect { subject }.to change(ActionMailer::Base.deliveries, :size).by(1)
+    end
+
+    it 'calls the MR export service' do
+      expect(MergeRequests::ExportCsvService).to receive(:new).with(anything, project).once.and_call_original
+
+      subject
+    end
+
+    it 'calls the MergeRequest finder' do
+      expect(MergeRequestsFinder).to receive(:new).once.and_call_original
+
+      subject
+    end
+  end
+
+  context 'when issuable type is User' do
+    let(:issuable_type) { :user }
+
+    it { expect { subject }.to raise_error(ArgumentError) }
+  end
+end