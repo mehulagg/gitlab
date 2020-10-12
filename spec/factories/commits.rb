2a25c3bb103632a63037fc9c6bb4d6be5f22bc54
Peter Leitzen <pleitzen@gitlab.com>
2020-10-13 01:41:30 +0200

    Avoid RSpec stubbing in commit factory
    Stubbing does not play well with test-prof's `let_it_be`.

--- spec/factories/commits.rb
+++ spec/factories/commits.rb
@@ -5,7 +5,8 @@ require_relative '../support/helpers/repo_helpers'
 FactoryBot.define do
   factory :commit do
     transient do
-      author { nil }
+      author { build_stubbed(:author) }
+      parent_ids { [] }
     end
 
     git_commit do
@@ -27,21 +28,14 @@ FactoryBot.define do
       new(git_commit, project)
     end
 
-    after(:build) do |commit, evaluator|
-      allow(commit).to receive(:author).and_return(evaluator.author || build_stubbed(:author))
-      allow(commit).to receive(:parent_ids).and_return([])
-    end
-
     trait :merge_commit do
-      after(:build) do |commit|
-        allow(commit).to receive(:parent_ids).and_return(Array.new(2) { SecureRandom.hex(20) })
+      parent_ids do
+        Array.new(2) { SecureRandom.hex(20) }
       end
     end
 
     trait :without_author do
-      after(:build) do |commit|
-        allow(commit).to receive(:author).and_return nil
-      end
+      author { nil }
     end
   end
 end
