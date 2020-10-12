4bcc441dcb210f5c2fa50690b8126cb0aa5ea920
Sean Arnold <sarnold@gitlab.com>
2020-10-12 19:16:59 +0000

    Apply 1 suggestion(s) to 1 file(s)

--- ee/spec/models/incident_management/incident_sla_spec.rb
+++ ee/spec/models/incident_management/incident_sla_spec.rb
@@ -3,9 +3,6 @@
 require 'spec_helper'
 
 RSpec.describe IncidentManagement::IncidentSla do
-  subject(:incident_sla) { build(:incident_sla) }
-
-  it { is_expected.to be_valid }
 
   describe 'associations' do
     it { is_expected.to belong_to(:issue).required }
