e77cdc3c5cb0e440cf4f55d702c4287ddb9395ad
Philip Cunningham <pcunningham@gitlab.com>
2020-10-13 21:35:12 +0100

    wip

--- ee/app/graphql/mutations/dast_site_validations/create.rb
+++ ee/app/graphql/mutations/dast_site_validations/create.rb
@@ -34,7 +34,37 @@ module Mutations
       authorize :create_on_demand_dast_scan
 
       def resolve(full_path:, dast_site_token_id:, validation_path:, strategy: :text_file)
-        # nothing to see here yet...
+        project = authorized_find_project!(full_path: full_path)
+        raise Gitlab::Graphql::Errors::ResourceNotAvailable, 'Feature disabled' unless allowed?(project)
+
+        dast_site_token = dast_site_token_id.find
+
+        dast_site_validation = DastSiteValidation.new(
+          dast_site_token: dast_site_token,
+          validation_strategy: strategy
+        )
+
+        if dast_site_validation.save
+          success_response(dast_site_validation)
+        else
+          error_response(dast_site_validation)
+        end
+      end
+
+      private
+
+      def allowed?(project)
+        Feature.enabled?(:security_on_demand_scans_site_validation, project)
+      end
+
+      def success_response(dast_site_validation)
+        status = "#{dast_site_validation.state}_VALIDATION".upcase
+
+        { errors: [], id: validation.to_global_id, status: status }
+      end
+
+      def error_response(dast_site_validation)
+        { errors: dast_site_validation.errors.full_messages }
       end
     end
   end
