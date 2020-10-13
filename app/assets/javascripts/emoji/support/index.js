e06dd20df23a888c32aee3686634923f128cefc1
Tim Zallmann <tzallmann@gitlab.com>
2020-10-13 17:21:31 +0200

    Fixed Prettifying of emoji bot check

--- app/assets/javascripts/emoji/support/index.js
+++ app/assets/javascripts/emoji/support/index.js
@@ -6,7 +6,11 @@ let browserUnicodeSupportMap;
 
 export default function isEmojiUnicodeSupportedByBrowser(emojiUnicode, unicodeVersion) {
   // Skipping the map creation for Bots + RSPec
-  if (navigator.userAgent.indexOf("HeadlessChrome") > -1 || navigator.userAgent.indexOf("Lighthouse") > -1 || navigator.userAgent.indexOf("Speedindex") > -1) {
+  if (
+    navigator.userAgent.indexOf('HeadlessChrome') > -1 ||
+    navigator.userAgent.indexOf('Lighthouse') > -1 ||
+    navigator.userAgent.indexOf('Speedindex') > -1
+  ) {
     return true;
   }
   browserUnicodeSupportMap = browserUnicodeSupportMap || getUnicodeSupportMap();
