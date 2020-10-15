c77e071cb06b68dc2a018384c630236648acdd35
mfluharty <mfluharty@gitlab.com>
2020-10-15 17:09:41 -0600

    Mock recent_failures with random number

--- app/serializers/test_case_entity.rb
+++ app/serializers/test_case_entity.rb
@@ -10,6 +10,7 @@ class TestCaseEntity < Grape::Entity
   expose :execution_time
   expose :system_output
   expose :stack_trace
+  expose :recent_failures
   expose :attachment_url, if: -> (*) { can_read_screenshots? } do |test_case|
     expose_url(test_case.attachment_url)
   end
@@ -17,6 +18,10 @@ class TestCaseEntity < Grape::Entity
   private
 
   alias_method :test_case, :object
+
+  def recent_failures
+    rand(5)
+  end
 
   def can_read_screenshots?
     Feature.enabled?(:junit_pipeline_screenshots_view, options[:project]) && test_case.has_attachment?
