da76fca050c0bc31d6a57b5ded614b449fbd2859
David O'Regan <doregan@gitlab.com>
2020-10-15 07:57:52 +0100

    Support incident creation as guest

--- spec/features/incidents/user_creates_new_incident_spec.rb
+++ spec/features/incidents/user_creates_new_incident_spec.rb
@@ -5,27 +5,54 @@ require 'spec_helper'
 RSpec.describe 'Incident Management index', :js do
   let_it_be(:project) { create(:project) }
   let_it_be(:developer) { create(:user) }
+  let_it_be(:guest)  { create(:user) }
   let_it_be(:incident) { create(:incident, project: project) }
 
   before_all do
     project.add_developer(developer)
-  end
-
-  before do
-    sign_in(developer)
-
-    visit project_incidents_path(project)
-    wait_for_requests
+    project.add_guest(guest)
   end
 
   context 'when a developer displays the incident list' do
+    before do
+      sign_in(developer)
+  
+      visit project_incidents_path(project)
+      wait_for_all_requests
+    end
+
     it 'shows the create new issue button' do
       expect(page).to have_selector('.create-incident-button')
     end
 
     it 'shows the create issue page with the Incident type pre-selected when clicked' do
       find('.create-incident-button').click
-      wait_for_requests
+      wait_for_all_requests
+
+      expect(page).to have_selector(".dropdown-menu-toggle")
+      expect(page).to have_selector(".js-issuable-type-filter-dropdown-wrap")
+
+      page.within('.js-issuable-type-filter-dropdown-wrap') do
+        expect(page).to have_content('Incident')
+      end
+    end
+  end
+
+  context 'when a guest displays the incident list' do
+    before do
+      sign_in(guest)
+  
+      visit project_incidents_path(project)
+      wait_for_all_requests
+    end
+
+    it 'shows the create new issue button' do
+      expect(page).to have_selector('.create-incident-button')
+    end
+
+    it 'shows the create issue page with the Incident type pre-selected when clicked' do
+      find('.create-incident-button').click
+      wait_for_all_requests
 
       expect(page).to have_selector(".dropdown-menu-toggle")
       expect(page).to have_selector(".js-issuable-type-filter-dropdown-wrap")
