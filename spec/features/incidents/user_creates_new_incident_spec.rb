d37860d77143341ea02e3496bac75fd29b60bb2a
David O'Regan <doregan@gitlab.com>
2020-10-15 11:08:19 +0100

    Update from feedback

--- spec/features/incidents/user_creates_new_incident_spec.rb
+++ spec/features/incidents/user_creates_new_incident_spec.rb
@@ -5,7 +5,7 @@ require 'spec_helper'
 RSpec.describe 'Incident Management index', :js do
   let_it_be(:project) { create(:project) }
   let_it_be(:developer) { create(:user) }
-  let_it_be(:guest)  { create(:user) }
+  let_it_be(:guest) { create(:user) }
   let_it_be(:incident) { create(:incident, project: project) }
 
   before_all do
@@ -13,24 +13,17 @@ RSpec.describe 'Incident Management index', :js do
     project.add_guest(guest)
   end
 
-  context 'when a developer displays the incident list' do
-    before do
-      sign_in(developer)
-  
-      visit project_incidents_path(project)
-      wait_for_all_requests
-    end
-
+  shared_examples 'create incident form' do
     it 'shows the create new issue button' do
       expect(page).to have_selector('.create-incident-button')
     end
 
-    it 'shows the create issue page with the Incident type pre-selected when clicked' do
+    it 'when clicked shows the create issue page with the Incident type pre-selected' do
       find('.create-incident-button').click
       wait_for_all_requests
 
-      expect(page).to have_selector(".dropdown-menu-toggle")
-      expect(page).to have_selector(".js-issuable-type-filter-dropdown-wrap")
+      expect(page).to have_selector('.dropdown-menu-toggle')
+      expect(page).to have_selector('.js-issuable-type-filter-dropdown-wrap')
 
       page.within('.js-issuable-type-filter-dropdown-wrap') do
         expect(page).to have_content('Incident')
@@ -38,28 +31,25 @@ RSpec.describe 'Incident Management index', :js do
     end
   end
 
-  context 'when a guest displays the incident list' do
+  context 'when a developer displays the incident list' do
     before do
-      sign_in(guest)
-  
+      sign_in(developer)
+
       visit project_incidents_path(project)
       wait_for_all_requests
     end
 
-    it 'shows the create new issue button' do
-      expect(page).to have_selector('.create-incident-button')
-    end
+    it_behaves_like 'create incident form'
+  end
 
-    it 'shows the create issue page with the Incident type pre-selected when clicked' do
-      find('.create-incident-button').click
+  context 'when a guest displays the incident list' do
+    before do
+      sign_in(guest)
+
+      visit project_incidents_path(project)
       wait_for_all_requests
-
-      expect(page).to have_selector(".dropdown-menu-toggle")
-      expect(page).to have_selector(".js-issuable-type-filter-dropdown-wrap")
-
-      page.within('.js-issuable-type-filter-dropdown-wrap') do
-        expect(page).to have_content('Incident')
-      end
     end
+
+    it_behaves_like 'create incident form'
   end
 end
