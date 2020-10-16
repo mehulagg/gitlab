ee782957f21b01b99cf389f8ca9c5bf15919195e
Alex Kalderimis <akalderimis@gitlab.com>
2020-10-16 15:45:07 +0100

    Fix N+1 performance issue in CI GraphQL types
    This uses a new builds resolver for Pipeline.builds to ensure
    that we don't have N+1 performance issues when fetching jobs
    from multiple pipelines.

---
+++ app/graphql/resolvers/ci/pipeline_builds_resolver.rb
@@ -0,0 +1,49 @@
+# frozen_string_literal: true
+
+module Resolvers
+  module Ci
+    class PipelineBuildsResolver < CachingArrayResolver
+      alias_method :pipeline, :object
+
+      def cache_key(status: nil)
+        [pipeline, status]
+      end
+
+      def model_class
+        ::Ci::Build
+      end
+
+      def query_for(key)
+        pipeline_builds(key.first, key.second)
+      end
+
+      def store(key, build)
+        pipeline = key.first
+        # Prevent lookups for values we already know
+        build.pipeline = pipeline
+        build.project = pipeline.project
+        super
+      end
+
+      private
+
+      def pipeline_builds(pipeline, status)
+        # NOTE: Remove the else branch once the ci_jobs_finder_refactor FF is
+        # removed. https://gitlab.com/gitlab-org/gitlab/-/issues/245183
+        # rubocop: disable CodeReuse/ActiveRecord
+        if Feature.enabled?(:ci_jobs_finder_refactor, default_enabled: true)
+          params = { scope: status }
+          ::Ci::JobsFinder
+            .new(current_user: current_user, pipeline: pipeline, params: params)
+            .execute
+        elsif can?(current_user, :read_build, pipeline)
+          builds = pipeline.builds
+          builds = builds.where(status: status) if status.present?
+          builds
+        else
+          ::Ci::Build.none
+        end
+      end
+    end
+  end
+end
