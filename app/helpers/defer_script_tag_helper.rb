3de776e69a2cc337fcba26ad9a05e40c7f715f0d
Paul Slaughter <pslaughter@gitlab.com>
2020-10-19 12:54:02 -0500

    Comment about the importance of script defer
    See [this epic][1].
    
    [1]: https://gitlab.com/groups/gitlab-org/-/epics/4538#note_432159769

--- app/helpers/defer_script_tag_helper.rb
+++ app/helpers/defer_script_tag_helper.rb
@@ -2,6 +2,12 @@
 
 module DeferScriptTagHelper
   # Override the default ActionView `javascript_include_tag` helper to support page specific deferred loading
+  # --------
+  # WARNING:
+  # The FE entrypoints are now dependent on `defer` ordering so this is critical.
+  # Otherwise, we could end up running the FE script when the DOM is not ready yet.
+  # Please see https://gitlab.com/groups/gitlab-org/-/epics/4538#note_432159769.
+  # --------
   def javascript_include_tag(*sources)
     super(*sources, defer: true)
   end
