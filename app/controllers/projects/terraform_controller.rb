1edaeb9a3b5be3bab79c6460a56d7ce54047cee8
Emily Ring <ering@gitlab.com>
2020-10-20 11:48:31 -0400

    Added terraform tab and empty html

--- app/controllers/projects/terraform_controller.rb
+++ app/controllers/projects/terraform_controller.rb
@@ -1,6 +1,15 @@
 # frozen_string_literal: true
 
 class Projects::TerraformController < Projects::ApplicationController
+  before_action :authenticate_user!
+  before_action :authorize_can_read_terraform_state!
+
   def index
+  end
+
+  private
+
+  def authorize_can_read_terraform_state!
+    access_denied! unless can?(current_user, :read_terraform_state, project)
   end
 end
