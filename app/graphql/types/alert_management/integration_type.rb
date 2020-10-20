f9517cd6de99a25cb4713ab4e23cc480f367bdea
syasonik <syasonik@gitlab.com>
2020-10-19 23:51:54 -0400

    Add GraphQL queries for alert integrations

---
+++ app/graphql/types/alert_management/integration_type.rb
@@ -0,0 +1,58 @@
+# frozen_string_literal: true
+
+module Types
+  module AlertManagement
+    module IntegrationType
+      include Types::BaseInterface
+      graphql_name 'AlertManagementIntegration'
+
+      field :id,
+            GraphQL::ID_TYPE,
+            null: true,
+            description: 'ID of the integration'
+
+      field :type,
+            AlertManagement::IntegrationTypeEnum,
+            null: true,
+            description: 'Type of integration'
+
+      field :name,
+            GraphQL::STRING_TYPE,
+            null: true,
+            description: 'Name of the integration'
+
+      field :active,
+            GraphQL::BOOLEAN_TYPE,
+            null: true,
+            description: 'Whether the endpoint is currently accepting alerts'
+
+      field :token,
+            GraphQL::STRING_TYPE,
+            null: true,
+            description: 'Token used to authenticate alert notification requests'
+
+      field :url,
+            GraphQL::STRING_TYPE,
+            null: true,
+            description: 'Endpoint which accepts alert notifications'
+
+      field :api_url,
+            GraphQL::STRING_TYPE,
+            null: true,
+            description: 'URL at which metrics can be queried'
+
+      definition_methods do
+        def resolve_type(object, context)
+          if object.is_a?(::PrometheusService)
+            Types::AlertManagement::PrometheusIntegrationType
+          else
+            Types::AlertManagement::HttpIntegrationType
+          end
+        end
+      end
+
+      orphan_types Types::AlertManagement::PrometheusIntegrationType,
+                   Types::AlertManagement::HttpIntegrationType
+    end
+  end
+end
---
+++ app/graphql/types/alert_management/prometheus_integration_type.rb
@@ -0,0 +1,36 @@
+# frozen_string_literal: true
+
+module Types
+  module AlertManagement
+    class PrometheusIntegrationType < BaseObject
+      graphql_name 'AlertManagementPrometheusIntegration'
+      description "Describes an endpoint and credentials used to accept Prometheus alerts for a project"
+
+      implements(Types::AlertManagement::IntegrationType)
+
+      authorize :admin_project
+
+      alias_method :prometheus_service, :object
+
+      def name
+        prometheus_service.title
+      end
+
+      def type
+        :prometheus
+      end
+
+      def token
+        prometheus_service.project&.alerting_setting&.token
+      end
+
+      def url
+        ::Gitlab::Routing.url_helpers.notify_project_prometheus_alerts_url(prometheus_service.project, format: :json)
+      end
+
+      def active
+        prometheus_service.manual_configuration?
+      end
+    end
+  end
+end
