e23a45d866431be555d29aef03826eea05b4cb3b
nmilojevic1 <nmilojevic@gitlab.com>
2020-10-20 14:13:05 +0200

    Fix specs to include new strategies for middleware

--- spec/lib/gitlab/sidekiq_middleware/duplicate_jobs/server_spec.rb
+++ spec/lib/gitlab/sidekiq_middleware/duplicate_jobs/server_spec.rb
@@ -3,41 +3,53 @@
 require 'spec_helper'
 
 RSpec.describe Gitlab::SidekiqMiddleware::DuplicateJobs::Server, :clean_gitlab_redis_queues do
-  let(:worker_class) do
-    Class.new do
-      def self.name
-        'TestDeduplicationWorker'
+  shared_examples 'server duplicate job' do |strategy|
+    let(:worker_class) do
+      Class.new do
+        def self.name
+          'TestDeduplicationWorker'
+        end
+
+        include ApplicationWorker
+
+        deduplicate strategy
+
+        def perform(*args)
+        end
       end
+    end
 
-      include ApplicationWorker
+    before do
+      stub_const('TestDeduplicationWorker', worker_class)
+    end
 
-      def perform(*args)
+    around do |example|
+      with_sidekiq_server_middleware do |chain|
+        chain.add described_class
+        Sidekiq::Testing.inline! { example.run }
+      end
+    end
+
+    describe '#call' do
+      it 'removes the stored job from redis' do
+        bare_job = { 'class' => 'TestDeduplicationWorker', 'args' => ['hello'] }
+        job_definition = Gitlab::SidekiqMiddleware::DuplicateJobs::DuplicateJob.new(bare_job.dup, 'test_deduplication')
+
+        expect(Gitlab::SidekiqMiddleware::DuplicateJobs::DuplicateJob)
+          .to receive(:new).with(a_hash_including(bare_job), 'test_deduplication')
+                .and_return(job_definition).twice # once in client middleware
+        expect(job_definition).to receive(:delete!).and_call_original
+
+        TestDeduplicationWorker.perform_async('hello')
       end
     end
   end
 
-  before do
-    stub_const('TestDeduplicationWorker', worker_class)
+  context 'with until_executing strategy' do
+    it_behaves_like 'server duplicate job', :until_executing
   end
 
-  around do |example|
-    with_sidekiq_server_middleware do |chain|
-      chain.add described_class
-      Sidekiq::Testing.inline! { example.run }
-    end
-  end
-
-  describe '#call' do
-    it 'removes the stored job from redis' do
-      bare_job = { 'class' => 'TestDeduplicationWorker', 'args' => ['hello'] }
-      job_definition = Gitlab::SidekiqMiddleware::DuplicateJobs::DuplicateJob.new(bare_job.dup, 'test_deduplication')
-
-      expect(Gitlab::SidekiqMiddleware::DuplicateJobs::DuplicateJob)
-        .to receive(:new).with(a_hash_including(bare_job), 'test_deduplication')
-              .and_return(job_definition).twice # once in client middleware
-      expect(job_definition).to receive(:delete!).and_call_original
-
-      TestDeduplicationWorker.perform_async('hello')
-    end
+  context 'with until_executed strategy' do
+    it_behaves_like 'server duplicate job', :until_executed
   end
 end
