2bf0100f0fa0a083266c96b33581a07ef441a1c2
Avielle Wolfe <awolfe@gitlab.com>
2020-10-21 15:37:07 -0500

    Add jobs field to Ci::PipelineType
    The field returns commit status data

--- app/graphql/types/ci/pipeline_type.rb
+++ app/graphql/types/ci/pipeline_type.rb
@@ -56,6 +56,11 @@ module Types
             description: 'Specifies if a pipeline can be canceled',
             method: :cancelable?,
             null: false
+      field :jobs,
+            ::Types::Ci::JobType.connection_type,
+            null: true,
+            description: 'Jobs belonging to the pipeline',
+            method: :statuses
     end
   end
 end
