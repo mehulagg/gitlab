0e0b5ba3f0774d56572f37ac52ac8bcac548fb51
fjsanpedro <fjlopez@gitlab.com>
2020-10-21 15:27:48 +0000

    Expand Robots.txt parser with Allow rules

--- lib/gitlab/robots_txt/parser.rb
+++ lib/gitlab/robots_txt/parser.rb
@@ -3,34 +3,47 @@
 module Gitlab
   module RobotsTxt
     class Parser
-      attr_reader :disallow_rules
+      attr_reader :disallow_rules, :allow_rules
 
       def initialize(content)
         @raw_content = content
 
-        @disallow_rules = parse_raw_content!
+        @disallow_rules, @allow_rules = parse_raw_content!
       end
 
       def disallowed?(path)
+        return false if allow_rules.any? { |rule| path =~ rule }
+
         disallow_rules.any? { |rule| path =~ rule }
       end
 
       private
+        DISALLOW_TOKEN = 'Disallow:'
+        ALLOW_TOKEN = 'Allow:'
 
-      # This parser is very basic as it only knows about `Disallow:` lines,
-      # and simply ignores all other lines.
+      # This parser is very basic as it only knows about `Disallow:`
+      # and `Allow:` lines, and simply ignores all other lines.
       #
-      # Order of predecence, 'Allow:`, etc are ignored for now.
+      # `Allow` takes precedence over `Disallow`
       def parse_raw_content!
-        @raw_content.each_line.map do |line|
-          if line.start_with?('Disallow:')
-            value = line.sub('Disallow:', '').strip
-            value = Regexp.escape(value).gsub('\*', '.*')
-            Regexp.new("^#{value}")
-          else
-            nil
+        disallowed = []
+        allowed = []
+
+        @raw_content.each_line.each do |line|
+          if line.start_with?(DISALLOW_TOKEN)
+            disallowed << get_rule(line, DISALLOW_TOKEN)
+          elsif line.start_with?(ALLOW_TOKEN)
+            allowed << get_rule(line, ALLOW_TOKEN)
           end
-        end.compact
+        end
+
+        [disallowed, allowed]
+      end
+
+      def get_rule(line, rule_type)
+        value = line.sub(rule_type, '').strip
+        value = Regexp.escape(value).gsub('\*', '.*')
+        Regexp.new("^#{value}")
       end
     end
   end
