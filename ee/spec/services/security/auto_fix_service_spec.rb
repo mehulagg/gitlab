25f48f29612bbd11c15261a1f613c757fae8c775
Tetiana Chupryna <tchupryna@gitlab.com>
2020-10-21 18:05:56 +0300

    Use proper remediations in tests

--- ee/spec/services/security/auto_fix_service_spec.rb
+++ ee/spec/services/security/auto_fix_service_spec.rb
@@ -6,8 +6,23 @@ RSpec.describe Security::AutoFixService do
   describe '#execute' do
     subject { described_class.new(project).execute(ids) }
 
-    let_it_be(:project) { create(:project, :repository) }
-    let_it_be(:vulnerability_with_rem) { create(:vulnerabilities_finding_with_remediation, report_type: :dependency_scanning, summary: "Test remediation") }
+    let(:project) { create(:project,:custom_repo, files: { 'yarn.lock' => yarn_lock_content }) }
+    let(:vulnerability_with_rem) { create(:vulnerabilities_finding_with_remediation, :yarn_remediation, report_type: :dependency_scanning, summary: "Test remediation") }
+
+    let(:remediations_folder) { Rails.root.join('ee/spec/fixtures/security_reports/remediations') }
+    let(:yarn_lock_content) do
+      File.read(
+        File.join(remediations_folder, "yarn.lock")
+      )
+    end
+
+    let(:remediation_diff) do
+      Base64.encode64(
+        File.read(
+          File.join(remediations_folder, "remediation.patch")
+        )
+      )
+    end
 
     context 'with enabled auto-fix' do
       let!(:setting) { create(:project_security_setting, project: project) }
@@ -22,6 +37,7 @@ RSpec.describe Security::AutoFixService do
 
         expect(Vulnerabilities::Feedback.count).to eq(1)
         expect(MergeRequest.count).to eq(1)
+        expect(MergeRequest.last.title).to eq('Resolve vulnerability: Cipher with no integrity')
       end
     end
   end
