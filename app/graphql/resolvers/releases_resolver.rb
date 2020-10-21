a7088bf57698be2463ba00ab6be588ffa8b68ba5
Nathan Friend <nathan@gitlab.com>
2020-10-21 10:13:06 -0400

    WIP: Need to add tests
    Need to add tests

--- app/graphql/resolvers/releases_resolver.rb
+++ app/graphql/resolvers/releases_resolver.rb
@@ -4,6 +4,10 @@ module Resolvers
   class ReleasesResolver < BaseResolver
     type Types::ReleaseType.connection_type, null: true
 
+    argument :sort, Types::ReleaseSortEnum,
+             required: false, default_value: :released_at_desc,
+             description: 'Sort releases by this criteria'
+
     alias_method :project, :object
 
     # This resolver has a custom singular resolver
@@ -11,12 +15,20 @@ module Resolvers
       Resolvers::ReleaseResolver
     end
 
-    def resolve(**args)
+    def resolve(sort:)
       return unless Feature.enabled?(:graphql_release_data, project, default_enabled: true)
+
+      sort_to_params_map = {
+        released_at_desc: { order_by: 'released_at', sort: 'desc' },
+        released_at_asc: { order_by: 'released_at', sort: 'asc' },
+        created_desc: { order_by: 'created_at', sort: 'desc' },
+        created_asc: { order_by: 'created_at', sort: 'asc' }
+      }
 
       ReleasesFinder.new(
         project,
-        current_user
+        current_user,
+        sort_to_params_map[sort]
       ).execute
     end
   end
