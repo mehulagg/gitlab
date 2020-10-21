2777769ab56945a03a809e77556840e3518076f1
Avielle Wolfe <awolfe@gitlab.com>
2020-10-21 17:08:46 -0500

    Add specs for Types::Security::ReportTypeEnum

--- ee/app/graphql/types/security/report_type_enum.rb
+++ ee/app/graphql/types/security/report_type_enum.rb
@@ -3,6 +3,8 @@
 module Types
   module Security
     class ReportTypeEnum < BaseEnum
+      graphql_name 'SecurityReportTypeEnum'
+
       ::Security::SecurityJobsFinder.allowed_job_types.each do |report_type|
         value report_type.upcase, value: report_type, description: "#{report_type.to_s.capitalize.humanize} scan report"
       end
