eef753e6f12b63fbcf61d794d73340ce857dde6e
Sean Arnold <sarnold@gitlab.com>
2020-10-22 11:36:18 +1300

    Tweak expectation for service hooks

--- spec/support/shared_examples/services/alert_management_shared_examples.rb
+++ spec/support/shared_examples/services/alert_management_shared_examples.rb
@@ -8,11 +8,9 @@ RSpec.shared_examples 'creates an alert management alert' do
   end
 
   it 'executes the alert service hooks' do
-    slack_service = create(:service, type: 'SlackService', project: project, alert_events: true, active: true)
+    expect(alert).not_to receive(:execute_services)
 
     subject
-
-    expect(ProjectServiceWorker).to have_received(:perform_async).with(slack_service.id, an_instance_of(Hash))
   end
 end
 
