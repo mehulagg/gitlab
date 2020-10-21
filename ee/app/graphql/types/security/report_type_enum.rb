3ae0a612ccf000ed9f08b49e5051caac0209a946
Avielle Wolfe <awolfe@gitlab.com>
2020-10-21 17:32:46 -0500

    Attempt to fix linting error

--- ee/app/graphql/types/security/report_type_enum.rb
+++ ee/app/graphql/types/security/report_type_enum.rb
@@ -6,7 +6,7 @@ module Types
       graphql_name 'SecurityReportTypeEnum'
 
       ::Security::SecurityJobsFinder.allowed_job_types.each do |report_type|
-        value report_type.upcase, value: report_type, description: "#{report_type.to_s.capitalize.humanize} scan report"
+        value report_type.upcase, value: report_type, description: "#{report_type.to_s.upcase.humanize} scan report"
       end
     end
   end
