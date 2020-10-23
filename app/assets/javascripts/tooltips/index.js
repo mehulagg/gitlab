820ab8308025f8c1d6402dc190e117468012092f
Enrique Alcantara <ealcantara@gitlab.com>
2020-10-23 13:29:03 -0400

    Use GlTooltips in awards_handler
    This MR replaces Bootstrap tooltip API with
    GitLab tooltips. It removes two methods are
    not referenced anywhere as well.

--- app/assets/javascripts/tooltips/index.js
+++ app/assets/javascripts/tooltips/index.js
@@ -68,7 +68,7 @@ const invokeBootstrapApi = (elements, method) => {
   }
 };
 
-const isGlTooltipsEnabled = () => Boolean(window.gon.glTooltipsEnabled);
+const isGlTooltipsEnabled = () => true;
 
 const tooltipApiInvoker = ({ glHandler, bsHandler }) => (elements, ...params) => {
   if (isGlTooltipsEnabled()) {
