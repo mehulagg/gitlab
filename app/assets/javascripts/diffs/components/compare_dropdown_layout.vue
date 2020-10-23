04244b8e089fa08f7d634156e3c72ead5341a798
nuwe1 <marvinelly18@gmail.com>
2020-10-23 12:47:40 +0300

    Apply suggestions to file

--- app/assets/javascripts/diffs/components/compare_dropdown_layout.vue
+++ app/assets/javascripts/diffs/components/compare_dropdown_layout.vue
@@ -1,10 +1,9 @@
 <script>
-import { GlIcon, GlDropdown, GlDropdownItem } from '@gitlab/ui';
+import { GlDropdown, GlDropdownItem } from '@gitlab/ui';
 import TimeAgo from '~/vue_shared/components/time_ago_tooltip.vue';
 
 export default {
   components: {
-    GlIcon,
     GlDropdown,
     GlDropdownItem,
     TimeAgo,
@@ -24,30 +23,33 @@ export default {
 </script>
 
 <template>
-  <gl-dropdown :text="selectedVersionName" category="tertiary" variant="default" size="medium" block="true">
-    <gl-dropdown-item v-for="version in versions" :key="version.id" :is-check-item="true" :is-checked="true">
-      <a :class="{ 'is-active': version.selected }" :href="version.href">
-        <div>
-          <strong>
-            {{ version.versionName }}
-            <template v-if="version.isHead">{{ s__('DiffsCompareBaseBranch|(HEAD)') }}</template>
-            <template v-else-if="version.isBase">{{
-              s__('DiffsCompareBaseBranch|(base)')
-            }}</template>
-          </strong>
-        </div>
-        <div>
-          <small class="commit-sha"> {{ version.short_commit_sha }} </small>
-        </div>
-        <div>
-          <small>
-            <template v-if="version.commitsText">
-              {{ version.commitsText }}
-            </template>
-            <time-ago v-if="version.created_at" :time="version.created_at" class="js-timeago" />
-          </small>
-        </div>
-      </a>
+  <gl-dropdown :text="selectedVersionName" category="tertiary" variant="default" size="medium">
+    <gl-dropdown-item
+      v-for="version in versions"
+      :key="version.id"
+      :is-check-item="true"
+      :is-checked="true"
+      :class="{ 'is-active': version.selected }"
+      :href="version.href"
+    >
+      <div>
+        <strong>
+          {{ version.versionName }}
+          <template v-if="version.isHead">{{ s__('DiffsCompareBaseBranch|(HEAD)') }}</template>
+          <template v-else-if="version.isBase">{{ s__('DiffsCompareBaseBranch|(base)') }}</template>
+        </strong>
+      </div>
+      <div>
+        <small class="commit-sha"> {{ version.short_commit_sha }} </small>
+      </div>
+      <div>
+        <small>
+          <template v-if="version.commitsText">
+            {{ version.commitsText }}
+          </template>
+          <time-ago v-if="version.created_at" :time="version.created_at" class="js-timeago" />
+        </small>
+      </div>
     </gl-dropdown-item>
   </gl-dropdown>
 </template>
