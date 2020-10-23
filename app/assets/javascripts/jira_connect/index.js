c29fb7fcea1904416e50c81451a0a1fc5d7be2c4
Justin Ho <hduong@gitlab.com>
2020-10-23 12:09:03 +0700

    Add Vue and GitLab UI to Jira Connect app
    - Add empty Vue setup
    - Add GitLab UI CSS

--- app/assets/javascripts/jira_connect/index.js
+++ app/assets/javascripts/jira_connect/index.js
@@ -1,3 +1,15 @@
-function initJiraConnect() {}
+import Vue from 'vue';
+import App from './components/app.vue';
+
+function initJiraConnect() {
+  const el = document.querySelector('.js-jira-connect-app');
+
+  return new Vue({
+    el,
+    render(createElement) {
+      return createElement(App, {});
+    },
+  });
+}
 
 document.addEventListener('DOMContentLoaded', initJiraConnect);
