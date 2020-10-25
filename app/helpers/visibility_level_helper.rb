d4a8c4079246452afd82dabae388a2078c7e9e7f
Ben Bodenmiller <bbodenmiller@hotmail.com>
2020-10-25 08:50:43 +0000

    Clarify that external users cannot access all internal projects

--- app/helpers/visibility_level_helper.rb
+++ app/helpers/visibility_level_helper.rb
@@ -31,7 +31,7 @@ module VisibilityLevelHelper
     when Gitlab::VisibilityLevel::PRIVATE
       _("Project access must be granted explicitly to each user. If this project is part of a group, access will be granted to members of the group.")
     when Gitlab::VisibilityLevel::INTERNAL
-      _("The project can be accessed by any logged in user.")
+      _("The project can be accessed by any logged in user except external users.")
     when Gitlab::VisibilityLevel::PUBLIC
       _("The project can be accessed without any authentication.")
     end
