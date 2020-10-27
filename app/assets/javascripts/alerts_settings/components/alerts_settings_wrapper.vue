b5d2d63f7db05e6ddf8c593308d7fdd845830347
David O'Regan <doregan@gitlab.com>
2020-10-27 07:29:56 +0000

    Update form layout and translations

--- app/assets/javascripts/alerts_settings/components/alerts_settings_wrapper.vue
+++ app/assets/javascripts/alerts_settings/components/alerts_settings_wrapper.vue
@@ -2,7 +2,10 @@
 import { s__ } from '~/locale';
 import glFeatureFlagsMixin from '~/vue_shared/mixins/gl_feature_flags_mixin';
 import { fetchPolicies } from '~/lib/graphql';
+import createFlash from '~/flash';
 import getIntegrationsQuery from '../graphql/queries/get_integrations.query.graphql';
+import createHttpIntegrationMutation from '../graphql/mutations/create_http_integration.mutation.graphql';
+import createPrometheusIntegrationMutation from '../graphql/mutations/create_prometheus_integration.mutation.graphql';
 import IntegrationsList from './alerts_integrations_list.vue';
 import SettingsFormOld from './alerts_settings_form_old.vue';
 import SettingsFormNew from './alerts_settings_form_new.vue';
@@ -49,6 +52,7 @@ export default {
   data() {
     return {
       errored: false,
+      isUpdating: false,
       integrations: {},
     };
   },
@@ -61,14 +65,40 @@ export default {
         {
           name: s__('AlertSettings|HTTP endpoint'),
           type: s__('AlertsIntegrations|HTTP endpoint'),
-          active: this.generic.activated,
+          active: this.generic.active,
         },
         {
           name: s__('AlertSettings|External Prometheus'),
           type: s__('AlertsIntegrations|Prometheus'),
-          active: this.prometheus.activated,
+          active: this.prometheus.active,
         },
       ];
+    },
+  },
+  methods: {
+    onCreateNewIntegration({ type, variables }) {
+      this.isUpdating = true;
+      this.$apollo
+        .mutate({
+          mutation:
+            type === 'HTTP' ? createHttpIntegrationMutation : createPrometheusIntegrationMutation,
+          variables: {
+            ...variables,
+            projectPath: this.projectPath,
+          },
+        })
+        .then(({ data: { httpIntegrationCreate, prometheusIntegrationCreate } = {} } = {}) => {
+          // TODO: Handle ee or user recoverable errors via generic handler here
+          // eslint-disable-next-line no-console
+          console.debug(httpIntegrationCreate, prometheusIntegrationCreate);
+        })
+        .catch(err => {
+          this.errored = true;
+          createFlash({ message: err });
+        })
+        .finally(() => {
+          this.isUpdating = false;
+        });
     },
   },
 };
@@ -80,7 +110,11 @@ export default {
       :integrations="glFeatures.httpIntegrationsList ? integrations.list : intergrationsOptionsOld"
       :loading="loading"
     />
-    <settings-form-new v-if="glFeatures.httpIntegrationsList" />
+    <settings-form-new
+      v-if="glFeatures.httpIntegrationsList"
+      :loading="loading"
+      @on-create-new-integration="onCreateNewIntegration"
+    />
     <settings-form-old v-else />
   </div>
 </template>
