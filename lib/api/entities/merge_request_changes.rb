f7c486c92b9bfaf199d20b29d0ea5a11a67ae997
Kerri Miller <kerrizor@kerrizor.com>
2020-10-26 17:52:57 -0700

    Replace raw_diffs call with diffs.diffs for testing

--- lib/api/entities/merge_request_changes.rb
+++ lib/api/entities/merge_request_changes.rb
@@ -4,7 +4,7 @@ module API
   module Entities
     class MergeRequestChanges < MergeRequest
       expose :diffs, as: :changes, using: Entities::Diff do |compare, _|
-        compare.raw_diffs(limits: false).to_a
+        Array(compare.diffs.diffs)
       end
     end
   end
