ec6026f942e339c55dbb5931f3cca40e8e18736e
Tom Quirk <tquirk@gitlab.com>
2020-10-27 15:48:07 +0930

    Refactor design scaler tests
    This commit is in light of the previous commit, where GlButtonGroup is now
    used in the component.

--- spec/frontend/design_management/components/design_scaler_spec.js
+++ spec/frontend/design_management/components/design_scaler_spec.js
@@ -1,67 +1,102 @@
-import { shallowMount } from '@vue/test-utils';
+import { shallowMount, mount } from '@vue/test-utils';
+import { GlButton } from '@gitlab/ui';
 import DesignScaler from '~/design_management/components/design_scaler.vue';
 
 describe('Design management design scaler component', () => {
   let wrapper;
 
-  function createComponent(propsData, data = {}) {
-    wrapper = shallowMount(DesignScaler, {
+  const getButtons = () => wrapper.findAll(GlButton);
+  const getDecreaseScaleButton = () => getButtons().at(0);
+  const getResetScaleButton = () => getButtons().at(1);
+  const getIncreaseScaleButton = () => getButtons().at(2);
+
+  const setScale = scale => wrapper.vm.setScale(scale);
+
+  function createComponent({ propsData, mountFn = shallowMount } = {}) {
+    wrapper = mountFn(DesignScaler, {
       propsData,
     });
-    wrapper.setData(data);
   }
+
+  const mountComponent = (args = {}) => createComponent({ ...args, mountFn: mount });
 
   afterEach(() => {
     wrapper.destroy();
   });
 
-  const getButton = type => {
-    const buttonTypeOrder = ['minus', 'reset', 'plus'];
-    const buttons = wrapper.findAll('button');
-    return buttons.at(buttonTypeOrder.indexOf(type));
-  };
+  describe('when `scale` value is greater than 1', () => {
+    beforeEach(async () => {
+      mountComponent();
+
+      setScale(1.6);
+      await wrapper.vm.$nextTick();
+    });
+
+    it('emits @scale event when "reset" button clicked', () => {
+      getResetScaleButton().trigger('click');
+      expect(wrapper.emitted('scale')[1]).toEqual([1]);
+    });
+
+    it('emits @scale event when "decrement" button clicked', async () => {
+      getDecreaseScaleButton().trigger('click');
+      expect(wrapper.emitted('scale')[1]).toEqual([1.4]);
+    });
+
+    it('"reset" button is enabled', () => {
+      const resetButton = getResetScaleButton();
+
+      expect(resetButton.exists()).toBe(true);
+      expect(resetButton.props('disabled')).toBe(false);
+    });
+
+    it('"decrement" button is enabled', () => {
+      const decrementButton = getDecreaseScaleButton();
+
+      expect(decrementButton.exists()).toBe(true);
+      expect(decrementButton.props('disabled')).toBe(false);
+    });
+  });
 
   it('emits @scale event when "plus" button clicked', () => {
-    createComponent();
+    mountComponent();
 
-    getButton('plus').trigger('click');
+    getIncreaseScaleButton().trigger('click');
     expect(wrapper.emitted('scale')).toEqual([[1.2]]);
   });
 
-  it('emits @scale event when "reset" button clicked (scale > 1)', () => {
-    createComponent({}, { scale: 1.6 });
-    return wrapper.vm.$nextTick().then(() => {
-      getButton('reset').trigger('click');
-      expect(wrapper.emitted('scale')).toEqual([[1]]);
+  describe('when `scale` value is 1', () => {
+    beforeEach(() => {
+      createComponent();
+    });
+
+    it('"reset" button is disabled', () => {
+      const resetButton = getResetScaleButton();
+
+      expect(resetButton.exists()).toBe(true);
+      expect(resetButton.props('disabled')).toBe(true);
+    });
+
+    it('"decrement" button is disabled', () => {
+      const decrementButton = getDecreaseScaleButton();
+
+      expect(decrementButton.exists()).toBe(true);
+      expect(decrementButton.props('disabled')).toBe(true);
     });
   });
 
-  it('emits @scale event when "minus" button clicked (scale > 1)', () => {
-    createComponent({}, { scale: 1.6 });
+  describe('when `scale` value is 2 (maximum)', () => {
+    beforeEach(async () => {
+      mountComponent();
 
-    return wrapper.vm.$nextTick().then(() => {
-      getButton('minus').trigger('click');
-      expect(wrapper.emitted('scale')).toEqual([[1.4]]);
+      setScale(2);
+      await wrapper.vm.$nextTick();
     });
-  });
 
-  it('minus and reset buttons are disabled when scale === 1', () => {
-    createComponent();
+    it('"increment" button is disabled', () => {
+      const incrementButton = getIncreaseScaleButton();
 
-    expect(wrapper.element).toMatchSnapshot();
-  });
-
-  it('minus and reset buttons are enabled when scale > 1', () => {
-    createComponent({}, { scale: 1.2 });
-    return wrapper.vm.$nextTick().then(() => {
-      expect(wrapper.element).toMatchSnapshot();
-    });
-  });
-
-  it('plus button is disabled when scale === 2', () => {
-    createComponent({}, { scale: 2 });
-    return wrapper.vm.$nextTick().then(() => {
-      expect(wrapper.element).toMatchSnapshot();
+      expect(incrementButton.exists()).toBe(true);
+      expect(incrementButton.props('disabled')).toBe(true);
     });
   });
 });
