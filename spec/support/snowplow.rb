1dade88cfa8c93205004be94bf4b92df2df7c72a
Alishan Ladhani <aladhani@gitlab.com>
2020-10-28 12:58:20 -0400

    Fix test helper

--- spec/support/snowplow.rb
+++ spec/support/snowplow.rb
@@ -7,16 +7,16 @@ RSpec.configure do |config|
     # WebMock is set up to allow requests to `localhost`
     host = 'localhost'
 
-    allow(Gitlab::Tracking)
+    allow(Gitlab::Tracking::Destinations::Snowplow)
       .to receive(:emitter)
       .and_return(SnowplowTracker::Emitter.new(host, buffer_size: buffer_size))
 
     stub_application_setting(snowplow_enabled: true)
 
-    allow(Gitlab::Tracking).to receive(:event).and_call_original # rubocop:disable RSpec/ExpectGitlabTracking
+    allow(Gitlab::Tracking::Destinations::Snowplow).to receive(:event).and_call_original # rubocop:disable RSpec/ExpectGitlabTracking
   end
 
-  config.after(:each, :snowplow) do
-    Gitlab::Tracking.send(:snowplow).flush
-  end
+  # config.after(:each, :snowplow) do
+  #   Gitlab::Tracking::Destinations::Snowplow.send(:snowplow).flush
+  # end
 end
