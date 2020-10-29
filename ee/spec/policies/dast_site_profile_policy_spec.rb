6c9f3779f24457da28b72c4880975b448a827264
Philip Cunningham <pcunningham@gitlab.com>
2020-10-29 14:22:59 +0000

    wip

--- ee/spec/policies/dast_site_profile_policy_spec.rb
+++ ee/spec/policies/dast_site_profile_policy_spec.rb
@@ -3,9 +3,7 @@
 require 'spec_helper'
 
 RSpec.describe DastSiteProfilePolicy do
-  describe 'create_on_demand_dast_scan' do
-    let(:record) { create(:dast_site_profile, project: project) }
+  let(:record) { create(:dast_site_profile, project: project) }
 
-    it_behaves_like 'a dast on-demand scan policy'
-  end
+  it_behaves_like 'a dast on-demand scan policy'
 end
--- ee/spec/support/shared_examples/policies/dast_on_demand_scans_shared_examples.rb
+++ ee/spec/support/shared_examples/policies/dast_on_demand_scans_shared_examples.rb
@@ -11,53 +11,55 @@ RSpec.shared_examples 'a dast on-demand scan policy' do
     stub_licensed_features(security_on_demand_scans: true)
   end
 
-  context 'when a user does not have access to the project' do
-    it { is_expected.to be_disallowed(:create_on_demand_dast_scan) }
-  end
-
-  context 'when the user is a guest' do
-    before do
-      project.add_guest(user)
+  describe 'create_on_demand_dast_scan' do
+    context 'when a user does not have access to the project' do
+      it { is_expected.to be_disallowed(:create_on_demand_dast_scan) }
     end
 
-    it { is_expected.to be_disallowed(:create_on_demand_dast_scan) }
-  end
-
-  context 'when the user is a developer' do
-    before do
-      project.add_developer(user)
-    end
-
-    it { is_expected.to be_allowed(:create_on_demand_dast_scan) }
-  end
-
-  context 'when the user is a maintainer' do
-    before do
-      project.add_maintainer(user)
-    end
-
-    it { is_expected.to be_allowed(:create_on_demand_dast_scan) }
-  end
-
-  context 'when the user is an owner' do
-    before do
-      group.add_owner(user)
-    end
-
-    it { is_expected.to be_allowed(:create_on_demand_dast_scan) }
-  end
-
-  context 'when the user is allowed' do
-    before do
-      project.add_developer(user)
-    end
-
-    context 'when on demand scan licensed feature is not available' do
+    context 'when the user is a guest' do
       before do
-        stub_licensed_features(security_on_demand_scans: false)
+        project.add_guest(user)
       end
 
       it { is_expected.to be_disallowed(:create_on_demand_dast_scan) }
+    end
+
+    context 'when the user is a developer' do
+      before do
+        project.add_developer(user)
+      end
+
+      it { is_expected.to be_allowed(:create_on_demand_dast_scan) }
+    end
+
+    context 'when the user is a maintainer' do
+      before do
+        project.add_maintainer(user)
+      end
+
+      it { is_expected.to be_allowed(:create_on_demand_dast_scan) }
+    end
+
+    context 'when the user is an owner' do
+      before do
+        group.add_owner(user)
+      end
+
+      it { is_expected.to be_allowed(:create_on_demand_dast_scan) }
+    end
+
+    context 'when the user is allowed' do
+      before do
+        project.add_developer(user)
+      end
+
+      context 'when on demand scan licensed feature is not available' do
+        before do
+          stub_licensed_features(security_on_demand_scans: false)
+        end
+
+        it { is_expected.to be_disallowed(:create_on_demand_dast_scan) }
+      end
     end
   end
 end
