ea6db33ed617199753aecec9fe9a9337fad95f00
Vijay Hawoldar <vhawoldar@gitlab.com>
2020-10-29 17:14:42 +0000

    Stop Snippet fixtures from repeatedly cloning
    Instead, we can use a repo bundle and create the
    fixture repositories from that

--- db/fixtures/development/12_snippets.rb
+++ db/fixtures/development/12_snippets.rb
@@ -1,8 +1,8 @@
 require './spec/support/sidekiq_middleware'
 
-SNIPPET_REPO_URL = "https://gitlab.com/gitlab-org/gitlab-snippet-test.git"
-
 Gitlab::Seeder.quiet do
+  bundle_path = File.join(Rails.root, 'db/fixtures/development/bundles/gitlab-snippet-test.bundle')
+
   20.times do |i|
     user = User.not_mass_generated.sample
 
@@ -14,7 +14,7 @@ Gitlab::Seeder.quiet do
       content: 'foo'
     }).tap do |snippet|
       unless snippet.repository_exists?
-        snippet.repository.import_repository(SNIPPET_REPO_URL)
+        snippet.repository.create_from_bundle(bundle_path)
       end
 
       snippet.track_snippet_repository(snippet.repository.storage)
