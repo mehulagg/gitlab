0a2323bdc81efd3e894a35341d0da44acc66d9f9
Jonathan Schafer <jschafer@gitlab.com>
2020-10-29 15:54:51 -0500

    Add build service for vuln issues

--- ee/app/controllers/projects/security/vulnerabilities_controller.rb
+++ ee/app/controllers/projects/security/vulnerabilities_controller.rb
@@ -13,19 +13,39 @@ module Projects
 
       feature_category :vulnerability_management
 
+      respond_to :html
+
       def show
         pipeline = vulnerability.finding.pipelines.first
         @pipeline = pipeline if Ability.allowed?(current_user, :read_pipeline, pipeline)
         @gfm_form = true
       end
 
-      def create_issue
+      def save_issue
         result = ::Issues::CreateFromVulnerabilityService
           .new(
             container: vulnerability.project,
             current_user: current_user,
             params: {
               vulnerability: vulnerability,
+              link_type: ::Vulnerabilities::IssueLink.link_types[:created]
+            })
+          .execute
+
+        if result[:status] == :success
+          render json: issue_serializer.represent(result[:issue], only: [:web_url])
+        else
+          render json: result[:message], status: :unprocessable_entity
+        end
+      end
+
+      def create_issue
+        result = ::Issues::BuildFromVulnerabilityService
+          .new(
+            container: @vulnerability.project,
+            current_user: current_user,
+            params: {
+              vulnerability: @vulnerability,
               link_type: ::Vulnerabilities::IssueLink.link_types[:created]
             })
           .execute
@@ -48,6 +68,39 @@ module Projects
 
       def issue_serializer
         IssueSerializer.new(current_user: current_user)
+      end
+
+      def issue_params
+        params.require(:issue).permit(
+          *issue_params_attributes,
+          sentry_issue_attributes: [:sentry_issue_identifier]
+        )
+      end
+    
+      def issue_params_attributes
+        %i[
+          title
+          assignee_id
+          position
+          description
+          confidential
+          milestone_id
+          due_date
+          state_event
+          task_num
+          lock_version
+          discussion_locked
+        ] + [{ label_ids: [], assignee_ids: [], update_task: [:index, :checked, :line_number, :line_source] }]
+      end
+    
+      def issue_create_params
+        create_params = %i[
+          issue_type
+        ]
+    
+        params.require(:issue).permit(
+          *create_params
+        ).merge(issue_params)
       end
     end
   end
