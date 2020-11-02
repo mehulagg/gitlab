9ae6fa6c1f91a26dcb7873bfe7e049fbd8bc79b2
syasonik <syasonik@gitlab.com>
2020-11-02 16:25:11 -0500

    Do not include deleted integration is response

--- spec/requests/api/graphql/mutations/alert_management/http_integration/destroy_spec.rb
+++ spec/requests/api/graphql/mutations/alert_management/http_integration/destroy_spec.rb
@@ -39,17 +39,8 @@ RSpec.describe 'Removing an HTTP Integration' do
   it 'removes the integration' do
     post_graphql_mutation(mutation, current_user: user)
 
-    integration_response = mutation_response['integration']
-
     expect(response).to have_gitlab_http_status(:success)
-    expect(integration_response['id']).to eq(GitlabSchema.id_from_object(integration).to_s)
-    expect(integration_response['type']).to eq('HTTP')
-    expect(integration_response['name']).to eq(integration.name)
-    expect(integration_response['active']).to eq(integration.active)
-    expect(integration_response['token']).to eq(integration.token)
-    expect(integration_response['url']).to eq(integration.url)
-    expect(integration_response['apiUrl']).to eq(nil)
-
+    expect(mutation_response['integration']).to be_nil
     expect { integration.reload }.to raise_error ActiveRecord::RecordNotFound
   end
 end
