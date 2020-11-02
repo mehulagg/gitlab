109f935a83dc2dac2f40e8befb26100c873024f1
sfang97 <sfang@gitlab.com>
2020-11-02 12:36:11 -0600

    Move bot removal into removeexpiredmembers worker
    Instead of making a new worker, move project bot removal logic into
    remove expired members worker

--- app/workers/remove_expired_members_worker.rb
+++ app/workers/remove_expired_members_worker.rb
@@ -10,6 +10,12 @@ class RemoveExpiredMembersWorker # rubocop:disable Scalability/IdempotentWorker
   def perform
     Member.expired.find_each do |member|
       Members::DestroyService.new.execute(member, skip_authorization: true)
+
+      expired_user = User.find(member.user_id)
+
+      if expired_user.project_bot?
+        Users::DestroyService.new(expired_user).execute(expired_user, skip_authorization: true, hard_delete: true)
+      end
     rescue => ex
       logger.error("Expired Member ID=#{member.id} cannot be removed - #{ex}")
     end
