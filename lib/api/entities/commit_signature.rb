27c60a248f14f0edeb1e336545cadb150dccf872
Kerri Miller <kerrizor@kerrizor.com>
2020-11-02 16:35:43 -0800

    Use Entities::GpgCommitSignature for Rugged::Commit

--- lib/api/entities/commit_signature.rb
+++ lib/api/entities/commit_signature.rb
@@ -5,10 +5,20 @@ module API
     class CommitSignature < Grape::Entity
       expose :signature_type
       expose :signature, merge: true do |commit, options|
-        if commit.signature.is_a?(GpgSignature)
-          ::API::Entities::GpgCommitSignature.represent commit.signature, options
+        if commit.signature.is_a?(GpgSignature) || commit.raw_commit_from_rugged?
+          ::API::Entities::GpgCommitSignature.represent commit_signature(commit), options
         elsif commit.signature.is_a?(X509CommitSignature)
           ::API::Entities::X509Signature.represent commit.signature, options
+        end
+      end
+
+      private
+
+      def commit_signature(commit)
+        if commit.raw_commit_from_rugged?
+          Gitlab::Gpg::Commit.new(commit).signature
+        else
+          commit.signature
         end
       end
     end
