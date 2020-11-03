4e4b747627d7dd517d2947e4119c37aab493d369
Furkan Ayhan <furkanayhn@gmail.com>
2020-11-03 13:30:27 +0300

    apply review feedbacks

--- spec/lib/gitlab/ci/pipeline/chain/cancel_pending_pipelines_spec.rb
+++ spec/lib/gitlab/ci/pipeline/chain/cancel_pending_pipelines_spec.rb
@@ -3,10 +3,10 @@
 require 'spec_helper'
 
 RSpec.describe Gitlab::Ci::Pipeline::Chain::CancelPendingPipelines do
-  let(:project) { create(:project) }
-  let(:user) { create(:user) }
-  let(:prev_pipeline) { create(:ci_pipeline, project: project) }
-  let(:new_commit) { create(:commit, project: project)  }
+  let_it_be(:project) { create(:project) }
+  let_it_be(:user) { create(:user) }
+  let_it_be(:prev_pipeline) { create(:ci_pipeline, project: project) }
+  let(:new_commit) { create(:commit, project: project) }
   let(:pipeline) { create(:ci_pipeline, project: project, sha: new_commit.sha) }
 
   let(:command) do
@@ -15,16 +15,18 @@ RSpec.describe Gitlab::Ci::Pipeline::Chain::CancelPendingPipelines do
 
   let(:step) { described_class.new(pipeline, command) }
 
-  before do
+  before_all do
     create(:ci_build, :interruptible, :running, project: project, pipeline: prev_pipeline)
     create(:ci_build, :interruptible, :success, project: project, pipeline: prev_pipeline)
     create(:ci_build, :created, project: project, pipeline: prev_pipeline)
+  end
 
+  before do
     create(:ci_build, :interruptible, project: project, pipeline: pipeline)
   end
 
   describe '#perform!' do
-    subject { step.perform! }
+    subject(:perform) { step.perform! }
 
     before do
       expect(prev_pipeline.builds.pluck(:status)).to contain_exactly('running', 'success', 'created')
@@ -37,7 +39,7 @@ RSpec.describe Gitlab::Ci::Pipeline::Chain::CancelPendingPipelines do
       end
 
       it 'cancels only previous interruptible builds' do
-        subject
+        perform
 
         expect(prev_pipeline.builds.pluck(:status)).to contain_exactly('canceled', 'success', 'canceled')
         expect(pipeline.builds.pluck(:status)).to contain_exactly('pending')
@@ -46,27 +48,43 @@ RSpec.describe Gitlab::Ci::Pipeline::Chain::CancelPendingPipelines do
       context 'when the previous pipeline has a child pipeline' do
         let(:child_pipeline) { create(:ci_pipeline, child_of: prev_pipeline) }
 
-        before do
-          create(:ci_build, :interruptible, :running, project: project, pipeline: child_pipeline)
-        end
-
-        it 'cancels interruptible builds of child pipeline' do
-          expect(child_pipeline.builds.pluck(:status)).to contain_exactly('running')
-
-          subject
-
-          expect(child_pipeline.builds.pluck(:status)).to contain_exactly('canceled')
-        end
-
-        context 'when FF ci_auto_cancel_all_pipelines is disabled' do
+        context 'when the child pipeline has an interruptible job' do
           before do
-            stub_feature_flags(ci_auto_cancel_all_pipelines: false)
+            create(:ci_build, :interruptible, :running, project: project, pipeline: child_pipeline)
           end
 
-          it 'does not cancel interruptible builds of child pipeline' do
+          it 'cancels interruptible builds of child pipeline' do
             expect(child_pipeline.builds.pluck(:status)).to contain_exactly('running')
 
-            subject
+            perform
+
+            expect(child_pipeline.builds.pluck(:status)).to contain_exactly('canceled')
+          end
+
+          context 'when FF ci_auto_cancel_all_pipelines is disabled' do
+            before do
+              stub_feature_flags(ci_auto_cancel_all_pipelines: false)
+            end
+
+            it 'does not cancel interruptible builds of child pipeline' do
+              expect(child_pipeline.builds.pluck(:status)).to contain_exactly('running')
+
+              perform
+
+              expect(child_pipeline.builds.pluck(:status)).to contain_exactly('running')
+            end
+          end
+        end
+
+        context 'when the child pipeline has not an interruptible job' do
+          before do
+            create(:ci_build, :running, project: project, pipeline: child_pipeline)
+          end
+
+          it 'does not cancel the build of child pipeline' do
+            expect(child_pipeline.builds.pluck(:status)).to contain_exactly('running')
+
+            perform
 
             expect(child_pipeline.builds.pluck(:status)).to contain_exactly('running')
           end
