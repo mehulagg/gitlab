5099c7630ab29cf985ee5ea0d538f34ff7999670
Nicolò Maria Mezzopera <nicolo.mezzopera@gmail.com>
2020-11-03 16:36:32 +0100

    Adjust path generator and add test
    - source
    - tests

--- app/assets/javascripts/registry/explorer/utils.js
+++ app/assets/javascripts/registry/explorer/utils.js
@@ -3,6 +3,6 @@ export const decodeAndParse = param => JSON.parse(window.atob(param));
 // eslint-disable-next-line @gitlab/require-i18n-strings
 export const pathGenerator = (imageDetails, ending = 'tags?format=json') => {
   // this method is a temporary workaround, to be removed with graphql implementation
-  const basePath = imageDetails.path.replace(imageDetails.name, '');
+  const basePath = imageDetails.path.replace(`/${imageDetails.name}`, '');
   return `/${basePath}/registry/repository/${imageDetails.id}/${ending}`;
 };
