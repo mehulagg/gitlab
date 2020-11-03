440190478ec4a8d7b30935c3f92d165656a289c2
Alper Akgun <aakgun@gitlab.com>
2020-11-03 16:43:18 +0300

    Enhance and add close icon to "thank you" banner

--- app/assets/javascripts/groups/index.js
+++ app/assets/javascripts/groups/index.js
@@ -1,4 +1,5 @@
 import Vue from 'vue';
+import UserCallout from '~/user_callout';
 import { parseBoolean } from '~/lib/utils/common_utils';
 import Translate from '../vue_shared/translate';
 import GroupFilterableList from './groups_filterable_list';
@@ -15,6 +16,9 @@ Vue.use(Translate);
 export default (containerId = 'js-groups-tree', endpoint, action = '') => {
   const containerEl = document.getElementById(containerId);
   let dataEl;
+
+  // eslint-disable-next-line no-new
+  new UserCallout();
 
   // Don't do anything if element doesn't exist (No groups)
   // This is for when the user enters directly to the page via URL
