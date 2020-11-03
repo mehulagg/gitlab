31bb263eeb16ed117b844045c5c08283dce2c15f
Zack Cuddy <zcuddy@gitlab.com>
2020-11-03 11:14:55 -0600

    Global Search - Radio Filters
    This change is behind a feature
    flag :search_facets.
    
    This was split off for the sake
    of MVC and the final product
    can be seen here:
    https://gitlab.com/gitlab-org/gitlab/-/merge_requests/46595
    
    This change replaces the dropdown
    filter with radio buttons.
    
    These will then be used as
    the components in the
    sidebar in the next MR.

---
+++ app/assets/javascripts/search/sidebar/components/confidentiality_filter.vue
@@ -0,0 +1,28 @@
+<script>
+import { mapState } from 'vuex';
+import confidentialFilterData from '../constants/confidential_filter_data';
+import RadioFilter from './radio_filter.vue';
+
+export default {
+  name: 'ConfidentialityFilter',
+  components: {
+    RadioFilter,
+  },
+  computed: {
+    ...mapState(['query']),
+    supportedScopes() {
+      return Object.values(confidentialFilterData.scopes);
+    },
+    showDropdown() {
+      return this.supportedScopes.includes(this.query.scope);
+    },
+  },
+  confidentialFilterData,
+};
+</script>
+
+<template>
+  <div v-if="showDropdown">
+    <radio-filter :filter-data="$options.confidentialFilterData" />
+  </div>
+</template>
---
+++ app/assets/javascripts/search/sidebar/components/status_filter.vue
@@ -0,0 +1,28 @@
+<script>
+import { mapState } from 'vuex';
+import stateFilterData from '../constants/state_filter_data';
+import RadioFilter from './radio_filter.vue';
+
+export default {
+  name: 'StatusFilter',
+  components: {
+    RadioFilter,
+  },
+  computed: {
+    ...mapState(['query']),
+    supportedScopes() {
+      return Object.values(stateFilterData.scopes);
+    },
+    showDropdown() {
+      return this.supportedScopes.includes(this.query.scope);
+    },
+  },
+  stateFilterData,
+};
+</script>
+
+<template>
+  <div v-if="showDropdown">
+    <radio-filter :filter-data="$options.stateFilterData" />
+  </div>
+</template>
