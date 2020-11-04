18f891d33aef27340342ce8f2687318571eefa03
David O'Regan <doregan@gitlab.com>
2020-11-04 14:40:32 +0000

    Additional integrations
    Indicate that no additional integrations
    can be added for Core users

--- app/assets/javascripts/alerts_settings/components/alerts_settings_wrapper.vue
+++ app/assets/javascripts/alerts_settings/components/alerts_settings_wrapper.vue
@@ -19,6 +19,11 @@ import {
   updateStoreAfterIntegrationDelete,
   updateStoreAfterIntegrationAdd,
 } from '../utils/cache_updates';
+import {
+  DELETE_INTEGRATION_ERROR,
+  ADD_INTEGRATION_ERROR,
+  RESET_INTEGRATION_TOKEN_ERROR,
+} from '../utils/error_messages';
 
 export default {
   typeSet,
@@ -43,6 +48,9 @@ export default {
     },
     projectPath: {
       default: '',
+    },
+    multiIntegrations: {
+      default: false,
     },
   },
   apollo: {
@@ -91,6 +99,11 @@ export default {
         },
       ];
     },
+    canAddIntegration() {
+      return (
+        this.multiIntegrations || (!this.multiIntegrations && this.integrations?.list?.length < 2)
+      );
+    },
   },
   methods: {
     createNewIntegration({ type, variables }) {
@@ -121,8 +134,8 @@ export default {
             type: FLASH_TYPES.SUCCESS,
           });
         })
-        .catch(err => {
-          createFlash({ message: err });
+        .catch(() => {
+          createFlash({ message: ADD_INTEGRATION_ERROR });
         })
         .finally(() => {
           this.isUpdating = false;
@@ -187,8 +200,8 @@ export default {
             });
           },
         )
-        .catch(err => {
-          createFlash({ message: err });
+        .catch(() => {
+          createFlash({ message: RESET_INTEGRATION_TOKEN_ERROR });
         })
         .finally(() => {
           this.isUpdating = false;
@@ -222,9 +235,9 @@ export default {
             type: FLASH_TYPES.SUCCESS,
           });
         })
-        .catch(err => {
+        .catch(() => {
           this.errored = true;
-          createFlash({ message: err });
+          createFlash({ message: DELETE_INTEGRATION_ERROR });
         })
         .finally(() => {
           this.isUpdating = false;
@@ -249,6 +262,7 @@ export default {
       v-if="glFeatures.httpIntegrationsList"
       :loading="isUpdating"
       :current-integration="currentIntegration"
+      :can-add-integration="canAddIntegration"
       @create-new-integration="createNewIntegration"
       @update-integration="updateIntegration"
       @reset-token="resetToken"
