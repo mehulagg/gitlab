42b0d3faf05f7919b1082224b9d186480ce34eaa
fjsanpedro <fjlopez@gitlab.com>
2020-11-04 16:02:16 +0000

    Remove programmingLanguage prop

--- spec/features/projects/show/schema_markup_spec.rb
+++ spec/features/projects/show/schema_markup_spec.rb
@@ -3,11 +3,7 @@
 require 'spec_helper'
 
 RSpec.describe 'Projects > Show > Schema Markup' do
-  let_it_be(:project) do
-    create(:project, :repository, :public, :with_avatar, description: 'foobar', tag_list: 'tag1, tag2').tap do |p|
-      ::Projects::DetectRepositoryLanguagesService.new(p).execute
-    end
-  end
+  let_it_be(:project) { create(:project, :repository, :public, :with_avatar, description: 'foobar', tag_list: 'tag1, tag2') }
 
   it 'shows SoftwareSourceCode structured markup', :js do
     visit project_path(project)
@@ -19,7 +15,6 @@ RSpec.describe 'Projects > Show > Schema Markup' do
       expect(page).to have_selector('[itemprop="name"]')
       expect(page).to have_selector('[itemprop="identifier"]')
       expect(page).to have_selector('[itemprop="abstract"]')
-      expect(page).to have_selector('[itemprop="programmingLanguage"]')
       expect(page).to have_selector('[itemprop="license"]')
       expect(page).to have_selector('[itemprop="keywords"]')
       expect(page).to have_selector('[itemprop="about"]')
