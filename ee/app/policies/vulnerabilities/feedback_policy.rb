29725f32964036ad9182078e350d1a1e56083f75
Tetiana Chupryna <tchupryna@gitlab.com>
2020-11-05 16:04:32 +0200

    Update feedback policy

--- ee/app/policies/vulnerabilities/feedback_policy.rb
+++ ee/app/policies/vulnerabilities/feedback_policy.rb
@@ -7,16 +7,21 @@ module Vulnerabilities
     condition(:issue) { @subject.for_issue? }
     condition(:merge_request) { @subject.for_merge_request? }
     condition(:dismissal) { @subject.for_dismissal? }
-
-    with_options scope: :user, score: 0
-    condition(:security_bot) { @user&.security_bot? }
+    condition(:auto_fix_enabled) { @subject.project.security_setting&.auto_fix_enabled&.any? }
 
     rule { issue & ~can?(:create_issue) }.prevent :create_vulnerability_feedback
 
     rule do
       merge_request &
-        ~security_bot &
-        (~can?(:create_merge_request_in) | ~can?(:create_merge_request_from))
+        ~(can?(:create_merge_request_in) & can?(:create_merge_request_from))
+    end.prevent :create_vulnerability_feedback
+
+    # Prevent Security bot from creating vulnerability feedback
+    # if auto-fix feature is disabled
+    rule do
+      merge_request &
+        security_bot &
+        ~auto_fix_enabled
     end.prevent :create_vulnerability_feedback
 
     rule { ~dismissal }.prevent :destroy_vulnerability_feedback, :update_vulnerability_feedback
