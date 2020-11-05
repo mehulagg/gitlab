1daa36c1fcd00831ede963cceb6088c928d81fe6
fjsanpedro <fjlopez@gitlab.com>
2020-11-05 16:33:34 +0000

    Add schema markup to breadcrumb

--- app/helpers/breadcrumbs_helper.rb
+++ app/helpers/breadcrumbs_helper.rb
@@ -12,7 +12,7 @@ module BreadcrumbsHelper
   def breadcrumb_title_link
     return @breadcrumb_link if @breadcrumb_link
 
-    request.path
+    "#{request.base_url}#{request.path}"
   end
 
   def breadcrumb_title(title)
@@ -31,5 +31,31 @@ module BreadcrumbsHelper
     @breadcrumb_dropdown_links ||= {}
     @breadcrumb_dropdown_links[location] ||= []
     @breadcrumb_dropdown_links[location] << link
+  end
+
+  def push_to_schema_breadcrumb(text, link)
+    @push_schema_breadcrumb ||= []
+    @push_schema_breadcrumb.push(schema_list_item(text, link, @push_schema_breadcrumb.size + 1))
+  end
+
+  def schema_breadcrumb_json
+    {
+      '@context': 'https://schema.org',
+      '@type': 'BreadcrumbList',
+      'itemListElement': @push_schema_breadcrumb
+    }.to_json
+  end
+
+  private
+
+  def schema_list_item(text, link, position)
+    {
+      '@type' => 'ListItem',
+      'position' => position,
+      'item' => {
+        '@id' => link,
+        'name' => text
+      }
+    }
   end
 end
