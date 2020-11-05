a55d316562ff551b7cc7414c089f3994417ac425
David O'Regan <doregan@gitlab.com>
2020-11-05 08:50:17 +0000

    Fix broken specs

--- spec/helpers/operations_helper_spec.rb
+++ spec/helpers/operations_helper_spec.rb
@@ -44,8 +44,7 @@ RSpec.describe OperationsHelper do
           'prometheus_activated' => 'false',
           'prometheus_url' => notify_project_prometheus_alerts_url(project, format: :json),
           'disabled' => 'false',
-          'project_path' => project.full_path,
-          'multi_integrations' => 'false'
+          'project_path' => project.full_path
         )
       end
     end
