21c4423292b5f5652aa0ff08523feeb1463a58b5
Nicolò Maria Mezzopera <nicolo.mezzopera@gmail.com>
2020-11-06 11:01:04 +0100

    Remove unused utility

--- app/assets/javascripts/registry/explorer/utils.js
+++ app/assets/javascripts/registry/explorer/utils.js
@@ -1,5 +1,3 @@
-export const decodeAndParse = param => JSON.parse(window.atob(param));
-
 // eslint-disable-next-line @gitlab/require-i18n-strings
 export const pathGenerator = (imageDetails, ending = 'tags?format=json') => {
   // this method is a temporary workaround, to be removed with graphql implementation
