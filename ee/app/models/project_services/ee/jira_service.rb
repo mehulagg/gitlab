2acff06df088db1088ca031b90060b9198437823
Alan Paruszewski <mparuszewski@gitlab.com>
2020-11-06 09:08:31 +0100

    Add link to generate issue from vulnerability in Jira

--- ee/app/models/project_services/ee/jira_service.rb
+++ ee/app/models/project_services/ee/jira_service.rb
@@ -7,5 +7,19 @@ module EE
     prepended do
       validates :project_key, presence: true, if: :issues_enabled
     end
+
+    def new_issue_url_with_predefined_fields(summary, description)
+      return unless project.try(:jira_vulnerabilities_integration_enabled?)
+
+      escaped_summary = CGI.escape(summary)
+      escaped_description = CGI.escape(description)
+      "#{url}/secure/CreateIssueDetails!init.jspa?pid=#{project_id}&issuetype=#{issue_key}&summary=#{escaped_summary}&description=#{escaped_description}"
+    end
+
+    def project_id
+      strong_memoize(:project_id) do
+        client_url.present? ? jira_request { client.Project.find(project_key).id } : nil
+      end
+    end
   end
 end
