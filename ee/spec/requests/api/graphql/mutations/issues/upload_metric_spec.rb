ef1471cc50c3c1626d9b284fb88fd9dba64ef7de
Sean Arnold <sarnold@gitlab.com>
2020-11-06 13:55:49 +1300

    Add graphql specs
    - Update documentations

---
+++ ee/spec/requests/api/graphql/mutations/issues/upload_metric_spec.rb
@@ -0,0 +1,42 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+RSpec.describe Mutations::Issues::UploadMetric do
+  include GraphqlHelpers
+
+  let_it_be(:current_user) { create(:user) }
+  let_it_be(:project) { create(:project, :public) }
+  let_it_be(:issue) { create(:incident, project: project) }
+  let(:file) { fixture_file_upload('spec/fixtures/rails_sample.jpg', 'image/jpg') }
+
+  subject(:mutation) do
+    described_class.new(object: nil, context: { current_user:  current_user }, field: nil)
+  end
+
+  describe '#resolve' do
+    subject(:resolve) do
+      mutation.resolve(project_path: project.full_path, iid: issue.iid, file: file, url: 'http://www.gitlab.com')
+    end
+
+    context 'user has no permission' do
+      it "raises an error" do
+        expect { resolve }.to raise_error(Gitlab::Graphql::Errors::ResourceNotAvailable)
+      end
+    end
+
+    context 'user has permission' do
+      before do
+        project.add_developer(current_user)
+      end
+
+      it 'creates the metric image' do
+        expect { subject }.
+          to change(issue.metric_images, :count)
+
+        expect(subject[:issue]).to eq(issue)
+        expect(subject[:errors]).to be_empty
+      end
+    end
+  end
+end
