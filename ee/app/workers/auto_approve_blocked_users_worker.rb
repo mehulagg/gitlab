d2d066f8e377d52e8c99095410c9bb3eebe7508e
a_luna <aluna@gitlab.com>
2020-11-05 19:04:01 -0500

    Add worker to call the Users::ApproveService
    If the feature is enabled and the cap is increased by an admin, users in
    blocked_pending_approval state should be auto approved.

---
+++ ee/app/workers/auto_approve_blocked_users_worker.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+class AutoApproveBlockedUsersWorker
+  include ApplicationWorker
+
+  feature_category :license_compliance
+
+  def perform(current_user_id)
+    current_user = User.find(current_user_id)
+
+    User.blocked_pending_approval.find_each do |user|
+      Users::ApproveService.new(current_user).execute(user)
+    end
+  end
+end
