3a5e7a482ccd860fafa2b3b13faf328587ee82e3
Jiaan Louw <jlouw@gitlab.com>
2020-11-06 17:22:10 +0200

    Add export for merge commit custody report
    This adds a export dropdown to the existing list all
    merge commits button in the Compliance Dashboard, it
    allows for a merge commit-specific chain of custody
    report to be generate for the provided commit hash.

--- ee/spec/frontend/compliance_dashboard/components/merge_requests/merge_commits_export_button_spec.js
+++ ee/spec/frontend/compliance_dashboard/components/merge_requests/merge_commits_export_button_spec.js
@@ -1,38 +1,52 @@
-import { shallowMount } from '@vue/test-utils';
-import { GlButton } from '@gitlab/ui';
+import { mount, shallowMount } from '@vue/test-utils';
+import { GlFormInput, GlForm, GlFormGroup } from '@gitlab/ui';
 
 import MergeCommitsExportButton from 'ee/compliance_dashboard/components/merge_requests/merge_commits_export_button.vue';
+import { INPUT_DEBOUNCE } from 'ee/compliance_dashboard/constants';
 
+
+const VALID_COMMIT_SHA = '92d10c15';
 const CSV_EXPORT_PATH = '/merge_commit_reports';
+const STRING_OVER_40 = new Array(42).join('a');
 
 describe('MergeCommitsExportButton component', () => {
   let wrapper;
 
-  const findCsvExportButton = () => wrapper.find(GlButton);
+  const findCommitForm = () => wrapper.find(GlForm);
+  const findCommitInput = () => wrapper.find(GlFormInput);
+  const findCommitInputGroup = () => wrapper.find(GlFormGroup);
+  const findCommitInputFeedback = () => wrapper.find('.invalid-feedback');
+  const findCommitExportButton = () => wrapper.find('[data-test-id="merge-commit-submit-button"]');
+  const findCsvExportButton = () => wrapper.find({ ref: 'listMergeCommitsButton' });
 
-  const createComponent = (props = {}) => {
-    return shallowMount(MergeCommitsExportButton, {
+  const createComponent = (mountFn = shallowMount) => {
+    return mountFn(MergeCommitsExportButton, {
       propsData: {
         mergeCommitsCsvExportPath: CSV_EXPORT_PATH,
-        ...props,
       },
     });
   };
-
-  beforeEach(() => {
-    wrapper = createComponent();
-  });
 
   afterEach(() => {
     wrapper.destroy();
   });
 
-  describe('Merge commit CSV export button', () => {
+  describe('Layout', () => {
+    beforeEach(() => {
+      wrapper = createComponent();
+    });
+
     it('matches the snapshot', () => {
       expect(wrapper.element).toMatchSnapshot();
     });
+  });
 
-    it('renders the merge commits csv export button', () => {
+  describe('Merge commit CSV export all button', () => {
+    beforeEach(() => {
+      wrapper = createComponent(mount);
+    });
+
+    it('renders the button', () => {
       expect(findCsvExportButton().exists()).toBe(true);
     });
 
@@ -42,6 +56,68 @@ describe('MergeCommitsExportButton component', () => {
 
     it('links to the csv download path', () => {
       expect(findCsvExportButton().attributes('href')).toEqual(CSV_EXPORT_PATH);
+    });
+  });
+
+  describe('Merge commit custody report', () => {
+    beforeEach(() => {
+      wrapper = createComponent();
+    });
+
+    it('renders the form', () => {
+      expect(findCommitForm().exists()).toBe(true);
+    });
+
+    it('renders the input', () => {
+      expect(findCommitInput().exists()).toBe(true);
+    });
+
+    it('renders the input label', () => {
+      expect(findCommitInputGroup().attributes('label')).toBe('Merge commit SHA');
+    });
+
+    it('renders the submit button', () => {
+      expect(findCommitExportButton().exists()).toBe(true);
+    });
+
+    it('sets a debounce time on the input', () => {
+      expect(findCommitInput().attributes('debounce')).toEqual(INPUT_DEBOUNCE.toString());
+    });
+
+    it('sets the form action to the csv download path', () => {
+      expect(findCommitForm().attributes('action')).toEqual(CSV_EXPORT_PATH);
+    });
+
+    it('sets the invalid input feedback message', () => {
+      wrapper = createComponent(mount);
+
+      expect(findCommitInputFeedback().text()).toBe('Invalid hash');
+    });
+
+    describe.each`
+      commitSha           | formError
+      ${VALID_COMMIT_SHA} | ${false}
+      ${'__aaa'}          | ${true}
+      ${'aaa'}            | ${true}
+      ${STRING_OVER_40}   | ${true}
+    `(`commit hash input validation`, ({ commitSha, formError }) => {
+      beforeEach(() => {
+        wrapper = createComponent(mount);
+        findCommitInput().setValue(commitSha);
+        findCommitInput().trigger('blur');
+      });
+
+      describe(`when the commit sha is ${commitSha}`, () => {
+        it(`${formError ? 'shows' : 'hides'} that the input is invalid`, () => {
+          expect(findCommitInputGroup().classes('is-invalid')).toBe(formError);
+        });
+
+        it(`${formError ? 'disables' : 'enables'} the submit button`, () => {
+          expect(findCommitExportButton().attributes('disabled')).toBe(
+            formError ? 'disabled' : undefined,
+          );
+        });
+      });
     });
   });
 });
