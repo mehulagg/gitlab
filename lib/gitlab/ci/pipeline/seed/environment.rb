a39dbe15aa06057564deb1b71d9739c5cca798c7
Amy Troschinetz <atroschinetz@gitlab.com>
2020-11-09 10:18:44 -0800

    Auto-stop env kicks off when env is created

--- lib/gitlab/ci/pipeline/seed/environment.rb
+++ lib/gitlab/ci/pipeline/seed/environment.rb
@@ -12,11 +12,21 @@ module Gitlab
           end
 
           def to_resource
-            job.project.environments
-              .safe_find_or_create_by(name: expanded_environment_name)
+            job.project.environments.safe_find_or_create_by(
+              name: expanded_environment_name,
+              auto_stop_at: auto_stop_at
+            )
           end
 
           private
+
+          def auto_stop_at
+            parsed_result = ChronicDuration.parse(job.environment_auto_stop_in)
+            parsed_result.seconds.from_now
+          rescue ChronicDuration::DurationParseError
+            # Question: Should this error be reported somewhere?
+            nil
+          end
 
           def expanded_environment_name
             job.expanded_environment_name
