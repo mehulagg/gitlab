0759e3999a90cbf01ac12b31678cd05da326be24
Florie Guibert <fguibert@gitlab.com>
2020-11-09 14:50:37 +1000

    Board refactor - Split BoardColumn component
    Consolidate graphQLBoardLists feature flag by splitting BoardColumn
    component to isolate boardStore to help with deprecation.

--- app/assets/javascripts/boards/components/board_column_new.vue
+++ app/assets/javascripts/boards/components/board_column_new.vue
@@ -1,21 +1,17 @@
 <script>
-import Sortable from 'sortablejs';
+import { mapGetters, mapActions, mapState } from 'vuex';
 import BoardListHeader from 'ee_else_ce/boards/components/board_list_header.vue';
 import EmptyComponent from '~/vue_shared/components/empty_component';
 import glFeatureFlagMixin from '~/vue_shared/mixins/gl_feature_flags_mixin';
-import BoardList from './board_list.vue';
-import boardsStore from '../stores/boards_store';
+import BoardListNew from './board_list_new.vue';
 import eventHub from '../eventhub';
-import { getBoardSortableDefaultOptions, sortableEnd } from '../mixins/sortable_default_options';
 import { ListType } from '../constants';
-
-// This component is being replaced in favor of './board_column_new.vue' for GraphQL boards
 
 export default {
   components: {
     BoardPromotionState: EmptyComponent,
     BoardListHeader,
-    BoardList,
+    BoardList: BoardListNew,
   },
   mixins: [glFeatureFlagMixin()],
   props: {
@@ -39,65 +35,41 @@ export default {
       default: '',
     },
   },
-  data() {
-    return {
-      detailIssue: boardsStore.detail,
-      filter: boardsStore.filter,
-    };
-  },
   computed: {
+    ...mapState(['filterParams']),
+    ...mapGetters(['getIssues']),
     showBoardListAndBoardInfo() {
       return this.list.type !== ListType.promotion;
     },
-    uniqueKey() {
-      // eslint-disable-next-line @gitlab/require-i18n-strings
-      return `boards.${this.boardId}.${this.list.type}.${this.list.id}`;
-    },
     listIssues() {
-      return this.list.issues;
+      return this.getIssues(this.list.id);
+    },
+    shouldFetchIssues() {
+      return this.list.type !== ListType.blank;
     },
   },
   watch: {
-    filter: {
+    filterParams: {
       handler() {
-        this.list.page = 1;
-        this.list.getIssues(true).catch(() => {
-          // TODO: handle request error
-        });
+        if (this.shouldFetchIssues) {
+          this.fetchIssuesForList({ listId: this.list.id });
+        }
       },
       deep: true,
+      immediate: true,
     },
   },
   mounted() {
-    const instance = this;
-
-    const sortableOptions = getBoardSortableDefaultOptions({
-      disabled: this.disabled,
-      group: 'boards',
-      draggable: '.is-draggable',
-      handle: '.js-board-handle',
-      onEnd(e) {
-        sortableEnd();
-
-        const sortable = this;
-
-        if (e.newIndex !== undefined && e.oldIndex !== e.newIndex) {
-          const order = sortable.toArray();
-          const list = boardsStore.findList('id', parseInt(e.item.dataset.id, 10));
-
-          instance.$nextTick(() => {
-            boardsStore.moveList(list, order);
-          });
-        }
-      },
-    });
-
-    Sortable.create(this.$el.parentNode, sortableOptions);
+    if (this.shouldFetchIssues) {
+      this.fetchIssuesForList({ listId: this.list.id });
+    }
   },
   methods: {
+    ...mapActions(['fetchIssuesForList']),
     showListNewIssueForm(listId) {
       eventHub.$emit('showForm', listId);
     },
+    // TODO: Reordering of lists https://gitlab.com/gitlab-org/gitlab/-/issues/280515
   },
 };
 </script>
--- app/assets/javascripts/boards/components/board_content.vue
+++ app/assets/javascripts/boards/components/board_content.vue
@@ -2,12 +2,13 @@
 import { mapState, mapGetters, mapActions } from 'vuex';
 import { sortBy } from 'lodash';
 import BoardColumn from 'ee_else_ce/boards/components/board_column.vue';
+import BoardColumnNew from 'ee_else_ce/boards/components/board_column_new.vue';
 import { GlAlert } from '@gitlab/ui';
 import glFeatureFlagMixin from '~/vue_shared/mixins/gl_feature_flags_mixin';
 
 export default {
   components: {
-    BoardColumn,
+    BoardColumn: gon.features?.graphqlBoardLists ? BoardColumnNew : BoardColumn,
     BoardContentSidebar: () => import('ee_component/boards/components/board_content_sidebar.vue'),
     EpicsSwimlanes: () => import('ee_component/boards/components/epics_swimlanes.vue'),
     GlAlert,
