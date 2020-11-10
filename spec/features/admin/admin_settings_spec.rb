7b00b040080e671cef01c930c2eec4000a057b9f
peterhegman <phegman@gitlab.com>
2020-11-10 11:54:48 -0800

    Add user callout for instances with registration enabled by default
    Shown only to admins to let them know that anybody can register on
    their instance

--- spec/features/admin/admin_settings_spec.rb
+++ spec/features/admin/admin_settings_spec.rb
@@ -621,6 +621,51 @@ RSpec.describe 'Admin updates settings', :clean_gitlab_redis_shared_state, :do_n
     end
   end
 
+  context 'registration enabled callout' do
+    context 'when "Sign-up enabled" setting is `true`' do
+      before do
+        stub_application_setting(signup_enabled: true)
+      end
+
+      context 'when an admin is logged in' do
+        before do
+          sign_in(admin)
+          visit root_dashboard_path
+        end
+
+        it 'displays callout' do
+          expect(page).to have_content 'Open registration is enabled on your instance.'
+          expect(page).to have_link 'View setting', href: general_admin_application_settings_path(anchor: 'js-signup-settings')
+        end
+
+        context 'when callout is dismissed', :js do
+          before do
+            find('[data-testid="close-registration-enabled-callout"]').click
+
+            visit root_dashboard_path
+          end
+
+          it 'does not display callout' do
+            expect(page).not_to have_content 'Open registration is enabled on your instance.'
+          end
+        end
+      end
+
+      context 'when a non-admin is logged in' do
+        let_it_be(:non_admin) { create(:user) }
+
+        before do
+          sign_in(non_admin)
+          visit root_dashboard_path
+        end
+
+        it 'does not display callout' do
+          expect(page).not_to have_content 'Open registration is enabled on your instance.'
+        end
+      end
+    end
+  end
+
   def check_all_events
     page.check('Active')
     page.check('Push')
