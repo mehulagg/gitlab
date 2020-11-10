aad57758b7a878eea85fe73cf4bdbecd8e6aef60
Mike Kozono <mkozono@gmail.com>
2020-11-09 18:45:48 -0800

    Simplify calculate_checksum! method
    The `update_verification_state` method also sets verification_checksum
    to checksum, so we don't need to do it here.

--- ee/lib/gitlab/geo/replicable_model.rb
+++ ee/lib/gitlab/geo/replicable_model.rb
@@ -48,13 +48,10 @@ module Gitlab
         raise NotImplementedError, 'There is no Replicator defined for this model'
       end
 
-      # Clear model verification checksum and force recalculation
       def calculate_checksum!
-        self.verification_checksum = nil
+        return unless checksummable?
 
-        return unless needs_checksum?
-
-        self.verification_checksum = self.class.hexdigest(replicator.carrierwave_uploader.path)
+        self.class.hexdigest(replicator.carrierwave_uploader.path)
       end
 
       # Checks whether model needs checksum to be performed
