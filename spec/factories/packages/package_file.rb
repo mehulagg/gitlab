6ed818d702a584563e3c10f8fe59691e4395fd58
Mike Kozono <mkozono@gmail.com>
2020-11-09 17:40:22 -0800

    Use the verification state machine's scopes
    Instead of deriving verification state from various checksum-related
    fields, we can now query for records with a particular state.

--- spec/factories/packages/package_file.rb
+++ spec/factories/packages/package_file.rb
@@ -152,12 +152,14 @@ FactoryBot.define do
       file_store { Packages::PackageFileUploader::Store::REMOTE }
     end
 
-    trait(:checksummed) do
+    trait(:verification_succeeded) do
       verification_checksum { 'abc' }
+      verification_state { Packages::PackageFile.verification_state_value[:verification_succeeded] }
     end
 
-    trait(:checksum_failure) do
+    trait(:verification_failed) do
       verification_failure { 'Could not calculate the checksum' }
+      verification_state { Packages::PackageFile.verification_state_value[:verification_failed] }
     end
 
     factory :package_file_with_file, traits: [:jar]
