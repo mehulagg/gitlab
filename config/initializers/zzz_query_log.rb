01824f37bdb1622b80d9ddb8a3d6b6e4e73c0192
Adam Hegyi <ahegyi@gitlab.com>
2020-11-11 06:12:16 +0100

    Find out what is this long query

--- config/initializers/zzz_query_log.rb
+++ config/initializers/zzz_query_log.rb
@@ -1,5 +1,9 @@
-f = File.open(Rails.root.join("log", "queries.log"), "a")
+# f = File.open(Rails.root.join("log", "queries.log"), "a")
 ActiveSupport::Notifications.subscribe(/sql.active_record/) do |name, start, finish, message_id, values|
-  f.puts values[:sql]
-  f.puts "-----"
+
+  if values[:sql].include?("999901, 999902, 999903, 999904, 999905, 999906")
+    raise 'LONG QUERY'
+  end
+  # f.puts values[:sql]
+  # f.puts "-----"
 end
