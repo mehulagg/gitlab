e03a823d063c749b74d8b461a08eaf19764828e9
Jannik Lehmann <jlehmann@gitlab.com>
2020-11-12 16:44:28 +0100

    Fix Linting Issue

--- ee/app/assets/javascripts/pages/security/dashboard/settings/index.js
+++ ee/app/assets/javascripts/pages/security/dashboard/settings/index.js
@@ -1,3 +1,2 @@
 import initInstanceSecurityDashboardSettings from 'ee/security_dashboard/instance_dashboard_settings_init';
 initInstanceSecurityDashboardSettings(document.getElementById('js-security'));
-
