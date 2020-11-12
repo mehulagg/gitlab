0c97a1781f1d90a0c794d7cea61a17929ede4610
Samantha Ming <sming@gitlab.com>
2020-11-11 20:47:38 -0800

    Collapse approval rules and move it under reviewer
    Issue: https://gitlab.com/gitlab-org/gitlab/-/issues/232817

--- ee/app/assets/javascripts/approvals/components/mr_edit/app.vue
+++ ee/app/assets/javascripts/approvals/components/mr_edit/app.vue
@@ -1,20 +1,51 @@
 <script>
+import { uniqueId } from 'lodash';
+import { GlIcon, GlButton, GlCollapse, GlCollapseToggleDirective } from '@gitlab/ui';
 import App from '../app.vue';
 import MrRules from './mr_rules.vue';
 import MrRulesHiddenInputs from './mr_rules_hidden_inputs.vue';
 
 export default {
   components: {
+    GlIcon,
+    GlButton,
+    GlCollapse,
     App,
     MrRules,
     MrRulesHiddenInputs,
+  },
+  directives: {
+    CollapseToggle: GlCollapseToggleDirective,
+  },
+  data() {
+    return {
+      collapseId: uniqueId('approval-rules-expandable-section-'),
+      visible: false,
+    };
+  },
+  computed: {
+    toggleIcon() {
+      return this.visible ? 'chevron-down' : 'chevron-right';
+    },
   },
 };
 </script>
 
 <template>
-  <app>
-    <mr-rules slot="rules" />
-    <mr-rules-hidden-inputs slot="footer" />
-  </app>
+  <!-- SAM: This need to be behind feature flag!!! -->
+  <div>
+    <div>
+      <gl-button v-collapse-toggle="collapseId" variant="link">
+        <gl-icon :name="toggleIcon" />
+        {{ __('Show approval rules') }}
+      </gl-button>
+
+      <gl-collapse :id="collapseId" v-model="visible" class="gl-mt-3">
+        <app>
+          <mr-rules slot="rules" />
+          <mr-rules-hidden-inputs slot="footer" />
+        </app>
+      </gl-collapse>
+    </div>
+  </div>
 </template>
