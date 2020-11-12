292a34e2737049f1cbe6f76b78d6c479d0e8e07c
Mark Florian <mflorian@gitlab.com>
2020-11-12 16:59:54 +0000

    Create CE security_reports Vuex store
    This creates a CE security_reports store that is a subset of the EE
    version. This subset is roughly the minimum necessary to render counts
    on the CE security MR widget. See this [epic][1] for more details about
    the overall direction.
    
    The majority of this change is just existing code that's been moved
    and/or refactored. The only _genuinely_ new piece of code is the new
    store's entry point:
    `app/assets/javascripts/vue_shared/security_reports/store/index.js`.
    
    Since the CE version only supports SAST and Secret Detection, this MR
    also refactored the getters to generalise over an array of report types,
    rather than hard code them all into every getter. This means that these
    getters can be shared as-is between CE and EE, and behave correctly due
    to the `reportTypes` field added to the state of both stores. These
    getters are tested in both CE and EE, to verify the different behaviour
    based on the available report types.
    
    The `noBaseInAllReports` getter turned out not to be used anywhere, so
    it was deleted.
    
    Two existing getters, `anyReportHasIssues` and `areAllReportsLoading`
    didn't have tests, so tests were added for them in both CE and EE.
    
    Since the getters now more explicitly depend on the report types
    installed in the store, constants were extracted to make their
    connection explicit, and reduce the chance that typos would lead to
    runtime errors.
    
    The vulnerability severity constants were moved to a common location in
    CE.
    
    The definitions of the `LOADING`, `SUCCESS` and `ERROR` constants were
    deleted from the `security_reports` directory, since their definitions
    already exist in the `~/reports/constants.js` file. These constants are
    intended to be used with/by the `ReportSection` component, so it makes
    sense not to redefine them elsewhere.
    
    Part of https://gitlab.com/gitlab-org/gitlab/-/issues/273423.
    
    [1]: https://gitlab.com/groups/gitlab-org/-/epics/4394

--- app/assets/javascripts/vue_shared/security_reports/store/utils.js
+++ app/assets/javascripts/vue_shared/security_reports/store/utils.js
@@ -1,5 +1,7 @@
 import pollUntilComplete from '~/lib/utils/poll_until_complete';
 import axios from '~/lib/utils/axios_utils';
+import { __, n__, sprintf } from '~/locale';
+import { CRITICAL, HIGH } from '~/vulnerabilities/constants';
 import {
   FEEDBACK_TYPE_DISMISSAL,
   FEEDBACK_TYPE_ISSUE,
@@ -71,5 +73,77 @@ export const parseDiff = (diff, enrichData) => {
     added: diff.added ? diff.added.map(enrichVulnerability) : [],
     fixed: diff.fixed ? diff.fixed.map(enrichVulnerability) : [],
     existing: diff.existing ? diff.existing.map(enrichVulnerability) : [],
+  };
+};
+
+const createCountMessage = ({ critical, high, other, total }) => {
+  const otherMessage = n__('%d Other', '%d Others', other);
+  const countMessage = __(
+    '%{criticalStart}%{critical} Critical%{criticalEnd} %{highStart}%{high} High%{highEnd} and %{otherStart}%{otherMessage}%{otherEnd}',
+  );
+  return total ? sprintf(countMessage, { critical, high, otherMessage }) : '';
+};
+
+const createStatusMessage = ({ reportType, status, total }) => {
+  const vulnMessage = n__('vulnerability', 'vulnerabilities', total);
+  let message;
+  if (status) {
+    message = __('%{reportType} %{status}');
+  } else if (!total) {
+    message = __('%{reportType} detected %{totalStart}no%{totalEnd} vulnerabilities.');
+  } else {
+    message = __(
+      '%{reportType} detected %{totalStart}%{total}%{totalEnd} potential %{vulnMessage}',
+    );
+  }
+  return sprintf(message, { reportType, status, total, vulnMessage });
+};
+
+/**
+ * Counts vulnerabilities.
+ * Returns the amount of critical, high, and other vulnerabilities.
+ *
+ * @param {Array} vulnerabilities The raw vulnerabilities to parse
+ * @returns {{critical: number, high: number, other: number}}
+ */
+export const countVulnerabilities = (vulnerabilities = []) => {
+  const critical = vulnerabilities.filter(vuln => vuln.severity === CRITICAL).length;
+  const high = vulnerabilities.filter(vuln => vuln.severity === HIGH).length;
+  const other = vulnerabilities.length - critical - high;
+
+  return {
+    critical,
+    high,
+    other,
+  };
+};
+
+/**
+ * Takes an object of options and returns the object with an externalized string representing
+ * the critical, high, and other severity vulnerabilities for a given report.
+ *
+ * The resulting string _may_ still contain sprintf-style placeholders. These
+ * are left in place so they can be replaced with markup, via the
+ * SecuritySummary component.
+ * @param {{reportType: string, status: string, critical: number, high: number, other: number}} options
+ * @returns {Object} the parameters with an externalized string
+ */
+export const groupedTextBuilder = ({
+  reportType = '',
+  status = '',
+  critical = 0,
+  high = 0,
+  other = 0,
+} = {}) => {
+  const total = critical + high + other;
+
+  return {
+    countMessage: createCountMessage({ critical, high, other, total }),
+    message: createStatusMessage({ reportType, status, total }),
+    critical,
+    high,
+    other,
+    status,
+    total,
   };
 };
