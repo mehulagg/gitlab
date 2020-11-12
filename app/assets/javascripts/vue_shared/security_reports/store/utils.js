20e75875250082ebff85b09ff11a7a7ee36ed82f
Mark Florian <mflorian@gitlab.com>
2020-11-12 18:05:41 +0000

    move a bunch of utils to CE, and move severities to CE

--- app/assets/javascripts/vue_shared/security_reports/store/utils.js
+++ app/assets/javascripts/vue_shared/security_reports/store/utils.js
@@ -1,5 +1,6 @@
 import pollUntilComplete from '~/lib/utils/poll_until_complete';
 import axios from '~/lib/utils/axios_utils';
+import { __, n__, sprintf } from '~/locale';
 import {
   FEEDBACK_TYPE_DISMISSAL,
   FEEDBACK_TYPE_ISSUE,
@@ -72,4 +73,112 @@ export const parseDiff = (diff, enrichData) => {
     fixed: diff.fixed ? diff.fixed.map(enrichVulnerability) : [],
     existing: diff.existing ? diff.existing.map(enrichVulnerability) : [],
   };
+};
+
+const createCountMessage = ({ critical, high, other, total }) => {
+  const otherMessage = n__('%d Other', '%d Others', other);
+  const countMessage = __(
+    '%{criticalStart}%{critical} Critical%{criticalEnd} %{highStart}%{high} High%{highEnd} and %{otherStart}%{otherMessage}%{otherEnd}',
+  );
+  return total ? sprintf(countMessage, { critical, high, otherMessage }) : '';
+};
+
+const createStatusMessage = ({ reportType, status, total }) => {
+  const vulnMessage = n__('vulnerability', 'vulnerabilities', total);
+  let message;
+  if (status) {
+    message = __('%{reportType} %{status}');
+  } else if (!total) {
+    message = __('%{reportType} detected %{totalStart}no%{totalEnd} vulnerabilities.');
+  } else {
+    message = __(
+      '%{reportType} detected %{totalStart}%{total}%{totalEnd} potential %{vulnMessage}',
+    );
+  }
+  return sprintf(message, { reportType, status, total, vulnMessage });
+};
+
+/**
+ * Counts vulnerabilities.
+ * Returns the amount of critical, high, and other vulnerabilities.
+ *
+ * @param {Array} vulnerabilities The raw vulnerabilities to parse
+ * @returns {{critical: number, high: number, other: number}}
+ */
+export const countVulnerabilities = (vulnerabilities = []) => {
+  const critical = vulnerabilities.filter(vuln => vuln.severity === CRITICAL).length;
+  const high = vulnerabilities.filter(vuln => vuln.severity === HIGH).length;
+  const other = vulnerabilities.length - critical - high;
+
+  return {
+    critical,
+    high,
+    other,
+  };
+};
+
+/**
+ * Takes an object of options and returns the object with an externalized string representing
+ * the critical, high, and other severity vulnerabilities for a given report.
+ *
+ * The resulting string _may_ still contain sprintf-style placeholders. These
+ * are left in place so they can be replaced with markup, via the
+ * SecuritySummary component.
+ * @param {{reportType: string, status: string, critical: number, high: number, other: number}} options
+ * @returns {Object} the parameters with an externalized string
+ */
+export const groupedTextBuilder = ({
+  reportType = '',
+  status = '',
+  critical = 0,
+  high = 0,
+  other = 0,
+} = {}) => {
+  const total = critical + high + other;
+
+  return {
+    countMessage: createCountMessage({ critical, high, other, total }),
+    message: createStatusMessage({ reportType, status, total }),
+    critical,
+    high,
+    other,
+    status,
+    total,
+  };
+};
+
+export const statusIcon = (loading = false, failed = false, newIssues = 0, neutralIssues = 0) => {
+  if (loading) {
+    return 'loading';
+  }
+
+  if (failed || newIssues > 0 || neutralIssues > 0) {
+    return 'warning';
+  }
+
+  return 'success';
+};
+
+/**
+ * Generates a report message based on some of the report parameters and supplied messages.
+ *
+ * @param {Object} report The report to generate the text for
+ * @param {String} reportType The report type. e.g. SAST
+ * @param {String} errorMessage The message to show if there's an error in the report
+ * @param {String} loadingMessage The message to show if the report is still loading
+ * @returns {String}
+ */
+export const groupedReportText = (report, reportType, errorMessage, loadingMessage) => {
+  if (report.hasError) {
+    return { message: errorMessage };
+  }
+
+  if (report.isLoading) {
+    return { message: loadingMessage };
+  }
+
+  return groupedTextBuilder({
+    reportType,
+    ...countVulnerabilities(report.newIssues),
+  });
 };
