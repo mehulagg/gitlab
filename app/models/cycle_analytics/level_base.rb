53c037b53761ddc21877d62d1e3937554dcb5b57
Adam Hegyi <ahegyi@gitlab.com>
2020-11-13 13:47:02 +0100

    Optionally use new VSA backend on the project level
    This change introduces a feature flag (new_project_level_vsa_backend)
    where the new backend implementation is going to be used on the project
    level value stream analytics page.

--- app/models/cycle_analytics/level_base.rb
+++ app/models/cycle_analytics/level_base.rb
@@ -4,6 +4,45 @@ module CycleAnalytics
   module LevelBase
     STAGES = %i[issue plan code test review staging].freeze
 
+    # This is a temporary adapter class which makes the new value stream (cycle analytics)
+    # backend compatible with the old implementation.
+    class LegacyStageAdapter
+      def initialize(stage, options)
+        @stage = stage
+        @options = options
+      end
+
+      # rubocop: disable CodeReuse/Presenter
+      def as_json
+        presenter = Analytics::CycleAnalytics::StagePresenter.new(stage)
+
+        AnalyticsStageEntity.new(OpenStruct.new(
+                                   title: presenter.title,
+                                   description: presenter.description,
+                                   legend: presenter.legend,
+                                   name: stage.name,
+                                   project_median: project_median
+                                 )).as_json
+      end
+      # rubocop: enable CodeReuse/Presenter
+
+      def events
+        data_collector.records_fetcher.serialized_records
+      end
+
+      def project_median
+        data_collector.median.seconds
+      end
+
+      private
+
+      attr_reader :stage, :options
+
+      def data_collector
+        @data_collector ||= Gitlab::Analytics::CycleAnalytics::DataCollector.new(stage: stage, params: options)
+      end
+    end
+
     def all_medians_by_stage
       STAGES.each_with_object({}) do |stage_name, medians_per_stage|
         medians_per_stage[stage_name] = self[stage_name].project_median
@@ -11,7 +50,7 @@ module CycleAnalytics
     end
 
     def stats
-      @stats ||= STAGES.map do |stage_name|
+      STAGES.map do |stage_name|
         self[stage_name].as_json
       end
     end
@@ -21,7 +60,17 @@ module CycleAnalytics
     end
 
     def [](stage_name)
-      Gitlab::CycleAnalytics::Stage[stage_name].new(options: options)
+      if Feature.enabled?(:new_project_level_vsa_backend)
+        stage_params = stage_params_by_name(stage_name).merge(project: project)
+        stage = Analytics::CycleAnalytics::ProjectStage.new(stage_params)
+        LegacyStageAdapter.new(stage, options)
+      else
+        Gitlab::CycleAnalytics::Stage[stage_name].new(options: options)
+      end
+    end
+
+    def stage_params_by_name(name)
+      Gitlab::Analytics::CycleAnalytics::DefaultStages.all.find { |raw_stage| raw_stage[:name].to_s.eql?(name.to_s) } || raise("Default stage '#{name}' not found")
     end
   end
 end
