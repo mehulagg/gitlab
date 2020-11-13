d356c9dd499e202ef4332a8f4f3d2fd21d8d23c2
Chad Woolley <cwoolley@gitlab.com>
2020-11-13 14:20:54 -0700

    Add frontend analytics for projects dropdown
    See https://gitlab.com/gitlab-org/gitlab/-/issues/275968

--- app/assets/javascripts/frequent_items/components/frequent_items_search_input.vue
+++ app/assets/javascripts/frequent_items/components/frequent_items_search_input.vue
@@ -4,6 +4,7 @@ import { mapActions } from 'vuex';
 import { GlIcon } from '@gitlab/ui';
 import eventHub from '../event_hub';
 import frequentItemsMixin from './frequent_items_mixin';
+import Tracking from '~/tracking';
 
 export default {
   components: {
@@ -22,6 +23,13 @@ export default {
   },
   watch: {
     searchQuery: debounce(function debounceSearchQuery() {
+      const trackEvent = 'type_search_query';
+      const trackCategory = undefined; // will be default set in event method
+
+      Tracking.event(trackCategory, trackEvent, {
+        label: 'projects_dropdown_frequent_items_search_input',
+      });
+
       this.setSearchQuery(this.searchQuery);
     }, 500),
   },
