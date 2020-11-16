d6f8e355626cea64fa2cfafe4167f0b9022cb250
a_luna <aluna@gitlab.com>
2020-11-16 16:34:47 -0500

    Register users with blocked_pending_approval state
    If the admin_new_user_signups_cap setting is defined, users will be
    marked as blocked_pending_approval. After the current user count is
    checked and the cap is not exceeded the user will be automatically
    activated.

--- ee/app/controllers/ee/registrations_controller.rb
+++ ee/app/controllers/ee/registrations_controller.rb
@@ -13,5 +13,19 @@ module EE
 
       super(confirmed: confirmed) + ", experiments:#{experiments}"
     end
+
+    override :set_user_state
+    def set_user_state
+      return unless apply_user_signup_cap?
+
+      resource.state = ::RegistrationsController::BLOCKED_PENDING_APPROVAL_STATE
+    end
+
+    def apply_user_signup_cap?
+      return true if ::Gitlab::CurrentSettings.require_admin_approval_after_user_signup
+
+      ::Feature.enabled?(:admin_new_user_signups_cap) &&
+        ::Gitlab::CurrentSettings.new_user_signups_cap
+    end
   end
 end
--- ee/spec/controllers/registrations_controller_spec.rb
+++ ee/spec/controllers/registrations_controller_spec.rb
@@ -3,11 +3,12 @@
 require 'spec_helper'
 
 RSpec.describe RegistrationsController do
-  let_it_be(:user) { create(:user) }
-
   describe '#create' do
-    let(:base_user_params) { build_stubbed(:user).slice(:first_name, :last_name, :username, :email, :password) }
-    let(:user_params) { { user: base_user_params } }
+    let_it_be(:base_user_params) { build_stubbed(:user).slice(:first_name, :last_name, :username, :password) }
+    let_it_be(:new_user_email) { 'new@user.com' }
+    let(:user_params) { { user: base_user_params.merge(email: new_user_email) } }
+
+    subject { post :create, params: user_params }
 
     context 'when reCAPTCHA experiment enabled' do
       it "logs a 'User Created' message including the experiment state" do
@@ -15,7 +16,103 @@ RSpec.describe RegistrationsController do
 
         expect(Gitlab::AppLogger).to receive(:info).with(/\AUser Created: .+experiment_growth_recaptcha\?true\z/).and_call_original
 
-        post :create, params: user_params
+        subject
+      end
+    end
+
+    shared_examples 'blocked user by default' do
+      it 'registers the user in blocked_pending_approval state' do
+        subject
+        created_user = User.find_by(email: new_user_email)
+
+        expect(created_user).to be_present
+        expect(created_user).to be_blocked_pending_approval
+      end
+
+      it 'does not log in the user after sign up' do
+        subject
+
+        expect(controller.current_user).to be_nil
+      end
+
+      it 'shows flash message after signing up' do
+        subject
+
+        expect(response).to redirect_to(new_user_session_path(anchor: 'login-pane'))
+        expect(flash[:notice])
+          .to match(/your account is awaiting approval from your GitLab administrator/)
+      end
+    end
+
+    shared_examples 'active user by default' do
+      it 'registers the user in active state' do
+        subject
+        created_user = User.find_by(email: new_user_email)
+
+        expect(created_user).to be_present
+        expect(created_user).to be_active
+      end
+
+      it 'does not show any flash message after signing up' do
+        subject
+
+        expect(flash[:notice]).to be_nil
+      end
+    end
+
+    context 'when require admin approval setting is enabled' do
+      before do
+        stub_application_setting(require_admin_approval_after_user_signup: true)
+      end
+
+      it_behaves_like 'blocked user by default'
+    end
+
+    context 'when require admin approval setting is disabled' do
+      before do
+        stub_application_setting(require_admin_approval_after_user_signup: false)
+      end
+
+      it_behaves_like 'active user by default'
+
+      context 'when user signup cap feature is enabled' do
+        before do
+          stub_application_setting(new_user_signups_cap: true)
+        end
+
+        it_behaves_like 'blocked user by default'
+      end
+    end
+
+    context 'when user signup cap setting is enabled' do
+      before do
+        stub_application_setting(new_user_signups_cap: true)
+      end
+
+      it_behaves_like 'blocked user by default'
+
+      context 'when feature flag is disabled' do
+        before do
+          stub_feature_flags(admin_new_user_signups_cap: false)
+        end
+
+        it_behaves_like 'active user by default'
+      end
+    end
+
+    context 'when user signup cap feature is disabled' do
+      before do
+        stub_application_setting(new_user_signups_cap: false)
+      end
+
+      it_behaves_like 'active user by default'
+
+      context 'when require admin approval setting is enabled' do
+        before do
+          stub_application_setting(require_admin_approval_after_user_signup: true)
+        end
+
+        it_behaves_like 'blocked user by default'
       end
     end
   end
