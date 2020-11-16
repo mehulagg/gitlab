c0a21c2e261b70e3ac1af54ff676ceaf1f4d50a8
Andy Soiron <asoiron@gitlab.com>
2020-11-16 21:15:03 +0100

    Fix kwargs warning in microsoft_teams notifier

--- lib/microsoft_teams/notifier.rb
+++ lib/microsoft_teams/notifier.rb
@@ -32,7 +32,7 @@ module MicrosoftTeams
 
       result['title'] = options[:title]
       result['summary'] = options[:summary]
-      result['sections'] << MicrosoftTeams::Activity.new(options[:activity]).prepare
+      result['sections'] << MicrosoftTeams::Activity.new(**options[:activity]).prepare
 
       attachments = options[:attachments]
       unless attachments.blank?
