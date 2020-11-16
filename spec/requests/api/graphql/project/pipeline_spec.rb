e5989e7da915dd75ecb1d9dbdb29594019168d84
Philip Cunningham <pcunningham@gitlab.com>
2020-11-16 15:04:45 +0000

    Address db reviewer feedback

--- spec/requests/api/graphql/project/pipeline_spec.rb
+++ spec/requests/api/graphql/project/pipeline_spec.rb
@@ -35,4 +35,31 @@ RSpec.describe 'getting pipeline information nested in a project' do
 
     expect(pipeline_graphql_data.dig('configSource')).to eq('UNKNOWN_SOURCE')
   end
+
+  context 'batching' do
+    let(:pipeline2) { create(:ci_pipeline, project: project) }
+    let(:pipeline3) { create(:ci_pipeline, project: project) }
+
+    let(:query) do
+      graphql_query_for(
+        'project',
+        { 'fullPath' => project.full_path },
+        [
+          "pipeline1: #{query_graphql_field('pipeline', iid: pipeline.iid.to_s)}",
+          "pipeline2: #{query_graphql_field('pipeline', iid: pipeline2.iid.to_s)}",
+          "pipeline3: #{query_graphql_field('pipeline', iid: pipeline3.iid.to_s)}"
+        ].join(' ')
+      )
+    end
+
+    it 'executes the finder once' do
+      mock = double(Ci::PipelinesFinder)
+      opts = { iids: [pipeline.iid, pipeline2.iid, pipeline3.iid].map(&:to_s) }
+
+      expect(Ci::PipelinesFinder).to receive(:new).once.with(project, current_user, opts).and_return(mock)
+      expect(mock).to receive(:execute).once.and_return(Ci::Pipeline.none)
+
+      post_graphql(query, current_user: current_user)
+    end
+  end
 end
