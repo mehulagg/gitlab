a300a8a4e9527f08da71ae95978ec1eb432bccf1
Alex Kalderimis <akalderimis@gitlab.com>
2020-11-16 13:42:11 +0000

    Eliminate N+1 performance issues in MergeRequest.pipelines
    This requires the use of the CachingArrayResolver due to the way
    MergeRequest.pipelines is implemented.

--- app/graphql/resolvers/merge_request_pipelines_resolver.rb
+++ app/graphql/resolvers/merge_request_pipelines_resolver.rb
@@ -3,14 +3,26 @@
 module Resolvers
   class MergeRequestPipelinesResolver < BaseResolver
     include ::ResolvesPipelines
+    include ::CachingArrayResolver
 
     alias_method :merge_request, :object
 
     def resolve(**args)
       return unless project
 
-      resolve_pipelines(project, args)
-        .merge(merge_request.all_pipelines)
+      super
+    end
+
+    def query_for(args)
+      resolve_pipelines(project, args).merge(merge_request.all_pipelines)
+    end
+
+    def model_class
+      ::Ci::Pipeline
+    end
+
+    def query_input(**args)
+      args
     end
 
     def project
