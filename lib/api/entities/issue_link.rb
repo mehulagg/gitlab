07f72ed900297f94655c7cd1cbbccd0d33118ff8
Yorick Peterse <yorick@yorickpeterse.com>
2020-11-18 17:18:17 +0100

    Expose creation/update times for issue links
    The issue links API now exposes the fields created_at and updated_at for
    each issue link. This allows clients to determine when an issue link is
    created or updated.
    
    See https://gitlab.com/gitlab-org/gitlab/-/issues/283948 and
    https://gitlab.com/gitlab-com/gl-infra/delivery/-/issues/1250 for more
    information.

--- lib/api/entities/issue_link.rb
+++ lib/api/entities/issue_link.rb
@@ -6,6 +6,8 @@ module API
       expose :source, as: :source_issue, using: ::API::Entities::IssueBasic
       expose :target, as: :target_issue, using: ::API::Entities::IssueBasic
       expose :link_type
+      expose :created_at
+      expose :updated_at
     end
   end
 end
