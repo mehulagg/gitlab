1950248fd9c6df2683efec4223bf36d9dc57c946
Lee Tickett <lee@tickett.net>
2020-11-18 13:28:44 +0000

    Apply 1 suggestion(s) to 1 file(s)

--- app/controllers/groups/application_controller.rb
+++ app/controllers/groups/application_controller.rb
@@ -15,7 +15,7 @@ class Groups::ApplicationController < ApplicationController
   private
 
   def group
-    if controller_name == "groups" && (action_name == "details" || action_name == "show" || action_name == "index")
+    if controller_name == "groups" && %w(details show index).include?(action_name)
       @group_projects_sort = set_sort_order(Project::SORTING_PREFERENCE_FIELD, sort_value_name)
     end
 
