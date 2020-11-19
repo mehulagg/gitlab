766140a59e9792a46d378c2c6cb61aee78ed2a50
Terri Chu <tchu@gitlab.com>
2020-11-19 16:54:16 -0500

    fixup! Migration to add new data to issues documents

--- ee/elastic/migrate/20201116142400_add_new_data_to_issues_documents.rb
+++ ee/elastic/migrate/20201116142400_add_new_data_to_issues_documents.rb
@@ -3,17 +3,19 @@
 class AddNewDataToIssuesDocuments < Elastic::Migration
   batch_update!
 
+  BATCH_SIZE = 1000
+
   def migrate
     if completed?
       log "Skipping adding issues_access_level fields to issues documents migration since it is already applied"
       return
     end
 
-    log "Adding issues_access_level fields to issues documents"
+    log "Adding issues_access_level fields to issues documents for batch of #{BATCH_SIZE} documents"
 
     # get a batch of issues missing data
     query = {
-      size: 1000,
+      size: BATCH_SIZE,
       query: {
         bool: {
           must: {
@@ -32,43 +34,25 @@ class AddNewDataToIssuesDocuments < Elastic::Migration
       }
     }
 
-    # work through them in batches
-    loop do
-      results = client.search(index: helper.target_index_name, body: query)
-      hits = results.dig('hits', 'hits')
-      break if hits.empty?
+    # work a batch of issues
+    results = client.search(index: helper.target_index_name, body: query)
+    hits = results.dig('hits', 'hits')
 
-      hits.each do |hit|
-        id = hit.dig('_source', 'id')
-        es_id = hit.dig('_id')
-        es_parent = hit.dig('_source', 'join_field', 'parent')
+    hits.each do |hit|
+      id = hit.dig('_source', 'id')
+      es_id = hit.dig('_id')
+      es_parent = hit.dig('_source', 'join_field', 'parent')
 
-        # ensure that any issues missing from the database will be removed from Elasticsearch
-        # as the data is back-filled
-        issue_document_reference = Gitlab::Elastic::DocumentReference.new(Issue.class.name, id, es_id, es_parent)
-        Elastic::ProcessBookkeepingService.track!(issue_document_reference)
-      end
+      # ensure that any issues missing from the database will be removed from Elasticsearch
+      # as the data is back-filled
+      issue_document_reference = Gitlab::Elastic::DocumentReference.new(Issue.class.name, id, es_id, es_parent)
+      Elastic::ProcessBookkeepingService.track!(issue_document_reference)
     end
 
-    # kick off indexing for each individually
-    # do we need to wait or how do we tell that it's done?
-
-    log 'Adding issues_access_level fields to issues documents is completed'
+    log "Adding issues_access_level fields to issues documents is completed for batch of #{BATCH_SIZE} documents"
   end
 
   def completed?
-    # TODO - there are orphaned issues in the index
-    # that will not allow us to use this for the completed?
-    # need to figure out what is going to allow us to know this is done
-    # could create a whole new queue (like in ProcessBookkeepingService)
-    # but not using that queue would mean we lose deduplication benefit
-    # We could check all of the issues missing data and check if any of them exist in the database, if
-    # any do exist, then we know this is not completed. How much paging/size to use here is a question
-
-    # can use the idea below if we don't use the database to find the issue, construct the document reference correctly
-
-    # query counts all issue documents that are missing the
-    # field issues_access_level
     query = {
       query: {
         match_all: {}
