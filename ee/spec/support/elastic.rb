d1c94ad5ef50d5bfa8b12d37e4563b9a75ac7881
Dmitry Gruzd <dgruzd@gitlab.com>
2020-11-19 10:17:37 +0300

    Fix intermitten elastic-related specs failures

--- ee/spec/support/elastic.rb
+++ ee/spec/support/elastic.rb
@@ -2,16 +2,25 @@
 
 RSpec.configure do |config|
   config.before(:each, :elastic) do
+    helper = Gitlab::Elastic::Helper.default
+    @migrations_index_name = helper.migrations_index_name
+
     Elastic::ProcessBookkeepingService.clear_tracking!
-    Gitlab::Elastic::Helper.default.delete_index
-    Gitlab::Elastic::Helper.default.create_empty_index(options: { settings: { number_of_replicas: 0 } })
-    @migrations_index_name = Gitlab::Elastic::Helper.default.create_migrations_index
+
+    helper.delete_index(index_name: @migrations_index_name)
+    helper.delete_index
+
+    helper.create_empty_index(options: { settings: { number_of_replicas: 0 } })
+    helper.create_migrations_index
   end
 
   config.after(:each, :elastic) do
-    Gitlab::Elastic::Helper.default.delete_index
+    helper = Gitlab::Elastic::Helper.default
+
+    helper.delete_index
+    helper.delete_index(index_name: @migrations_index_name)
+
     Elastic::ProcessBookkeepingService.clear_tracking!
-    Gitlab::Elastic::Helper.default.delete_index(index_name: @migrations_index_name)
   end
 
   config.include ElasticsearchHelpers, :elastic
