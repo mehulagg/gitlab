e2a5aecbd80811ce4a748f6739594fdcc732feeb
jerasmus <jerasmus@gitlab.com>
2020-11-20 13:15:56 +0200

    Add apply suggestion component
    Added an apply suggestion component

---
+++ app/assets/javascripts/vue_shared/components/markdown/apply_suggestion.vue
@@ -0,0 +1,58 @@
+<script>
+import { GlDropdown, GlDropdownForm, GlFormTextarea, GlButton } from '@gitlab/ui';
+import { __, sprintf } from '~/locale';
+
+export default {
+  components: { GlDropdown, GlDropdownForm, GlFormTextarea, GlButton },
+  props: {
+    disabled: {
+      type: Boolean,
+      required: true,
+    },
+    fileName: {
+      type: String,
+      required: true,
+    },
+  },
+  data() {
+    return {
+      message: null,
+      buttonText: __('Apply suggestion'),
+      headerText: __('Apply suggestion commit message'),
+    };
+  },
+  methods: {
+    onApply() {
+      this.$emit('apply', this.message || this.placeholderText);
+    },
+  },
+  computed: {
+    placeholderText() {
+      return sprintf(__('Apply suggestion on %{fileName}'), { fileName: this.fileName });
+    },
+  },
+};
+</script>
+
+<template>
+  <gl-dropdown
+    :text="buttonText"
+    :header-text="headerText"
+    :disabled="disabled"
+    boundary="window"
+    right="true"
+    menu-class="gl-w-full! gl-pb-0!"
+  >
+    <gl-dropdown-form class="gl-m-3!">
+      <gl-form-textarea :placeholder="placeholderText" />
+      <gl-button
+        class="js-apply-btn gl-w-quarter! gl-mt-3 gl-text-center! float-right"
+        category="secondary"
+        variant="success"
+        @click="onApply"
+      >
+        {{ __('Apply') }}
+      </gl-button>
+    </gl-dropdown-form>
+  </gl-dropdown>
+</template>
