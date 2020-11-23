8f63a5570187126e6bfbae98fa097338ff501bab
Emily Ring <ering@gitlab.com>
2020-11-23 10:06:36 -0500

    Added version field to terraform version
    Added version field to graphql state_version_type
    Updated related docs and tests

--- spec/graphql/types/terraform/state_version_type_spec.rb
+++ spec/graphql/types/terraform/state_version_type_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe GitlabSchema.types['TerraformStateVersion'] do
   it { expect(described_class).to require_graphql_authorizations(:read_terraform_state) }
 
   describe 'fields' do
-    let(:fields) { %i[id created_by_user job created_at updated_at] }
+    let(:fields) { %i[id created_by_user job created_at updated_at version] }
 
     it { expect(described_class).to have_graphql_fields(fields) }
 
@@ -16,5 +16,6 @@ RSpec.describe GitlabSchema.types['TerraformStateVersion'] do
     it { expect(described_class.fields['job'].type).not_to be_non_null }
     it { expect(described_class.fields['createdAt'].type).to be_non_null }
     it { expect(described_class.fields['updatedAt'].type).to be_non_null }
+    it { expect(described_class.fields['version'].type).not_to be_non_null }
   end
 end
