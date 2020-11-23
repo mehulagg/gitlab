213a21c88f814a2ec55401d4b977f12d5b81a7ae
Thong Kuah <tkuah@gitlab.com>
2020-11-24 09:52:07 +1300

    Autocorrect LastKeywordArgument for specs
    Based on deprecations/ files downloaded from
    https://gitlab.com/gitlab-org/gitlab/-/pipelines/220179974

--- ee/spec/support/shared_examples/services/geo/geo_request_service_shared_examples.rb
+++ ee/spec/support/shared_examples/services/geo/geo_request_service_shared_examples.rb
@@ -17,14 +17,14 @@ RSpec.shared_examples 'a geo RequestService' do
       allow(Gitlab::HTTP).to receive(:perform_request).and_return(response)
       expect(subject).to receive(:log_error).with("Could not connect to Geo primary node - HTTP Status Code: 401 Unauthorized\nTest")
 
-      expect(subject.execute(args)).to be_falsey
+      expect(subject.execute(**args)).to be_falsey
     end
 
     it 'alerts on bad SSL certficate' do
       allow(Gitlab::HTTP).to receive(:perform_request).and_raise(OpenSSL::SSL::SSLError.new('bad certificate'))
       expect(subject).to receive(:log_error).with(/Failed to Net::HTTP::(Put|Post) to primary url: /, kind_of(OpenSSL::SSL::SSLError))
 
-      expect(subject.execute(args)).to be_falsey
+      expect(subject.execute(**args)).to be_falsey
     end
 
     it 'handles connection refused' do
@@ -32,7 +32,7 @@ RSpec.shared_examples 'a geo RequestService' do
 
       expect(subject).to receive(:log_error).with(/Failed to Net::HTTP::(Put|Post) to primary url: /, kind_of(Errno::ECONNREFUSED))
 
-      expect(subject.execute(args)).to be_falsey
+      expect(subject.execute(**args)).to be_falsey
     end
 
     it 'returns meaningful error message when primary uses incorrect db key' do
@@ -43,13 +43,13 @@ RSpec.shared_examples 'a geo RequestService' do
         kind_of(OpenSSL::Cipher::CipherError)
       )
 
-      expect(subject.execute(args)).to be_falsey
+      expect(subject.execute(**args)).to be_falsey
     end
 
     it 'gracefully handles case when primary is deleted' do
       primary.destroy!
 
-      expect(subject.execute(args)).to be_falsey
+      expect(subject.execute(**args)).to be_falsey
     end
   end
 end
