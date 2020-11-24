1d33860e655e0a151ebae86a120d38c1fd95fc36
Alex Kalderimis <akalderimis@gitlab.com>
2020-11-24 17:55:21 +0000

    Create testable abstractions for arguments and fields
    This moves our GraphQL helpers away from string-orientated operations to
    the use of abstractions for arguments and field selections that are more
    testable, composable and debugging friendly.

--- spec/support_specs/helpers/graphql_helpers_spec.rb
+++ spec/support_specs/helpers/graphql_helpers_spec.rb
@@ -5,6 +5,58 @@ require 'spec_helper'
 RSpec.describe GraphqlHelpers do
   include GraphqlHelpers
 
+  describe '.all_graphql_fields_for' do
+    it 'returns a FieldSelection' do
+      selection = all_graphql_fields_for('User', max_depth: 1)
+
+      expect(selection).to be_a(::Graphql::FieldSelection)
+    end
+
+    it 'returns nil if the depth is too shallow' do
+      selection = all_graphql_fields_for('User', max_depth: 0)
+
+      expect(selection).to be_nil
+    end
+
+    it 'can select just the scalar fields' do
+      selection = all_graphql_fields_for('User', max_depth: 1)
+
+      expect(selection.paths.map(&:join))
+        .to match_array(%w[avatarUrl email groupCount id location name state username webPath webUrl])
+    end
+
+    it 'selects only as far as 3 levels by default' do
+      selection = all_graphql_fields_for('User')
+
+      expect(selection.paths).to all(have_attributes(size: (be <= 3)))
+
+      # Representative sample
+      expect(selection.paths).to include(
+        %w[userPermissions createSnippet],
+        %w[todos nodes id],
+        %w[starredProjects nodes name],
+        %w[authoredMergeRequests count],
+        %w[assignedMergeRequests pageInfo startCursor]
+      )
+    end
+
+    it 'selects only as far as requested' do
+      selection = all_graphql_fields_for('User', max_depth: 2)
+
+      expect(selection.paths).to all(have_attributes(size: (be <= 2)))
+    end
+
+    it 'omits fields that have required arguments' do
+      selection = all_graphql_fields_for('DesignCollection', max_depth: 3)
+
+      expect(selection.paths).not_to be_empty
+
+      expect(selection.paths).not_to include(
+        %w[designAtVersion id]
+      )
+    end
+  end
+
   describe '.graphql_mutation' do
     shared_examples 'correct mutation definition' do
       it 'returns correct mutation definition' do
