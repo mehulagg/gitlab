2b1abb8fa7811bc886591d3391f14040dadfb421
Albert Salim <asalim@gitlab.com>
2020-11-27 15:51:41 +0800

    FIXME: Dummy commit to trigger tests

--- spec/support/sidekiq.rb
+++ spec/support/sidekiq.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+# FIXME: Dummy commit
+
 RSpec.configure do |config|
   def gitlab_sidekiq_inline(&block)
     # We need to cleanup the queues before running jobs in specs because the
