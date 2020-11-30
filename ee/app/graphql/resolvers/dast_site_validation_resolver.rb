41c847090f11d3b454385b81e42ed785e5162bb0
Philip Cunningham <pcunningham@gitlab.com>
2020-11-30 13:02:39 +1100

    Filter DastSiteValidations by normalizedTargetUrl
    Allow filtering in GraphQL.

--- ee/app/graphql/resolvers/dast_site_validation_resolver.rb
+++ ee/app/graphql/resolvers/dast_site_validation_resolver.rb
@@ -6,13 +6,25 @@ module Resolvers
 
     type Types::DastSiteValidationType.connection_type, null: true
 
+    argument :normalized_target_url, GraphQL::STRING_TYPE, required: false,
+             description: 'Normalized URL of the target to be scanned'
+
+    when_single do
+      argument :target_url, GraphQL::STRING_TYPE, required: true,
+               description: 'target URL of the DAST Site Validation'
+    end
+
     def resolve(**args)
       unless ::Feature.enabled?(:security_on_demand_scans_site_validation, project)
         raise ::Gitlab::Graphql::Errors::ResourceNotAvailable, 'Feature disabled'
       end
 
-      url_base = DastSiteValidation.get_normalized_url_base(args[:target_url])
-      DastSiteValidationsFinder.new(project_id: project.id, url_base: url_base).execute.first
+      if args[:target_url]
+        url_base = DastSiteValidation.get_normalized_url_base(args[:target_url])
+        DastSiteValidationsFinder.new(project_id: project.id, url_base: url_base).execute
+      else
+        DastSiteValidationsFinder.new(project_id: project.id).execute
+      end
     end
   end
 end
--- ee/spec/graphql/resolvers/dast_site_validation_resolver_spec.rb
+++ ee/spec/graphql/resolvers/dast_site_validation_resolver_spec.rb
@@ -8,10 +8,10 @@ RSpec.describe Resolvers::DastSiteValidationResolver do
   let_it_be(:target_url) { generate(:url) }
   let_it_be(:current_user) { create(:user) }
   let_it_be(:project) { create(:project) }
-  let_it_be(:dast_site_token) { create(:dast_site_token, project: project, url: target_url) }
-  let_it_be(:dast_site_validation) { create(:dast_site_validation, dast_site_token: dast_site_token) }
-
-  subject { sync(resolve_dast_site_validations(target_url: target_url)) }
+  let_it_be(:dast_site_token1) { create(:dast_site_token, project: project, url: target_url) }
+  let_it_be(:dast_site_validation1) { create(:dast_site_validation, dast_site_token: dast_site_token1) }
+  let_it_be(:dast_site_token2) { create(:dast_site_token, project: project, url: generate(:url)) }
+  let_it_be(:dast_site_validation2) { create(:dast_site_validation, dast_site_token: dast_site_token2) }
 
   before do
     project.add_maintainer(current_user)
@@ -21,13 +21,25 @@ RSpec.describe Resolvers::DastSiteValidationResolver do
     expect(described_class).to have_nullable_graphql_type(Types::DastSiteValidationType.connection_type)
   end
 
-  it 'returns DAST site validation' do
-    is_expected.to eq(dast_site_validation)
+  context 'when resolving a single DAST site validation' do
+    subject { sync(single_dast_site_validation(target_url: target_url)) }
+
+    it { is_expected.to contain_exactly(dast_site_validation1) }
+  end
+
+  context 'when resolving multiple DAST site validations' do
+    subject { sync(dast_site_validations) }
+
+    it { is_expected.to contain_exactly(dast_site_validation2, dast_site_validation1) }
   end
 
   private
 
-  def resolve_dast_site_validations(args = {}, context = { current_user: current_user })
+  def dast_site_validations(args = {}, context = { current_user: current_user })
+    resolve(described_class, obj: project, args: args, ctx: context)
+  end
+
+  def single_dast_site_validation(args = {}, context = { current_user: current_user })
     resolve(described_class, obj: project, args: args, ctx: context)
   end
 end
