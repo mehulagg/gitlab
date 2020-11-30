e2af56d2fcbae4f7604bbf90b01d69f317c71732
Ezekiel Kigbo <ekigbo@gitlab.com>
2020-11-30 17:23:30 +1100

    [skip ci]  Add basic layout for default fields

--- ee/app/assets/javascripts/analytics/cycle_analytics/components/value_stream_form.vue
+++ ee/app/assets/javascripts/analytics/cycle_analytics/components/value_stream_form.vue
@@ -5,28 +5,18 @@ import {
   GlForm,
   GlFormInput,
   GlFormGroup,
+  GlFormText,
   GlModal,
-  GlFormRadioGroup,
 } from '@gitlab/ui';
 import { debounce } from 'lodash';
 import { mapState, mapActions } from 'vuex';
 import { sprintf, __, s__ } from '~/locale';
 import { capitalizeFirstCharacter } from '~/lib/utils/text_utility';
-// import { DEFAULT_STAGE_NAMES } from '../../constants';
 import { DATA_REFETCH_DELAY } from '../../shared/constants';
 
 const ERRORS = {
   MIN_LENGTH: s__('CreateValueStreamForm|Name is required'),
   MAX_LENGTH: s__('CreateValueStreamForm|Maximum length 100 characters'),
-};
-
-const defaultStageFields = {
-  name: '',
-  isCustom: true, // ? maybe?
-  startEventIdentifier: null,
-  startEventLabelId: null,
-  endEventIdentifier: null,
-  endEventLabelId: null,
 };
 
 const NAME_MAX_LENGTH = 100;
@@ -47,8 +37,7 @@ const I18N = {
   CREATE_VALUE_STREAM: __('Create Value Stream'),
   CREATED: __("'%{name}' Value Stream created"),
   CANCEL: __('Cancel'),
-  MODAL_TITLE: __('Value Stream Name'),
-  FIELD_NAME_LABEL: __('Name'),
+  FIELD_NAME_LABEL: __('Value Stream name'),
   FIELD_NAME_PLACEHOLDER: __('Example: My Value Stream'),
 };
 
@@ -63,14 +52,12 @@ const PRESET_OPTIONS = [
   },
 ];
 
-const DEFAULT_STAGE_CONFIG = ['issue', 'plan', 'code', 'test', 'review', 'staging', 'total'].map(
-  id => ({
-    id,
-    title: capitalizeFirstCharacter(id),
-    hidden: false,
-    custom: false,
-  }),
-);
+const DEFAULT_STAGE_CONFIG = ['issue', 'plan', 'code', 'test', 'review', 'staging'].map(id => ({
+  id,
+  name: capitalizeFirstCharacter(id),
+  hidden: false,
+  custom: false,
+}));
 
 export default {
   name: 'ValueStreamForm',
@@ -80,8 +67,8 @@ export default {
     GlForm,
     GlFormInput,
     GlFormGroup,
+    GlFormText,
     GlModal,
-    GlFormRadioGroup,
   },
   props: {
     initialData: {
@@ -89,16 +76,26 @@ export default {
       required: false,
       default: () => ({}),
     },
+    hasPathNavigation: {
+      type: Boolean,
+      required: false,
+      default: false,
+    },
   },
   data() {
-    console.log('DEFAULT_STAGE_CONFIG', DEFAULT_STAGE_CONFIG);
+    const { hasPathNavigation, initialData } = this;
+    const additionalFields = hasPathNavigation
+      ? {
+          selectedPreset: PRESET_OPTIONS[0].value,
+          presetOptions: PRESET_OPTIONS,
+          stages: DEFAULT_STAGE_CONFIG,
+          ...initialData,
+        }
+      : {};
     return {
-      errors: {},
       name: '',
-      selectedPreset: PRESET_OPTIONS[0].value,
-      presetOptions: PRESET_OPTIONS,
-      stages: [...DEFAULT_STAGE_CONFIG, { ...defaultStageFields }],
-      ...this.initialData,
+      errors: {},
+      ...additionalFields,
     };
   },
   computed: {
@@ -142,7 +139,6 @@ export default {
     } else {
       this.onHandleInput();
     }
-    console.log('mounted', this.stages);
   },
   methods: {
     ...mapActions(['createValueStream']),
@@ -150,18 +146,15 @@ export default {
       const { name } = this;
       this.errors = validate({ name });
     }, DATA_REFETCH_DELAY),
-    onAddStage() {
-      this.stages.push({ ...defaultStageFields });
-    },
     isFirstStage(i) {
       return i === 0;
     },
     isLastStage(i) {
-      return i === this.stages.length;
+      return i === this.stages?.length - 1;
     },
     onSubmit() {
-      const { name } = this;
-      return this.createValueStream({ name }).then(() => {
+      const { name, stages } = this;
+      return this.createValueStream({ name, stages }).then(() => {
         if (!this.hasFormErrors) {
           this.$toast.show(sprintf(this.$options.I18N.CREATED, { name }), {
             position: 'top-center',
@@ -178,18 +171,13 @@ export default {
   <gl-modal
     data-testid="value-stream-form-modal"
     modal-id="value-stream-form-modal"
-    :title="$options.I18N.MODAL_TITLE"
+    scrollable
+    :title="$options.I18N.CREATE_VALUE_STREAM"
     :action-primary="primaryProps"
     :action-cancel="{ text: $options.I18N.CANCEL }"
-    :action-secondary="{
-      text: s__('CreateValueStreamForm|Add another stage'),
-      attributes: [{ variant: 'info' }],
-    }"
-    @secondary.prevent="onAddStage"
     @primary.prevent="onSubmit"
   >
     <gl-form>
-      <gl-form-radio-group v-model="selectedPreset" :options="presetOptions" name="preset" />
       <gl-form-group
         :label="$options.I18N.FIELD_NAME_LABEL"
         label-for="create-value-stream-name"
@@ -206,29 +194,37 @@ export default {
           @input="onHandleInput"
         />
       </gl-form-group>
-      <hr />
-      <gl-form-group
-        v-for="(stage, i) in stages"
-        :key="stage.id"
-        :label="sprintf(__('Stage %{i}'), { i: i + 1 })"
-      >
-        <gl-form-input
-          v-if="stage.custom"
-          v-model.trim="stage.title"
-          :name="`create-value-stream-stage-${i}`"
-          :placeholder="s__('CreateValueStreamForm|Enter stage name')"
-          :state="isValid"
-          required
-          @input="onHandleInput"
-        />
-        <span v-else>{{ stage.title }}</span>
-        <gl-button-group>
-          <gl-button :disabled="isFirstStage(i)" icon="arrow-down" />
-          <gl-button :disabled="isLastStage(i)" icon="arrow-up" />
-        </gl-button-group>
-        &nbsp;
-        <gl-button icon="archive" />
-      </gl-form-group>
+      <div v-if="hasPathNavigation">
+        <hr />
+        <gl-form-group
+          v-for="(stage, i) in stages"
+          :key="stage.id"
+          :label="sprintf(__('Stage %{i}'), { i: i + 1 })"
+        >
+          <div class="gl-display-flex gl-flex-direction-row gl-justify-content-space-between">
+            <div>
+              <gl-form-input
+                v-if="stage.custom"
+                v-model.trim="stage.name"
+                :name="`create-value-stream-stage-${i}`"
+                :placeholder="s__('CreateValueStreamForm|Enter stage name')"
+                :state="isValid"
+                required
+                @input="onHandleInput"
+              />
+              <span v-else>{{ stage.name }}</span>
+            </div>
+            <div>
+              <gl-button-group>
+                <gl-button :disabled="isLastStage(i)" icon="arrow-down" />
+                <gl-button :disabled="isFirstStage(i)" icon="arrow-up" />
+              </gl-button-group>
+              &nbsp;
+              <gl-button icon="archive" />
+            </div>
+          </div>
+        </gl-form-group>
+      </div>
     </gl-form>
   </gl-modal>
 </template>
