84c290308c89471d77f3e07a0aa115a230de5097
Tom Quirk <tquirk@gitlab.com>
2020-11-30 14:23:04 +0930

    Begin moving some HAML to Vue

--- app/assets/javascripts/jira_connect/components/app.vue
+++ app/assets/javascripts/jira_connect/components/app.vue
@@ -1,3 +1,39 @@
+<script>
+export default {
+  name: 'JiraConnectApp',
+  props: {
+    isGitlabConnected: {
+      type: String,
+      required: true,
+    },
+    jiraConnectUsersPath: {
+      type: String,
+      required: true,
+    },
+  },
+};
+</script>
+
 <template>
-  <div></div>
+  <div>
+    <h1>GitLab for Jira Configuration</h1>
+
+    <template v-if="!isGitlabConnected">
+      <h2 class="heading-with-border">Sign in to GitLab.com to get started.</h2>
+
+      <div class="gl-mt-5">
+        <a
+          :href="jiraConnectUsersPath"
+          target="_blank"
+          class="ak-button ak-button__appearance-primary js-jira-connect-sign-in"
+        >
+          Sign in to GitLab
+        </a>
+      </div>
+
+      <div class="gl-mt-5">
+        <p>Note: this integration only works with accounts on GitLab.com (SaaS).</p>
+      </div>
+    </template>
+  </div>
 </template>
--- app/assets/javascripts/jira_connect/index.js
+++ app/assets/javascripts/jira_connect/index.js
@@ -1,13 +1,25 @@
 import Vue from 'vue';
 import App from './components/app.vue';
 
+const parseProps = dataset => {
+  const { isGitlabConnected, jiraConnectUsersPath } = dataset;
+
+  return {
+    jiraConnectUsersPath,
+    isGitlabConnected: JSON.parse(isGitlabConnected),
+  };
+};
+
 function initJiraConnect() {
   const el = document.querySelector('.js-jira-connect-app');
+  if (!el) return null;
 
   return new Vue({
     el,
     render(createElement) {
-      return createElement(App, {});
+      return createElement(App, {
+        props: parseProps(el.dataset),
+      });
     },
   });
 }
