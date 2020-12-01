52a6ed95ace47c29b3ac30a0c0f9b04f86e87208
Aleksei Lipniagov <alipniagov@gitlab.com>
2020-12-01 16:48:07 +0300

    Update puma & puma_worker_killer to upstream (5.1)
    Update Puma and Puma Worker Killer gems

--- config/puma.example.development.rb
+++ config/puma.example.development.rb
@@ -79,6 +79,9 @@ tag 'gitlab-puma-worker'
 #
 worker_timeout 60
 
+# https://github.com/puma/puma/blob/master/5.0-Upgrade.md#lower-latency-better-throughput
+wait_for_less_busy_worker 0.001
+
 # Use json formatter
 require_relative "/home/git/gitlab/lib/gitlab/puma_logging/json_formatter"
 
