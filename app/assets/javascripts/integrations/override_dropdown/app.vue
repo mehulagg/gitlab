bc64db78149b2580f0f51d8cdd8a4a8ca3d2eeb6
Tom Quirk <tquirk@gitlab.com>
2020-12-01 14:14:41 +0930

    PoC: separate Vue app for integ. override dropdown

---
+++ app/assets/javascripts/integrations/override_dropdown/app.vue
@@ -0,0 +1,29 @@
+<script>
+import { mapState, mapActions, mapGetters } from 'vuex';
+
+import OverrideDropdown from '../edit/components/override_dropdown.vue';
+
+export default {
+  name: 'IntegrationForm',
+  components: {
+    OverrideDropdown,
+  },
+  computed: {
+    ...mapGetters(['propsSource']),
+    ...mapState(['defaultState', 'override']),
+  },
+  methods: {
+    ...mapActions(['setOverride']),
+  },
+};
+</script>
+
+<template>
+  <override-dropdown
+    v-if="defaultState !== null"
+    :inherit-from-id="defaultState.id"
+    :override="override"
+    :learn-more-path="propsSource.learnMorePath"
+    @change="setOverride"
+  />
+</template>
