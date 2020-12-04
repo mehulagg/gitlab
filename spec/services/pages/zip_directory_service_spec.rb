252fb49cc83e877cf43d658f8252f6362762e2da
Vladimir Shushlin <v.shushlin@gmail.com>
2020-12-04 20:04:33 +0300

    WIP

--- spec/services/pages/zip_directory_service_spec.rb
+++ spec/services/pages/zip_directory_service_spec.rb
@@ -3,13 +3,35 @@
 require 'spec_helper'
 
 RSpec.describe Pages::ZipDirectoryService do
-  def create_pages_directory(dir)
-    pages_dir = File.join(dir, "/public")
+  let(:dirs) do
+    [
+      "public",
+      "public/nested"
+    ]
+  end
 
-    Dir.mkdir pages_dir
+  let(:files) do
+    [
+      ["public/index.html", "index"],
+      ["public/nested/nested.html", "nested"]
+    ]
+  end
 
-    File.open(File.join(pages_dir, "index.html"), "w") do |f|
-      f.write("some html")
+  let(:links) do
+    [
+      []
+    ]
+  end
+
+  def create_pages_directory(work_dir)
+    dirs.each do |dir|
+      Dir.mkdir File.join(work_dir, dir)
+    end
+
+    files.each do |file_name, file_content|
+      File.open(File.join(work_dir, file_name), "w") do |f|
+        f.write(file_content)
+      end
     end
   end
 
@@ -22,9 +44,19 @@ RSpec.describe Pages::ZipDirectoryService do
       described_class.new(dir, archive).execute
 
       Zip::File.open(archive) do |zip_file|
-        expect(zip_file.entries.count).to eq(2)
-        expect(zip_file.glob('public').first.name).to eq("public/")
+        expect(zip_file.entries.count).to eq(dirs.count + files.count)
+
+        dirs.each do |dir|
+          entry = zip_file.glob(dir).first
+          expect(entry.name).to eq(dir + "/")
+          expect(entry.ftype).to eq(:directory)
+        end
+
+        files.each do |file_name, file_content|
+          entry = zip_file.glob(file_name).first
+        end
         expect(zip_file.glob('public/index.html').first.name).to eq("public/index.html")
+        binding.pry
       end
     end
   end
