bb663a819d89467d45684f440cfe3605f542bc61
Drew Blessing <drew@blessing.io>
2020-12-04 16:02:23 -0600

    Allow SAML response to set certain user attributes on creation
    Allow SAML to set user attributes via additional attribute
    statements in the SAML response. These allowed attributes are
    currently limited to `can_create_group` and `projects_limit`.

--- ee/lib/gitlab/auth/group_saml/auth_hash.rb
+++ ee/lib/gitlab/auth/group_saml/auth_hash.rb
@@ -4,8 +4,19 @@ module Gitlab
   module Auth
     module GroupSaml
       class AuthHash < Gitlab::Auth::Saml::AuthHash
+        ALLOWED_USER_ATTRIBUTES = %w(can_create_group projects_limit).freeze
+
         def groups
           Array.wrap(get_raw('groups') || get_raw('Groups'))
+        end
+
+        ALLOWED_USER_ATTRIBUTES.each do |attribute|
+          define_method(attribute) do
+            value = get_raw(attribute)
+            return unless value
+
+            Array(value).first
+          end
         end
       end
     end
