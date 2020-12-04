2d06c506ac26c96f602e888ffc3f33d5c79e2716
serenafang <sfang@gitlab.com>
2020-12-04 00:42:47 +0000

    Add specs to audit PAT
    Add success and fail specs for admin and non-admin

--- ee/spec/services/personal_access_tokens/create_service_audit_log_spec.rb
+++ ee/spec/services/personal_access_tokens/create_service_audit_log_spec.rb
@@ -3,20 +3,60 @@
 require 'spec_helper'
 
 RSpec.describe PersonalAccessTokens::CreateService do
+
   describe '#execute' do
-    subject { service.execute }
-
     let(:user) { create(:user) }
-    let(:params) { { name: 'Test token', impersonation: true, scopes: [:api], expires_at: Date.today + 1.month } }
-    let(:service) { described_class.new(current_user: user, target_user: user, params: params) }
+    let(:params) { { name: 'admin-token', impersonation: true, scopes: [:api], expires_at: Date.today + 1.month } }
 
-    it 'creates audit logs' do
-      expect(::AuditEventService)
-        .to receive(:new)
-        .with(user, user, action: :custom, custom_message: /Created personal access token with id \d+/, ip_address: nil)
-        .and_call_original
+    context 'when non-admin user' do
+      context 'when user creates their own token'
+        it 'creates audit logs with success message' do
+          expect(::AuditEventService)
+            .to receive(:new)
+            .with(user, user, action: :custom, custom_message: /Created personal access token with id \d+/, ip_address: nil)
+            .and_call_original
 
-      subject
+          PersonalAccessTokens::CreateService.new(current_user: user, target_user: user, params: params).execute
+        end
+      end
+
+      context 'when user attempts to create a token for a different user' do
+        let(:other_user) { create(:user) }
+
+        it 'creates audit logs with failure message' do
+          expect(::AuditEventService)
+            .to receive(:new)
+            .with(user, other_user, action: :custom, custom_message: 'Attempted to create personal access token but failed with message: Not permitted to create', ip_address: nil)
+            .and_call_original
+
+          PersonalAccessTokens::CreateService.new(current_user: user, target_user: other_user, params: params).execute
+        end
+      end
+    end
+
+    context 'when admin' do
+      let(:admin) { create(:user, :admin) }
+
+      context 'with admin mode enabled', :enable_admin_mode do
+        it 'creates audit logs with success message' do
+          expect(::AuditEventService)
+            .to receive(:new)
+            .with(admin, user, action: :custom, custom_message: /Created personal access token with id \d+/, ip_address: nil)
+            .and_call_original
+
+          PersonalAccessTokens::CreateService.new(current_user: admin, target_user: user, params: params).execute
+        end
+      end
+
+      context 'with admin mode disabled' do
+        it 'creates audit logs with failure message' do
+          expect(::AuditEventService)
+          .to receive(:new)
+          .with(admin, user, action: :custom, custom_message: 'Attempted to create personal access token but failed with message: Not permitted to create', ip_address: nil)
+          .and_call_original
+
+        PersonalAccessTokens::CreateService.new(current_user: admin, target_user: user, params: params).execute
+      end
     end
   end
 end
