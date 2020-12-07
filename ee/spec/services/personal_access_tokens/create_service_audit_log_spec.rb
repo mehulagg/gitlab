a0356cf3d016139f81fedadcc0b87cd44e9b997f
serenafang <sfang@gitlab.com>
2020-12-07 19:18:35 +0000

    Apply MR review suggestions
    Repeated block into new method

--- ee/spec/services/personal_access_tokens/create_service_audit_log_spec.rb
+++ ee/spec/services/personal_access_tokens/create_service_audit_log_spec.rb
@@ -4,18 +4,15 @@ require 'spec_helper'
 
 RSpec.describe PersonalAccessTokens::CreateService do
   describe '#execute' do
-    let(:user) { create(:user) }
+    let_it_be(:user) { create(:user) }
     let(:params) { { name: 'admin-token', impersonation: true, scopes: [:api], expires_at: Date.today + 1.month } }
 
     context 'when non-admin user' do
       context 'when user creates their own token' do
         it 'creates audit logs with success message' do
-          expect(::AuditEventService)
-            .to receive(:new)
-            .with(user, user, action: :custom, custom_message: /Created personal access token with id \d+/, ip_address: nil)
-            .and_call_original
+          expect_to_log_failure(user, user, /Created personal access token with id \d+/)
 
-          PersonalAccessTokens::CreateService.new(current_user: user, target_user: user, params: params).execute
+          described_class.new(current_user: user, target_user: user, params: params).execute
         end
       end
 
@@ -23,12 +20,9 @@ RSpec.describe PersonalAccessTokens::CreateService do
         let(:other_user) { create(:user) }
 
         it 'creates audit logs with failure message' do
-          expect(::AuditEventService)
-            .to receive(:new)
-            .with(user, other_user, action: :custom, custom_message: 'Attempted to create personal access token but failed with message: Not permitted to create', ip_address: nil)
-            .and_call_original
+          expect_to_log_failure(user, other_user,  'Attempted to create personal access token but failed with message: Not permitted to create')
 
-          PersonalAccessTokens::CreateService.new(current_user: user, target_user: other_user, params: params).execute
+          described_class.new(current_user: user, target_user: other_user, params: params).execute
         end
       end
     end
@@ -36,27 +30,25 @@ RSpec.describe PersonalAccessTokens::CreateService do
     context 'when admin' do
       let(:admin) { create(:user, :admin) }
 
-      context 'with admin mode enabled', :enable_admin_mode do
-        it 'creates audit logs with success message' do
-          expect(::AuditEventService)
-            .to receive(:new)
-            .with(admin, user, action: :custom, custom_message: /Created personal access token with id \d+/, ip_address: nil)
-            .and_call_original
+      it 'with admin mode enabled', :enable_admin_mode do
+        expect_to_log_failure(admin, user, /Created personal access token with id \d+/)
 
-          PersonalAccessTokens::CreateService.new(current_user: admin, target_user: user, params: params).execute
-        end
+        described_class.new(current_user: admin, target_user: user, params: params).execute
       end
 
       context 'with admin mode disabled' do
         it 'creates audit logs with failure message' do
-          expect(::AuditEventService)
-            .to receive(:new)
-            .with(admin, user, action: :custom, custom_message: 'Attempted to create personal access token but failed with message: Not permitted to create', ip_address: nil)
-            .and_call_original
+          expect_to_log_failure(admin, user, 'Attempted to create personal access token but failed with message: Not permitted to create')
 
-          PersonalAccessTokens::CreateService.new(current_user: admin, target_user: user, params: params).execute
+          described_class.new(current_user: admin, target_user: user, params: params).execute
         end
       end
     end
+  end
+
+  def expect_to_log_failure(current_user, target_user, message)
+    expect(::AuditEventService).to receive(:new)
+      .with(current_user, target_user, action: :custom, custom_message: message, ip_address: nil)
+      .and_call_original
   end
 end
