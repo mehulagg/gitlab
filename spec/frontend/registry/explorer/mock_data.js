f329e44527cdee26d105659ff7804903499ff545
Nicolò Maria Mezzopera <nicolo.mezzopera@gmail.com>
2020-12-07 12:22:45 +0100

    Refactor details page to graphql

--- spec/frontend/registry/explorer/mock_data.js
+++ spec/frontend/registry/explorer/mock_data.js
@@ -192,3 +192,96 @@ export const graphQLImageDeleteMockError = {
     },
   },
 };
+
+export const containerRepositoryMock = {
+  id: 'gid://gitlab/ContainerRepository/26',
+  name: 'rails-12009',
+  path: 'gitlab-org/gitlab-test/rails-12009',
+  status: null,
+  location: 'host.docker.internal:5000/gitlab-org/gitlab-test/rails-12009',
+  canDelete: true,
+  createdAt: '2020-11-03T13:29:21Z',
+  tagsCount: 13,
+  expirationPolicyStartedAt: null,
+};
+
+export const tagsPageInfo = {
+  __typename: 'PageInfo',
+  hasNextPage: true,
+  hasPreviousPage: true,
+  startCursor: 'MQ',
+  endCursor: 'MTA',
+};
+
+export const tagsMock = [
+  {
+    digest: 'sha256:2cf3d2fdac1b04a14301d47d51cb88dcd26714c74f91440eeee99ce399089062',
+    location: 'host.docker.internal:5000/gitlab-org/gitlab-test/rails-12009:beta-24753',
+    path: 'gitlab-org/gitlab-test/rails-12009:beta-24753',
+    name: 'beta-24753',
+    revision: 'c2613843ab33aabf847965442b13a8b55a56ae28837ce182627c0716eb08c02b',
+    shortRevision: 'c2613843a',
+    createdAt: '2020-11-03T13:29:38+00:00',
+    totalSize: 105,
+    canDelete: true,
+    __typename: 'ContainerRepositoryTag',
+  },
+  {
+    digest: 'sha256:7f94f97dff89ffd122cafe50cd32329adf682356a7a96f69cbfe313ee589791c',
+    location: 'host.docker.internal:5000/gitlab-org/gitlab-test/rails-12009:beta-31075',
+    path: 'gitlab-org/gitlab-test/rails-12009:beta-31075',
+    name: 'beta-31075',
+    revision: 'df44e7228f0f255c73e35b6f0699624a615f42746e3e8e2e4b3804a6d6fc3292',
+    shortRevision: 'df44e7228',
+    createdAt: '2020-11-03T13:29:32+00:00',
+    totalSize: 104,
+    canDelete: true,
+    __typename: 'ContainerRepositoryTag',
+  },
+];
+
+export const graphQLImageDetailsMock = override => ({
+  data: {
+    containerRepository: {
+      ...containerRepositoryMock,
+
+      tags: {
+        nodes: tagsMock,
+        pageInfo: { ...tagsPageInfo },
+        __typename: 'ContainerRepositoryTagConnection',
+      },
+      __typename: 'ContainerRepositoryDetails',
+      ...override,
+    },
+  },
+});
+
+export const graphQLImageDetailsEmptyTagsMock = {
+  data: {
+    containerRepository: {
+      ...containerRepositoryMock,
+      tags: {
+        nodes: [],
+        pageInfo: {
+          __typename: 'PageInfo',
+          hasNextPage: false,
+          hasPreviousPage: false,
+          startCursor: '',
+          endCursor: '',
+        },
+        __typename: 'ContainerRepositoryTagConnection',
+      },
+      __typename: 'ContainerRepositoryDetails',
+    },
+  },
+};
+
+export const graphQLDeleteImageRepositoryTagsMock = {
+  data: {
+    destroyContainerRepositoryTags: {
+      deletedTagNames: [],
+      errors: [],
+      __typename: 'DestroyContainerRepositoryTagsPayload',
+    },
+  },
+};
