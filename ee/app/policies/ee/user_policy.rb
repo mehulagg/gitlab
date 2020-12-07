8193e8224a303ed72fdc161dedd340191b8294c3
Aishwarya Subramanian <asubramanian@gitlab.com>
2020-12-07 13:23:30 -0600

    Added querying

--- ee/app/policies/ee/user_policy.rb
+++ ee/app/policies/ee/user_policy.rb
@@ -10,18 +10,12 @@ module EE
         ::Gitlab::CurrentSettings.current_application_settings.updating_name_disabled_for_users
       end
 
-      condition(:export_user_permissions_available) do
-        ::License.feature_available?(:export_user_permissions)
-      end
-
       rule { can?(:update_user) }.enable :update_name
 
       rule do
         updating_name_disabled_for_users &
         ~admin
       end.prevent :update_name
-
-      rule { export_user_permissions_available & admin }.enable :export_user_permissions
     end
   end
 end
--- ee/app/services/members/permissions_export_service.rb
+++ ee/app/services/members/permissions_export_service.rb
@@ -12,7 +12,7 @@ module Members
     def csv_data
       return ServiceResponse.error(message: 'Insufficient permissions') unless allowed?
 
-      ServiceResponse.success
+      ServiceResponse.success(payload: csv_builder.render(TARGET_FILESIZE)) #change to use async menthod
     end
 
     private
@@ -21,6 +21,25 @@ module Members
 
     def allowed?
       current_user.can?(:export_user_permissions)
+    end
+
+    def csv_builder
+      @csv_builder ||= CsvBuilder.new(data, header_to_value_hash)
+    end
+
+    def data
+      Member.includes(:user, source: [children: :route])
+    end
+
+    def header_to_value_hash
+      {
+        'Username' => -> (member) { member.user&.username },
+        'Email' => -> (member) { member.user&.email },
+        'Type' => 'source_type',
+        'Path' => -> (member) { member.source&.path },
+        'Access Level' => -> (member) { member.human_access },
+        'Inherited memberships' => -> (member) { member.source.children.map(&:full_path).join('|') if member.source_type == 'Namespace' }
+      }
     end
   end
 end
