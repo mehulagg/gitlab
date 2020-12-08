f8e72403f8ea0556f8229a73ec092ff3597384bd
Vladimir Shushlin <v.shushlin@gmail.com>
2020-12-08 14:45:48 +0300

    Improve specs for zip directory service

--- spec/services/pages/zip_directory_service_spec.rb
+++ spec/services/pages/zip_directory_service_spec.rb
@@ -3,17 +3,43 @@
 require 'spec_helper'
 
 RSpec.describe Pages::ZipDirectoryService do
-  def create_pages_directory(dir)
-    pages_dir = File.join(dir, "/public")
+  let(:dirs) do
+    [
+      "public",
+      "public/nested"
+    ]
+  end
 
-    Dir.mkdir pages_dir
+  let(:files) do
+    [
+      ["public/index.html", "index"],
+      ["public/nested/nested.html", "nested"]
+    ]
+  end
 
-    File.open(File.join(pages_dir, "index.html"), "w") do |f|
-      f.write("some html")
+  let(:links) do
+    [
+      ["public/link.html", "../index.html"]
+    ]
+  end
+
+  def create_pages_directory(work_dir)
+    dirs.each do |dir|
+      Dir.mkdir File.join(work_dir, dir)
+    end
+
+    files.each do |file_name, file_content|
+      File.open(File.join(work_dir, file_name), "w") do |f|
+        f.write(file_content)
+      end
+    end
+
+    links.each do |new_name, target|
+      File.symlink target, File.join(work_dir, new_name)
     end
   end
 
-  it 'zip pages directory' do
+  it 'zips pages directory' do
     archive = Tempfile.new("archive.zip")
 
     Dir.mktmpdir do |dir|
@@ -22,9 +48,25 @@ RSpec.describe Pages::ZipDirectoryService do
       described_class.new(dir, archive).execute
 
       Zip::File.open(archive) do |zip_file|
-        expect(zip_file.entries.count).to eq(2)
-        expect(zip_file.glob('public').first.name).to eq("public/")
-        expect(zip_file.glob('public/index.html').first.name).to eq("public/index.html")
+        expect(zip_file.entries.count).to eq(dirs.count + files.count + links.count)
+
+        dirs.each do |dir|
+          entry = zip_file.glob(dir).first
+          expect(entry.name).to eq(dir + "/")
+          expect(entry.ftype).to eq(:directory)
+        end
+
+        files.each do |file_name, file_content|
+          entry = zip_file.glob(file_name).first
+          expect(entry.name).to eq(file_name)
+          expect(entry.get_input_stream.read).to eq(file_content)
+        end
+
+        links.each do |new_name, target|
+          entry = zip_file.glob(new_name).first
+          expect(entry.name).to eq(new_name)
+          expect(entry.get_input_stream.read).to eq(target)
+        end
       end
     end
   end
