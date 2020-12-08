80e7bb72f559a0d6e3b21b9bd450445ee57c7fd9
Mark Lapierre <mlapierre@gitlab.com>
2020-12-08 13:43:50 +1100

    Set the default branch name

--- db/fixtures/production/001_application_settings.rb
+++ db/fixtures/production/001_application_settings.rb
@@ -2,3 +2,6 @@
 
 puts "Creating the default ApplicationSetting record.".color(:green)
 Gitlab::CurrentSettings.current_application_settings
+
+puts "Set the default branch name.".color(:green)
+ApplicationSetting.current_without_cache.update!(default_branch_name: 'main')
