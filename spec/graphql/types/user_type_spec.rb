edb1fae575053331a619f011bfcb943a78167434
Alex Kalderimis <akalderimis@gitlab.com>
2020-12-08 11:28:58 +0000

    Add support for MRs assigned for review
    This is in analogue to authoredMergeRequests and assignedMergeRequests.
    
    Support for this feature is complete at the model and  finder level, and
    this change exposes that at the API level.

--- spec/graphql/types/user_type_spec.rb
+++ spec/graphql/types/user_type_spec.rb
@@ -25,6 +25,7 @@ RSpec.describe GitlabSchema.types['User'] do
       location
       authoredMergeRequests
       assignedMergeRequests
+      reviewedMergeRequests
       groupMemberships
       groupCount
       projectMemberships
