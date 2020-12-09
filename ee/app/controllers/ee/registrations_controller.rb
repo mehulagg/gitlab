3eb8fccffc5bda2d462db3f3026ef40a9f3207f5
Drew Blessing <drew@blessing.io>
2020-12-09 14:48:02 -0600

    GitLab.com users without password must contact to delete account
    Require GitLab.com users to contact support for account deletion
    when they do not have a local password set. For example, when
    they created their account via OAuth sign-in. This ensures
    we are able to verify ownership/identity prior to deletion.

--- ee/app/controllers/ee/registrations_controller.rb
+++ ee/app/controllers/ee/registrations_controller.rb
@@ -5,11 +5,19 @@ module EE
     extend ActiveSupport::Concern
     extend ::Gitlab::Utils::Override
 
+    prepended do
+      before_action :ensure_can_remove_self, only: [:destroy]
+    end
+
     private
 
     override :set_blocked_pending_approval?
     def set_blocked_pending_approval?
       super || ::Gitlab::CurrentSettings.should_apply_user_signup_cap?
+    end
+
+    def ensure_can_remove_self?
+      current_user&.can_remove_self?
     end
   end
 end
