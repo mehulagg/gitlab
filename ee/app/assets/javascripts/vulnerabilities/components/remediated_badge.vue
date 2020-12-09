fc87418af916bd1e4495e31ecd03969132d0b9cf
Savas Vedova <svedova@gitlab.com>
2020-12-09 23:57:33 +0300

    Fix console errors when changing filter
    - Add changelog

--- ee/app/assets/javascripts/vulnerabilities/components/remediated_badge.vue
+++ ee/app/assets/javascripts/vulnerabilities/components/remediated_badge.vue
@@ -7,6 +7,11 @@ export default {
     GlPopover,
     GlBadge,
   },
+  computed: {
+    target() {
+      return this.$refs.badge?.$el;
+    },
+  },
 };
 </script>
 
@@ -22,7 +27,7 @@ export default {
           'The vulnerability is no longer detected. Verify the vulnerability has been fixed or removed before changing its status.',
         )
       "
-      :target="() => $refs.badge.$el"
+      :target="() => target"
       :title="__('Vulnerability remediated. Review before resolving.')"
       placement="top"
       triggers="hover focus"
