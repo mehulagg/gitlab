9f1e825418b9ae98218b43f67150ff41018c0e5a
Lee Tickett <lee@tickett.net>
2020-12-10 08:19:14 +0000

    Applying suggestions

--- db/migrate/20201209163113_recreate_index_issue_email_participants_on_issue_id_and_email.rb
+++ db/migrate/20201209163113_recreate_index_issue_email_participants_on_issue_id_and_email.rb
@@ -7,13 +7,17 @@ class RecreateIndexIssueEmailParticipantsOnIssueIdAndEmail < ActiveRecord::Migra
 
   disable_ddl_transaction!
 
+  OLD_INDEX_NAME = 'index_issue_email_participants_on_issue_id_and_email'
+  NEW_INDEX_NAME = 'index_issue_email_participants_on_issue_id_and_lower_email'
+
   def up
-    add_concurrent_index :issue_email_participants, 'issue_id, lower(email)', unique: true, name: 'index_issue_email_participants_on_issue_id_and_lower_email'
-    remove_concurrent_index_by_name :issue_email_participants, 'index_issue_email_participants_on_issue_id_and_email'
+    # This table is currently empty, so no need to worry about unique index violations
+    add_concurrent_index :issue_email_participants, 'issue_id, lower(email)', unique: true, name: NEW_INDEX_NAME
+    remove_concurrent_index_by_name :issue_email_participants, OLD_INDEX_NAME
   end
 
   def down
-    add_concurrent_index :issue_email_participants, [:issue_id, :email], unique: true, name: 'index_issue_email_participants_on_issue_id_and_email'
-    remove_concurrent_index_by_name :issue_email_participants, 'index_issue_email_participants_on_issue_id_and_lower_email'
+    add_concurrent_index :issue_email_participants, [:issue_id, :email], unique: true, name: OLD_INDEX_NAME
+    remove_concurrent_index_by_name :issue_email_participants, NEW_INDEX_NAME
   end
 end
