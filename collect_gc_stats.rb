5ee5e6d9ace34d94b11546ea537a4d1b8df0c613
Matthias Kaeppler <mkaeppler@gitlab.com>
2020-12-11 11:41:20 +0100

    Add support to run GC stats in parallel

--- collect_gc_stats.rb
+++ collect_gc_stats.rb
@@ -3,18 +3,16 @@
 
 require 'benchmark'
 
-GROWTH_SETTINGS = {
+SETTINGS = {
+  'DEFAULTS' => [''],
   # Default: 10_000
   'RUBY_GC_HEAP_INIT_SLOTS' => %w[100000 1000000 5000000],
   # Default: 1.8
   'RUBY_GC_HEAP_GROWTH_FACTOR' => %w[1.2 1 0.8],
   # Default: 0 (disabled)
-  'RUBY_GC_HEAP_GROWTH_MAX_SLOTS' => %w[100000 10000 1000],
+  'RUBY_GC_HEAP_GROWTH_MAX_SLOTS' => %w[10000 100000 1000000],
   # Default: 2.0
-  'RUBY_GC_HEAP_OLDOBJECT_LIMIT_FACTOR' => %w[2.5 1.5 1]
-}.freeze
-
-FREE_SETTINGS = {
+  'RUBY_GC_HEAP_OLDOBJECT_LIMIT_FACTOR' => %w[2.5 1.5 1],
   # Default: 4096 (= 2^12)
   'RUBY_GC_HEAP_FREE_SLOTS' => %w[16384 2048 0],
   # Default: 0.20 (20%)
@@ -22,19 +20,13 @@ FREE_SETTINGS = {
   # Default: 0.40 (40%)
   'RUBY_GC_HEAP_FREE_SLOTS_GOAL_RATIO' => %w[0.2 0.01 0.001],
   # Default: 0.65 (65%)
-  'RUBY_GC_HEAP_FREE_SLOTS_MAX_RATIO' => %w[0.2 0.02 0.002]
-}.freeze
-
-MALLOC_SETTINGS = {
+  'RUBY_GC_HEAP_FREE_SLOTS_MAX_RATIO' => %w[0.2 0.02 0.002],
   # Default: 16MB
   'RUBY_GC_MALLOC_LIMIT' => %w[8388608 4194304 1048576],
   # Default: 32MB
   'RUBY_GC_MALLOC_LIMIT_MAX' => %w[16777216 8388608 1048576],
   # Default: 1.4
-  'RUBY_GC_MALLOC_LIMIT_GROWTH_FACTOR' => %w[1.6 1 0.8]
-}.freeze
-
-OLDMALLOC_SETTINGS = {
+  'RUBY_GC_MALLOC_LIMIT_GROWTH_FACTOR' => %w[1.6 1 0.8],
   # Default: 16MB
   'RUBY_GC_OLDMALLOC_LIMIT' => %w[8388608 4194304 1048576],
   # Default: 128MB
@@ -43,13 +35,9 @@ OLDMALLOC_SETTINGS = {
   'RUBY_GC_OLDMALLOC_LIMIT_GROWTH_FACTOR' => %w[1.4 1 0.8]
 }.freeze
 
-ALL_SETTINGS = [
-  GROWTH_SETTINGS, FREE_SETTINGS, MALLOC_SETTINGS, OLDMALLOC_SETTINGS
-].freeze
-
-ALL_ENV_VARS = ALL_SETTINGS.flat_map(&:keys)
-
-OUTFILE = './tmp/gc_settings_exp/gc_stats.csv'
+OUTDIR = './tmp/gc_settings_exp'
+OUTFILE = OUTDIR + '/gc_stats.csv'
+TEMPFILES = "#{OUTDIR}/*.csv.*"
 
 ALL_GCSTAT_KEYS = [
   :heap_allocated_pages,
@@ -95,14 +83,6 @@ USED_GCSTAT_KEYS = [
 CSV_USED_GCSTAT_KEYS = USED_GCSTAT_KEYS.join(',')
 CSV_HEADER = "setting,value,#{CSV_USED_GCSTAT_KEYS},RSS,gc_time_s,time_s\n"
 
-def print_env
-  ALL_ENV_VARS.each { |v| print_envvar(v) }
-end
-
-def print_envvar(var)
-  puts "#{$$}: #{var}=#{ENV[var]}"
-end
-
 def with_gc_setting(key, value, outfile)
   ENV[key] = value
   ENV['OUTFILE'] = outfile
@@ -125,20 +105,31 @@ def collect_stats(setting, value, outfile)
   File.open(outfile, 'a') { |f| f << ",#{time_s}\n" }
 end
 
-elapsed = Benchmark.realtime do
-  # run baseline calibration round
-  File.open(OUTFILE, 'w') do |f|
-    f << CSV_HEADER
-  end
-  collect_stats('DEFAULTS', '', OUTFILE)
+par = ENV['PAR']&.to_i || 2
+batch_size = (SETTINGS.size.to_f / par).ceil
+batches = SETTINGS.each_slice(batch_size)
 
-  ALL_SETTINGS.each_with_index do |settings_batch, n|
-    settings_batch.each do |setting, values|
-      values.each do |v|
-        collect_stats(setting, v, OUTFILE)
+puts "Requested parallelism: #{par} (batches: #{batches.size}, batch size: #{batch_size})"
+
+elapsed = Benchmark.realtime do
+  `rm -f #{TEMPFILES}`
+
+  batches.each_with_index do |settings_batch, n|
+    Process.fork do
+      batch_file = "#{OUTFILE}.#{n}"
+      settings_batch.each do |setting, values|
+        values.each do |v|
+          collect_stats(setting, v, batch_file)
+        end
       end
     end
   end
+
+  Process.waitall
+
+  File.open(OUTFILE, 'w') { |f| f << CSV_HEADER }
+ensure
+  `cat #{TEMPFILES} >> #{OUTFILE}`
 end
 
 puts "All done in #{elapsed} sec"
