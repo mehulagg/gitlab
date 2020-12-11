22a02406048acd948ac4b82d2c19db3891b95761
Arturo Herrero <arturo.herrero@gmail.com>
2020-12-11 15:09:25 +0000

    Apply 1 suggestion(s) to 1 file(s)

--- spec/models/service_spec.rb
+++ spec/models/service_spec.rb
@@ -921,6 +921,6 @@ RSpec.describe Service do
   end
 
   describe '.project_specific_services_names' do
-    it { expect(described_class.project_specific_services_names).to include('alerts', 'jenkins') }
+    it { expect(described_class.project_specific_services_names).to include(Service::PROJECT_SPECIFIC_SERVICE_NAMES) }
   end
 end
