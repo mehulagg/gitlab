887c22555b2da20c045cf654a56d70c159fd909a
Jonathan Schafer <jschafer@gitlab.com>
2020-12-14 11:48:49 -0600

    User count resolvers for EpicType

--- spec/graphql/resolvers/user_notes_count_resolver_spec.rb
+++ spec/graphql/resolvers/user_notes_count_resolver_spec.rb
@@ -85,6 +85,58 @@ RSpec.describe Resolvers::UserNotesCountResolver do
         end
       end
     end
+
+    context 'when counting notes from an epic' do
+      let_it_be(:epic) { create(:epic) }
+      let_it_be(:private_epic) { create(:epic, group: create(:group, :private)) }
+      let_it_be(:public_notes) { create_list(:note, 2, noteable: epic) }
+      let_it_be(:system_note) { create(:note, system: true, noteable: epic) }
+      let_it_be(:private_notes) { create_list(:note, 3, noteable: private_epic) }
+
+      before do
+        stub_licensed_features(epics: true)
+      end
+
+      context 'when counting notes from a public epic' do
+        subject { batch_sync { resolve_user_notes_count(epic) } }
+
+        it 'returns the number of non-system notes for the epic' do
+          expect(subject).to eq(2)
+        end
+      end
+
+      context 'when a user has permission to view notes' do
+        before do
+          private_epic.group.add_developer(user)
+        end
+
+        subject { batch_sync { resolve_user_notes_count(private_epic) } }
+
+        it 'returns the number of notes for the issue' do
+          expect(subject).to eq(3)
+        end
+      end
+
+      context 'when a user does not have permission to view notes' do
+        subject { batch_sync { resolve_user_notes_count(private_epic) } }
+
+        it 'raises an error' do
+          expect { subject }.to raise_error(Gitlab::Graphql::Errors::ResourceNotAvailable)
+        end
+      end
+
+      context 'when epics are not available' do
+        before do
+          stub_licensed_features(epics: false)
+        end
+
+        subject { batch_sync { resolve_user_notes_count(epic) } }
+
+        it 'raises an error' do
+          expect { subject }.to raise_error(Gitlab::Graphql::Errors::ResourceNotAvailable)
+        end
+      end
+    end
   end
 
   def resolve_user_notes_count(obj)
