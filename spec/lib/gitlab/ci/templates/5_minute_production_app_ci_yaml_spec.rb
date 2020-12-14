d634fe73e85aed8a89647baad207c88ba5453097
Dmytro Zaporozhets <dzaporozhets@gitlab.com>
2020-12-14 16:46:41 +0200

    Add more specs and test stage
    Signed-off-by: Dmytro Zaporozhets <dzaporozhets@gitlab.com>

--- spec/lib/gitlab/ci/templates/5_minute_production_app_ci_yaml_spec.rb
+++ spec/lib/gitlab/ci/templates/5_minute_production_app_ci_yaml_spec.rb
@@ -23,15 +23,37 @@ RSpec.describe '5-Minute-Production-App.gitlab-ci.yml' do
       expect(build_names).to match_array('build')
     end
 
-    context 'when AWS variables set' do
+    context 'when AWS variables are set' do
       before do
         create(:ci_variable, project: project, key: 'AWS_ACCESS_KEY_ID', value: 'AKIAIOSFODNN7EXAMPLE')
         create(:ci_variable, project: project, key: 'AWS_SECRET_ACCESS_KEY', value: 'wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY')
         create(:ci_variable, project: project, key: 'AWS_DEFAULT_REGION', value: 'us-west-2')
       end
 
-      it 'creates a build and a test job' do
-        expect(build_names).to match_array(%w(build terraform_apply deploy terraform_destroy))
+      context 'when CERT_EMAIL is set' do
+        before do
+          create(:ci_variable, project: project, key: 'CERT_EMAIL', value: 'admin@example.com')
+        end
+
+        it 'creates all jobs' do
+          expect(build_names).to match_array(%w(build terraform_apply deploy terraform_destroy))
+        end
+
+        context 'pipeline branch is protected' do
+          before do
+            create(:protected_branch, project: project, name: pipeline_branch)
+          end
+
+          it 'does not create a destroy job' do
+            expect(build_names).to match_array(%w(build terraform_apply deploy))
+          end
+        end
+      end
+
+      context 'when CERT_EMAIL is missing' do
+        it 'creates build and destroy jobs' do
+          expect(build_names).to match_array(%w(build terraform_destroy))
+        end
       end
     end
   end
