86a81605b5c73f66792ff0525c703f698692f1a5
George Koltsov <gkoltsov@gitlab.com>
2020-12-14 09:40:27 +0000

    Add Group Migration filtering

--- app/controllers/import/bulk_imports_controller.rb
+++ app/controllers/import/bulk_imports_controller.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class Import::BulkImportsController < ApplicationController
+  include ActionView::Helpers::SanitizeHelper
+
   before_action :ensure_group_import_enabled
   before_action :verify_blocked_uri, only: :status
 
@@ -42,7 +44,17 @@ class Import::BulkImportsController < ApplicationController
   end
 
   def importable_data
-    client.get('groups', top_level_only: true).parsed_response
+    client.get('groups', query_params).parsed_response
+  end
+
+  def query_params
+    query_params = {
+      top_level_only: true,
+      min_access_level: Gitlab::Access::MAINTAINER
+    }
+
+    query_params[:search] = sanitized_filter_param if sanitized_filter_param
+    query_params
   end
 
   def client
@@ -129,5 +141,9 @@ class Import::BulkImportsController < ApplicationController
       url: session[url_key],
       access_token: [access_token_key]
     }
+  end
+
+  def sanitized_filter_param
+    @filter ||= sanitize(params[:filter])&.downcase
   end
 end
