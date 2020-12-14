b2a9399591658650445c49c2bbf544d2ae0456c0
Imre Farkas <ifarkas@gitlab.com>
2020-12-14 14:04:17 +0100

    FortiAuthenticator strategy to rely on the correct config
    Previously access_token was call token, but the class was not updated.

--- spec/lib/gitlab/auth/otp/strategies/forti_authenticator_spec.rb
+++ spec/lib/gitlab/auth/otp/strategies/forti_authenticator_spec.rb
@@ -12,6 +12,12 @@ RSpec.describe Gitlab::Auth::Otp::Strategies::FortiAuthenticator do
   let(:api_token) { 's3cr3t' }
 
   let(:forti_authenticator_auth_url) { "https://#{host}:#{port}/api/v1/auth/" }
+  let(:headers) { { 'Content-Type': 'application/json' } }
+  let(:response_status) { 200 }
+  let(:request_body) do
+    { username: user.username,
+      token_code: otp_code }
+  end
 
   subject(:validate) { described_class.new(user).validate(otp_code) }
 
@@ -23,21 +29,25 @@ RSpec.describe Gitlab::Auth::Otp::Strategies::FortiAuthenticator do
       host: host,
       port: port,
       username: api_username,
-      token: api_token
+      access_token: api_token
     )
 
-    request_body = { username: user.username,
-                     token_code: otp_code }
-
     stub_request(:post, forti_authenticator_auth_url)
-      .with(body: JSON(request_body), headers: { 'Content-Type' => 'application/json' })
-      .to_return(status: response_status, body: '', headers: {})
+      .with(body: JSON(request_body), headers: headers)
+      .to_return(status: response_status, body: '', headers: headers)
   end
 
   context 'successful validation' do
-    let(:response_status) { 200 }
-
     it 'returns success' do
+      expect(Gitlab::HTTP).to(
+        receive(:post)
+          .with(forti_authenticator_auth_url,
+                headers: headers,
+                body: request_body.to_json,
+                basic_auth: { username: api_username, password: api_token })
+          .and_call_original
+      )
+
       expect(validate[:status]).to eq(:success)
     end
   end
@@ -47,6 +57,16 @@ RSpec.describe Gitlab::Auth::Otp::Strategies::FortiAuthenticator do
 
     it 'returns error' do
       expect(validate[:status]).to eq(:error)
+    end
+  end
+
+  context 'unexpected error' do
+    it 'returns error' do
+      error_message = 'boom!'
+      allow(Gitlab::HTTP).to receive(:post).and_raise(error_message)
+
+      expect(validate[:status]).to eq(:error)
+      expect(validate[:message]).to eq(error_message)
     end
   end
 
