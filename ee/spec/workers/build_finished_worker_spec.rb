6b82c244db69136a7cdd08b125438150b18efc3f
Saikat Sarkar <ssarkar@gitlab.com>
2020-12-15 11:46:46 -0700

    Avoid restricting the revocation token work for GitLab.com only

--- ee/spec/workers/build_finished_worker_spec.rb
+++ ee/spec/workers/build_finished_worker_spec.rb
@@ -23,15 +23,11 @@ RSpec.describe BuildFinishedWorker do
   describe '#revoke_secret_detection_token?' do
     using RSpec::Parameterized::TableSyntax
 
-    where(:dot_com, :token_revocation_enabled, :secret_detection_vulnerability_found, :expected_result) do
-      true  | true  | true  | true
-      true  | true  | false | false
-      true  | false | true  | false
-      true  | false | false | false
-      false | true  | true  | false
-      false | true  | false | false
-      false | false | true  | false
-      false | false | false | false
+    where(:token_revocation_enabled, :secret_detection_vulnerability_found, :expected_result) do
+      true  | true  | true
+      true  | false | false
+      false | true  | false
+      false | false | false
     end
 
     with_them do
