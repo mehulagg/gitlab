f555d3b11b50a70d2be7f2a84ea6f2c3a866cf89
David O'Regan <doregan@gitlab.com>
2020-12-15 05:32:09 +0000

    Update based on rebase

--- ee/spec/frontend/oncall_schedule/rotations/components/add_rotation_modal_spec.js
+++ ee/spec/frontend/oncall_schedule/rotations/components/add_rotation_modal_spec.js
@@ -77,9 +77,6 @@ describe('AddRotationModal', () => {
           participants,
         };
       },
-      propsData: {
-        schedule,
-      },
       provide: {
         projectPath,
       },
