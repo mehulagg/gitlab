6172ac8441b4e52a31ff6272c6b9b96d881bb8ee
Thomas Randolph <trandolph@gitlab.com>
2020-12-15 14:39:22 -0700

    Add utilities for Diff File Reviews and for Merge Requests
    Also add tests for the new utilities

---
+++ app/assets/javascripts/diffs/utils/file_reviews.js
@@ -0,0 +1,51 @@
+export function getReviewsForMergeRequest(mrPath) {
+  const reviewsForMr = localStorage.getItem(mrPath);
+  let reviews = {};
+
+  if (reviewsForMr) {
+    try {
+      reviews = JSON.parse(reviewsForMr);
+    } catch (err) {
+      reviews = {};
+    }
+  }
+
+  return reviews;
+}
+
+export function setReviewsForMergeRequest(mrPath, reviews) {
+  localStorage.setItem(mrPath, JSON.stringify(reviews));
+
+  return reviews;
+}
+
+export function getReviewForFile(reviews, file) {
+  const fileReviews = reviews[file.file_identifier_hash];
+
+  return file?.id && fileReviews ? fileReviews[file.id] : null;
+}
+
+export function reviewable(file) {
+  return Boolean(file.id) && Boolean(file.file_identifier_hash);
+}
+
+export function markFileReviewed(reviews, file) {
+  const usableReviews = { ...(reviews || {}) };
+  let updatedReviews = usableReviews;
+  let fileReviews;
+
+  if (reviewable(file)) {
+    fileReviews = { ...(usableReviews[file.file_identifier_hash] || {}) };
+
+    fileReviews[file.id] = {
+      date: new Date().toISOString(),
+    };
+
+    updatedReviews = {
+      ...usableReviews,
+      [file.file_identifier_hash]: fileReviews,
+    };
+  }
+
+  return updatedReviews;
+}
