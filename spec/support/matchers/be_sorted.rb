b4fdb0d25762842918b0698b9285decd834c753f
Alex Kalderimis <akalderimis@gitlab.com>
2020-12-20 20:57:00 +0000

    Add nicer API for be_sorted

--- spec/support/matchers/be_sorted.rb
+++ spec/support/matchers/be_sorted.rb
@@ -9,13 +9,63 @@
 # ```
 # expect(collection).to be_sorted(:field, :desc)
 # ```
-RSpec::Matchers.define :be_sorted do |by, order = :asc|
-  match do |actual|
-    next true unless actual.present? # emtpy collection is sorted
+RSpec::Matchers.define :be_sorted do |on = :itself, order = :asc|
+  def by(&blk)
+    @comparator = blk
+    self
+  end
 
-    actual
-      .then { |collection| by ? collection.sort_by(&by) : collection.sort }
-      .then { |sorted_collection| order.to_sym == :desc ? sorted_collection.reverse : sorted_collection }
-      .then { |sorted_collection| sorted_collection == actual }
+  def asc
+    @direction = :asc
+    self
+  end
+
+  def desc
+    @direction = :desc
+    self
+  end
+
+  def format_with(proc)
+    @format_with = proc
+    self
+  end
+
+  define_method :comparator do
+    @comparator || on
+  end
+
+  define_method :descending? do
+    (@direction || order.to_sym) == :desc
+  end
+
+  def order(items)
+    descending? ? items.reverse : items
+  end
+
+  def sort(items)
+    items.sort_by(&comparator)
+  end
+
+  match do |actual|
+    next true unless actual.present? # empty collection is sorted
+
+    actual = actual.to_a if actual.respond_to?(:to_a) && !actual.respond_to?(:sort_by)
+
+    @got = actual
+    @expected = order(sort(actual))
+
+    @expected == actual
+  end
+
+  def failure_message
+    "Expected #{show(@expected)}, got #{show(@got)}"
+  end
+
+  def show(things)
+    if @format_with
+      things.map(&@format_with)
+    else
+      things
+    end
   end
 end
