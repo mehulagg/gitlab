b97a37af53116b722aa6f123a93bd778f497ba33
Paul Slaughter <pslaughter@gitlab.com>
2020-12-23 11:45:18 -0600

    Use @gitlab/favicon-overlay instead of common_utils
    - This helps us remove some more karma specs
      Which can overall improve our CI cost.
    - Also, keeps our adapter methods that wrap
      this new library but moves them to their own
      modules.

--- jest.config.base.js
+++ jest.config.base.js
@@ -91,7 +91,7 @@ module.exports = path => {
       '^.+\\.(md|zip|png)$': 'jest-raw-loader',
     },
     transformIgnorePatterns: [
-      'node_modules/(?!(@gitlab/ui|bootstrap-vue|three|monaco-editor|monaco-yaml)/)',
+      'node_modules/(?!(@gitlab/ui|@gitlab/favicon-overlay|bootstrap-vue|three|monaco-editor|monaco-yaml)/)',
     ],
     timers: 'fake',
     testEnvironment: '<rootDir>/spec/frontend/environment.js',
