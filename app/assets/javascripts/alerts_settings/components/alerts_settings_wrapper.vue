bb5874292c860cc83a0991e6f9ecf6b5d4fb5b3f
David O'Regan <doregan@gitlab.com>
2020-12-23 16:34:03 +0000

    Refactor(opsgenie): remove Opsgenie integration frontend code from Incident management
    Remove the frontend code for
    Opegenie configuration within
    Incident management

--- app/assets/javascripts/alerts_settings/components/alerts_settings_wrapper.vue
+++ app/assets/javascripts/alerts_settings/components/alerts_settings_wrapper.vue
@@ -1,5 +1,5 @@
 <script>
-import { GlAlert, GlLink, GlSprintf } from '@gitlab/ui';
+import { GlLink, GlSprintf } from '@gitlab/ui';
 import { s__ } from '~/locale';
 import { fetchPolicies } from '~/lib/graphql';
 import createFlash, { FLASH_TYPES } from '~/flash';
@@ -41,8 +41,6 @@ export default {
     ),
   },
   components: {
-    // TODO: Will be removed in 13.7 as part of: https://gitlab.com/gitlab-org/gitlab/-/issues/273657
-    GlAlert,
     GlLink,
     GlSprintf,
     IntegrationsList,
@@ -53,10 +51,6 @@ export default {
       default: {},
     },
     prometheus: {
-      default: {},
-    },
-    // TODO: Will be removed in 13.7 as part of: https://gitlab.com/gitlab-org/gitlab/-/issues/273657
-    opsgenie: {
       default: {},
     },
     projectPath: {
@@ -104,13 +98,6 @@ export default {
     },
     canAddIntegration() {
       return this.multiIntegrations || this.integrations?.list?.length < 2;
-    },
-    canManageOpsgenie() {
-      return (
-        this.opsgenie.active ||
-        this.integrations?.list?.every(({ active }) => active === false) ||
-        this.integrations?.list?.length === 0
-      );
     },
   },
   methods: {
@@ -317,27 +304,7 @@ export default {
 
 <template>
   <div>
-    <!-- TODO: Will be removed in 13.7 as part of: https://gitlab.com/gitlab-org/gitlab/-/issues/273657 -->
-    <gl-alert v-if="opsgenie.active" :dismissible="false" variant="tip">
-      <gl-sprintf
-        :message="
-          s__(
-            'AlertSettings|We will soon be introducing the ability to create multiple unique HTTP endpoints. When this functionality is live,  you will be able to configure an integration with Opsgenie to surface Opsgenie alerts in GitLab. This will replace the current Opsgenie integration which will be deprecated. %{linkStart}More Information%{linkEnd}',
-          )
-        "
-      >
-        <template #link="{ content }">
-          <gl-link
-            class="gl-display-inline-block"
-            href="https://gitlab.com/gitlab-org/gitlab/-/issues/273657"
-            target="_blank"
-            >{{ content }}</gl-link
-          >
-        </template>
-      </gl-sprintf>
-    </gl-alert>
     <integrations-list
-      v-else
       :integrations="integrations.list"
       :loading="loading"
       @edit-integration="editIntegration"
@@ -346,7 +313,6 @@ export default {
     <alert-settings-form
       :loading="isUpdating"
       :can-add-integration="canAddIntegration"
-      :can-manage-opsgenie="canManageOpsgenie"
       @create-new-integration="createNewIntegration"
       @update-integration="updateIntegration"
       @reset-token="resetToken"
