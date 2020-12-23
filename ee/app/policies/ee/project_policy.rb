37dde21b56e842511b30a6ed54b9d92884a2de66
Eugenia Grieff <egrieff@gitlab.com>
2020-12-23 19:34:34 +0000

    Add mutation to export requirements
    - Add export requirements policy
    - Update schema

--- ee/app/policies/ee/project_policy.rb
+++ ee/app/policies/ee/project_policy.rb
@@ -370,6 +370,7 @@ module EE
         enable :admin_requirement
         enable :update_requirement
         enable :import_requirements
+        enable :export_requirements
       end
 
       rule { requirements_available & owner }.enable :destroy_requirement
