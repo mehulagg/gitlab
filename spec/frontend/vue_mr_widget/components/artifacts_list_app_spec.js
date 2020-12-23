db179bb44dfdae9211d2f3b6b863e87370a73a35
Vitaly Slobodin <vslobodin@gitlab.com>
2020-12-23 14:28:27 +0000

    Apply 1 suggestion(s) to 1 file(s)

--- spec/frontend/vue_mr_widget/components/artifacts_list_app_spec.js
+++ spec/frontend/vue_mr_widget/components/artifacts_list_app_spec.js
@@ -101,7 +101,7 @@ describe('Merge Requests Artifacts list app', () => {
         findTitle().trigger('click');
         await nextTick();
 
-        expect(findTableRows().length).toEqual(2);
+        expect(findTableRows().length).toBe(2);
       });
     });
   });
