6276c3d9f43ed4bdcf11ff00472a70dcbf839774
Takuya Noguchi <takninnovationresearch@gmail.com>
2020-12-30 17:23:09 +0000

    Remove legacy container image checking from Project container top
    ensure_root_container_repository was originally introduced in
    the following merge reqeust:
    https://gitlab.com/gitlab-org/gitlab-foss/-/merge_requests/10109
    
    The JSON endnpoint removing in the MR was implicitly deprecated
    in the following merge request:
    https://gitlab.com/gitlab-org/gitlab/-/merge_requests/27814
    
    Signed-off-by: Takuya Noguchi <takninnovationresearch@gmail.com>

--- app/controllers/projects/registry/repositories_controller.rb
+++ app/controllers/projects/registry/repositories_controller.rb
@@ -6,23 +6,8 @@ module Projects
       include PackagesHelper
 
       before_action :authorize_update_container_image!, only: [:destroy]
-      before_action :ensure_root_container_repository!, only: [:index]
 
       def index
-        respond_to do |format|
-          format.html
-          format.json do
-            @images = ContainerRepositoriesFinder.new(user: current_user, subject: project, params: params.slice(:name))
-                                                 .execute
-
-            track_package_event(:list_repositories, :container)
-
-            serializer = ContainerRepositoriesSerializer
-              .new(project: project, current_user: current_user)
-
-            render json: serializer.with_pagination(request, response).represent(@images)
-          end
-        end
       end
 
       # The show action renders index to allow frontend routing to work on page refresh
@@ -44,23 +29,6 @@ module Projects
 
       def image
         @image ||= project.container_repositories.find(params[:id])
-      end
-
-      ##
-      # Container repository object for root project path.
-      #
-      # Needed to maintain a backwards compatibility.
-      #
-      def ensure_root_container_repository!
-        ::ContainerRegistry::Path.new(@project.full_path).tap do |path|
-          break if path.has_repository?
-
-          ::ContainerRepository.build_from_path(path).tap do |repository|
-            repository.save! if repository.has_tags?
-          end
-        end
-      rescue ContainerRegistry::Path::InvalidRegistryPathError
-        @character_error = true
       end
     end
   end
