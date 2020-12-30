e2e75f384fe99633ebc97001bfab5f9952690d90
Mehmet Emin INAC <minac@gitlab.com>
2020-12-30 17:58:17 +0100

    Initial commit

--- app/graphql/types/base_enum.rb
+++ app/graphql/types/base_enum.rb
@@ -5,6 +5,12 @@ module Types
     extend GitlabStyleDeprecations
 
     class << self
+      # Registers enum values by the given Hash object like
+      # the return value of ActiveRecord enum methods.
+      def values_from_hash(enum)
+        enum.keys.map(&:to_s).each { |val| value(val.upcase, value: val) }
+      end
+
       def value(*args, **kwargs, &block)
         enum[args[0].downcase] = kwargs[:value] || args[0]
         kwargs = gitlab_deprecation(kwargs)
