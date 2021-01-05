415549b663d257125865bb05dbf14538c2b3ed32
Terri Chu <tchu@gitlab.com>
2021-01-05 11:56:51 -0500

    fixup! Invalidate Elasticsearch cache when moving projects

--- app/services/groups/transfer_service.rb
+++ app/services/groups/transfer_service.rb
@@ -4,7 +4,7 @@ module Groups
   class TransferService < Groups::BaseService
     TransferError = Class.new(StandardError)
 
-    attr_reader :error, :new_parent_group, :group
+    attr_reader :error, :new_parent_group
 
     def initialize(group, user, params = {})
       super
