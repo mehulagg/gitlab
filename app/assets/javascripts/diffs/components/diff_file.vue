efdf1810814c8141b2dac6f8c465370cfd7bf298
Thomas Randolph <trandolph@gitlab.com>
2021-01-05 14:34:28 -0700

    Collapse the file when it mounts if it's expanded and reviewed

--- app/assets/javascripts/diffs/components/diff_file.vue
+++ app/assets/javascripts/diffs/components/diff_file.vue
@@ -10,7 +10,9 @@ import notesEventHub from '../../notes/event_hub';
 import DiffFileHeader from './diff_file_header.vue';
 import DiffContent from './diff_content.vue';
 import { diffViewerErrors } from '~/ide/constants';
+
 import { collapsedType, isCollapsed } from '../utils/diff_file';
+
 import {
   DIFF_FILE_AUTOMATIC_COLLAPSE,
   DIFF_FILE_MANUAL_COLLAPSE,
@@ -180,6 +182,10 @@ export default {
   mounted() {
     if (this.hasDiff) {
       this.postRender();
+    }
+
+    if( this.reviewed && !this.isCollapsed ){
+      this.handleToggle();
     }
   },
   beforeDestroy() {
