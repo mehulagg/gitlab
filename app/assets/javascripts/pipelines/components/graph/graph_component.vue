6a256c7dc9c26542ce4802a6f03079eca86a24af
Sarah GP <sarah.groff.palermo@gmail.com>
2021-01-06 18:47:44 +0100

    Add test for large graphs

--- app/assets/javascripts/pipelines/components/graph/graph_component.vue
+++ app/assets/javascripts/pipelines/components/graph/graph_component.vue
@@ -1,4 +1,6 @@
 <script>
+import { GlAlert } from '@gitlab/ui';
+import { __ } from '~/locale';
 import LinkedGraphWrapper from '../graph_shared/linked_graph_wrapper.vue';
 import LinksLayer from '../graph_shared/links_layer.vue';
 import LinkedPipelinesColumn from './linked_pipelines_column.vue';
@@ -8,6 +10,7 @@ import { DOWNSTREAM, MAIN, UPSTREAM } from './constants';
 export default {
   name: 'PipelineGraph',
   components: {
+    GlAlert,
     LinksLayer,
     LinkedGraphWrapper,
     LinkedPipelinesColumn,
@@ -35,14 +38,21 @@ export default {
   },
   CONTAINER_REF: 'PIPELINE_LINKS_CONTAINER_REF',
   CONTAINER_ID: 'pipeline-links-container',
+  MAX_STAGES: 1000,
   data() {
     return {
+      alertDismissed: false,
       hoveredJobName: '',
       pipelineExpanded: {
         jobName: '',
         expanded: false,
       },
+      showLinksOverride: false,
     };
+  },
+  i18n: {
+    showLinksAnyways: __('Show links anyways'),
+    tooManyStages: __('This graph has a large number of stages and showing the links may be slow.')
   },
   computed: {
     downstreamPipelines() {
@@ -57,12 +67,27 @@ export default {
     hasUpstreamPipelines() {
       return Boolean(this.pipeline?.upstream?.length > 0);
     },
-    // The two show checks prevent upstream / downstream from showing redundant linked columns
+    linkWrapper() {
+      return this.showLinkedLayers || this.showLinksOverride  ? 'links-layer' : 'div';
+    },
+    numStages() {
+      return this.graph.reduce((acc, { groups }) => {
+        return acc + Number(groups.length)
+      }, 0)
+    },
+    showAlert() {
+      return !this.showLinkedLayers && !this.alertDismissed;
+    },
+    // The show downstream check prevents showing redundant linked columns
     showDownstreamPipelines() {
       return (
         this.hasDownstreamPipelines && this.type !== this.$options.pipelineTypeConstants.UPSTREAM
       );
     },
+    showLinkedLayers() {
+      return this.numStages < this.$options.MAX_STAGES;
+    },
+    // The show upstream check prevents showing redundant linked columns
     showUpstreamPipelines() {
       return (
         this.hasUpstreamPipelines && this.type !== this.$options.pipelineTypeConstants.DOWNSTREAM
@@ -73,6 +98,13 @@ export default {
     },
   },
   methods: {
+    dismissAlert() {
+      this.alertDismissed = true;
+    },
+    overrideShowLinks() {
+      this.dismissAlert();
+      this.showLinksOverride = true;
+    },
     setJob(jobName) {
       this.hoveredJobName = jobName;
     },
@@ -87,13 +119,21 @@ export default {
 </script>
 <template>
   <div class="js-pipeline-graph">
+    <gl-alert
+      v-if="showAlert"
+      :primary-button-text="$options.i18n.showLinksAnyways"
+      @primaryAction="overrideShowLinks"
+      @dismiss="dismissAlert"
+    >
+      {{ $options.i18n.tooManyStages }}
+    </gl-alert>
     <div
       class="gl-pipeline-min-h gl-display-flex gl-position-relative gl-overflow-auto gl-bg-gray-10 gl-white-space-nowrap"
       :class="{ 'gl-py-5': !isLinkedPipeline }"
       :id="$options.CONTAINER_ID"
       :ref="$options.CONTAINER_REF"
     >
-      <links-layer
+      <component :is='linkWrapper'
         :pipeline-data="graph"
         :container-id="$options.CONTAINER_ID"
         :container-ref="$options.CONTAINER_REF"
@@ -134,7 +174,7 @@ export default {
             />
           </template>
         </linked-graph-wrapper>
-      </links-layer>
+      </component>
     </div>
   </div>
 </template>
