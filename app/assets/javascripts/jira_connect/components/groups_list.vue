4d42707b7c13bfa522d739bc72c39a76a0ccfb71
Justin Ho <hduong@gitlab.com>
2021-01-06 22:00:07 +0700

    Add listing structure with tabs and pagination
    - Extract defaultPerPage to a constant
    - Add tabs with 1 tab for "Groups and subgroups"
    - Add loading icon when groups listing is loading
    - Add empty state when there are no availble groups
    - Add pagination for groups

--- app/assets/javascripts/jira_connect/components/groups_list.vue
+++ app/assets/javascripts/jira_connect/components/groups_list.vue
@@ -1,8 +1,16 @@
 <script>
+import { GlTabs, GlTab, GlLoadingIcon, GlPagination } from '@gitlab/ui';
 import { parseIntPagination, normalizeHeaders } from '~/lib/utils/common_utils';
 import { fetchGroups } from '~/jira_connect/api';
+import { defaultPerPage } from '~/jira_connect/constants';
 
 export default {
+  components: {
+    GlTabs,
+    GlTab,
+    GlLoadingIcon,
+    GlPagination,
+  },
   inject: {
     groupsPath: {
       default: '',
@@ -13,7 +21,7 @@ export default {
       groups: null,
       isLoading: false,
       page: 1,
-      perPage: 10,
+      perPage: defaultPerPage,
       totalItems: 0,
     };
   },
@@ -45,6 +53,39 @@ export default {
 
 <template>
   <div>
-    <p v-for="group in groups" :key="group.id">{{ group.name }}</p>
+    <gl-tabs>
+      <gl-tab :title="__('Groups and subgroups')" class="gl-pt-3">
+        <gl-loading-icon v-if="isLoading" size="md" />
+        <template v-else-if="groups.length === 0">
+          <div class="gl-text-center">
+            <div class="h5">{{ s__('Integrations|No available namespaces.') }}</div>
+            <p class="gl-mt-5">
+              {{
+                s__(
+                  'Integrations|You must have owner or maintainer permissions to link namespaces.',
+                )
+              }}
+            </p>
+          </div>
+        </template>
+        <template v-else>
+          <ul class="gl-list-style-none gl-pl-0">
+            <li v-for="group in groups" :key="group.id">{{ group.name }}</li>
+            <!-- <groups-list-item
+              v-for="namespace in namespaces"
+              :key="namespace.id"
+              :group="namespace"
+            /> -->
+          </ul>
+        </template>
+        <gl-pagination
+          v-if="totalItems > perPage && groups.length > 0"
+          v-model="page"
+          :per-page="perPage"
+          :total-items="totalItems"
+          @input="loadGroups"
+        />
+      </gl-tab>
+    </gl-tabs>
   </div>
 </template>
