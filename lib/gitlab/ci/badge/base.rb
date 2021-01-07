8429bfd89761e921e2a4b398e42a40eecadfcb82
Allison Browne <abrowne@gitlab.com>
2021-01-07 17:17:05 -0500

    Namespace badge under Gitlab Ci
    Gitlab::Badge => Gitlab::Ci::Badge

---
+++ lib/gitlab/ci/badge/base.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+module Gitlab
+  module Ci
+    module Badge
+      class Base
+        def entity
+          raise NotImplementedError
+        end
+
+        def status
+          raise NotImplementedError
+        end
+
+        def metadata
+          raise NotImplementedError
+        end
+
+        def template
+          raise NotImplementedError
+        end
+      end
+    end
+  end
+end
