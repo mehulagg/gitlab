df52b02165b72ca8068c2a8e7461b3abf118bfae
Matthias Kaeppler <mkaeppler@gitlab.com>
2021-01-07 17:08:23 +0100

    Extract system stats logic into System module
    This makes it reusable elsewhere.

--- app/controllers/metrics_controller.rb
+++ app/controllers/metrics_controller.rb
@@ -22,27 +22,12 @@ class MetricsController < ActionController::Base
     # perform a major mark-and-sweep before collecting stats
     GC.start
 
-    render json: ruby_vm_stats
+    render json: Gitlab::Metrics::System.summary
   end
 
   private
 
   def metrics_service
     @metrics_service ||= MetricsService.new
-  end
-
-  def ruby_vm_stats
-    sys = Gitlab::Metrics::System
-    proportional_mem = sys.memory_usage_uss_pss
-    {
-      'version': RUBY_DESCRIPTION,
-      'gc_stat': GC.stat,
-      'memory_rss': sys.memory_usage_rss,
-      'memory_uss': proportional_mem[:uss],
-      'memory_pss': proportional_mem[:pss],
-      'time_cputime': sys.cpu_time,
-      'time_realtime': sys.real_time,
-      'time_monotonic': sys.monotonic_time
-    }
   end
 end
