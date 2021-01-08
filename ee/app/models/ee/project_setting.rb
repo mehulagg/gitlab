aa0ccb1a69553b621ff2b718015978a8c0a29cb0
Mayra Cabrera <mcabrera@gitlab.com>
2021-01-08 10:49:40 -0600

    Prevent editing commits if signed commits required
    On https://gitlab.com/groups/gitlab-org/-/epics/4983, we want to allow
    editing commit messages on unprotected branches. Until a better
    solution is found, we can't edit commit messages that are signed, so if
    "Required signed commits" option is enabled, this setting shouldn't be
    enabled.
    
    This commits prevents for editing commit setting to be enabled if signed
    commits is required.
    
    Related to https://gitlab.com/gitlab-org/gitlab/-/issues/292493

--- ee/app/models/ee/project_setting.rb
+++ ee/app/models/ee/project_setting.rb
@@ -8,6 +8,23 @@ module EE
       belongs_to :push_rule
 
       scope :has_vulnerabilities, -> { where('has_vulnerabilities IS TRUE') }
+
+      validate :allowed_to_edit_commits
+
+      private
+
+      def allowed_to_edit_commits
+        return unless signed_commits_required?
+
+        error_message = "Signed commits is required on this project, editing commit messges can't be enabled."
+        errors.add(:allow_editing_commit_messages, error_message)
+      end
+
+      def signed_commits_required?
+        return false unless push_rule
+
+        push_rule.reject_unsigned_commits?
+      end
     end
   end
 end
