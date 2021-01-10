e7fb5385f731eaeefc8188ee9050a3cd4afd19fb
Himanshu Kapoor <hkapoor@gitlab.com>
2021-01-10 23:25:21 +0530

    Wiki: Add author avatar and link
    Add author avatar and link to a wiki's last modified info

--- lib/gitlab/git/wiki_page_version.rb
+++ lib/gitlab/git/wiki_page_version.rb
@@ -10,7 +10,12 @@ module Gitlab
         @format = format
       end
 
-      delegate :message, :sha, :id, :author_name, :authored_date, to: :commit
+      delegate :message, :sha, :id, :author_name, :author_email, :authored_date, to: :commit
+
+      def author_url
+        author = ::User.find_by_any_email(author_email)
+        UserPresenter.new(author).web_url
+      end
     end
   end
 end
