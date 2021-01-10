91aebbe8d6b9858eebaf2c5355d797b599244daf
Chad Woolley <cwoolley@gitlab.com>
2021-01-10 15:59:34 -0700

    Add reCAPTCHA fields to snippet mutations
    NOTE: This is a work-in-progress branch, still sorting out
    how to split the logic from
    https://gitlab.com/gitlab-org/gitlab/-/merge_requests/50559
    across multiple MRs.

--- app/graphql/mutations/concerns/mutations/can_mutate_spammable.rb
+++ app/graphql/mutations/concerns/mutations/can_mutate_spammable.rb
@@ -6,12 +6,40 @@ module Mutations
   module CanMutateSpammable
     extend ActiveSupport::Concern
 
+    # NOTE: The arguments and fields are intentionally named with 'captcha' instead of 'recaptcha',
+    # so that they can be applied to future alternative captcha implementations other than
+    # reCAPTCHA (e.g. FriendlyCaptcha) without having to change the names and descriptions in the API.
     included do
+      argument :captcha_response, GraphQL::STRING_TYPE,
+               required: false,
+               description: 'A valid captcha response value obtained by using the provided captchaSiteKey with a captcha API to present a challenge to be solved on the client.'
+
+      argument :spam_log_id, GraphQL::INT_TYPE,
+               required: false,
+               description: 'The spam log ID which must be returned along with a valid captcha response in order for the operation to be completed.'
+
       field :spam,
             GraphQL::BOOLEAN_TYPE,
             null: true,
-            description: 'Indicates whether the operation returns a record detected as spam.'
+            description: 'Indicates that the operation was detected as definite spam. There is no option to resubmit the request with a captcha response.'
+
+      field :needs_captcha_response,
+            GraphQL::BOOLEAN_TYPE,
+            null: true,
+            description: 'Indicates that the operation was detected as possible spam and not completed. If captcha is enabled, the request must be resubmitted with a valid captcha response and spam_log_id included in order for the operation to be completed.'
+
+      field :spam_log_id,
+            GraphQL::INT_TYPE,
+            null: true,
+            description: 'The spam log ID which must be returned along with a valid captcha response in order for the operation to be completed.'
+
+      field :captcha_site_key,
+            GraphQL::STRING_TYPE,
+            null: true,
+            description: 'The captcha site key which must be used to render a challenge for the user to solve in order to obtain a valid captchaResponse value.'
     end
+
+    private
 
     # additional_spam_params    -> hash
     #
