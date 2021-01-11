e942783943420a0ad4ec16fd2dd65245b71ed58c
Philip Cunningham <pcunningham@gitlab.com>
2021-01-11 17:50:24 +1100

    wip

--- ee/spec/models/dast_scan_spec.rb
+++ ee/spec/models/dast_scan_spec.rb
@@ -1,5 +1,13 @@
-require 'rails_helper'
+# frozen_string_literal: true
+
+require 'spec_helper'
 
 RSpec.describe DastScan, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject { create(:dast_scan) }
+
+  describe 'associations' do
+    it { is_expected.to belong_to(:project) }
+    it { is_expected.to belong_to(:dast_site_profile) }
+    it { is_expected.to belong_to(:dast_scanner_profile) }
+  end
 end
