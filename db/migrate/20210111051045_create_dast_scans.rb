5f036cfdcff4ebfe7be0c58b23bce493d2389deb
Philip Cunningham <pcunningham@gitlab.com>
2021-01-11 16:24:53 +1100

    wip

--- db/migrate/20210111051045_create_dast_scans.rb
+++ db/migrate/20210111051045_create_dast_scans.rb
@@ -1,13 +1,22 @@
 class CreateDastScans < ActiveRecord::Migration[6.0]
-  def change
+  def up
     create_table :dast_scans do |t|
-      t.references :project, foreign_key: true
-      t.references :dast_site_profile, foreign_key: true
-      t.references :dast_scanner_profile, foreign_key: true
+      t.bigint :project_id, null: false
+      t.bigint :dast_site_profile_id, null: false
+      t.bigint :dast_scanner_profile_id, null: false
+
+      t.timestamps_with_timezone
+
       t.text :name
       t.text :description
-
-      t.timestamps
     end
+
+    add_index :dast_scans, :project_id
+    add_index :dast_scans, :dast_site_profile_id
+    add_index :dast_scans, :dast_scanner_profile_id
+  end
+
+  def down
+    drop_table :dast_scans
   end
 end
