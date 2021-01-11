f5a799a994dd76eb7860a8dd15520f119cb689c4
Chad Woolley <cwoolley@gitlab.com>
2021-01-10 23:20:35 -0700

    Refactor common reCAPTCHA logic to be usable from services
    Refactor common reCAPTCHA logic out of spammable_actions.rb to be usable
    from service layer. Adds support which will be used in subsequent MRs
    to implement the reCAPTCHA workflow via GraphQL. See
    https://gitlab.com/gitlab-org/gitlab/-/issues/217722
    for more details.

--- app/services/concerns/spam_check_methods.rb
+++ app/services/concerns/spam_check_methods.rb
@@ -9,22 +9,44 @@
 # - params with :request
 
 module SpamCheckMethods
-  # rubocop:disable Gitlab/ModuleWithInstanceVariables
-  def filter_spam_check_params
-    @request            = params.delete(:request)
-    @api                = params.delete(:api)
-    @recaptcha_verified = params.delete(:recaptcha_verified)
-    @spam_log_id        = params.delete(:spam_log_id)
-  end
-  # rubocop:enable Gitlab/ModuleWithInstanceVariables
+  extend ActiveSupport::Concern
 
-  # In order to be proceed to the spam check process, @spammable has to be
-  # a dirty instance, which means it should be already assigned with the new
-  # attribute values.
-  # rubocop:disable Gitlab/ModuleWithInstanceVariables
+  include SpamRecaptchaSupport
+
+  # The `:request` attr_reader is necessary to call Recaptcha::Verify#verify_recaptcha, because
+  # it directly accesses `#request`.
+  attr_reader :request
+
+  # Removes spam/recaptcha related entries from params, and stores them as instance variables
+  # on the current object. This is a necessary first step in order for subsequent service-specific
+  # params validation to not fail because they attempt to use these parameters to create
+  # or update model attributes.
+  def filter_spam_check_params
+    # rubocop:disable Gitlab/ModuleWithInstanceVariables
+    @request = params.delete(:request)
+    @api = params.delete(:api)
+    # NOTE: @recaptcha_verified is only provided here via a param in the REST API flow. The GraphQL
+    #   flow gets it as a return value from calling #verify_spammable_recaptcha!, and sets it directly
+    @recaptcha_verified = params.delete(:recaptcha_verified)
+    @spam_log_id = params.delete(:spam_log_id)
+    @recaptcha_response = params.delete(:recaptcha_response)
+    # rubocop:enable Gitlab/ModuleWithInstanceVariables
+  end
+
+  # Performs the spam checking of a Spammable via the SpamActionService, using the instance
+  # variables previously stored out of params by #filter_spam_check_params.
+  #
+  # Modifies the spammable argument to set relevant attributes and errors.
+  #
+  # Returns a hash of fields which are required by the GraphQL mutation resolver to populate the
+  # response fields which support the client-side GraphQL spam/recaptcha workflow.
+  #
+  # The spammable must be a dirty instance, which means it should be already assigned with the
+  # new attribute values.
   def spam_check(spammable, user, action:)
     raise ArgumentError.new('Please provide an action, such as :create') unless action
 
+    # rubocop:disable Gitlab/ModuleWithInstanceVariables
     Spam::SpamActionService.new(
       spammable: spammable,
       request: @request,
@@ -34,6 +56,13 @@ module SpamCheckMethods
       api: @api,
       recaptcha_verified: @recaptcha_verified,
       spam_log_id: @spam_log_id)
+    # rubocop:enable Gitlab/ModuleWithInstanceVariables
+
+    {
+      spam: spammable.spam?,
+      needs_recaptcha_response: render_recaptcha?(spammable),
+      spam_log_id: spammable.spam_log&.id,
+      recaptcha_site_key: Gitlab::CurrentSettings.recaptcha_site_key
+    }
   end
-  # rubocop:enable Gitlab/ModuleWithInstanceVariables
 end
