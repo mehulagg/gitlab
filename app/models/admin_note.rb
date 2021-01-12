13ff340df8f67f3ee16daece7b55914d01b5869c
Harish Ramachandran <harish@gitlab.com>
2021-01-12 10:42:53 -0500

    Mention frozen string literals

--- app/models/admin_note.rb
+++ app/models/admin_note.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class AdminNote < ApplicationRecord
   belongs_to :namespace
   validates :namespace_id, presence: true
--- db/migrate/20201218194311_create_admin_notes.rb
+++ db/migrate/20201218194311_create_admin_notes.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class CreateAdminNotes < ActiveRecord::Migration[6.0]
   DOWNTIME = false
 
