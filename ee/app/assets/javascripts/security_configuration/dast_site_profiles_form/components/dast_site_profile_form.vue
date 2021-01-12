97d0e7d2e6ca6c4d94a78f5c9bb8efb5c9a355b5
Dheeraj Joshi <djoshi@gitlab.com>
2021-01-12 15:24:33 +0530

    Add referer check for previous page

--- ee/app/assets/javascripts/security_configuration/dast_site_profiles_form/components/dast_site_profile_form.vue
+++ ee/app/assets/javascripts/security_configuration/dast_site_profiles_form/components/dast_site_profile_form.vue
@@ -120,7 +120,7 @@ export default {
     }
   },
   created() {
-    // this.redirectToOnDemandScansPage = true;
+    this.redirectToOnDemandScansPage = document.referrer.includes(this.onDemandScansPath);
   },
   methods: {
     onSubmit() {
