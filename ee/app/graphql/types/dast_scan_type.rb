31cb5dd943411529d03fab2742277a3e6cdef934
Philip Cunningham <pcunningham@gitlab.com>
2021-01-13 15:46:16 +1100

    Create DastScanType and DastScanPolicy
    Creates new GraphQL type and policy for DastScan feature.

---
+++ ee/app/graphql/types/dast_scan_type.rb
@@ -0,0 +1,32 @@
+# frozen_string_literal: true
+
+module Types
+  class DastScanType < BaseObject
+    graphql_name 'DastScan'
+    description 'Represents a DAST Scan'
+
+    authorize :create_on_demand_dast_scan
+
+    field :id, ::Types::GlobalIDType[::DastScan], null: false,
+          description: 'ID of the scan.'
+
+    field :name, GraphQL::STRING_TYPE, null: true,
+          description: 'The name of the scan.'
+
+    field :description, GraphQL::STRING_TYPE, null: true,
+          description: 'The description of the scan.'
+
+    field :dast_site_profile_id, ::Types::GlobalIDType[::DastSiteProfile], null: true,
+          description: 'ID of the site profile to be used for the scan.'
+
+    field :dast_scanner_profile_id, ::Types::GlobalIDType[::DastScannerProfile], null: true,
+          description: 'ID of the scanner profile to be used for the scan.'
+
+    field :edit_path, GraphQL::STRING_TYPE, null: true,
+          description: 'Relative web path to the edit page of the scan.'
+
+    def edit_path
+      Rails.application.routes.url_helpers.edit_project_on_demand_scan_path(object.project, object)
+    end
+  end
+end
