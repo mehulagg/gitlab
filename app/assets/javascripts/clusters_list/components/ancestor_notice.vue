4ffdbdc01decafa4d1313b2b86a002158c511c53
Veethika Mishra <6115425-v_mishra@users.noreply.gitlab.com>
2021-01-14 07:19:39 +0000

    Apply 2 suggestion(s) to 1 file(s)

--- app/assets/javascripts/clusters_list/components/ancestor_notice.vue
+++ app/assets/javascripts/clusters_list/components/ancestor_notice.vue
@@ -15,21 +15,19 @@ export default {
 </script>
 
 <template>
-  <gl-alert v-if="hasAncestorClusters" variant="info" :dismissible="false" class="gl-mt-3">
-    <p>
-      <gl-sprintf
-        :message="
-          s__(
-            'ClusterIntegration|Clusters are utilized by selecting the nearest ancestor with a matching environment scope. For example, project clusters will override group clusters. %{linkStart}More information%{linkEnd}',
-          )
-        "
-      >
-        <template #link="{ content }">
-          <gl-link :href="ancestorHelperPath">
-            <strong>{{ content }}</strong>
-          </gl-link>
-        </template>
-      </gl-sprintf>
-    </p>
+  <gl-alert v-if="hasAncestorClusters" variant="info" :dismissible="false" class="gl-my-4">
+    <gl-sprintf
+      :message="
+        s__(
+          'ClusterIntegration|Clusters are utilized by selecting the nearest ancestor with a matching environment scope. For example, project clusters will override group clusters. %{linkStart}More information%{linkEnd}',
+        )
+      "
+    >
+      <template #link="{ content }">
+        <gl-link :href="ancestorHelperPath">
+          <strong>{{ content }}</strong>
+        </gl-link>
+      </template>
+    </gl-sprintf>
   </gl-alert>
 </template>
