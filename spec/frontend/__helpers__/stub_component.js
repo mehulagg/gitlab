ca311de718e7d3dccf93c0a17668adbfdfec6424
Paul Slaughter <pslaughter@gitlab.com>
2021-01-15 11:35:17 -0600

    Update stub_component with createStubbedMethods
    This is helpful at creating a stub for things like GlModal
    which technically has public methods in its API

--- spec/frontend/__helpers__/stub_component.js
+++ spec/frontend/__helpers__/stub_component.js
@@ -1,7 +1,22 @@
+const createStubbedMethods = (methods = {}) => {
+  if (!methods) {
+    return {};
+  }
+
+  return Object.keys(methods).reduce(
+    (acc, key) =>
+      Object.assign(acc, {
+        [key]: () => {},
+      }),
+    {},
+  );
+};
+
 export function stubComponent(Component, options = {}) {
   return {
     props: Component.props,
     model: Component.model,
+    methods: createStubbedMethods(Component.methods),
     // Do not render any slots/scoped slots except default
     // This differs from VTU behavior which renders all slots
     template: '<div><slot></slot></div>',
