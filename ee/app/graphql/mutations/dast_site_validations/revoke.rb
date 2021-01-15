429a57f5eb82ba2e7b698c54349df74529a54d0a
Philip Cunningham <pcunningham@gitlab.com>
2021-01-15 17:59:54 +1100

    Add dastSiteValidationRevoke GraphQL mutation
    Add ability to revoke ability to run active scans.

---
+++ ee/app/graphql/mutations/dast_site_validations/revoke.rb
@@ -0,0 +1,49 @@
+# frozen_string_literal: true
+
+module Mutations
+  module DastSiteValidations
+    class Revoke < BaseMutation
+      include FindsProject
+
+      graphql_name 'DastSiteValidationRevoke'
+
+      argument :full_path, GraphQL::ID_TYPE,
+               required: true,
+               description: 'The project the site validation belongs to.'
+
+      argument :normalized_target_url, GraphQL::STRING_TYPE,
+               required: true,
+               description: 'Normalized URL of the target to be revoked.'
+
+      authorize :create_on_demand_dast_scan
+
+      def resolve(full_path:, normalized_target_url:)
+        project = authorized_find!(full_path)
+        raise Gitlab::Graphql::Errors::ResourceNotAvailable, 'Feature disabled' unless allowed?(project)
+
+        response = ::DastSiteValidations::RevokeService.new(
+          container: project,
+          params: { url_base: normalized_target_url }
+        ).execute
+
+        return error_response(response.errors) if response.error?
+
+        success_response
+      end
+
+      private
+
+      def allowed?(project)
+        Feature.enabled?(:security_on_demand_scans_site_validation, project, default_enabled: :yaml)
+      end
+
+      def error_response(errors)
+        { errors: errors }
+      end
+
+      def success_response
+        { errors: [] }
+      end
+    end
+  end
+end
