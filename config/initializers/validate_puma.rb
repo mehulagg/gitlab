74d06f0f8de267d0a71164322f471cdcfe320b03
Matthias Kaeppler <mkaeppler@gitlab.com>
2021-01-18 14:07:47 +0100

    Add env var that bypasses Puma cluster validation
    We often need to run Puma in single-mode for performance
    testing, but the application isn't fully set up yet
    to support that. Bypassing this check as a non-published
    developer switch makes our life easier.

--- config/initializers/validate_puma.rb
+++ config/initializers/validate_puma.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
 if Gitlab::Runtime.puma? && ::Puma.cli_config.options[:workers].to_i == 0
+  return if Gitlab::Utils.to_boolean(ENV['PUMA_SKIP_CLUSTER_VALIDATION'])
+
   raise 'Puma is only supported in Cluster-mode: workers > 0'
 end
