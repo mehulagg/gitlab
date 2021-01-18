6d753a672f7a59e72760e66c343ab27c2844d81f
Nicolò Maria Mezzopera <nicolo.mezzopera@gmail.com>
2021-01-18 14:11:30 +0100

    Add basic section constants

---
+++ app/assets/javascripts/packages_and_registries/settings/group/constants.js
@@ -0,0 +1,6 @@
+import { s__ } from '~/locale';
+
+export const PACKAGE_SETTINGS_HEADER = s__('PackageRegistry|Package Registry');
+export const PACKAGE_SETTINGS_DESCRIPTION = s__(
+  'PackageRegistry|GitLab Packages allows organizations to utilize GitLab as a private repository for a variety of common package formats',
+);
