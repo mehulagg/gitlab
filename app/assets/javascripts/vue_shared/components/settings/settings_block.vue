39a57e3f4b1fe884c32ed875693f9c63b055577d
Nicolò Maria Mezzopera <nicolo.mezzopera@gmail.com>
2021-01-18 14:10:54 +0100

    Add new settings block component
    - source
    - test
    - snapshots

---
+++ app/assets/javascripts/vue_shared/components/settings/settings_block.vue
@@ -0,0 +1,43 @@
+<script>
+import { __ } from '~/locale';
+
+export default {
+  props: {
+    defaultExpanded: {
+      type: Boolean,
+      default: false,
+      required: false,
+    },
+  },
+  data() {
+    return {
+      sectionExpanded: false,
+    };
+  },
+  computed: {
+    expanded() {
+      return this.defaultExpanded || this.sectionExpanded;
+    },
+    toggleText() {
+      return this.expanded ? __('Collapse') : __('Expand');
+    },
+  },
+};
+</script>
+
+<template>
+  <section class="settings no-animate" :class="{ expanded }">
+    <div class="settings-header">
+      <h4><slot name="title"></slot></h4>
+      <button data-testid="expand-button" class="btn" @click="sectionExpanded = !sectionExpanded">
+        {{ toggleText }}
+      </button>
+      <p>
+        <slot name="description"></slot>
+      </p>
+    </div>
+    <div class="settings-content">
+      <slot></slot>
+    </div>
+  </section>
+</template>
