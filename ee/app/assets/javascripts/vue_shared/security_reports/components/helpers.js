9d3ebd2a782f7b29bc022b9b8199b896df10bb42
Fernando <farias@gitlab.com>
2021-01-18 20:55:57 -0600

    Show request/reply empty body
    * Update unit tests

--- ee/app/assets/javascripts/vue_shared/security_reports/components/helpers.js
+++ ee/app/assets/javascripts/vue_shared/security_reports/components/helpers.js
@@ -1,3 +1,6 @@
 import { EMPTY_BODY_MESSAGE } from './constants';
 
-export const bodyWithFallBack = (body) => body || EMPTY_BODY_MESSAGE;
+export const bodyWithFallBack = (body) => {
+  // Return string if defined or blank string, otherwise fallback
+  return body === '' ? '' : body || EMPTY_BODY_MESSAGE;
+};
