893788e0be1dc539b695d37a51ffbd3c97a8671a
Austin Regnery <aregnery@gitlab.com>
2021-01-19 09:48:51 -0500

    Change code to control

--- ee/spec/views/layouts/nav/sidebar/_push_rules_link.html.haml_spec.rb
+++ ee/spec/views/layouts/nav/sidebar/_push_rules_link.html.haml_spec.rb
@@ -3,23 +3,23 @@
 require 'spec_helper'
 
 RSpec.describe 'layouts/nav/sidebar/_push_rules_link' do
-  context 'license includes Source Code feature' do
+  context 'license includes source control feature' do
     it 'shows the link' do
       stub_licensed_features(push_rules: true)
 
       render
 
-      expect(rendered).to have_link 'Source Code'
+      expect(rendered).to have_link 'Source Control'
     end
   end
 
-  context 'license does not include Source Code feature' do
+  context 'license does not include source control feature' do
     it 'hides the link' do
       stub_licensed_features(push_rules: false)
 
       render
 
-      expect(rendered).not_to have_link 'Source Code'
+      expect(rendered).not_to have_link 'Source Control'
     end
   end
 end
