f129588100c7a55a1963692c0a764c187e94f9e9
Austin Regnery <aregnery@gitlab.com>
2021-01-19 08:51:11 -0500

    Updated spec to check for source code

--- ee/spec/views/layouts/nav/sidebar/_push_rules_link.html.haml_spec.rb
+++ ee/spec/views/layouts/nav/sidebar/_push_rules_link.html.haml_spec.rb
@@ -3,23 +3,23 @@
 require 'spec_helper'
 
 RSpec.describe 'layouts/nav/sidebar/_push_rules_link' do
-  context 'license includes push rules feature' do
+  context 'license includes Source Code feature' do
     it 'shows the link' do
       stub_licensed_features(push_rules: true)
 
       render
 
-      expect(rendered).to have_link 'Push Rules'
+      expect(rendered).to have_link 'Source Code'
     end
   end
 
-  context 'license does not include push rules feature' do
+  context 'license does not include Source Code feature' do
     it 'hides the link' do
       stub_licensed_features(push_rules: false)
 
       render
 
-      expect(rendered).not_to have_link 'Push Rules'
+      expect(rendered).not_to have_link 'Source Code'
     end
   end
 end
