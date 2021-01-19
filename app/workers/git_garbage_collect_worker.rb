cf7292010218484a451e01147a9333578c27e0b3
Francisco Javier López <fjlopez@gitlab.com>
2021-01-19 18:36:20 +0100

    Add comment

--- app/workers/git_garbage_collect_worker.rb
+++ app/workers/git_garbage_collect_worker.rb
@@ -1,5 +1,9 @@
 # frozen_string_literal: true
 
+# According to our docs, we can only remove workers on major releases
+# https://docs.gitlab.com/ee/development/sidekiq_style_guide.html#removing-workers.
+#
+# So we need to still maintainer this until 14.0 but with the current functionality.
 class GitGarbageCollectWorker # rubocop:disable Scalability/IdempotentWorker
   include ApplicationWorker
 
