a60cfd54e9c89f4beb792578c8246f3e7a73b159
Zack Cuddy <zcuddy@gitlab.com>
2021-01-20 09:35:50 -0600

    Sample

---
+++ app/assets/javascripts/search/topbar/components/scope_tabs.vue
@@ -0,0 +1,29 @@
+<script>
+  export default {
+    name: 'ScopeTabs',
+    props: {
+      scopeTabs: {
+        type: Object,
+        required: false,
+        default: () => ([]),
+      },
+    },
+    mounted() {
+      const url = `${scopeTabs[0].url}`
+
+      return api
+        .get(url)
+        .then(({ data }) => showCount(el, data.count))
+        .catch((e) => {
+          // eslint-disable-next-line no-console
+          console.error(`Failed to fetch search count from '${url}'.`, e);
+        });
+    }
+  }
+</script>
+
+<template>
+  <div>
+    <gl-tabs v-for="tabs in scopeTabs"/>
+  </div>
+</template>
