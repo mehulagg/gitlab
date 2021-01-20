d9b7adfa0bf8c699317f1a1281e539cb7ad6598a
Serena Fang <sfang@gitlab.com>
2021-01-20 17:22:30 -0600

    Add frozen string literal comment

--- spec/requests/api/resource_access_tokens_spec.rb
+++ spec/requests/api/resource_access_tokens_spec.rb
@@ -1,18 +1,19 @@
+# frozen_string_literal: true
+
 require "spec_helper"
 
 RSpec.describe API::ResourceAccessTokens do
   describe "GET /:id/access_tokens" do
-    context "when the resource is a project" do 
+    context "when the resource is a project" do
       let_it_be(:admin) { create(:admin) }
       let_it_be(:resource) { create(:project) }
- 
-      before do 
+
+      before do
         resource.add_maintainer(admin)
-        
       end
-      
+
       it "gets a list of resource access tokens for the specified project" do
-        access_token = ResourceAccessTokens::CreateService.new(admin, resource, {} ).execute
+        ResourceAccessTokens::CreateService.new(admin, resource, {} ).execute
         get api("/projects/#{resource.id}/access_tokens", admin)
 
         expect(response).to have_gitlab_http_status(:ok)
