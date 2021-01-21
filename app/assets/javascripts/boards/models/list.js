167e407fa6672bab6b49210d20ffd90f271044fa
Heinrich Lee Yu <heinrich@gitlab.com>
2021-01-21 16:49:05 +0800

    Add iteration to board list settings
    Also adds the ListIteration model

--- app/assets/javascripts/boards/models/list.js
+++ app/assets/javascripts/boards/models/list.js
@@ -5,6 +5,7 @@ import ListAssignee from './assignee';
 import { deprecatedCreateFlash as flash } from '~/flash';
 import boardsStore from '../stores/boards_store';
 import ListMilestone from './milestone';
+import ListIetration from './iteration';
 import 'ee_else_ce/boards/models/issue';
 
 const TYPES = {
@@ -57,6 +58,9 @@ class List {
     } else if (IS_EE && obj.milestone) {
       this.milestone = new ListMilestone(obj.milestone);
       this.title = this.milestone.title;
+    } else if (IS_EE && obj.iteration) {
+      this.iteration = new ListIteration(obj.iteration);
+      this.title = this.iteration.title;
     }
 
     // doNotFetchIssues is a temporary workaround until issues are fetched using GraphQL on issue boards
