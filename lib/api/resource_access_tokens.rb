9d30fa7811d27f8479a6fca042d22cf599dd4f22
Serena Fang <sfang@gitlab.com>
2021-01-21 14:45:36 -0600

    Improve prat api specs
    Added more specs for failure cases

--- lib/api/resource_access_tokens.rb
+++ lib/api/resource_access_tokens.rb
@@ -19,13 +19,12 @@ module API
     end
 
     %w[group project].each do |source_type|
-      params do
-        requires :id, type: String, desc: "The #{source_type} ID"
-        optional :token_id, type: String, desc: "The ID of the token"
-      end
       resource source_type.pluralize, requirements: API::NAMESPACE_OR_PROJECT_REQUIREMENTS do
         desc 'Get list of all access tokens for the specified resource' do
           detail 'This feature was introduced in GitLab 13.9.'
+        end
+        params do
+          requires :id, type: String, desc: "The #{source_type} ID"
         end
         get ":id/access_tokens" do
           resource = find_source(source_type, params[:id])
@@ -38,6 +37,10 @@ module API
         desc 'Revokes a resource access token' do
           detail 'This feature was introduced in GitLab 13.9.'
         end
+        params do
+          requires :id, type: String, desc: "The #{source_type} ID"
+          optional :token_id, type: String, desc: "The ID of the token"
+        end
         delete ':id/access_tokens/:token_id' do
           resource = find_source(source_type, params[:id])
           token = find_token(params[:token_id])
@@ -48,13 +51,14 @@ module API
             token
           ).execute
 
-          service.success? ? no_content! : bad_request!(nil)
+          service.success? ? no_content! : bad_request!(service.message)
         end
 
         desc 'Create a resource access token' do
           detail 'This feature was introduced in GitLab 13.9.'
         end
         params do
+          requires :id, type: String, desc: "The #{source_type} ID"
           requires :name, type: String, desc: "Resource access token name"
           requires :scopes, type: Array[String], desc: "The permissions of the token"
           optional :expires_at, type: Date, desc: "The expiration date of the token"
@@ -71,7 +75,7 @@ module API
           if token_response.success?
             present token_response.payload[:access_token], with: Entities::PersonalAccessToken
           else
-            token_response.message
+            bad_request!(token_response.message)
           end
         end
       end
--- spec/requests/api/resource_access_tokens_spec.rb
+++ spec/requests/api/resource_access_tokens_spec.rb
@@ -3,56 +3,128 @@
 require "spec_helper"
 
 RSpec.describe API::ResourceAccessTokens do
-  before do
-    resource.add_maintainer(admin)
-  end
+  context 'when the resource is a project' do
+    let_it_be(:admin) { create(:admin) }
+    let_it_be(:user) { create(:user) }
+    let_it_be(:resource) { create(:project) }
 
-  let_it_be(:admin) { create(:admin) }
-  let_it_be(:resource) { create(:project) }
-
-  describe "GET /:id/access_tokens" do
-    context "when the resource is a project" do
+    describe "GET /:id/access_tokens" do
       before do
         resource.add_maintainer(admin)
-      end
-
-      it "gets a list of resource access tokens for the specified project" do
         ResourceAccessTokens::CreateService.new(admin, resource, {} ).execute
-        get api("/projects/#{resource.id}/access_tokens", admin)
+      end
 
-        expect(response).to have_gitlab_http_status(:ok)
+      context "when the user has valid permissions" do
+        it "gets a list of resource access tokens for the specified project" do
+          get api("/projects/#{resource.id}/access_tokens", admin)
+
+          expect(response).to have_gitlab_http_status(:ok)
+        end
+      end
+
+      context "when the user does not have valid permissions" do
+        it "returns 401 Unauthorized" do
+          get api("/projects/#{resource.id}/access_tokens", user)
+
+          expect(response).to have_gitlab_http_status(:unauthorized)
+        end
       end
     end
-  end
 
-  describe "DELETE /:id/access_tokens/:token_id" do
-    context "when the resource is a project" do
+    describe "DELETE /:id/access_tokens/:token_id" do
       before do
         resource.add_maintainer(admin)
+        @token = ResourceAccessTokens::CreateService.new(admin, resource, {} ).execute
       end
 
-      it "deletes the project access token from the project" do
-        token = ResourceAccessTokens::CreateService.new(admin, resource, {} ).execute
+      context "when the user has valid permissions" do
+        it "deletes the project access token from the project" do
+          delete api("/projects/#{resource.id}/access_tokens/#{@token.payload[:access_token].id}", admin)
 
-        delete api("/projects/#{resource.id}/access_tokens/#{token.payload[:access_token].id}", admin)
+          expect(response).to have_gitlab_http_status(:no_content)
+        end
+      end
 
-        expect(response).to have_gitlab_http_status(:no_content)
+      context "when the user does not have valid permissions" do
+        before do
+          resource.add_developer(user)
+        end
+
+        it "does not delete the token, and returns 400" do
+          delete api("/projects/#{resource.id}/access_tokens/#{@token.payload[:access_token].id}", user)
+
+          expect(response).to have_gitlab_http_status(:bad_request)
+          expect(response.body).to include("#{user.name} cannot delete #{@token.payload[:access_token].user.name}")
+        end
+      end
+
+      context "when attempting to delete a non-existent project access token" do
+        it "does not delete the token, and returns 404" do
+          delete api("/projects/#{resource.id}/access_tokens/1337", admin)
+
+          expect(response).to have_gitlab_http_status(:not_found)
+        end
       end
     end
-  end
 
-  describe "POST /:id/access_tokens" do
-    context 'when resource is a project'
-    let_it_be(:params) { { name: 'Random bot', scopes: ["api"] } }
+    describe "POST /:id/access_tokens" do
+      context "when the user has valid permissions" do
+        before do
+          resource.add_maintainer(admin)
+        end
 
-    before do
-      resource.add_maintainer(admin)
-    end
+        context "with valid params" do
+          context "with full params" do
+            let_it_be(:full_params) { { name: 'test', scopes: ["api"], expires_at: Date.today + 1.month } }
 
-    it 'creates a project access token with the params' do
-      post api("/projects/#{resource.id}/access_tokens", admin), params: params
+            it 'creates a project access token with the params' do
+              post api("/projects/#{resource.id}/access_tokens", admin), params: full_params
 
-      expect(response).to have_gitlab_http_status(:created)
+              expect(response).to have_gitlab_http_status(:created)
+              expect(json_response["name"]).to eq('test')
+              expect(json_response["scopes"]).to eq(["api"])
+              expect(json_response["expires_at"]).to eq((Date.today + 1.month).iso8601)
+            end
+          end
+
+          context "when 'expires_at' is not set" do
+            let_it_be(:params_missing_expires_at) { { name: 'test', scopes: ["api"] } }
+
+            it 'creates a project access token with the params' do
+              post api("/projects/#{resource.id}/access_tokens", admin), params: params_missing_expires_at
+
+              expect(response).to have_gitlab_http_status(:created)
+              expect(json_response["name"]).to eq('test')
+              expect(json_response["scopes"]).to eq(["api"])
+              expect(json_response["expires_at"]).to eq(nil)
+            end
+          end
+        end
+
+        context "with invalid params" do
+          context "when missing the 'name' param" do
+            let_it_be(:params_missing_name) { { scopes: ["api"], expires_at: 5.days.from_now } }
+
+            it 'does not creates a project access token with the params' do
+              post api("/projects/#{resource.id}/access_tokens", admin), params: params_missing_name
+
+              expect(response).to have_gitlab_http_status(:bad_request)
+              expect(response.body).to include("name is missing")
+            end
+          end
+
+          context "when missing the 'scopes' param" do
+            let_it_be(:params_missing_scope) { { name: 'test', expires_at: 5.days.from_now } }
+
+            it 'does not creates a project access token with the params' do
+              post api("/projects/#{resource.id}/access_tokens", admin), params: params_missing_scope
+
+              expect(response).to have_gitlab_http_status(:bad_request)
+              expect(response.body).to include("scopes is missing")
+            end
+          end
+        end
+      end
     end
   end
 end
