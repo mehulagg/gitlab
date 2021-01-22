f15ae415a493a58bbb2d85c1618d6a114b54818d
Alexander Turinske <aturinske@gitlab.com>
2021-01-22 12:07:43 -0700

    Replace bootstrap tabs behavior with GitLab's
    - remove .nav-tabs class and data-toggle to remove bootstrap
      tabs usage
    - create custom tabs interation handler to mimic bootstrap

--- app/assets/javascripts/pages/groups/new/index.js
+++ app/assets/javascripts/pages/groups/new/index.js
@@ -1,9 +1,11 @@
 import $ from 'jquery';
+import Vue from 'vue';
 import BindInOut from '~/behaviors/bind_in_out';
 import initFilePickers from '~/file_pickers';
 import Group from '~/group';
 import LinkedTabs from '~/lib/utils/bootstrap_linked_tabs';
 import GroupPathValidator from './group_path_validator';
+import Tabs from './group_tabs.vue';
 
 const parentId = $('#group_parent_id');
 if (!parentId.val()) {
@@ -13,6 +15,25 @@ BindInOut.initAll();
 initFilePickers();
 
 new Group(); // eslint-disable-line no-new
+
+// eslint-disable-next-line no-new
+new Vue({
+  el: document.querySelector('.js-gl-group-tabs'),
+  render: (h) =>
+    h(Tabs, {
+      on: {
+        input: (current) => {
+          document.querySelectorAll('.gitlab-tab-content .tab-pane').forEach((el, index) => {
+            if (current === index) {
+              el.classList.add('active');
+            } else {
+              el.classList.remove('active');
+            }
+          });
+        },
+      },
+    }),
+});
 
 const CONTAINER_SELECTOR = '.group-edit-container .nav-tabs';
 const DEFAULT_ACTION = '#create-group-pane';
