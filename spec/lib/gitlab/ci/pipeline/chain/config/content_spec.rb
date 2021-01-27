df7d66b481e4620aeab19c479eff0e6350d46710
Aishwarya Subramanian <asubramanian@gitlab.com>
2021-01-27 12:47:51 -0600

    Added specs for content spec

--- spec/lib/gitlab/ci/pipeline/chain/config/content_spec.rb
+++ spec/lib/gitlab/ci/pipeline/chain/config/content_spec.rb
@@ -163,6 +163,32 @@ RSpec.describe Gitlab::Ci::Pipeline::Chain::Config::Content do
       end
     end
 
+    context 'when project has compliance pipeline configuration defined' do
+      let(:ci_config_path) { nil }
+      let(:compliance_group) { create(:group, :private, name: "compliance") }
+      let(:compliance_project) { create(:project, namespace: compliance_group, name: "hippa") }
+
+      let(:framework) { create(:compliance_framework, namespace_id: compliance_group.id, pipeline_configuration_full_path: ".compliance-gitlab-ci.yml@compliance/hippa") }
+      let!(:framework_project_setting) { create(:compliance_framework_project_setting, project: project, framework_id: framework.id) }
+
+      let(:content_result) do
+        <<~EOY
+          ---
+          include:
+          - project: compliance/hippa
+            file: ".compliance-gitlab-ci.yml"
+        EOY
+      end
+
+      it 'includes compliance pipeline configuration content' do
+        subject.perform!
+
+        expect(pipeline.config_source).to eq 'external_project_source'
+        expect(pipeline.pipeline_config.content).to eq(content_result)
+        expect(command.config_content).to eq(content_result)
+      end
+    end
+
     context 'when config is not defined anywhere' do
       let(:ci_config_path) { nil }
 
