69ba84deb4319570556c5e882abdb4bb1ee35817
Ian Baum <ibaum@gitlab.com>
2021-01-27 11:18:40 -0600

    Use documented process for adding not null restraint
    * Adding validates to Geo::LfsObjectRegistry model
    * Making migrations post migrations
    * Adding from: to: to make default change roll backable

--- ee/db/geo/post_migrate/20210120225014_migrate_lfs_object_registry.rb
+++ ee/db/geo/post_migrate/20210120225014_migrate_lfs_object_registry.rb
@@ -10,9 +10,9 @@ class MigrateLfsObjectRegistry < ActiveRecord::Migration[6.0]
   def change
     table_name = :lfs_object_registry
 
-    change_column_null table_name, :lfs_object_id, false
-    change_column_null table_name, :created_at, false
-    change_column_default table_name, :retry_count, 0
+    add_not_null_constraint table_name, :lfs_object_id, validate: false
+    add_not_null_constraint table_name, :created_at, validate: false
+    change_column_default table_name, :retry_count, from: nil, to: 0
     change_column table_name, :retry_count, :integer, limit: 2
     add_column table_name, :state, :integer, null: false, limit: 2, default: 0
     add_column table_name, :last_synced_at, :datetime_with_timezone
