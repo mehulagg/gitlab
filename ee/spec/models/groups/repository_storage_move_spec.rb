fa6726b527414bbd3dd24468d9988af2c1f95d30
Francisco Javier López <fjlopez@gitlab.com>
2021-01-27 19:01:25 +0100

    Remove unnecessary key

--- ee/spec/models/groups/repository_storage_move_spec.rb
+++ ee/spec/models/groups/repository_storage_move_spec.rb
@@ -3,7 +3,7 @@
 require 'spec_helper'
 
 RSpec.describe Groups::RepositoryStorageMove, type: :model do
-  it_behaves_like 'handles repository moves', check_worker: false do
+  it_behaves_like 'handles repository moves' do
     let_it_be_with_refind(:container) { create(:group) }
 
     let(:repository_storage_factory_key) { :group_repository_storage_move }
