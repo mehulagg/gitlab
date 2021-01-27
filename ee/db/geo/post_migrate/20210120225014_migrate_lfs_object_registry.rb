8d803ebb6eb8b027fcbce44a01d23da0884f5c54
Ian Baum <ibaum@gitlab.com>
2021-01-27 15:37:39 -0600

    Not able to use constant for table name in migrations

--- ee/db/geo/post_migrate/20210120225014_migrate_lfs_object_registry.rb
+++ ee/db/geo/post_migrate/20210120225014_migrate_lfs_object_registry.rb
@@ -5,29 +5,26 @@ class MigrateLfsObjectRegistry < ActiveRecord::Migration[6.0]
 
   DOWNTIME = false
 
-  TABLE_NAME = :lfs_object_registry
-
   disable_ddl_transaction!
 
   def up
-    add_not_null_constraint TABLE_NAME, :lfs_object_id, validate: false
-    add_not_null_constraint TABLE_NAME, :created_at, validate: false
-    change_column_default TABLE_NAME, :retry_count, from: nil, to: 0
-    change_column TABLE_NAME, :retry_count, :integer, limit: 2
-    add_column TABLE_NAME, :state, :integer, null: false, limit: 2, default: 0
-    add_column TABLE_NAME, :last_synced_at, :datetime_with_timezone
-    add_column TABLE_NAME, :last_sync_failure, :text
-
-    add_text_limit TABLE_NAME, :last_sync_failure, 255
+    add_not_null_constraint :lfs_object_registry, :lfs_object_id, validate: false
+    add_not_null_constraint :lfs_object_registry, :created_at, validate: false
+    change_column_default :lfs_object_registry, :retry_count, from: nil, to: 0
+    change_column :lfs_object_registry, :retry_count, :integer, limit: 2
+    add_column :lfs_object_registry, :state, :integer, null: false, limit: 2, default: 0
+    add_column :lfs_object_registry, :last_synced_at, :datetime_with_timezone
+    add_column :lfs_object_registry, :last_sync_failure, :text
+    add_text_limit :lfs_object_registry, :last_sync_failure, 255
   end
 
   def down
-    remove_not_null_constraint TABLE_NAME, :lfs_object_id
-    remove_not_null_constraint TABLE_NAME, :created_at
-    change_column_default TABLE_NAME, :retry_count, from: 0, to: nil
-    change_column TABLE_NAME, :retry_count, :integer, limit: nil
-    remove_column TABLE_NAME, :state
-    remove_column TABLE_NAME, :last_synced_at
-    remove_column TABLE_NAME, :last_sync_failure
+    remove_not_null_constraint :lfs_object_registry, :lfs_object_id
+    remove_not_null_constraint :lfs_object_registry, :created_at
+    change_column_default :lfs_object_registry, :retry_count, from: 0, to: nil
+    change_column :lfs_object_registry, :retry_count, :integer, limit: nil
+    remove_column :lfs_object_registry, :state
+    remove_column :lfs_object_registry, :last_synced_at
+    remove_column :lfs_object_registry, :last_sync_failure
   end
 end
