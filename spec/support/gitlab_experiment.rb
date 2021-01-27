2ad39235b4dd0b2eef36d8093e90b5edb04fc4f8
jejacks0n <jjackson@gitlab.com>
2021-01-26 21:56:01 -0700

    This is a quick fix for Feature use

--- spec/support/gitlab_experiment.rb
+++ spec/support/gitlab_experiment.rb
@@ -3,5 +3,14 @@
 # Require the provided spec helper and matchers.
 require 'gitlab/experiment/rspec'
 
+module Gitlab::Experiment::RSpecHelpers
+  def stub_experiments(experiments)
+    # quick fix: https://gitlab.com/gitlab-org/gitlab/-/issues/300104
+    experiments.each { |name, _| Feature.persist_used!(name) }
+
+    super
+  end
+end
+
 # Disable all caching for experiments in tests.
 Gitlab::Experiment::Configuration.cache = nil
