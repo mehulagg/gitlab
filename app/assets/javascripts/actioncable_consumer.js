bb3ff94786112e9705addb44de0532af9b9ca3cc
Heinrich Lee Yu <heinrich@gitlab.com>
2021-01-28 00:38:51 +0800

    Increase ActionCable retry interval

--- app/assets/javascripts/actioncable_consumer.js
+++ app/assets/javascripts/actioncable_consumer.js
@@ -1,3 +1,5 @@
-import { createConsumer } from '@rails/actioncable';
+import { ConnectionMonitor, createConsumer } from '@rails/actioncable';
+
+ConnectionMonitor.pollInterval.max = 300;
 
 export default createConsumer();
