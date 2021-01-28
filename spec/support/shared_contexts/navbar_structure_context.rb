ac07043b0df7884fa444c0ffd8e12995fdf71b4d
Jannik Lehmann <jlehmann@gitlab.com>
2021-01-28 09:16:25 +0000

    Fix Linting

--- spec/support/shared_contexts/navbar_structure_context.rb
+++ spec/support/shared_contexts/navbar_structure_context.rb
@@ -19,7 +19,7 @@ RSpec.shared_context 'project navbar structure' do
       nav_item: _('Security & Compliance'),
       nav_sub_items: [
         _('Configuration'),
-        (_('Audit Events') if Gitlab.ee?),
+        (_('Audit Events') if Gitlab.ee?)
       ]
     }
   end
