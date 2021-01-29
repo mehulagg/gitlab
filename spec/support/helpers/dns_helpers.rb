0b4370f5d1a20153835025d065b4a52b5fd01f1b
Harsh Chouraria <hchouraria@gitlab.com>
2021-01-29 13:11:02 +0000

    Fix DNS mock helpers
    The existing DNS mock helpers do not support receiving just the timeout
    keyword option passed to the getaddrinfo call, leading to the fixture
    failures in the pipeline tests.

--- spec/support/helpers/dns_helpers.rb
+++ spec/support/helpers/dns_helpers.rb
@@ -13,6 +13,7 @@ module DnsHelpers
 
   def stub_all_dns!
     allow(Addrinfo).to receive(:getaddrinfo).with(anything, anything, nil, :STREAM).and_return([])
+    allow(Addrinfo).to receive(:getaddrinfo).with(anything, anything, nil, :STREAM, anything).and_return([])
     allow(Addrinfo).to receive(:getaddrinfo).with(anything, anything, nil, :STREAM, anything, anything).and_return([])
   end
 
@@ -20,11 +21,15 @@ module DnsHelpers
     allow(Addrinfo).to receive(:getaddrinfo).with(/\Afoobar\.\w|(\d{1,3}\.){4,}\d{1,3}\z/i, anything, nil, :STREAM) do
       raise SocketError.new("getaddrinfo: Name or service not known")
     end
+    allow(Addrinfo).to receive(:getaddrinfo).with(/\Afoobar\.\w|(\d{1,3}\.){4,}\d{1,3}\z/i, anything, nil, :STREAM, anything) do
+      raise SocketError.new("getaddrinfo: Name or service not known")
+    end
   end
 
   def permit_local_dns!
     local_addresses = /\A(127|10)\.0\.0\.\d{1,3}|(192\.168|172\.16)\.\d{1,3}\.\d{1,3}|0\.0\.0\.0|localhost\z/i
     allow(Addrinfo).to receive(:getaddrinfo).with(local_addresses, anything, nil, :STREAM).and_call_original
+    allow(Addrinfo).to receive(:getaddrinfo).with(local_addresses, anything, nil, :STREAM, anything).and_call_original
     allow(Addrinfo).to receive(:getaddrinfo).with(local_addresses, anything, nil, :STREAM, anything, anything, any_args).and_call_original
   end
 end
