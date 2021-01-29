9833f2599c67afef0cae68baca39e2c7abda6d82
Sanad Liaquat <sliaquat@gitlab.com>
2021-01-29 19:18:14 +0000

    Apply 1 suggestion(s) to 1 file(s)

--- qa/qa/specs/features/ee/browser_ui/1_manage/group/group_ldap_sync_spec.rb
+++ qa/qa/specs/features/ee/browser_ui/1_manage/group/group_ldap_sync_spec.rb
@@ -15,6 +15,7 @@ module QA
         @original_personal_access_token = Runtime::Env.personal_access_token
 
         # Todo: Remove the 5 lines below when invite_members_group_modal feature flag is enabled by default or removed
+# We need to nil out any existing personal token generated for the non-admin LDAP user and also set Runtime::Env.ldap_username=nil so that a new admin token is created for use to enable the feature flag.
         Runtime::Env.personal_access_token = nil
         ldap_username = Runtime::Env.ldap_username
         Runtime::Env.ldap_username = nil
