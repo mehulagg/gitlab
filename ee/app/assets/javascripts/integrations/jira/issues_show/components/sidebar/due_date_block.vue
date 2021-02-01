674c9f829b734fb20993ff4c3c78ad2fcdc26972
Tom Quirk <tquirk@gitlab.com>
2021-02-01 15:22:26 +0930

    Init due_date_block component for issuable sidebar

---
+++ ee/app/assets/javascripts/integrations/jira/issues_show/components/sidebar/due_date_block.vue
@@ -0,0 +1,34 @@
+<script>
+import { GlIcon } from '@gitlab/ui';
+
+export default {
+  components: { GlIcon },
+  props: {
+    dueDate: {
+      type: Date,
+      required: false,
+      default: null,
+    },
+  },
+};
+</script>
+
+<template>
+  <div>
+    <div class="sidebar-collapsed-icon">
+      <gl-icon name="calendar" />
+      <span v-if="dueDate"> {{ dueDate }} </span>
+      <span v-else> {{ __('None') }} </span>
+    </div>
+
+    <div class="title hide-collapsed">
+      {{ __('Due date') }}
+    </div>
+    <div class="value hide-collapsed">
+      <span class="value-content">
+        <span v-if="dueDate" class="gl-font-weight-bold"> {{ dueDate }} </span>
+        <span v-else class="no-value"> {{ __('None') }} </span>
+      </span>
+    </div>
+  </div>
+</template>
