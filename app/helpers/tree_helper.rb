8b20f0ad61f29ea7566f33aba2f07ffa2725de8b
jboyson <jboyson@gitlab.com>
2021-02-02 18:21:19 -0600

    Remove unused files
    These have been replaced with Vue components.

--- app/helpers/tree_helper.rb
+++ app/helpers/tree_helper.rb
@@ -6,50 +6,7 @@ module TreeHelper
 
   FILE_LIMIT = 1_000
 
-  # Sorts a repository's tree so that folders are before files and renders
-  # their corresponding partials
-  #
-  # tree - A `Tree` object for the current tree
-  # rubocop: disable CodeReuse/ActiveRecord
-  def render_tree(tree)
-    # Sort submodules and folders together by name ahead of files
-    folders, files, submodules = tree.trees, tree.blobs, tree.submodules
-    tree = []
-    items = (folders + submodules).sort_by(&:name) + files
-
-    if items.size > FILE_LIMIT
-      tree << render(partial: 'projects/tree/truncated_notice_tree_row',
-                     locals: { limit: FILE_LIMIT, total: items.size })
-      items = items.take(FILE_LIMIT)
-    end
-
-    tree << render(partial: 'projects/tree/tree_row', collection: items) if items.present?
-    tree.join.html_safe
-  end
   # rubocop: enable CodeReuse/ActiveRecord
-
-  # Return an image icon depending on the file type and mode
-  #
-  # type - String type of the tree item; either 'folder' or 'file'
-  # mode - File unix mode
-  # name - File name
-  def tree_icon(type, mode, name)
-    sprite_icon(file_type_icon_class(type, mode, name))
-  end
-
-  # Using Rails `*_path` methods can be slow, especially when generating
-  # many paths, as with a repository tree that has thousands of items.
-  def fast_project_blob_path(project, blob_path)
-    ActionDispatch::Journey::Router::Utils.escape_path(
-      File.join(relative_url_root, project.path_with_namespace, '-', 'blob', blob_path)
-    )
-  end
-
-  def fast_project_tree_path(project, tree_path)
-    ActionDispatch::Journey::Router::Utils.escape_path(
-      File.join(relative_url_root, project.path_with_namespace, '-', 'tree', tree_path)
-    )
-  end
 
   # Simple shortcut to File.join
   def tree_join(*args)
@@ -96,14 +53,6 @@ module TreeHelper
     "#{username}-#{ref}-patch-#{epoch}"
   end
 
-  def tree_edit_project(project = @project)
-    if can?(current_user, :push_code, project)
-      project
-    elsif current_user && current_user.already_forked?(project)
-      current_user.fork_of(project)
-    end
-  end
-
   def edit_in_new_fork_notice_now
     _("You're not allowed to make changes to this project directly. "\
       "A fork of this project is being created that you can make changes in, so you can submit a merge request.")
@@ -147,11 +96,6 @@ module TreeHelper
         yield(part, part_path)
       end
     end
-  end
-
-  def up_dir_path
-    file = File.join(@path, "..")
-    tree_join(@ref, file)
   end
 
   # returns the relative path of the first subdir that doesn't have only one directory descendant
