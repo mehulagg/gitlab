fd5c077041a6a4ef0bb1218524faf32b4d307487
Sean McGivern <sean@gitlab.com>
2021-02-04 15:14:49 +0000

    Allow setting Gitaly feature flags for individual projects
    When we call Gitaly and set feature flags, we can't easily guarantee
    that we can get the current project (if there even is such a thing).
    However, there are some cases where we do have easy access to a project,
    if one exists: when we're performing Git HTTP or SSH operations.
    
    This allows - but doesn't require - Gitaly feature flags to be set for a
    project, as long as the call site supports it. In this commit, that
    means the two cases above (Git HTTP and SSH).

--- lib/feature/gitaly.rb
+++ lib/feature/gitaly.rb
@@ -5,25 +5,30 @@ class Feature
     PREFIX = "gitaly_"
 
     class << self
-      def enabled?(feature_flag)
+      def enabled?(feature_flag, project = nil)
         return false unless Feature::FlipperFeature.table_exists?
 
-        Feature.enabled?("#{PREFIX}#{feature_flag}")
+        feature_flag = "#{PREFIX}#{feature_flag}" unless feature_flag.starts_with?(PREFIX)
+
+        Feature.enabled?(feature_flag, project)
       rescue ActiveRecord::NoDatabaseError, PG::ConnectionBad
         false
       end
 
-      def server_feature_flags
+      def server_feature_flags(project = nil)
         # We need to check that both the DB connection and table exists
         return {} unless ::Gitlab::Database.cached_table_exists?(FlipperFeature.table_name)
 
-        Feature.persisted_names
+        Feature
+          .persisted_names
           .select { |f| f.start_with?(PREFIX) }
-          .map do |f|
-          flag = f.delete_prefix(PREFIX)
+          .to_h { |f| [gitaly_feature_flag_name(f), enabled?(f, project).to_s] }
+      end
 
-          ["gitaly-feature-#{flag.tr('_', '-')}", enabled?(flag).to_s]
-        end.to_h
+      private
+
+      def gitaly_feature_flag_name(feature_flag)
+        "gitaly-feature-#{feature_flag.delete_prefix(PREFIX).tr('_', '-')}"
       end
     end
   end
--- spec/lib/feature/gitaly_spec.rb
+++ spec/lib/feature/gitaly_spec.rb
@@ -3,35 +3,81 @@
 require 'spec_helper'
 
 RSpec.describe Feature::Gitaly do
-  let(:feature_flag) { "mep_mep" }
+  let_it_be(:project) { create(:project) }
+  let_it_be(:project_2) { create(:project) }
 
   describe ".enabled?" do
-    context 'when the gate is closed' do
-      before do
-        stub_feature_flags(gitaly_mep_mep: false)
+    it 'accepts a flag with or without the prefix' do
+      stub_feature_flags(gitaly_mep_mep: true)
+
+      expect(described_class.enabled?('mep_mep')).to be(true)
+      expect(described_class.enabled?('gitaly_mep_mep')).to be(true)
+    end
+
+    context 'when the flag is set globally' do
+      let(:feature_flag) { "mep_mep" }
+
+      context 'when the gate is closed' do
+        before do
+          stub_feature_flags(gitaly_mep_mep: false)
+        end
+
+        it 'returns false' do
+          expect(described_class.enabled?(feature_flag)).to be(false)
+        end
       end
 
-      it 'returns false' do
-        expect(described_class.enabled?(feature_flag)).to be(false)
+      context 'when the flag defaults to on' do
+        it 'returns true' do
+          expect(described_class.enabled?(feature_flag)).to be(true)
+        end
       end
     end
 
-    context 'when the flag defaults to on' do
-      it 'returns true' do
-        expect(described_class.enabled?(feature_flag)).to be(true)
+    context 'when the flag is enabled for a particular project' do
+      let(:feature_flag) { 'deny_disk_access' }
+
+      before do
+        stub_feature_flags(gitaly_deny_disk_access: project)
+      end
+
+      it 'returns true for that project' do
+        expect(described_class.enabled?(feature_flag, project)).to be(true)
+      end
+
+      it 'returns false for any other project' do
+        expect(described_class.enabled?(feature_flag, project_2)).to be(false)
+      end
+
+      it 'returns false when no project is passed' do
+        expect(described_class.enabled?(feature_flag)).to be(false)
       end
     end
   end
 
   describe ".server_feature_flags" do
     before do
-      stub_feature_flags(gitaly_mep_mep: true, foo: true)
+      stub_feature_flags(gitaly_mep_mep: true, gitaly_deny_disk_access: project, foo: false)
     end
 
     subject { described_class.server_feature_flags }
 
-    it { is_expected.to be_a(Hash) }
-    it { is_expected.to eq("gitaly-feature-mep-mep" => "true") }
+    it 'returns a hash of flags starting with the prefix, with dashes instead of underscores' do
+      expect(subject).to eq('gitaly-feature-mep-mep' => 'true',
+                            'gitaly-feature-deny-disk-access' => 'false')
+    end
+
+    context 'when a project is passed' do
+      it 'returns the value for the flag on the given project' do
+        expect(described_class.server_feature_flags(project))
+          .to eq('gitaly-feature-mep-mep' => 'true',
+                 'gitaly-feature-deny-disk-access' => 'true')
+
+        expect(described_class.server_feature_flags(project_2))
+          .to eq('gitaly-feature-mep-mep' => 'true',
+                 'gitaly-feature-deny-disk-access' => 'false')
+      end
+    end
 
     context 'when table does not exist' do
       before do
