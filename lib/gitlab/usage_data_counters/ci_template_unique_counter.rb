e0518bed1e8231384c685186559f28f9a96c796a
Hordur Freyr Yngvason <hfyngvason@gitlab.com>
2021-02-04 14:05:14 +0000

    Track YAML-less Auto DevOps inclusions separately
    In https://gitlab.com/gitlab-org/gitlab/-/merge_requests/51391 we added
    tracking for include:template. Unfortunately, YAML-less Auto DevOps also
    uses include:template for Auto-DevOps.gitab-ci.yml, making it currently
    impossible to differentiate .gitlab-ci.yml template usage from implicit
    usage in our numbers.
    
    This changes it so that pipelines from the auto_devops_source config
    source (i.e. YAML-free Auto DevOps pipelines) are counted separately.

--- lib/gitlab/usage_data_counters/ci_template_unique_counter.rb
+++ lib/gitlab/usage_data_counters/ci_template_unique_counter.rb
@@ -4,6 +4,7 @@ module Gitlab::UsageDataCounters
   class CiTemplateUniqueCounter
     REDIS_SLOT = 'ci_templates'.freeze
 
+    # NOTE! Events with config_source :auto_devops_source get suffixed with _implicit
     TEMPLATE_TO_EVENT = {
       'Auto-DevOps.gitlab-ci.yml' => 'auto_devops',
       'AWS/CF-Provision-and-Deploy-EC2.gitlab-ci.yml' => 'aws_cf_deploy_ec2',
@@ -17,19 +18,21 @@ module Gitlab::UsageDataCounters
     }.freeze
 
     class << self
-      def track_unique_project_event(project_id:, template:)
+      def track_unique_project_event(project_id:, template:, config_source:)
         return if Feature.disabled?(:usage_data_track_ci_templates_unique_projects, default_enabled: :yaml)
 
-        if event = unique_project_event(template)
+        if event = unique_project_event(template, config_source)
           Gitlab::UsageDataCounters::HLLRedisCounter.track_event(event, values: project_id)
         end
       end
 
       private
 
-      def unique_project_event(template)
+      def unique_project_event(template, config_source)
         if name = TEMPLATE_TO_EVENT[template]
-          "p_#{REDIS_SLOT}_#{name}"
+          suffix = '_implicit' if config_source == :auto_devops_source
+
+          "p_#{REDIS_SLOT}_#{name}#{suffix}"
         end
       end
     end
--- spec/lib/gitlab/usage_data_counters/ci_template_unique_counter_spec.rb
+++ spec/lib/gitlab/usage_data_counters/ci_template_unique_counter_spec.rb
@@ -6,25 +6,34 @@ RSpec.describe Gitlab::UsageDataCounters::CiTemplateUniqueCounter do
   let(:project_id) { 1 }
 
   describe '.track_unique_project_event' do
-    described_class::TEMPLATE_TO_EVENT.keys.each do |template|
-      context "when given template #{template}" do
-        it_behaves_like 'tracking unique hll events', :usage_data_track_ci_templates_unique_projects do
-          subject(:request) { described_class.track_unique_project_event(project_id: project_id, template: template) }
+    using RSpec::Parameterized::TableSyntax
 
-          let(:target_id) { "p_ci_templates_#{described_class::TEMPLATE_TO_EVENT[template]}" }
-          let(:expected_type) { instance_of(Integer) }
-        end
-      end
+    where(:config_source, :expected_suffix) do
+      :repository_source  | ''
+      :auto_devops_source | '_implicit'
     end
 
-    it 'does not track templates outside of TEMPLATE_TO_EVENT' do
-      expect(Gitlab::UsageDataCounters::HLLRedisCounter).not_to(
-        receive(:track_event)
-      )
-      Dir.glob(File.join('lib', 'gitlab', 'ci', 'templates', '**'), base: Rails.root) do |template|
-        next if described_class::TEMPLATE_TO_EVENT.key?(template)
+    with_them do
+      described_class::TEMPLATE_TO_EVENT.keys.each do |template|
+        context "when given template #{template}" do
+          it_behaves_like 'tracking unique hll events', :usage_data_track_ci_templates_unique_projects do
+            subject(:request) { described_class.track_unique_project_event(project_id: project_id, template: template, config_source: config_source) }
 
-        described_class.track_unique_project_event(project_id: 1, template: template)
+            let(:target_id) { "p_ci_templates_#{described_class::TEMPLATE_TO_EVENT[template]}#{expected_suffix}" }
+            let(:expected_type) { instance_of(Integer) }
+          end
+        end
+      end
+
+      it 'does not track templates outside of TEMPLATE_TO_EVENT' do
+        expect(Gitlab::UsageDataCounters::HLLRedisCounter).not_to(
+          receive(:track_event)
+        )
+        Dir.glob(File.join('lib', 'gitlab', 'ci', 'templates', '**'), base: Rails.root) do |template|
+          next if described_class::TEMPLATE_TO_EVENT.key?(template)
+
+          described_class.track_unique_project_event(project_id: project_id, template: template, config_source: config_source)
+        end
       end
     end
   end
