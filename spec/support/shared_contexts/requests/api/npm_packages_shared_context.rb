b77a8ac800eda0bcf2b9cea9962558f7c1e8b4f4
David Fernandez <dfernandez@gitlab.com>
2021-02-05 15:33:40 +0100

    Refactor NPM requests specs for shared endpoints
    Use tabled based specs to better test different conditions.
    Make sure that we use the Bearer http authorization header and not the
    request parameter.
    Move package tags shared examples in a dedicated file for npm specs.

--- spec/support/shared_contexts/requests/api/npm_packages_shared_context.rb
+++ spec/support/shared_contexts/requests/api/npm_packages_shared_context.rb
@@ -4,6 +4,35 @@ RSpec.shared_context 'npm api setup' do
   include PackagesManagerApiSpecHelpers
   include HttpBasicAuthHelpers
 
+  let_it_be(:user, reload: true) { create(:user) }
+  let_it_be(:group) { create(:group) }
+  let_it_be(:project, reload: true) { create(:project, :public, namespace: group) }
+  let_it_be(:package, reload: true) { create(:npm_package, project: project, name: "@#{group.path}/scoped_package") }
+  let_it_be(:token) { create(:oauth_access_token, scopes: 'api', resource_owner: user) }
+  let_it_be(:personal_access_token) { create(:personal_access_token, user: user) }
+  let_it_be(:job, reload: true) { create(:ci_build, user: user, status: :running) }
+  let_it_be(:deploy_token) { create(:deploy_token, read_package_registry: true, write_package_registry: true) }
+  let_it_be(:project_deploy_token) { create(:project_deploy_token, deploy_token: deploy_token, project: project) }
+
+  let(:package_name) { package.name }
+end
+
+RSpec.shared_context 'set package name from package name type' do
+  let(:package_name) do
+    case package_name_type
+    when :scoped_naming_convention
+      "@#{group.path}/scoped-package"
+    when :non_existing
+      'non-existing-package'
+    end
+  end
+end
+# frozen_string_literal: true
+
+RSpec.shared_context 'npm api setup' do
+  include PackagesManagerApiSpecHelpers
+  include HttpBasicAuthHelpers
+
   let_it_be(:user) { create(:user) }
   let_it_be(:group) { create(:group) }
   let_it_be(:project, reload: true) { create(:project, :public, namespace: group) }
@@ -15,8 +44,19 @@ RSpec.shared_context 'npm api setup' do
   let_it_be(:project_deploy_token) { create(:project_deploy_token, deploy_token: deploy_token, project: project) }
 
   let(:package_name) { package.name }
+end
 
-  before do
-    project.add_developer(user)
+RSpec.shared_context 'set package name from package name type' do
+  let(:package_name) do
+    case package_name_type
+    when :scoped_naming_convention
+      "@#{group.path}/scoped-package"
+    when :scoped_no_naming_convention
+      '@any-scope/scoped-package'
+    when :unscoped
+      'unscoped-package'
+    when :non_existing
+      'non-existing-package'
+    end
   end
 end
