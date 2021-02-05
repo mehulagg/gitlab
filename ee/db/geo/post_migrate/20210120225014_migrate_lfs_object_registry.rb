8a6c15173376ba2ecccb095452e877db5c91ed2d
Ian Baum <ibaum@gitlab.com>
2021-02-05 16:30:20 -0500

    Remove adding limit to retry_count
    * Moving from integer to smallint is problematic, and possibly
      unnecessary

--- ee/db/geo/post_migrate/20210120225014_migrate_lfs_object_registry.rb
+++ ee/db/geo/post_migrate/20210120225014_migrate_lfs_object_registry.rb
@@ -11,7 +11,6 @@ class MigrateLfsObjectRegistry < ActiveRecord::Migration[6.0]
     add_not_null_constraint :lfs_object_registry, :lfs_object_id, validate: false
     add_not_null_constraint :lfs_object_registry, :created_at, validate: false
     change_column_default :lfs_object_registry, :retry_count, from: nil, to: 0
-    change_column :lfs_object_registry, :retry_count, :integer, limit: 2
     add_column :lfs_object_registry, :state, :integer, null: false, limit: 2, default: 0
     add_column :lfs_object_registry, :last_synced_at, :datetime_with_timezone
     add_column :lfs_object_registry, :last_sync_failure, :text
@@ -22,7 +21,6 @@ class MigrateLfsObjectRegistry < ActiveRecord::Migration[6.0]
     remove_not_null_constraint :lfs_object_registry, :lfs_object_id
     remove_not_null_constraint :lfs_object_registry, :created_at
     change_column_default :lfs_object_registry, :retry_count, from: 0, to: nil
-    change_column :lfs_object_registry, :retry_count, :integer, limit: nil
     remove_column :lfs_object_registry, :state
     remove_column :lfs_object_registry, :last_synced_at
     remove_column :lfs_object_registry, :last_sync_failure
