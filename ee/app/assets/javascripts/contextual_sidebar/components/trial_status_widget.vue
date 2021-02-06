ccf6c0d3d7ee5ecba6d7821adbe19c7db8e0753a
Dallas Reedy <dreedy@gitlab.com>
2021-02-05 17:17:34 -0800

    WIP: add popover on hover of trial status widget

--- ee/app/assets/javascripts/contextual_sidebar/components/trial_status_widget.vue
+++ ee/app/assets/javascripts/contextual_sidebar/components/trial_status_widget.vue
@@ -1,9 +1,11 @@
 <script>
-import { GlLink, GlProgressBar } from '@gitlab/ui';
+import { GlButton, GlLink, GlPopover, GlProgressBar } from '@gitlab/ui';
 
 export default {
   components: {
+    GlButton,
     GlLink,
+    GlPopover,
     GlProgressBar,
   },
   props: {
@@ -28,19 +30,60 @@ export default {
 </script>
 
 <template>
-  <gl-link :title="title" :href="href">
-    <div class="gl-display-flex gl-flex-direction-column gl-align-items-stretch gl-w-full">
-      <span class="gl-display-flex gl-align-items-center">
-        <span class="nav-icon-container svg-container">
-          <img :src="navIconImagePath" width="16" class="svg" />
+  <div>
+    <gl-link :title="title" :href="href" id="trial-status-sidebar-widget">
+      <div class="gl-display-flex gl-flex-direction-column gl-align-items-stretch gl-w-full">
+        <span class="gl-display-flex gl-align-items-center">
+          <span class="nav-icon-container svg-container">
+            <img :src="navIconImagePath" width="16" class="svg" />
+          </span>
+          <span class="nav-item-name gl-white-space-normal">
+            {{ title }}
+          </span>
         </span>
-        <span class="nav-item-name gl-white-space-normal">
-          {{ title }}
+        <span class="gl-display-flex gl-align-items-stretch gl-mt-3">
+          <gl-progress-bar :value="percentageComplete" class="gl-flex-grow-1" />
         </span>
-      </span>
-      <span class="gl-display-flex gl-align-items-stretch gl-mt-3">
-        <gl-progress-bar :value="percentageComplete" class="gl-flex-grow-1" />
-      </span>
-    </div>
-  </gl-link>
+      </div>
+    </gl-link>
+
+    <gl-popover
+      target="trial-status-sidebar-widget"
+      triggers="hover"
+      placement="rightbottom"
+      boundary="viewport"
+      :delay="{ hide: 400 }"
+    >
+      <template #title>Hey there 👋</template>
+
+      Your trial ends on <b>YYYY-MM-DD</b>. We hope you are enjoying GitLab Gold. To continue using
+      GitLab Gold after your trial ends, you will need to buy a subscription. You can also choose
+      Silver or Bronze if their features meet your needs.
+
+      <div class="gl-mt-5">
+        <gl-button
+          href="#in-app-purchase-flow"
+          category="primary"
+          variant="confirm"
+          size="small"
+          class="gl-mb-0"
+          block
+          title="Upgrade {group_name} to Gold"
+        >
+          <span class="gl-font-sm">Upgrade {group_name} to Gold</span>
+        </gl-button>
+        <gl-button
+          href="#billing-page"
+          category="secondary"
+          variant="confirm"
+          size="small"
+          class="gl-mb-0"
+          block
+          title="Compare all plans"
+        >
+          <span class="gl-font-sm">Compare all plans</span>
+        </gl-button>
+      </div>
+    </gl-popover>
+  </div>
 </template>
