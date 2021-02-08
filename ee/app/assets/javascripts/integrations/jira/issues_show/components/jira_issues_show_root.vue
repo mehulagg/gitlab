a8204c3f201d8c32e67505243c18aa203e17f2bd
Tom Quirk <tquirk@gitlab.com>
2021-02-08 14:17:49 +0930

    Add error handling to Jira Issues Show root

--- ee/app/assets/javascripts/integrations/jira/issues_show/components/jira_issues_show_root.vue
+++ ee/app/assets/javascripts/integrations/jira/issues_show/components/jira_issues_show_root.vue
@@ -5,6 +5,7 @@ import JiraIssueSidebar from 'ee/integrations/jira/issues_show/components/sideba
 import { issueStates, issueStateLabels } from 'ee/integrations/jira/issues_show/constants';
 import IssuableShow from '~/issuable_show/components/issuable_show_root.vue';
 import { convertObjectPropsToCamelCase } from '~/lib/utils/common_utils';
+import { s__ } from '~/locale';
 
 export default {
   name: 'JiraIssuesShow',
@@ -23,6 +24,7 @@ export default {
   data() {
     return {
       isLoading: true,
+      errorMessage: null,
       issue: {},
     };
   },
@@ -40,48 +42,70 @@ export default {
       return this.isIssueOpen ? 'issue-open-m' : 'mobile-issue-close';
     },
   },
-  async mounted() {
-    this.issue = convertObjectPropsToCamelCase(await fetchIssue(this.issuesShowPath), {
-      deep: true,
-    });
-    this.isLoading = false;
+  mounted() {
+    this.loadIssue();
+  },
+  methods: {
+    loadIssue() {
+      fetchIssue(this.issuesShowPath)
+        .then((issue) => {
+          this.issue = convertObjectPropsToCamelCase(issue, { deep: true });
+        })
+        .catch(() => {
+          this.errorMessage = s__(
+            'JiraService|Failed to load Jira issue. Please view issue in jira, or try again.',
+          );
+        })
+        .finally(() => {
+          this.isLoading = false;
+        });
+    },
+    clearErrorMessage() {
+      this.errorMessage = null;
+    },
   },
 };
 </script>
 
 <template>
   <div class="gl-mt-5">
-    <gl-alert
-      variant="info"
-      :dismissible="false"
-      :title="s__('JiraService|This issue is synchronized with Jira')"
-      class="gl-mb-2"
-    >
-      <gl-sprintf
-        :message="
-          s__(
-            `JiraService|Not all data may be displayed here. To view more details or make changes to this issue, go to %{linkStart}Jira%{linkEnd}.`,
-          )
-        "
-      >
-        <template #link="{ content }">
-          <gl-link :href="issue.webUrl" target="_blank">{{ content }}</gl-link>
-        </template>
-      </gl-sprintf>
+    <gl-alert v-if="errorMessage" variant="danger" class="gl-mb-2" @dismiss="clearErrorMessage">
+      {{ errorMessage }}
     </gl-alert>
 
-    <issuable-show
-      v-if="!isLoading"
-      :issuable="issue"
-      :enable-edit="false"
-      :status-badge-class="statusBadgeClass"
-      :status-icon="statusIcon"
-    >
-      <template #status-badge>{{ statusBadgeText }}</template>
+    <template v-if="!isLoading">
+      <gl-alert
+        variant="info"
+        :dismissible="false"
+        :title="s__('JiraService|This issue is synchronized with Jira')"
+        class="gl-mb-2"
+      >
+        <gl-sprintf
+          :message="
+            s__(
+              `JiraService|Not all data may be displayed here. To view more details or make changes to this issue, go to %{linkStart}Jira%{linkEnd}.`,
+            )
+          "
+        >
+          <template #link="{ content }">
+            <gl-link :href="issue.webUrl" target="_blank">{{ content }}</gl-link>
+          </template>
+        </gl-sprintf>
+      </gl-alert>
 
-      <template #right-sidebar-items="{ sidebarExpanded }">
-        <jira-issue-sidebar :sidebar-expanded="sidebarExpanded" :issue="issue" />
-      </template>
-    </issuable-show>
+      <issuable-show
+        v-if="!isLoading"
+        :issuable="issue"
+        :enable-edit="false"
+        :status-badge-class="statusBadgeClass"
+        :status-icon="statusIcon"
+      >
+        <template #status-badge>{{ statusBadgeText }}</template>
+
+        <template #right-sidebar-items="{ sidebarExpanded }">
+          <jira-issue-sidebar :sidebar-expanded="sidebarExpanded" :issue="issue" />
+        </template>
+      </issuable-show>
+    </template>
   </div>
 </template>
