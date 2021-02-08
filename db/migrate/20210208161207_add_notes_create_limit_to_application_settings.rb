45cb77963aaf3fdb88981b9c2253895b59845943
Eugenia Grieff <egrieff@gitlab.com>
2021-02-08 17:41:11 +0000

    fix

--- db/migrate/20210208161207_add_notes_create_limit_to_application_settings.rb
+++ db/migrate/20210208161207_add_notes_create_limit_to_application_settings.rb
@@ -1,8 +1,5 @@
 # frozen_string_literal: true
 
-# See https://docs.gitlab.com/ee/development/migration_style_guide.html
-# for more information on how to write migrations for GitLab.
-
 class AddNotesCreateLimitToApplicationSettings < ActiveRecord::Migration[6.0]
   DOWNTIME = false
 
