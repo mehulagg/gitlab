11e19b475260c8b8b59e85dc83ce46c8be6ec3b7
Jiaan Louw <jlouw@gitlab.com>
2021-02-08 20:26:22 +0200

    Export all actions as constants
    This change allows us to easily import
    all the action components and to iterate
    through them.

--- app/assets/javascripts/admin/users/constants.js
+++ app/assets/javascripts/admin/users/constants.js
@@ -1,5 +1,17 @@
+import Activate from './components/actions/activate.vue';
+import Block from './components/actions/block.vue';
+import Deactivate from './components/actions/deactivate.vue';
+import DeleteWithContributions from './components/actions/delete_with_contributions.vue';
+import Delete from './components/actions/delete.vue';
+import Unblock from './components/actions/unblock.vue';
+import Unlock from './components/actions/unlock.vue';
+
 export const USER_AVATAR_SIZE = 32;
 
 export const SHORT_DATE_FORMAT = 'd mmm, yyyy';
 
 export const LENGTH_OF_USER_NOTE_TOOLTIP = 100;
+
+export const ACTION_COMPONENTS = { Activate, Block, Deactivate, Unblock, Unlock };
+
+export const DELETE_ACTION_COMPONENTS = { DeleteWithContributions, Delete };
