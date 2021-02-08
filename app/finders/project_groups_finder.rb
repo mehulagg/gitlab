95a0053795dc42aacb2248ec5892873c36ef78c2
Vasilii Iakliushin <viakliushin@gitlab.com>
2021-02-08 16:39:18 +0100

    Add groups endpoint for Projects API
    Contributes to https://gitlab.com/gitlab-org/gitlab/-/issues/28902
    
    * Add `ProjectGroupFinder` to fetch ancestor and shared groups for the
    project
    * Add `projects/:id/groups` endpoint to Project API
    * Add documentation for the new endpoint

---
+++ app/finders/project_groups_finder.rb
@@ -0,0 +1,71 @@
+# frozen_string_literal: true
+
+# ProjectGroupsFinder
+#
+# Used to filter Groups by a set of params
+#
+# Arguments:
+#   project
+#   current_user - which user is requesting groups
+#   params:
+#     with_shared: boolean
+#     shared_min_access_level: integer
+#     skip_groups: array of integers
+#
+class ProjectGroupsFinder < UnionFinder
+  include CustomAttributesFilter
+
+  def initialize(project:, current_user: nil, params: {})
+    @project = project
+    @current_user = current_user
+    @params = params
+  end
+
+  def execute
+    return [] unless authorized?
+
+    items = all_groups.map do |item|
+      item = by_custom_attributes(item)
+      item = exclude_group_ids(item)
+      item
+    end
+
+    find_union(items, Group).with_route.order_id_desc
+  end
+
+  private
+
+  attr_reader :project, :current_user, :params
+
+  def authorized?
+    Ability.allowed?(current_user, :read_project, project)
+  end
+
+  # rubocop: disable CodeReuse/ActiveRecord
+  def all_groups
+    groups = []
+    groups << project.group&.self_and_ancestors if project.group
+
+    if params[:with_shared]
+      shared_groups = project.invited_groups
+
+      if params[:shared_min_access_level]
+        shared_groups = shared_groups.where(
+          'project_group_links.group_access >= ?', params[:shared_min_access_level]
+        )
+      end
+
+      groups << shared_groups
+    end
+
+    groups << Group.none if groups.empty?
+    groups
+  end
+  # rubocop: enable CodeReuse/ActiveRecord
+
+  def exclude_group_ids(groups)
+    return groups unless params[:skip_groups]
+
+    groups.id_not_in(params[:skip_groups])
+  end
+end
