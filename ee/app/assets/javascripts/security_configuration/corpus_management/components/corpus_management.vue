7975442022f886b76842a621335a085c74756465
Fernando <farias@gitlab.com>
2021-02-07 23:48:51 -0600

    Bootstrap Apollo client
    * Create directories for resolvers, queries, and fragments
    * Create apollo client instance and bind to vue instance

--- ee/app/assets/javascripts/security_configuration/corpus_management/components/corpus_management.vue
+++ ee/app/assets/javascripts/security_configuration/corpus_management/components/corpus_management.vue
@@ -10,5 +10,5 @@ export default {
 </script>
 
 <template>
-  <span></span>
+  <span>foo</span>
 </template>
--- ee/app/assets/javascripts/security_configuration/corpus_management/corpus_management_bundle.js
+++ ee/app/assets/javascripts/security_configuration/corpus_management/corpus_management_bundle.js
@@ -1,5 +1,11 @@
+import { defaultDataIdFromObject } from 'apollo-cache-inmemory';
 import Vue from 'vue';
+import VueApollo from 'vue-apollo';
+import createDefaultClient from '~/lib/graphql';
+//import resolvers from './graphql/resolvers';
 import CorpusManagement from './components/corpus_management.vue';
+
+Vue.use(VueApollo);
 
 export default () => {
   const el = document.querySelector('.js-corpus-management');
@@ -7,6 +13,14 @@ export default () => {
   if (!el) {
     return undefined;
   }
+
+  const defaultClient = createDefaultClient({}, {
+    cacheConfig: {
+      dataIdFromObject: (object) => {
+          return object.id || defaultDataIdFromObject(object)
+      },
+    },
+  });
 
   const {
     dataset: { projectFullPath },
@@ -16,8 +30,11 @@ export default () => {
     projectFullPath,
   };
 
+  debugger;
+
   return new Vue({
     el,
+    apolloProvider: new VueApollo({ defaultClient }),
     render(h) {
       return h(CorpusManagement, {
         props,
