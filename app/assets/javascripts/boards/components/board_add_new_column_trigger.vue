5bb65ba24797c001e1f3c82af09d06abc5436426
Simon Knox <simon@gitlab.com>
2021-02-09 15:15:22 +1100

    Reorder and align boards search bar buttons
    Group by button next to Show labels, so buttons are all together
    Align label text for toggles and dropdowns
    Vetically align all buttons
    Convert Group by and fullscreen buttons to gl-buttons
    Add flex + center

--- app/assets/javascripts/boards/components/board_add_new_column_trigger.vue
+++ app/assets/javascripts/boards/components/board_add_new_column_trigger.vue
@@ -13,7 +13,7 @@ export default {
 </script>
 
 <template>
-  <span class="gl-ml-4">
+  <span class="gl-ml-3 gl-display-flex gl-align-items-center">
     <gl-button variant="success" @click="setAddColumnFormVisibility(true)"
       >{{ __('Create list') }}
     </gl-button>
