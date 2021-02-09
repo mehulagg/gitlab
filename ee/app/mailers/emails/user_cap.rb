b2accd6ddb4661a166d7d25e5ca844281e3f9dc7
Corinna Wiesner <cwiesner@gitlab.com>
2021-02-09 11:40:15 +0100

    Modify email text and replace some doc links

--- ee/app/mailers/emails/user_cap.rb
+++ ee/app/mailers/emails/user_cap.rb
@@ -7,8 +7,9 @@ module Emails
       email = user.notification_email
 
       @url_to_user_cap = 'https://docs.gitlab.com/ee/user/admin_area/settings/sign_up_restrictions.html#user-cap'
-      @url_to_pending_users = 'https://docs.gitlab.com/ee/user/admin_area/approving_users.html'
-      @url_to_approve = 'https://docs.gitlab.com/ee/user/admin_area/approving_users.html#approving-a-user'
+      @url_to_pending_users = 'https://docs.gitlab.com/ee/user/admin_area/approving_users.html#view-user-sign-ups-pending-approval'
+      @url_to_manage_pending_users = 'https://docs.gitlab.com/ee/user/admin_area/approving_users.html#approve-or-reject-a-user-sign-up'
+      @url_to_adjust_user_cap = 'https://docs.gitlab.com/ee/user/admin_area/settings/sign_up_restrictions.html#set-the-user-cap-number'
       @url_to_docs = 'https://docs.gitlab.com/'
       @url_to_support = 'https://about.gitlab.com/support/'
 
