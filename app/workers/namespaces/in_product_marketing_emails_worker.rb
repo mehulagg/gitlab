924010196d7ad7ad5660fcc19c9960deb9eefc4e
Alex Buijs <abuijs@gitlab.com>
2021-02-09 14:06:38 +0100

    Enable on 50% of self managed instances with usage ping enabled

--- app/workers/namespaces/in_product_marketing_emails_worker.rb
+++ app/workers/namespaces/in_product_marketing_emails_worker.rb
@@ -9,9 +9,25 @@ module Namespaces
     urgency :low
 
     def perform
-      return unless Gitlab::Experimentation.active?(:in_product_marketing_emails)
+      return unless enabled_on_dotcom? || enabled_on_self_managed?
 
       Namespaces::InProductMarketingEmailsService.send_for_all_tracks_and_intervals
+    end
+
+    private
+
+    def enabled_on_dotcom?
+      Gitlab.com? && Gitlab::Experimentation.active?(:in_product_marketing_emails)
+    end
+
+    # Enable on 50% of self managed instances where usage ping is enabled
+    def enabled_on_self_managed?
+      return if Gitlab.com?
+      return unless Gitlab::CurrentSettings.usage_ping_enabled?
+      return if User.single_user&.requires_usage_stats_consent?
+
+      uuid = Gitlab::CurrentSettings.uuid
+      Zlib.crc32(uuid).even?
     end
   end
 end
