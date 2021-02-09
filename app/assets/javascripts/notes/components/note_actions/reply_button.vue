b3f47681d6d00a644c2b9829d7202cbe9400f1ee
Coung Ngo <cngo@gitlab.com>
2021-02-09 17:31:25 +0000

    Align note actions icons
    Aligns the emoji icon with the other icons, and darkens its color
    to match the others

--- app/assets/javascripts/notes/components/note_actions/reply_button.vue
+++ app/assets/javascripts/notes/components/note_actions/reply_button.vue
@@ -13,18 +13,16 @@ export default {
 </script>
 
 <template>
-  <div class="gl-ml-2">
-    <gl-button
-      ref="button"
-      v-gl-tooltip
-      data-track-event="click_button"
-      data-track-label="reply_comment_button"
-      category="tertiary"
-      size="small"
-      icon="comment"
-      :title="__('Reply to comment')"
-      :aria-label="__('Reply to comment')"
-      @click="$emit('startReplying')"
-    />
-  </div>
+  <gl-button
+    ref="button"
+    v-gl-tooltip
+    data-track-event="click_button"
+    data-track-label="reply_comment_button"
+    category="tertiary"
+    size="small"
+    icon="comment"
+    :title="__('Reply to comment')"
+    :aria-label="__('Reply to comment')"
+    @click="$emit('startReplying')"
+  />
 </template>
