e81dfbc8ec1e442ca574809e5235a3006113fd35
sstern <sstern@gitlab.com>
2021-02-09 11:29:04 -0800

    Add glfilteredsearch to group boards
    Adding GLFilteredSearch to group issue baords
    as first iteration to replace old filtered search
    implementation

---
+++ app/assets/javascripts/boards/components/filtered_search.vue
@@ -0,0 +1,50 @@
+<script>
+import { mapActions, mapGetters } from 'vuex';
+import { historyPushState } from '~/lib/utils/common_utils';
+import { setUrlParams } from '~/lib/utils/url_utility';
+import FilteredSearch from '~/vue_shared/components/filtered_search_bar/filtered_search_bar_root.vue';
+
+export default {
+  components: { FilteredSearch },
+  props: {
+    search: {
+      type: String,
+      required: false,
+      default: '',
+    },
+  },
+  mounted() {
+    const searchWrapper = document.querySelector('.filtered-search-wrapper');
+
+    searchWrapper.classList.add('gl-display-none!');
+  },
+  methods: {
+    ...mapActions(['performSearch']),
+    handleSearch(filters) {
+      let itemValue = '';
+      const [item] = filters;
+
+      if (filters.length === 0) {
+        itemValue = '';
+      } else {
+        itemValue = item?.value?.data;
+      }
+
+      historyPushState(setUrlParams({ search: itemValue }, window.location.href));
+
+      this.performSearch();
+    },
+  },
+};
+</script>
+
+<template>
+  <filtered-search
+    class="gl-w-full"
+    namespace=""
+    :tokens="[]"
+    :search-input-placeholder="'Search'"
+    :initial-filter-value="[{ type: 'filtered-search-term', value: { data: search } }]"
+    @onFilter="handleSearch"
+  />
+</template>
