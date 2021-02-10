99c99db5fde7167e0d7dae39013846ecb49dc644
Aleksei Lipniagov <alipniagov@gitlab.com>
2021-02-10 15:54:59 +0300

    Make puma single check more sturdy

--- config/initializers/validate_puma.rb
+++ config/initializers/validate_puma.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-if Gitlab::Runtime.max_puma_workers == 0
+if Gitlab::Runtime.puma? && Gitlab::Runtime.max_puma_workers == 0
   raise 'Puma is only supported in Clustered mode (workers > 0)' if Gitlab.com?
 
   warn 'WARNING: Puma is running in Single mode (workers = 0). Some features may not work. Please refer to https://gitlab.com/groups/gitlab-org/-/epics/5303 for info.'
