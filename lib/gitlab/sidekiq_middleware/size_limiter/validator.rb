2c9acdbee4465c22537f9eeeccd18f906f3cb04a
Quang-Minh Nguyen <qmnguyen@gitlab.com>
2021-02-10 14:46:19 +0700

    Move the ExceedLimitError out of the validator
    Issue https://gitlab.com/gitlab-com/gl-infra/scalability/-/issues/825

--- lib/gitlab/sidekiq_middleware/size_limiter/validator.rb
+++ lib/gitlab/sidekiq_middleware/size_limiter/validator.rb
@@ -3,28 +3,6 @@
 module Gitlab
   module SidekiqMiddleware
     module SizeLimiter
-      # A custom exception for size limiter. It contains worker class and its
-      # size to easier track later
-      class ExceedLimitError < StandardError
-        attr_reader :worker_class, :size, :size_limit
-
-        def initialize(worker_class, size, size_limit)
-          @worker_class = worker_class
-          @size = size
-          @size_limit = size_limit
-
-          super "#{@worker_class} job exceeds payload size limit"
-        end
-
-        def sentry_extra_data
-          {
-            worker_class: @worker_class.to_s,
-            size: @size.to_f,
-            size_limit: @size_limit.to_f
-          }
-        end
-      end
-
       # Validate a Sidekiq job payload limit based on current configuration.
       # This validator pulls the configuration from the environment variables:
       #
