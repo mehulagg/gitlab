3225d74aaef2ad02fe2f50f0304081a0526e1cf2
Miguel Rincon <mrincon@gitlab.com>
2021-02-11 18:29:06 +0100

    Refactor pipeline specs
    This change updates the pipeline table specs to use async/await.
    
    It also reorganizes specs to use axios mocks more efficiently and avoid
    repetition, while removing a few redundant specs.

--- spec/frontend/commit/commit_pipeline_status_component_spec.js
+++ spec/frontend/commit/commit_pipeline_status_component_spec.js
@@ -142,7 +142,7 @@ describe('Commit pipeline status component', () => {
       });
 
       it('renders CI icon', () => {
-        expect(findCiIcon().attributes('title')).toEqual('Pipeline: pending');
+        expect(findCiIcon().attributes('title')).toEqual('Pipeline: passed');
         expect(findCiIcon().props('status')).toEqual(mockCiStatus);
       });
     });
