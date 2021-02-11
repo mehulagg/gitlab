c8b72249b6e8f875ed4c713f0668207377604043
nmilojevic1 <nmilojevic@gitlab.com>
2021-02-11 14:12:38 +0100

    Move API::API files to the WebEngine
    - Copy all api files to the web engine
    - Copy all api specs to the web engine

--- engines/web_engine/lib/web_engine/engine.rb
+++ engines/web_engine/lib/web_engine/engine.rb
@@ -2,13 +2,17 @@
 
 module WebEngine
   class Engine < ::Rails::Engine
-    initializer :before_set_eager_paths, before: :set_load_path do
+    initializer :before_set_eager_paths, before: :set_load_path do |app|
       config.eager_load_paths.push(*%W[#{config.root}/lib
                                        #{config.root}/app/graphql/resolvers/concerns
                                        #{config.root}/app/graphql/mutations/concerns
                                        #{config.root}/app/graphql/types/concerns])
 
+      app.config.paths['draw_routes'] << 'engines/web_engine/config/routes'
+
       if Gitlab.ee?
+        app.config.paths['draw_routes'] << 'engines/web_engine/ee/config/routes'
+
         ee_paths = config.eager_load_paths.each_with_object([]) do |path, memo|
           ee_path = config.root
                       .join('ee', Pathname.new(path).relative_path_from(config.root))
@@ -16,6 +20,12 @@ module WebEngine
         end
         # Eager load should load CE first
         config.eager_load_paths.push(*ee_paths)
+      end
+    end
+
+    initializer :engine_load_config_initializers, after: :load_config_initializers do
+      Dir[WebEngine::Engine.root.join('config/initializers_after_application/*.rb')].sort.each do |initializer|
+        load_config_initializer(initializer)
       end
     end
   end
