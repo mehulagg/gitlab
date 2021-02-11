4b7e9360976f6dcc161791e70ae596be7660d764
Illya Klymov <xanf@xanf.me>
2021-02-11 15:48:55 +0200

    Refactor import_projects app to use gitlab-ui
    - Use gitlab-ui CSS styles and components

--- spec/frontend/import_entities/import_projects/components/import_projects_table_spec.js
+++ spec/frontend/import_entities/import_projects/components/import_projects_table_spec.js
@@ -1,7 +1,7 @@
 import { nextTick } from 'vue';
 import Vuex from 'vuex';
 import { createLocalVue, shallowMount } from '@vue/test-utils';
-import { GlLoadingIcon, GlButton, GlIntersectionObserver } from '@gitlab/ui';
+import { GlLoadingIcon, GlButton, GlIntersectionObserver, GlFormInput } from '@gitlab/ui';
 import state from '~/import_entities/import_projects/store/state';
 import * as getters from '~/import_entities/import_projects/store/getters';
 import { STATUSES } from '~/import_entities/constants';
@@ -12,7 +12,9 @@ describe('ImportProjectsTable', () => {
   let wrapper;
 
   const findFilterField = () =>
-    wrapper.find('input[data-qa-selector="githubish_import_filter_field"]');
+    wrapper
+      .findAllComponents(GlFormInput)
+      .wrappers.find((w) => w.attributes('placeholder') === 'Filter your repositories by name');
 
   const providerTitle = 'THE PROVIDER';
   const providerRepo = {
@@ -205,7 +207,7 @@ describe('ImportProjectsTable', () => {
   it('does not render filtering input field when filterable is false', () => {
     createComponent({ filterable: false });
 
-    expect(findFilterField().exists()).toBe(false);
+    expect(findFilterField()).toBe(undefined);
   });
 
   describe('when paginatable is set to true', () => {
