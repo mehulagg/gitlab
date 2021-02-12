704061d757f433e4ca6ccfd83ef8f16190077643
Samantha Ming <sming@gitlab.com>
2021-02-12 06:53:18 -0800

    Add fork project form
    Issue: https://gitlab.com/gitlab-org/gitlab/-/issues/15013

--- app/assets/javascripts/pages/projects/forks/new/index.js
+++ app/assets/javascripts/pages/projects/forks/new/index.js
@@ -1,8 +1,42 @@
 import Vue from 'vue';
+import ForkForm from './components/fork_form.vue';
 import ForkGroupsList from './components/fork_groups_list.vue';
 
 document.addEventListener('DOMContentLoaded', () => {
   const mountElement = document.getElementById('fork-groups-mount-element');
+
+  if (gon.features.forkProjectForm) {
+    const {
+      endpoint,
+      newGroupPath,
+      projectFullPath,
+      visibilityHelpPath,
+      projectId,
+      projectName,
+      projectPath,
+      projectDescription,
+      projectVisibility,
+    } = mountElement.dataset;
+
+    return new Vue({
+      el: mountElement,
+      render(h) {
+        return h(ForkForm, {
+          props: {
+            endpoint,
+            newGroupPath,
+            projectFullPath,
+            visibilityHelpPath,
+            projectId,
+            projectName,
+            projectPath,
+            projectDescription,
+            projectVisibility,
+          },
+        });
+      },
+    });
+  }
 
   const { endpoint } = mountElement.dataset;
 
