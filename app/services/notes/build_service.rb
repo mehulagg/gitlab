2f1d259c13fb65e6a9cff1473acf9dacde448282
Lee Tickett <lee@tickett.net>
2021-02-13 03:11:37 +0000

    Default reply confidentiality

--- app/services/notes/build_service.rb
+++ app/services/notes/build_service.rb
@@ -5,6 +5,7 @@ module Notes
     def execute
       should_resolve = false
       in_reply_to_discussion_id = params.delete(:in_reply_to_discussion_id)
+      parent_confidential = nil
 
       if in_reply_to_discussion_id.present?
         discussion = find_discussion(in_reply_to_discussion_id)
@@ -17,6 +18,7 @@ module Notes
 
         discussion = discussion.convert_to_discussion! if discussion.can_convert_to_discussion?
 
+        parent_confidential = discussion.notes.first.confidential
         params.merge!(discussion.reply_attributes)
         should_resolve = discussion.resolved?
       end
@@ -24,6 +26,7 @@ module Notes
       note = Note.new(params)
       note.project = project
       note.author = current_user
+      note.confidential = should_set_confidential(parent_confidential, note)
 
       if should_resolve
         note.resolve_without_save(current_user)
@@ -38,6 +41,12 @@ module Notes
       else
         Note.find_discussion(discussion_id)
       end
+    end
+
+    def should_set_confidential(parent_confidential, note)
+      return parent_confidential unless parent_confidential.nil?
+
+      params.delete(:confidential) if can?(current_user, :mark_note_as_confidential, note)
     end
   end
 end
