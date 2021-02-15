f826d1e7a2365c2da2bcc455fd2cf7097e675245
Andreas Brandl <abrandl@gitlab.com>
2021-02-15 18:51:18 +0100

    Move Observation into its own file

--- lib/gitlab/database/migrations/instrumentation.rb
+++ lib/gitlab/database/migrations/instrumentation.rb
@@ -3,13 +3,6 @@
 module Gitlab
   module Database
     module Migrations
-      Observation = Struct.new(
-        :migration,
-        :walltime,
-        :success,
-        :total_database_size_change
-      )
-
       class Instrumentation
         attr_reader :observations
 
