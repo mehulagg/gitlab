bacc0e9f17d2d60060294300eff422d1ea2c1f74
Mehmet Emin INAC <minac@gitlab.com>
2021-02-17 20:06:50 +0100

    Mark the test as pending

--- ee/spec/services/security/merge_reports_service_spec.rb
+++ ee/spec/services/security/merge_reports_service_spec.rb
@@ -273,7 +273,7 @@ RSpec.describe Security::MergeReportsService, '#execute' do
 
       subject(:merged_report) { described_class.new(pre_merged_report, retirejs_report).execute }
 
-      it 'keeps the finding from `retirejs` as it has higher priority' do
+      it 'keeps the finding from `retirejs` as it has higher priority', pending: 'https://gitlab.com/gitlab-org/gitlab/-/issues/296520' do
         expect(merged_report.findings).to include(finding_id_5)
       end
     end
