a4ad1aa5f1d359aa8cea59cc6bb07688dc554ce7
George Koltsov <gkoltsov@gitlab.com>
2021-02-17 16:36:20 +0000

    Migrate Epic Events when using Group Migration
    - Add Epic events to the Group Migration (Bulk Import)

--- lib/bulk_imports/common/transformers/user_reference_transformer.rb
+++ lib/bulk_imports/common/transformers/user_reference_transformer.rb
@@ -3,13 +3,19 @@
 module BulkImports
   module Common
     module Transformers
-      class AwardEmojiTransformer
+      class UserReferenceTransformer
+        DEFAULT_REFERENCE = 'user_id'
+
+        def initialize(options = {})
+          @reference = options[:reference] || DEFAULT_REFERENCE
+        end
+
         def transform(context, data)
           user = find_user(context, data&.dig('user', 'public_email')) || context.current_user
 
           data
             .except('user')
-            .merge('user_id' => user.id)
+            .merge(@reference => user.id)
         end
 
         private
--- spec/lib/bulk_imports/common/transformers/user_reference_transformer_spec.rb
+++ spec/lib/bulk_imports/common/transformers/user_reference_transformer_spec.rb
@@ -2,7 +2,7 @@
 
 require 'spec_helper'
 
-RSpec.describe BulkImports::Common::Transformers::AwardEmojiTransformer do
+RSpec.describe BulkImports::Common::Transformers::UserReferenceTransformer do
   describe '#transform' do
     let_it_be(:user) { create(:user) }
     let_it_be(:group) { create(:group) }
