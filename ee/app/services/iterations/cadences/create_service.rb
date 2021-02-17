b6df4c97bc689de66fe44462dc4c41a307bbd26d
Alexandru Croitor <acroitor@gitlab.com>
2021-02-17 14:03:30 +0200

    Add create iteration cadence service and GraphQL mutation
    Implement create iteration cadence service and GraphQL mutation
    
    re https://gitlab.com/gitlab-org/gitlab/-/issues/293864

---
+++ ee/app/services/iterations/cadences/create_service.rb
@@ -0,0 +1,35 @@
+# frozen_string_literal: true
+
+module Iterations
+  module Cadences
+    class CreateService
+      include Gitlab::Allowable
+
+      attr_accessor :group, :current_user, :params
+
+      def initialize(group, user, params = {})
+        @group, @current_user, @params = group, user, params.dup
+      end
+
+      def execute
+        return ::ServiceResponse.error(message: _('Operation not allowed'), http_status: 403) unless can_create_iterations_cadence?
+
+        iterations_cadence = group.iterations_cadences.new(params)
+
+        if iterations_cadence.save
+          ::ServiceResponse.success(message: _('New iterations cadence created'), payload: { iterations_cadence: iterations_cadence })
+        else
+          ::ServiceResponse.error(message: _('Error creating new iterations cadence'), payload: { errors: iterations_cadence.errors })
+        end
+      end
+
+      private
+
+      def can_create_iterations_cadence?
+        group.iterations_cadences_enabled? &&
+          group.feature_available?(:iterations) &&
+          can?(current_user, :create_iterations_cadence, group)
+      end
+    end
+  end
+end
