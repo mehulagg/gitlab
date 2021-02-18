f3e5b40eca3cb53bffd3057f4f4317196137facf
Michael Kozono <mkozono@gmail.com>
2021-02-18 03:12:30 +0000

    Apply 1 suggestion(s) to 1 file(s)

--- ee/spec/mailers/emails/user_cap_spec.rb
+++ ee/spec/mailers/emails/user_cap_spec.rb
@@ -10,14 +10,6 @@ RSpec.describe Emails::UserCap do
   describe "#user_cap_reached" do
     subject { Notify.user_cap_reached(user.id) }
 
-    let_it_be(:user_cap) { 100 }
-    let_it_be(:url_to_user_cap_docs) do
-      'https://docs.gitlab.com/ee/user/admin_area/settings/sign_up_restrictions.html#user-cap'
-    end
-
-    before do
-      allow(::Gitlab::CurrentSettings).to receive(:new_user_signups_cap?).and_return(user_cap)
-    end
 
     it { is_expected.to have_subject('Important information about usage on your GitLab instance') }
     it { is_expected.to be_delivered_to([user.notification_email]) }
