43fdeafe073569bdd6a775e4f7875fe492263b35
Justin Ho <hduong@gitlab.com>
2021-02-18 16:34:44 +0700

    Add issue reference to Jira issue details page
    - Use issue.references.relative as the issue reference
    - Add tooltip boundary as a prop to clipboard_button

--- ee/app/assets/javascripts/integrations/jira/issues_show/components/sidebar.vue
+++ ee/app/assets/javascripts/integrations/jira/issues_show/components/sidebar.vue
@@ -1,9 +1,11 @@
 <script>
+import IssueReference from 'ee/integrations/jira/issues_show/components/issue_reference.vue';
 import LabelsSelect from '~/vue_shared/components/sidebar/labels_select_vue/labels_select_root.vue';
 
 export default {
   components: {
     LabelsSelect,
+    IssueReference,
   },
   props: {
     sidebarExpanded: {
@@ -14,15 +16,22 @@ export default {
       type: Array,
       required: true,
     },
+    reference: {
+      type: String,
+      required: true,
+    },
   },
 };
 </script>
 
 <template>
-  <labels-select
-    :selected-labels="selectedLabels"
-    variant="sidebar"
-    class="block labels js-labels-block"
-    >{{ __('None') }}</labels-select
-  >
+  <div>
+    <labels-select
+      :selected-labels="selectedLabels"
+      variant="sidebar"
+      class="block labels js-labels-block"
+      >{{ __('None') }}</labels-select
+    >
+    <issue-reference :reference="reference" />
+  </div>
 </template>
