148d1c5ab5cad6d372050032d5e980859934f594
Dmytro Zaporozhets <dzaporozhets@gitlab.com>
2021-02-18 13:21:56 +0200

    Remove outdated ci db columns
    Signed-off-by: Dmytro Zaporozhets <dzaporozhets@gitlab.com>

---
+++ db/post_migrate/20210218105431_remove_deprecated_ci_builds_columns.rb
@@ -0,0 +1,48 @@
+# frozen_string_literal: true
+
+# See https://docs.gitlab.com/ee/development/migration_style_guide.html
+# for more information on how to write migrations for GitLab.
+
+class RemoveDeprecatedCiBuildsColumns < ActiveRecord::Migration[6.0]
+  # Uncomment the following include if you require helper functions:
+  # include Gitlab::Database::MigrationHelpers
+
+  # Set this constant to true if this migration requires downtime.
+  DOWNTIME = false
+
+  # When a migration requires downtime you **must** uncomment the following
+  # constant and define a short and easy to understand explanation as to why the
+  # migration requires downtime.
+  # DOWNTIME_REASON = ''
+
+  # When using the methods "add_concurrent_index", "remove_concurrent_index" or
+  # "add_column_with_default" you must disable the use of transactions
+  # as these methods can not run in an existing transaction.
+  # When using "add_concurrent_index" or "remove_concurrent_index" methods make sure
+  # that either of them is the _only_ method called in the migration,
+  # any other changes should go in a separate migration.
+  # This ensures that upon failure _only_ the index creation or removing fails
+  # and can be retried or reverted easily.
+  #
+  # To disable transactions uncomment the following line and remove these
+  # comments:
+  # disable_ddl_transaction!
+
+  def up
+    remove_column :ci_builds, :artifacts_file
+    remove_column :ci_builds, :artifacts_file_store
+    remove_column :ci_builds, :artifacts_metadata
+    remove_column :ci_builds, :artifacts_metadata_store
+    remove_column :ci_builds, :artifacts_size
+    remove_column :ci_builds, :commands
+  end
+
+  def down
+    add_column :ci_builds, :artifacts_file, :text
+    add_column :ci_builds, :artifacts_file_store, :integer
+    add_column :ci_builds, :artifacts_metadata, :text
+    add_column :ci_builds, :artifacts_metadata_store, :integer
+    add_column :ci_builds, :artifacts_size, :bigint
+    add_column :ci_builds, :commands, :text
+  end
+end
