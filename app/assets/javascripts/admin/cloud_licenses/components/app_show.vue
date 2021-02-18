167b1ec5be3e77fb909ba25ff71727c5c07a48fa
jakeburden <jburden@gitlab.com>
2021-02-18 00:59:36 -0500

    Update UI form layout

--- app/assets/javascripts/admin/cloud_licenses/components/app_show.vue
+++ app/assets/javascripts/admin/cloud_licenses/components/app_show.vue
@@ -1,11 +1,10 @@
 <script>
-import { GlButton, GlCard, GlForm, GlFormGroup, GlFormInput } from '@gitlab/ui';
+import { GlButton, GlForm, GlFormGroup, GlFormInput } from '@gitlab/ui';
 
 export default {
   name: 'CloudLicenseShowApp',
   components: {
     GlButton,
-    GlCard,
     GlForm,
     GlFormGroup,
     GlFormInput,
@@ -14,28 +13,30 @@ export default {
 </script>
 
 <template>
-  <div class="gl-mt-3">
-    <gl-card>
-      <template #header>
-        <h3 class="gl-my-0 gl-font-weight-bold">
-          {{ __('This instance is currently using the Core plan.') }}
-        </h3>
-      </template>
-      <gl-form>
-        <gl-form-group
-          id="activation-code-group"
-          label="Paste your activation code below"
-          label-for="activation-code-input"
-        >
-          <gl-form-input
-            id="activation-code-input"
-            required
-            :placeholder="__('Paste your activation code')"
-          />
-        </gl-form-group>
+  <div
+    class="gl-w-half gl-mx-auto gl-display-flex gl-justify-content-center gl-text-center gl-flex-direction-column"
+  >
+    <h3 class="gl-mt-6 gl-font-weight-bold">
+      {{ __('This instance is currently using the Core plan.') }}
+    </h3>
 
+    <gl-form class="gl-mt-7">
+      <gl-form-group
+        id="activation-code-group"
+        label="Paste your activation code below"
+        label-for="activation-code-input"
+      >
+        <gl-form-input
+          id="activation-code-input"
+          required
+          :placeholder="__('Paste your activation code')"
+        />
+      </gl-form-group>
+
+      <div class="gl-display-flex gl-justify-content-end">
+        <gl-button type="reset" variant="secondary" class="gl-mr-3">{{ __('Cancel') }}</gl-button>
         <gl-button
-          class="mr-auto js-no-auto-disable"
+          class="gl-mr-3"
           category="primary"
           variant="success"
           type="submit"
@@ -43,7 +44,7 @@ export default {
         >
           {{ __('Activate') }}
         </gl-button>
-      </gl-form>
-    </gl-card>
+      </div>
+    </gl-form>
   </div>
 </template>
