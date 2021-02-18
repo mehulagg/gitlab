0863613e57c5aee8d65be723c1f61e8b16e07f10
Jannik Lehmann <jlehmann@gitlab.com>
2021-02-18 10:38:24 +0000

    Remove obsolete DOMContentLoadedEventListener

--- app/assets/javascripts/pages/projects/feature_flags_user_lists/show/index.js
+++ app/assets/javascripts/pages/projects/feature_flags_user_lists/show/index.js
@@ -1,18 +1,18 @@
+/* eslint-disable no-new */
+
 import Vue from 'vue';
 import Vuex from 'vuex';
 import UserList from '~/user_lists/components/user_list.vue';
 import createStore from '~/user_lists/store/show';
 
 Vue.use(Vuex);
+const el = document.getElementById('js-edit-user-list');
 
-document.addEventListener('DOMContentLoaded', () => {
-  const el = document.getElementById('js-edit-user-list');
-  return new Vue({
-    el,
-    store: createStore(el.dataset),
-    render(h) {
-      const { emptyStatePath } = el.dataset;
-      return h(UserList, { props: { emptyStatePath } });
-    },
-  });
+new Vue({
+  el,
+  store: createStore(el.dataset),
+  render(h) {
+    const { emptyStatePath } = el.dataset;
+    return h(UserList, { props: { emptyStatePath } });
+  },
 });
