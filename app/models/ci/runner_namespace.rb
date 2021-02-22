3caf9f834ef22dae3cc8e2549ca7bad2f6b75d0a
Heinrich Lee Yu <heinrich@gitlab.com>
2021-02-22 10:20:35 +0800

    Fix N+1 when validating groups
    Removes the validates_associated call which calls `#valid?` on each
    runner of the group which triggers an N+1 query

--- app/models/ci/runner_namespace.rb
+++ app/models/ci/runner_namespace.rb
@@ -4,7 +4,7 @@ module Ci
   class RunnerNamespace < ApplicationRecord
     extend Gitlab::Ci::Model
 
-    belongs_to :runner, inverse_of: :runner_namespaces, validate: true
+    belongs_to :runner, inverse_of: :runner_namespaces
     belongs_to :namespace, inverse_of: :runner_namespaces, class_name: '::Namespace'
     belongs_to :group, class_name: '::Group', foreign_key: :namespace_id
 
