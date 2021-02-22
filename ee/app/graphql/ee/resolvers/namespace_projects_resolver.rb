ae60876fbcc4825c95aac6c0d58f3f5894382535
Maxime Orefice <morefice@gitlab.com>
2021-02-22 17:13:19 -0500

    Fix coverage summary data for all projects
    This commits introduces a new argument to our projects graphql api
    which allows us to filter projects by coverage data.

--- ee/app/graphql/ee/resolvers/namespace_projects_resolver.rb
+++ ee/app/graphql/ee/resolvers/namespace_projects_resolver.rb
@@ -6,16 +6,22 @@ module EE
       extend ActiveSupport::Concern
 
       prepended do
+        argument :with_coverage, GraphQL::BOOLEAN_TYPE,
+             required: false,
+             default_value: false,
+             description: 'Returns only the projects which have coverages.'
+
         argument :has_vulnerabilities, GraphQL::BOOLEAN_TYPE,
                  required: false,
                  default_value: false,
                  description: 'Returns only the projects which have vulnerabilities.'
       end
 
-      def resolve(include_subgroups:, search:, sort:, has_vulnerabilities: false)
+      def resolve(include_subgroups:, search:, sort:, has_vulnerabilities: false, with_coverage: false)
         projects = super(include_subgroups: include_subgroups, search: search, sort: sort)
         projects = projects.has_vulnerabilities if has_vulnerabilities
         projects = projects.order_by_total_repository_size_excess_desc(namespace.actual_size_limit) if sort == :storage
+        projects = projects.with_code_coverages if with_coverage
         projects
       end
     end
