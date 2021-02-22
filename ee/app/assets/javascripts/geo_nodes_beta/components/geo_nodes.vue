872f42ec9b7086c77006500ea1bda73376f0ea25
Zack Cuddy <zcuddy@gitlab.com>
2021-02-22 14:04:07 -0600

    Geo Node Status 2.0 - UI Init
    This change hooks up the Vuex store
    to the UI and adds the top level elements.

---
+++ ee/app/assets/javascripts/geo_nodes_beta/components/geo_nodes.vue
@@ -0,0 +1,24 @@
+<script>
+import { __ } from '~/locale';
+
+export default {
+  name: 'GeoNodes',
+  props: {
+    node: {
+      type: Object,
+      required: true,
+    },
+  },
+  computed: {
+    siteTitle() {
+      return this.node.primary ? __('Primary site') : __('Secondary site');
+    },
+  },
+};
+</script>
+
+<template>
+  <div>
+    <h4 class="gl-font-lg gl-my-5">{{ siteTitle }}</h4>
+  </div>
+</template>
