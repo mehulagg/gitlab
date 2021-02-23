ae2ff6a9a37a9f96030c69ae5cd1598aae0424c1
Zack Cuddy <zcuddy@gitlab.com>
2021-02-23 16:03:55 -0600

    Geo Node Status 2.0 - Loading and Empty States
    This change adds an empty state and
    loading state for the Geo Nodes Beta.
    
    This change also hides the Add site
    button when no nodes exist per a
    previoius Geo requirment.

---
+++ ee/app/assets/javascripts/geo_nodes_beta/components/geo_nodes_empty_state.vue
@@ -0,0 +1,34 @@
+<script>
+import { GlEmptyState, GlLink } from '@gitlab/ui';
+import { GEO_FEATURE_URL } from '../constants';
+
+export default {
+  name: 'GeoNodesEmptyState',
+  components: {
+    GlEmptyState,
+    GlLink,
+  },
+  props: {
+    svgPath: {
+      type: String,
+      required: true,
+    },
+  },
+  GEO_FEATURE_URL,
+};
+</script>
+
+<template>
+  <gl-empty-state :title="__('Discover GitLab Geo')" :svg-path="svgPath">
+    <template #description>
+      <span>{{
+        __(
+          'Make everyone on your team more productive regardless of their location. GitLab Geo creates read-only mirrors of your GitLab instance so you can reduce the time it takes to clone and fetch large repos.',
+        )
+      }}</span>
+      <gl-link class="gl-ml-2" :href="$options.GEO_FEATURE_URL" target="_blank">{{
+        __('Learn more')
+      }}</gl-link>
+    </template>
+  </gl-empty-state>
+</template>
