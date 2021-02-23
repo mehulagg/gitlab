f866b39bfddebec85edba49eafaf61798769aad3
Patrick Bajao <ebajao@gitlab.com>
2021-02-23 16:46:40 +0800

    Implement new preparing internal merge_status
    When we check if MR is mergeable we also check the CI pipeline.
    Since we are creating the pipeline asynchronously via
    `MergeRequests::AfterCreateService`, there's a possible race
    condition wherein the pipeline isn't created yet but we already
    checked if MR is mergeable. That can result to inaccurate outcome.
    
    To ensure that whenever we check if MR is mergeable, we introduced
    a new internal state called `preparing`. When the MR is in this
    state, it means it's still being prepared (e.g. creating a pipeline
    ) and it's not mergeable.
    
    Once the preparation is finished, we mark the MR as unchecked so
    we can check its mergeability when the user views the MR page.

--- spec/models/merge_request_spec.rb
+++ spec/models/merge_request_spec.rb
@@ -2881,6 +2881,11 @@ RSpec.describe MergeRequest, factory_default: :keep do
   describe '#mergeable?' do
     subject { build_stubbed(:merge_request) }
 
+    it 'returns false if still preparing' do
+      expect(subject).to receive(:preparing?) { true }
+      expect(subject.mergeable?).to be_falsey
+    end
+
     it 'returns false if #mergeable_state? is false' do
       expect(subject).to receive(:mergeable_state?) { false }
 
@@ -3071,6 +3076,7 @@ RSpec.describe MergeRequest, factory_default: :keep do
     subject { build(:merge_request, merge_status: status) }
 
     where(:status, :public_status) do
+      'preparing' | 'checking'
       'cannot_be_merged_rechecking' | 'checking'
       'checking'                    | 'checking'
       'cannot_be_merged'            | 'cannot_be_merged'
