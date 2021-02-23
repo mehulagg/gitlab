cc488ba06ede0f019b76264f7107cdb37b14795f
Allison Browne <abrowne@gitlab.com>
2021-02-23 17:42:58 -0500

    Add failing tests

--- spec/factories/ci/build_trace_chunks.rb
+++ spec/factories/ci/build_trace_chunks.rb
@@ -3,8 +3,9 @@
 FactoryBot.define do
   factory :ci_build_trace_chunk, class: 'Ci::BuildTraceChunk' do
     build factory: :ci_build
-    chunk_index { 0 }
+    chunk_index { generate(:iid) }
     data_store { :redis }
+    
 
     trait :redis_with_data do
       data_store { :redis }
