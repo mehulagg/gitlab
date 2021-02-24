cd17dd1aedb095b93e93cc1c2872e5274db48f5b
Tan Le <tle@gitlab.com>
2021-02-24 11:43:16 +1100

    Reduce database calls in approval rules specs
    This commit converts `let` to `let_it_be` to reduce the number of
    database calls when setting up test data. All Rubocop violations on
    touched files are also fixed.

--- ee/spec/features/merge_request/user_edits_approval_rules_mr_spec.rb
+++ ee/spec/features/merge_request/user_edits_approval_rules_mr_spec.rb
@@ -5,9 +5,9 @@ require 'spec_helper'
 RSpec.describe 'Merge request > User edits MR with approval rules', :js do
   include Select2Helper
 
-  include_context 'project with approval rules'
+  include_context 'with project with approval rules'
 
-  let(:merge_request) { create(:merge_request, source_project: project) }
+  let_it_be(:merge_request) { create(:merge_request, source_project: project) }
   let(:mr_rule_names) { %w[foo lorem ipsum] }
   let(:modal_id) { '#mr-edit-approvals-create-modal' }
   let(:members_selector) { "#{modal_id} input[name=members]" }
@@ -25,13 +25,15 @@ RSpec.describe 'Merge request > User edits MR with approval rules', :js do
   end
 
   before do
-    project.update_attribute(:disable_overriding_approvers_per_merge_request, false)
+    project.update!(disable_overriding_approvers_per_merge_request: false)
     stub_licensed_features(multiple_approval_rules: true)
 
     approver = create(:user)
     mr_rule_names.each do |name|
-      create(:approval_merge_request_rule,
-        merge_request: merge_request, approvals_required: 1, name: name, users: [approver])
+      create(
+        :approval_merge_request_rule,
+        merge_request: merge_request, approvals_required: 1, name: name, users: [approver]
+      )
     end
 
     sign_in(author)
@@ -66,7 +68,7 @@ RSpec.describe 'Merge request > User edits MR with approval rules', :js do
   end
 
   context "with public group" do
-    let!(:group) { create(:group, :public) }
+    let_it_be(:group) { create(:group, :public) }
 
     before do
       group.add_developer create(:user)
