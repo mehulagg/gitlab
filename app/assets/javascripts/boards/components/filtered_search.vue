9956a5ccbaaceb30371c7369f5c7b83d5d746583
sstern <sstern@gitlab.com>
2021-02-24 10:51:19 -0800

    Address comments

--- app/assets/javascripts/boards/components/filtered_search.vue
+++ app/assets/javascripts/boards/components/filtered_search.vue
@@ -2,15 +2,24 @@
 import { mapActions } from 'vuex';
 import { historyPushState } from '~/lib/utils/common_utils';
 import { setUrlParams } from '~/lib/utils/url_utility';
+import { __ } from '~/locale';
 import FilteredSearch from '~/vue_shared/components/filtered_search_bar/filtered_search_bar_root.vue';
 
 export default {
+  i18n: {
+    search: __('Search'),
+  },
   components: { FilteredSearch },
   props: {
     search: {
       type: String,
       required: false,
       default: '',
+    },
+  },
+  computed: {
+    initialSearch() {
+      return [{ type: 'filtered-search-term', value: { data: this.search } }];
     },
   },
   mounted() {
@@ -43,8 +52,8 @@ export default {
     class="gl-w-full"
     namespace=""
     :tokens="[]"
-    :search-input-placeholder="'Search'"
-    :initial-filter-value="[{ type: 'filtered-search-term', value: { data: search } }]"
+    :search-input-placeholder="$options.i18n.search"
+    :initial-filter-value="initialSearch"
     @onFilter="handleSearch"
   />
 </template>
