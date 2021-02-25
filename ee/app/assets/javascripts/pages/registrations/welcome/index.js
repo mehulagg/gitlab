7873001f7b879ed2fabeafd1991d84a973b07510
Ammar Alakkad <am.alakkad@gmail.com>
2021-02-25 15:55:03 +0300

    Add company_name field to the frontend
    It also add a javascript to toggle the section visibility

--- ee/app/assets/javascripts/pages/registrations/welcome/index.js
+++ ee/app/assets/javascripts/pages/registrations/welcome/index.js
@@ -4,12 +4,15 @@ mountProgressBar();
 
 const emailUpdatesForm = document.querySelector('.js-email-opt-in');
 const setupForCompany = document.querySelector('.js-setup-for-company');
+const setupForCompanyDetails = document.querySelector('.js-setup_for_company_details');
 const setupForMe = document.querySelector('.js-setup-for-me');
 
 setupForCompany.addEventListener('change', () => {
   emailUpdatesForm.classList.add('hidden');
+  setupForCompanyDetails.classList.remove('hidden');
 });
 
 setupForMe.addEventListener('change', () => {
   emailUpdatesForm.classList.remove('hidden');
+  setupForCompanyDetails.classList.add('hidden');
 });
