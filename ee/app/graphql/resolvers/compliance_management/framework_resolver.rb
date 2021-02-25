359e155d04f993d295716ed3b1a435f36cd07069
Aishwarya Subramanian <asubramanian@gitlab.com>
2021-02-25 10:39:58 -0600

    Added namespace policy

--- ee/app/graphql/resolvers/compliance_management/framework_resolver.rb
+++ ee/app/graphql/resolvers/compliance_management/framework_resolver.rb
@@ -7,11 +7,9 @@ module Resolvers
 
       type ::Types::ComplianceManagement::ComplianceFrameworkType.connection_type, null: true
 
-      when_single do
-        argument :id, ::Types::GlobalIDType[::ComplianceManagement::Framework],
-                 description: 'Global ID of a specific compliance framework to return.',
-                 required: false
-      end
+      argument :id, ::Types::GlobalIDType[::ComplianceManagement::Framework],
+               description: 'Global ID of a specific compliance framework to return.',
+               required: false
 
       def resolve(id: nil)
         id = ::Types::GlobalIDType[::ComplianceManagement::Framework].coerce_isolated_input(id) unless id.nil?
@@ -31,8 +29,9 @@ module Resolvers
       def evaluate(namespace_ids, by_namespace_id, loader)
         frameworks(namespace_ids).group_by(&:namespace_id).each do |ns_id, group|
           by_namespace_id[ns_id].each do |fw_id|
+            authorize!
+
             group.each do |fw|
-              next unless allowed?
               next unless fw_id.nil? || fw_id.to_i == fw.id
 
               loader.call([ns_id, fw_id]) { |array| array << fw }
@@ -45,8 +44,8 @@ module Resolvers
         ::ComplianceManagement::Framework.with_namespaces(namespace_ids)
       end
 
-      def allowed?
-        Ability.allowed?(context[:current_user], :admin_compliance_framework, object)
+      def authorize!
+        Ability.allowed?(context[:current_user], :admin_compliance_framework, object) || raise_resource_not_available_error!
       end
     end
   end
