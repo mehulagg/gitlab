1fca1dd2ba66ab84616c36f4488a8d18bf96437b
Doug Stull <dstull@gitlab.com>
2021-02-25 09:49:11 -0500

    Remove invite members version a experiment/ff
    - declared success - so we will now show this on every
      priveledged user.

--- spec/support/shared_examples/features/issuable_invite_members_shared_examples.rb
+++ spec/support/shared_examples/features/issuable_invite_members_shared_examples.rb
@@ -1,11 +1,7 @@
 # frozen_string_literal: true
 
 RSpec.shared_examples 'issuable invite members experiments' do
-  context 'when invite_members_version_a experiment is enabled' do
-    before do
-      stub_experiment_for_subject(invite_members_version_a: true)
-    end
-
+  context 'when a privileged user can invite' do
     it 'shows a link for inviting members and follows through to the members page' do
       project.add_maintainer(user)
       visit issuable_path
