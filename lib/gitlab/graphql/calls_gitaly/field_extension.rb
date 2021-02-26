0f83feba390f5734a017481f8d1edd88c2cd1d06
Alex Kalderimis <akalderimis@gitlab.com>
2021-02-26 16:14:17 +0000

    Fixes to deal with out-of-order execution

--- lib/gitlab/graphql/calls_gitaly/field_extension.rb
+++ lib/gitlab/graphql/calls_gitaly/field_extension.rb
@@ -9,36 +9,77 @@ module Gitlab
       # need it (i.e. it has a constant complexity or knows that it calls
       # gitaly)
       class FieldExtension < ::GraphQL::Schema::FieldExtension
-        def resolve(object:, arguments:, context:, **rest)
-          yield(object, arguments, current_gitaly_call_count)
+        include Laziness
+
+        def resolve(object:, arguments:, **rest)
+          yield(object, arguments, [current_gitaly_call_count, accounted_for])
         end
 
         def after_resolve(value:, memo:, **rest)
-          count = current_gitaly_call_count - memo
+          (value, count) = value_with_count(value, memo)
           calls_gitaly_check(count)
+          accounted_for(count)
 
           value
         end
 
         private
 
+        # Resolutions are not nested nicely (due to laziness), so we have to
+        # know not just how many calls were made before resolution started, but
+        # also how many were accounted for by fields with the correct settings
+        # in between.
+        #
+        # e.g. the following is not just plausible, but common:
+        #
+        #   enter A.user (lazy)
+        #   enter A.x
+        #   leave A.x
+        #   enter A.calls_gitaly
+        #   leave A.calls_gitaly (accounts for 1 call)
+        #   leave A.user
+        #
+        # In this circumstance we need to mark the calls made by A.calls_gitaly
+        # as accounted for, even though they were made after we yielded
+        # in A.user
+        def value_with_count(value, (previous_count, previous_accounted_for))
+          newly_accounted_for = accounted_for - previous_accounted_for
+          value = force(value)
+          count = [current_gitaly_call_count - (previous_count + newly_accounted_for), 0].max
+
+          [value, count]
+        end
+
         def current_gitaly_call_count
           Gitlab::GitalyClient.get_request_count || 0
         end
 
         def calls_gitaly_check(calls)
-          return if calls < 1
-
-          # Will inform you if there needs to be `calls_gitaly: true` as a kwarg in the field declaration
-          # if there is at least 1 Gitaly call involved with the field resolution.
-          field_name = field.graphql_name
-          object_name = field.owner_type.graphql_name
+          return if calls < 1 || field.may_call_gitaly?
 
           error = RuntimeError.new(<<~ERROR)
-            #{object_name}.#{field_name} calls Gitaly!
-            Please either specify a constant complexity or add `calls_gitaly: true` to the field declaration
+            #{field_name} unexpectedly calls Gitaly!
+
+            Please either specify a constant complexity or add `calls_gitaly: true`
+            to the field declaration
           ERROR
           Gitlab::ErrorTracking.track_and_raise_for_dev_exception(error)
+        end
+
+        def accounted_for(count = nil)
+          return 0 unless Gitlab::SafeRequestStore.active?
+
+          Gitlab::SafeRequestStore["graphql_gitaly_accounted_for"] ||= 0
+
+          if count.nil?
+            Gitlab::SafeRequestStore["graphql_gitaly_accounted_for"]
+          else
+            Gitlab::SafeRequestStore["graphql_gitaly_accounted_for"] += count
+          end
+        end
+
+        def field_name
+          "#{field.owner_type.graphql_name}.#{field.graphql_name}"
         end
       end
     end
