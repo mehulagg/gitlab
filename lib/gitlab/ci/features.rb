342121ad3b770f7b94bd4b98d649a01a6505ed34
Shinya Maeda <shinya@gitlab.com>
2021-03-01 10:45:01 +0700

    Remove the MR pipelines for forked project feature flag
    This commit removes the
    'ci_disallow_to_create_merge_request_pipelines_in_target_project'
    feature flag as the feature is deemed stable.

--- lib/gitlab/ci/features.rb
+++ lib/gitlab/ci/features.rb
@@ -31,13 +31,6 @@ module Gitlab
         ::Feature.enabled?(:ci_raise_job_rules_without_workflow_rules_warning, default_enabled: true)
       end
 
-      # NOTE: The feature flag `disallow_to_create_merge_request_pipelines_in_target_project`
-      # is a safe switch to disable the feature for a particular project when something went wrong,
-      # therefore it's not supposed to be enabled by default.
-      def self.disallow_to_create_merge_request_pipelines_in_target_project?(target_project)
-        ::Feature.enabled?(:ci_disallow_to_create_merge_request_pipelines_in_target_project, target_project)
-      end
-
       def self.project_transactionless_destroy?(project)
         Feature.enabled?(:project_transactionless_destroy, project, default_enabled: false)
       end
