196ea324f60b31da6bd34aa7de0c87d715effbcd
Zack Cuddy <zcuddy@gitlab.com>
2021-03-01 11:08:03 -0600

    Geo Node Status 2.0 - Node Header Details
    This change adds the Health Status,
    Actions, and Last Updated info
    to the Node header.

---
+++ ee/app/assets/javascripts/geo_nodes_beta/components/header/geo_node_header.vue
@@ -0,0 +1,67 @@
+<script>
+import { GlIcon, GlBadge } from '@gitlab/ui';
+import GeoNodeActions from './geo_node_actions.vue';
+import GeoNodeHealthStatus from './geo_node_health_status.vue';
+import GeoNodeLastUpdated from './geo_node_last_updated.vue';
+
+export default {
+  name: 'GeoNodeHeader',
+  components: {
+    GlIcon,
+    GlBadge,
+    GeoNodeHealthStatus,
+    GeoNodeLastUpdated,
+    GeoNodeActions,
+  },
+  props: {
+    node: {
+      type: Object,
+      required: true,
+    },
+    collapsed: {
+      type: Boolean,
+      required: false,
+      default: false,
+    },
+  },
+  computed: {
+    chevronIcon() {
+      return this.collapsed ? 'chevron-right' : 'chevron-down';
+    },
+  },
+};
+</script>
+
+<template>
+  <div
+    class="gl-display-grid geo-node-header-grid-columns gl-bg-gray-10 gl-border-b-1 gl-border-b-solid gl-border-b-gray-100 gl-border-t-1 gl-border-t-solid gl-border-t-gray-100 gl-py-3 gl-px-5"
+  >
+    <div class="gl-display-flex gl-align-items-center">
+      <gl-icon
+        class="gl-text-blue-500 gl-mr-3 gl-cursor-pointer"
+        :name="chevronIcon"
+        @click="$emit('collapse')"
+      />
+      <div
+        class="gl-display-flex gl-flex-direction-column gl-md-flex-direction-row gl-md-align-items-center gl-flex-fill-1"
+      >
+        <div class="gl-display-flex gl-align-items-center gl-flex-fill-1">
+          <gl-badge v-if="node.current" variant="info" class="gl-mr-2">{{
+            __('Current')
+          }}</gl-badge>
+          <h4 class="gl-font-lg">{{ node.name }}</h4>
+        </div>
+        <div class="gl-display-flex gl-align-items-center gl-flex-fill-2">
+          <geo-node-health-status :status="node.healthStatus" />
+          <geo-node-last-updated
+            class="gl-ml-2"
+            :status-check-timestamp="node.lastSuccessfulStatusCheckTimestamp * 1000"
+          />
+        </div>
+      </div>
+    </div>
+    <div class="gl-display-flex gl-align-items-center gl-justify-content-end">
+      <geo-node-actions :primary="node.primary" />
+    </div>
+  </div>
+</template>
---
+++ ee/spec/frontend/geo_nodes_beta/components/header/geo_node_actions_spec.js
@@ -0,0 +1,115 @@
+import { GlDropdown, GlDropdownItem, GlButton } from '@gitlab/ui';
+import { createLocalVue, shallowMount } from '@vue/test-utils';
+import Vuex from 'vuex';
+import GeoNodeActions from 'ee/geo_nodes_beta/components/header/geo_node_actions.vue';
+import { MOCK_PRIMARY_VERSION, MOCK_REPLICABLE_TYPES } from 'ee_jest/geo_nodes_beta/mock_data';
+
+const localVue = createLocalVue();
+localVue.use(Vuex);
+
+describe('GeoNodeActions', () => {
+  let wrapper;
+
+  const defaultProps = {
+    primary: true,
+  };
+
+  const createComponent = (initialState, props) => {
+    const store = new Vuex.Store({
+      state: {
+        primaryVersion: MOCK_PRIMARY_VERSION.version,
+        primaryRevision: MOCK_PRIMARY_VERSION.revision,
+        replicableTypes: MOCK_REPLICABLE_TYPES,
+        ...initialState,
+      },
+    });
+
+    wrapper = shallowMount(GeoNodeActions, {
+      localVue,
+      store,
+      propsData: {
+        ...defaultProps,
+        ...props,
+      },
+    });
+  };
+
+  afterEach(() => {
+    wrapper.destroy();
+    wrapper = null;
+  });
+
+  const findGeoMobileActionsDropdown = () => wrapper.find(GlDropdown);
+  const findGeoMobileActionsDropdownItems = () => wrapper.findAll(GlDropdownItem);
+  const findGeoMobileActionsRemoveDropdownItem = () =>
+    wrapper.find('[data-testid="geoMobileRemoveAction"]');
+  const findGeoDesktopActions = () => wrapper.find('[data-testid="geoDesktopActions"]');
+  const findGeoDesktopActionsButtons = () => wrapper.findAll(GlButton);
+  const findGeoDesktopActionsRemoveButton = () =>
+    wrapper.find('[data-testid="geoDesktopRemoveAction"]');
+
+  describe('template', () => {
+    describe('always', () => {
+      beforeEach(() => {
+        createComponent();
+      });
+
+      describe('for Mobile Actions', () => {
+        it('renders Dropdown with correct visibility class', () => {
+          expect(findGeoMobileActionsDropdown().exists()).toBe(true);
+          expect(findGeoMobileActionsDropdown().classes()).toStrictEqual(['gl-lg-display-none']);
+        });
+
+        it('renders an Edit and Remove dropdown item', () => {
+          expect(findGeoMobileActionsDropdownItems().wrappers.map((w) => w.text())).toStrictEqual([
+            'Edit',
+            'Remove',
+          ]);
+        });
+      });
+
+      describe('for Desktop Actions', () => {
+        it('renders container with correct visibility class', () => {
+          expect(findGeoDesktopActions().exists()).toBe(true);
+          expect(findGeoDesktopActions().classes()).toStrictEqual([
+            'gl-display-none',
+            'gl-lg-display-flex',
+          ]);
+        });
+
+        it('renders an Edit and Remove button', () => {
+          expect(findGeoDesktopActionsButtons().wrappers.map((w) => w.text())).toStrictEqual([
+            'Edit',
+            'Remove',
+          ]);
+        });
+      });
+    });
+
+    describe.each`
+      primary  | disabled     | dropdownClass
+      ${true}  | ${'true'}    | ${'gl-text-gray-400'}
+      ${false} | ${undefined} | ${'gl-text-red-500'}
+    `(`conditionally`, ({ primary, disabled, dropdownClass }) => {
+      beforeEach(() => {
+        createComponent(null, { primary });
+      });
+
+      describe(`when primary is ${primary}`, () => {
+        it('disables the Mobile Remove dropdown item', () => {
+          expect(findGeoMobileActionsRemoveDropdownItem().attributes('disabled')).toBe(disabled);
+        });
+
+        it('adds disabled class to the Mobile Remove dropdown item', () => {
+          expect(findGeoMobileActionsRemoveDropdownItem().find('span').classes(dropdownClass)).toBe(
+            true,
+          );
+        });
+
+        it('disables the Desktop Remove button', () => {
+          expect(findGeoDesktopActionsRemoveButton().attributes('disabled')).toBe(disabled);
+        });
+      });
+    });
+  });
+});
