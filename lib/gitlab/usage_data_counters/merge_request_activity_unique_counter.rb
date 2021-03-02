f83b842223fcd8c16bc8b56807c20e930a2bfd98
Patrick Bajao <ebajao@gitlab.com>
2021-03-02 19:50:23 +0800

    Add tracking to merge request assignees/reviewers changes
    We need to track how many users change the assignees/reviewers of
    MRs.
    
    To get that, whenever the assignees/reviewers changes for a merge
    request, we track the following events:
    
    - i_code_review_user_assignees_changed
    - i_code_review_user_reviewers_changed

--- lib/gitlab/usage_data_counters/merge_request_activity_unique_counter.rb
+++ lib/gitlab/usage_data_counters/merge_request_activity_unique_counter.rb
@@ -35,6 +35,8 @@ module Gitlab
       MR_EDIT_MR_TITLE_ACTION = 'i_code_review_edit_mr_title'
       MR_EDIT_MR_DESC_ACTION = 'i_code_review_edit_mr_desc'
       MR_CREATE_FROM_ISSUE_ACTION = 'i_code_review_user_create_mr_from_issue'
+      MR_ASSIGNEES_CHANGED_ACTION = 'i_code_review_user_assignees_changed'
+      MR_REVIEWERS_CHANGED_ACTION = 'i_code_review_user_reviewers_changed'
 
       class << self
         def track_mr_diffs_action(merge_request:)
@@ -151,6 +153,14 @@ module Gitlab
 
         def track_mr_create_from_issue(user:)
           track_unique_action_by_user(MR_CREATE_FROM_ISSUE_ACTION, user)
+        end
+
+        def track_assignees_changed_action(user:)
+          track_unique_action_by_user(MR_ASSIGNEES_CHANGED_ACTION, user)
+        end
+
+        def track_reviewers_changed_action(user:)
+          track_unique_action_by_user(MR_REVIEWERS_CHANGED_ACTION, user)
         end
 
         private
