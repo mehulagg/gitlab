62715050fdd99eae092b2ebecbcbedf2fc46e910
Donald Cook <dcook@gitlab.com>
2021-03-01 16:52:07 -0800

    Started spec

---
+++ spec/frontend/sidebar/components/reference/sidebar_reference_widget_spec.js
@@ -0,0 +1,11 @@
+import { GlIcon } from '@gitlab/ui';
+import { shallowMount } from '@vue/test-utils';
+import SidebarReferenceWidget from '~/sidebar/components/reference/sidebar_reference_widget.vue';
+
+describe('Sidebar Reference Widget', () => {
+  it('shows loading icon when query is loading', () => {
+  });
+
+  it('displays reference', () => {
+  });
+});
