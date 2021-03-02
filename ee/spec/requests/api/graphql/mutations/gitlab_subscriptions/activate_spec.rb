3fd69037b6b03d29a4007ca025942d3d01a6aaa0
Vijay Hawoldar <vhawoldar@gitlab.com>
2021-03-02 15:31:54 +0000

    Fix broken tests due to method rename

--- ee/spec/requests/api/graphql/mutations/gitlab_subscriptions/activate_spec.rb
+++ ee/spec/requests/api/graphql/mutations/gitlab_subscriptions/activate_spec.rb
@@ -32,7 +32,7 @@ RSpec.describe 'Activate a subscription' do
   end
 
   before do
-    allow(Gitlab::SubscriptionPortal::Clients::Graphql).to receive(:http_post).and_return(remote_response)
+    allow(Gitlab::SubscriptionPortal::Clients::Graphql).to receive(:execute).and_return(remote_response)
   end
 
   it 'persists authentication token' do
