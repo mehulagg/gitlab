4725969acddc2518a4f7752fec2b0dc400e8e246
Tom Quirk <tquirk@gitlab.com>
2021-03-02 13:33:44 +0930

    Init storybook for vue_shared
    Adds Storybook dependencies, config,
    and a story for todo_button.vue.

---
+++ .storybook/main.js
@@ -0,0 +1,4 @@
+module.exports = {
+  stories: ['../app/assets/javascripts/vue_shared/components/**/*.stories.js'],
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-controls'],
+};
---
+++ .storybook/preview.js
@@ -0,0 +1,11 @@
+// const stylesheetsRequireCtx = require.context(
+//   '../app/assets/stylesheets',
+//   true,
+//   /application\.scss$/,
+// );
+
+// stylesheetsRequireCtx('./application.scss');
+
+export const parameters = {
+  actions: { argTypesRegex: '^on[A-Z].*' },
+};
