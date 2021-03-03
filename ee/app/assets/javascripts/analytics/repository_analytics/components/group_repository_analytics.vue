2ddd560d0c67f7a82ecffd683286486696795a09
mfluharty <mfluharty@gitlab.com>
2021-03-02 21:00:14 -0700

    Track group coverage visits in vue instead
    Count unique users in the same way as other group::testing metrics
    So we can use the unique user counts in aggregated metrics

--- ee/app/assets/javascripts/analytics/repository_analytics/components/group_repository_analytics.vue
+++ ee/app/assets/javascripts/analytics/repository_analytics/components/group_repository_analytics.vue
@@ -1,4 +1,5 @@
 <script>
+import api from '~/api';
 import { s__ } from '~/locale';
 import glFeatureFlagsMixin from '~/vue_shared/mixins/gl_feature_flags_mixin';
 import DownloadTestCoverage from './download_test_coverage.vue';
@@ -13,9 +14,15 @@ export default {
     DownloadTestCoverage,
   },
   mixins: [glFeatureFlagsMixin()],
+  mounted() {
+    if (this.glFeatures.usageDataITestingGroupCodeCoverageVisitTotal) {
+      api.trackRedisHllUserEvent(this.$options.groupCoverageVisitEvent);
+    }
+  },
   text: {
     codeCoverageHeader: s__('RepositoriesAnalytics|Test Code Coverage'),
   },
+  groupCoverageVisitEvent: 'i_testing_group_code_coverage_visit_total',
 };
 </script>
 
