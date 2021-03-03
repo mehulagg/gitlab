d20d743862b8373ac814387023a16e5848d283f7
Aishwarya Subramanian <asubramanian@gitlab.com>
2021-03-03 16:32:48 -0600

    Return not found is feature not available

--- ee/lib/api/merge_request_jira_issue_link.rb
+++ ee/lib/api/merge_request_jira_issue_link.rb
@@ -3,10 +3,15 @@
 module API
   class MergeRequestJiraIssueLink < ::API::Base
     before { authenticate_non_get! }
+    before { check_jira_issue_link_feature_available! }
 
     feature_category :source_code_management
 
     helpers do
+      def check_jira_issue_link_feature_available!
+        not_found! unless user_project.jira_issue_association_required_to_merge_enabled?
+      end
+
       def present_issue_link(merge_request)
         present merge_request, with: ::API::Entities::MergeRequests::JiraIssueLink, current_user: current_user
       end
@@ -24,6 +29,7 @@ module API
         desc 'List Jira Issue link for merge request'
         get 'jira_issue_link' do
           not_found!("Merge Request") unless can?(current_user, :read_merge_request, user_project)
+          not_found! unless user_project.project_setting.prevent_merge_without_jira_issue
 
           merge_request = find_merge_request_with_access(params[:merge_request_iid])
 
