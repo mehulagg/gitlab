707af95c507276ed425685b56460bfcfa0d43b7b
Anastasia McDonald <amcdonald@gitlab.com>
2021-03-03 16:46:30 +1300

    Fix style issues
    As per prettier cli

--- ee/app/assets/javascripts/epic/components/epic_form.vue
+++ ee/app/assets/javascripts/epic/components/epic_form.vue
@@ -158,7 +158,11 @@ export default {
           </template>
         </markdown-field>
       </gl-form-group>
-      <gl-form-group :label="__('Confidentiality')" label-for="epic-confidentiality" data-qa-selector="confidential_epic_checkbox">
+      <gl-form-group
+        :label="__('Confidentiality')"
+        label-for="epic-confidentiality"
+        data-qa-selector="confidential_epic_checkbox"
+      >
         <gl-form-checkbox
           id="epic-confidentiality"
           v-model="confidential"
