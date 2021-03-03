c259e0d298ea8144c48363bcfc33045bd20f4425
Alex Ives <aives@gitlab.com>
2021-03-03 14:06:34 -0600

    Add usage data collection to geo secondaries
    - Add a cron worker to collect and update the secondary usage data table
    - Add a placeholder that does the data collection
    - Add a new cron to cron_manager and settings, running weekly on Sunday.
    
    Relates to https://gitlab.com/gitlab-org/gitlab/issues/323220

---
+++ ee/app/workers/geo/secondary_usage_data_cron_worker.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+module Geo
+  class SecondaryUsageDataCronWorker # rubocop:disable Scalability/IdempotentWorker
+    LEASE_KEY = 'secondary_usage_data_worker:ping'
+    LEASE_TIMEOUT = 86400
+
+    include ApplicationWorker
+    include CronjobQueue # rubocop:disable Scalability/CronWorkerContext
+    include ::Gitlab::Geo::LogHelpers
+    include Gitlab::ExclusiveLeaseHelpers
+
+    feature_category :geo_replication
+
+    def perform
+      return unless Gitlab::Geo.secondary?
+      return unless Feature.enabled?(:geo_secondary_usage_data_collection)
+
+      in_lock(LEASE_KEY, ttl: LEASE_TIMEOUT) do
+        # Splay the request over a minute to avoid thundering herd problems.
+        sleep(rand(0.0..60.0).round(3))
+
+        SecondaryUsageData.update_metrics!
+      end
+    end
+  end
+end
