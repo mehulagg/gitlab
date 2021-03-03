872b40f7cf88eebe012bf628186bfe8fdfd00ae8
Aishwarya Subramanian <asubramanian@gitlab.com>
2021-03-03 15:49:44 -0600

    Present Jira issue link entity

--- ee/lib/api/merge_request_jira_issue_link.rb
+++ ee/lib/api/merge_request_jira_issue_link.rb
@@ -7,7 +7,7 @@ module API
     feature_category :source_code_management
 
     helpers do
-      def present_approval(merge_request)
+      def present_issue_link(merge_request)
         present merge_request, with: ::API::Entities::MergeRequests::JiraIssueLink, current_user: current_user
       end
     end
@@ -21,13 +21,13 @@ module API
         #   merge_request_iid (required)  - IID of MR
         # Examples:
         #   GET /projects/:id/merge_requests/:merge_request_iid/jira_issue_link
-        desc 'List Jira enforcement for merge request'
+        desc 'List Jira Issue link for merge request'
         get 'jira_issue_link' do
           not_found!("Merge Request") unless can?(current_user, :read_merge_request, user_project)
 
           merge_request = find_merge_request_with_access(params[:merge_request_iid])
 
-          present_approval(merge_request)
+          present_issue_link(merge_request)
         end
       end
     end
