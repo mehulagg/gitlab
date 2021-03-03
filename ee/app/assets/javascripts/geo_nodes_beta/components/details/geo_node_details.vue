ac3b718efbd386d444441dcf16e519823666f324
Zack Cuddy <zcuddy@gitlab.com>
2021-03-03 14:07:43 -0600

    Geo Node Status 2.0 - Primary Details
    This change adds the core node and
    primary node details to the UI.
    
    This leaves placeholder text
    for the Secondary node for now.

---
+++ ee/app/assets/javascripts/geo_nodes_beta/components/details/geo_node_details.vue
@@ -0,0 +1,41 @@
+<script>
+import GeoNodeCoreDetails from './geo_node_core_details.vue';
+import GeoNodePrimaryOtherInfo from './primary_node/geo_node_primary_other_info.vue';
+import GeoNodeVerificationInfo from './primary_node/geo_node_verification_info.vue';
+
+export default {
+  name: 'GeoNodeDetails',
+  components: {
+    GeoNodeCoreDetails,
+    GeoNodePrimaryOtherInfo,
+    GeoNodeVerificationInfo,
+  },
+  props: {
+    node: {
+      type: Object,
+      required: true,
+    },
+  },
+};
+</script>
+
+<template>
+  <div class="gl-display-grid geo-node-details-grid-columns gl-p-5">
+    <geo-node-core-details :node="node" />
+    <div>
+      <div
+        v-if="node.primary"
+        class="gl-display-flex gl-sm-flex-direction-column gl-align-items-flex-start gl-h-full gl-w-full"
+      >
+        <geo-node-verification-info
+          class="gl-flex-fill-1 gl-mb-5 gl-md-mb-0 gl-md-mr-5 gl-h-full gl-w-full"
+          :node="node"
+        />
+        <geo-node-primary-other-info class="gl-flex-fill-1 gl-h-full gl-w-full" :node="node" />
+      </div>
+      <div v-else class="gl-display-flex gl-flex-direction-column gl-h-full gl-w-full">
+        <p>{{ s__('Geo|Secondary Details') }}</p>
+      </div>
+    </div>
+  </div>
+</template>
