fead182ad0031bf74e35e26b06ad8f63bcdc2d58
Aishwarya Subramanian <asubramanian@gitlab.com>
2021-03-03 17:04:50 -0600

    Renamed jira issue link to jira issue

--- ee/lib/api/entities/merge_requests/jira_issue.rb
+++ ee/lib/api/entities/merge_requests/jira_issue.rb
@@ -3,7 +3,7 @@
 module API
   module Entities
     module MergeRequests
-      class JiraIssueLink < Grape::Entity
+      class JiraIssue < Grape::Entity
         expose :is_present do |merge_request|
           Atlassian::JiraIssueKeyExtractor.has_keys?(merge_request.title, merge_request.description)
         end
--- ee/lib/api/merge_request_jira_issue_link.rb
+++ ee/lib/api/merge_request_jira_issue_link.rb
@@ -3,37 +3,37 @@
 module API
   class MergeRequestJiraIssueLink < ::API::Base
     before { authenticate_non_get! }
-    before { check_jira_issue_link_feature_available! }
+    before { check_jira_issue_feature_available! }
 
     feature_category :source_code_management
 
     helpers do
-      def check_jira_issue_link_feature_available!
+      def check_jira_issue_feature_available!
         not_found! unless user_project.jira_issue_association_required_to_merge_enabled?
       end
 
-      def present_issue_link(merge_request)
-        present merge_request, with: ::API::Entities::MergeRequests::JiraIssueLink, current_user: current_user
+      def present_jira_issue(merge_request)
+        present merge_request, with: ::API::Entities::MergeRequests::JiraIssue, current_user: current_user
       end
     end
 
     resource :projects, requirements: ::API::API::NAMESPACE_OR_PROJECT_REQUIREMENTS do
       segment ':id/merge_requests/:merge_request_iid' do
-        # Get the status of the merge request's approvals
+        # Get the Jira Issue link association for a merge request
         #
         # Parameters:
         #   id (required)                 - The ID of a project
         #   merge_request_iid (required)  - IID of MR
         # Examples:
-        #   GET /projects/:id/merge_requests/:merge_request_iid/jira_issue_link
-        desc 'List Jira Issue link for merge request'
-        get 'jira_issue_link' do
+        #   GET /projects/:id/merge_requests/:merge_request_iid/jira_issue
+        desc 'Get Jira Issue association for merge request'
+        get 'jira_issue' do
           not_found!("Merge Request") unless can?(current_user, :read_merge_request, user_project)
           not_found! unless user_project.project_setting.prevent_merge_without_jira_issue
 
           merge_request = find_merge_request_with_access(params[:merge_request_iid])
 
-          present_issue_link(merge_request)
+          present_jira_issue(merge_request)
         end
       end
     end
