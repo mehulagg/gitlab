b7f14f57eafe87bae388436b78cea6cf9dbebc5a
Denys Mishunov <dmishunov@gitlab.com>
2021-03-05 00:12:44 +0100

    Removed unnecessary getProjectData service

--- app/assets/javascripts/ide/services/index.js
+++ app/assets/javascripts/ide/services/index.js
@@ -1,16 +1,6 @@
 import Api from '~/api';
 import axios from '~/lib/utils/axios_utils';
 import { joinPaths, escapeFileUrl } from '~/lib/utils/url_utility';
-import getUserPermissions from '../queries/getUserPermissions.query.graphql';
-import { query } from './gql';
-
-const fetchApiProjectData = (projectPath) => Api.project(projectPath).then(({ data }) => data);
-
-const fetchGqlProjectData = (projectPath) =>
-  query({
-    query: getUserPermissions,
-    variables: { projectPath },
-  }).then(({ data }) => data.project);
 
 export default {
   getFileData(endpoint) {
@@ -58,18 +48,6 @@ export default {
         },
       )
       .then(({ data }) => data);
-  },
-  getProjectData(namespace, project) {
-    const projectPath = `${namespace}/${project}`;
-
-    return Promise.all([fetchApiProjectData(projectPath), fetchGqlProjectData(projectPath)]).then(
-      ([apiProjectData, gqlProjectData]) => ({
-        data: {
-          ...apiProjectData,
-          ...gqlProjectData,
-        },
-      }),
-    );
   },
   getProjectMergeRequests(projectId, params = {}) {
     return Api.projectMergeRequests(projectId, params);
