d566c9ec69c480a5aeca15ad30956e4661a42606
Nicolò Maria Mezzopera <nicolo.mezzopera@gmail.com>
2021-03-04 12:32:23 +0100

    Wire installation title into conan
    - source
    - tests
    - snapshots

--- spec/frontend/packages/details/components/conan_installation_spec.js
+++ spec/frontend/packages/details/components/conan_installation_spec.js
@@ -1,6 +1,7 @@
 import { shallowMount, createLocalVue } from '@vue/test-utils';
 import Vuex from 'vuex';
 import ConanInstallation from '~/packages/details/components/conan_installation.vue';
+import InstallationTitle from '~/packages/details/components/installation_title.vue';
 import CodeInstructions from '~/vue_shared/components/registry/code_instruction.vue';
 import { conanPackage as packageEntity } from '../../mock_data';
 import { registryUrl as conanPath } from '../mock_data';
@@ -26,6 +27,7 @@ describe('ConanInstallation', () => {
   });
 
   const findCodeInstructions = () => wrapper.findAll(CodeInstructions);
+  const findInstallationTitle = () => wrapper.findComponent(InstallationTitle);
 
   function createComponent() {
     wrapper = shallowMount(ConanInstallation, {
@@ -44,6 +46,18 @@ describe('ConanInstallation', () => {
 
   it('renders all the messages', () => {
     expect(wrapper.element).toMatchSnapshot();
+  });
+
+  describe('install command switch', () => {
+    it('has the installation title component', () => {
+      createComponent();
+
+      expect(findInstallationTitle().exists()).toBe(true);
+      expect(findInstallationTitle().props()).toMatchObject({
+        storageKey: 'package_conan_installation_instructions',
+        options: [{ value: 'conan', label: 'Show Conan commands' }],
+      });
+    });
   });
 
   describe('installation commands', () => {
