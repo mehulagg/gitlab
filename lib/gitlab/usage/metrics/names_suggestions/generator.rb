2a8a6b2bb4679c2b775aeeb1a7f98942cd31e62b
Mikolaj Wawrzyniak <mwawrzyniak@gitlab.com>
2021-03-04 15:52:02 +0100

    Include constraints in suggested metrics name

--- lib/gitlab/usage/metrics/names_suggestions/generator.rb
+++ lib/gitlab/usage/metrics/names_suggestions/generator.rb
@@ -13,11 +13,11 @@ module Gitlab
             private
 
             def count(relation, column = nil, batch: true, batch_size: nil, start: nil, finish: nil)
-              "count_#{parse_target_and_source(column, relation)}"
+              name_suggestion(column, relation, 'count')
             end
 
             def distinct_count(relation, column = nil, batch: true, batch_size: nil, start: nil, finish: nil)
-              "count_distinct_#{parse_target_and_source(column, relation)}"
+              name_suggestion(column, relation, 'count_distinct', :distinct)
             end
 
             def redis_usage_counter
@@ -29,23 +29,67 @@ module Gitlab
             end
 
             def sum(relation, column, *rest)
-              "sum_#{parse_target_and_source(column, relation)}"
+              name_suggestion(column, relation, 'sum')
             end
 
             def estimate_batch_distinct_count(relation, column = nil, *rest)
-              "estimate_distinct_#{parse_target_and_source(column, relation)}"
+              name_suggestion(column, relation, 'estimate_distinct_count')
             end
 
             def add(*args)
               "add_#{args.join('_and_')}"
             end
 
+            def name_suggestion(column, relation, prefix, distinct = nil)
+              parts = [prefix]
+              target = parse_target(relation, column)
 
-            def parse_target_and_source(column, relation)
               if column
-                "#{column}_from_#{relation.table_name}"
+                parts << target
+                parts << 'from'
+              end
+
+              source = parse_source(relation)
+              constraints = parse_constraints(column, distinct, relation)
+
+              if constraints.include?(source)
+                parts << "<adjective describing: '#{constraints}'>"
+              end
+
+              parts << source
+              parts.compact.join('_')
+            end
+
+            def parse_constraints(column, distinct, relation)
+              connection = relation.connection
+              RelationParsers::Constraints.new(connection).accept(arel(relation, column, distinct), collector(connection)).value
+            end
+
+            def parse_target(relation, column)
+              if column.nil?
+                "#{relation.table_name}.#{relation.primary_key}"
+              elsif column.is_a?(Arel::Attribute)
+                "#{column.relation.name}.#{column.name}"
               else
-                relation.table_name
+                column
+              end
+            end
+
+            def parse_source(relation)
+              relation.table_name
+            end
+
+            def collector(connection)
+              Arel::Collectors::SubstituteBinds.new(connection, Arel::Collectors::SQLString.new)
+            end
+
+            def arel(relation, column, distinct)
+              column ||= relation.primary_key
+
+              if column.is_a?(Arel::Attribute)
+                relation.select(column.count(distinct)).arel
+              else
+                relation.select(relation.all.table[column].count(distinct)).arel
               end
             end
           end
--- spec/lib/gitlab/usage/metrics/names_suggestions/generator_spec.rb
+++ spec/lib/gitlab/usage/metrics/names_suggestions/generator_spec.rb
@@ -4,34 +4,45 @@ require 'spec_helper'
 
 RSpec.describe Gitlab::Usage::Metrics::NamesSuggestions::Generator do
   describe '#generate' do
-    context 'for count metrics' do
+    shared_examples 'name suggestion' do
       it 'return correct name' do
-        expect(described_class.generate('counts.boards')).to eq 'count_boards'
+        expect(described_class.generate(key_path)).to eq name_suggestion
       end
+    end
+
+    context 'for count metrics' do
+      let(:key_path) { 'counts.boards' }
+      let(:name_suggestion) { 'count_boards' }
+
+      it_behaves_like 'name suggestion'
     end
 
     context 'for count distinct metrics' do
-      it 'return correct name' do
-        expect(described_class.generate('counts.issues_using_zoom_quick_actions')).to eq 'count_distinct_issue_id_from_zoom_meetings'
-      end
+      let(:key_path) { 'counts.issues_using_zoom_quick_actions' }
+      let(:name_suggestion) { 'count_distinct_issue_id_from_zoom_meetings' }
+
+      it_behaves_like 'name suggestion'
     end
 
     context 'for sum metrics' do
-      it 'return correct name' do
-        expect(described_class.generate('counts.jira_imports_total_imported_issues_count')).to eq 'sum_imported_issues_count_from_jira_imports'
-      end
+      let(:key_path) { 'counts.jira_imports_total_imported_issues_count' }
+      let(:name_suggestion) { "add_count_<adjective describing: '(snippets.type = 'PersonalSnippet')'>_snippets_and_count_<adjective describing: '(snippets.type = 'ProjectSnippet')'>_snippets" }
+
+      it_behaves_like 'name suggestion'
     end
 
     context 'for add metrics' do
-      it 'return correct name' do
-        expect(described_class.generate('counts.snippets')).to eq 'add_count_snippets_and_count_snippets'
-      end
+      let(:key_path) { 'counts.snippets' }
+      let(:name_suggestion) { 'add_count_snippets_and_count_snippets' }
+
+      it_behaves_like 'name suggestion'
     end
 
     context 'for redis metrics' do
-      it 'return correct name' do
-        expect(described_class.generate('analytics_unique_visits.analytics_unique_visits_for_any_target')).to eq 'names_suggestions_for_redis_counters_are_not_supported_yet'
-      end
+      let(:key_path) { 'analytics_unique_visits.analytics_unique_visits_for_any_target' }
+      let(:name_suggestion) { 'names_suggestions_for_redis_counters_are_not_supported_yet' }
+
+      it_behaves_like 'name suggestion'
     end
   end
 end
