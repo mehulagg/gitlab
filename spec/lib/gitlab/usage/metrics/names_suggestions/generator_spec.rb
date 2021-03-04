455afa288d6785821f7f5567f1e98233bde61c84
Mikolaj Wawrzyniak <mwawrzyniak@gitlab.com>
2021-03-04 15:52:02 +0100

    Include constraints in suggested metrics name
    Enchance automated metric names suggestion with inclusion of
    variable parts based on underlaying query constraints.

--- spec/lib/gitlab/usage/metrics/names_suggestions/generator_spec.rb
+++ spec/lib/gitlab/usage/metrics/names_suggestions/generator_spec.rb
@@ -10,40 +10,58 @@ RSpec.describe Gitlab::Usage::Metrics::NamesSuggestions::Generator do
   end
 
   describe '#generate' do
-    context 'for count metrics' do
+    shared_examples 'name suggestion' do
       it 'return correct name' do
-        expect(described_class.generate('counts.boards')).to eq 'count_boards'
+        expect(described_class.generate(key_path)).to eq name_suggestion
       end
     end
 
-    context 'for count distinct metrics' do
-      it 'return correct name' do
-        expect(described_class.generate('counts.issues_using_zoom_quick_actions')).to eq 'count_distinct_issue_id_from_zoom_meetings'
-      end
+    context 'for count with default column metrics' do
+      # corresponding metric is collected with count(Board)
+      let(:key_path) { 'counts.boards' }
+      let(:name_suggestion) { 'count_boards' }
+
+      it_behaves_like 'name suggestion'
+    end
+
+    context 'for count distinct with column defined metrics' do
+      # corresponding metric is collected with distinct_count(ZoomMeeting, :issue_id)
+      let(:key_path) { 'counts.issues_using_zoom_quick_actions' }
+      let(:name_suggestion) { 'count_distinct_issue_id_from_zoom_meetings' }
+
+      it_behaves_like 'name suggestion'
     end
 
     context 'for sum metrics' do
-      it 'return correct name' do
-        expect(described_class.generate('counts.jira_imports_total_imported_issues_count')).to eq 'sum_imported_issues_count_from_jira_imports'
-      end
+      # corresponding metric is collected with sum(JiraImportState.finished, :imported_issues_count)
+      let(:key_path) { 'counts.jira_imports_total_imported_issues_count' }
+      let(:name_suggestion) { "sum_imported_issues_count_from_<adjective describing: '(jira_imports.status = 4)'>_jira_imports" }
+
+      it_behaves_like 'name suggestion'
     end
 
     context 'for add metrics' do
-      it 'return correct name' do
-        expect(described_class.generate('counts.snippets')).to eq 'add_count_snippets_and_count_snippets'
-      end
+      # corresponding metric is collected with add(data[:personal_snippets], data[:project_snippets])
+      let(:key_path) { 'counts.snippets' }
+      let(:name_suggestion) { "add_count_<adjective describing: '(snippets.type = 'PersonalSnippet')'>_snippets_and_count_<adjective describing: '(snippets.type = 'ProjectSnippet')'>_snippets" }
+
+      it_behaves_like 'name suggestion'
     end
 
     context 'for redis metrics' do
-      it 'return correct name' do
-        expect(described_class.generate('analytics_unique_visits.analytics_unique_visits_for_any_target')).to eq '<please fill metric name>'
-      end
+      # corresponding metric is collected with redis_usage_data { unique_visit_service.unique_visits_for(targets: :analytics) }
+      let(:key_path) { 'analytics_unique_visits.analytics_unique_visits_for_any_target' }
+      let(:name_suggestion) { '<please fill metric name>' }
+
+      it_behaves_like 'name suggestion'
     end
 
     context 'for alt_usage_data metrics' do
-      it 'return correct name' do
-        expect(described_class.generate('settings.operating_system')).to eq '<please fill metric name>'
-      end
+      # corresponding metric is collected with alt_usage_data(fallback: nil) { operating_system }
+      let(:key_path) { 'settings.operating_system' }
+      let(:name_suggestion) { '<please fill metric name>' }
+
+      it_behaves_like 'name suggestion'
     end
   end
 end
