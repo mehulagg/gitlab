d68fdd8ba996841a36e7b8770dc387246a26675c
Miguel Rincon <mrincon@gitlab.com>
2021-03-05 14:16:13 +0100

    Improve ref name

--- app/assets/javascripts/pipelines/components/pipelines_list/stage.vue
+++ app/assets/javascripts/pipelines/components/pipelines_list/stage.vue
@@ -79,7 +79,7 @@ export default {
           this.isLoading = false;
         })
         .catch(() => {
-          this.$refs.stageGlDropdown.hide();
+          this.$refs.dropdown.hide();
           this.isLoading = false;
 
           Flash(__('Something went wrong on our end.'));
@@ -90,7 +90,7 @@ export default {
     },
     pipelineActionRequestComplete() {
       // close the dropdown in MR widget
-      this.$refs.stageGlDropdown.hide();
+      this.$refs.dropdown.hide();
 
       // warn the pipelines table to update
       this.$emit('pipelineActionRequestComplete');
@@ -101,7 +101,7 @@ export default {
 
 <template>
   <gl-dropdown
-    ref="stageGlDropdown"
+    ref="dropdown"
     v-gl-tooltip.hover
     data-testid="mini-pipeline-graph-dropdown"
     :title="stage.title"
