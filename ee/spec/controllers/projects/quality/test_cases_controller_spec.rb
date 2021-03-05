a21021ebc542dc8ea34e0e060cfcd9757617e470
Felipe Artur <felipefac@gmail.com>
2021-03-05 15:38:34 -0300

    Fix TestCasesController permission inconsistency

--- ee/spec/controllers/projects/quality/test_cases_controller_spec.rb
+++ ee/spec/controllers/projects/quality/test_cases_controller_spec.rb
@@ -4,13 +4,11 @@ require 'spec_helper'
 
 RSpec.describe Projects::Quality::TestCasesController do
   let_it_be(:project) { create(:project) }
-  let_it_be(:user) { create(:user) }
 
   shared_examples_for 'test case action' do |template|
     context 'with authorized user' do
       before do
-        project.add_developer(user)
-        sign_in(user)
+        sign_in(authorized_user)
       end
 
       context 'when feature is available' do
@@ -41,7 +39,7 @@ RSpec.describe Projects::Quality::TestCasesController do
 
     context 'with unauthorized user' do
       before do
-        sign_in(user)
+        sign_in(unauthorized_user)
       end
 
       context 'when feature is available' do
@@ -69,18 +67,26 @@ RSpec.describe Projects::Quality::TestCasesController do
 
   describe 'GET' do
     describe '#index' do
+      let_it_be(:authorized_user) { create(:project_member, :guest, project: project).user }
+      let_it_be(:unauthorized_user) { create(:user) }
+
       subject { get :index, params: { namespace_id: project.namespace, project_id: project } }
 
       it_behaves_like 'test case action', :index
     end
 
     describe '#new' do
+      let_it_be(:authorized_user) { create(:project_member, :reporter, project: project).user }
+      let_it_be(:unauthorized_user) { create(:user) }
+
       subject { get :new, params: { namespace_id: project.namespace, project_id: project } }
 
       it_behaves_like 'test case action', :new
     end
 
     describe '#show' do
+      let_it_be(:authorized_user) { create(:project_member, :reporter, project: project).user }
+      let_it_be(:unauthorized_user) { create(:user) }
       let_it_be(:test_case) { create(:quality_test_case, project: project) }
 
       subject { get :show, params: { namespace_id: project.namespace, project_id: project, id: test_case } }
@@ -90,8 +96,7 @@ RSpec.describe Projects::Quality::TestCasesController do
       context 'when feature is enabled and user has access' do
         before do
           stub_licensed_features(quality_management: true)
-          project.add_developer(user)
-          sign_in(user)
+          sign_in(authorized_user)
         end
 
         it 'assigns test case related variables' do
