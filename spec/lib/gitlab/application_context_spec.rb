e575816e7d1526414a61e3c7558433d1492b6ede
Alex Buijs <abuijs@gitlab.com>
2021-03-05 16:40:55 +0100

    Refactor Labkit::Context to Gitlab::ApplicationContext
    From Labkit::Context.with_context
    To Gitlab::ApplicationContext.with_base_context

--- spec/lib/gitlab/application_context_spec.rb
+++ spec/lib/gitlab/application_context_spec.rb
@@ -27,6 +27,20 @@ RSpec.describe Gitlab::ApplicationContext do
     end
   end
 
+  describe '.with_base_context' do
+    it 'yields the block' do
+      expect { |b| described_class.with_base_context({}, &b) }.to yield_control
+    end
+
+    it 'passes the attributes unaltered on to labkit' do
+      attrs = { foo: :bar }
+
+      expect(Labkit::Context).to receive(:with_context).with(attrs)
+
+      described_class.with_base_context(attrs) {}
+    end
+  end
+
   describe '.push' do
     it 'passes the expected context on to labkit' do
       fake_proc = duck_type(:call)
