7605e872a203c97c874490518c0a243bd2acf796
Doug Stull <dstull@gitlab.com>
2021-03-05 16:02:48 -0500

    Change invite member modal to use bold for project/group name
    - instead of all caps...better design.

--- app/assets/javascripts/invite_members/components/invite_members_modal.vue
+++ app/assets/javascripts/invite_members/components/invite_members_modal.vue
@@ -76,7 +76,7 @@ export default {
       const inviteTo = this.isProject ? 'toProject' : 'toGroup';
 
       return sprintf(this.$options.labels[this.inviteeType][inviteTo].introText, {
-        name: this.name.toUpperCase(),
+        name: `%{strongStart}${this.name}%{strongEnd}`,
       });
     },
     toastOptions() {
@@ -215,10 +215,10 @@ export default {
       searchField: s__('InviteMembersModal|GitLab member or Email address'),
       placeHolder: s__('InviteMembersModal|Search for members to invite'),
       toGroup: {
-        introText: s__("InviteMembersModal|You're inviting members to the %{name} group"),
+        introText: s__("InviteMembersModal|You're inviting members to the %{name} group."),
       },
       toProject: {
-        introText: s__("InviteMembersModal|You're inviting members to the %{name} project"),
+        introText: s__("InviteMembersModal|You're inviting members to the %{name} project."),
       },
     },
     group: {
@@ -226,10 +226,10 @@ export default {
       searchField: s__('InviteMembersModal|Select a group to invite'),
       placeHolder: s__('InviteMembersModal|Search for a group to invite'),
       toGroup: {
-        introText: s__("InviteMembersModal|You're inviting a group to the %{name} group"),
+        introText: s__("InviteMembersModal|You're inviting a group to the %{name} group."),
       },
       toProject: {
-        introText: s__("InviteMembersModal|You're inviting a group to the %{name} project"),
+        introText: s__("InviteMembersModal|You're inviting a group to the %{name} project."),
       },
     },
     accessLevel: s__('InviteMembersModal|Choose a role permission'),
@@ -253,7 +253,13 @@ export default {
     :header-close-label="$options.labels.headerCloseLabel"
   >
     <div>
-      <p ref="introText">{{ introText }}</p>
+      <p ref="introText">
+        <gl-sprintf :message="introText">
+          <template #strong="{ content }">
+            <strong>{{ content }}</strong>
+          </template>
+        </gl-sprintf>
+      </p>
 
       <label :id="$options.membersTokenSelectLabelId" class="gl-font-weight-bold gl-mt-5">{{
         $options.labels[inviteeType].searchField
