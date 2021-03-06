58833c656d0c7319d4b0162484f41c316aaa4b3c
Donald Cook <dcook@gitlab.com>
2021-03-05 17:29:45 -0800

    Added test and template tweaks
    Also fixed clipboard_button tooltip going offscreen

--- app/assets/javascripts/sidebar/components/reference/sidebar_reference_widget.vue
+++ app/assets/javascripts/sidebar/components/reference/sidebar_reference_widget.vue
@@ -1,23 +1,16 @@
 <script>
-import { GlIcon, GlLoadingIcon, GlTooltipDirective } from '@gitlab/ui';
+import { GlLoadingIcon, GlTooltipDirective } from '@gitlab/ui';
 import { __ } from '~/locale';
 import { referenceQueries } from '~/sidebar/constants';
-import SidebarEditableItem from '~/sidebar/components/sidebar_editable_item.vue';
 import ClipboardButton from '~/vue_shared/components/clipboard_button.vue';
 
 export default {
   directives: {
     GlTooltip: GlTooltipDirective,
   },
-  tracking: {
-    event: 'click_button',
-    label: 'right_sidebar',
-    property: 'reference',
-  },
   components: {
-    GlIcon,
-    GlLoadingIcon,
     ClipboardButton,
+    GlLoadingIcon,
   },
   inject: ['fullPath', 'iid'],
   props: {
@@ -28,6 +21,7 @@ export default {
   },
   data() {
     return {
+      reference: '',
       title: __('Copy reference'),
       text: __('Reference'),
     };
@@ -53,35 +47,35 @@ export default {
       return this.$apollo.queries.reference.loading;
     },
   },
+  methods: {
+    updateTooltip() {
+      this.title = __('Copied!');
+    },
+  },
 };
 </script>
 
 <template>
-  <sidebar-editable-item
-    ref="editable"
-    title="text"
-    :tracking="$options.tracking"
-    :loading="isLoading"
-    class="block confidentiality"
-  >
-    <template #collapsed>
-      <div v-gl-tooltip.viewport.left :title="title" class="sidebar-collapsed-icon">
-        <gl-icon :size="16" name="copy-to-clipboard" class="sidebar-item-icon inline" />
-      </div>
-    </template>
-    <template #default>
-      <div class="cross-project-reference hide-collapsed">
-        <span>
-          {{ text }}: {{ reference }}
-          <gl-loading-icon v-if="isLoading" inline :label="text" />
-        </span>
-        <clipboard-button
-          :title="title"
-          :text="reference"
-          css-class="btn-clipboard btn-link gl-line-height-24! gl-display-block"
-          tooltip-placement="left"
-        />
-      </div>
-    </template>
-  </sidebar-editable-item>
+  <div class="project-reference sub-block">
+    <clipboard-button
+      :title="title"
+      :text="reference"
+      css-class="btn-clipboard btn-link sidebar-collapsed-icon dont-change-state"
+      tooltip-placement="left"
+      @click="updateTooltip()"
+    />
+    <div class="cross-project-reference hide-collapsed">
+      <span>
+        {{ text }}: {{ reference }}
+        <gl-loading-icon v-if="isLoading" inline :label="text" />
+      </span>
+      <clipboard-button
+        :title="title"
+        :text="reference"
+        css-class="btn-clipboard btn-link gl-line-height-24! gl-display-block"
+        tooltip-placement="left"
+        @click="updateTooltip()"
+      />
+    </div>
+  </div>
 </template>
--- spec/frontend/sidebar/components/reference/sidebar_reference_widget_spec.js
+++ spec/frontend/sidebar/components/reference/sidebar_reference_widget_spec.js
@@ -1,11 +1,41 @@
-import { GlIcon } from '@gitlab/ui';
-import { shallowMount } from '@vue/test-utils';
+import { shallowMount, createLocalVue } from '@vue/test-utils';
+import VueApollo from 'vue-apollo';
+import createMockApollo from 'helpers/mock_apollo_helper';
+import waitForPromises from 'helpers/wait_for_promises';
 import SidebarReferenceWidget from '~/sidebar/components/reference/sidebar_reference_widget.vue';
+import issueReferenceQuery from '~/sidebar/queries/issue_reference.query.graphql';
+import { issueReferenceResponse } from '../../mock_data';
+
+const localVue = createLocalVue();
+localVue.use(VueApollo);
 
 describe('Sidebar Reference Widget', () => {
-  it('shows loading icon when query is loading', () => {
-  });
+  let wrapper;
+  let fakeApollo;
+  const referenceText = 'reference';
 
-  it('displays reference', () => {
+  const createComponent = ({
+    referenceQueryHandler = jest.fn().mockResolvedValue(issueReferenceResponse(referenceText)),
+  } = {}) => {
+    fakeApollo = createMockApollo([[issueReferenceQuery, referenceQueryHandler]]);
+
+    wrapper = shallowMount(SidebarReferenceWidget, {
+      localVue,
+      apolloProvider: fakeApollo,
+      provide: {
+        fullPath: 'group/project',
+        iid: '1',
+      },
+      propsData: {
+        issuableType: 'issue',
+      },
+    });
+  };
+
+  it('displays the reference text', async () => {
+    createComponent();
+
+    await waitForPromises();
+    expect(wrapper.text()).toContain(referenceText);
   });
 });
