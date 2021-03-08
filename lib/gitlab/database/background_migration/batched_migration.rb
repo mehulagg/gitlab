7a4554b1ff70abd43d746f3a26add8ba088f0732
pbair <pbair@gitlab.com>
2021-03-08 15:45:45 -0500

    Store only bare class names for batched migrations
    When storing `job_class_name` and `batch_class_name` values in the
    database, save only the bare class name, rather than the fully qualified
    name. The enclosing module will be dynamically attached to the class
    name in the application, when the class is requested.

--- lib/gitlab/database/background_migration/batched_migration.rb
+++ lib/gitlab/database/background_migration/batched_migration.rb
@@ -4,6 +4,9 @@ module Gitlab
   module Database
     module BackgroundMigration
       class BatchedMigration < ActiveRecord::Base # rubocop:disable Rails/ApplicationRecord
+        JOB_CLASS_MODULE = 'Gitlab::BackgroundMigration'
+        BATCH_CLASS_MODULE = "#{JOB_CLASS_MODULE}::BatchingStrategies".freeze
+
         self.table_name = :batched_background_migrations
 
         has_many :batched_jobs, foreign_key: :batched_background_migration_id
@@ -37,19 +40,19 @@ module Gitlab
         end
 
         def job_class
-          job_class_name.constantize
+          "#{JOB_CLASS_MODULE}::#{job_class_name}".constantize
         end
 
         def batch_class
-          batch_class_name.constantize
+          "#{BATCH_CLASS_MODULE}::#{batch_class_name}".constantize
         end
 
         def job_class_name=(class_name)
-          write_attribute(:job_class_name, self.class.remove_toplevel_prefix(class_name))
+          write_attribute(:job_class_name, class_name.demodulize)
         end
 
         def batch_class_name=(class_name)
-          write_attribute(:batch_class_name, self.class.remove_toplevel_prefix(class_name))
+          write_attribute(:batch_class_name, class_name.demodulize)
         end
       end
     end
