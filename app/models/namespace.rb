1fb3d25dfb2005ae90dc6ae4c0bc1ffd2c56bb20
Adam Hegyi <ahegyi@gitlab.com>
2021-03-09 10:30:16 +0100

    Move recursive namespace query to INNER JOIN
    Move recursive namespace query to INNER JOIN

--- app/models/namespace.rb
+++ app/models/namespace.rb
@@ -260,8 +260,16 @@ class Namespace < ApplicationRecord
   # Includes projects from this namespace and projects from all subgroups
   # that belongs to this namespace
   def all_projects
-    namespace = user? ? self : self_and_descendants
-    Project.where(namespace: namespace)
+    if Feature.enabled?(:recursive_namesapce_lookup_as_inner_join)
+      if user?
+        Project.where(namespace: self)
+      else
+        Project.joins("INNER JOIN (#{self_and_descendants.select(:id).to_sql}) namespaces on namespaces.id=projects.namespace_id")
+      end
+    else
+      namespace = user? ? self : self_and_descendants
+      Project.where(namespace: namespace)
+    end
   end
 
   # Includes pipelines from this namespace and pipelines from all subgroups
