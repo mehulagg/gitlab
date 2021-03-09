aefb3f948f576640634e547d85e3b0540e97f405
Zack Cuddy <zcuddy@gitlab.com>
2021-03-09 15:29:36 -0600

    Geo Node Status 2.0 - Replication Summary
    This change replaces the placeholder
    text for the Secondary replication
    summary with actual components.
    
    This change leaves placeholder text
    for the Relication Counts. This will
    be implemented in the next MR.
    
    This change also leaves placeholder text
    for the replication details below.

--- ee/spec/frontend/geo_nodes_beta/components/details/secondary_node/geo_node_replication_summary_spec.js
+++ ee/spec/frontend/geo_nodes_beta/components/details/secondary_node/geo_node_replication_summary_spec.js
@@ -1,8 +1,14 @@
 import { GlButton } from '@gitlab/ui';
 import { mount } from '@vue/test-utils';
+import Vue from 'vue';
+import Vuex from 'vuex';
+import GeoNodeReplicationStatus from 'ee/geo_nodes_beta/components/details/secondary_node/geo_node_replication_status.vue';
 import GeoNodeReplicationSummary from 'ee/geo_nodes_beta/components/details/secondary_node/geo_node_replication_summary.vue';
+import GeoNodeSyncSettings from 'ee/geo_nodes_beta/components/details/secondary_node/geo_node_sync_settings.vue';
 import { MOCK_NODES } from 'ee_jest/geo_nodes_beta/mock_data';
 import { extendedWrapper } from 'helpers/vue_test_utils_helper';
+
+Vue.use(Vuex);
 
 describe('GeoNodeReplicationSummary', () => {
   let wrapper;
@@ -11,9 +17,18 @@ describe('GeoNodeReplicationSummary', () => {
     node: MOCK_NODES[1],
   };
 
-  const createComponent = (initialState, props) => {
+  const createComponent = (props) => {
+    const store = new Vuex.Store({
+      // Mocking these since we are using full `mount`.
+      getters: {
+        verificationInfo: () => () => [],
+        syncInfo: () => () => [],
+      },
+    });
+
     wrapper = extendedWrapper(
       mount(GeoNodeReplicationSummary, {
+        store,
         propsData: {
           ...defaultProps,
           ...props,
@@ -27,9 +42,9 @@ describe('GeoNodeReplicationSummary', () => {
   });
 
   const findGlButton = () => wrapper.findComponent(GlButton);
-  const findGeoNodeReplicationStatus = () => wrapper.findByTestId('replication-status');
+  const findGeoNodeReplicationStatus = () => wrapper.findComponent(GeoNodeReplicationStatus);
   const findGeoNodeReplicationCounts = () => wrapper.findByTestId('replication-counts');
-  const findGeoNodeSyncSettings = () => wrapper.findByTestId('sync-settings');
+  const findGeoNodeSyncSettings = () => wrapper.findComponent(GeoNodeSyncSettings);
 
   describe('template', () => {
     beforeEach(() => {
