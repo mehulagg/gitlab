b83f6aa731817720fccd9d0fcd44c885dcf548b3
Himanshu Kapoor <hkapoor@gitlab.com>
2021-03-11 18:00:39 +0100

    Add tests for search settings added to project pages
    Search settings is now added in Integrations, Webhooks, Access Tokens
    and Pages

--- spec/features/projects/settings/user_searches_in_settings_spec.rb
+++ spec/features/projects/settings/user_searches_in_settings_spec.rb
@@ -16,6 +16,30 @@ RSpec.describe 'User searches project settings', :js do
     it_behaves_like 'can search settings with feature flag check', 'Naming', 'Visibility'
   end
 
+  context 'in Integrations page' do
+    before do
+      visit project_settings_integrations_path(project)
+    end
+
+    it_behaves_like 'can highlight results', 'third-party applications'
+  end
+
+  context 'in Webhooks page' do
+    before do
+      visit project_hooks_path(project)
+    end
+
+    it_behaves_like 'can highlight results', 'Secret token'
+  end
+
+  context 'in Access Tokens page' do
+    before do
+      visit project_settings_access_tokens_path(project)
+    end
+
+    it_behaves_like 'can highlight results', 'Expires at'
+  end
+
   context 'in Repository page' do
     before do
       visit project_settings_repository_path(project)
@@ -38,5 +62,13 @@ RSpec.describe 'User searches project settings', :js do
     end
 
     it_behaves_like 'can search settings', 'Alerts', 'Error tracking'
+  end
+
+  context 'in Webhooks page' do
+    before do
+      visit project_pages_path(project)
+    end
+
+    it_behaves_like 'can highlight results', 'static websites'
   end
 end
