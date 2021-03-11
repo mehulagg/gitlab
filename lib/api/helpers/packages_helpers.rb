9af9dda862eaa58c12fd650320ef92015df89a25
Alishan Ladhani <aladhani@gitlab.com>
2021-03-11 11:28:30 -0500

    Update package helper

--- lib/api/helpers/packages_helpers.rb
+++ lib/api/helpers/packages_helpers.rb
@@ -50,7 +50,7 @@ module API
 
       def track_package_event(event_name, scope, **args)
         ::Packages::CreateEventService.new(nil, current_user, event_name: event_name, scope: scope).execute
-        track_event(event_name, **args)
+        ::Gitlab::Tracking.event(args[:category] || self.options[:for].name, event_name.to_s, **args)
       end
     end
   end
