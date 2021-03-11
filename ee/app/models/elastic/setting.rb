81a3a6005cca66d7b9e2043973a71941000aae2b
Dmitry Gruzd <dgruzd@gitlab.com>
2021-03-11 17:04:13 +0300

    fixup! Advanced Search: standalone shard/replica settings

--- ee/app/models/elastic/setting.rb
+++ ee/app/models/elastic/setting.rb
@@ -6,11 +6,21 @@ module Elastic
 
     validates :number_of_replicas, :number_of_shards, presence: true
 
-    def self.current
-      first || create!(
-        number_of_replicas: { default: 1 },
-        number_of_shards: { default: 5 }
-      )
+    class << self
+      def current
+        first || create!(
+          number_of_replicas: { default: 1 },
+          number_of_shards: { default: 5 }
+        )
+      end
+
+      def number_of_shards
+        current.number_of_shards.with_indifferent_access
+      end
+
+      def number_of_replicas
+        current.number_of_replicas.with_indifferent_access
+      end
     end
   end
 end
