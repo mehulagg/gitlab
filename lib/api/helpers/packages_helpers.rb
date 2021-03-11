9beb4669b3eea59bedf7732777d1dc6caae55ccb
Alishan Ladhani <aladhani@gitlab.com>
2021-03-11 15:34:25 -0500

    Remove extra category argument

--- lib/api/helpers/packages_helpers.rb
+++ lib/api/helpers/packages_helpers.rb
@@ -50,7 +50,7 @@ module API
 
       def track_package_event(event_name, scope, **args)
         ::Packages::CreateEventService.new(nil, current_user, event_name: event_name, scope: scope).execute
-        ::Gitlab::Tracking.event(args[:category] || self.options[:for].name, event_name.to_s, **args)
+        ::Gitlab::Tracking.event(args.delete(:category) || self.options[:for].name, event_name.to_s, **args)
       end
     end
   end
