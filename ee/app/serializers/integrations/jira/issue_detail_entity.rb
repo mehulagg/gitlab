3d3d12e347b424a99a6ac66d77e5d9dcbeaf7218
Arturo Herrero <arturo.herrero@gmail.com>
2021-03-12 17:34:01 +0000

    Jira Issue: Fix comments' datetime

--- ee/app/serializers/integrations/jira/issue_detail_entity.rb
+++ ee/app/serializers/integrations/jira/issue_detail_entity.rb
@@ -21,10 +21,28 @@ module Integrations
           {
             id: comment['id'],
             body_html: Banzai::Pipeline::JiraGfmPipeline.call(comment['body'], project: project)[:output].to_html,
-            created_at: comment['created'].to_datetime.utc,
-            updated_at: comment['updated'].to_datetime.utc,
+            created_at: parse_time(comment['created'], comment['author']['timeZone']),
+            updated_at: parse_time(comment['updated'], comment['author']['timeZone']),
             author: jira_user(comment['author'])
           }
+        end
+      end
+
+      private
+
+      # datetime can be in any of the following formats:
+      #   "Just now"
+      #   "2 days ago 10:05 AM"
+      #   "24/Feb/21 7:19 AM"
+      #   "08/Jul/20 12:13 PM"
+      #   "2021-02-12T16:57:36.175+0000"
+      def parse_time(datetime, timezone)
+        Time.use_zone(timezone) do
+          # Chronic cannot parse a datetime in the format of "24/Feb/21 7:19 AM",
+          # we have to remove the AM/PM suffixes.
+          datetime.delete_suffix!(' AM')
+          datetime.delete_suffix!(' PM')
+          Chronic.parse(datetime).utc
         end
       end
     end
