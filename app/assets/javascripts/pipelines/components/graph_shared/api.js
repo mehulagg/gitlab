dd19c24985a784a87684931375a9a69aab65f9db
Sarah GP <sarah.groff.palermo@gmail.com>
2021-03-12 18:31:28 +0100

    Adjust link performance

--- app/assets/javascripts/pipelines/components/graph_shared/api.js
+++ app/assets/javascripts/pipelines/components/graph_shared/api.js
@@ -2,7 +2,8 @@ import axios from '~/lib/utils/axios_utils';
 import { reportToSentry } from '../graph/utils';
 
 export const reportPerformance = (path, stats) => {
-  axios.post(path, stats).catch((err) => {
+  console.log('report called');
+  axios.post(path, stats).then(data => console.log(data)).catch((err) => {
     reportToSentry('links_inner_perf', `error: ${err}`);
   });
 };
