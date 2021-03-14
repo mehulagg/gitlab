31615b66a3749392b9392a7504eff2a63982412a
Philip Cunningham <pcunningham@gitlab.com>
2021-03-14 13:50:38 +1100

    Fix issue with dastProfiles.branch GraphQL field
    - Replace stub
    - Update specs
    - Add changelog entry

--- ee/spec/models/dast/branch_spec.rb
+++ ee/spec/models/dast/branch_spec.rb
@@ -3,18 +3,13 @@
 require 'spec_helper'
 
 RSpec.describe Dast::Branch do
-  let_it_be(:project) { create(:project) }
+  let_it_be(:dast_profile) { create(:dast_profile) }
+  let_it_be(:dast_profile_with_repository) { create(:dast_profile, project: create(:project, :repository)) }
 
-  subject { described_class.new(project) }
+  subject { described_class.new(dast_profile) }
 
   describe 'instance methods' do
     context 'when the associated project does not have a repository' do
-      describe '#name' do
-        it 'returns nil' do
-          expect(subject.name).to be_nil
-        end
-      end
-
       describe '#exists' do
         it 'returns false' do
           expect(subject.exists).to eq(false)
@@ -22,18 +17,40 @@ RSpec.describe Dast::Branch do
       end
     end
 
-    context 'when the associated project has a repository' do
-      let_it_be(:project) { create(:project, :repository) }
+    context 'when profile.branch_name is nil' do
+      let_it_be(:dast_profile) { create(:dast_profile, branch_name: nil) }
 
       describe '#name' do
-        it 'returns the default_branch' do
-          expect(subject.name).to eq(project.default_branch)
+        it 'returns nil' do
+          expect(subject.name).to be_nil
         end
       end
 
-      describe '#exists' do
-        it 'returns true' do
-          expect(subject.exists).to eq(true)
+      context 'when the associated project has a repository' do
+        subject { described_class.new(dast_profile_with_repository) }
+
+        describe '#exists' do
+          it 'returns true' do
+            expect(subject.exists).to eq(true)
+          end
+        end
+      end
+    end
+
+    context 'when profile.branch_name is not nil' do
+      describe '#name' do
+        it 'returns profile.branch_name' do
+          expect(subject.name).to eq(dast_profile.branch_name)
+        end
+      end
+
+      context 'when the branch exists in the associated repository' do
+        subject { described_class.new(dast_profile_with_repository) }
+
+        describe '#exists' do
+          it 'returns true' do
+            expect(subject.exists).to eq(true)
+          end
         end
       end
     end
