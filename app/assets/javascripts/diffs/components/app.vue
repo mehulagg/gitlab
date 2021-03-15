f302a9298fa49b04378d1f795712e3d1cf89a1c5
mfluharty <mfluharty@gitlab.com>
2021-03-15 14:59:16 -0600

    Render codequality badge conditionally per file
    Pass file-level data to diff file
    Pass boolean to diff file header
    Render badge if file has code quality changes

--- app/assets/javascripts/diffs/components/app.vue
+++ app/assets/javascripts/diffs/components/app.vue
@@ -179,7 +179,12 @@ export default {
       'viewDiffsFileByFile',
       'mrReviews',
     ]),
-    ...mapGetters('diffs', ['whichCollapsedTypes', 'isParallelView', 'currentDiffIndex']),
+    ...mapGetters('diffs', [
+      'whichCollapsedTypes',
+      'isParallelView',
+      'currentDiffIndex',
+      'fileCodequalityDiff',
+    ]),
     ...mapGetters(['isNotesFetched', 'getNoteableData']),
     diffs() {
       if (!this.viewDiffsFileByFile) {
@@ -521,6 +526,7 @@ export default {
               :help-page-path="helpPagePath"
               :can-current-user-fork="canCurrentUserFork"
               :view-diffs-file-by-file="viewDiffsFileByFile"
+              :codequality-diff="fileCodequalityDiff(file.file_path)"
             />
             <div
               v-if="showFileByFileNavigation"
