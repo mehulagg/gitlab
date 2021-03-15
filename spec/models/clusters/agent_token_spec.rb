154bebe2565abbdcec216645ea09efb2fc3fe024
Emily Ring <ering@gitlab.com>
2021-03-15 13:01:12 -0400

    Add tracking method to cluster agent token
    Add track_usage to cluster agent token model and kas API.
    Add last_used_at to GraphQl.
    Update associated tests and docs.

--- spec/models/clusters/agent_token_spec.rb
+++ spec/models/clusters/agent_token_spec.rb
@@ -24,4 +24,53 @@ RSpec.describe Clusters::AgentToken do
       expect(agent_token.token.length).to be >= 50
     end
   end
+
+  describe '#track_usage' do
+    let(:agent_token) { create(:cluster_agent_token) }
+
+    subject { agent_token.track_usage }
+
+    context 'when last_used_at was updated recently' do
+      before do
+        agent_token.update!(last_used_at: 10.minutes.ago)
+      end
+
+      it 'updates cache but not database' do
+        expect_redis_update
+
+        expect { subject }.not_to change { agent_token.reload.read_attribute(:last_used_at) }
+      end
+    end
+
+    context 'when last_used_at was not updated recently' do
+      it 'updates cache and database' do
+        expect_redis_update
+        does_db_update
+      end
+
+      context 'with invalid token' do
+        before do
+          agent_token.description = SecureRandom.hex(2000)
+        end
+
+        it 'still updates caches and database' do
+          expect(agent_token).to be_invalid
+
+          expect_redis_update
+          does_db_update
+        end
+      end
+    end
+
+    def expect_redis_update
+      Gitlab::Redis::Cache.with do |redis|
+        redis_key = agent_token.send(:cache_attribute_key)
+        expect(redis).to receive(:set).with(redis_key, anything, any_args)
+      end
+    end
+
+    def does_db_update
+      expect { subject }.to change { agent_token.reload.read_attribute(:last_used_at) }
+    end
+  end
 end
