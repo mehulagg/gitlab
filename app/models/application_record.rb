32cb1485f14efe75771b138ed25aca7b7882a748
Quang-Minh Nguyen <qmnguyen@gitlab.com>
2021-03-15 17:05:36 +0700

    Rename EE::ApplicationRecord to EE::ApplicationRecordHelpers
    Issue https://gitlab.com/gitlab-org/gitlab/-/issues/322133

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -84,4 +84,4 @@ class ApplicationRecord < ActiveRecord::Base
   end
 end
 
-ApplicationRecord.prepend_if_ee('EE::ApplicationRecord')
+ApplicationRecord.prepend_if_ee('EE::ApplicationRecordHelpers')
