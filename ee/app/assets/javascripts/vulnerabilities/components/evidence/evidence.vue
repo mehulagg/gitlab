ab4bbd77247ef52e7cb41c91bc0d94000876f096
Dave Pisek <dpisek@gitlab.com>
2021-03-15 21:45:56 +1100

    Add generic report data parent component
    * Adds component with collapsible header

---
+++ ee/app/assets/javascripts/vulnerabilities/components/evidence/evidence.vue
@@ -0,0 +1,48 @@
+<script>
+import { GlCollapse, GlIcon } from '@gitlab/ui';
+
+export default {
+  components: {
+    GlCollapse,
+    GlIcon,
+  },
+  props: {
+    details: {
+      type: Object,
+      required: true,
+    },
+  },
+  data() {
+    return {
+      showSection: false,
+    };
+  },
+  computed: {
+    headerIcon() {
+      return this.showSection ? 'chevron-down' : 'chevron-right';
+    },
+  },
+  methods: {
+    toggleShowSection() {
+      this.showSection = !this.showSection;
+    },
+  },
+};
+</script>
+
+<template>
+  <section>
+    <header>
+      <h3
+        class="gl-display-inline-flex gl-align-items-center gl-mb-0! gl-cursor-pointer"
+        @click="toggleShowSection"
+      >
+        <gl-icon name="angle-right" class="gl-mr-2" :class="{ 'gl-rotate-90': showSection }" />
+        {{ s__('Vulnerability|Evidence') }}
+      </h3>
+    </header>
+    <gl-collapse :visible="showSection">
+      <div>{{ __('Something is here!') }}</div>
+    </gl-collapse>
+  </section>
+</template>
