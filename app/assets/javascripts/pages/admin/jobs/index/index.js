26d1b24b0230641c6fa932db7eda8de8d08a63b6
Jannik Lehmann <jlehmann@gitlab.com>
2021-03-15 14:53:18 +0000

    Remove obsolete DOMContentLoadedEventListener

--- app/assets/javascripts/pages/admin/jobs/index/index.js
+++ app/assets/javascripts/pages/admin/jobs/index/index.js
@@ -5,30 +5,28 @@ import stopJobsModal from './components/stop_jobs_modal.vue';
 
 Vue.use(Translate);
 
-document.addEventListener('DOMContentLoaded', () => {
-  const buttonId = 'js-stop-jobs-button';
-  const modalId = 'stop-jobs-modal';
-  const stopJobsButton = document.getElementById(buttonId);
-  if (stopJobsButton) {
-    // eslint-disable-next-line no-new
-    new Vue({
-      el: `#js-${modalId}`,
-      components: {
-        stopJobsModal,
-      },
-      mounted() {
-        stopJobsButton.classList.remove('disabled');
-        stopJobsButton.addEventListener('click', () => {
-          this.$root.$emit(BV_SHOW_MODAL, modalId, `#${buttonId}`);
-        });
-      },
-      render(createElement) {
-        return createElement(modalId, {
-          props: {
-            url: stopJobsButton.dataset.url,
-          },
-        });
-      },
-    });
-  }
-});
+const buttonId = 'js-stop-jobs-button';
+const modalId = 'stop-jobs-modal';
+const stopJobsButton = document.getElementById(buttonId);
+if (stopJobsButton) {
+  // eslint-disable-next-line no-new
+  new Vue({
+    el: `#js-${modalId}`,
+    components: {
+      stopJobsModal,
+    },
+    mounted() {
+      stopJobsButton.classList.remove('disabled');
+      stopJobsButton.addEventListener('click', () => {
+        this.$root.$emit(BV_SHOW_MODAL, modalId, `#${buttonId}`);
+      });
+    },
+    render(createElement) {
+      return createElement(modalId, {
+        props: {
+          url: stopJobsButton.dataset.url,
+        },
+      });
+    },
+  });
+}
