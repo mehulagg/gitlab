6c8ecb1c0651078fdff1422a9b384fb34e6f6cdb
mfluharty <mfluharty@gitlab.com>
2021-03-15 15:08:24 -0600

    Make the badge a link to the codequality MR widget

--- ee/app/assets/javascripts/diffs/components/code_quality_badge.vue
+++ ee/app/assets/javascripts/diffs/components/code_quality_badge.vue
@@ -1,5 +1,7 @@
 <script>
 import { GlBadge, GlTooltipDirective } from '@gitlab/ui';
+import { scrollToElement, historyPushState } from '~/lib/utils/common_utils';
+import { stripFinalUrlSegment } from '~/lib/utils/url_utility';
 import { __ } from '~/locale';
 import glFeatureFlagsMixin from '~/vue_shared/mixins/gl_feature_flags_mixin';
 
@@ -11,6 +13,19 @@ export default {
     GlTooltip: GlTooltipDirective,
   },
   mixins: [glFeatureFlagsMixin()],
+  methods: {
+    onClick() {
+      // switch to the MR "Overview" tab and scroll to the codequality widget
+      window.mrTabs.eventHub.$once('MergeRequestTabChange', () => {
+        setTimeout(() => scrollToElement('.js-codequality-widget'), 0);
+      });
+      window.mrTabs.tabShown('show');
+
+      // update browser location/history to reflect the tab change
+      const newUrl = stripFinalUrlSegment(window.location.href);
+      historyPushState(newUrl);
+    },
+  },
   i18n: {
     badgeTitle: __('Code Quality'),
     badgeTooltip: __(
@@ -21,7 +36,9 @@ export default {
 </script>
 
 <template>
-  <gl-badge v-gl-tooltip :title="$options.i18n.badgeTooltip" icon="information">
-    {{ $options.i18n.badgeTitle }}
-  </gl-badge>
+  <span @click="onClick">
+    <gl-badge v-gl-tooltip :title="$options.i18n.badgeTooltip" icon="information">
+      {{ $options.i18n.badgeTitle }}
+    </gl-badge>
+  </span>
 </template>
