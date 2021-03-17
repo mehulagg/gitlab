0f3b7145e0b05502fc17f9313603a61207e68a43
Sanad Liaquat (Personal) <sliaquat@gitlab.com>
2021-03-17 14:22:15 +0500

    Added resource and moved page into component

--- qa/qa/specs/features/browser_ui/1_manage/project/project_access_token_spec.rb
+++ qa/qa/specs/features/browser_ui/1_manage/project/project_access_token_spec.rb
@@ -4,7 +4,7 @@ module QA
   RSpec.describe 'Manage' do
     describe 'Project access tokens' do
       it 'allows a user to create and use a project access token', testcase: 'https://gitlab.com/gitlab-org/quality/testcases/-/issues/1705' do
-
+        QA::Resource::ProjectAccessToken.fabricate_via_browser_ui!
       end
     end
   end
