f9700d811074464cd0575215e0eb0eb1e7e09f91
Sanad Liaquat (Personal) <sliaquat@gitlab.com>
2021-03-17 11:11:54 +0500

    Initial commit

---
+++ qa/qa/specs/features/browser_ui/1_manage/project/project_access_token_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+module QA
+  RSpec.describe 'Manage' do
+    describe 'Project access tokens' do
+      it 'allows a user to create and use a project access token', testcase: 'https://gitlab.com/gitlab-org/quality/testcases/-/issues/1705' do
+
+      end
+    end
+  end
+end
