8ade6aba2c3e7b7c6eae99aca8afff1dc0b291d6
Mehmet Emin INAC <minac@gitlab.com>
2021-03-17 17:42:42 +0100

    Refactor analyzer specs

--- spec/lib/gitlab/graphql/query_analyzers/logger_analyzer_spec.rb
+++ spec/lib/gitlab/graphql/query_analyzers/logger_analyzer_spec.rb
@@ -3,11 +3,12 @@
 require 'spec_helper'
 
 RSpec.describe Gitlab::Graphql::QueryAnalyzers::LoggerAnalyzer do
-  subject { described_class.new }
+  let(:analyzer) { described_class.new }
 
   describe '#initial_value' do
-    it 'filters out sensitive variables' do
-      doc = GraphQL.parse <<-GRAPHQL
+    let(:query) { GraphQL::Query.new(GitlabSchema, document: document, context: {}, variables: { body: "some note" }) }
+    let(:document) do
+      GraphQL.parse <<-GRAPHQL
         mutation createNote($body: String!) {
           createNote(input: {noteableId: "1", body: $body}) {
             note {
@@ -16,30 +17,31 @@ RSpec.describe Gitlab::Graphql::QueryAnalyzers::LoggerAnalyzer do
           }
         }
       GRAPHQL
-
-      query = GraphQL::Query.new(GitlabSchema, document: doc, context: {}, variables: { body: "some note" })
-
-      expect(subject.initial_value(query)[:variables]).to eq('{:body=>"[FILTERED]"}')
     end
+
+    subject { analyzer.initial_value(query).fetch(:variables) }
+
+    it { is_expected.to eq('{:body=>"[FILTERED]"}') }
   end
 
   describe '#final_value' do
     let(:monotonic_time_before) { 42 }
     let(:monotonic_time_after) { 500 }
     let(:monotonic_time_duration) { monotonic_time_after - monotonic_time_before }
+    let(:query) { spy(:query) }
+    let(:memo) { analyzer.initial_value(query) }
 
-    it 'returns a duration in seconds' do
+    subject(:final_value) { analyzer.final_value(memo) }
+
+    before do
       allow(GraphQL::Analysis).to receive(:analyze_query).and_return([4, 2, [[], []]])
       allow(Gitlab::Metrics::System).to receive(:monotonic_time).and_return(monotonic_time_before, monotonic_time_after)
       allow(Gitlab::GraphqlLogger).to receive(:info)
+    end
 
-      expected_duration = monotonic_time_duration
-      memo = subject.initial_value(spy('query'))
-
-      subject.final_value(memo)
-
-      expect(memo).to have_key(:duration_s)
-      expect(memo[:duration_s]).to eq(expected_duration)
+    it 'inserts duration in seconds to memo and sets request store' do
+      expect { final_value }.to change { memo[:duration_s] }.to(monotonic_time_duration)
+                            .and change { RequestStore.store[:graphql_logs] }.to([memo])
     end
   end
 end
