13a176c2f0fe8f360866d973437229babbda9f05
mfluharty <mfluharty@gitlab.com>
2021-03-17 12:57:47 -0600

    Sort issues in full codequality report by severity
    unknown, blocker, critical, major, minor, info

--- ee/app/assets/javascripts/codequality_report/store/constants.js
+++ ee/app/assets/javascripts/codequality_report/store/constants.js
@@ -1,2 +1,4 @@
 export const VIEW_EVENT_FEATURE_FLAG = 'usageDataITestingFullCodeQualityReportTotal';
 export const VIEW_EVENT_NAME = 'i_testing_full_code_quality_report_total';
+
+export const SEVERITY_SORT_ORDER = ['unknown', 'blocker', 'critical', 'major', 'minor', 'info'];
