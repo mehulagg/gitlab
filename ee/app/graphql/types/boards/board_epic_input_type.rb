b05e850028e6747f0d82e59da897aba134d2753c
Felipe Artur <felipefac@gmail.com>
2021-03-18 16:28:12 -0300

    Add negated epic board params

--- ee/app/graphql/types/boards/board_epic_input_type.rb
+++ ee/app/graphql/types/boards/board_epic_input_type.rb
@@ -5,6 +5,10 @@ module Types
     class BoardEpicInputType < BoardIssuableInputBaseType
       graphql_name 'EpicFilters'
 
+      argument :not, BoardIssuableInputBaseType,
+               required: false,
+               description: 'List of negated params. Warning: this argument is experimental and a subject to change in future.'
+
       argument :search, GraphQL::STRING_TYPE,
                required: false,
                description: 'Search query for epic title or description.'
