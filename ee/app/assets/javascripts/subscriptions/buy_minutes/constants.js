4275fbec260c295e7cf547e6db7f727cea82083b
mlunoe <michael.lunoe@gmail.com>
2021-03-18 14:29:15 +0100

    Feat(Buy Minutes): add data parsing + error state

---
+++ ee/app/assets/javascripts/subscriptions/buy_minutes/constants.js
@@ -0,0 +1,6 @@
+import { __ } from '~/locale';
+
+export const ERROR_FETCHING_DATA_HEADER = __('Could not get the data properly');
+export const ERROR_FETCHING_DATA_DESCRIPTION = __(
+  'Please try and refresh the page. If the problem persists please contact support.',
+);
--- ee/app/assets/javascripts/subscriptions/buy_minutes/index.js
+++ ee/app/assets/javascripts/subscriptions/buy_minutes/index.js
@@ -1,16 +1,34 @@
 import Vue from 'vue';
+import EnsureData from '~/vue_shared/components/ensure_data.vue';
 import App from './components/app.vue';
+import { ERROR_FETCHING_DATA_HEADER, ERROR_FETCHING_DATA_DESCRIPTION } from './constants';
+import { parseData } from './utils';
 
-export default () => {
-  const el = document.getElementById('js-buy-minutes');
+export default (el) => {
+  const { emptyStateSvgPath, ...dataset } = el.dataset;
 
   return new Vue({
     el,
     components: {
+      EnsureData,
       App,
     },
     render(createElement) {
-      return createElement(App);
+      return createElement(EnsureData, {
+        props: {
+          parse: parseData,
+          data: dataset,
+          shouldLog: false,
+          title: ERROR_FETCHING_DATA_HEADER,
+          description: ERROR_FETCHING_DATA_DESCRIPTION,
+          svgPath: emptyStateSvgPath,
+        },
+        scopedSlots: {
+          app(props) {
+            return createElement({ extends: App, provide: props });
+          },
+        },
+      });
     },
   });
 };
