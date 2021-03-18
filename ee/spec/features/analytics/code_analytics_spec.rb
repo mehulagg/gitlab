466263440e8c5bc96dfe6436de325ff2a6ece290
peterhegman <phegman@gitlab.com>
2021-03-18 12:25:05 -0700

    Hide "New merge request" button "Code review" page from auditors
    Auditors have read-only permissions so can't create merge requests.
    Button returned a 404

--- ee/spec/features/analytics/code_analytics_spec.rb
+++ ee/spec/features/analytics/code_analytics_spec.rb
@@ -2,38 +2,67 @@
 
 require 'spec_helper'
 
-RSpec.describe 'CodeReviewAnalytics Filtered Search', :js do
+RSpec.describe 'CodeReviewAnalytics', :js do
   let_it_be(:user) { create(:user) }
   let_it_be(:project) { create(:project) }
+  let_it_be(:auditor) { create(:user, auditor: true) }
 
   before do
     stub_licensed_features(code_review_analytics: true)
 
-    project.add_reporter(user)
-
-    sign_in(user)
-
-    visit project_analytics_code_reviews_path(project)
+    project.add_developer(user)
   end
 
-  it 'renders the filtered search bar correctly' do
-    page.within('.content-wrapper .content .vue-filtered-search-bar-container') do
-      expect(page).to have_selector('.gl-search-box-by-click')
-      expect(page.find('.gl-filtered-search-term-input')[:placeholder]).to eq('Filter results')
+  context 'filtered search' do
+    before do
+      sign_in(user)
+
+      visit project_analytics_code_reviews_path(project)
+    end
+
+    it 'renders the filtered search bar correctly' do
+      page.within('.content-wrapper .content .vue-filtered-search-bar-container') do
+        expect(page).to have_selector('.gl-search-box-by-click')
+        expect(page.find('.gl-filtered-search-term-input')[:placeholder]).to eq('Filter results')
+      end
+    end
+
+    it 'displays label and milestone in search hint' do
+      page.within('.content-wrapper .content .vue-filtered-search-bar-container') do
+        page.find('.gl-search-box-by-click').click
+
+        expect(page).to have_selector('.gl-filtered-search-suggestion-list')
+
+        hints = page.find_all('.gl-filtered-search-suggestion-list > li')
+
+        expect(hints.length).to eq(2)
+        expect(hints[0]).to have_content('Milestone')
+        expect(hints[1]).to have_content('Label')
+      end
     end
   end
 
-  it 'displays label and milestone in search hint' do
-    page.within('.content-wrapper .content .vue-filtered-search-bar-container') do
-      page.find('.gl-search-box-by-click').click
+  context 'empty state' do
+    it 'shows empty state with "New merge request" button' do
+      sign_in(user)
 
-      expect(page).to have_selector('.gl-filtered-search-suggestion-list')
+      visit project_analytics_code_reviews_path(project)
 
-      hints = page.find_all('.gl-filtered-search-suggestion-list > li')
+      expect(page).to have_content("You don't have any open merge requests")
+      expect(page).to have_link('New merge request')
+    end
 
-      expect(hints.length).to eq(2)
-      expect(hints[0]).to have_content('Milestone')
-      expect(hints[1]).to have_content('Label')
+    context 'when signed in user is an Auditor' do
+      before do
+        sign_in(auditor)
+
+        visit project_analytics_code_reviews_path(project)
+      end
+
+      it 'shows empty state without "New merge request" button' do
+        expect(page).to have_content("You don't have any open merge requests")
+        expect(page).not_to have_link('New merge request')
+      end
     end
   end
 end
