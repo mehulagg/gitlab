aedf97d377558168382cbd4e7c8bf91f4279836b
Dave Pisek <dpisek@gitlab.com>
2021-03-18 11:58:45 +1100

    Move integration specs module name mappers
    This MR moves the module-name-mappers, which are specific to
    the integration suit from jest.config.base to jest.config.integration.

--- jest.config.integration.js
+++ jest.config.integration.js
@@ -1,5 +1,23 @@
-const baseConfig = require('./jest.config.base');
+const IS_EE = require('./config/helpers/is_ee_env');
+const getBaseConfig = require('./jest.config.base');
+
+const baseConfig = getBaseConfig('spec/frontend_integration');
+
+const moduleNameMapperIntegration = {
+  '^test_helpers(/.*)$': '<rootDir>/spec/frontend_integration/test_helpers$1',
+  '^ee_else_ce_test_helpers(/.*)$': '<rootDir>/spec/frontend_integration/test_helpers$1',
+};
+
+if (IS_EE) {
+  Object.assign(moduleNameMapperIntegration, {
+    '^ee_else_ce_test_helpers(/.*)$': '<rootDir>/ee/spec/frontend_integration/test_helpers$1',
+  });
+}
 
 module.exports = {
-  ...baseConfig('spec/frontend_integration'),
+  ...baseConfig,
+  moduleNameMapper: {
+    ...baseConfig.moduleNameMapper,
+    ...moduleNameMapperIntegration,
+  },
 };
