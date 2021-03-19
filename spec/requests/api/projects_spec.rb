0119aa4c49ad2567ee98a030f5f32b88a8d9fb39
Sean McGivern <sean@gitlab.com>
2021-03-19 17:44:13 +0000

    Immediately unlink Rack::Multipart temporary files
    Rack writes files from multipart/form-data requests to disk in a
    temporary file. Rack includes a middleware to clean these up -
    Rack::TempfileReaper - but that won't withstand a process being sent
    SIGKILL. To handle that case, we can immediately unlink the created
    temporary file, which means it will be removed once we're done with it
    or the current process goes away.
    
    For development mode and test mode, we have to ensure that this new
    middleware is before Gitlab::Middleware::Static, otherwise we might not
    get the chance to set our own middleware.
    
    With direct upload configured, GitLab mostly doesn't accept
    multipart/form-data requests in a way where they reach Rack directly -
    they typically go via Workhorse which accelerates them - but there are
    cases where it can happen, and direct upload is still only an option.
    
    To test this manually, we can set `$GITLAB_API_TOKEN_LOCAL` to a
    personal access token for the API in the local environment,
    `$PATH_TO_FILE` to be a path to a (preferably large) file to be
    uploaded, and break the actual saving of uploads (in the default case
    with GDK, stop Minio):
    
        curl -H "Private-Token: $GITLAB_API_TOKEN_LOCAL" \
          -F "file=@$PATH_TO_FILE" \
          http://localhost:3000/api/v4/projects/1/uploads
    
    Once the upload is finished and the request fails, we'll see the file we
    uploaded in `$TMPDIR`:
    
        $ ls -l $TMPDIR/RackMultipart* | awk '{ print $5, $8 }'
        952107008 17:40
    
    With this change, that won't happen: we'll see the file created and
    immediately unlinked, so no matter what happens, it won't stick around
    on disk. (This specific test case is handled by Rack::TempfileReaper in
    later versions of Rack, but it still depends on manual cleanup.)

--- spec/requests/api/projects_spec.rb
+++ spec/requests/api/projects_spec.rb
@@ -1473,8 +1473,24 @@ RSpec.describe API::Projects do
       expect(json_response['alt']).to eq("dk")
       expect(json_response['url']).to start_with("/uploads/")
       expect(json_response['url']).to end_with("/dk.png")
-
       expect(json_response['full_path']).to start_with("/#{project.namespace.path}/#{project.path}/uploads")
+    end
+
+    it "does not leave the temporary file in place after uploading, even when the tempfile reaper does not run" do
+      tempfile = Tempfile.new('foo')
+      path = tempfile.path
+
+      allow_any_instance_of(Rack::TempfileReaper).to receive(:call) do |instance, env|
+        instance.instance_variable_get(:@app).call(env)
+      end
+
+      expect(path).not_to be(nil)
+      expect(Rack::Multipart::Parser::TEMPFILE_FACTORY).to receive(:call).and_return(tempfile)
+
+      post api("/projects/#{project.id}/uploads", user), params: { file: fixture_file_upload("spec/fixtures/dk.png", "image/png") }
+
+      expect(tempfile.path).to be(nil)
+      expect(File.exist?(path)).to be(false)
     end
   end
 
