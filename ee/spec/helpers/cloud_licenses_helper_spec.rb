f41dd8549ee2b4b62f2f7001b59ea88e9208faf7
Angelo Gulina <agulina@gitlab.com>
2021-03-22 15:54:23 +0100

    Add licenses helper to display plan name

---
+++ ee/spec/helpers/cloud_licenses_helper_spec.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+RSpec.describe CloudLicensesHelper do
+  describe '#cloud_licenses_view_data' do
+    context 'when there is a current license' do
+      it 'returns the data for the view' do
+        custom_plan = 'custom plan'
+        license = double('License', plan: custom_plan)
+        allow(License).to receive(:current).and_return(license)
+
+        expect(cloud_licenses_view_data).to eq({ current_plan_title: 'Custom Plan' })
+      end
+    end
+  end
+end
