0f30aaf6e560400cb43ae6845c2bdac3df456f13
wortschi <mwortschack@gitlab.com>
2021-03-22 08:40:29 +0100

    Fix import order

--- app/assets/javascripts/pages/groups/settings/index.js
+++ app/assets/javascripts/pages/groups/settings/index.js
@@ -1,5 +1,5 @@
-import initSearchSettings from '~/search_settings';
 import initRevokeButton from '~/deploy_tokens/init_revoke_button';
+import initSearchSettings from '~/search_settings';
 
 initSearchSettings();
 initRevokeButton();
