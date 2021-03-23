0f22ff241397015890b3f1fe0bc8fbc76231eb7d
Florie Guibert <fguibert@gitlab.com>
2021-03-23 10:48:52 +1000

    Delete Epic Board
    Add ability to delete an epic board from boards selector dropdown

--- ee/app/assets/javascripts/boards/components/board_form.vue
+++ ee/app/assets/javascripts/boards/components/board_form.vue
@@ -3,16 +3,16 @@
 // extends a valid Vue single file component.
 /* eslint-disable @gitlab/no-runtime-template-compiler */
 import { mapGetters } from 'vuex';
+import { fullBoardId } from '~/boards/boards_util';
 import BoardFormFoss from '~/boards/components/board_form.vue';
+import { fullEpicBoardId } from '../boards_util';
 import createEpicBoardMutation from '../graphql/epic_board_create.mutation.graphql';
+import destroyEpicBoardMutation from '../graphql/epic_board_destroy.mutation.graphql';
 
 export default {
   extends: BoardFormFoss,
   computed: {
     ...mapGetters(['isEpicBoard']),
-    epicBoardCreateQuery() {
-      return createEpicBoardMutation;
-    },
     mutationVariables() {
       // TODO: Epic board scope will be added in a future iteration: https://gitlab.com/gitlab-org/gitlab/-/issues/231389
       return {
@@ -29,7 +29,7 @@ export default {
     },
     async createOrUpdateBoard() {
       const response = await this.$apollo.mutate({
-        mutation: this.isEpicBoard ? this.epicBoardCreateQuery : this.currentMutation,
+        mutation: this.isEpicBoard ? createEpicBoardMutation : this.currentMutation,
         variables: { input: this.mutationVariables },
       });
 
@@ -40,6 +40,14 @@ export default {
       }
 
       return this.boardUpdateResponse(response.data);
+    },
+    async deleteBoard() {
+      await this.$apollo.mutate({
+        mutation: this.isEpicBoard ? destroyEpicBoardMutation : this.deleteMutation,
+        variables: {
+          id: this.isEpicBoard ? fullEpicBoardId(this.board.id) : fullBoardId(this.board.id),
+        },
+      });
     },
   },
 };
