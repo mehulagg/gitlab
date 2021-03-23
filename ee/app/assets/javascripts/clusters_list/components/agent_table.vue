81346af0a9ab1c46e481dd2ac281003e37acf4b7
Nicholas Klick <nklick@gitlab.com>
2021-03-23 01:10:14 -0400

    Add padding for learn more link

--- ee/app/assets/javascripts/clusters_list/components/agent_table.vue
+++ ee/app/assets/javascripts/clusters_list/components/agent_table.vue
@@ -32,7 +32,7 @@ export default {
 
 <template>
   <div>
-    <div class="gl-display-block gl-text-right gl-my-4">
+    <div class="gl-display-block gl-text-right gl-my-4 learn-more">
       <gl-link
         href="https://docs.gitlab.com/ee/user/clusters/agent/#get-started-with-gitops-and-the-gitlab-agent"
         target="_blank"
