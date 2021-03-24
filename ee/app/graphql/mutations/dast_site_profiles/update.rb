61ab2972b19bf56c6c83272d9b7d279827bdf713
Philip Cunningham <pcunningham@gitlab.com>
2021-03-24 14:14:17 +1100

    Allow additional DAST scan config to be updated
    - Add new services
    - Update existing services
    - Add specs

--- ee/app/graphql/mutations/dast_site_profiles/update.rb
+++ ee/app/graphql/mutations/dast_site_profiles/update.rb
@@ -47,19 +47,35 @@ module Mutations
 
       authorize :create_on_demand_dast_scan
 
-      def resolve(full_path:, id:, profile_name:, target_url: nil, excluded_urls: nil, request_headers: nil, auth: nil)
+      def resolve(full_path:, id:, profile_name:, target_url: nil, **params)
         project = authorized_find!(full_path)
+
+        auth_params = feature_flagged(project, params[:auth], default: {})
 
         # TODO: remove explicit coercion once compatibility layer has been removed
         # See: https://gitlab.com/gitlab-org/gitlab/-/issues/257883
-        params = {
+        dast_site_profile_params = {
           id: SiteProfileID.coerce_isolated_input(id).model_id,
           name: profile_name,
           target_url: target_url,
-          excluded_urls: feature_flagged_excluded_urls(project, excluded_urls)
-        }.compact
+          excluded_urls: feature_flagged(project, params[:excluded_urls]),
+          auth_enabled: auth_params[:enabled],
+          auth_url: auth_params[:url],
+          auth_username_field: auth_params[:username_field],
+          auth_password_field: auth_params[:password_field],
+          auth_username: auth_params[:username],
+          auth_password: auth_params[:password]
+        }
 
-        result = ::DastSiteProfiles::UpdateService.new(project, current_user).execute(**params)
+        if params.key?(:request_headers)
+          dast_site_profile_params[:request_headers] = feature_flagged(project, params[:request_headers])
+        end
+
+        if auth_params.key?(:password)
+          dast_site_profile_params[:auth_password] = auth_params[:password]
+        end
+
+        result = ::DastSiteProfiles::UpdateService.new(project, current_user).execute(**dast_site_profile_params)
 
         if result.success?
           { id: result.payload.to_global_id, errors: [] }
@@ -70,10 +86,10 @@ module Mutations
 
       private
 
-      def feature_flagged_excluded_urls(project, excluded_urls)
-        return unless Feature.enabled?(:security_dast_site_profiles_additional_fields, project, default_enabled: :yaml)
+      def feature_flagged(project, value, opts = {})
+        return opts[:default] unless Feature.enabled?(:security_dast_site_profiles_additional_fields, project, default_enabled: :yaml)
 
-        excluded_urls
+        value || opts[:default]
       end
     end
   end
--- ee/app/services/dast_site_profiles/update_service.rb
+++ ee/app/services/dast_site_profiles/update_service.rb
@@ -2,23 +2,32 @@
 
 module DastSiteProfiles
   class UpdateService < BaseService
+    attr_reader :dast_site_profile
+
     def execute(id:, **params)
       return ServiceResponse.error(message: 'Insufficient permissions') unless allowed?
 
-      dast_site_profile = find_dast_site_profile!(id)
+      @dast_site_profile = find_dast_site_profile!(id)
 
-      return ServiceResponse.error(message: "Cannot modify #{dast_site_profile.name} referenced in security policy") if referenced_in_security_policy?(dast_site_profile)
+      return ServiceResponse.error(message: "Cannot modify #{dast_site_profile.name} referenced in security policy") if referenced_in_security_policy?
 
       ActiveRecord::Base.transaction do
         if target_url = params.delete(:target_url)
           params[:dast_site] = DastSites::FindOrCreateService.new(project, current_user).execute!(url: target_url)
         end
 
+        response = handle_secret_variable(params, :request_headers, Dast::SiteProfileSecretVariable::REQUEST_HEADERS)
+        break response if response.error?
+
+        response = handle_secret_variable(params, :auth_password, Dast::SiteProfileSecretVariable::PASSWORD)
+        break response if response.error?
+
+        params.compact!
+
         dast_site_profile.update!(params)
 
         ServiceResponse.success(payload: dast_site_profile)
       end
-
     rescue ActiveRecord::RecordNotFound => err
       ServiceResponse.error(message: "#{err.model} not found")
     rescue ActiveRecord::RecordInvalid => err
@@ -31,13 +40,47 @@ module DastSiteProfiles
       Ability.allowed?(current_user, :create_on_demand_dast_scan, project)
     end
 
-    def referenced_in_security_policy?(profile)
-      profile.referenced_in_security_policies.present?
+    def referenced_in_security_policy?
+      dast_site_profile.referenced_in_security_policies.present?
+    end
+
+    def handle_secret_variable(params, arg, key)
+      return ServiceResponse.success unless params.has_key?(arg)
+
+      manage_secret_variable(key, params[arg]).tap { params.delete(arg) }
+    end
+
+    def manage_secret_variable(key, value)
+      return delete_secret_variable(key) unless value
+
+      response = Dast::SiteProfileSecretVariables::CreateOrUpdateService.new(
+        container: project,
+        current_user: current_user,
+        params: { dast_site_profile: dast_site_profile, key: key, raw_value: value }
+      ).execute
+
+      return ServiceResponse.error(payload: dast_site_profile, message: response.errors) if response.error?
+
+      response
     end
 
     # rubocop: disable CodeReuse/ActiveRecord
     def find_dast_site_profile!(id)
       DastSiteProfilesFinder.new(project_id: project.id, id: id).execute.first!
+    end
+    # rubocop: enable CodeReuse/ActiveRecord
+
+    # rubocop: disable CodeReuse/ActiveRecord
+    def delete_secret_variable(key)
+      variable = dast_site_profile.secret_variables.find_by(key: key)
+
+      return ServiceResponse.success unless variable
+
+      Dast::SiteProfileSecretVariables::DestroyService.new(
+        container: project,
+        current_user: current_user,
+        params: { dast_site_profile_secret_variable: variable }
+      ).execute
     end
     # rubocop: enable CodeReuse/ActiveRecord
   end
