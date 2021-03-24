03835c966a899458462a3a80836a712d4fdeee35
Lee Tickett <lee@tickett.net>
2021-03-24 09:05:06 +0000

    Expose merge request timelogs via graphql

--- app/models/timelog.rb
+++ app/models/timelog.rb
@@ -2,6 +2,7 @@
 
 class Timelog < ApplicationRecord
   include Importable
+  include FromUnion
 
   validates :time_spent, :user, presence: true
   validate :issuable_id_is_present, unless: :importing?
@@ -17,6 +18,18 @@ class Timelog < ApplicationRecord
       Project.select(1).where(namespace: group.self_and_descendants)
         .where('issues.project_id = projects.id')
     )
+  end
+
+  scope :for_merge_requests_in_group, -> (group) do
+    joins(:merge_request).where(
+      'EXISTS (?)',
+      Project.select(1).where(namespace: group.self_and_descendants)
+        .where('merge_requests.target_project_id = projects.id')
+    )
+  end
+
+  scope :in_group, -> (group) do
+    from_union([for_issues_in_group(group), for_merge_requests_in_group(group)])
   end
 
   scope :between_times, -> (start_time, end_time) do
--- spec/models/timelog_spec.rb
+++ spec/models/timelog_spec.rb
@@ -51,27 +51,43 @@ RSpec.describe Timelog do
   end
 
   describe 'scopes' do
+    let(:group) { create(:group) }
+    let(:group_issue) { create(:issue, project: create(:project, group: group)) }
+    let(:group_merge_request) { create(:merge_request, source_project: create(:project, group: group)) }
+    let(:subgroup) { create(:group, parent: group) }
+    let(:subgroup_issue) { create(:issue, project: create(:project, group: subgroup)) }
+    let(:subgroup_merge_request) { create(:merge_request, source_project: create(:project, group: subgroup)) }
+
+    let(:timelog) { create(:issue_timelog, spent_at: 65.days.ago) }
+    let(:timelog1) { create(:issue_timelog, spent_at: 15.days.ago, issue: group_issue) }
+    let(:timelog2) { create(:issue_timelog, spent_at: 5.days.ago, issue: subgroup_issue) }
+    let(:timelog3) { create(:merge_request_timelog, spent_at: 65.days.ago) }
+    let(:timelog4) { create(:merge_request_timelog, spent_at: 15.days.ago, merge_request: group_merge_request) }
+    let(:timelog5) { create(:merge_request_timelog, spent_at: 5.days.ago, merge_request: subgroup_merge_request) }
+
+    describe 'in_group' do
+      it 'return timelogs created for group issues and merge requests' do
+        expect(described_class.in_group(group)).to contain_exactly(timelog1, timelog2, timelog4, timelog5)
+      end
+    end
+
     describe 'for_issues_in_group' do
       it 'return timelogs created for group issues' do
-        group = create(:group)
-        subgroup = create(:group, parent: group)
-
-        create(:issue_timelog)
-        timelog1 = create(:issue_timelog, issue: create(:issue, project: create(:project, group: group)))
-        timelog2 = create(:issue_timelog, issue: create(:issue, project: create(:project, group: subgroup)))
-
         expect(described_class.for_issues_in_group(group)).to contain_exactly(timelog1, timelog2)
+      end
+    end
+
+    describe 'for_merge_requests_in_group' do
+      it 'return timelogs created for group merge_requests' do
+        expect(described_class.for_merge_requests_in_group(group)).to contain_exactly(timelog4, timelog5)
       end
     end
 
     describe 'between_times' do
       it 'returns collection of timelogs within given times' do
-        create(:issue_timelog, spent_at: 65.days.ago)
-        timelog1 = create(:issue_timelog, spent_at: 15.days.ago)
-        timelog2 = create(:issue_timelog, spent_at: 5.days.ago)
-        timelogs = described_class.between_times(20.days.ago, 1.day.ago)
+        timelogs = described_class.between_times(20.days.ago, 10.days.ago)
 
-        expect(timelogs).to contain_exactly(timelog1, timelog2)
+        expect(timelogs).to contain_exactly(timelog1, timelog4)
       end
     end
   end
