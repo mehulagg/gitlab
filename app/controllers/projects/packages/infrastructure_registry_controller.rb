60c3693b4fab1eed460c87cbcd6b6e57edd496f4
Nicolò Maria Mezzopera <nicolo.mezzopera@gmail.com>
2021-03-24 12:01:25 +0100

    Add new controller and view file

---
+++ app/controllers/projects/packages/infrastructure_registry_controller.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+module Projects
+  module Packages
+    class InfrastructureRegistryController < Projects::ApplicationController
+
+      feature_category :infrastructure_registry
+
+    end
+  end
+end
