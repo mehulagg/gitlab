6f4b3e317965eab4d15446bf6d3a8231de025dee
Paul Slaughter <pslaughter@gitlab.com>
2021-03-24 09:18:01 -0500

    Add gitlab_schema.gql warning in integration spec
    - A missing gitlab_schema.graphql file can cause
      unrelated and hard to debug issues.
    - This can be overwritten with GL_IGNORE_WARNINGS=1

--- jest.config.integration.js
+++ jest.config.integration.js
@@ -1,4 +1,32 @@
+const fs = require('fs');
 const baseConfig = require('./jest.config.base');
+
+const GRAPHQL_SCHEMA_PATH = 'tmp/tests/graphql/gitlab_schema.graphql';
+const GRAPHQL_SCHEMA_JOB = 'bundle exec rake gitlab:graphql:schema:dump';
+
+const shouldIgnoreWarnings = JSON.parse(process.env.GL_IGNORE_WARNINGS || '0');
+
+const failCheck = (message) => {
+  console.error(message);
+
+  if (!shouldIgnoreWarnings) {
+    process.exit(1);
+  }
+};
+
+const checkGraphqlSchema = () => {
+  if (!fs.existsSync(GRAPHQL_SCHEMA_PATH)) {
+    const message = `
+ERROR: Expected to find "${GRAPHQL_SCHEMA_PATH}" but file does not exist. Try running:
+
+    ${GRAPHQL_SCHEMA_JOB}
+`;
+
+    failCheck(message);
+  }
+};
+
+checkGraphqlSchema();
 
 module.exports = {
   ...baseConfig('spec/frontend_integration', {
