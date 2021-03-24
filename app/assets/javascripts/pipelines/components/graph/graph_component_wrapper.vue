b5178b219065a58ae6b2f236aac9c0070fe7d79d
Sarah GP <sarah.groff.palermo@gmail.com>
2021-03-24 10:01:32 +0100

    Appease the linter

--- app/assets/javascripts/pipelines/components/graph/graph_component_wrapper.vue
+++ app/assets/javascripts/pipelines/components/graph/graph_component_wrapper.vue
@@ -68,7 +68,7 @@ export default {
         };
       },
       skip() {
-        return !Boolean(this.pipelineProjectPath && this.pipelineIid);
+        return !(this.pipelineProjectPath && this.pipelineIid);
       },
       update(data) {
         /*
