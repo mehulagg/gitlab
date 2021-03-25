e6788378cb6a2462f07a13f15011c02c438c9037
Anton Smith <asmith@gitlab.com>
2021-03-26 08:56:23 +1300

    Remove attachments since it is not required

--- lib/gitlab/slash_commands/presenters/issue_new.rb
+++ lib/gitlab/slash_commands/presenters/issue_new.rb
@@ -12,14 +12,6 @@ module Gitlab
 
         private
 
-        def fallback_message
-          "New issue #{issue.to_reference}: #{issue.title}"
-        end
-
-        def fields_with_markdown
-          %i(pretext text)
-        end
-
         def pretext
           "I created an issue on #{author_profile_link}'s behalf: *#{issue_link}* in #{project_link}"
         end
@@ -30,14 +22,7 @@ module Gitlab
 
         def response_message(custom_pretext: pretext)
           {
-            attachments: [
-            {
-              fallback:     fallback_message,
-              pretext:      custom_pretext,
-              text:         text,
-              mrkdwn_in:    fields_with_markdown
-            }
-          ]
+            text: pretext
           }
         end
       end
