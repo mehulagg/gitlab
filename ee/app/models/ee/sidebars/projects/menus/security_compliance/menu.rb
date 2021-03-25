df040d16e1588f65a8b6bfc4912f83554443fb62
Francisco Javier López <fjlopez@gitlab.com>
2021-03-25 20:15:56 +0100

    Fix security menu selection

--- ee/app/models/ee/sidebars/projects/menus/security_compliance/menu.rb
+++ ee/app/models/ee/sidebars/projects/menus/security_compliance/menu.rb
@@ -10,62 +10,52 @@ module EE
 
             override :link_to_href
             def link_to_href
-              # TODO This elements catches the link from the first element displayed
-              # We should iterate over all renderable items and return the first link
-              # project_security_configuration_path(context.project)
-              if !has_renderable_items? && context.show_discover_project_security
-                project_security_discover_path(context.project)
-              end
+              # Depending on the items enabled,
+              # the link and html attributes of the menu changes
+              return selected_menu.link_to_href if has_renderable_items?
+
+              project_security_discover_path(context.project) if context.show_discover_project_security
             end
 
-            # override :link_to_attributes
-            # def link_to_attributes
-            #   {
-            #     data: { qa_selector: 'audit_events_settings_link' }
-            #   }
-            # end
+            override :link_to_attributes
+            def link_to_attributes
+              super[:data] = selected_menu.link_to_attributes[:data]
+            end
 
             override :configure_menu_items
             def configure_menu_items
               super
 
-              insert_item_before(::Sidebars::Projects::Menus::SecurityCompliance::MenuItems::Configuration, ::Sidebars::Projects::Menus::SecurityCompliance::MenuItems::Dashboard.new(context))
+              top_list_items[:dashboard] = ::Sidebars::Projects::Menus::SecurityCompliance::MenuItems::Dashboard.new(context)
+              top_list_items[:audit_events] = ::Sidebars::Projects::Menus::SecurityCompliance::MenuItems::AuditEvents.new(context)
+              top_list_items[:dependencies] = ::Sidebars::Projects::Menus::SecurityCompliance::MenuItems::DependencyList.new(context)
+
+              insert_item_before(::Sidebars::Projects::Menus::SecurityCompliance::MenuItems::Configuration, top_list_items[:dashboard])
               insert_item_after(::Sidebars::Projects::Menus::SecurityCompliance::MenuItems::Dashboard, ::Sidebars::Projects::Menus::SecurityCompliance::MenuItems::VulnerabilityReport.new(context))
               insert_item_after(::Sidebars::Projects::Menus::SecurityCompliance::MenuItems::VulnerabilityReport, ::Sidebars::Projects::Menus::SecurityCompliance::MenuItems::OnDemandScans.new(context))
-              insert_item_after(::Sidebars::Projects::Menus::SecurityCompliance::MenuItems::OnDemandScans, ::Sidebars::Projects::Menus::SecurityCompliance::MenuItems::DependencyList.new(context))
+              insert_item_after(::Sidebars::Projects::Menus::SecurityCompliance::MenuItems::OnDemandScans, top_list_items[:dependencies])
               insert_item_after(::Sidebars::Projects::Menus::SecurityCompliance::MenuItems::DependencyList, ::Sidebars::Projects::Menus::SecurityCompliance::MenuItems::LicenseCompliance.new(context))
               insert_item_after(::Sidebars::Projects::Menus::SecurityCompliance::MenuItems::LicenseCompliance, ::Sidebars::Projects::Menus::SecurityCompliance::MenuItems::ThreatMonitoring.new(context))
               insert_item_after(::Sidebars::Projects::Menus::SecurityCompliance::MenuItems::ThreatMonitoring, ::Sidebars::Projects::Menus::SecurityCompliance::MenuItems::ScanPolicies.new(context))
-              add_item(::Sidebars::Projects::Menus::SecurityCompliance::MenuItems::AuditEvents.new(context))
+              insert_item_after(::Sidebars::Projects::Menus::SecurityCompliance::MenuItems::Configuration, top_list_items[:audit_events])
             end
 
-            # private
+            private
 
-            # def selected_menu
-            #   dashboard_menu = renderable_items.find { |i| it.is_a?(::Sidebars::Projects::Menus::SecurityCompliance::MenuItems::Dashboard) }
+            # This method selects which item is the one to retrieve info from
+            # for the menu
+            def selected_menu
+              @selected_menu ||= begin
+                return top_list_items[:dashboard] if top_list_items[:dashboard].render?
+                return top_list_items[:audit_events] if top_list_items[:audit_events].render?
 
-            #   [
-            #    ::Sidebars::Projects::Menus::SecurityCompliance::MenuItems::AuditEvents
-            #    ::Sidebars::Projects::Menus::SecurityCompliance::MenuItems::DependencyList].each do |menu_type|
-            #     renderable_items.select do |item|
-            #       item.is_a?()
-            #     end
-            #   end
-            # end
+                top_list_items[:dependencies]
+              end
+            end
 
-            # def top_level_link(project)
-            #   return project_security_dashboard_index_path(project) if project_nav_tab?(:security)
-            #   return project_audit_events_path(project) if project_nav_tab?(:audit_events)
-
-            #   project_dependencies_path(project)
-            # end
-
-            # def top_level_qa_selector(project)
-            #   return 'security_dashboard_link' if project_nav_tab?(:security)
-            #   return 'audit_events_settings_link' if project_nav_tab?(:audit_events)
-
-            #   'dependency_list_link'
-            # end
+            def top_list_items
+              @top_list_items ||= {}
+            end
           end
         end
       end
