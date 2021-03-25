3ec30b4c1bf1f4d6e0c9d8f382094fcbff127cfc
Nicolò Maria Mezzopera <nicolo.mezzopera@gmail.com>
2021-03-25 10:04:51 +0100

    Update category to package

--- app/controllers/projects/packages/infrastructure_registry_controller.rb
+++ app/controllers/projects/packages/infrastructure_registry_controller.rb
@@ -3,7 +3,7 @@
 module Projects
   module Packages
     class InfrastructureRegistryController < Projects::ApplicationController
-      feature_category :infrastructure_registry
+      feature_category :package_registry
     end
   end
 end
