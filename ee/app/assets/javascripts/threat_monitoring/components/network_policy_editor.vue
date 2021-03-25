502dbd5a41030bc9aa1cfe73fe1a84e7736cd287
Denys Mishunov <dmishunov@gitlab.com>
2021-03-25 17:17:06 +0100

    Switching the custom editor to Source Editor

--- ee/app/assets/javascripts/threat_monitoring/components/network_policy_editor.vue
+++ ee/app/assets/javascripts/threat_monitoring/components/network_policy_editor.vue
@@ -1,7 +1,10 @@
 <script>
-import { editor as monacoEditor } from 'monaco-editor';
+import EditorLite from '~/vue_shared/components/editor_lite.vue';
 
 export default {
+  components: {
+    EditorLite,
+  },
   props: {
     value: {
       type: String,
@@ -18,50 +21,38 @@ export default {
       default: 300,
     },
   },
-  data() {
-    return { editor: null };
-  },
-  watch: {
-    value(val) {
-      if (val === this.editor.getValue()) return;
-      this.editor.setValue(val);
-    },
-  },
-  beforeDestroy() {
-    this.editor.dispose();
-  },
-  mounted() {
-    if (!this.editor) {
-      this.setupEditor();
-    }
-  },
-  methods: {
-    setupEditor() {
-      this.editor = monacoEditor.create(this.$refs.editor, {
-        value: this.value,
-        language: 'yaml',
+  computed: {
+    editorOptions() {
+      return {
         lineNumbers: 'off',
         minimap: { enabled: false },
         folding: false,
         renderIndentGuides: false,
         renderWhitespace: 'boundary',
         renderLineHighlight: 'none',
-        glyphMargin: false,
         lineDecorationsWidth: 0,
         lineNumbersMinChars: 0,
         occurrencesHighlight: false,
         hideCursorInOverviewRuler: true,
         overviewRulerBorder: false,
         readOnly: this.readOnly,
-      });
-      this.editor.onDidChangeModelContent(() => {
-        this.$emit('input', this.editor.getValue());
-      });
+      };
+    },
+  },
+  methods: {
+    notifyAboutUpdates() {
+      this.$emit('input', this.editor.getValue());
     },
   },
 };
 </script>
 
 <template>
-  <div ref="editor" class="gl-overflow-hidden" :style="{ height: `${height}px` }"></div>
+  <editor-lite
+    :value="value"
+    file-name="*.yaml"
+    extensions="editor_ci_schema_ext"
+    :editor-options="editorOptions"
+    @input="notifyAboutUpdates"
+  />
 </template>
