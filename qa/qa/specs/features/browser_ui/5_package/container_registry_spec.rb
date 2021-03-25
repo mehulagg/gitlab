6e2784c389115b96d5a36cf0df941a741a342310
Sofia Vistas <svistas@gitlab.com>
2021-03-25 09:51:30 +0200

    Add a Container Registry test for Self-managed users
    Prior to this change, the container registry test
    was only running on gitlab managed environments with
    fixed docker+machine runners
    
    This change adds coverage to the test gap for
    gitlab self-managed environments

--- qa/qa/specs/features/browser_ui/5_package/container_registry_spec.rb
+++ qa/qa/specs/features/browser_ui/5_package/container_registry_spec.rb
@@ -2,51 +2,57 @@
 
 module QA
   RSpec.describe 'Package', :orchestrated do
-    describe 'Container Registry', only: { subdomain: :staging } do
-      let(:project) do
-        Resource::Project.fabricate_via_api! do |project|
-          project.name = 'project-with-registry'
-          project.template_name = 'express'
-        end
-      end
-
-      let(:registry_repository) do
-        Resource::RegistryRepository.fabricate! do |repository|
-          repository.name = "#{project.path_with_namespace}"
-          repository.project = project
-        end
-      end
-
-      let!(:gitlab_ci_yaml) do
-        <<~YAML
-          build:
-            image: docker:19.03.12
-            stage: build
-            services:
-              - docker:19.03.12-dind
-            variables:
-              IMAGE_TAG: $CI_REGISTRY_IMAGE:$CI_COMMIT_REF_SLUG
-            script:
-              - docker login -u $CI_REGISTRY_USER -p $CI_REGISTRY_PASSWORD $CI_REGISTRY
-              - docker build -t $IMAGE_TAG .
-              - docker push $IMAGE_TAG
-        YAML
+    describe 'Self-managed Container Registry', :ssh_tunnel, :orchestrated do
+      before do
+        Flow::SignUp.disable_sign_ups
       end
 
       after do
-        registry_repository&.remove_via_api!
+        @runner.remove_via_api!
       end
 
-      it 'pushes project image to the container registry and deletes tag', testcase: 'https://gitlab.com/gitlab-org/quality/testcases/-/issues/1699' do
-        Flow::Login.sign_in
+      it "pushes image and deletes tag", testcase: 'https://gitlab.com/gitlab-org/quality/testcases/-/issues/1743' do
+        project = Resource::Project.fabricate_via_api! do |project|
+          project.name = 'project-with-registry'
+          project.template_name = 'express'
+        end
+
+        @runner = Resource::Runner.fabricate! do |runner|
+          runner.name = "qa-runner-#{Time.now.to_i}"
+          runner.tags = ["runner-for-#{project.name}"]
+          runner.executor = :docker
+          runner.project = project
+          runner.executor_image = 'docker:19.03.12'
+        end
+
         project.visit!
 
         Resource::Repository::Commit.fabricate_via_api! do |commit|
           commit.project = project
           commit.commit_message = 'Add .gitlab-ci.yml'
           commit.add_files([{
-                              file_path: '.gitlab-ci.yml',
-                              content: gitlab_ci_yaml
+                                file_path: '.gitlab-ci.yml',
+                                content:
+                                    <<~YAML
+                                    build:
+                                      image: docker:19.03.12
+                                      stage: build
+                                      services:
+                                        - docker:19.03.12-dind
+                                      variables:
+                                        IMAGE_TAG: $CI_REGISTRY_IMAGE:$CI_COMMIT_REF_SLUG
+                                        DOCKER_TLS_CERTDIR: "/certs"
+                                      before_script:
+                                        - mkdir -p $HOME/.docker/
+                                        - 'echo "{ \\"proxies\\": { \\"default\\": { \\"httpProxy\\": \\"http://$CI_SERVER_HOST\\", \\"httpsProxy\\": \\"http://$CI_SERVER_HOST\\", \\"noProxy\\": \\"docker:2375,docker:2376\\" } } }" > $HOME/.docker/config.json'
+                                        - cat $HOME/.docker/config.json
+                                      script:
+                                        - docker login -u $CI_REGISTRY_USER -p $CI_REGISTRY_PASSWORD $CI_REGISTRY
+                                        - docker build -t $IMAGE_TAG .
+                                        - docker push $IMAGE_TAG
+                                      tags:
+                                        - "runner-for-#{project.name}"
+                                    YAML
                             }])
         end
 
@@ -63,9 +69,74 @@ module QA
         Page::Project::Menu.perform(&:go_to_container_registry)
 
         Page::Project::Registry::Show.perform do |registry|
-          expect(registry).to have_registry_repository(registry_repository.name)
+          expect(registry).to have_registry_repository(project.path_with_namespace)
 
-          registry.click_on_image(registry_repository.name)
+          registry.click_on_image(project.path_with_namespace)
+          expect(registry).to have_tag('master')
+
+          registry.click_delete
+          expect(registry).not_to have_tag('master')
+        end
+      end
+    end
+
+    describe 'GitLab-managed Container Registry', only: { subdomain: :staging } do
+      after do
+        @registry&.remove_via_api!
+      end
+
+      it 'pushes image and deletes tag', testcase: 'https://gitlab.com/gitlab-org/quality/testcases/-/issues/1699' do
+        project = Resource::Project.fabricate_via_api! do |project|
+          project.name = 'project-with-registry'
+          project.template_name = 'express'
+        end
+
+        @registry = Resource::RegistryRepository.fabricate! do |repository|
+          repository.name = "#{project.path_with_namespace}"
+          repository.project = project
+        end
+
+        Flow::Login.sign_in
+        project.visit!
+
+        Resource::Repository::Commit.fabricate_via_api! do |commit|
+          commit.project = project
+          commit.commit_message = 'Add .gitlab-ci.yml'
+          commit.add_files([{
+                                file_path: '.gitlab-ci.yml',
+                                content:
+                                    <<~YAML
+                                      build:
+                                        image: docker:19.03.12
+                                        stage: build
+                                        services:
+                                          - docker:19.03.12-dind
+                                        variables:
+                                          IMAGE_TAG: $CI_REGISTRY_IMAGE:$CI_COMMIT_REF_SLUG
+                                        script:
+                                          - docker login -u $CI_REGISTRY_USER -p $CI_REGISTRY_PASSWORD $CI_REGISTRY
+                                          - docker build -t $IMAGE_TAG .
+                                          - docker push $IMAGE_TAG
+                                    YAML
+                            }])
+        end
+
+        Flow::Pipeline.visit_latest_pipeline
+
+        Page::Project::Pipeline::Show.perform do |pipeline|
+          pipeline.click_job('build')
+        end
+
+        Page::Project::Job::Show.perform do |job|
+          expect(job).to be_successful(timeout: 800)
+        end
+
+        Page::Project::Menu.perform(&:go_to_container_registry)
+
+        Page::Project::Registry::Show.perform do |registry|
+          expect(registry).to have_registry_repository(registry.name)
+
+          registry.click_on_image(registry.name)
           expect(registry).to have_tag('master')
 
           registry.click_delete
