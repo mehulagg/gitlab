9259354f8fc652a758ff78790b7451cbc858b3cc
Maxime Orefice <morefice@gitlab.com>
2021-03-26 17:19:55 -0400

    Apply code review feedback

--- db/migrate/20210325150837_add_verification_state_to_ci_pipeline_artifact.rb
+++ db/migrate/20210325150837_add_verification_state_to_ci_pipeline_artifact.rb
@@ -5,11 +5,11 @@ class AddVerificationStateToCiPipelineArtifact < ActiveRecord::Migration[6.0]
 
   def change
     change_table(:ci_pipeline_artifacts, bulk: true) do |t|
-      t.integer :verification_state, default: 0, limit: 2, null: false
-      t.integer :verification_retry_count, limit: 2
       t.column :verification_started_at, :datetime_with_timezone
       t.column :verification_retry_at, :datetime_with_timezone
       t.column :verified_at, :datetime_with_timezone
+      t.integer :verification_state, default: 0, limit: 2, null: false
+      t.integer :verification_retry_count, limit: 2
       t.binary :verification_checksum, using: 'verification_checksum::bytea'
 
       t.text :verification_failure # rubocop:disable Migration/AddLimitToTextColumns
