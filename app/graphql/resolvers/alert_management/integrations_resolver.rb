2ef07ac4c0a989d6790f247a1c9b3d325c67be68
Vitali Tatarintev <Kalastiuz@gmail.com>
2021-03-26 15:40:36 +0100

    Allow filtering integrations GraphQL by ID

--- app/graphql/resolvers/alert_management/integrations_resolver.rb
+++ app/graphql/resolvers/alert_management/integrations_resolver.rb
@@ -5,24 +5,54 @@ module Resolvers
     class IntegrationsResolver < BaseResolver
       alias_method :project, :object
 
+      argument :id, ::Types::GlobalIDType,
+               required: false,
+               description: 'ID of the integration.'
+
       type Types::AlertManagement::IntegrationType.connection_type, null: true
 
-      def resolve(**args)
-        http_integrations + prometheus_integrations
+      def resolve(id: nil)
+        if id
+          [integration_by(gid: id)]
+        else
+          http_integrations + prometheus_integrations
+        end
       end
 
       private
 
+      def integration_by(gid:)
+        # Expected type is unknown. Can be either `AlertManagement::HttpIntegration` or `PrometheusService`
+        integration = GitlabSchema.object_from_id(gid)&.sync # rubocop:disable Graphql/GIDExpectedType
+
+        integration if integration&.class.in?(expected_integration_types) && project == integration&.project
+      end
+
       def prometheus_integrations
-        return [] unless Ability.allowed?(current_user, :admin_project, project)
+        return [] unless prometheus_integrations_allowed?
 
         Array(project.prometheus_service)
       end
 
       def http_integrations
-        return [] unless Ability.allowed?(current_user, :admin_operations, project)
+        return [] unless http_integrations_allowed?
 
         ::AlertManagement::HttpIntegrationsFinder.new(project, {}).execute
+      end
+
+      def prometheus_integrations_allowed?
+        Ability.allowed?(current_user, :admin_project, project)
+      end
+
+      def http_integrations_allowed?
+        Ability.allowed?(current_user, :admin_operations, project)
+      end
+
+      def expected_integration_types
+        [].tap do |types|
+          types << ::AlertManagement::HttpIntegration if http_integrations_allowed?
+          types << ::PrometheusService if prometheus_integrations_allowed?
+        end
       end
     end
   end
