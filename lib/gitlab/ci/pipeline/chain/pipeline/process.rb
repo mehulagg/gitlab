7f8e480470320df5eb920abc49f0b78f9aa4ff6a
Marius Bobin <mbobin@gitlab.com>
2021-03-26 16:40:10 +0200

    Add feature flag check

--- lib/gitlab/ci/pipeline/chain/pipeline/process.rb
+++ lib/gitlab/ci/pipeline/chain/pipeline/process.rb
@@ -8,7 +8,11 @@ module Gitlab
           # After pipeline has been successfully created we can start processing it.
           class Process < Chain::Base
             def perform!
-              ::Ci::Pipelines::InitialProcessWorker.perform_async(pipeline.id)
+              if ::Feature.enabled?(:ci_async_initial_pipeline_processing, project, default_enabled: :yaml)
+                ::Ci::Pipelines::InitialProcessWorker.perform_async(pipeline.id)
+              else
+                ::Ci::ProcessPipelineService.new(@pipeline).execute
+              end
             end
 
             def break?
