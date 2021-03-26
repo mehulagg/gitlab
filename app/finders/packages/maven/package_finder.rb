5e40f58d139902923148c363286b0f531c0e063e
David Fernandez <dfernandez@gitlab.com>
2021-03-26 16:02:49 +0100

    Add improvements to the Maven API files endpoints
    1. Update `Packages::Package.for_projects` to not eager load projects
    2. Update `Packages::Maven::PackageFinder` to use the minimum role
    needed to access packages
    3. Update `Packages::Maven::PackageFinder` to access the
    `packages_packages` table only once instead of three times.

--- app/finders/packages/maven/package_finder.rb
+++ app/finders/packages/maven/package_finder.rb
@@ -3,13 +3,17 @@
 module Packages
   module Maven
     class PackageFinder
-      attr_reader :path, :current_user, :project, :group
+      include ::Packages::FinderHelper
+      include Gitlab::Utils::StrongMemoize
 
-      def initialize(path, current_user, project: nil, group: nil)
+      attr_reader :path, :current_user, :project, :group, :order_by_package_file
+
+      def initialize(path, current_user, project: nil, group: nil, order_by_package_file: false)
         @path = path
         @current_user = current_user
         @project = project
         @group = group
+        @order_by_package_file = order_by_package_file
       end
 
       def execute
@@ -34,7 +38,11 @@ module Packages
 
       def packages_with_path
         matching_packages = base.only_maven_packages_with_path(path, use_cte: group.present?)
-        matching_packages = matching_packages.order_by_package_file if versionless_package?(matching_packages)
+        if group_level_improvements?
+          matching_packages = matching_packages.order_by_package_file if order_by_package_file
+        else
+          matching_packages = matching_packages.order_by_package_file if versionless_package?(matching_packages)
+        end
 
         matching_packages
       end
@@ -54,13 +62,23 @@ module Packages
       # Produces a query that retrieves packages from multiple projects that
       # the current user can view within a group.
       def packages_for_multiple_projects
-        ::Packages::Package.for_projects(projects_visible_to_current_user)
+        if group_level_improvements?
+          packages_visible_to_user(current_user, within_group: group)
+        else
+          ::Packages::Package.for_projects(projects_visible_to_current_user)
+        end
       end
 
       # Returns the projects that the current user can view within a group.
       def projects_visible_to_current_user
         group.all_projects
              .public_or_visible_to_user(current_user)
+      end
+
+      def group_level_improvements?
+        strong_memoize(:group_level_improvements) do
+          Feature.enabled?(:maven_packages_group_level_improvements)
+        end
       end
     end
   end
