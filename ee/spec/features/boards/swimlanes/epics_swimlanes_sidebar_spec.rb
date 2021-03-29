d346ee7c701c3f90b47747e3f10b72331415f2c6
Eulyeon Ko <eko@gitlab.com>
2021-03-29 17:13:44 +0900

    TEMP

--- ee/spec/features/boards/swimlanes/epics_swimlanes_sidebar_spec.rb
+++ ee/spec/features/boards/swimlanes/epics_swimlanes_sidebar_spec.rb
@@ -3,71 +3,56 @@
 require 'spec_helper'
 
 RSpec.describe 'epics swimlanes sidebar', :js do
-  let_it_be(:user) { create(:user) }
+  let_it_be(:user)  { create(:user) }
   let_it_be(:group) { create(:group, :public) }
   let_it_be(:project, reload: true) { create(:project, :public, group: group) }
 
   let_it_be(:board) { create(:board, project: project) }
   let_it_be(:label) { create(:label, project: project, name: 'Label 1') }
-  let_it_be(:list) { create(:list, board: board, label: label, position: 0) }
+  let_it_be(:list)  { create(:list, board: board, label: label, position: 0) }
   let_it_be(:epic1) { create(:epic, group: group) }
   let_it_be(:epic2) { create(:epic, group: group) }
 
-  let_it_be(:issue1, reload: true) { create(:issue, project: project) }
-  let_it_be(:epic_issue1, reload: true) { create(:epic_issue, epic: epic1, issue: issue1) }
+  let_it_be(:issue, reload: true)      { create(:issue, project: project) }
+  let_it_be(:epic_issue, reload: true) { create(:epic_issue, epic: epic1, issue: issue) }
+
+  let(:boards_path) { project_boards_path(project, group_by: 'epic') }
 
   before do
     project.add_maintainer(user)
 
     stub_licensed_features(epics: true, swimlanes: true)
+
     sign_in(user)
 
-    visit project_boards_path(project)
-    wait_for_requests
+    load_epic_swimlanes
+
+    first_card.click
   end
 
-  context 'when closing sidebar' do
-    let(:issue_card) { first("[data-testid='board-epic-lane-issues'] [data-testid='board_card']") }
-
-    it 'unhighlights the active issue card' do
-      load_epic_swimlanes
-
-      issue_card.click
-
-      find("[data-testid='sidebar-drawer'] .gl-drawer-close-button").click
-
-      expect(issue_card[:class]).not_to include('is-active')
-      expect(issue_card[:class]).not_to include('multi-select')
-    end
-  end
+  it_behaves_like 'issue boards sidebar'
 
   context 'epic dropdown' do
-    before do
+    before_all do
       group.add_owner(user)
     end
 
     context 'when the issue is associated with an epic' do
       it 'displays name of epic and links to it' do
-        load_epic_swimlanes
-        click_first_issue_card
-
         page.within('[data-testid="sidebar-epic"]') do
           expect(page).to have_link(epic1.title, href: epic_path(epic1))
         end
       end
 
       it 'updates the epic associated with the issue' do
-        load_epic_swimlanes
-        click_first_issue_card
-
         page.within('[data-testid="sidebar-epic"]') do
           find("[data-testid='edit-button']").click
 
-          wait_for_all_requests
+          wait_for_requests
 
           find('.gl-new-dropdown-item', text: epic2.title).click
 
-          wait_for_all_requests
+          wait_for_requests
 
           expect(page).to have_link(epic2.title, href: epic_path(epic2))
         end
@@ -75,152 +60,20 @@ RSpec.describe 'epics swimlanes sidebar', :js do
     end
   end
 
-  context 'notifications subscription' do
-    it 'displays notifications toggle' do
-      load_epic_swimlanes
-      click_first_issue_card
-
-      page.within('[data-testid="sidebar-notifications"]') do
-        expect(page).to have_selector('[data-testid="notification-subscribe-toggle"]')
-        expect(page).to have_content('Notifications')
-        expect(page).not_to have_content('Notifications have been disabled by the project or group owner')
-      end
-    end
-
-    it 'shows toggle as on then as off as user toggles to subscribe and unsubscribe' do
-      load_epic_swimlanes
-      click_first_issue_card
-
-      toggle = find('[data-testid="notification-subscribe-toggle"]')
-
-      toggle.click
-
-      expect(toggle).to have_css("button.is-checked")
-
-      toggle.click
-
-      expect(toggle).not_to have_css("button.is-checked")
-    end
-
-    context 'when notifications have been disabled' do
-      before do
-        project.update_attribute(:emails_disabled, true)
-
-        load_epic_swimlanes
-      end
-
-      it 'displays a message that notifications have been disabled' do
-        click_first_issue_card
-
-        page.within('[data-testid="sidebar-notifications"]') do
-          expect(page).not_to have_selector('[data-testid="notification-subscribe-toggle"]')
-          expect(page).to have_content('Notifications have been disabled by the project or group owner')
-        end
-      end
-    end
-  end
-
-  context 'time tracking' do
-    it 'displays time tracking feature with default message' do
-      load_epic_swimlanes
-      click_first_issue_card
-
-      page.within('[data-testid="time-tracker"]') do
-        expect(page).to have_content('Time tracking')
-        expect(page).to have_content('No estimate or time spent')
-      end
-    end
-
-    context 'when only spent time is recorded' do
-      before do
-        issue1.timelogs.create!(time_spent: 3600, user: user)
-
-        load_epic_swimlanes
-
-        click_first_issue_card
-      end
-
-      it 'shows the total time spent only' do
-        page.within('[data-testid="time-tracker"]') do
-          expect(page).to have_content('Spent: 1h')
-          expect(page).not_to have_content('Estimated')
-        end
-      end
-    end
-
-    context 'when only estimated time is recorded' do
-      before do
-        issue1.update!(time_estimate: 3600)
-
-        load_epic_swimlanes
-
-        click_first_issue_card
-      end
-
-      it 'shows the estimated time only' do
-        page.within('[data-testid="time-tracker"]') do
-          expect(page).to have_content('Estimated: 1h')
-          expect(page).not_to have_content('Spent')
-        end
-      end
-    end
-
-    context 'when estimated and spent times are available' do
-      before do
-        issue1.update!(time_estimate: 3600)
-        issue1.timelogs.create!(time_spent: 1800, user: user)
-
-        load_epic_swimlanes
-
-        click_first_issue_card
-      end
-
-      it 'shows time tracking progress bar' do
-        page.within('[data-testid="time-tracker"]') do
-          expect(page).to have_selector('[data-testid="timeTrackingComparisonPane"]')
-        end
-      end
-
-      it 'shows both estimated and spent time text' do
-        page.within('[data-testid="time-tracker"]') do
-          expect(page).to have_content('Spent 30m')
-          expect(page).to have_content('Est 1h')
-        end
-      end
-    end
-
-    context 'when limitedToHours instance option is turned on' do
-      before do
-        stub_application_setting(time_tracking_limit_to_hours: true)
-
-        # 3600+3600*24 = 1d 1h or 25h
-        issue1.timelogs.create!(time_spent: 3600 + 3600 * 24, user: user)
-
-        load_epic_swimlanes
-
-        click_first_issue_card
-      end
-
-      it 'shows the total time spent only' do
-        page.within('[data-testid="time-tracker"]') do
-          expect(page).to have_content('Spent: 25h')
-        end
-      end
-    end
-  end
-
-  def click_first_issue_card
-    page.within("[data-testid='board-epic-lane-issues']") do
-      first("[data-testid='board_card']").click
-    end
-  end
-
   def load_epic_swimlanes
+    visit project_boards_path(project)
+
+    wait_for_requests
+
     page.within('.board-swimlanes-toggle-wrapper') do
       page.find('.dropdown-toggle').click
       page.find('.dropdown-item', text: 'Epic').click
     end
 
-    wait_for_all_requests
+    wait_for_requests
+  end
+
+  def first_card
+    find("[data-testid='board-epic-lane-issues']").first("[data-testid='board_card']")
   end
 end
