6deefeacf44dfaf2911d504e18e0f76c31db61ae
Miguel Rincon <mrincon@gitlab.com>
2021-03-29 11:06:01 +0200

    Remove unsued class in ci badge

--- app/assets/javascripts/vue_shared/components/ci_badge_link.vue
+++ app/assets/javascripts/vue_shared/components/ci_badge_link.vue
@@ -56,13 +56,13 @@ export default {
     },
     cssClass() {
       const className = this.status.group;
-      return className ? `ci-status ci-${className} qa-status-badge` : 'ci-status qa-status-badge';
+      return className ? `ci-${className}` : '';
     },
   },
 };
 </script>
 <template>
-  <a v-gl-tooltip :href="detailsPath" :class="cssClass" :title="title">
+  <a v-gl-tooltip :href="detailsPath" class="ci-status" :class="cssClass" :title="title">
     <ci-icon :status="status" :css-classes="iconClasses" />
 
     <template v-if="showText">
