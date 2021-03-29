3a74eda552a5cda368517ca7f0af60b61cdf77fc
Pedro Pombeiro <noreply@pedro.pombei.ro>
2021-03-29 15:55:57 +0200

    Deprecate groupId and projectId instead

--- app/graphql/resolvers/ci/runner_setup_resolver.rb
+++ app/graphql/resolvers/ci/runner_setup_resolver.rb
@@ -14,7 +14,17 @@ module Resolvers
         required: true,
         description: 'Architecture to generate the instructions for.'
 
-      def resolve(platform:, architecture:)
+      argument :project_id, ::Types::GlobalIDType[::Project],
+        required: false,
+        deprecated: { reason: 'No longer used', milestone: '13.11' },
+        description: 'Project to register the runner for.'
+
+      argument :group_id, ::Types::GlobalIDType[::Group],
+        required: false,
+        deprecated: { reason: 'No longer used', milestone: '13.11' },
+        description: 'Group to register the runner for.'
+
+      def resolve(platform:, architecture:, **args)
         instructions = Gitlab::Ci::RunnerInstructions.new(
           current_user: current_user,
           os: platform,
@@ -25,12 +35,34 @@ module Resolvers
           install_instructions: instructions.install_script || other_install_instructions(platform),
           register_instructions: instructions.register_command
         }
+      ensure
+        raise Gitlab::Graphql::Errors::ResourceNotAvailable, 'User is not authorized to register a runner for the specified resource!' if instructions.errors.include?('Gitlab::Access::AccessDeniedError')
       end
 
       private
 
       def other_install_instructions(platform)
         Gitlab::Ci::RunnerInstructions::OTHER_ENVIRONMENTS[platform.to_sym][:installation_instructions_url]
+      end
+
+      def target_param(args)
+        project_param(args[:project_id]) || group_param(args[:group_id]) || {}
+      end
+
+      def project_param(project_id)
+        return unless project_id
+
+        { project: find_object(project_id) }
+      end
+
+      def group_param(group_id)
+        return unless group_id
+
+        { group: find_object(group_id) }
+      end
+
+      def find_object(gid)
+        GlobalID::Locator.locate(gid)
       end
     end
   end
--- spec/graphql/resolvers/ci/runner_setup_resolver_spec.rb
+++ spec/graphql/resolvers/ci/runner_setup_resolver_spec.rb
@@ -8,10 +8,12 @@ RSpec.describe Resolvers::Ci::RunnerSetupResolver do
   describe '#resolve' do
     let(:user) { create(:user) }
 
-    subject(:resolve_subject) { resolve(described_class, ctx: { current_user: user }, args: { platform: platform, architecture: 'amd64' }) }
+    subject(:resolve_subject) { resolve(described_class, ctx: { current_user: user }, args: { platform: platform, architecture: 'amd64' }.merge(target_param)) }
 
     context 'with container platforms' do
       let(:platform) { 'docker' }
+      let(:project) { create(:project) }
+      let(:target_param) { { project_id: project.to_global_id } }
 
       it 'returns install instructions' do
         expect(resolve_subject[:install_instructions]).not_to eq(nil)
@@ -25,9 +27,80 @@ RSpec.describe Resolvers::Ci::RunnerSetupResolver do
     context 'with regular platforms' do
       let(:platform) { 'linux' }
 
-      it 'returns install and register instructions' do
-        expect(resolve_subject.keys).to contain_exactly(:install_instructions, :register_instructions)
-        expect(resolve_subject.values).not_to include(nil)
+      context 'without target parameter' do
+        let(:target_param) { {} }
+
+        context 'when user is not admin' do
+          it 'returns install and register instructions' do
+            expect(resolve_subject.keys).to contain_exactly(:install_instructions, :register_instructions)
+            expect(resolve_subject.values).not_to include(nil)
+          end
+        end
+
+        context 'when user is admin' do
+          before do
+            user.update!(admin: true)
+          end
+
+          it 'returns install and register instructions' do
+            expect(resolve_subject.keys).to contain_exactly(:install_instructions, :register_instructions)
+            expect(resolve_subject.values).not_to include(nil)
+          end
+        end
+      end
+
+      context 'with project target parameter' do
+        let(:project) { create(:project) }
+        let(:target_param) { { project_id: project.to_global_id } }
+
+        context 'when user has access to admin builds on project' do
+          before do
+            project.add_maintainer(user)
+          end
+
+          it 'returns install and register instructions' do
+            expect(resolve_subject.keys).to contain_exactly(:install_instructions, :register_instructions)
+            expect(resolve_subject.values).not_to include(nil)
+          end
+        end
+
+        context 'when user does not have access to admin builds on project' do
+          before do
+            project.add_developer(user)
+          end
+
+          it 'returns install and register instructions' do
+            expect(resolve_subject.keys).to contain_exactly(:install_instructions, :register_instructions)
+            expect(resolve_subject.values).not_to include(nil)
+          end
+        end
+      end
+
+      context 'with group target parameter' do
+        let(:group) { create(:group) }
+        let(:target_param) { { group_id: group.to_global_id } }
+
+        context 'when user has access to admin builds on group' do
+          before do
+            group.add_owner(user)
+          end
+
+          it 'returns install and register instructions' do
+            expect(resolve_subject.keys).to contain_exactly(:install_instructions, :register_instructions)
+            expect(resolve_subject.values).not_to include(nil)
+          end
+        end
+
+        context 'when user does not have access to admin builds on group' do
+          before do
+            group.add_developer(user)
+          end
+
+          it 'returns install and register instructions' do
+            expect(resolve_subject.keys).to contain_exactly(:install_instructions, :register_instructions)
+            expect(resolve_subject.values).not_to include(nil)
+          end
+        end
       end
     end
   end
