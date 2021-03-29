68c613055cc6c278e961ebdfa77f60244cf97748
Serena Fang <sfang@gitlab.com>
2021-03-29 14:43:22 -0500

    Check group root ancestor setting

--- app/policies/group_policy.rb
+++ app/policies/group_policy.rb
@@ -253,7 +253,7 @@ class GroupPolicy < BasePolicy
   end
 
   def resource_access_token_creation_allowed?
-    group.resource_access_token_creation_allowed?
+    group.root_ancestor.resource_access_token_creation_allowed?
   end
 end
 
