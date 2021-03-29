a66d96c8d666dd6e84ae757962f8cf8c100e223d
Ammar Alakkad <am.alakkad@gmail.com>
2021-03-29 17:49:13 +0300

    Add title to cloud license page and center content

--- ee/app/assets/javascripts/pages/admin/cloud_licenses/components/app.vue
+++ ee/app/assets/javascripts/pages/admin/cloud_licenses/components/app.vue
@@ -23,9 +23,15 @@ export default {
 
 <template>
   <div class="gl-display-flex gl-justify-content-center gl-flex-direction-column">
-    <h3 class="gl-mb-7 gl-mt-6 gl-text-center">
-      {{ s__('CloudLicense|This instance is currently using the Core plan.') }}
-    </h3>
-    <cloud-license-subscription-activation-form v-if="!subscriptionData" />
+    <h3>{{ s__('CloudLicense|Your Subscription') }}</h3>
+    <hr />
+    <div class="row">
+      <div class="col-12 col-lg-8 offset-lg-2">
+        <h3 class="gl-mb-7 gl-mt-6 gl-text-center">
+          {{ s__('CloudLicense|This instance is currently using the Core plan.') }}
+        </h3>
+        <cloud-license-subscription-activation-form v-if="!subscriptionData" />
+      </div>
+    </div>
   </div>
 </template>
