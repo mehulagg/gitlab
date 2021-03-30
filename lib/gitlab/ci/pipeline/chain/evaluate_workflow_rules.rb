626b08c57052bc56f37bfb0a7e47214669ffa1e9
drew cimino <dcimino@gitlab.com>
2021-03-30 15:54:58 -0400

    EvaluateWorkflowRules for branch and merge_request
    Add specific "when" values "branch" and "merge_request" for explicit
    pipeline execution control in preprocessing stage.

--- lib/gitlab/ci/pipeline/chain/evaluate_workflow_rules.rb
+++ lib/gitlab/ci/pipeline/chain/evaluate_workflow_rules.rb
@@ -21,7 +21,19 @@ module Gitlab
           private
 
           def workflow_passed?
-            workflow_rules_result.pass?
+            if merge_request_pipeline?
+              workflow_rules_result.pass? && configured_for_merge_request?
+            else
+              workflow_rules_result.pass? && configured_for_branch?
+            end
+          end
+
+          def configured_for_branch?
+            (%w(always branch branches) & Array(workflow_rules_result.when)).any?
+          end
+
+          def configured_for_merge_request?
+            (%w(always merge_request merge_requests) & Array(workflow_rules_result.when)).any?
           end
 
           def workflow_rules_result
@@ -31,8 +43,11 @@ module Gitlab
           end
 
           def workflow_rules
-            Gitlab::Ci::Build::Rules.new(
-              workflow_rules_config, default_when: 'always')
+            if merge_request_pipeline?
+              Gitlab::Ci::Build::Rules.new(workflow_rules_config, default_when: 'never')
+            else
+              Gitlab::Ci::Build::Rules.new(workflow_rules_config, default_when: 'always')
+            end
           end
 
           def global_context
@@ -42,6 +57,10 @@ module Gitlab
 
           def has_workflow_rules?
             workflow_rules_config.present?
+          end
+
+          def merge_request_pipeline?
+            @command.merge_request.present?
           end
 
           def workflow_rules_config
