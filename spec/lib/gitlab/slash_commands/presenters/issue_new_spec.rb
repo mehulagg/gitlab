47f67289e4ddd70ea065b3820db5f99f2dbca199
Anton Smith <asmith@gitlab.com>
2021-03-30 02:18:47 +0000

    Apply 3 suggestion(s) to 1 file(s)

--- spec/lib/gitlab/slash_commands/presenters/issue_new_spec.rb
+++ spec/lib/gitlab/slash_commands/presenters/issue_new_spec.rb
@@ -1,20 +1,22 @@
 # frozen_string_literal: true
 
-require 'spec_helper'
-include Rails.application.routes.url_helpers
 
 RSpec.describe Gitlab::SlashCommands::Presenters::IssueNew do
+  include Gitlab::Routing
   let(:project) { create(:project) }
   let(:issue) { create(:issue, project: project) }
-  let(:text) { subject[:text].first }
 
   subject { described_class.new(issue).present }
 
   it { is_expected.to be_a(Hash) }
 
   it 'shows the issue' do
-    expect(subject[:response_type]).to be(:in_channel)
-    expect(subject).to have_key(:text)
-    expect(subject[:text]).to eq("I created an issue on <#{url_for(issue.author)}|#{issue.author.to_reference}>'s behalf: *<#{project_issue_url(issue.project, issue)}|#{issue.to_reference}>* in <#{project.web_url}|#{project.full_name}>")
+    expected_text = "I created an issue on <#{url_for(issue.author)}|#{issue.author.to_reference}>'s behalf: *<#{project_issue_url(issue.project, issue)}|#{issue.to_reference}>* in <#{project.web_url}|#{project.full_name}>"
+
+    expect(subject).to eq(
+      response_type: :in_channel,
+      status: 200,
+      text: expected_text
+    )
   end
 end
