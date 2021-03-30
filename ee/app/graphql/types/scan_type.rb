20aa53bf4d93d5892f1558c8d2103036d9da1bc5
Mehmet Emin INAC <minac@gitlab.com>
2021-03-30 21:47:17 +0200

    Fix `errors` attribute of ScanType on GraphQL

--- ee/app/graphql/types/scan_type.rb
+++ ee/app/graphql/types/scan_type.rb
@@ -3,6 +3,8 @@
 module Types
   # rubocop: disable Graphql/AuthorizeTypes
   class ScanType < BaseObject
+    ERROR_MESSAGE_FORMAT = '[%<type>s] %<message>s'
+
     graphql_name 'Scan'
     description 'Represents the security scan information'
 
@@ -12,7 +14,7 @@ module Types
     field :errors, [GraphQL::STRING_TYPE], null: false, description: 'List of errors.'
 
     def errors
-      object.info['errors'].to_a
+      object.info['errors'].to_a.map { |error| format(ERROR_MESSAGE_FORMAT, error.symbolize_keys) }
     end
   end
 end
--- ee/spec/graphql/types/scan_type_spec.rb
+++ ee/spec/graphql/types/scan_type_spec.rb
@@ -7,4 +7,47 @@ RSpec.describe GitlabSchema.types['Scan'] do
 
   it { expect(described_class).to have_graphql_fields(fields) }
   it { expect(described_class).to require_graphql_authorizations(:read_scan) }
+
+  describe 'serialized values' do
+    let(:user) { create(:user) }
+    let(:build) { create(:ee_ci_build, :dast, name: 'foo') }
+    let(:response) { GitlabSchema.execute(query, context: context, variables: variables).as_json }
+    let(:context) { { current_user: user } }
+    let(:variables) { { projectPath: build.project.full_path, pipelineID: build.pipeline.iid } }
+    let(:expected_dast_data) { { 'name' => 'foo', 'errors' => ['[foo] bar'] } }
+    let(:query) do
+      %(
+        query getScanInfo($projectPath: ID!, $pipelineID: ID!) {
+          project(fullPath: $projectPath) {
+            pipeline(iid: $pipelineID) {
+              securityReportSummary {
+                dast {
+                  vulnerabilitiesCount
+                  scans {
+                    nodes {
+                      name
+                      errors
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }
+      )
+    end
+
+    subject(:scan_data) { response.dig('data', 'project', 'pipeline', 'securityReportSummary', 'dast', 'scans', 'nodes', 0) }
+
+    before do
+      stub_licensed_features(security_dashboard: true)
+
+      build.project.add_developer(user)
+      build.security_scans.first.update!(info: { 'errors' => [{ 'type' => 'foo', 'message' => 'bar' }] })
+    end
+
+    it 'returns the correct data' do
+      expect(scan_data).to eq(expected_dast_data)
+    end
+  end
 end
