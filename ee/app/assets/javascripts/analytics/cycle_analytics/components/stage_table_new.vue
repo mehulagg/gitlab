65f6a593131dc14fe6b05b39284a7cd744aeb2c9
Ezekiel Kigbo <ekigbo@gitlab.com>
2021-03-30 19:56:01 +1100

    [skip ci]

--- ee/app/assets/javascripts/analytics/cycle_analytics/components/stage_table_new.vue
+++ ee/app/assets/javascripts/analytics/cycle_analytics/components/stage_table_new.vue
@@ -44,6 +44,9 @@ export default {
     isMrLink(url = '') {
       return url.includes('/merge_request');
     },
+    itemTitle(item) {
+      return item.title || item.name;
+    },
   },
   fields: [
     { key: 'issues', label: __('Issues'), thClass: 'gl-w-half' },
@@ -54,30 +57,40 @@ export default {
 <template>
   <div data-testid="vsa-stage-table">
     <gl-loading-icon v-if="isLoading" class="gl-mt-4" size="md" />
-    <gl-table v-else :fields="$options.fields" :items="stageEvents">
+    <gl-table
+      v-else
+      head-variant="white"
+      stacked="lg"
+      thead-class="border-bottom"
+      show-empty
+      :fields="$options.fields"
+      :items="stageEvents"
+      :empty-text="__('No members found')"
+    >
       <template #cell(issues)="{ item }">
-        <h5>
-          <gl-link :href="item.url">{{ item.title }}</gl-link>
-        </h5>
-        <ul class="gl-list-style-none">
-          <li>
-            <template v-if="isMrLink(item.url)">
-              <gl-link :href="item.url">!{{ item.iid }}</gl-link>
-            </template>
-            <template v-else>
-              <gl-link :href="item.url">#{{ item.iid }}</gl-link>
-            </template>
-            &middot;
-            <span>
-              {{ s__('OpenedNDaysAgo|Opened') }}
-              <gl-link :href="item.url">{{ item.createdAt }}</gl-link>
-            </span>
-            <span>
-              {{ s__('ByAuthor|by') }}
-              <gl-link :href="item.author.webUrl">{{ item.author.name }}</gl-link>
-            </span>
-          </li>
-        </ul>
+        <div v-if="item.iid">
+          <h5>{{ itemTitle(item) }}</h5>
+          <ul class="gl-list-style-none gl-m-0 gl-p-0 gl-text-black-normal">
+            <li>
+              <template v-if="isMrLink(item.url)">
+                <gl-link :href="item.url">!{{ item.iid }}</gl-link>
+              </template>
+              <template v-else>
+                <gl-link :href="item.url">#{{ item.iid }}</gl-link>
+              </template>
+              <span class="gl-font-lg">&middot;</span>
+              <span>
+                {{ s__('OpenedNDaysAgo|Opened') }}
+                <gl-link :href="item.url">{{ item.createdAt }}</gl-link>
+              </span>
+              <span>
+                {{ s__('ByAuthor|by') }}
+                <gl-link :href="item.author.webUrl">{{ item.author.name }}</gl-link>
+              </span>
+            </li>
+          </ul>
+        </div>
+        <div v-if="item.id"></div>
       </template>
       <template #cell(time)="{ item }">
         <!-- TODO: update the total time function to use the rounding units for the path nav -->
