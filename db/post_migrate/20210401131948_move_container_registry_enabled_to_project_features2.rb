794f103e94b0f85959bb0c725ebffd8a618ebf46
Reuben Pereira <rpereira@gitlab.com>
2021-04-01 19:56:58 +0530

    Reschedule background migration to copy container_registry_enabled
    The previous background migration missed 10 jobs, so this time we
    will be tracking the jobs by passing in the track_jobs: true
    parameter.

---
+++ db/post_migrate/20210401131948_move_container_registry_enabled_to_project_features2.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+class MoveContainerRegistryEnabledToProjectFeatures2 < ActiveRecord::Migration[6.0]
+  include Gitlab::Database::MigrationHelpers
+
+  DOWNTIME = false
+
+  BATCH_SIZE = 30_000
+  MIGRATION = 'MoveContainerRegistryEnabledToProjectFeature'
+
+  disable_ddl_transaction!
+
+  class Project < ActiveRecord::Base
+    include EachBatch
+    self.table_name = 'projects'
+  end
+
+  def up
+    queue_background_migration_jobs_by_range_at_intervals(Project, MIGRATION, 2.minutes, batch_size: BATCH_SIZE, track_jobs: true)
+  end
+
+  def down
+    # no-op
+  end
+end
