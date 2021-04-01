801e8c4711aa9eebfece1c6f1c1a6df6ffbfe5e7
Alishan Ladhani <aladhani@gitlab.com>
2021-04-01 15:23:53 -0400

    Add ee option

--- lib/generators/gitlab/snowplow_event_definition_generator.rb
+++ lib/generators/gitlab/snowplow_event_definition_generator.rb
@@ -5,23 +5,32 @@ require 'rails/generators'
 module Gitlab
   class SnowplowEventDefinitionGenerator < Rails::Generators::Base
     TOP_LEVEL_DIR = 'config'
+    TOP_LEVEL_DIR_EE = 'ee'
 
     source_root File.expand_path('../../../generator_templates/snowplow_event_definition', __dir__)
 
     desc 'Generates a metric definition yml file'
 
-    def create_metric_file
+    class_option :ee, type: :boolean, optional: true, default: false, desc: 'Indicates if event is for ee'
+
+    def create_event_file
       template "event_definition.yml", file_path
     end
 
     private
 
     def file_path
-      File.join(TOP_LEVEL_DIR, 'events', "#{file_name}.yml")
+      path = File.join(TOP_LEVEL_DIR, 'events', "#{file_name}.yml")
+      path = File.join(TOP_LEVEL_DIR_EE, path) if ee?
+      path
     end
 
     def file_name
       "#{Time.now.utc.strftime("%Y%m%d%H%M%S")}"
+    end
+
+    def ee?
+      options[:ee]
     end
   end
 end
