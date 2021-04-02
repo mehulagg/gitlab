6fa02edeeec3aaac0fa5ae29e2e1ea15fc01c2b8
Costel Maxim <cmaxim@gitlab.com>
2021-04-02 12:35:09 +0000

    Update fuzzing template path

--- scripts/review_apps/seed-fuzzing-test-data.sh
+++ scripts/review_apps/seed-fuzzing-test-data.sh
@@ -8,7 +8,7 @@ function create_user() {
     # We set "can_create_group=false" because we don't want the DAST user to create groups.
     # Otherwise, the DAST user likely creates a group and enables 2FA for all group members,
     # which leads to the DAST scan getting "stuck" on the 2FA set up page.
-    # Once https://gitlab.com/gitlab-org/gitlab/-/issues/231447 is resolved, we can use 
+    # Once https://gitlab.com/gitlab-org/gitlab/-/issues/231447 is resolved, we can use
     # DAST_AUTH_EXCLUDE_URLS instead to prevent DAST from enabling 2FA.
     curl --silent --show-error --header "PRIVATE-TOKEN: ${REVIEW_APPS_ROOT_TOKEN}" \
         --data "email=${user}@example.com" \
