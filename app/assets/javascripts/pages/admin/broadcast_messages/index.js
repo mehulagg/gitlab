17ace1e231ab149206130f107a16df96b3fb1034
Mike Greiling <mike@pixelcog.com>
2021-04-02 17:03:00 -0500

    Move js-remove-tr behavior to appropriate page-specific scripts

--- app/assets/javascripts/pages/admin/broadcast_messages/index.js
+++ app/assets/javascripts/pages/admin/broadcast_messages/index.js
@@ -1,3 +1,14 @@
+import $ from 'jquery';
 import initBroadcastMessagesForm from './broadcast_message';
 
-document.addEventListener('DOMContentLoaded', initBroadcastMessagesForm);
+document.addEventListener('DOMContentLoaded', () => {
+  initBroadcastMessagesForm();
+
+  $('.js-remove-tr').on('ajax:before', function removeTRAjaxBeforeCallback() {
+    $(this).parent().find('.btn').addClass('disabled');
+  });
+
+  $('.js-remove-tr').on('ajax:success', function removeTRAjaxSuccessCallback() {
+    $(this).closest('tr').addClass('gl-display-none!');
+  });
+});
