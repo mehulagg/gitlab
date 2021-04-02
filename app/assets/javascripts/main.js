d089cbe41cc18af4961c8d0d83a12fec5af52345
Mike Greiling <mike@pixelcog.com>
2021-04-02 17:03:00 -0500

    Move js-remove-tr behavior to appropriate page-specific scripts

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -115,14 +115,6 @@ function deferredInitialisation() {
     $(this).closest('li').addClass('gl-display-none!');
   });
 
-  $('.js-remove-tr').on('ajax:before', function removeTRAjaxBeforeCallback() {
-    $(this).hide();
-  });
-
-  $('.js-remove-tr').on('ajax:success', function removeTRAjaxSuccessCallback() {
-    $(this).closest('tr').addClass('gl-display-none!');
-  });
-
   const glTooltipDelay = localStorage.getItem('gl-tooltip-delay');
   const delay = glTooltipDelay ? JSON.parse(glTooltipDelay) : 0;
 
--- app/assets/javascripts/pages/admin/spam_logs/index.js
+++ app/assets/javascripts/pages/admin/spam_logs/index.js
@@ -1,13 +1,8 @@
 import $ from 'jquery';
-import UsersSelect from '~/users_select';
-import AbuseReports from './abuse_reports';
-
-new AbuseReports(); /* eslint-disable-line no-new */
-new UsersSelect(); /* eslint-disable-line no-new */
 
 document.addEventListener('DOMContentLoaded', () => {
   $('.js-remove-tr').on('ajax:before', function removeTRAjaxBeforeCallback() {
-    $(this).parent().find('.btn').addClass('disabled');
+    $(this).addClass('disabled');
   });
 
   $('.js-remove-tr').on('ajax:success', function removeTRAjaxSuccessCallback() {
