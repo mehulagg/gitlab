c5d9ae4fd854064be146fe0cbb0145dd89d0963d
Michael Leopard <mleopard@gitlab.com>
2021-04-05 18:54:56 +0000

    Update CiCdSettingsUpdate mutation
    Update mutation to accept MR pipelines and merge trains settings
    Update GraphQL reference to reflect updated payload schema

--- spec/requests/api/graphql/mutations/ci/ci_cd_settings_update_spec.rb
+++ spec/requests/api/graphql/mutations/ci/ci_cd_settings_update_spec.rb
@@ -5,9 +5,13 @@ require 'spec_helper'
 RSpec.describe 'CiCdSettingsUpdate' do
   include GraphqlHelpers
 
-  let_it_be(:project) { create(:project, keep_latest_artifact: true) }
-  let(:variables) { { full_path: project.full_path, keep_latest_artifact: false } }
+  let_it_be(:project) { create(:project, keep_latest_artifact: true, merge_pipelines_enabled: false, merge_trains_enabled: false) }
+  let(:variables) { { full_path: project.full_path, keep_latest_artifact: false, merge_pipelines_enabled: false, merge_trains_enabled: false } }
   let(:mutation) { graphql_mutation(:ci_cd_settings_update, variables) }
+
+  def mutation_response
+    graphql_mutation_response(:ci_cd_settings_update)
+  end
 
   context 'when unauthorized' do
     let(:user) { create(:user) }
@@ -52,6 +56,28 @@ RSpec.describe 'CiCdSettingsUpdate' do
         post_graphql_mutation(mutation, current_user: user)
 
         expect(graphql_errors).not_to be_empty
+      end
+    end
+
+    context 'when merge trains are set to true and merge pipelines are set to false' do
+      let(:variables) { { full_path: project.full_path, merge_pipelines_enabled: false, merge_trains_enabled: true } }
+
+      it 'does not enable merge trains' do
+        post_graphql_mutation(mutation, current_user: user)
+
+        expect(mutation_response.dig('ciCdSettings', 'mergeTrainsEnabled')).to eq(false)
+      end
+    end
+
+    context 'when merge trains and merge pipelines are set to true' do
+      let(:variables) { { full_path: project.full_path, merge_pipelines_enabled: true, merge_trains_enabled: true } }
+
+      it 'enables merge pipelines and merge trains' do
+        post_graphql_mutation(mutation, current_user: user)
+        project.reload
+
+        expect(project.merge_pipelines_enabled).to eq(true)
+        expect(project.merge_trains_enabled).to eq(true)
       end
     end
   end
