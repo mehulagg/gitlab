e921a1b3becc1a0d383e0e5157b2b048a2ef7597
Enrique Alcantara <ealcantara@gitlab.com>
2021-04-06 11:58:26 -0400

    Provide a Markdown renderer to the Content Editor
    Refactor the Content Editor to decouple Markdown serializing
    from the Editor object. The goal is allowing to customize
    Markdown serialization by configuring a separate serialization
    object

---
+++ app/assets/javascripts/content_editor/index.js
@@ -0,0 +1,3 @@
+export { default as createMarkdownSerializer } from './services/markdown_serializer';
+export { default as createEditor } from './services/create_editor';
+export { default as ContentEditor } from './components/content_editor.vue';
--- spec/frontend/content_editor/markdown_processing_spec.js
+++ spec/frontend/content_editor/markdown_processing_spec.js
@@ -1,13 +1,13 @@
-import createEditor from '~/content_editor/services/create_editor';
-import toMarkdown from '~/content_editor/services/to_markdown';
+import { createEditor, createMarkdownSerializer } from '~/content_editor';
 import { loadMarkdownApiExamples, loadMarkdownApiResult } from './markdown_processing_examples';
 
 describe('markdown processing', () => {
   // Ensure we generate same markdown that was provided to Markdown API.
   it.each(loadMarkdownApiExamples())('correctly handles %s', async (testName, markdown) => {
     const { html } = loadMarkdownApiResult(testName);
-    const editor = await createEditor({ content: html });
+    const serializer = createMarkdownSerializer({ render: () => html });
+    const editor = await createEditor({ content: markdown, serializer });
 
-    expect(toMarkdown(editor.state.doc)).toBe(markdown);
+    expect(editor.getSerializedContent()).toBe(markdown);
   });
 });
