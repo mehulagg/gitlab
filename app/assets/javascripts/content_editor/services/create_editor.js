420806fb0c7fe3da8e30985cab127afd7fbf880a
Enrique Alcantara <ealcantara@gitlab.com>
2021-04-06 11:58:26 -0400

    Provide a Markdown renderer to the Content Editor
    Refactor the Content Editor to decouple Markdown serializing
    from the Editor object. The goal is allowing to customize
    Markdown serialization by configuring a separate serializator
    object

--- app/assets/javascripts/content_editor/services/create_editor.js
+++ app/assets/javascripts/content_editor/services/create_editor.js
@@ -1,11 +1,24 @@
 import { Editor } from 'tiptap';
 import { Bold, Code } from 'tiptap-extensions';
 
-const createEditor = ({ content } = {}) => {
-  return new Editor({
+const createEditor = async ({ content, serializer } = {}) => {
+  const editor = new Editor({
     extensions: [new Bold(), new Code()],
-    content,
   });
+
+  editor.setSerializedContent = async (serializedContent) => {
+    editor.setContent(
+      await serializer.deserialize({ schema: editor.schema, content: serializedContent }),
+    );
+  };
+
+  editor.getSerializedContent = () => {
+    return serializer.serialize({ schema: editor.schema, content: editor.getJSON() });
+  };
+
+  await editor.setSerializedContent(content);
+
+  return editor;
 };
 
 export default createEditor;
