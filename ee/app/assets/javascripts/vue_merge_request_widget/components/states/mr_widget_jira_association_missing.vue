46deb59b2c257c17c2504936967000a9a25de472
Jiaan Louw <jlouw@gitlab.com>
2021-04-06 15:16:43 +0200

    Add jira association missing state to MR widget
    - Adds a new MR state jiraAssociationMissing
    - Adds a MrWidgetJiraAssociationMissing component

---
+++ ee/app/assets/javascripts/vue_merge_request_widget/components/states/mr_widget_jira_association_missing.vue
@@ -0,0 +1,29 @@
+<script>
+import { GlButton } from '@gitlab/ui';
+import StatusIcon from '~/vue_merge_request_widget/components/mr_widget_status_icon.vue';
+
+export default {
+  name: 'MrWidgetJiraAssociationMissing',
+  components: {
+    GlButton,
+    StatusIcon,
+  },
+};
+</script>
+<template>
+  <div class="mr-widget-body media">
+    <div class="space-children gl-display-flex">
+      <status-icon status="warning" />
+      <gl-button category="primary" variant="success" disabled size="small">
+        {{ s__('mrWidget|Merge') }}
+      </gl-button>
+    </div>
+    <div class="media-body">
+      <strong class="gl-font-weight-bold gl-text-gray-700 gl-pl-2">
+        {{
+          s__('mrWidget|To merge, a Jira issue key must be mentioned in the title or description.')
+        }}
+      </strong>
+    </div>
+  </div>
+</template>
