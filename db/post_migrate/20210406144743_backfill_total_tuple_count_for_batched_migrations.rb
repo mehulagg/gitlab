d8d8a375e22c3919d4d08847a39c59b0e446e06a
Andreas Brandl <abrandl@gitlab.com>
2021-04-06 16:54:33 +0200

    Add backfill for total_tuple_count

---
+++ db/post_migrate/20210406144743_backfill_total_tuple_count_for_batched_migrations.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+class BackfillTotalTupleCountForBatchedMigrations < ActiveRecord::Migration[6.0]
+  # Set this constant to true if this migration requires downtime.
+  DOWNTIME = false
+
+  def up
+    return unless should_run?
+
+    Gitlab::Database::BackgroundMigration::BatchedMigration.all.each do |migration|
+      total_tuple_count = Gitlab::Database::PgClass.for_table(migration.table_name)&.cardinality_estimate
+
+      migration.update(total_tuple_count: total_tuple_count)
+    end
+  end
+
+  def down
+    return unless should_run?
+
+    Gitlab::Database::BackgroundMigration::BatchedMigration.update_all(total_tuple_count: nil)
+  end
+
+  private
+
+  def should_run?
+    Gitlab.dev_or_test_env? || Gitlab.com?
+  end
+end
