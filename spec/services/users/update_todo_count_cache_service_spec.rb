5b9889c0eea603b4757eab3fe17ad00a2c258354
Markus Koller <mkoller@gitlab.com>
2021-04-06 16:49:31 +0200

    Improve spec setup

--- spec/services/users/update_todo_count_cache_service_spec.rb
+++ spec/services/users/update_todo_count_cache_service_spec.rb
@@ -3,17 +3,11 @@
 require 'spec_helper'
 
 RSpec.describe Users::UpdateTodoCountCacheService do
-  let(:users) { create(:user) }
-  let(:execute_service) { described_class.new(users).execute }
-
   describe '#execute' do
     let_it_be(:user1) { create(:user) }
     let_it_be(:user2) { create(:user) }
 
     before do
-      allow(Rails).to receive(:cache).and_return(memory_store)
-      Rails.cache.clear
-
       user1.update_todos_count_cache
       user2.update_todos_count_cache
 
@@ -23,21 +17,16 @@ RSpec.describe Users::UpdateTodoCountCacheService do
       create(:todo, user: user2, state: :pending)
     end
 
-    let(:memory_store) { ActiveSupport::Cache.lookup_store(:memory_store) }
-
-    it 'updates the todos_counts for users' do
+    it 'updates the todos_counts for users', :use_clean_rails_memory_store_caching do
       expect { described_class.new([user1, user2]).execute }
-        .to change(user1, :todos_done_count)
-        .and change(user1, :todos_pending_count)
-        .and change(user2, :todos_done_count)
-        .and change(user2, :todos_pending_count)
+        .to change(user1, :todos_done_count).from(0).to(1)
+        .and change(user1, :todos_pending_count).from(0).to(1)
+        .and change(user2, :todos_done_count).from(0).to(1)
+        .and change(user2, :todos_pending_count).from(0).to(1)
     end
 
     it 'avoids N+1 queries' do
-      Rails.cache.clear
       control_count = ActiveRecord::QueryRecorder.new { described_class.new([user1]).execute }.count
-
-      Rails.cache.clear
 
       expect { described_class.new([user1, user2]).execute }.not_to exceed_query_limit(control_count)
     end
@@ -53,8 +42,8 @@ RSpec.describe Users::UpdateTodoCountCacheService do
       allow(user1).to receive(:count_cache_validity_period).and_return(1.minute)
       allow(user2).to receive(:count_cache_validity_period).and_return(1.hour)
 
-      expect(memory_store).to receive(:write).with(['users', user1.id, anything], anything, expires_in: 1.minute).twice
-      expect(memory_store).to receive(:write).with(['users', user2.id, anything], anything, expires_in: 1.hour).twice
+      expect(Rails.cache).to receive(:write).with(['users', user1.id, anything], anything, expires_in: 1.minute).twice
+      expect(Rails.cache).to receive(:write).with(['users', user2.id, anything], anything, expires_in: 1.hour).twice
 
       described_class.new([user1, user2]).execute
     end
