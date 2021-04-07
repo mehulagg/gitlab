ffebe636175fd6788de9707e81020f242f3cb642
Angelo Gulina <agulina@gitlab.com>
2021-04-07 12:29:15 +0200

    [skip-ci] Add subscription breakdown view

---
+++ ee/app/assets/javascripts/pages/admin/cloud_licenses/components/subscription_breakdown.vue
@@ -0,0 +1,89 @@
+<script>
+import { GlButton } from '@gitlab/ui';
+import {
+  licensedToHeaderText,
+  manageSubscriptionButtonText,
+  subscriptionDetailsHeaderText,
+  syncSubscriptionButtonText,
+} from '../constants';
+import SubscriptionDetailsCard from './subscription_details_card.vue';
+import SubscriptionDetailsHistory from './subscription_details_history.vue';
+import SubscriptionDetailsUserInfo from './subscription_details_user_info.vue';
+
+export default {
+  i18n: {
+    licensedToHeaderText,
+    manageSubscriptionButtonText,
+    subscriptionDetailsHeaderText,
+    syncSubscriptionButtonText,
+  },
+  name: 'SubscriptionBreakdown',
+  components: {
+    SubscriptionDetailsHistory,
+    GlButton,
+    SubscriptionDetailsCard,
+    SubscriptionDetailsUserInfo,
+  },
+  props: {
+    subscription: {
+      type: Object,
+      required: true,
+    },
+    subscriptionList: {
+      type: Array,
+      required: true,
+    },
+  },
+  data() {
+    return {
+      subscriptionDetailsFields: ['id', 'plan', 'lastSync', 'startsAt', 'renews'],
+      licensedToFields: ['name', 'email', 'company'],
+    };
+  },
+  computed: {
+    hasSubscription() {
+      return Boolean(Object.keys(this.subscription).length);
+    },
+    hasSubscriptionHistory() {
+      return Boolean(this.subscriptionList.length);
+    },
+  },
+};
+</script>
+
+<template>
+  <div>
+    <section class="row gl-mb-5">
+      <div class="col-md-6 gl-mb-5">
+        <subscription-details-card
+          :details-fields="subscriptionDetailsFields"
+          :header-text="$options.i18n.subscriptionDetailsHeaderText"
+          :subscription="subscription"
+        >
+          <template #footer>
+            <gl-button category="primary" variant="confirm">
+              {{ $options.i18n.syncSubscriptionButtonText }}
+            </gl-button>
+            <gl-button>
+              {{ $options.i18n.manageSubscriptionButtonText }}
+            </gl-button>
+          </template>
+        </subscription-details-card>
+      </div>
+
+      <div class="col-md-6 gl-mb-5">
+        <subscription-details-card
+          :details-fields="licensedToFields"
+          :header-text="$options.i18n.licensedToHeaderText"
+          :subscription="subscription"
+        />
+      </div>
+    </section>
+    <subscription-details-user-info v-if="hasSubscription" :subscription="subscription" />
+    <subscription-details-history
+      v-if="hasSubscriptionHistory"
+      :current-subscription-id="subscription.id"
+      :subscription-list="subscriptionList"
+    />
+  </div>
+</template>
