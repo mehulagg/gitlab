eefc4495564d7e0a78b12a39299f1ef9a1791d14
Felipe Artur <felipefac@gmail.com>
2021-04-07 16:02:55 -0300

    Allow to update epic board lists
    Introduce EpicLists::UpdateService

---
+++ ee/spec/services/boards/epic_lists/update_service_spec.rb
@@ -0,0 +1,50 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+RSpec.describe Boards::EpicLists::UpdateService do
+  let(:user) { create(:user) }
+  let!(:list) { create(:epic_list, epic_board: board, position: 0) }
+
+  before do
+    stub_licensed_features(epics: true)
+  end
+
+  describe '#execute' do
+    let(:service) { described_class.new(board.resource_parent, user, params) }
+
+    # context 'when position parameter is present' do
+    #   let(:params) { { position: 1 } }
+
+    #   context 'for projects' do
+    #     let(:project) { create(:project, :private) }
+    #     let(:board) { create(:board, project: project) }
+
+    #     it_behaves_like 'moving list'
+    #   end
+    # end
+
+    context 'when collapsed parameter is present' do
+      let(:params) { { collapsed: true } }
+
+      context 'for projects' do
+        let(:group) { create(:group, :private) }
+        let(:board) { create(:epic_board, group: group) }
+
+        it_behaves_like 'updating list preferences'
+      end
+    end
+
+    # context 'when position and collapsed are both present' do
+    #   let(:params) { { collapsed: true, position: 1 } }
+
+    #   context 'for projects' do
+    #     let(:project) { create(:project, :private) }
+    #     let(:board) { create(:board, project: project) }
+
+    #     it_behaves_like 'moving list'
+    #     it_behaves_like 'updating list preferences'
+    #   end
+    # end
+  end
+end
