3f9959166032191af8a93d2063ab17359162b517
Rémy Coutable <remy@rymai.me>
2021-04-08 16:02:39 +0200

    Patch Knapsack to take context timing into account
    Signed-off-by: Rémy Coutable <remy@rymai.me>

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -49,6 +49,39 @@ end
 
 if ENV['CI'] && ENV['KNAPSACK_GENERATE_REPORT'] && !ENV['NO_KNAPSACK']
   require 'knapsack'
+
+  module Knapsack
+    module Adapters
+      class RSpecAdapter < BaseAdapter
+        def bind_time_tracker
+          ::RSpec.configure do |config|
+            # Original version is `config.prepend_before(:each) do`: https://github.com/KnapsackPro/knapsack/blob/v1.17.0/lib/knapsack/adapters/rspec_adapter.rb#L9
+            config.prepend_before(:context) do
+              current_example_group =
+                if ::RSpec.respond_to?(:current_example)
+                  ::RSpec.current_example.metadata[:example_group]
+                else
+                  example.metadata
+                end
+
+              Knapsack.tracker.test_path = RSpecAdapter.test_path(current_example_group)
+              Knapsack.tracker.start_timer
+            end
+
+            # Original version is `config.append_after(:each) do`: https://github.com/KnapsackPro/knapsack/blob/v1.17.0/lib/knapsack/adapters/rspec_adapter.rb#L20
+            config.append_after(:context) do
+              Knapsack.tracker.stop_timer
+            end
+
+            config.after(:suite) do
+              Knapsack.logger.info(Presenter.global_time)
+            end
+          end
+        end
+      end
+    end
+  end
+
   Knapsack::Adapters::RSpecAdapter.bind
 end
 
