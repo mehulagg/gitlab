e219aa38db21579f901f4a1f80d1454c8d0da49c
Francisco Javier López <fjlopez@gitlab.com>
2021-04-08 11:39:11 +0200

    Updating code and structure

--- app/models/sidebars/panel.rb
+++ app/models/sidebars/panel.rb
@@ -55,5 +55,21 @@ module Sidebars
     def container
       context.container
     end
+
+    # Auxiliar method that helps with the migration from
+    # regular views to the new logic
+    def render_raw_scope_menu_partial
+      # No-op
+    end
+
+    # Auxiliar method that helps with the migration from
+    # regular views to the new logic.
+    #
+    # Any menu inside this partial will be added after
+    # all the menus added in the `configure_menus`
+    # method.
+    def render_raw_menus_partial
+      # No-op
+    end
   end
 end
