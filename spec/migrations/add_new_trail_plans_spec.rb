a12d887b954846faaacb2837a9b53d49e339d74b
Shreyas Agarwal <sagarwal@gitlab.com>
2021-04-09 17:07:36 +0500

    Fix the rubocop smells

--- spec/migrations/add_new_trail_plans_spec.rb
+++ spec/migrations/add_new_trail_plans_spec.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require 'spec_helper'
 
 require_migration!
@@ -10,7 +12,7 @@ RSpec.describe AddNewTrailPlans, :migration do
 
     it 'creates 2 entries within the plans table' do
       expect { migrate! }.to change { AddNewTrailPlans::Plan.count }.by 2
-      expect(AddNewTrailPlans::Plan.last(2).pluck(:name)).to match_array(['ultimate_trial', 'premium_trial'])
+      expect(AddNewTrailPlans::Plan.last(2).pluck(:name)).to match_array(%w(ultimate_trial premium_trial))
     end
 
     it 'creates 2 entries for plan limits' do
@@ -22,19 +24,18 @@ RSpec.describe AddNewTrailPlans, :migration do
     before do
       table(:plans).create!(name: 'random')
       allow(Gitlab).to receive(:dev_env_org_or_com?).and_return true
-
     end
 
     it 'removes the newly added ultimate and premium trial entries' do
       reversible_migration do |migration|
         migration.before -> {
-          expect(AddNewTrailPlans::Plan.count).to eql(1)
-          expect(AddNewTrailPlans::PlanLimits.count).to eql(0)
+          expect(AddNewTrailPlans::Plan.count).to be(1)
+          expect(AddNewTrailPlans::PlanLimits.count).to be(0)
         }
 
         migration.after -> {
-          expect(AddNewTrailPlans::Plan.count).to eql(3)
-          expect(AddNewTrailPlans::PlanLimits.count).to eql(2)
+          expect(AddNewTrailPlans::Plan.count).to be(3)
+          expect(AddNewTrailPlans::PlanLimits.count).to be(2)
         }
       end
     end
