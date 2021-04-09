733ceca38878578bbb542d13f6c10872542dddf7
Albert Salim <asalim@gitlab.com>
2021-04-09 15:30:21 +0800

    Include context in spec time tracking

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -49,7 +49,45 @@ end
 
 if ENV['CI'] && ENV['KNAPSACK_GENERATE_REPORT'] && !ENV['NO_KNAPSACK']
   require 'knapsack'
-  Knapsack::Adapters::RSpecAdapter.bind
+
+  module Tooling
+    class GitLabKnapsackRspecAdapter < Knapsack::Adapters::BaseAdapter
+      def bind_time_tracker
+        ::RSpec.configure do |config|
+          # Original version starts timer in `config.prepend_before(:each) do`
+          # https://github.com/KnapsackPro/knapsack/blob/v1.17.0/lib/knapsack/adapters/rspec_adapter.rb#L9
+          config.prepend_before(:context) do
+            Knapsack.tracker.start_timer
+          end
+
+          # Original version is `config.prepend_before(:each) do`
+          # https://github.com/KnapsackPro/knapsack/blob/v1.17.0/lib/knapsack/adapters/rspec_adapter.rb#L9
+          config.prepend_before(:each) do # rubocop:disable RSpec/HookArgument
+            current_example_group =
+              if ::RSpec.respond_to?(:current_example)
+                ::RSpec.current_example.metadata[:example_group]
+              else
+                example.metadata
+              end
+
+            Knapsack.tracker.test_path = RSpecAdapter.test_path(current_example_group)
+          end
+
+          # Original version stops timer in `config.append_after(:each) do`
+          # https://github.com/KnapsackPro/knapsack/blob/v1.17.0/lib/knapsack/adapters/rspec_adapter.rb#L20
+          config.append_after(:context) do
+            Knapsack.tracker.stop_timer
+          end
+
+          config.after(:suite) do
+            Knapsack.logger.info(Presenter.global_time)
+          end
+        end
+      end
+    end
+  end
+
+  Tooling::GitLabKnapsackRspecAdapter.bind
 end
 
 # require rainbow gem String monkeypatch, so we can test SystemChecks
@@ -81,7 +119,7 @@ RSpec.configure do |config|
   config.run_all_when_everything_filtered = true
 
   config.use_transactional_fixtures = true
-  config.use_instantiated_fixtures  = false
+  config.use_instantiated_fixtures = false
   config.fixture_path = Rails.root
 
   config.verbose_retry = true
