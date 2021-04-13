512731d98d60c6c9ae8fa1ba6ba7f08fd9ffc015
Simon Knox <simon@gitlab.com>
2021-04-13 10:31:38 +1000

    Add boilerplate for iteration cadences form
    Adds route, controller, and template file to simplify reviewing

---
+++ ee/app/controllers/groups/cadences_controller.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+class Groups::CadencesController < Groups::ApplicationController
+  before_action :check_cadences_available!
+  before_action :authorize_show_cadence!, only: [:index]
+  before_action :authorize_create_cadence!, only: [:new]
+
+  feature_category :issue_tracking
+
+  def new; end
+
+  def index; end
+
+  private
+
+  def check_cadences_available!
+    render_404 unless group.iteration_cadences_feature_flag_enabled?
+  end
+
+  def authorize_create_cadence!
+    render_404 unless can?(current_user, :create_iteration, group)
+  end
+
+  def authorize_show_cadence!
+    render_404 unless can?(current_user, :create_iteration, group)
+  end
+end
