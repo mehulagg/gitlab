c0294a6c2cfd8c5371ff4f5a66a4f3b3bc2778b0
Thomas Randolph <trandolph@gitlab.com>
2021-04-12 22:39:03 -0600

    Identify merge requests

--- app/assets/javascripts/diffs/utils/merge_request.js
+++ app/assets/javascripts/diffs/utils/merge_request.js
@@ -1,3 +1,5 @@
+import { uuids } from "./uuids";
+
 const endpointRE = /^(\/?(.+?)\/(.+?)\/-\/merge_requests\/(\d+)).*$/i;
 
 export function getDerivedMergeRequestInformation({ endpoint } = {}) {
@@ -17,4 +19,14 @@ export function getDerivedMergeRequestInformation({ endpoint } = {}) {
     project,
     id,
   };
+}
+
+export function identifier( { metadata } ){
+  const { userOrGroup, project, id } = getDerivedMergeRequestInformation({ "endpoint": metadata.latest_version_path });
+  // This is a *count* not an index! Version 2 is index 2! If there's only 1 version, it's null!
+  const version = metadata.merge_request_diff.version_index || 1;
+
+  return uuids({
+    seeds: [userOrGroup, project, id, version],
+  })[0];
 }
