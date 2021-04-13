ecb286720d8de997d9958737b4ac5bb120850ece
Ammar Alakkad <am.alakkad@gmail.com>
2021-04-13 08:06:55 +0300

    Add extra blocks to subscripton activateion form

--- ee/app/assets/javascripts/pages/admin/cloud_licenses/components/subscription_activation_form.vue
+++ ee/app/assets/javascripts/pages/admin/cloud_licenses/components/subscription_activation_form.vue
@@ -25,6 +25,7 @@ export default {
     GlSprintf,
     GlLink,
   },
+  inject: ['freeTrialPath', 'buySubscriptionPath'],
   data() {
     return {
       activationCode: null,
@@ -72,65 +73,116 @@ export default {
 };
 </script>
 <template>
-  <gl-card>
-    <template #header>
-      <h5 class="gl-my-0 gl-font-weight-bold">{{ s__('CloudLicense|Activate subscription') }}</h5>
-    </template>
+  <div>
+    <gl-card>
+      <template #header>
+        <h5 class="gl-my-0 gl-font-weight-bold">{{ s__('CloudLicense|Activate subscription') }}</h5>
+      </template>
 
-    <p>
-      <gl-sprintf
-        :message="
-          s__('CloudLicense|Learn how to %{linkStart}activate your subscription%{linkEnd}.')
-        "
-      >
-        <template #link="{ content }">
-          <gl-link href="" target="_blank">{{ content }}</gl-link>
-        </template>
-      </gl-sprintf>
-    </p>
-    <gl-form @submit.stop.prevent="submit">
-      <gl-form-group class="gl-mb-0">
-        <div class="gl-display-flex gl-flex-wrap">
-          <label class="gl-w-full" for="activation-code-group">
-            {{ s__('CloudLicense|Activation code') }}
-          </label>
-          <gl-form-input
-            id="activation-code-group"
-            v-model="activationCode"
-            :disabled="isLoading"
-            :placeholder="s__('CloudLicense|Paste your activation code')"
-            class="gl-w-full gl-mb-4"
-            required
-          />
+      <p>
+        <gl-sprintf
+          :message="
+            s__('CloudLicense|Learn how to %{linkStart}activate your subscription%{linkEnd}.')
+          "
+        >
+          <template #link="{ content }">
+            <gl-link href="" target="_blank">{{ content }}</gl-link>
+          </template>
+        </gl-sprintf>
+      </p>
+      <gl-form @submit.stop.prevent="submit">
+        <gl-form-group class="gl-mb-0">
+          <div class="gl-display-flex gl-flex-wrap">
+            <label class="gl-w-full" for="activation-code-group">
+              {{ s__('CloudLicense|Activation code') }}
+            </label>
+            <gl-form-input
+              id="activation-code-group"
+              v-model="activationCode"
+              :disabled="isLoading"
+              :placeholder="s__('CloudLicense|Paste your activation code')"
+              class="gl-w-full gl-mb-4"
+              required
+            />
 
-          <gl-form-checkbox v-model="termsAccepted">
-            <gl-sprintf
-              :message="
-                s__(
-                  'CloudLicense|I agree that my use of the GitLab Software is subject to the Subscription Agreement located at the %{linkStart}Terms of Service%{linkEnd}, unless otherwise agreed to in writing with GitLab.',
-                )
-              "
+            <gl-form-checkbox v-model="termsAccepted">
+              <gl-sprintf
+                :message="
+                  s__(
+                    'CloudLicense|I agree that my use of the GitLab Software is subject to the Subscription Agreement located at the %{linkStart}Terms of Service%{linkEnd}, unless otherwise agreed to in writing with GitLab.',
+                  )
+                "
+              >
+                <template #link="{ content }">
+                  <gl-link href="https://about.gitlab.com/terms/" target="_blank">{{
+                    content
+                  }}</gl-link>
+                </template>
+              </gl-sprintf>
+            </gl-form-checkbox>
+
+            <gl-button
+              :disabled="activateButtonDisabled"
+              category="primary"
+              class="gl-mt-6"
+              data-testid="activate-button"
+              type="submit"
+              variant="confirm"
             >
-              <template #link="{ content }">
-                <gl-link href="https://about.gitlab.com/terms/" target="_blank">{{
-                  content
-                }}</gl-link>
-              </template>
-            </gl-sprintf>
-          </gl-form-checkbox>
+              {{ s__('CloudLicense|Activate') }}
+            </gl-button>
+          </div>
+        </gl-form-group>
+      </gl-form>
+    </gl-card>
 
+    <div class="row gl-mt-7">
+      <div class="col-lg-6">
+        <gl-card>
+          <template #header>
+            <h5 class="gl-my-0 gl-font-weight-bold">{{ s__('CloudLicense|Free trial') }}</h5>
+          </template>
+          <p>
+            {{
+              s__(
+                'CloudLicense|You can start a free trial of GitLab Ultimate without any obligation or payment details.',
+              )
+            }}
+          </p>
           <gl-button
-            :disabled="activateButtonDisabled"
-            category="primary"
+            category="secondary"
             class="gl-mt-6"
-            data-testid="activate-button"
-            type="submit"
+            data-testid="free-trial-button"
             variant="confirm"
+            :href="freeTrialPath"
           >
-            {{ s__('CloudLicense|Activate') }}
+            {{ s__('CloudLicense|Start free trial') }}
           </gl-button>
-        </div>
-      </gl-form-group>
-    </gl-form>
-  </gl-card>
+        </gl-card>
+      </div>
+      <div class="col-lg-6">
+        <gl-card>
+          <template #header>
+            <h5 class="gl-my-0 gl-font-weight-bold">{{ s__('CloudLicense|Subscription') }}</h5>
+          </template>
+          <p>
+            {{
+              s__(
+                'CloudLicense|Ready to get started? A GitLab plan is ideal for scaling organizations and for multi team usage.',
+              )
+            }}
+          </p>
+          <gl-button
+            category="secondary"
+            class="gl-mt-6"
+            data-testid="buy-subscription-button"
+            variant="confirm"
+            :href="buySubscriptionPath"
+          >
+            {{ s__('CloudLicense|Buy subscription') }}
+          </gl-button>
+        </gl-card>
+      </div>
+    </div>
+  </div>
 </template>
