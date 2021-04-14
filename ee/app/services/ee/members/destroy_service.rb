f02dd52391edde3d42a22b1a7324350262fd75cc
Sean Arnold <sarnold@gitlab.com>
2021-04-14 12:44:43 +1200

    Remove from rotations if removed from group/proj
    - Add scope
    - Add new service
    - Add specs

--- ee/app/services/ee/members/destroy_service.rb
+++ ee/app/services/ee/members/destroy_service.rb
@@ -14,6 +14,7 @@ module EE
 
         cleanup_group_identity(member)
         cleanup_group_deletion_schedule(member) if member.source.is_a?(Group)
+        cleanup_oncall_rotations(member)
       end
 
       private
@@ -48,6 +49,16 @@ module EE
         return unless deletion_schedule
 
         deletion_schedule.destroy if deletion_schedule.deleting_user == member.user
+      end
+
+      def cleanup_oncall_rotations(member)
+        user = member.user
+        user.oncall_rotations.each do |rotation|
+          ::IncidentManagement::OncallRotations::RemoveParticipantService.new(
+            rotation,
+            user
+          ).execute
+        end
       end
     end
   end
