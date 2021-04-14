f62b47472783a61131343188c15983c45cb07f08
mlunoe <michael.lunoe@gmail.com>
2021-04-14 11:45:02 +0200

    Refactor(CI Minutes): use customer graphql client

--- ee/app/assets/javascripts/subscriptions/buy_minutes/index.js
+++ ee/app/assets/javascripts/subscriptions/buy_minutes/index.js
@@ -1,10 +1,8 @@
 import Vue from 'vue';
-import ensureData from '~/ensure_data';
 import { parseBoolean } from '~/lib/utils/common_utils';
 import App from './components/app.vue';
 import apolloProvider from './graphql';
 import seedQuery from './graphql/queries/seed.query.graphql';
-import { parseData } from './utils';
 
 const arrayToGraphqlArray = (arr, typename) =>
   Array.from(arr, (item) => Object.assign(item, { __typename: typename }));
@@ -15,8 +13,6 @@ const writeInitialDataToApolloProvider = (dataset) => {
   apolloProvider.clients.defaultClient.cache.writeQuery({
     query: seedQuery,
     data: {
-      // eslint-disable-next-line @gitlab/require-i18n-strings
-      plans: arrayToGraphqlArray(JSON.parse(dataset.ciMinutesPlans), 'Plan'),
       // eslint-disable-next-line @gitlab/require-i18n-strings
       namespaces: arrayToGraphqlArray(JSON.parse(dataset.groupData), 'Namespace'),
       newUser: parseBoolean(newUser),
@@ -31,18 +27,13 @@ export default (el) => {
     return null;
   }
 
-  const ExtendedApp = ensureData(App, {
-    parseData,
-    data: el.dataset,
-  });
-
   writeInitialDataToApolloProvider(el.dataset);
 
   return new Vue({
     el,
     apolloProvider,
     render(createElement) {
-      return createElement(ExtendedApp);
+      return createElement(App);
     },
   });
 };
--- ee/spec/frontend/subscriptions/buy_minutes/index_spec.js
+++ ee/spec/frontend/subscriptions/buy_minutes/index_spec.js
@@ -1,13 +1,7 @@
-import { GlEmptyState } from '@gitlab/ui';
-import * as Sentry from '@sentry/browser';
 import { createWrapper } from '@vue/test-utils';
 
 import initBuyMinutesApp from 'ee/subscriptions/buy_minutes';
-import * as utils from 'ee/subscriptions/buy_minutes/utils';
 import StepOrderApp from 'ee/vue_shared/purchase_flow/components/step_order_app.vue';
-import { mockCiMinutesPlans, mockParsedCiMinutesPlans } from './mock_data';
-
-jest.mock('ee/subscriptions/buy_minutes/utils');
 
 describe('initBuyMinutesApp', () => {
   let vm;
@@ -15,14 +9,10 @@ describe('initBuyMinutesApp', () => {
 
   function createComponent() {
     const el = document.createElement('div');
-    Object.assign(el.dataset, { ciMinutesPlans: mockCiMinutesPlans, groupData: '[]' });
+    Object.assign(el.dataset, { groupData: '[]' });
     vm = initBuyMinutesApp(el).$mount();
     wrapper = createWrapper(vm);
   }
-
-  beforeEach(() => {
-    Sentry.captureException = jest.fn();
-  });
 
   afterEach(() => {
     if (vm) {
@@ -30,29 +20,10 @@ describe('initBuyMinutesApp', () => {
     }
     wrapper.destroy();
     vm = null;
-    Sentry.captureException.mockClear();
-    utils.parseData.mockClear();
   });
 
-  describe('when parsing fails', () => {
-    it('displays the EmptyState', () => {
-      utils.parseData.mockImplementation(() => {
-        throw new Error();
-      });
-      createComponent();
-      expect(wrapper.find(StepOrderApp).exists()).toBe(false);
-      expect(wrapper.find(GlEmptyState).exists()).toBe(true);
-      expect(Sentry.captureException).not.toHaveBeenCalled();
-    });
-  });
-
-  describe('when parsing succeeds', () => {
-    it('displays the StepOrderApp', () => {
-      utils.parseData.mockImplementation(() => mockParsedCiMinutesPlans);
-      createComponent();
-      expect(wrapper.find(GlEmptyState).exists()).toBe(false);
-      expect(wrapper.find(StepOrderApp).exists()).toBe(true);
-      expect(Sentry.captureException).not.toHaveBeenCalled();
-    });
+  it('displays the StepOrderApp', () => {
+    createComponent();
+    expect(wrapper.find(StepOrderApp).exists()).toBe(true);
   });
 });
