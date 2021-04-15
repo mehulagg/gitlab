9552f64562f12ba40b84c15f5a0e8b745bc71f8c
Chad Woolley <cwoolley@gitlab.com>
2021-04-15 13:53:23 -0700

    WIP - change CreateRequirementService superclass

--- ee/app/services/requirements_management/create_requirement_service.rb
+++ ee/app/services/requirements_management/create_requirement_service.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 module RequirementsManagement
-  class CreateRequirementService < BaseService
+  class CreateRequirementService < ::Projects::BaseProjectService
     include Gitlab::Allowable
 
     def execute
