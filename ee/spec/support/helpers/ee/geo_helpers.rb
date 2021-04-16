c964106385fab4ebe9011e85197129d44cce3901
Igor Drozdov <idrozdov@gitlab.com>
2021-04-16 17:56:51 +0300

    Avoid stubbing object geo node
    Stubbing an object restricts us from using let_it_be
    because stubbing is not allowed on a frozen object

--- ee/spec/support/helpers/ee/geo_helpers.rb
+++ ee/spec/support/helpers/ee/geo_helpers.rb
@@ -4,7 +4,6 @@ module EE
   module GeoHelpers
     def stub_current_geo_node(node)
       allow(::Gitlab::Geo).to receive(:current_node).and_return(node)
-      allow(node).to receive(:current?).and_return(true) unless node.nil?
     end
 
     def stub_current_node_name(name)
@@ -19,14 +18,6 @@ module EE
     def stub_secondary_node
       allow(::Gitlab::Geo).to receive(:primary?).and_return(false)
       allow(::Gitlab::Geo).to receive(:secondary?).and_return(true)
-    end
-
-    def stub_node_disabled(node)
-      allow(node).to receive(:enabled?).and_return(false)
-    end
-
-    def stub_selective_sync(node, value)
-      allow(node).to receive(:selective_sync?).and_return(value)
     end
 
     def create_project_on_shard(shard_name)
