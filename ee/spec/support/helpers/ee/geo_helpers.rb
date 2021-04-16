d0a31fd0a0c53c6d60a98ba8dc32161be7acb554
Igor Drozdov <idrozdov@gitlab.com>
2021-04-16 17:56:51 +0300

    Set current node without stubbing object

--- ee/spec/support/helpers/ee/geo_helpers.rb
+++ ee/spec/support/helpers/ee/geo_helpers.rb
@@ -3,8 +3,7 @@
 module EE
   module GeoHelpers
     def stub_current_geo_node(node)
-      allow(::Gitlab::Geo).to receive(:current_node).and_return(node)
-      allow(node).to receive(:current?).and_return(true) unless node.nil?
+      stub_current_node_name(node.name)
     end
 
     def stub_current_node_name(name)
