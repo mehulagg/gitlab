e37349f156ea80766f080657f969e84e3bf6dcc5
Thong Kuah <tkuah@gitlab.com>
2021-04-19 08:58:57 +1200

    Freeze let_it_be when factory_default is keep

--- spec/support/let_it_be.rb
+++ spec/support/let_it_be.rb
@@ -1,9 +1,18 @@
 # frozen_string_literal: true
 
 TestProf::LetItBe.configure do |config|
-  config.alias_to :let_it_be_with_refind, refind: true
-end
+  # The :freeze modifier actually deep freezes the record and currently loaded
+  # associations, which results in too many failures
+  config.register_modifier :light_freeze do |record, val|
+    next record unless val
 
-TestProf::LetItBe.configure do |config|
+    if record.respond_to?(:freeze)
+      record.freeze
+    end
+
+    record
+  end
+
+  config.alias_to :let_it_be_with_refind, refind: true
   config.alias_to :let_it_be_with_reload, reload: true
 end
