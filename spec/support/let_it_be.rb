079e398ff1eb1c35227791e82dd411b7fd70ab8c
Thong Kuah <tkuah@gitlab.com>
2021-04-19 09:20:53 +1200

    Lightly freeze let_it_be objects instead

--- spec/support/let_it_be.rb
+++ spec/support/let_it_be.rb
@@ -1,7 +1,19 @@
 # frozen_string_literal: true
 
 TestProf::LetItBe.configure do |config|
-  config.default_modifiers[:freeze] = true
+  # The :freeze modifier actually deep freezes the record and currently loaded
+  # associations, which results in too many failures
+  config.register_modifier :light_freeze do |record, val|
+    next record unless val
+
+    if record.is_a?(::ActiveRecord::Base)
+      record.freeze
+    end
+
+    record
+  end
+
+  config.default_modifiers[:light_freeze] = true
 
   config.alias_to :let_it_be_with_refind, refind: true
   config.alias_to :let_it_be_with_reload, reload: true
