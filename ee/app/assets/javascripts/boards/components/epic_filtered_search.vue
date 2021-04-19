e47c9e89b8d82edab287f09d36c02279adc1fa59
sstern <sstern@gitlab.com>
2021-04-19 15:19:05 -0700

    WIP

--- ee/app/assets/javascripts/boards/components/epic_filtered_search.vue
+++ ee/app/assets/javascripts/boards/components/epic_filtered_search.vue
@@ -1,8 +1,6 @@
 <script>
-import { mapActions, mapState } from 'vuex';
-import { updateHistory, setUrlParams } from '~/lib/utils/url_utility';
+import BoardFilteredSearch from '~/boards/components/board_filtered_search.vue';
 import { __ } from '~/locale';
-import FilteredSearch from '~/vue_shared/components/filtered_search_bar/filtered_search_bar_root.vue';
 import AuthorToken from '~/vue_shared/components/filtered_search_bar/tokens/author_token.vue';
 import LabelToken from '~/vue_shared/components/filtered_search_bar/tokens/label_token.vue';
 import groupLabelsQuery from '../graphql/group_labels.query.graphql';
@@ -14,15 +12,9 @@ export default {
     label: __('Label'),
     author: __('Author'),
   },
-  components: { FilteredSearch },
-  inject: ['initialFilterParams'],
-  data() {
-    return {
-      filterParams: this.initialFilterParams,
-    };
-  },
+  components: { BoardFilteredSearch },
+  inject: ['fullPath'],
   computed: {
-    ...mapState(['fullPath']),
     tokens() {
       return [
         {
@@ -47,74 +39,8 @@ export default {
         },
       ];
     },
-    urlParams() {
-      const { authorUsername, labelName, search } = this.filterParams;
-
-      return {
-        author_username: authorUsername,
-        'label_name[]': labelName,
-        search,
-      };
-    },
   },
   methods: {
-    ...mapActions(['performSearch']),
-    getFilteredSearchValue() {
-      const { authorUsername, labelName, search } = this.filterParams;
-      const filteredSearchValue = [];
-
-      if (authorUsername) {
-        filteredSearchValue.push({
-          type: 'author_username',
-          value: { data: authorUsername },
-        });
-      }
-
-      if (labelName?.length) {
-        filteredSearchValue.push(
-          ...labelName.map((label) => ({
-            type: 'label_name',
-            value: { data: label },
-          })),
-        );
-      }
-
-      if (search) {
-        filteredSearchValue.push(search);
-      }
-
-      return filteredSearchValue;
-    },
-    getFilterParams(filters = []) {
-      const filterParams = {};
-      const labels = [];
-      const plainText = [];
-
-      filters.forEach((filter) => {
-        switch (filter.type) {
-          case 'author_username':
-            filterParams.authorUsername = filter.value.data;
-            break;
-          case 'label_name':
-            labels.push(filter.value.data);
-            break;
-          case 'filtered-search-term':
-            if (filter.value.data) plainText.push(filter.value.data);
-            break;
-          default:
-            break;
-        }
-      });
-
-      if (labels.length) {
-        filterParams.labelName = labels;
-      }
-
-      if (plainText.length) {
-        filterParams.search = plainText.join(' ');
-      }
-      return filterParams;
-    },
     fetchAuthors(authorsSearchTerm) {
       return this.$apollo
         .query({
@@ -137,28 +63,10 @@ export default {
         })
         .then(({ data }) => data.group?.labels.nodes || []);
     },
-    handleFilterEpics(filters) {
-      this.filterParams = this.getFilterParams(filters);
-      updateHistory({
-        url: setUrlParams(this.urlParams, window.location.href, true, false, true),
-        title: document.title,
-        replace: true,
-      });
-
-      this.performSearch();
-    },
   },
 };
 </script>
 
 <template>
-  <filtered-search
-    data-testid="epic-filtered-search"
-    class="gl-w-full"
-    namespace=""
-    :tokens="tokens"
-    :search-input-placeholder="$options.i18n.search"
-    :initial-filter-value="getFilteredSearchValue()"
-    @onFilter="handleFilterEpics"
-  />
+  <board-filtered-search :tokens="tokens" />
 </template>
---
+++ spec/frontend/boards/components/board_filtered_search_spec.js
@@ -0,0 +1,139 @@
+import { createLocalVue, shallowMount } from '@vue/test-utils';
+import Vuex from 'vuex';
+import EpicFilteredSearch from 'ee_component/boards/components/epic_filtered_search.vue';
+import { createStore } from '~/boards/stores';
+import * as urlUtility from '~/lib/utils/url_utility';
+import { __ } from '~/locale';
+import FilteredSearchBarRoot from '~/vue_shared/components/filtered_search_bar/filtered_search_bar_root.vue';
+import AuthorToken from '~/vue_shared/components/filtered_search_bar/tokens/author_token.vue';
+import LabelToken from '~/vue_shared/components/filtered_search_bar/tokens/label_token.vue';
+
+const localVue = createLocalVue();
+localVue.use(Vuex);
+
+describe('BoardFilteredSearch', () => {
+  let wrapper;
+  let store;
+
+  const createComponent = ({ initialFilterParams = {} } = {}) => {
+    wrapper = shallowMount(EpicFilteredSearch, {
+      localVue,
+      provide: { initialFilterParams },
+      store,
+    });
+  };
+
+  const findFilteredSearch = () => wrapper.findComponent(FilteredSearchBarRoot);
+
+  beforeEach(() => {
+    // this needed for actions call for performSearch
+    window.gon = { features: {} };
+  });
+
+  afterEach(() => {
+    wrapper.destroy();
+  });
+
+  describe('default', () => {
+    beforeEach(() => {
+      store = createStore();
+
+      jest.spyOn(store, 'dispatch');
+
+      createComponent();
+    });
+
+    it('renders FilteredSearch', () => {
+      expect(findFilteredSearch().exists()).toBe(true);
+    });
+
+    it('passes the correct tokens to FilteredSearch', () => {
+      const tokens = [
+        {
+          icon: 'labels',
+          title: __('Label'),
+          type: 'label_name',
+          operators: [{ value: '=', description: 'is' }],
+          token: LabelToken,
+          unique: false,
+          symbol: '~',
+          fetchLabels: wrapper.vm.fetchLabels,
+        },
+        {
+          icon: 'pencil',
+          title: __('Author'),
+          type: 'author_username',
+          operators: [{ value: '=', description: 'is' }],
+          symbol: '@',
+          token: AuthorToken,
+          unique: true,
+          fetchAuthors: wrapper.vm.fetchAuthors,
+        },
+      ];
+
+      expect(findFilteredSearch().props('tokens')).toEqual(tokens);
+    });
+
+    describe('when onFilter is emitted', () => {
+      it('calls performSearch', () => {
+        findFilteredSearch().vm.$emit('onFilter', [{ value: { data: '' } }]);
+
+        expect(store.dispatch).toHaveBeenCalledWith('performSearch');
+      });
+
+      it('calls historyPushState', () => {
+        jest.spyOn(urlUtility, 'updateHistory');
+        findFilteredSearch().vm.$emit('onFilter', [{ value: { data: 'searchQuery' } }]);
+
+        expect(urlUtility.updateHistory).toHaveBeenCalledWith({
+          replace: true,
+          title: '',
+          url: 'http://test.host/',
+        });
+      });
+    });
+  });
+
+  describe('when searching', () => {
+    beforeEach(() => {
+      store = createStore();
+
+      jest.spyOn(store, 'dispatch');
+
+      createComponent();
+    });
+
+    it('sets the url params to the correct results', async () => {
+      const mockFilters = [
+        { type: 'author_username', value: { data: 'root' } },
+        { type: 'label_name', value: { data: 'label' } },
+        { type: 'label_name', value: { data: 'label2' } },
+      ];
+      jest.spyOn(urlUtility, 'updateHistory');
+      findFilteredSearch().vm.$emit('onFilter', mockFilters);
+
+      expect(urlUtility.updateHistory).toHaveBeenCalledWith({
+        title: '',
+        replace: true,
+        url: 'http://test.host/?author_username=root&label_name[]=label&label_name[]=label2',
+      });
+    });
+  });
+
+  describe('when url params are already set', () => {
+    beforeEach(() => {
+      store = createStore();
+
+      jest.spyOn(store, 'dispatch');
+
+      createComponent({ initialFilterParams: { authorUsername: 'root', labelName: ['label'] } });
+    });
+
+    it('passes the correct props to FitlerSearchBar', async () => {
+      expect(findFilteredSearch().props('initialFilterValue')).toEqual([
+        { type: 'author_username', value: { data: 'root' } },
+        { type: 'label_name', value: { data: 'label' } },
+      ]);
+    });
+  });
+});
