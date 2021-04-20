ac0fd1692551fec2d41b72924aab9c8a8640cd7c
Maxime Orefice <morefice@gitlab.com>
2021-04-20 16:28:44 -0400

    Add downlodable artifacts endpoint
    This commit creates a new endpoint which will be used to query
    downloadable artifacts from the index pipeline page.

---
+++ spec/serializers/ci/downloadable_artifact_entity_spec.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+RSpec.describe Ci::DownloadableArtifactEntity do
+  let(:pipeline) { create(:ci_pipeline, :with_codequality_reports) }
+  let(:user) { create(:user) }
+  let(:request) { EntityRequest.new({ current_user: user }) }
+  let(:entity) { described_class.new(pipeline, request: request) }
+
+  describe '#as_json' do
+    subject { entity.as_json }
+
+    it 'contains required fields', :aggregate_failures do
+      expect(subject).to include(:artifacts)
+      expect(subject[:artifacts].size).to eq(1)
+    end
+
+    context 'when user cannot read job artifact' do
+      let!(:build) { create(:ci_build, :success, :artifacts, :non_public_artifacts, pipeline: pipeline) }
+
+      it 'returns only artifacts readable by user' do
+        expect(subject[:artifacts].size).to eq(1)
+      end
+    end
+  end
+end
