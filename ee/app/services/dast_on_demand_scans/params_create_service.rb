0935dd23697f0ef21f5a6937d4803a681a0deed2
Philip Cunningham <pcunningham@gitlab.com>
2021-04-20 12:49:41 +1000

    Add support for setting DAST_API_SPECIFICATION
    - Extend configuration service
    - Extend params service

--- ee/app/services/dast_on_demand_scans/params_create_service.rb
+++ ee/app/services/dast_on_demand_scans/params_create_service.rb
@@ -9,7 +9,7 @@ module DastOnDemandScans
       return ServiceResponse.error(message: 'Cannot run active scan against unvalidated target') unless active_scan_allowed?
 
       ServiceResponse.success(
-        payload: default_config.merge(site_profile_config, scanner_profile_config)
+        payload: default_config.merge(target_config, site_profile_config, scanner_profile_config)
       )
     end
 
@@ -62,11 +62,15 @@ module DastOnDemandScans
     end
 
     def default_config
-      {
-        dast_profile: dast_profile,
-        branch: branch,
-        target_url: dast_site&.url
-      }
+      { dast_profile: dast_profile, branch: branch }
+    end
+
+    def target_config
+      url = dast_site&.url
+
+      return { api_specification: url } if dast_site_profile.target_type == 'api'
+
+      { target_url: url }
     end
 
     def site_profile_config
