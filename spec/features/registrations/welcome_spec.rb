da59fcbd2d0f9abf455f119a43d54cb4edbee9dd
jejacks0n <jjackson@gitlab.com>
2021-04-20 13:08:56 -0600

    Add initial jobs_to_be_done experiment
    - This includes the view partial and related javascript.

--- spec/features/registrations/welcome_spec.rb
+++ spec/features/registrations/welcome_spec.rb
@@ -3,19 +3,34 @@
 require 'spec_helper'
 
 RSpec.describe 'Welcome screen' do
-  let(:user) { create(:user) }
+  let_it_be(:user) { create(:user, :unconfirmed) }
+  let_it_be(:group) { create(:group) }
 
   before do
+    group.add_owner(user)
     gitlab_sign_in(user)
-
-    visit users_sign_up_welcome_path
   end
 
   it 'shows the email opt in' do
+    visit users_sign_up_welcome_path
+
     select 'Software Developer', from: 'user_role'
     check 'user_email_opted_in'
     click_button 'Get started!'
 
     expect(user.reload.email_opted_in).to eq(true)
+  end
+
+  it 'allows specifying other for the jobs_to_be_done experiment', :js, :experiment do
+    stub_experiments(jobs_to_be_done: :candidate)
+    visit users_sign_up_welcome_path
+
+    expect(page).not_to have_content('Why are you signing up? (Optional)')
+
+    select 'A different reason', from: 'jobs_to_be_done'
+
+    expect(page).to have_content('Why are you signing up? (Optional)')
+
+    fill_in 'jobs_to_be_done_other', with: 'My reason'
   end
 end
