72ded18d8518ea2a7f71303631d61afe8ec76ce9
alinamihaila <amihaila@gitlab.com>
2021-04-21 13:02:56 +0300

    Add base class for metric instrumentation
      - Add time constraint
      - Add key processor
      - Add UsageDataMetrics
      - Add uuid metric as example
      - Add RedisHLLMetric instrumentation
      - Add DatabaseMetric instrumentation
      - Add GenericMetric instrumentation

---
+++ lib/gitlab/usage/metrics/instrumentations/base_metric.rb
@@ -0,0 +1,42 @@
+# frozen_string_literal: true
+
+module Gitlab
+  module Usage
+    module Metrics
+      module Instrumentations
+        class BaseMetric
+          include Gitlab::Utils::UsageData
+          # Usage example
+          #
+          # class UuidMetric < BaseMetric
+          #   value do
+          #     Gitlab::CurrentSettings.uuid
+          #   end
+          # end
+          attr_reader :time_constraints
+
+          def initialize(time_constraints:)
+            @time_constraints = time_constraints
+          end
+
+          alias_method :database_time_constraints, :time_constraints
+          alias_method :redis_hll_time_constraints, :time_constraints
+
+          class << self
+            def value(&block)
+              @metric_value = block
+            end
+
+            attr_reader :metric_value
+          end
+
+          def value
+            alt_usage_data do
+              self.class.metric_value.call
+            end
+          end
+        end
+      end
+    end
+  end
+end
