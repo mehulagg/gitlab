fd629cc15398a83290efb53bad2b1efbe478bde4
Lin Jen-Shin <godfat@godfat.org>
2021-04-22 02:07:00 +0800

    Avoid cd as it'll change subsequent shell scripts
    Which we'll need to run `scripts/api/play_job.rb`

--- scripts/review_apps/review-apps.sh
+++ scripts/review_apps/review-apps.sh
@@ -281,13 +281,12 @@ function download_chart() {
 
   curl --location -o gitlab.tar.bz2 "https://gitlab.com/gitlab-org/charts/gitlab/-/archive/${GITLAB_HELM_CHART_REF}/gitlab-${GITLAB_HELM_CHART_REF}.tar.bz2"
   tar -xjf gitlab.tar.bz2
-  cd "gitlab-${GITLAB_HELM_CHART_REF}"
 
   echoinfo "Adding the gitlab repo to Helm..."
   helm repo add gitlab https://charts.gitlab.io
 
   echoinfo "Building the gitlab chart's dependencies..."
-  helm dependency build .
+  helm dependency build "gitlab-${GITLAB_HELM_CHART_REF}"
 }
 
 function base_config_changed() {
