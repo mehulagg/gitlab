9fe4eb2dc417df92b516ac13150136183aaa378a
Ash McKenzie <amckenzie@gitlab.com>
2021-04-21 13:43:56 +1000

    Send error output to stderr

--- scripts/lint-docs-metadata.sh
+++ scripts/lint-docs-metadata.sh
@@ -8,7 +8,7 @@ for file in $1
 do
   if [ "$(head -n1 "$file")" != "---" ]
   then
-    printf "%sDocumentation metadata missing in %s%s\n" "$(tput setaf 1)" "$file" "$(tput sgr0)"
+    printf "%sDocumentation metadata missing in %s%s\n" "$(tput setaf 1)" "$file" "$(tput sgr0)" >&2
     (( counter++ ))
   else
     printf "Documentation metadata found in %s.\n" "$file"
@@ -17,8 +17,8 @@ done
 
 if [ "$counter" -gt 0 ]
 then
-  printf "\n%sDocumentation metadata is missing in changed documentation files.%s For more information, see https://docs.gitlab.com/ee/development/documentation/#metadata.\n" "$(tput setaf 1)" "$(tput sgr0)"
   false
+  printf "\n%sDocumentation metadata is missing in changed documentation files.%s For more information, see https://docs.gitlab.com/ee/development/documentation/#metadata.\n" "$(tput setaf 1)" "$(tput sgr0)" >&2
 else
   printf "\n%sDocumentation metadata found in all changed documentation files.%s\n" "$(tput setaf 2)" "$(tput sgr0)"
   true
