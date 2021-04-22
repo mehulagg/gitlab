3d99a9d23a96f240d915a30b6489f73106516cc0
a_luna <aluna@gitlab.com>
2021-04-22 16:50:25 -0500

    Add extra steps after change is saved
    - Create system note
    - Track usage
    - Hooks

--- app/services/merge_requests/add_spent_time_service.rb
+++ app/services/merge_requests/add_spent_time_service.rb
@@ -3,6 +3,8 @@
 module MergeRequests
   class AddSpentTimeService < UpdateService
     def execute(merge_request)
+      old_associations = associations_before_update(merge_request)
+
       # Do not touch when saving the issuable if only changes position within a list. We should call
       # this method at this point to capture all possible changes.
       should_touch = update_timestamp?(merge_request)
@@ -11,6 +13,17 @@ module MergeRequests
 
       merge_request_saved = merge_request.with_transaction_returning_status do
         merge_request.save(touch: should_touch)
+      end
+
+      if merge_request_saved
+        create_system_notes(merge_request)
+
+        # track usage
+        old_timelogs = old_associations.fetch(:timelogs, [])
+        changed_fields = merge_request.previous_changes.keys
+        track_time_estimate_and_spend_edits(merge_request, old_timelogs, changed_fields)
+
+        execute_hooks(merge_request, 'update', old_associations: old_associations)
       end
 
       merge_request
--- spec/services/merge_requests/add_spent_time_service_spec.rb
+++ spec/services/merge_requests/add_spent_time_service_spec.rb
@@ -3,9 +3,9 @@
 require 'spec_helper'
 
 RSpec.describe MergeRequests::AddSpentTimeService do
-  let_it_be(:project) { create(:project, :public, :repository) }
-  let_it_be(:merge_request) { create(:merge_request, :simple, :unique_branches, source_project: project) }
   let_it_be(:user) { create(:user) }
+  let_it_be(:project) { create(:project, :public, :repository) }
+  let_it_be_with_reload(:merge_request) { create(:merge_request, :simple, :unique_branches, source_project: project) }
 
   let(:params) { { spend_time: { duration: 1500, user_id: user.id } } }
   let(:service) { described_class.new(project, user, params) }
@@ -18,7 +18,26 @@ RSpec.describe MergeRequests::AddSpentTimeService do
     it 'creates a new timelog with the specified duration' do
       expect { service.execute(merge_request) }.to change { Timelog.count }.from(0).to(1)
 
-      expect(merge_request.timelogs.last.time_spent).to eq(1500)
+      timelog = merge_request.timelogs.last
+
+      expect(timelog).not_to be_nil
+      expect(timelog.time_spent).to eq(1500)
+    end
+
+    it 'creates a system note with the time added' do
+      expect { service.execute(merge_request) }.to change { Note.count }.from(0).to(1)
+
+      system_note = merge_request.notes.last
+
+      expect(system_note).not_to be_nil
+      expect(system_note.note_html).to include('added 25m of time spent')
+    end
+
+    it 'saves usage data' do
+      expect(Gitlab::UsageDataCounters::MergeRequestActivityUniqueCounter)
+        .to receive(:track_time_spent_changed_action).once.with(user: user)
+
+      service.execute(merge_request)
     end
 
     it 'is more efficient than using the full update-service' do
