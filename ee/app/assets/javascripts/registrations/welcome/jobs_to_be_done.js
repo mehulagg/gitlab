4e8e0cd49d7d3834a7e64de9af124be180f08c44
jejacks0n <jjackson@gitlab.com>
2021-04-22 11:58:49 -0600

    Add form tracking for jobs_to_be_done experiment

--- ee/app/assets/javascripts/registrations/welcome/jobs_to_be_done.js
+++ ee/app/assets/javascripts/registrations/welcome/jobs_to_be_done.js
@@ -1,7 +1,17 @@
-const select = document.querySelector('.js-jobs-to-be-done-dropdown')
+// import { getExperimentContexts } from '~/experimentation/utils';
+// import Tracking from '~/tracking';
+
+const select = document.querySelector('.js-jobs-to-be-done-dropdown');
 if (select) {
+  // Tracking.enableFormTracking(
+  //   { fields: { whitelist: ['jobs_to_be_done', 'jobs_to_be_done_other'] } },
+  //   getExperimentContexts('jobs_to_be_done'),
+  // )
+  //
   select.addEventListener('change', () => {
-    const otherSelected = select.value === 'other'
-    document.querySelector('.js-jobs-to-be-done-other-group').classList.toggle('hidden', !otherSelected)
-  })
+    const otherSelected = select.value === 'other';
+    document
+      .querySelector('.js-jobs-to-be-done-other-group')
+      .classList.toggle('hidden', !otherSelected);
+  });
 }
