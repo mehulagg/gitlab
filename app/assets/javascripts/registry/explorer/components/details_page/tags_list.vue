132705ef6a4f45b2091d35c1401639a08efe7fbb
Nicolò Maria Mezzopera <nicolo.mezzopera@gmail.com>
2021-04-22 12:47:26 +0200

    Enhance tags list with pagination and apolo
    - source
    - tests
    - mock data

--- app/assets/javascripts/registry/explorer/components/details_page/tags_list.vue
+++ app/assets/javascripts/registry/explorer/components/details_page/tags_list.vue
@@ -1,19 +1,32 @@
 <script>
-import { GlButton } from '@gitlab/ui';
-import { REMOVE_TAGS_BUTTON_TITLE, TAGS_LIST_TITLE } from '../../constants/index';
+import { GlButton, GlKeysetPagination } from '@gitlab/ui';
+import createFlash from '~/flash';
+import { joinPaths } from '~/lib/utils/url_utility';
+import {
+  REMOVE_TAGS_BUTTON_TITLE,
+  TAGS_LIST_TITLE,
+  GRAPHQL_PAGE_SIZE,
+  FETCH_IMAGES_LIST_ERROR_MESSAGE,
+} from '../../constants/index';
+import getContainerRepositoryTagsQuery from '../../graphql/queries/get_container_repository_tags.query.graphql';
+import EmptyState from './empty_state.vue';
 import TagsListRow from './tags_list_row.vue';
+import TagsLoader from './tags_loader.vue';
 
 export default {
   name: 'TagsList',
   components: {
     GlButton,
+    GlKeysetPagination,
     TagsListRow,
+    EmptyState,
+    TagsLoader,
   },
+  inject: ['config'],
   props: {
-    tags: {
-      type: Array,
-      required: false,
-      default: () => [],
+    id: {
+      type: [Number, String],
+      required: true,
     },
     isMobile: {
       type: Boolean,
@@ -25,17 +38,47 @@ export default {
       default: false,
       required: false,
     },
+    isLoading: {
+      type: Boolean,
+      default: false,
+      required: false,
+    },
   },
   i18n: {
     REMOVE_TAGS_BUTTON_TITLE,
     TAGS_LIST_TITLE,
   },
+  apollo: {
+    tags: {
+      query: getContainerRepositoryTagsQuery,
+      variables() {
+        return this.queryVariables;
+      },
+      update(data) {
+        return data.containerRepository?.tags?.nodes || [];
+      },
+      result({ data }) {
+        this.tagsPageInfo = data.containerRepository?.tags?.pageInfo;
+      },
+      error() {
+        createFlash({ message: FETCH_IMAGES_LIST_ERROR_MESSAGE });
+      },
+    },
+  },
   data() {
     return {
       selectedItems: {},
+      tags: [],
+      tagsPageInfo: {},
     };
   },
   computed: {
+    queryVariables() {
+      return {
+        id: joinPaths(this.config.gidPrefix, `${this.id}`),
+        first: GRAPHQL_PAGE_SIZE,
+      };
+    },
     hasSelectedItems() {
       return this.tags.some((tag) => this.selectedItems[tag.name]);
     },
@@ -45,10 +88,49 @@ export default {
     multiDeleteButtonIsDisabled() {
       return !this.hasSelectedItems || this.disabled;
     },
+    showPagination() {
+      return this.tagsPageInfo.hasPreviousPage || this.tagsPageInfo.hasNextPage;
+    },
+    hasNoTags() {
+      return this.tags.length === 0;
+    },
+    internalLoading() {
+      return this.isLoading || this.$apollo.queries.tags.loading;
+    },
   },
   methods: {
     updateSelectedItems(name) {
       this.$set(this.selectedItems, name, !this.selectedItems[name]);
+    },
+    mapTagsToBeDleeted(items) {
+      return this.tags.filter((tag) => items[tag.name]);
+    },
+    fetchNextPage() {
+      if (this.tagsPageInfo?.hasNextPage) {
+        this.$apollo.queries.tags.fetchMore({
+          variables: {
+            after: this.tagsPageInfo?.endCursor,
+            first: GRAPHQL_PAGE_SIZE,
+          },
+          updateQuery(previousResult, { fetchMoreResult }) {
+            return fetchMoreResult;
+          },
+        });
+      }
+    },
+    fetchPreviousPage() {
+      if (this.tagsPageInfo?.hasPreviousPage) {
+        this.$apollo.queries.tags.fetchMore({
+          variables: {
+            first: null,
+            before: this.tagsPageInfo?.startCursor,
+            last: GRAPHQL_PAGE_SIZE,
+          },
+          updateQuery(previousResult, { fetchMoreResult }) {
+            return fetchMoreResult;
+          },
+        });
+      }
     },
   },
 };
@@ -56,31 +138,47 @@ export default {
 
 <template>
   <div>
-    <div class="gl-display-flex gl-justify-content-space-between gl-mb-3">
-      <h5 data-testid="list-title">
-        {{ $options.i18n.TAGS_LIST_TITLE }}
-      </h5>
+    <tags-loader v-if="internalLoading" />
+    <template v-else>
+      <empty-state v-if="hasNoTags" :no-containers-image="config.noContainersImage" />
+      <template v-else>
+        <div class="gl-display-flex gl-justify-content-space-between gl-mb-3">
+          <h5 data-testid="list-title">
+            {{ $options.i18n.TAGS_LIST_TITLE }}
+          </h5>
 
-      <gl-button
-        v-if="showMultiDeleteButton"
-        :disabled="multiDeleteButtonIsDisabled"
-        category="secondary"
-        variant="danger"
-        @click="$emit('delete', selectedItems)"
-      >
-        {{ $options.i18n.REMOVE_TAGS_BUTTON_TITLE }}
-      </gl-button>
-    </div>
-    <tags-list-row
-      v-for="(tag, index) in tags"
-      :key="tag.path"
-      :tag="tag"
-      :first="index === 0"
-      :selected="selectedItems[tag.name]"
-      :is-mobile="isMobile"
-      :disabled="disabled"
-      @select="updateSelectedItems(tag.name)"
-      @delete="$emit('delete', { [tag.name]: true })"
-    />
+          <gl-button
+            v-if="showMultiDeleteButton"
+            :disabled="multiDeleteButtonIsDisabled"
+            category="secondary"
+            variant="danger"
+            @click="$emit('delete', mapTagsToBeDleeted(selectedItems))"
+          >
+            {{ $options.i18n.REMOVE_TAGS_BUTTON_TITLE }}
+          </gl-button>
+        </div>
+        <tags-list-row
+          v-for="(tag, index) in tags"
+          :key="tag.path"
+          :tag="tag"
+          :first="index === 0"
+          :selected="selectedItems[tag.name]"
+          :is-mobile="isMobile"
+          :disabled="disabled"
+          @select="updateSelectedItems(tag.name)"
+          @delete="$emit('delete', mapTagsToBeDleeted({ [tag.name]: true }))"
+        />
+        <div class="gl-display-flex gl-justify-content-center">
+          <gl-keyset-pagination
+            v-if="showPagination"
+            :has-next-page="tagsPageInfo.hasNextPage"
+            :has-previous-page="tagsPageInfo.hasPreviousPage"
+            class="gl-mt-3"
+            @prev="fetchPreviousPage"
+            @next="fetchNextPage"
+          />
+        </div>
+      </template>
+    </template>
   </div>
 </template>
