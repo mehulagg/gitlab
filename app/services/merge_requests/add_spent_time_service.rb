93d676d5398c19cd1314a7dd73ac5c2cc6ce0dc6
a_luna <aluna@gitlab.com>
2021-04-22 17:17:27 -0500

    Remove should_touch check

--- app/services/merge_requests/add_spent_time_service.rb
+++ app/services/merge_requests/add_spent_time_service.rb
@@ -5,14 +5,10 @@ module MergeRequests
     def execute(merge_request)
       old_associations = associations_before_update(merge_request)
 
-      # Do not touch when saving the issuable if only changes position within a list. We should call
-      # this method at this point to capture all possible changes.
-      should_touch = update_timestamp?(merge_request)
-
       merge_request.spend_time(params[:spend_time])
 
       merge_request_saved = merge_request.with_transaction_returning_status do
-        merge_request.save(touch: should_touch)
+        merge_request.save
       end
 
       if merge_request_saved
