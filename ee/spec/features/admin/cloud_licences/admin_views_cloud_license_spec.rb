f27c598a6ad7b2a132b0857ef1f9df75c6f2bcff
Angelo Gulina <agulina@gitlab.com>
2021-04-22 18:17:07 +0200

    [skip-ci] Add feature specs

--- ee/spec/features/admin/cloud_licences/admin_views_cloud_license_spec.rb
+++ ee/spec/features/admin/cloud_licences/admin_views_cloud_license_spec.rb
@@ -12,16 +12,26 @@ RSpec.describe "Admin views Cloud License", :js do
     allow(License).to receive(:current).and_return(license)
   end
 
-  License::EE_ALL_PLANS.each do |plan|
-    context "#{plan} license" do
-      let_it_be(:license) { build(:license, plan: plan) }
+  context "Cloud license" do
+    let_it_be(:license) { create_current_license(type: License::CLOUD_LICENSE_TYPE, plan: License::ULTIMATE_PLAN) }
 
-      it 'displays the subscription details' do
-        visit(admin_cloud_license_path)
+    before do
+      visit(admin_cloud_license_path)
+    end
 
-        page.within(find('#content-body', match: :first)) do
-          expect(page).to have_content("Subscription details")
-        end
+    it 'displays the subscription details' do
+      page.within(find('#content-body', match: :first)) do
+        expect(page).to have_content("Subscription details")
+      end
+    end
+
+    it 'syncs the subscription' do
+      page.within(find('#content-body', match: :first)) do
+        wait_for_requests
+
+        click_link "Sync subscription details"
+
+        expect(page).to have_content("Subscription details")
       end
     end
   end
