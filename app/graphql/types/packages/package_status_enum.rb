5a91cd4be0758fcf04562c0ee90cff93b154c037
Steve Abrams <sabrams@gitlab.com>
2021-04-23 15:57:54 -0600

    Fix lint error

--- app/graphql/types/packages/package_status_enum.rb
+++ app/graphql/types/packages/package_status_enum.rb
@@ -12,7 +12,7 @@ module Types
 
       ::Packages::Package.statuses.keys.each do |status|
         status_name = PACKAGE_STATUS_NAMES.fetch(status.to_sym, status.capitalize)
-        value status.to_s.upcase, "Packages from the #{status_name} package manager", value: status.to_s
+        value status.to_s.upcase, description: "Packages from the #{status_name} package manager", value: status.to_s
       end
     end
   end
