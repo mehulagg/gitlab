8f6e77f99d7a864797face80761bcc0021622294
Justin Ho <hduong@gitlab.com>
2021-04-23 15:32:24 +0700

    Add empty state and mock data to dropdown

--- ee/app/assets/javascripts/integrations/jira/issues_show/components/sidebar/issue_field_dropdown.vue
+++ ee/app/assets/javascripts/integrations/jira/issues_show/components/sidebar/issue_field_dropdown.vue
@@ -1,9 +1,11 @@
 <script>
-import { GlDropdown, GlLoadingIcon } from '@gitlab/ui';
+import { GlDropdown, GlDropdownItem, GlDropdownText, GlLoadingIcon } from '@gitlab/ui';
 
 export default {
   components: {
     GlDropdown,
+    GlDropdownItem,
+    GlDropdownText,
     GlLoadingIcon,
   },
   props: {
@@ -21,15 +23,35 @@ export default {
   data() {
     return {
       isLoading: true,
+      items: [],
     };
+  },
+  computed: {
+    noItems() {
+      return this.items.length === 0;
+    },
+  },
+  mounted() {
+    this.$refs.dropdown.show();
+
+    // We are using mock data here which should come from the backend
+    setTimeout(() => {
+      // eslint-disable-next-line @gitlab/require-i18n-strings
+      this.items = [{ title: 'In Progress' }, { title: 'Done' }];
+      this.isLoading = false;
+    }, 1000);
   },
 };
 </script>
 
 <template>
-  <gl-dropdown :text="text" :header-text="title" block lazy>
+  <gl-dropdown ref="dropdown" :text="text" :header-text="title" block lazy>
     <div v-if="isLoading" class="gl-h-13">
       <gl-loading-icon size="md" />
+    </div>
+    <div v-else>
+      <gl-dropdown-text v-if="noItems">{{ __('No items') }}</gl-dropdown-text>
+      <gl-dropdown-item v-for="item in items" :key="item.title">{{ item.title }}</gl-dropdown-item>
     </div>
   </gl-dropdown>
 </template>
