8e4385f3c620b6e426f8f9aeee1b01fd49f67f1c
Philip Cunningham <pcunningham@gitlab.com>
2021-04-27 16:10:59 +1000

    Remove deprecated RunDastScan mutation
    - Deprecated since %13.4
    - Scheduled for removal in %14.0

--- ee/app/graphql/ee/types/deprecated_mutations.rb
+++ ee/app/graphql/ee/types/deprecated_mutations.rb
@@ -6,7 +6,6 @@ module EE
       extend ActiveSupport::Concern
 
       prepended do
-        mount_mutation ::Mutations::Pipelines::RunDastScan, deprecated: { reason: 'Use DastOnDemandScanCreate', milestone: '13.4' }
         mount_aliased_mutation 'DismissVulnerability',
                              ::Mutations::Vulnerabilities::Dismiss,
                              deprecated: { reason: 'Use vulnerabilityDismiss', milestone: '13.5' }
--- ee/spec/graphql/types/mutation_type_spec.rb
+++ ee/spec/graphql/types/mutation_type_spec.rb
@@ -3,22 +3,6 @@
 require 'spec_helper'
 
 RSpec.describe GitlabSchema.types['Mutation'] do
-  describe 'deprecated mutations' do
-    using RSpec::Parameterized::TableSyntax
-
-    where(:field_name, :reason, :milestone) do
-      'RunDastScan' | 'Use DastOnDemandScanCreate' | '13.4'
-    end
-
-    with_them do
-      let(:field) { get_field(field_name) }
-      let(:deprecation_reason) { "#{reason}. Deprecated in #{milestone}." }
-
-      it { expect(field).to be_present }
-      it { expect(field.deprecation_reason).to eq(deprecation_reason) }
-    end
-  end
-
   describe 'aliased deprecated mutations' do
     using RSpec::Parameterized::TableSyntax
 
