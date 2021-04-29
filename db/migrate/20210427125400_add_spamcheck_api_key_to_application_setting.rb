691ece273c438e33d69b3b70f08f3f4e613048a0
jdsalaro <jsalazar@gitlab.com>
2021-04-29 02:50:16 +0200

    type at the end not needed for column removal, will likely fix pipeline failures for rollback after migration not leaving the db in the previous state

--- db/migrate/20210427125400_add_spamcheck_api_key_to_application_setting.rb
+++ db/migrate/20210427125400_add_spamcheck_api_key_to_application_setting.rb
@@ -8,7 +8,7 @@ class AddSpamcheckApiKeyToApplicationSetting < ActiveRecord::Migration[6.0]
   end
 
   def down
-    remove_column :application_settings, :encrypted_spam_check_api_key, :text
-    remove_column :application_settings, :encrypted_spam_check_api_key_iv, :text
+    remove_column :application_settings, :encrypted_spam_check_api_key
+    remove_column :application_settings, :encrypted_spam_check_api_key_iv
   end
 end
