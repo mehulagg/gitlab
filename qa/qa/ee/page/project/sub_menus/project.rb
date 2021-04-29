b97d46e351675e99ab4e6ab78838d07648afd449
Francisco Javier López <fjlopez@gitlab.com>
2021-04-29 12:17:59 +0200

    Add Analytics menu to project sidebar refactor

--- qa/qa/ee/page/project/sub_menus/project.rb
+++ qa/qa/ee/page/project/sub_menus/project.rb
@@ -7,9 +7,9 @@ module QA
         module SubMenus
           module Project
             def click_project_insights_link
-              hover_element(:analytics_link) do
-                within_submenu(:analytics_sidebar_submenu) do
-                  click_element(:project_insights_link)
+              hover_element(:sidebar_menu_link, menu_item: 'Analytics') do
+                within_submenu do
+                  click_element(:sidebar_menu_item_link, menu_item: 'Insights')
                 end
               end
             end
