65093b9aeacec29c1146570f1bb60b66de5138c2
Lukas Eipert <leipert@gitlab.com>
2021-04-30 16:01:12 +0200

    Symlink GitLab UI in postinstall

--- scripts/frontend/postinstall.js
+++ scripts/frontend/postinstall.js
@@ -1,5 +1,33 @@
 const chalk = require('chalk');
 
+function setupSymlinkedGitLabUI() {
+  // eslint-disable-next-line global-require
+  const fs = require('fs');
+  // eslint-disable-next-line global-require
+  const path = require('path');
+  const gitlabRoot = path.resolve(path.join(__dirname, '..', '..'));
+  // TODO: This should exist
+  const gitlabUISrc = path.join(gitlabRoot, '../gitlab-ui');
+  const gitlabUIDir = path.join(gitlabRoot, 'node_modules/@gitlab/ui');
+
+  console.log(chalk.green('GDK with GitLab UI in watch mode detected'));
+  console.log('Setting up symlink');
+
+  if (fs.existsSync(gitlabUIDir)) {
+    const stat = fs.lstatSync(gitlabUIDir);
+    if (stat.isSymbolicLink()) {
+      console.log(`Already a symbolic link ${gitlabUIDir}`);
+      return;
+    }
+    if (stat.isDirectory()) {
+      fs.rm(gitlabUIDir, { recursive: true });
+    }
+  }
+  fs.symlinkSync(gitlabUISrc, gitlabUIDir);
+
+  // TODO: Force a webpack recompile
+}
+
 // check that fsevents is available if we're on macOS
 if (process.platform === 'darwin') {
   try {
@@ -20,3 +48,7 @@ if (process.platform === 'darwin') {
 }
 
 console.log(`${chalk.green('success')} Dependency postinstall check passed.`);
+
+if (process.env.GITLAB_UI_WATCH === 'true') {
+  setupSymlinkedGitLabUI();
+}
