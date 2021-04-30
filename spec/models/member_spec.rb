03bb87fb0c2f9a95ea8b0e827d6ebb8bd9c3d6a6
Manoj M J <mmj@gitlab.com>
2021-04-30 15:30:50 +0530

    Calculate effective access level of members in a project
    This change adds a method to calculate
    the effective access level of members in a specific
    project

--- spec/models/member_spec.rb
+++ spec/models/member_spec.rb
@@ -414,6 +414,20 @@ RSpec.describe Member do
       it { is_expected.not_to include @member_with_minimal_access }
     end
 
+    describe '.without_invites_and_requests' do
+      subject { described_class.without_invites_and_requests.to_a }
+
+      it { is_expected.to include @owner }
+      it { is_expected.to include @maintainer }
+      it { is_expected.to include @accepted_invite_member }
+      it { is_expected.to include @accepted_request_member }
+      it { is_expected.to include @blocked_maintainer }
+      it { is_expected.to include @blocked_developer }
+      it { is_expected.not_to include @invited_member }
+      it { is_expected.not_to include @requested_member }
+      it { is_expected.not_to include @member_with_minimal_access }
+    end
+
     describe '.distinct_on_user_with_max_access_level' do
       let_it_be(:other_group) { create(:group) }
       let_it_be(:member_with_lower_access_level) { create(:group_member, :developer, group: other_group, user: @owner_user) }
