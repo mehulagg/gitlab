6a8824c7c2fd8f1c5471ebb44a3b4520f5173561
Adam Cohen <acohen@gitlab.com>
2021-04-30 18:14:23 +1000

    Switch back to guard clause

--- app/models/issue/metrics.rb
+++ app/models/issue/metrics.rb
@@ -24,6 +24,8 @@ class Issue::Metrics < ApplicationRecord
   private
 
   def issue_assigned_to_list_label?
-    issue.labels.joins(:lists).exists?
+    return false if issue.labels.length == 0
+
+    issue.labels.includes(:lists).any? { |label| label.lists.present? }
   end
 end
