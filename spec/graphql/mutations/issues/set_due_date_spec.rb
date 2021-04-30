81f19920175f6cf3f317d4c5d34355cce02e31c5
John Hope <jhope@gitlab.com>
2021-04-30 16:40:17 +0100

    Swap let and let_it_be to fix lint check

--- spec/graphql/mutations/issues/set_due_date_spec.rb
+++ spec/graphql/mutations/issues/set_due_date_spec.rb
@@ -3,8 +3,8 @@
 require 'spec_helper'
 
 RSpec.describe Mutations::Issues::SetDueDate do
-  let(:issue) { create(:issue, due_date: '2021-05-01') }
   let_it_be(:user) { create(:user) }
+  let(:issue) { create(:issue, due_date: '2021-05-01') }
 
   subject(:mutation) { described_class.new(object: nil, context: { current_user: user }, field: nil) }
 
