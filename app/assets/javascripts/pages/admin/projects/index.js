5115831ed5c0aaae975baee5e2b2e5c68d8b134d
Mike Greiling <mike@pixelcog.com>
2021-05-03 03:10:40 -0500

    wip

--- app/assets/javascripts/pages/admin/projects/index.js
+++ app/assets/javascripts/pages/admin/projects/index.js
@@ -1,7 +1,10 @@
 import Vue from 'vue';
-import NamespaceSelect from '~/namespace_select';
+import { parseBoolean } from '~/lib/utils/common_utils';
+import { mergeUrlParams } from '~/lib/utils/url_utility';
+import OldNamespaceSelect from '~/namespace_select';
 import ProjectsList from '~/projects_list';
 import RemoveMemberModal from '~/vue_shared/components/remove_member_modal.vue';
+import NamespaceSelect from './components/namespace_select.vue';
 
 function mountRemoveMemberModal() {
   const el = document.querySelector('.js-remove-member-modal');
@@ -23,4 +26,36 @@ new ProjectsList(); // eslint-disable-line no-new
 
 document
   .querySelectorAll('.js-namespace-select')
-  .forEach((dropdown) => new NamespaceSelect({ dropdown }));
+  .forEach((dropdown) => new OldNamespaceSelect({ dropdown }));
+
+function mountNamespaceSelect() {
+  const el = document.querySelector('.js-new-namespace-select');
+  if (!el) {
+    return false;
+  }
+
+  const { showAny, fieldName, namespace, namespaceId, updateLocation } = el.dataset;
+
+  return new Vue({
+    el,
+    render(createComponent) {
+      return createComponent(NamespaceSelect, {
+        props: {
+          showAny: parseBoolean(showAny),
+          initialNamespaceId: namespaceId,
+          initialNamespace: namespace,
+          fieldName,
+        },
+        on: {
+          setNamespace(newNamespace) {
+            if (fieldName && updateLocation) {
+              window.location = mergeUrlParams({ [fieldName]: newNamespace }, window.location.href);
+            }
+          },
+        },
+      });
+    },
+  });
+}
+
+mountNamespaceSelect();
