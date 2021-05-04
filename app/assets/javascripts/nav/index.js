26f35f936110e52ad0303bbcafc385395a6a6eda
Paul Slaughter <pslaughter@gitlab.com>
2021-05-04 01:36:05 -0500

    Remove deferred import
    - This created a less than ideal user exp

--- app/assets/javascripts/nav/index.js
+++ app/assets/javascripts/nav/index.js
@@ -1,12 +1,11 @@
+import { mountTopNav, mountTopNavResponsive } from './mount';
+
 const tryMountTopNav = async () => {
   const el = document.getElementById('js-top-nav');
 
   if (!el) {
     return;
   }
-
-  // With combined_menu feature flag, there's a benefit to splitting up the import
-  const { mountTopNav } = await import(/* webpackChunkName: 'top_nav' */ './mount');
 
   mountTopNav(el);
 };
@@ -17,11 +16,6 @@ const tryMountTopNavResponsive = async () => {
   if (!el) {
     return;
   }
-
-  // With combined_menu feature flag, there's a benefit to splitting up the import
-  const { mountTopNavResponsive } = await import(
-    /* webpackChunkName: 'top_nav_responsive' */ './mount'
-  );
 
   mountTopNavResponsive(el);
 };
