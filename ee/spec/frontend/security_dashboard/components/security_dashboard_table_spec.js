08bc195e24b4eb48445ce8fda836e3e585c0ee42
Paul Gascou-Vaillancourt <paul.gascvail@gmail.com>
2021-05-04 12:08:03 -0400

    Animate the bulk actions section
    Adds an expand/collapse transition when showing/hiding the bulk action
    section in the vulnerability lists.
    
    Changelog: changed
    EE: true

--- ee/spec/frontend/security_dashboard/components/security_dashboard_table_spec.js
+++ ee/spec/frontend/security_dashboard/components/security_dashboard_table_spec.js
@@ -4,7 +4,6 @@ import Vuex from 'vuex';
 
 import SecurityDashboardTable from 'ee/security_dashboard/components/security_dashboard_table.vue';
 import SecurityDashboardTableRow from 'ee/security_dashboard/components/security_dashboard_table_row.vue';
-import SelectionSummary from 'ee/security_dashboard/components/selection_summary.vue';
 import createStore from 'ee/security_dashboard/store';
 
 import {
@@ -12,6 +11,7 @@ import {
   RECEIVE_VULNERABILITIES_SUCCESS,
   REQUEST_VULNERABILITIES,
 } from 'ee/security_dashboard/store/modules/vulnerabilities/mutation_types';
+import { extendedWrapper } from 'helpers/vue_test_utils_helper';
 
 import mockDataVulnerabilities from '../store/modules/vulnerabilities/data/mock_data_vulnerabilities';
 
@@ -25,10 +25,12 @@ describe('Security Dashboard Table', () => {
 
   beforeEach(() => {
     store = createStore();
-    wrapper = shallowMount(SecurityDashboardTable, {
-      localVue,
-      store,
-    });
+    wrapper = extendedWrapper(
+      shallowMount(SecurityDashboardTable, {
+        localVue,
+        store,
+      }),
+    );
     store.state.vulnerabilities.vulnerabilitiesEndpoint = vulnerabilitiesEndpoint;
   });
 
@@ -37,6 +39,7 @@ describe('Security Dashboard Table', () => {
   });
 
   const findCheckbox = () => wrapper.find(GlFormCheckbox);
+  const findSelectionSummaryCollapse = () => wrapper.findByTestId('selection-summary-collapse');
 
   describe('while loading', () => {
     beforeEach(() => {
@@ -63,7 +66,7 @@ describe('Security Dashboard Table', () => {
     });
 
     it('should not show the multi select box', () => {
-      expect(wrapper.find(SelectionSummary).exists()).toBe(false);
+      expect(findSelectionSummaryCollapse().attributes('visible')).toBeFalsy();
     });
 
     it('should show the select all as unchecked', () => {
@@ -76,7 +79,7 @@ describe('Security Dashboard Table', () => {
       });
 
       it('should show the multi select box', () => {
-        expect(wrapper.find(SelectionSummary).exists()).toBe(true);
+        expect(findSelectionSummaryCollapse().attributes('visible')).toBeTruthy();
       });
 
       it('should show the select all as checked', () => {
