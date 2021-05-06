f7a029a6b26fc1bfe0bb6195e55baf30d7cf553b
Frederic Caplette <fcaplette@gitlab.com>
2021-05-06 15:17:25 -0400

    Replace comment with link to an issue

--- app/assets/javascripts/pipelines/components/graph_shared/api.js
+++ app/assets/javascripts/pipelines/components/graph_shared/api.js
@@ -2,9 +2,7 @@ import axios from '~/lib/utils/axios_utils';
 import { reportToSentry } from '../../utils';
 
 export const reportPerformance = (path, stats) => {
-  // FIXME: This is a workaround for a flaky test where `path`
-  // was undefined and it ended up calling axios instead of
-  // axios-mock-adapter
+  // FIXME: https://gitlab.com/gitlab-org/gitlab/-/issues/330245
   if (!path) {
     return;
   }
