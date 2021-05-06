b6f953811ea3744e16fa7c49cefe0fac758ae878
Frederic Caplette <fcaplette@gitlab.com>
2021-05-06 15:00:50 -0400

    Fix flaky jest spec in links_layer_spec.js
    - Implement a mock for the post response
    - Add safeguard in case `path` is undefined in
    API call.

--- app/assets/javascripts/pipelines/components/graph_shared/api.js
+++ app/assets/javascripts/pipelines/components/graph_shared/api.js
@@ -2,6 +2,13 @@ import axios from '~/lib/utils/axios_utils';
 import { reportToSentry } from '../../utils';
 
 export const reportPerformance = (path, stats) => {
+  // FIXME: This is a workaround for a flaky test where `path`
+  // was undefined and it ended up calling axios insteam of
+  // axios-mock-adapter
+  if (!path) {
+    return;
+  }
+
   axios.post(path, stats).catch((err) => {
     reportToSentry('links_inner_perf', `error: ${err}`);
   });
