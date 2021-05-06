02731ee216b38853513a9c46b3031a74319f16e1
Nicolò Maria Mezzopera <nicolo.mezzopera@gmail.com>
2021-05-06 10:43:59 +0200

    Adjust test name after graphql rename

--- spec/graphql/types/packages/package_status_enum_spec.rb
+++ spec/graphql/types/packages/package_status_enum_spec.rb
@@ -2,7 +2,7 @@
 
 require 'spec_helper'
 
-RSpec.describe GitlabSchema.types['PackageStatusEnum'] do
+RSpec.describe GitlabSchema.types['PackageStatus'] do
   it 'exposes all package statuses' do
     expect(described_class.values.keys).to contain_exactly(*%w[DEFAULT HIDDEN PROCESSING ERROR])
   end
