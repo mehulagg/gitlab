b3fb3da3b097db40a6a5820b9f148f4e606936aa
Miguel Rincon <mrincon@gitlab.com>
2021-05-07 15:10:31 +0200

    Migrate admin runner list from HAML to Vue
    This change introduces feature flag: `runner_list_view_vue_ui`. To
    migrate the implementation of the runner admin section to Vue.
    
    This is an incomplete implementation, as some more features will be
    added gradually behind this feature flag.

--- app/assets/javascripts/runner/components/runner_type_badge.vue
+++ app/assets/javascripts/runner/components/runner_type_badge.vue
@@ -22,6 +22,7 @@ export default {
   components: {
     GlBadge,
   },
+
   props: {
     type: {
       type: String,
---
+++ spec/frontend/runner/components/runner_list_spec.js
@@ -0,0 +1,111 @@
+import { GlLink, GlSkeletonLoader } from '@gitlab/ui';
+import { mount } from '@vue/test-utils';
+import { extendedWrapper } from 'helpers/vue_test_utils_helper';
+import { getIdFromGraphQLId } from '~/graphql_shared/utils';
+import RunnerList from '~/runner/components/runner_list.vue';
+import { runnersData } from '../mock_data';
+
+const mockRunners = runnersData.data.runners.nodes;
+const mockActiveRunnersCount = mockRunners.length;
+
+describe('RunnerList', () => {
+  let wrapper;
+
+  const findActiveRunnersMessage = () => wrapper.findByTestId('active-runners-message');
+  const findSkeletonLoader = () => wrapper.findComponent(GlSkeletonLoader);
+  const findHeaders = () => wrapper.findAll('th');
+  const findRows = () => wrapper.findAll('[data-testid^="runner-row-"]');
+  const findCell = ({ row = 0, fieldKey }) =>
+    findRows().at(row).find(`[data-testid="td-${fieldKey}"]`);
+
+  const createComponent = ({ props = {} } = {}) => {
+    wrapper = extendedWrapper(
+      mount(RunnerList, {
+        propsData: {
+          runners: mockRunners,
+          activeRunnersCount: mockActiveRunnersCount,
+          ...props,
+        },
+      }),
+    );
+  };
+
+  beforeEach(() => {
+    createComponent();
+  });
+
+  afterEach(() => {
+    wrapper.destroy();
+  });
+
+  it('Displays active runner count', () => {
+    expect(findActiveRunnersMessage().text()).toBe(
+      `Runners currently online: ${mockActiveRunnersCount}`,
+    );
+  });
+
+  it('Displays a large active runner count', () => {
+    createComponent({ props: { activeRunnersCount: 2000 } });
+
+    expect(findActiveRunnersMessage().text()).toBe('Runners currently online: 2,000');
+  });
+
+  it('Displays headers', () => {
+    expect(findHeaders().at(0).text()).toBe('Type/State');
+    expect(findHeaders().at(1).text()).toBe('Runner');
+    expect(findHeaders().at(2).text()).toBe('Version');
+    expect(findHeaders().at(3).text()).toBe('IP Address');
+    expect(findHeaders().at(4).text()).toBe('Projects');
+    expect(findHeaders().at(5).text()).toBe('Jobs');
+    expect(findHeaders().at(6).text()).toBe('Tags');
+    expect(findHeaders().at(7).text()).toBe('Last contact');
+    expect(findHeaders().at(8).text()).toBe('');
+  });
+
+  it('Displays a list of runners', () => {
+    expect(findRows()).toHaveLength(2);
+
+    expect(findSkeletonLoader().exists()).toBe(false);
+  });
+
+  it('Displays details of a runner', () => {
+    const { id, description, version, ipAddress, shortSha } = mockRunners[0];
+
+    // Badges
+    expect(findCell({ fieldKey: 'type' }).text()).toMatchInterpolatedText('shared locked');
+
+    // Runner identifier
+    expect(findCell({ fieldKey: 'name' }).text()).toContain(
+      `#${getIdFromGraphQLId(id)} (${shortSha})`,
+    );
+    expect(findCell({ fieldKey: 'name' }).text()).toContain(description);
+
+    // Other fields: some cells are empty in the first iteration
+    // See https://gitlab.com/gitlab-org/gitlab/-/issues/329658#pending-features
+    expect(findCell({ fieldKey: 'version' }).text()).toBe(version);
+    expect(findCell({ fieldKey: 'ipAddress' }).text()).toBe(ipAddress);
+    expect(findCell({ fieldKey: 'projectCount' }).text()).toBe('');
+    expect(findCell({ fieldKey: 'jobCount' }).text()).toBe('');
+    expect(findCell({ fieldKey: 'tagList' }).text()).toBe('');
+    expect(findCell({ fieldKey: 'contactedAt' }).text()).toEqual(expect.any(String));
+    expect(findCell({ fieldKey: 'actions' }).text()).toBe('');
+  });
+
+  it('Links to the runner page', () => {
+    const { id } = mockRunners[0];
+
+    expect(findCell({ fieldKey: 'name' }).find(GlLink).attributes('href')).toBe(
+      `/admin/runners/${getIdFromGraphQLId(id)}`,
+    );
+  });
+
+  describe('When data is loading', () => {
+    beforeEach(() => {
+      createComponent({ props: { loading: true } });
+    });
+
+    it('shows an skeleton loader', () => {
+      expect(findSkeletonLoader().exists()).toBe(true);
+    });
+  });
+});
