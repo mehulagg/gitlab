6b90f4e65895804b7fcf3f2b6ff26d7922fbbc3f
Coung Ngo <cngo@gitlab.com>
2021-05-07 22:03:00 +0100

    Combine and rename bulk edit feature specs
    Combine and rename specs for improved organisation
    and consistency

--- ee/spec/features/issues/bulk_assignment_health_status_spec.rb
+++ ee/spec/features/issues/bulk_assignment_health_status_spec.rb
@@ -2,12 +2,17 @@
 
 require 'spec_helper'
 
-RSpec.describe 'Issues > Health status bulk assignment' do
+RSpec.describe 'Issues > Bulk edit issues' do
   let_it_be(:user) { create(:user) }
   let_it_be(:group) { create(:group, :public) }
+  let_it_be(:epic) { create(:epic, group: group) }
+  let_it_be(:iteration) { create(:iteration, group: group, title: "Iteration 1") }
   let_it_be(:project) { create(:project, :public, group: group) }
-  let_it_be(:issue1) { create(:issue, project: project, title: "Issue 1") }
-  let_it_be(:issue2) { create(:issue, project: project, title: "Issue 2") }
+  let_it_be(:project_without_group) { create(:project, :public) }
+
+  let!(:issue1) { create(:issue, project: project, title: "Issue 1") }
+  let!(:issue2) { create(:issue, project: project, title: "Issue 2") }
+  let!(:issue3) { create(:issue, project: project_without_group, title: "Issue 3") }
 
   shared_examples 'bulk edit option in sidebar' do |context|
     it 'is present when bulk edit is enabled' do
@@ -17,6 +22,40 @@ RSpec.describe 'Issues > Health status bulk assignment' do
 
     it 'is not present when bulk edit is disabled' do
       expect(page).not_to have_css('.issuable-sidebar')
+    end
+  end
+
+  shared_examples 'bulk edit epic' do |context|
+    before do
+      enable_bulk_update(context)
+    end
+
+    context 'epic', :js do
+      context 'to all issues' do
+        before do
+          check 'check-all-issues'
+          open_epic_dropdown [epic.title]
+          update_issues
+        end
+
+        it 'updates with selected epic' do
+          expect(issue1.reload.epic.title).to eq epic.title
+          expect(issue2.reload.epic.title).to eq epic.title
+        end
+      end
+
+      context 'to a issue' do
+        before do
+          check "selected_issue_#{issue1.id}"
+          open_epic_dropdown [epic.title]
+          update_issues
+        end
+
+        it 'updates with selected epic' do
+          expect(issue1.reload.epic.title).to eq epic.title
+          expect(issue2.reload.epic).to eq nil
+        end
+      end
     end
   end
 
@@ -54,6 +93,54 @@ RSpec.describe 'Issues > Health status bulk assignment' do
     end
   end
 
+  shared_examples 'bulk edit iteration' do |context|
+    context 'iteration', :js do
+      before do
+        enable_bulk_update(context)
+      end
+      context 'to all issues' do
+        before do
+          check 'check-all-issues'
+          open_iteration_dropdown ['Iteration 1']
+          update_issues
+        end
+
+        it 'updates the iteration' do
+          aggregate_failures 'each issue in list' do
+            expect(issue1.reload.iteration.name).to eq 'Iteration 1'
+            expect(issue2.reload.iteration.name).to eq 'Iteration 1'
+          end
+        end
+      end
+    end
+
+    context 'cannot find iteration when iterations is off', :js do
+      before do
+        stub_licensed_features(iterations: false)
+
+        enable_bulk_update(context)
+      end
+
+      it 'cannot find iteration dropdown' do
+        expect(page).not_to have_selector('[data-qa-selector="iteration_container"]')
+      end
+    end
+  end
+
+  shared_examples 'cannot find iterations when project does not have a group' do |context|
+    context 'cannot find iteration when group does not belong to project', :js do
+      before do
+        project_without_group.add_maintainer(user)
+
+        enable_bulk_update(context)
+      end
+
+      it 'cannot find iteration dropdown' do
+        expect(page).not_to have_selector('[data-qa-selector="iteration_container"]')
+      end
+    end
+  end
+
   shared_examples 'bulk edit health_status with insufficient permissions' do
     it 'cannot bulk assign health_status' do
       expect(page).not_to have_button 'Edit issues'
@@ -66,7 +153,7 @@ RSpec.describe 'Issues > Health status bulk assignment' do
     before do
       allow(group).to receive(:feature_enabled?).and_return(true)
 
-      stub_licensed_features(group_bulk_edit: true, issuable_health_status: true)
+      stub_licensed_features(epics: true, group_bulk_edit: true, issuable_health_status: true, iterations: true)
 
       group.add_maintainer(user)
 
@@ -75,21 +162,27 @@ RSpec.describe 'Issues > Health status bulk assignment' do
 
     context 'at group level' do
       it_behaves_like 'bulk edit option in sidebar', :group
+      it_behaves_like 'bulk edit epic', :group
       it_behaves_like 'bulk edit health status', :group
+      it_behaves_like 'bulk edit iteration', :group
     end
 
     context 'at project level' do
       it_behaves_like 'bulk edit option in sidebar', :project
+      it_behaves_like 'bulk edit epic', :project
       it_behaves_like 'bulk edit health status', :project
+      it_behaves_like 'bulk edit iteration', :project
+      it_behaves_like 'cannot find iterations when project does not have a group', :project_without_group
     end
   end
 
   context 'as a guest', :js do
     before do
-      sign_in user
       allow(group).to receive(:feature_enabled?).and_return(true)
 
-      stub_licensed_features(issuable_health_status: true)
+      stub_licensed_features(epics: true, group_bulk_edit: true, issuable_health_status: true, iterations: true)
+
+      sign_in user
     end
 
     context 'at group level' do
@@ -109,6 +202,15 @@ RSpec.describe 'Issues > Health status bulk assignment' do
     end
   end
 
+  def open_epic_dropdown(items = [])
+    page.within('.issues-bulk-update') do
+      click_button 'Select epic'
+      items.map do |item|
+        find('.gl-new-dropdown-item', text: item).click
+      end
+    end
+  end
+
   def open_health_status_dropdown(items = [])
     page.within('.issues-bulk-update') do
       click_button 'Select health status'
@@ -118,18 +220,13 @@ RSpec.describe 'Issues > Health status bulk assignment' do
     end
   end
 
-  def toggle_issue(issue, uncheck = false)
-    page.within('.issues-list') do
-      if uncheck
-        uncheck "selected_issue_#{issue.id}"
-      else
-        check "selected_issue_#{issue.id}"
+  def open_iteration_dropdown(items = [])
+    page.within('.issues-bulk-update') do
+      click_button 'Select iteration'
+      items.map do |item|
+        find('.dropdown-item', text: item).click
       end
     end
-  end
-
-  def uncheck_issue(issue)
-    toggle_issue(issue, uncheck: true)
   end
 
   def update_issues
@@ -140,6 +237,8 @@ RSpec.describe 'Issues > Health status bulk assignment' do
   def enable_bulk_update(context)
     if context == :project
       visit project_issues_path(project)
+    elsif context == :project_without_group
+      visit project_issues_path(project_without_group)
     else
       visit issues_group_path(group)
     end
