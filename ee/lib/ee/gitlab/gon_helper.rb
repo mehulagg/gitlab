3ab969e8348151e64531f5a344e5aeee6b3cbcfc
Vitaly Slobodin <vslobodin@gitlab.com>
2021-05-10 21:52:47 +0300

    Expose CustomersDot variables via gon on gl.com
    We plan to use a special modal in several places
    and to reduce number of changes we can
    just expose required variables via gon.

--- ee/lib/ee/gitlab/gon_helper.rb
+++ ee/lib/ee/gitlab/gon_helper.rb
@@ -10,6 +10,11 @@ module EE
         super
 
         gon.roadmap_epics_limit = 1000
+
+        if ::Gitlab.com?
+          gon.subscriptions_url = ::Gitlab::SubscriptionPortal::SUBSCRIPTIONS_URL
+          gon.payment_form_url  = ::Gitlab::SubscriptionPortal::PAYMENT_FORM_URL
+        end
       end
 
       # Exposes if a licensed feature is available.
