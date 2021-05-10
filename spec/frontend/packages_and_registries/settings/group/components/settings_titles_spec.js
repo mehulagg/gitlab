f2efe4e2e2f937a03be75771058753422cc02cf0
Nicolò Maria Mezzopera <nicolo.mezzopera@gmail.com>
2021-05-10 12:04:23 +0200

    Add settings titles component
    - source
    - tests
    - snapshots

---
+++ spec/frontend/packages_and_registries/settings/group/components/settings_titles_spec.js
@@ -0,0 +1,26 @@
+import { shallowMount } from '@vue/test-utils';
+import SettingsTitles from '~/packages_and_registries/settings/group/components/settings_titles.vue';
+
+describe('settings_titles', () => {
+  let wrapper;
+
+  const mountComponent = () => {
+    wrapper = shallowMount(SettingsTitles, {
+      propsData: {
+        title: 'foo',
+        subTitle: 'bar',
+      },
+    });
+  };
+
+  afterEach(() => {
+    wrapper.destroy();
+    wrapper = null;
+  });
+
+  it('renders properly', () => {
+    mountComponent();
+
+    expect(wrapper.element).toMatchSnapshot();
+  });
+});
