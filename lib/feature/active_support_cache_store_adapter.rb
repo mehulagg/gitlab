0c4ce2b24937103b10231cacc34b72919bd5abd3
Bob Van Landuyt <bob@gitlab.com>
2021-05-11 07:06:53 +0000

    Apply 1 suggestion(s) to 1 file(s)

--- lib/feature/active_support_cache_store_adapter.rb
+++ lib/feature/active_support_cache_store_adapter.rb
@@ -5,7 +5,7 @@
 class Feature
   class ActiveSupportCacheStoreAdapter < Flipper::Adapters::ActiveSupportCacheStore
     # This patch represents https://github.com/jnunemaker/flipper/pull/512. In
-    # Flipper 0.20.4 and later, we can remove this and just pass `write_through:
+    # Flipper 0.21.0 and later, we can remove this and just pass `write_through:
     # true` to the constructor in `Feature.build_flipper_instance`.
 
     extend ::Gitlab::Utils::Override
