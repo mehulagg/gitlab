d8ee3cf99ef462a8b3f6cbcee6195ebead2e23b7
Thong Kuah <tkuah@gitlab.com>
2021-05-11 15:47:07 +1200

    Move OnboardingProgress to be sharded
    This is so that Groups::CreateService works as we create group and
    OnboardingProgress in the same transaction.transaction

--- app/models/onboarding_progress.rb
+++ app/models/onboarding_progress.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-class OnboardingProgress < ApplicationRecord
+class OnboardingProgress < NamespaceShard
   belongs_to :namespace, optional: false
 
   validate :namespace_is_root_namespace
