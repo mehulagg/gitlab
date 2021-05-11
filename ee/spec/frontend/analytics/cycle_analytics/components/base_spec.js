a9f60708ed68744a07c1dfa36f42d248232da6e2
Ezekiel Kigbo <ekigbo@gitlab.com>
2021-05-11 12:48:56 +1000

    Removes the legacy stage table components
    With the removal of the value_stream_analytics_path_navigation
    feature flag, we can now remove the legacy stage table code.

--- ee/spec/frontend/analytics/cycle_analytics/components/base_spec.js
+++ ee/spec/frontend/analytics/cycle_analytics/components/base_spec.js
@@ -8,7 +8,7 @@ import DurationChart from 'ee/analytics/cycle_analytics/components/duration_char
 import FilterBar from 'ee/analytics/cycle_analytics/components/filter_bar.vue';
 import Metrics from 'ee/analytics/cycle_analytics/components/metrics.vue';
 import PathNavigation from 'ee/analytics/cycle_analytics/components/path_navigation.vue';
-import StageTableNew from 'ee/analytics/cycle_analytics/components/stage_table_new.vue';
+import StageTable from 'ee/analytics/cycle_analytics/components/stage_table.vue';
 import TypeOfWorkCharts from 'ee/analytics/cycle_analytics/components/type_of_work_charts.vue';
 import ValueStreamSelect from 'ee/analytics/cycle_analytics/components/value_stream_select.vue';
 import {
@@ -170,7 +170,7 @@ describe('Value Stream Analytics component', () => {
   };
 
   const displaysStageTable = (flag) => {
-    expect(wrapper.find(StageTableNew).exists()).toBe(flag);
+    expect(wrapper.find(StageTable).exists()).toBe(flag);
   };
 
   const displaysDurationChart = (flag) => {
