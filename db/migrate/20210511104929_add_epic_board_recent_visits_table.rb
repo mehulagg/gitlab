64f0c2b7bef94081197601487675c9190657b6bc
Arturo Herrero <arturo.herrero@gmail.com>
2021-05-11 13:27:01 +0100

    Create table if not exists

--- db/migrate/20210511104929_add_epic_board_recent_visits_table.rb
+++ db/migrate/20210511104929_add_epic_board_recent_visits_table.rb
@@ -5,11 +5,13 @@ class AddEpicBoardRecentVisitsTable < ActiveRecord::Migration[6.0]
 
   def up
     with_lock_retries do
-      create_table :boards_epic_board_recent_visits do |t|
-        t.references :user, index: true, null: false, foreign_key: { on_delete: :cascade }
-        t.references :epic_board, index: true, foreign_key: { to_table: :boards_epic_boards, on_delete: :cascade }, null: false
-        t.references :group, index: true, foreign_key: { to_table: :namespaces, on_delete: :cascade }, null: false
-        t.timestamps_with_timezone null: false
+      unless table_exists?(:boards_epic_board_recent_visits)
+        create_table :boards_epic_board_recent_visits do |t|
+          t.references :user, index: true, null: false, foreign_key: { on_delete: :cascade }
+          t.references :epic_board, index: true, foreign_key: { to_table: :boards_epic_boards, on_delete: :cascade }, null: false
+          t.references :group, index: true, foreign_key: { to_table: :namespaces, on_delete: :cascade }, null: false
+          t.timestamps_with_timezone null: false
+        end
       end
     end
   end
