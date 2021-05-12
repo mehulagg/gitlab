2c77475bf8bafb8e07426a9a903df7dd282213f6
Grzegorz Bizon <grzesiek.bizon@gmail.com>
2021-05-12 16:09:24 +0200

    Fix typo on the build transition from pending state

--- app/models/ci/build.rb
+++ app/models/ci/build.rb
@@ -302,7 +302,7 @@ module Ci
         end
       end
 
-      after transition pending: any do |build|
+      after_transition pending: any do |build|
         # TODO ensure that there is no race condition here, add test for this
         Ci::UpdateBuildQueueService.queue_pop!(build)
       end
