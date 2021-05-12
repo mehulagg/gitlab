98eb8cc317ebb8bbb1ab16d368356e2f20c2d730
Mark Lapierre <mlapierre@gitlab.com>
2021-05-12 10:57:58 +1000

    Add failing test

--- ee/spec/features/admin/cloud_licences/admin_views_cloud_license_spec.rb
+++ ee/spec/features/admin/cloud_licences/admin_views_cloud_license_spec.rb
@@ -87,7 +87,7 @@ RSpec.describe 'Admin views Cloud License', :js do
     context 'Upload Legacy License' do
       it 'shows a link to upload a legacy license' do
         page.within(find('#content-body', match: :first)) do
-          expect(page).to have_link('Upload a legacy license', href: new_admin_license_path)
+          expect(page).to have_link('Download a legacy license', href: new_admin_license_path)
         end
       end
     end
