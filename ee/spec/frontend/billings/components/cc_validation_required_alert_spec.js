757bd5ffc74c7289c33788a78e664b7e13583529
Vitaly Slobodin <vslobodin@gitlab.com>
2021-05-12 10:01:43 +0300

    Show success alert on successful validation result

--- ee/spec/frontend/billings/components/cc_validation_required_alert_spec.js
+++ ee/spec/frontend/billings/components/cc_validation_required_alert_spec.js
@@ -1,5 +1,6 @@
 import { GlAlert, GlSprintf } from '@gitlab/ui';
 import { shallowMount } from '@vue/test-utils';
+import AccountVerificationModal from 'ee/billings/components/account_verification_modal.vue';
 import CreditCardValidationRequiredAlert from 'ee/billings/components/cc_validation_required_alert.vue';
 import { TEST_HOST } from 'helpers/test_constants';
 
@@ -13,6 +14,8 @@ describe('CreditCardValidationRequiredAlert', () => {
       },
     });
   };
+
+  const findGlAlert = () => wrapper.findComponent(GlAlert);
 
   beforeEach(() => {
     window.gon = {
@@ -28,12 +31,24 @@ describe('CreditCardValidationRequiredAlert', () => {
   });
 
   it('renders title', () => {
-    expect(wrapper.findComponent(GlAlert).attributes('title')).toBe('User Verification Required');
+    expect(findGlAlert().attributes('title')).toBe('User Verification Required');
   });
 
   it('renders description', () => {
-    expect(wrapper.findComponent(GlAlert).text()).toContain(
-      'As a user on a free or trial namespace',
-    );
+    expect(findGlAlert().text()).toContain('As a user on a free or trial namespace');
+  });
+
+  it('renders danger alert', () => {
+    expect(findGlAlert().attributes('variant')).toBe('danger');
+  });
+
+  describe('on success', () => {
+    beforeEach(() => {
+      wrapper.findComponent(AccountVerificationModal).vm.$emit('success');
+    });
+
+    it('renders the success alert instead of danger', () => {
+      expect(findGlAlert().attributes('variant')).toBe('success');
+    });
   });
 });
