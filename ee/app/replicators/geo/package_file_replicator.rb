5665aaf8ebfb22d95f593657d76d515263aa4d29
Valery Sizov <valery@gitlab.com>
2021-05-12 12:47:31 +0300

    Geo: Remove released feature flag `geo_package_file_verification`
    Removes the feature flag, it was enabled by default for a long time

--- ee/app/replicators/geo/package_file_replicator.rb
+++ ee/app/replicators/geo/package_file_replicator.rb
@@ -9,11 +9,6 @@ module Geo
       ::Packages::PackageFile
     end
 
-    override :verification_feature_flag_enabled?
-    def self.verification_feature_flag_enabled?
-      Feature.enabled?(:geo_package_file_verification, default_enabled: :yaml)
-    end
-
     def carrierwave_uploader
       model_record.file
     end
