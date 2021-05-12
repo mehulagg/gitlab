87b20a4fb761643790e25b1b4875e4d205ebbf21
Doug Stull <dstull@gitlab.com>
2021-05-12 14:49:05 -0400

    Better separate suggestion api path into ee
    - was leaking into FOSS a bit too much.

--- app/helpers/registrations_helper.rb
+++ app/helpers/registrations_helper.rb
@@ -7,6 +7,19 @@ module RegistrationsHelper
       devise_mapping.omniauthable? &&
       button_based_providers_enabled?
   end
+
+  def signup_username_data_attributes
+    max_username_length = 255
+    min_username_length = 2
+
+    {
+      min_length: min_username_length,
+      min_length_message: s_('SignUp|Username is too short (minimum is %{min_length} characters).') % { min_length: min_username_length },
+      max_length: max_username_length,
+      max_length_message: s_('SignUp|Username is too long (maximum is %{max_length} characters).') % { max_length: max_username_length },
+      qa_selector: 'new_user_username_field'
+    }
+  end
 end
 
 RegistrationsHelper.prepend_mod_with('RegistrationsHelper')
