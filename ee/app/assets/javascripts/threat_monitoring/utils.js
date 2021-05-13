d5b8df65f92cacd076a847f28b22764cada433c4
Alexander Turinske <aturinske@gitlab.com>
2021-05-13 13:13:57 -0600

    Fix policy list drawer height
    - use content wrapper to retrive top height

---
+++ ee/app/assets/javascripts/threat_monitoring/utils.js
@@ -0,0 +1,16 @@
+/**
+ * Get the height of the wrapper page element
+ * This height can be used to determine where the highest element goes in a page
+ * Useful for gl-drawer's header-height prop
+ * @param {String} class the content wrapper class
+ * @returns {String} height in px
+ */
+export const getContentWrapperHeight = (contentWrapperClass) => {
+  const wrapperEl = document.querySelector(contentWrapperClass);
+
+  if (wrapperEl) {
+    return `${wrapperEl.offsetTop}px`;
+  }
+
+  return '';
+};
