01ce85f567b5717b1bcb63addac8fc42a5379057
Donald Cook <dcook@gitlab.com>
2021-05-17 11:45:09 -0700

    Frontend to add before and after logic to epic board drag

--- ee/app/assets/javascripts/boards/stores/actions.js
+++ ee/app/assets/javascripts/boards/stores/actions.js
@@ -497,6 +497,8 @@ export default {
           boardId: fullEpicBoardId(boardId),
           fromListId,
           toListId,
+          moveBeforeId: moveBeforeId && fullEpicId(moveBeforeId),
+          moveAfterId: moveAfterId && fullEpicId(moveAfterId),
         },
       })
       .then(({ data }) => {
