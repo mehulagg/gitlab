08320e764f5dd528b85a25b76a7de58852373397
Florie Guibert <fguibert@gitlab.com>
2021-05-18 10:07:15 +1000

    Fix ability for non project member to subscribe to an issue

--- spec/features/issues/user_toggles_subscription_spec.rb
+++ spec/features/issues/user_toggles_subscription_spec.rb
@@ -5,16 +5,55 @@ require "spec_helper"
 RSpec.describe "User toggles subscription", :js do
   let(:project) { create(:project_empty_repo, :public) }
   let(:user) { create(:user) }
+  let(:user2) { create(:user) }
   let(:issue) { create(:issue, project: project, author: user) }
 
-  before do
-    project.add_developer(user)
-    sign_in(user)
+  context 'user is not logged in' do
+    before do
+      visit(project_issue_path(project, issue))
+    end
 
-    visit(project_issue_path(project, issue))
+    it 'toggle does not display' do
+      expect(page).not_to have_button('Notifications')
+    end
   end
 
-  it "unsubscribes from issue" do
+  context 'user is logged in' do
+    before do
+      project.add_developer(user)
+      sign_in(user)
+
+      visit(project_issue_path(project, issue))
+    end
+
+    it 'unsubscribes from issue' do
+      unsubscribe
+    end
+
+    context 'when project emails are disabled' do
+      let(:project) { create(:project_empty_repo, :public, emails_disabled: true) }
+
+      it 'is disabled' do
+        expect(page).to have_content('Disabled by project owner')
+        expect(page).to have_button('Notifications', class: 'is-disabled')
+      end
+    end
+  end
+  
+  context 'user is logged without edit permission' do
+    before do
+      sign_in(user2)
+
+      visit(project_issue_path(project, issue))
+    end
+
+    it 'unsubscribes from issue' do
+      binding.pry
+      unsubscribe
+    end
+  end
+
+  def unsubscribe
     subscription_button = find('[data-testid="subscription-toggle"]')
 
     # Check we're subscribed.
@@ -26,14 +65,5 @@ RSpec.describe "User toggles subscription", :js do
 
     # Check we're unsubscribed.
     expect(subscription_button).to have_css("button:not(.is-checked)")
-  end
-
-  context 'when project emails are disabled' do
-    let(:project) { create(:project_empty_repo, :public, emails_disabled: true) }
-
-    it 'is disabled' do
-      expect(page).to have_content('Disabled by project owner')
-      expect(page).to have_button('Notifications', class: 'is-disabled')
-    end
   end
 end
