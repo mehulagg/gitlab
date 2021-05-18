96fc013e79552e366d7d04765860dad748de45ca
David Fernandez <dfernandez@gitlab.com>
2021-05-18 11:03:20 +0200

    Add post migration for unused expiration policies
    Disable container expiration policies that are linked to a project that
    doesn't have any container repository.
    
    Changelog: performance

---
+++ db/post_migrate/20210518074332_disable_expiration_policies_linked_to_no_container_images.rb
@@ -0,0 +1,37 @@
+# frozen_string_literal: true
+
+class DisableExpirationPoliciesLinkedToNoContainerImages < ActiveRecord::Migration[6.0]
+  disable_ddl_transaction!
+
+  BATCH_SIZE = 2000
+
+  class ContainerExpirationPolicy < ActiveRecord::Base
+    include ::EachBatch
+
+    self.table_name = 'container_expiration_policies'
+  end
+
+  class ContainerRepository < ActiveRecord::Base
+    self.table_name = 'container_repositories'
+  end
+
+  def up
+    policies = ContainerExpirationPolicy
+      .where(enabled: true)
+      .where.not(
+        'EXISTS (?)',
+        ContainerRepository.select(1)
+                           .where(
+                             'container_repositories.project_id = container_expiration_policies.project_id'
+                           )
+      )
+
+    policies.each_batch(of: BATCH_SIZE) do |batch, index|
+      policies.update_all(enabled: false)
+    end
+  end
+
+  def down
+    # this migration is irreversible
+  end
+end
