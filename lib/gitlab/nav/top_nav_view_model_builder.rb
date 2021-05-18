b0b10f4688a50c872d8f9885a4023976ba23d07e
Paul Slaughter <pslaughter@gitlab.com>
2021-05-18 12:44:34 -0500

    Fix shortcuts for top nav redesign
    - We're using the view model since all
      the logic is already encapsulated there
      and needs to be reused.
    - We use the HAML because it's important
      this is not coupled to the JavaScript
      which might be async loaded +
      will work on feature specs without
      enabled JS

--- lib/gitlab/nav/top_nav_view_model_builder.rb
+++ lib/gitlab/nav/top_nav_view_model_builder.rb
@@ -6,9 +6,26 @@ module Gitlab
       def initialize
         @menu_builder = ::Gitlab::Nav::TopNavMenuBuilder.new
         @views = {}
+        @shortcuts = []
       end
 
       delegate :add_primary_menu_item, :add_secondary_menu_item, to: :@menu_builder
+
+      def add_shortcut(**args)
+        item = ::Gitlab::Nav::TopNavMenuItem.build(**args)
+
+        @shortcuts.push(item)
+      end
+
+      def add_primary_menu_item_with_shortcut(shortcut_class:, shortcut_href: nil, **args)
+        self.add_primary_menu_item(**args)
+        self.add_shortcut(
+          id: "#{args.fetch(:id)}-shortcut",
+          title: args.fetch(:title),
+          href: shortcut_href || args.fetch(:href),
+          css_class: shortcut_class
+        )
+      end
 
       def add_view(name, props)
         @views[name] = props
@@ -19,6 +36,7 @@ module Gitlab
 
         menu.merge({
           views: @views,
+          shortcuts: @shortcuts,
           activeTitle: _('Menu')
         })
       end
