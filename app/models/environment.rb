ddc970112032c343ffe3d071e0a378b8574433b1
Kamil Trzciński <ayufan@ayufan.eu>
2021-05-19 17:34:49 +0200

    Fix `disable_joins:` for `Envs`

--- app/models/environment.rb
+++ app/models/environment.rb
@@ -26,10 +26,10 @@ class Environment < ApplicationRecord
 
   has_one :last_deployment, -> { success.distinct_on_environment }, class_name: 'Deployment', inverse_of: :environment
   has_one :last_deployable, through: :last_deployment, source: 'deployable', source_type: 'CommitStatus', disable_joins: true
-  has_one :last_pipeline, through: :last_deployable, source: 'pipeline', disable_joins: true
+  has_one :last_pipeline, through: :last_deployable, source: 'pipeline'
   has_one :last_visible_deployment, -> { visible.distinct_on_environment }, inverse_of: :environment, class_name: 'Deployment'
   has_one :last_visible_deployable, through: :last_visible_deployment, source: 'deployable', source_type: 'CommitStatus', disable_joins: true
-  has_one :last_visible_pipeline, through: :last_visible_deployable, source: 'pipeline', disable_joins: true
+  has_one :last_visible_pipeline, through: :last_visible_deployable, source: 'pipeline'
   has_one :upcoming_deployment, -> { running.distinct_on_environment }, class_name: 'Deployment', inverse_of: :environment
   has_one :latest_opened_most_severe_alert, -> { order_severity_with_open_prometheus_alert }, class_name: 'AlertManagement::Alert', inverse_of: :environment
 
