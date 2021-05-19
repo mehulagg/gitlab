cb69036cbcd203842c6a69fbbcd16dc5ee5528a2
Kamil Trzciński <ayufan@ayufan.eu>
2021-05-19 14:40:31 +0000

    Update asset_proxy_settings.rb

--- config/initializers/asset_proxy_settings.rb
+++ config/initializers/asset_proxy_settings.rb
@@ -3,6 +3,6 @@
 #
 # Asset proxy settings
 #
-# ActiveSupport.on_load(:active_record) do
-#   Banzai::Filter::AssetProxyFilter.initialize_settings
-# end
+ActiveSupport.on_load(:active_record) do
+  Banzai::Filter::AssetProxyFilter.initialize_settings
+end
