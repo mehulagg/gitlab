1b739bc9c9fcbfdf7f1bfca66a986dfe19440c28
Kamil Trzciński <ayufan@ayufan.eu>
2021-05-20 14:59:54 +0200

    Improve dynamic shards

--- lib/gitlab/database/dynamic_shards.rb
+++ lib/gitlab/database/dynamic_shards.rb
@@ -1,7 +1,5 @@
 module DynamicShards
   def database_configuration
-    puts "Dynamic Shards"
-
     super.to_h do |env, config|
       if config.is_a?(Hash) && config.all? { |_, v| v.is_a?(Hash) }
         # if all hash, it means that this is new multi-database spec
@@ -10,12 +8,13 @@ module DynamicShards
       end
 
       # Hack for CI tests to ensure that we always have `gitlabhq_test` since code depends on it...
-      primary_db = config["database"] == "gitlabhq_test" ? "gitlabhq_test" : "#{config["database"]}_primary"
+      # This is in dev env to not mangle other testing DBs
+      unless ENV['CI']
+        config["database"] += "_primary"
+      end
 
       shards = {
-        "primary" => config.merge(
-          database: primary_db
-        ),
+        "primary" => config,
         "ci" => config.merge(
           database: "#{config["database"]}_ci",
           migrations_paths: "db/migrate_ci"
