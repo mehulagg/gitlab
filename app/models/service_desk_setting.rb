e20c4df3c90a50a1dca31b320c1afd9784502d12
Felipe Artur <felipefac@gmail.com>
2021-05-20 16:15:01 -0300

    Prevent projects with same slug from having same project key
    Invalidate project records with same slug
    and service desk project key

--- app/models/service_desk_setting.rb
+++ app/models/service_desk_setting.rb
@@ -6,6 +6,7 @@ class ServiceDeskSetting < ApplicationRecord
   belongs_to :project
   validates :project_id, presence: true
   validate :valid_issue_template
+  validate :valid_project_key
   validates :outgoing_name, length: { maximum: 255 }, allow_blank: true
   validates :project_key, length: { maximum: 255 }, allow_blank: true, format: { with: /\A[a-z0-9_]+\z/ }
 
@@ -25,6 +26,24 @@ class ServiceDeskSetting < ApplicationRecord
   def valid_issue_template
     if issue_template_missing?
       errors.add(:issue_template_key, 'is empty or does not exist')
+    end
+  end
+
+  def valid_service_desk_key
+    if has_projects_with_same_key_and_slug?
+      errors.add(:project_key, 'Already in use for another service desk address.')
+    end
+  end
+
+  private
+
+  def has_projects_with_same_slug_and_key?
+    return false unless project_key
+
+    projects = Project.with_service_desk_key(project_key)
+
+    projects.any? do |project|
+      project.full_path_slug == self.full_path_slug
     end
   end
 end
