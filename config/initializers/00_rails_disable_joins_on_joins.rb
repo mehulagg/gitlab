d3f36f9bebe32ff2899bef9b59d899fa2a044285
Kamil Trzciński <ayufan@ayufan.eu>
2021-05-20 09:44:22 +0200

    Disallow joins cross databases

---
+++ config/initializers/00_rails_disable_joins_on_joins.rb
@@ -0,0 +1,26 @@
+module DisableJoinsOnJoins
+  module Reflection
+    extend ActiveSupport::Concern
+
+    EagerJoinDisallowed = Class.new(ArgumentError)
+
+    def check_eager_loadable!
+      # active_record: source
+      # klass: target
+
+      super
+
+      source_ci = active_record < Ci::ApplicationRecord
+      target_ci = klass < Ci::ApplicationRecord
+
+      unless source_ci == target_ci
+        raise EagerJoinDisallowed, <<-MSG.squish
+          The association scope '#{active_record.name}.#{name}' is cross shard.
+          Eager loading / joining is disallowed.
+        MSG
+      end
+    end
+  end
+end
+
+ActiveRecord::Reflection::AssociationReflection.prepend(DisableJoinsOnJoins::Reflection)
