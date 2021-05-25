95531d7eef8e2d760d9dbd74e62b21cf2b5597ae
Thomas Randolph <trandolph@gitlab.com>
2021-05-24 22:03:53 -0600

    Show Diff byte difference when it's not diffable by lines

--- app/assets/javascripts/diffs/components/diff_stats.vue
+++ app/assets/javascripts/diffs/components/diff_stats.vue
@@ -2,10 +2,17 @@
 import { GlIcon } from '@gitlab/ui';
 import { isNumber } from 'lodash';
 import { n__ } from '~/locale';
+import { numberToHumanSize, changeInPercent } from '../../lib/utils/number_utils';
+import { isNotDiffable } from '../utils/diff_file';
 
 export default {
   components: { GlIcon },
   props: {
+    diffFile: {
+      type: Object,
+      required: false,
+      default: () => null,
+    },
     addedLines: {
       type: Number,
       required: true,
@@ -33,6 +40,36 @@ export default {
     hasDiffFiles() {
       return isNumber(this.diffFilesLength) && this.diffFilesLength >= 0;
     },
+    notDiffable() {
+      return this.diffFile ? isNotDiffable(this.diffFile) : false;
+    },
+    bytes() {
+      const computes = {};
+      let color = '';
+      let sign = '';
+      let percent = 0;
+      let diff = 0;
+
+      if (this.diffFile) {
+        percent = changeInPercent(this.diffFile.old_size, this.diffFile.new_size);
+        diff = this.diffFile.new_size - this.diffFile.old_size;
+        sign = diff >= 0 ? '+' : '';
+
+        if (diff > 0) {
+          color = 'cgreen';
+        } else if (diff < 0) {
+          color = 'cred';
+        }
+      }
+
+      computes.text = `${sign}${numberToHumanSize(diff)} (${sign}${percent}%)`;
+      computes.percent = percent;
+      computes.changed = diff;
+      computes.color = color;
+      computes.sign = sign;
+
+      return computes;
+    },
   },
 };
 </script>
@@ -45,23 +82,28 @@ export default {
       'd-none d-sm-inline-flex': !isCompareVersionsHeader,
     }"
   >
-    <div v-if="hasDiffFiles" class="diff-stats-group">
-      <gl-icon name="doc-code" class="diff-stats-icon text-secondary" />
-      <span class="text-secondary bold">{{ diffFilesCountText }} {{ filesText }}</span>
+    <div v-if="notDiffable" :class="bytes.color">
+      {{ bytes.text }}
     </div>
-    <div
-      class="diff-stats-group cgreen d-flex align-items-center"
-      :class="{ bold: isCompareVersionsHeader }"
-    >
-      <span>+</span>
-      <span class="js-file-addition-line">{{ addedLines }}</span>
-    </div>
-    <div
-      class="diff-stats-group cred d-flex align-items-center"
-      :class="{ bold: isCompareVersionsHeader }"
-    >
-      <span>-</span>
-      <span class="js-file-deletion-line">{{ removedLines }}</span>
+    <div v-else class="diff-stats-contents">
+      <div v-if="hasDiffFiles" class="diff-stats-group">
+        <gl-icon name="doc-code" class="diff-stats-icon text-secondary" />
+        <span class="text-secondary bold">{{ diffFilesCountText }} {{ filesText }}</span>
+      </div>
+      <div
+        class="diff-stats-group cgreen d-flex align-items-center"
+        :class="{ bold: isCompareVersionsHeader }"
+      >
+        <span>+</span>
+        <span class="js-file-addition-line">{{ addedLines }}</span>
+      </div>
+      <div
+        class="diff-stats-group cred d-flex align-items-center"
+        :class="{ bold: isCompareVersionsHeader }"
+      >
+        <span>-</span>
+        <span class="js-file-deletion-line">{{ removedLines }}</span>
+      </div>
     </div>
   </div>
 </template>
