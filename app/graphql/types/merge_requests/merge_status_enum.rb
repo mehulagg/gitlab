90779e9e414d72a155ae6d5bda5077d544808cce
Alex Kalderimis <akalderimis@gitlab.com>
2021-05-28 09:30:49 +0100

    Improve layout to please rubocop

--- app/graphql/types/merge_requests/merge_status_enum.rb
+++ app/graphql/types/merge_requests/merge_status_enum.rb
@@ -6,11 +6,21 @@ module Types
       graphql_name 'MergeStatus'
       description 'Representation of whether a GitLab merge request be merged.'
 
-      value 'UNCHECKED', value: 'unchecked', description: 'Merge status has not been checked.'
-      value 'CHECKING', value: 'checking', description: 'Currently checking for mergeability.'
-      value 'CAN_BE_MERGED', value: 'can_be_merged', description: 'There are no conflicts between the source and target branches.'
-      value  'CANNOT_BE_MERGED', value: 'cannot_be_merged', description: 'There are conflicts between the source and target branches.'
-      value 'CANNOT_BE_MERGED_RECHECK', value: 'cannot_be_merged_recheck', description: 'Currently unchecked. The previous state was cannot be merged.'
+      value 'UNCHECKED',
+            value: 'unchecked',
+            description: 'Merge status has not been checked.'
+      value 'CHECKING',
+            value: 'checking',
+            description: 'Currently checking for mergeability.'
+      value 'CAN_BE_MERGED',
+            value: 'can_be_merged',
+            description: 'There are no conflicts between the source and target branches.'
+      value 'CANNOT_BE_MERGED',
+            value: 'cannot_be_merged',
+            description: 'There are conflicts between the source and target branches.'
+      value 'CANNOT_BE_MERGED_RECHECK',
+            value: 'cannot_be_merged_recheck',
+            description: 'Currently unchecked. The previous state was cannot be merged.'
     end
   end
 end
