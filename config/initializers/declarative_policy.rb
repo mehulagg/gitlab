9f1384dc799b6ace4ecf19f0473fa3eee6baab52
Alex Kalderimis <akalderimis@gitlab.com>
2021-05-31 11:08:39 +0100

    Use AR interlock when loading policies

--- config/initializers/declarative_policy.rb
+++ config/initializers/declarative_policy.rb
@@ -2,4 +2,8 @@
 
 DeclarativePolicy.configure do
   named_policy :global, ::GlobalPolicy
+
+  class_for do |name|
+    ActiveSupport::Dependencies.interlock.permit_concurrent_loads { name.constantize }
+  end
 end
