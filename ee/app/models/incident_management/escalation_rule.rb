12f3b42af78d79e105510910b7dc92ba4870d533
Sean Arnold <sarnold@gitlab.com>
2021-05-31 16:56:36 +1200

    Add logic to create escalations on alert process
    - Add tests for prometheus and regular alerts
    - Tidy up some other specs

--- ee/app/models/incident_management/escalation_rule.rb
+++ ee/app/models/incident_management/escalation_rule.rb
@@ -16,7 +16,5 @@ module IncidentManagement
               numericality: { only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 24.hours }
 
     validates :policy_id, uniqueness: { scope: [:oncall_schedule_id, :status, :elapsed_time_seconds], message: _('Must have a unique policy, status, and elapsed time') }
-
-    scope :ordered_by_elapsed_time_seconds, -> { order(elapsed_time_seconds: :desc) }
   end
 end
