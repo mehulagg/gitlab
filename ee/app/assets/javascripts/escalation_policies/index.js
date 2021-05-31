429fb292fccc7596dda69da599cacd2d0e7b68e8
Olena Horal-Koretska <ohoralkoretska@gitlab.com>
2021-05-31 13:59:58 +0300

    Redefine Apollo cache unique identifier for oncall-schedules query

--- ee/app/assets/javascripts/escalation_policies/index.js
+++ ee/app/assets/javascripts/escalation_policies/index.js
@@ -1,3 +1,4 @@
+import { defaultDataIdFromObject } from 'apollo-cache-inmemory';
 import Vue from 'vue';
 import VueApollo from 'vue-apollo';
 import createDefaultClient from '~/lib/graphql';
@@ -6,7 +7,20 @@ import EscalationPoliciesWrapper from './components/escalation_policies_wrapper.
 Vue.use(VueApollo);
 
 const apolloProvider = new VueApollo({
-  defaultClient: createDefaultClient(),
+  defaultClient: createDefaultClient(
+    {},
+    {
+      cacheConfig: {
+        dataIdFromObject: (object) => {
+          // eslint-disable-next-line no-underscore-dangle
+          if (object.__typename === 'IncidentManagementOncallSchedule') {
+            return object.iid;
+          }
+          return defaultDataIdFromObject(object);
+        },
+      },
+    },
+  ),
 });
 
 export default () => {
