3566513cf24c484abcd99f61d5f93018ccc95fc0
Brandon Labuschagne <blabuschagne@gitlab.com>
2021-06-01 16:44:34 +0200

    Ensure DA cell flag is reactive
    The component was not refreshing the
    displayed values when the enabled prop
    was updated
    
    This commit fixes that
    
    Changelog: fixed
    EE: true

--- ee/spec/frontend/analytics/devops_report/devops_adoption/components/devops_adoption_table_cell_flag_spec.js
+++ ee/spec/frontend/analytics/devops_report/devops_adoption/components/devops_adoption_table_cell_flag_spec.js
@@ -36,6 +36,26 @@ describe('DevopsAdoptionTableCellFlag', () => {
       expect(tooltip).toBeDefined();
       expect(tooltip.value).toBe('Adopted');
     });
+
+    describe('reactivity', () => {
+      beforeEach(async () => {
+        wrapper.setProps({ enabled: false });
+
+        await wrapper.vm.$nextTick();
+      });
+
+      it('displays the corrent class when updated', () => {
+        expect(wrapper.classes()).toContain('circle');
+        expect(wrapper.classes()).not.toContain('circle-enabled');
+      });
+
+      it('displays the corrent toolip when updated', () => {
+        const tooltip = getBinding(wrapper.element, 'gl-tooltip');
+
+        expect(tooltip).toBeDefined();
+        expect(tooltip.value).toBe('Not adopted');
+      });
+    });
   });
 
   describe('disabled', () => {
@@ -51,6 +71,26 @@ describe('DevopsAdoptionTableCellFlag', () => {
 
       expect(tooltip).toBeDefined();
       expect(tooltip.value).toBe('Not adopted');
+    });
+
+    describe('reactivity', () => {
+      beforeEach(async () => {
+        wrapper.setProps({ enabled: true });
+
+        await wrapper.vm.$nextTick();
+      });
+
+      it('displays the corrent class when updated', () => {
+        expect(wrapper.classes()).toContain('circle');
+        expect(wrapper.classes()).toContain('circle-enabled');
+      });
+
+      it('displays the corrent toolip when updated', () => {
+        const tooltip = getBinding(wrapper.element, 'gl-tooltip');
+
+        expect(tooltip).toBeDefined();
+        expect(tooltip.value).toBe('Adopted');
+      });
     });
   });
 });
