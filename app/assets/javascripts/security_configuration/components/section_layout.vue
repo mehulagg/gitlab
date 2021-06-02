392dcf182dfa32e12aaed0f77e85bf464383b0a2
Jannik Lehmann <jlehmann@gitlab.com>
2021-06-02 18:41:29 +0200

    Add compliance tab to redesigned Security-Configuration Page
    This Commit solves https://gitlab.com/gitlab-org/gitlab/-/issues/328395
    It introduces the compliance Tab to the
    redesigned Security-Configuration Page..
    
    Changelog: added

---
+++ app/assets/javascripts/security_configuration/components/section_layout.vue
@@ -0,0 +1,23 @@
+<script>
+export default {
+  name: 'SectionLayout',
+  props: {
+    heading: {
+      type: String,
+      required: true,
+    },
+  },
+};
+</script>
+
+<template>
+  <div class="row">
+    <div class="col-lg-5">
+      <h2 class="gl-font-size-h2 gl-mt-0" data-test-id="layout-heading">{{ heading }}</h2>
+      <slot name="latest-pipeline"></slot>
+    </div>
+    <div class="col-lg-7">
+      <slot name="features"></slot>
+    </div>
+  </div>
+</template>
