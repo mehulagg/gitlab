4596ca65ccdca2ab206aad0b20564e4fa415f1da
Dmitry Gruzd <dgruzd@gitlab.com>
2021-06-02 13:17:27 +0300

    fixup! Remove old Advanced Search migrations

--- ee/app/services/elastic/data_migration_service.rb
+++ ee/app/services/elastic/data_migration_service.rb
@@ -25,8 +25,13 @@ module Elastic
         migrations.find { |m| m.version == version }
       end
 
+      def find_by_name(name)
+        migrations.find { |migration| migration.name_for_key == name.to_s.underscore }
+      end
+
       def find_by_name!(name)
-        migration = migrations.find { |migration| migration.name_for_key == name.to_s.underscore }
+        migration = find_by_name(name)
+
         raise ArgumentError, "Couldn't find Elastic::Migration with name='#{name}'" unless migration
         raise ArgumentError, "Elastic::Migration with name='#{name}' is marked as obsolete" if migration.obsolete?
 
@@ -44,7 +49,7 @@ module Elastic
       end
 
       def migration_has_finished_uncached?(name)
-        migration = find_by_name!(name)
+        migration = find_by_name(name)
 
         !!migration&.load_from_index&.dig('_source', 'completed')
       end
