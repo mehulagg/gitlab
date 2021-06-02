33015a2dd179ad5e485a406ee74fc2c487068620
Dmitry Gruzd <dgruzd@gitlab.com>
2021-06-02 12:09:36 +0300

    fixup! Remove old Advanced Search migrations

--- ee/elastic/migrate/20201105181100_apply_max_analyzed_offset.rb
+++ ee/elastic/migrate/20201105181100_apply_max_analyzed_offset.rb
@@ -1,32 +1,15 @@
 # frozen_string_literal: true
 
 class ApplyMaxAnalyzedOffset < Elastic::Migration
-  # Important: Any update to the Elastic index mappings should be replicated in Elastic::Latest::Config
-
   def migrate
-    if max_analyzed_offset_setting == current_max_analyzed_offset
-      log "Skipping highlight.max_analyzed_offset migration since it is already applied"
-      return
-    end
+    log "Migration has been deleted in the last major version upgrade." \
+      "Migrations are supposed to be finished before upgrading major version https://docs.gitlab.com/ee/update/#upgrading-to-a-new-major-version ." \
+      "To correct this issue, recreate your index from scratch: https://docs.gitlab.com/ee/integration/elasticsearch.html#last-resort-to-recreate-an-index."
 
-    log "Setting highlight.max_analyzed_offset to #{max_analyzed_offset_setting}kb"
-    helper.update_settings(settings: { index: { 'highlight.max_analyzed_offset': max_analyzed_offset_setting } })
-    log 'Update of highlight.max_analyzed_offset is completed'
+    fail_migration_halt_error!
   end
 
-  # Check if the migration has completed
-  # Return true if completed, otherwise return false
   def completed?
-    max_analyzed_offset_setting == current_max_analyzed_offset
-  end
-
-  private
-
-  def max_analyzed_offset_setting
-    Gitlab::CurrentSettings.elasticsearch_indexed_file_size_limit_kb.kilobytes
-  end
-
-  def current_max_analyzed_offset
-    Gitlab::Elastic::Helper.default.get_settings.dig('highlight', 'max_analyzed_offset').to_i
+    false
   end
 end
