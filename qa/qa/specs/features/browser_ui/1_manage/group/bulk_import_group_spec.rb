843eb26f9fd990ff46c7f0e603d925b02bdc711d
Andrejs Cunskis <acunskis@gitlab.com>
2021-06-02 10:35:23 +0300

    Fabricate objects using user account

--- qa/qa/specs/features/browser_ui/1_manage/group/bulk_import_group_spec.rb
+++ qa/qa/specs/features/browser_ui/1_manage/group/bulk_import_group_spec.rb
@@ -3,19 +3,20 @@
 module QA
   RSpec.describe 'Manage', :requires_admin do
     describe 'Bulk group import' do
-      let!(:api_client) { Runtime::API::Client.as_admin }
+      let!(:admin_api_client) { Runtime::API::Client.as_admin }
       let!(:user) do
         Resource::User.fabricate_via_api! do |usr|
-          usr.api_client = api_client
+          usr.api_client = admin_api_client
           usr.hard_delete_on_api_removal = true
         end
       end
 
-      let!(:personal_access_token) { Runtime::API::Client.new(user: user).personal_access_token }
+      let!(:api_client) { Runtime::API::Client.new(user: user) }
+      let!(:personal_access_token) { api_client.personal_access_token }
 
       let!(:sandbox) do
         Resource::Sandbox.fabricate_via_api! do |group|
-          group.api_client = api_client
+          group.api_client = admin_api_client
         end
       end
 
@@ -61,7 +62,6 @@ module QA
 
       before do
         sandbox.add_member(user, Resource::Members::AccessLevel::MAINTAINER)
-        source_group.add_member(user, Resource::Members::AccessLevel::MAINTAINER)
 
         Flow::Login.sign_in(as: user)
         Page::Main::Menu.new.go_to_import_group
@@ -85,17 +85,13 @@ module QA
           aggregate_failures do
             expect(import_page).to have_imported_group(source_group.path, wait: 120)
 
-            expect { imported_group.reload! }.to eventually_eq(source_group).within(duration: 30)
-            expect { imported_subgroup.reload! }.to eventually_eq(subgroup).within(duration: 30)
+            expect { imported_group.reload! }.to eventually_eq(source_group).within(duration: 10)
+            expect { imported_subgroup.reload! }.to eventually_eq(subgroup).within(duration: 10)
           end
         end
       end
 
-      it(
-        'imports group labels',
-        testcase: 'https://gitlab.com/gitlab-org/quality/testcases/-/issues/1785',
-        quarantine: { issue: 'https://gitlab.com/gitlab-org/gitlab/-/issues/331704', type: :bug }
-      ) do
+      it 'imports group labels', testcase: 'https://gitlab.com/gitlab-org/quality/testcases/-/issues/1785' do
         Resource::GroupLabel.fabricate_via_api! do |label|
           label.api_client = api_client
           label.group = source_group
@@ -120,8 +116,8 @@ module QA
       end
 
       after do
-        user.remove_via_api!
         source_group.remove_via_api!
+        user.remove_via_api!
       end
 
       after(:all) do
