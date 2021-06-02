460318a2bb5c1fecdef4600edef6d10996e6646d
Andreas Brandl <abrandl@gitlab.com>
2021-06-02 11:14:31 +0200

    Restructure migration with added partial index

--- db/post_migrate/20210520105029_copy_pending_builds_to_pending_builds_table.rb
+++ db/post_migrate/20210520105029_copy_pending_builds_to_pending_builds_table.rb
@@ -4,6 +4,7 @@ class CopyPendingBuildsToPendingBuildsTable < ActiveRecord::Migration[6.0]
   include Gitlab::Database::MigrationHelpers
 
   BATCH_SIZE = 10000
+  TEMP_INDEX = 'ci_builds_pending_builds_id'
 
   disable_ddl_transaction!
 
@@ -22,21 +23,21 @@ class CopyPendingBuildsToPendingBuildsTable < ActiveRecord::Migration[6.0]
   end
 
   def up
+    # testing only
+    execute 'truncate ci_pending_builds'
+
+    add_concurrent_index :ci_builds, :id, where: "status = 'pending' AND type = 'Ci::Build'", name: TEMP_INDEX
+
     Build.pending.limit(1).pluck(:id).first.try do |id|
       Build.where('id >= ?', id).each_batch(of: BATCH_SIZE) do |batch|
         min_id, max_id = batch.pluck('MIN(id), MAX(id)').first
 
         execute <<~SQL
-          WITH pending_builds AS (
+          WITH pending_builds AS MATERIALIZED (
             SELECT id,
                    project_id
             FROM ci_builds
-            WHERE status = 'pending'
-              AND type = 'Ci::Build'
-              AND NOT EXISTS (
-                SELECT 1 FROM ci_pending_builds
-                  WHERE ci_pending_builds.build_id = ci_builds.id
-              )
+            WHERE status = 'pending' AND type = 'Ci::Build'
             AND id BETWEEN #{min_id} AND #{max_id}
           )
           INSERT INTO ci_pending_builds (build_id, project_id)
@@ -47,9 +48,12 @@ class CopyPendingBuildsToPendingBuildsTable < ActiveRecord::Migration[6.0]
         SQL
       end
     end
+  ensure
+    # commented out for testing
+    # remove_concurrent_index_by_name :ci_builds, name: TEMP_INDEX
   end
 
   def down
-    # noop
+    remove_concurrent_index_by_name :ci_builds, name: TEMP_INDEX
   end
 end
