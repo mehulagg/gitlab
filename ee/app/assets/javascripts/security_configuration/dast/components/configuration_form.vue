a458dc1ccc23ddb9ab1dbc6f142d6791269a3406
Dheeraj Joshi <djoshi@gitlab.com>
2021-06-03 17:29:33 +0530

    Add generate code snippet for DAST Configuration

--- ee/app/assets/javascripts/security_configuration/dast/components/configuration_form.vue
+++ ee/app/assets/javascripts/security_configuration/dast/components/configuration_form.vue
@@ -1,11 +1,15 @@
 <script>
-import { GlLink, GlSprintf } from '@gitlab/ui';
+import { GlLink, GlSprintf, GlButton, GlForm } from '@gitlab/ui';
+import ConfigurationSnippetModal from 'ee/security_configuration/components/configuration_snippet_modal.vue';
+import { CONFIGURATION_SNIPPET_MODAL_ID } from 'ee/security_configuration/components/constants';
 import { s__ } from '~/locale';
 
 export default {
   components: {
     GlLink,
     GlSprintf,
+    GlButton,
+    GlForm,
   },
   inject: {
     dastDocumentationPath: {
@@ -15,6 +19,17 @@ export default {
   i18n: {
     helpText: s__(`
       DastConfig|Customize DAST settings to suit your requirements. Configuration changes made here override these provided by GitLab and are excluded from updates. For details of more advanced configuration options, see the %{docsLinkStart}GitLab DAST documentation%{docsLinkEnd}.`),
+  },
+  data() {
+    return {
+      selectedSiteProfileId: '1',
+      selectedScannerProfileId: '2',
+    };
+  },
+  methods: {
+    onsubmit() {
+      // this.generateSnippet();
+    },
   },
 };
 </script>
@@ -28,5 +43,39 @@ export default {
         </template>
       </gl-sprintf>
     </p>
+
+    <gl-form @submit.prevent="onSubmit">
+      <!-- DAST Scanner Profile -->
+      {{ selectedScannerProfileId }}
+      <br />
+      <!-- DAST Site Profile -->
+      {{ selectedSiteProfileId }}
+
+      <hr />
+
+      <gl-button
+        :disabled="someFieldEmpty"
+        :loading="isLoading"
+        type="submit"
+        variant="confirm"
+        class="js-no-auto-disable"
+        data-testid="api-fuzzing-configuration-submit-button"
+        >{{ s__('APIFuzzing|Generate code snippet') }}</gl-button
+      >
+      <gl-button
+        :disabled="isLoading"
+        :href="securityConfigurationPath"
+        data-testid="api-fuzzing-configuration-cancel-button"
+        >{{ __('Cancel') }}</gl-button
+      >
+
+      <configuration-snippet-modal
+        :ref="$options.CONFIGURATION_SNIPPET_MODAL_ID"
+        :ci-yaml-edit-url="ciYamlEditPath"
+        :yaml="configurationYamlWithTips"
+        :redirect-param="$options.CODE_SNIPPET_SOURCE_API_FUZZING"
+        scan-type="DAST"
+      />
+    </gl-form>
   </section>
 </template>
