fc56890f2e6c8bb79ff094f9a433b91d3fdbebca
Paul Slaughter <pslaughter@gitlab.com>
2021-06-04 11:57:32 -0500

    Use echo in startup_css_changed output
    - The <<EOF format would collapse
      whitespace and look weird...

--- scripts/frontend/startup_css/startup_css_changed.sh
+++ scripts/frontend/startup_css/startup_css_changed.sh
@@ -14,27 +14,25 @@ git add ${startup_glob}
 
 if [ -n "$(git diff HEAD --name-only -- ${startup_glob})" ]; then
   diff=$(git diff HEAD -- ${startup_glob})
-  cat <<EOF
-
-Startup CSS changes detected!
-
-It looks like there have been recent changes which require
-regenerating the Startup CSS files.
-
-**What should I do now?**
-
-IMPORTANT: Please make sure to update your MR title with `[RUN AS-IF-FOSS]` and start a new MR pipeline
-
-To fix this job, consider one of the following options:
-
-  1. Regenerating locally with "yarn run generate:startup_css".
-  2. Copy and apply the following diff:
-
------ start diff -----
-$diff
-
------ end diff -------
-EOF
+  echo ""
+  echo "Startup CSS changes detected!"
+  echo ""
+  echo "It looks like there have been recent changes which require"
+  echo "regenerating the Startup CSS files."
+  echo ""
+  echo "**What should I do now?**"
+  echo ""
+  echo "IMPORTANT: Please make sure to update your MR title with ""[RUN AS-IF-FOSS]"" and start a new MR pipeline!"
+  echo ""
+  echo "To fix this job, consider one of the following options:"
+  echo ""
+  echo "  1. Regenerating locally with ""yarn run generate:startup_css""."
+  echo "  2. Copy and apply the following diff:"
+  echo ""
+  echo "----- start diff -----"
+  echo "$diff"
+  echo ""
+  echo "----- end diff -------"
 
   exit 1
 fi
