4c71dd9268414a1626fdf971460801d81d55f5a5
Albert Salim <asalim@gitlab.com>
2021-06-07 19:22:04 +0800

    Clean up unused code

--- scripts/review_apps/automated_cleanup.rb
+++ scripts/review_apps/automated_cleanup.rb
@@ -1,26 +1,11 @@
 # frozen_string_literal: true
 
 require 'gitlab'
-require_relative File.expand_path('../../tooling/lib/tooling/helm3_client.rb', __dir__)
-require_relative File.expand_path('../../tooling/lib/tooling/kubernetes_client.rb', __dir__)
 
 class AutomatedCleanup
   attr_reader :project_path, :gitlab_token
 
   DEPLOYMENTS_PER_PAGE = 100
-  IGNORED_HELM_ERRORS = [
-    'transport is closing',
-    'error upgrading connection',
-    'not found'
-  ].freeze
-  IGNORED_KUBERNETES_ERRORS = [
-    'NotFound'
-  ].freeze
-
-  def self.ee?
-    # Support former project name for `dev`
-    %w[gitlab gitlab-ee].include?(ENV['CI_PROJECT_NAME'])
-  end
 
   # $GITLAB_PROJECT_REVIEW_APP_CLEANUP_API_TOKEN => `Automated Review App Cleanup` project token
   def initialize(project_path: ENV['CI_PROJECT_PATH'], gitlab_token: ENV['GITLAB_PROJECT_REVIEW_APP_CLEANUP_API_TOKEN'])
@@ -40,18 +25,6 @@ class AutomatedCleanup
     end
   end
 
-  def review_apps_namespace
-    'review-apps'
-  end
-
-  def helm
-    @helm ||= Tooling::Helm3Client.new(namespace: review_apps_namespace)
-  end
-
-  def kubernetes
-    @kubernetes ||= Tooling::KubernetesClient.new(namespace: review_apps_namespace)
-  end
-
   def perform_gitlab_environment_cleanup!(days_for_stop:, days_for_delete:)
     puts "Checking for Review Apps not updated in the last #{days_for_stop} days..."
 
@@ -59,8 +32,6 @@ class AutomatedCleanup
     delete_threshold = threshold_time(days: days_for_delete)
     stop_threshold = threshold_time(days: days_for_stop)
     deployments_look_back_threshold = threshold_time(days: days_for_delete * 5)
-
-    releases_to_delete = []
 
     gitlab.deployments(project_path, per_page: DEPLOYMENTS_PER_PAGE, sort: 'desc').auto_paginate do |deployment|
       break if Time.parse(deployment.created_at) < deployments_look_back_threshold
@@ -75,24 +46,16 @@ class AutomatedCleanup
       deployed_at = Time.parse(last_deploy)
 
       if deployed_at < delete_threshold
-        deleted_environment = delete_environment(environment, deployment)
-        if deleted_environment
-          release = Tooling::Helm3Client::Release.new(environment.slug, 1, deployed_at.to_s, nil, nil, review_apps_namespace)
-          releases_to_delete << release
-        end
+        delete_environment(environment, deployment)
+      elsif deployed_at < stop_threshold
+        environment_state = fetch_environment(environment)&.state
+        stop_environment(environment, deployment) if environment_state && environment_state != 'stopped'
       else
-        if deployed_at >= stop_threshold
-          print_release_state(subject: 'Review App', release_name: environment.slug, release_date: last_deploy, action: 'leaving')
-        else
-          environment_state = fetch_environment(environment)&.state
-          stop_environment(environment, deployment) if environment_state && environment_state != 'stopped'
-        end
+        print_release_state(subject: 'Review App', release_name: environment.slug, release_date: last_deploy, action: 'leaving')
       end
 
       checked_environments << environment.slug
     end
-
-    delete_helm_releases(releases_to_delete)
   end
 
   private
@@ -120,26 +83,8 @@ class AutomatedCleanup
     puts "Review app '#{environment.name}' / '#{environment.slug}' (##{environment.id}) is forbidden: skipping it"
   end
 
-  def delete_helm_releases(releases)
-    return if releases.empty?
-
-    releases.each do |release|
-      print_release_state(subject: 'Release', release_name: release.name, release_status: release.status, release_date: release.last_update, action: 'cleaning')
-
-      Tooling::KubernetesClient.new(namespace: release.name).delete_namespace(wait: false)
-    rescue Tooling::KubernetesClient::CommandFailedError => ex
-      raise ex unless ignore_exception?(ex.message, IGNORED_KUBERNETES_ERRORS)
-
-      puts "Ignoring the following Kubernetes error:\n#{ex}\n"
-    end
-  end
-
   def threshold_time(days:)
     Time.now - days * 24 * 3600
-  end
-
-  def ignore_exception?(exception_message, exceptions_ignored)
-    exception_message.match?(/(#{exceptions_ignored})/)
   end
 
   def print_release_state(subject:, release_name:, release_date:, action:, release_status: nil)
