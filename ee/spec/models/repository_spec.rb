8a0c5e8b7c46ce7f3a2e2d671db47d9018bdd0e5
Patrick Steinhardt <psteinhardt@gitlab.com>
2021-06-07 15:30:19 +0200

    repository: Convert #fetch_upstream to use in-memory remotes
    Convert `Repository#fetch_upstream()` to use `fetch_remote()` with an
    in-memory remote. This allows us to skip the call to `add_remote()`: its
    backing RPC call is deprecated and will eventually be removed by Gitaly.
    
    The change reuses the `fetch_remote_params` feature flag which is also
    used by the GitHub importer and by `fetch_as_mirror()`. This is fine
    given that the feature flag hasn't yet been rolled out to production and
    is thus still disabled.

--- ee/spec/models/repository_spec.rb
+++ ee/spec/models/repository_spec.rb
@@ -49,6 +49,45 @@ RSpec.describe Repository do
     end
   end
 
+  describe '#fetch_upstream' do
+    let(:url) { "http://example.com" }
+
+    context 'when :fetch_remote_params is enabled' do
+      before do
+        stub_feature_flags(fetch_remote_params: true)
+      end
+
+      it 'fetches the URL without creating a remote' do
+        expect(repository).not_to receive(:add_remote)
+        expect(repository)
+          .to receive(:fetch_remote)
+          .with('upstream', url: url, mirror_refmap: ['+refs/heads/*:refs/remotes/upstream/*'], ssh_auth: nil, forced: true, check_tags_changed: true)
+          .and_return(nil)
+
+        repository.fetch_upstream(url, forced: true, check_tags_changed: true)
+      end
+    end
+
+    context 'when :fetch_remote_params is disabled' do
+      before do
+        stub_feature_flags(fetch_remote_params: false)
+      end
+
+      it 'creates a remote and fetches it' do
+        expect(repository)
+          .to receive(:add_remote)
+          .with('upstream', url)
+          .and_return(nil)
+        expect(repository)
+          .to receive(:fetch_remote)
+          .with('upstream', ssh_auth: nil, forced: true, check_tags_changed: true)
+          .and_return(nil)
+
+        repository.fetch_upstream(url, forced: true, check_tags_changed: true)
+      end
+    end
+  end
+
   describe '#with_config' do
     let(:rugged) { rugged_repo(repository) }
     let(:entries) do
