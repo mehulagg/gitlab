f9c6586d494f068a438c006497aa892f70528f6d
Eulyeon Ko <eko@gitlab.com>
2021-06-07 17:50:09 +0900

    Fetch from all parent groups in issue epic select
    The epic dropdown for issues should fetch epics from
    all ancestor groups, not just the immediate parent.
    
    Changelog: fixed
    MR: https://gitlab.com/gitlab-org/gitlab/-/merge_requests/63374
    EE: true

--- ee/spec/features/issues/epic_in_issue_sidebar_spec.rb
+++ ee/spec/features/issues/epic_in_issue_sidebar_spec.rb
@@ -12,38 +12,44 @@ RSpec.describe 'Epic in issue sidebar', :js do
   let_it_be(:issue) { create(:issue, project: project) }
   let_it_be(:epic_issue) { create(:epic_issue, epic: epic1, issue: issue) }
 
+  let_it_be(:subgroup) { create(:group, :public, parent: group) }
+  let_it_be(:subproject) { create(:project, :public, group: subgroup) }
+  let_it_be(:subepic) { create(:epic, group: subgroup, title: 'Xyz') }
+  let_it_be(:subissue) { create(:issue, project: subproject) }
+
+  let_it_be(:sidebar_epic_selector) { '[data-testid="sidebar-epic"]' }
+
   shared_examples 'epic in issue sidebar' do
+    before do
+      group.add_owner(user)
+      sign_in user
+    end
+
     context 'projects within a group' do
       before do
-        group.add_owner(user)
-        sign_in user
         visit project_issue_path(project, issue)
         wait_for_all_requests
       end
 
       it 'shows epic in issue sidebar' do
-        expect(page.find('[data-testid="sidebar-epic"]')).to have_content(epic1.title)
+        expect(page.find(sidebar_epic_selector)).to have_content(epic1.title)
       end
 
       it 'shows edit button in issue sidebar' do
-        expect(page.find('[data-testid="sidebar-epic"]')).to have_button('Edit')
+        expect(page.find(sidebar_epic_selector)).to have_button('Edit')
       end
 
       it 'shows epics select dropdown' do
-        page.within(find('[data-testid="sidebar-epic"]')) do
-          click_button 'Edit'
-
-          wait_for_all_requests
+        page.within(sidebar_epic_selector) do
+          click_edit
 
           expect(page).to have_selector('.gl-new-dropdown-contents .gl-new-dropdown-item', count: 4) # `No Epic` + 3 epics
         end
       end
 
       it 'supports searching for an epic' do
-        page.within(find('[data-testid="sidebar-epic"]')) do
-          click_button 'Edit'
-
-          wait_for_all_requests
+        page.within(sidebar_epic_selector) do
+          click_edit
 
           page.find('.gl-form-input').send_keys('Foo')
 
@@ -54,16 +60,29 @@ RSpec.describe 'Epic in issue sidebar', :js do
       end
 
       it 'select an epic from the dropdown' do
-        page.within(find('[data-testid="sidebar-epic"]')) do
-          click_button 'Edit'
-
-          wait_for_all_requests
+        page.within(sidebar_epic_selector) do
+          click_edit
 
           find('.gl-new-dropdown-item', text: epic2.title).click
 
           wait_for_all_requests
 
           expect(page.find('[data-testid="select-epic"]')).to have_content(epic2.title)
+        end
+      end
+    end
+
+    context 'project within a subgroup' do
+      before do
+        visit project_issue_path(subproject, issue)
+        wait_for_all_requests
+      end
+
+      it 'shows all epics belonging to the sub group and its parents' do
+        page.within(sidebar_epic_selector) do
+          click_edit
+
+          expect(page).to have_selector('.gl-new-dropdown-contents .gl-new-dropdown-item', count: 5) # `No Epic` + 3 root epics + 1 epic
         end
       end
     end
@@ -126,5 +145,11 @@ RSpec.describe 'Epic in issue sidebar', :js do
 
   def expect_no_epic
     expect(page).not_to have_selector('.block.epic')
+  end
+
+  def click_edit
+    click_button 'Edit'
+
+    wait_for_all_requests
   end
 end
