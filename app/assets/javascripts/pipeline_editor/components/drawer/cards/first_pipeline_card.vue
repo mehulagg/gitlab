73c71278ebc6594e47049b01baf0c2019e29e482
Sarah GP <sarah.groff.palermo@gmail.com>
2021-06-07 18:25:06 +0200

    Update card text

--- app/assets/javascripts/pipeline_editor/components/drawer/cards/first_pipeline_card.vue
+++ app/assets/javascripts/pipeline_editor/components/drawer/cards/first_pipeline_card.vue
@@ -7,16 +7,14 @@ export default {
   i18n: {
     title: s__('PipelineEditorTutorial|🚀 Run your first pipeline'),
     firstParagraph: s__(
-      'PipelineEditorTutorial|A typical GitLab pipeline consists of three stages: build, test and deploy. Each stage can have one or more jobs.',
+      'PipelineEditorTutorial|This template creates a simple test pipeline. To use it:',
     ),
-    secondParagraph: s__(
-      'PipelineEditorTutorial|In the example below, %{codeStart}build%{codeEnd} and %{codeStart}deploy%{codeEnd} each contain one job, and %{codeStart}test%{codeEnd} contains two jobs. Your scripts run in jobs like these.',
-    ),
-    thirdParagraph: s__(
-      'PipelineEditorTutorial|You can use %{linkStart}CI/CD examples and templates%{linkEnd} to get your first %{codeStart}.gitlab-ci.yml%{codeEnd} configuration file started. Your first pipeline runs when you commit the changes.',
-    ),
-    note: s__(
-      'PipelineEditorTutorial|If you’re using a self-managed GitLab instance, %{linkStart}make sure your instance has runners available.%{linkEnd}',
+    listItems: s__(
+      `PipelineEditorTutorial|
+        %{listItemStart}Commit the file to your repository. The pipeline then runs automatically.%{listItemEnd}
+        %{listItemStart}The pipeline status is at the top of the page.%{listItemEnd}
+        %{listItemStart}Select the pipeline ID to view the full details about your first pipeline run.%{listItemEnd}
+      `
     ),
   },
   components: {
@@ -33,35 +31,13 @@ export default {
     <template #default>
       <h4 class="gl-font-lg gl-mt-0">{{ $options.i18n.title }}</h4>
       <p class="gl-mb-3">{{ $options.i18n.firstParagraph }}</p>
-      <p class="gl-mb-3">
-        <gl-sprintf :message="$options.i18n.secondParagraph">
-          <template #code="{ content }">
-            <code>{{ content }}</code>
-          </template>
+      <ol class="gl-mb-3">
+        <gl-sprintf :message="$options.i18n.listItems">
+            <template #listItem="{ content }">
+              <li>{{ content }}</li>
+            </template>
         </gl-sprintf>
-      </p>
-      <pipeline-visual-reference />
-      <p class="gl-my-3">
-        <gl-sprintf :message="$options.i18n.thirdParagraph">
-          <template #link="{ content }">
-            <gl-link :href="ciExamplesHelpPagePath" target="_blank">
-              {{ content }}
-            </gl-link>
-          </template>
-          <template #code="{ content }">
-            <code>{{ content }}</code>
-          </template>
-        </gl-sprintf>
-      </p>
-      <p class="gl-mb-0">
-        <gl-sprintf :message="$options.i18n.note">
-          <template #link="{ content }">
-            <gl-link :href="runnerHelpPagePath" target="_blank">
-              {{ content }}
-            </gl-link>
-          </template>
-        </gl-sprintf>
-      </p>
+      </ol>
     </template>
   </gl-card>
 </template>
