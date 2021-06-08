77d82a83a6a28cba7b885f7d4c7ba0e4529c6cfb
Jose Vargas <jvargas@gitlab.com>
2021-06-08 15:31:49 -0500

    Add CIJobToken project allow list resolver

---
+++ app/graphql/resolvers/ci/ci_job_token_projects_resolver.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+module Resolvers
+  module Ci
+    class CiJobTokenProjectsResolver < BaseResolver
+      description 'Allowed projects that can use a project CI Job Token'
+
+      type ::Types::ProjectType.connection_type, null: true
+
+      def resolve
+        ci_job_token = ::Ci::JobToken::Scope.new(object)
+
+        ci_job_token.all_projects.map do | project |
+          {
+            id: project.id,
+            name: project.name
+          }
+        end
+      end
+    end
+  end
+end
