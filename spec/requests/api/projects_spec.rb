d8abf47e41f64c200d83a04cafc07a809efc8762
Maxime Orefice <morefice@gitlab.com>
2021-06-10 12:01:55 -0400

    Add keep_latest_artifact to REST API
    This commit adds the ability to toggle the keep_latest_artifact
    attribute for a given project with our REST API.
    
    Changelog: changed

--- spec/requests/api/projects_spec.rb
+++ spec/requests/api/projects_spec.rb
@@ -2038,6 +2038,7 @@ RSpec.describe API::Projects do
         expect(json_response['merge_method']).to eq(project.merge_method.to_s)
         expect(json_response['readme_url']).to eq(project.readme_url)
         expect(json_response).to have_key 'packages_enabled'
+        expect(json_response['keep_latest_artifact']).to be_present
       end
 
       it 'returns a group link with expiration date' do
@@ -3226,6 +3227,24 @@ RSpec.describe API::Projects do
 
       it 'enables the service_desk' do
         expect { subject }.to change { project.reload.service_desk_enabled }.to(true)
+      end
+    end
+
+    context 'when updating keep latest artifact' do
+      subject { put(api("/projects/#{project.id}", user), params: { keep_latest_artifact: true }) }
+
+      before do
+        project.update!(keep_latest_artifact: false)
+      end
+
+      it 'returns 200' do
+        subject
+
+        expect(response).to have_gitlab_http_status(:ok)
+      end
+
+      it 'enables keep_latest_artifact' do
+        expect { subject }.to change { project.reload.keep_latest_artifact }.to(true)
       end
     end
   end
