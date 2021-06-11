e07c772edf1deb34e174ebe195425b4a8c721ca1
Allison Browne <abrowne@gitlab.com>
2021-06-11 15:25:36 -0400

    Add authorization for job token scope
    Add authorization as well as specs

--- app/graphql/resolvers/ci/job_token_scope_resolver.rb
+++ app/graphql/resolvers/ci/job_token_scope_resolver.rb
@@ -3,14 +3,18 @@
 module Resolvers
   module Ci
     class JobTokenScopeResolver < BaseResolver
-      description 'Allowed projects that can use a project CI Job Token.'
+      include Gitlab::Graphql::Authorize::AuthorizeResource
 
+      authorize :admin_project
+      description 'Allowed projects that can use a project CI Job Token.'
       type ::Types::ProjectType.connection_type, null: true
 
       def resolve
-        return unless object.job_token_scope_enabled?
+        authorize!(object)
 
-        ci_job_token = ::Ci::JobToken::Scope.new(object).all_projects
+        return unless object.ci_job_token_scope_enabled?
+
+        ::Ci::JobToken::Scope.new(object).all_projects
       end
     end
   end
--- spec/graphql/resolvers/ci/job_token_scope_resolver_spec.rb
+++ spec/graphql/resolvers/ci/job_token_scope_resolver_spec.rb
@@ -5,38 +5,65 @@ require 'spec_helper'
 RSpec.describe Resolvers::Ci::JobTokenScopeResolver do
   include GraphqlHelpers
 
+  let_it_be(:current_user) { create(:user) }
   let_it_be(:project) { create(:project) }
 
   specify do
     expect(described_class).to have_nullable_graphql_type(::Types::ProjectType.connection_type)
   end
 
-  def resolve_ci_job_token_allow_list_projects(project, args)
-    resolve(described_class, obj: project, args: args)
-  end
+  subject(:resolve_scope) { resolve(described_class, ctx: { current_user: current_user }, obj: project) }
 
   describe '#resolve' do
-    it 'returns nil when scope is not enabled' do
-      allow(project).to receive(:job_token_scope_enabled?).and_return(false)
+    context 'with access to scope' do
+      before do
+        project.add_user(current_user, :maintainer)
+      end
 
-      result = resolve_ci_job_token_allow_list_projects(project, {})
+      it 'returns nil when scope is not enabled' do
+        allow(project).to receive(:ci_job_token_scope_enabled?).and_return(false)
 
-      expect(result).to eq(nil)
+        expect(resolve_scope).to eq(nil)
+      end
+
+      it 'returns the same project in the allow list of projects for the Ci Job Token' do
+        expect(resolve_scope).to contain_exactly(project)
+      end
+
+      context 'when linked projects are readable' do
+        context 'when another projects gets added to the allow list' do
+          let!(:link) { create(:ci_job_token_project_scope_link, source_project: project) }
+
+          before do
+            link.target_project.add_user(current_user, :developer)
+          end
+
+          it 'returns the same project and another project that is on the allow list' do
+            expect(resolve_scope).to contain_exactly(project, link.target_project)
+          end
+        end
+      end
+
+      context 'when linked projects are not readable' do
+        context 'when another projects gets added to the allow list' do
+          let!(:link) { create(:ci_job_token_project_scope_link, source_project: project) }
+
+          it 'returns the same project and another project that is on the allow list' do
+            expect(resolve_scope).to contain_exactly(project)
+          end
+        end
+      end
     end
 
-    it 'returns the same project in the allow list of projects for the Ci Job Token' do
-      result = resolve_ci_job_token_allow_list_projects(project, {})
+    context 'without access to scope' do
+      before do
+        project.add_user(current_user, :developer)
+      end
 
-      expect(result).to contain_exactly(project)
-    end
-
-    context 'when another projects gets added to the allow list' do
-      let!(:link) { create(:ci_job_token_project_scope_link, source_project: project) }
-
-      it 'returns the same project and another project that is on the allow list' do
-        result = resolve_ci_job_token_allow_list_projects(project, {})
-
-        expect(result).to contain_exactly(project, link.target_project)
+      it 'raises error' do
+        expect do
+          resolve_scope
+        end.to raise_error(Gitlab::Graphql::Errors::ResourceNotAvailable)
       end
     end
   end
