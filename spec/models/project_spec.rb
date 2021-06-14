ff66190b77f13ff3378f38ea42de415631d70c72
Alex Kalderimis <akalderimis@gitlab.com>
2021-06-14 19:42:12 +0100

    Rename emails_on_push service to integration
    This renames references to the project association from
    emails_on_push_service to emails_on_push_integration

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -49,7 +49,7 @@ RSpec.describe Project, factory_default: :keep do
     it { is_expected.to have_one(:datadog_service) }
     it { is_expected.to have_one(:discord_service) }
     it { is_expected.to have_one(:drone_ci_service) }
-    it { is_expected.to have_one(:emails_on_push_service) }
+    it { is_expected.to have_one(:emails_on_push_integration) }
     it { is_expected.to have_one(:pipelines_email_service) }
     it { is_expected.to have_one(:irker_service) }
     it { is_expected.to have_one(:pivotaltracker_service) }
