607743ef04f773c0f3ed90814bd587aef80e2754
a_luna <aluna@gitlab.com>
2021-06-15 12:40:21 -0500

    Remove unused var formatDate

--- ee/app/assets/javascripts/pages/admin/cloud_licenses/components/subscription_details_card.vue
+++ ee/app/assets/javascripts/pages/admin/cloud_licenses/components/subscription_details_card.vue
@@ -2,7 +2,7 @@
 import { GlCard } from '@gitlab/ui';
 import { identity } from 'lodash';
 import { getIdFromGraphQLId } from '~/graphql_shared/utils';
-import { formatDate, getTimeago } from '~/lib/utils/datetime_utility';
+import { getTimeago } from '~/lib/utils/datetime_utility';
 import { capitalizeFirstCharacter } from '~/lib/utils/text_utility';
 import SubscriptionDetailsTable from './subscription_details_table.vue';
 
