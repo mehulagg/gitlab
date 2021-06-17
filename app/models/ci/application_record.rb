10f4c9ce7f3a580ee463bc2de07dd3fc9d28220b
Thong Kuah <tkuah@gitlab.com>
2021-06-17 21:23:15 +1200

    Conditionally switch DB/connection for Ci::InstanceVariable
    This is still in development, and NOT READY FOR PRODUCTION USE.
    
    If a DB config with `ci` spec name is set up, use a separate database.

---
+++ app/models/ci/application_record.rb
@@ -0,0 +1,9 @@
+module Ci
+  class ApplicationRecord < ::ApplicationRecord
+    self.abstract_class = true
+
+    if Gitlab::Application.config.database_configuration[Rails.env]["ci"].present?
+      connects_to database: { writing: :ci, reading: :ci }
+    end
+  end
+end
