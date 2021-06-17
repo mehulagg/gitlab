11dd380272649091a3a0120d4edd2e11a1e158af
peterhegman <phegman@gitlab.com>
2021-06-17 15:16:16 -0700

    Add the ability to deep link into group/project member tabs
    Update query string when group/project member tabs are changed so tabs
    can be deep linked into.
    
    Changelog: added

--- app/assets/javascripts/members/components/filter_sort/members_filtered_search_bar.vue
+++ app/assets/javascripts/members/components/filter_sort/members_filtered_search_bar.vue
@@ -4,7 +4,11 @@ import { mapState } from 'vuex';
 import { getParameterByName, urlParamsToObject } from '~/lib/utils/common_utils';
 import { setUrlParams } from '~/lib/utils/url_utility';
 import { s__ } from '~/locale';
-import { SEARCH_TOKEN_TYPE, SORT_PARAM } from '~/members/constants';
+import {
+  SEARCH_TOKEN_TYPE,
+  SORT_QUERY_PARAM_NAME,
+  ACTIVE_TAB_QUERY_PARAM_NAME,
+} from '~/members/constants';
 import { OPERATOR_IS_ONLY } from '~/vue_shared/components/filtered_search_bar/constants';
 import FilteredSearchBar from '~/vue_shared/components/filtered_search_bar/filtered_search_bar_root.vue';
 
@@ -116,10 +120,15 @@ export default {
         return accumulator;
       }, {});
 
-      const sortParam = getParameterByName(SORT_PARAM);
+      const sortParamValue = getParameterByName(SORT_QUERY_PARAM_NAME);
+      const activeTabParamValue = getParameterByName(ACTIVE_TAB_QUERY_PARAM_NAME);
 
       window.location.href = setUrlParams(
-        { ...params, ...(sortParam && { sort: sortParam }) },
+        {
+          ...params,
+          ...(sortParamValue && { [SORT_QUERY_PARAM_NAME]: sortParamValue }),
+          ...(activeTabParamValue && { [ACTIVE_TAB_QUERY_PARAM_NAME]: activeTabParamValue }),
+        },
         window.location.href,
         true,
       );
