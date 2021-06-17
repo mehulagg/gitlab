c47b148acc5797eb71e89f1dc6e0943b0f3d3597
Simon Knox <simon@gitlab.com>
2021-06-17 20:58:22 +1000

    Add feature spec for updating cadence
    Doesn't test all fields but confirms basic functionality

--- ee/spec/features/groups/iterations/user_edits_iteration_cadence_spec.rb
+++ ee/spec/features/groups/iterations/user_edits_iteration_cadence_spec.rb
@@ -6,13 +6,38 @@ RSpec.describe 'User edits iteration cadence', :js do
   let_it_be(:group) { create(:group) }
   let_it_be(:user) { create(:group_member, :maintainer, user: create(:user), group: group ).user }
   let_it_be(:guest_user) { create(:group_member, :guest, user: create(:user), group: group ).user }
-  let_it_be(:cadence) { create(:iterations_cadence, group: group) }
+  let_it_be(:cadence) { create(:iterations_cadence, group: group, description: 'an example iteration cadence', duration_in_weeks: 3, iterations_in_advance: 2) }
 
   dropdown_selector = '[data-testid="actions-dropdown"]'
 
   context 'with license' do
     before do
       stub_licensed_features(iterations: true)
+    end
+
+    context 'as authorized user' do
+      before do
+        sign_in(user)
+      end
+
+      it 'prefills fields and allows updating values' do
+        visit edit_group_iteration_cadence_path(cadence.group, id: cadence.id)
+
+        wait_for_requests
+
+        aggregate_failures do
+          expect(title_input.value).to eq(cadence.title)
+          expect(description_input.value).to eq(cadence.description)
+          expect(start_date_input.value).to have_content(cadence.start_date)
+        end
+
+        updated_title = 'Updated cadence title'
+
+        fill_in('Title', with: updated_title)
+        click_button('Save cadence')
+
+        expect(page).to have_content(updated_title)
+      end
     end
 
     context 'as guest user' do
@@ -43,19 +68,15 @@ RSpec.describe 'User edits iteration cadence', :js do
     end
 
     def title_input
-      page.find('#iteration-title')
+      page.find('#cadence-title')
     end
 
     def description_input
-      page.find('#iteration-description')
+      page.find('#cadence-description')
     end
 
     def start_date_input
-      page.find('#iteration-start-date')
-    end
-
-    def due_date_input
-      page.find('#iteration-due-date')
+      page.find('#cadence-start-date')
     end
   end
 end
