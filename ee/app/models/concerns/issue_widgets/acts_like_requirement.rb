616df060fceb4fd374064f24d5839aa0d7198e63
charlie ablett <cablett@gitlab.com>
2021-06-17 22:16:04 +1200

    Sync destruction between requirements and issues

--- ee/app/models/concerns/issue_widgets/acts_like_requirement.rb
+++ ee/app/models/concerns/issue_widgets/acts_like_requirement.rb
@@ -6,7 +6,7 @@ module IssueWidgets
 
     included do
       has_many :test_reports, foreign_key: :issue_id, inverse_of: :requirement_issue, class_name: 'RequirementsManagement::TestReport'
-      has_one :requirement
+      has_one :requirement, dependent: :destroy
     end
   end
 end
