664b2667c036de31f05350fd50be534e13bf9b1c
David Fernandez <dfernandez@gitlab.com>
2021-06-17 14:23:04 +0200

    Improve gradle QA scenario
    by adding the pull action

--- qa/qa/specs/features/browser_ui/5_package/maven_gradle_repository_spec.rb
+++ qa/qa/specs/features/browser_ui/5_package/maven_gradle_repository_spec.rb
@@ -8,34 +8,38 @@ module QA
       let(:group_id) { 'com.gitlab.qa' }
       let(:artifact_id) { 'maven_gradle' }
       let(:package_name) { "#{group_id}/#{artifact_id}".tr('.', '/') }
-      let(:auth_token) do
-        unless Page::Main::Menu.perform(&:signed_in?)
-          Flow::Login.sign_in
-        end
+      let(:package_version) { '1.3.7' }
 
-        Resource::PersonalAccessToken.fabricate!.token
-      end
+      let(:personal_access_token) { Runtime::Env.personal_access_token }
 
-      let(:project) do
+      let(:package_project) do
         Resource::Project.fabricate_via_api! do |project|
           project.name = 'maven-with-gradle-project'
           project.initialize_with_readme = true
         end
       end
 
-      let(:package) do
-        Resource::Package.new.tap do |package|
-          package.name = package_name
-          package.project = project
+      let(:client_project) do
+        Resource::Project.fabricate_via_api! do |client_project|
+          client_project.name = 'gradle_client'
+          client_project.initialize_with_readme = true
+          client_project.group = package_project.group
         end
       end
 
-      let!(:runner) do
+      let(:package) do
+        Resource::Package.new.tap do |package|
+          package.name = package_name
+          package.project = package_project
+        end
+      end
+
+      let(:runner) do
         Resource::Runner.fabricate! do |runner|
           runner.name = "qa-runner-#{Time.now.to_i}"
-          runner.tags = ["runner-for-#{project.name}"]
+          runner.tags = ["runner-for-#{package_project.group.name}"]
           runner.executor = :docker
-          runner.project = project
+          runner.token = package_project.group.runners_token
         end
       end
 
@@ -44,64 +48,128 @@ module QA
         "#{uri.scheme}://#{uri.host}:#{uri.port}"
       end
 
+      let(:package_gitlab_ci_file) do
+        {
+          file_path: '.gitlab-ci.yml',
+          content:
+              <<~YAML
+                deploy:
+                  image: gradle:6.5-jdk11
+                  script:
+                  - 'gradle publish'
+                  only:
+                  - "#{package_project.default_branch}"
+                  tags:
+                  - "runner-for-#{package_project.group.name}"
+              YAML
+        }
+      end
+
+      let(:package_build_gradle_file) do
+        {
+          file_path: 'build.gradle',
+          content:
+              <<~EOF
+                plugins {
+                    id 'java'
+                    id 'maven-publish'
+                }
+
+                publishing {
+                    publications {
+                        library(MavenPublication) {
+                            groupId '#{group_id}'
+                            artifactId '#{artifact_id}'
+                            version '#{package_version}'
+                            from components.java
+                        }
+                    }
+                    repositories {
+                        maven {
+                            url "#{gitlab_address_with_port}/api/v4/projects/#{package_project.id}/packages/maven"
+                            credentials(HttpHeaderCredentials) {
+                                name = "Private-Token"
+                                value = "#{personal_access_token}"
+                            }
+                            authentication {
+                                header(HttpHeaderAuthentication)
+                            }
+                        }
+                    }
+                }
+              EOF
+        }
+      end
+
+      let(:client_gitlab_ci_file) {
+        {
+          file_path: '.gitlab-ci.yml',
+          content:
+              <<~YAML
+                build:
+                  image: gradle:6.5-jdk11
+                  script:
+                  - 'gradle build'
+                  only:
+                  - "#{client_project.default_branch}"
+                  tags:
+                  - "runner-for-#{client_project.group.name}"
+              YAML
+        }
+      }
+
+      let(:client_build_gradle_file) {
+        {
+          file_path: 'build.gradle',
+          content:
+              <<~EOF
+                plugins {
+                    id 'java'
+                }
+
+                repositories {
+                    maven {
+                        url "http://gdk.test:8000/api/v4/projects/#{package_project.id}/packages/maven"
+                        name "GitLab"
+                        credentials(HttpHeaderCredentials) {
+                            name = 'Private-Token'
+                            value = "#{personal_access_token}"
+                        }
+                        authentication {
+                            header(HttpHeaderAuthentication)
+                        }
+                    }
+                }
+
+                dependencies {
+                    implementation group: '#{group_id}', name: '#{artifact_id}', version: '#{package_version}'
+                }
+              EOF
+        }
+      }
+
+      before do
+        Flow::Login.sign_in
+        runner # force its creation
+      end
+
       after do
         runner.remove_via_api!
         package.remove_via_api!
+        package_project.remove_via_api!
+        client_project.remove_via_api!
       end
 
-      it 'publishes a maven package via gradle', testcase: 'https://gitlab.com/gitlab-org/quality/testcases/-/issues/1074' do
+      it 'pushes and pulls a maven package via gradle', testcase: 'https://gitlab.com/gitlab-org/quality/testcases/-/issues/1074' do
+        # pushing
         Resource::Repository::Commit.fabricate_via_api! do |commit|
-          commit.project = project
+          commit.project = package_project
           commit.commit_message = 'Add .gitlab-ci.yml'
-          commit.add_files([{
-                                file_path: '.gitlab-ci.yml',
-                                content:
-                                    <<~YAML
-                                      deploy:
-                                        image: gradle:6.5-jdk11
-                                        script:
-                                        - 'gradle publish'
-                                        only:
-                                        - "#{project.default_branch}"
-                                        tags:
-                                        - "runner-for-#{project.name}"
-                                    YAML
-                            },
-                            {
-                                file_path: 'build.gradle',
-                                content:
-                                    <<~EOF
-                                      plugins {
-                                          id 'java'
-                                          id 'maven-publish'
-                                      }
-
-                                      publishing {
-                                          publications {
-                                              library(MavenPublication) {
-                                                  groupId '#{group_id}'
-                                                  artifactId '#{artifact_id}'
-                                                  from components.java
-                                              }
-                                          }
-                                          repositories {
-                                              maven {
-                                                  url "#{gitlab_address_with_port}/api/v4/projects/#{project.id}/packages/maven"
-                                                  credentials(HttpHeaderCredentials) {
-                                                      name = "Private-Token"
-                                                      value = "#{auth_token}"
-                                                  }
-                                                  authentication {
-                                                      header(HttpHeaderAuthentication)
-                                                  }
-                                              }
-                                          }
-                                      }
-                                    EOF
-                            }])
+          commit.add_files([package_gitlab_ci_file, package_build_gradle_file])
         end
 
-        project.visit!
+        package_project.visit!
+
         Flow::Pipeline.visit_latest_pipeline
 
         Page::Project::Pipeline::Show.perform do |pipeline|
@@ -120,11 +188,27 @@ module QA
           index.click_package(package_name)
         end
 
-        Page::Project::Packages::Show.perform(&:click_delete)
+        Page::Project::Packages::Show.perform do |show|
+          expect(show).to have_package_info(package_name, package_version)
+        end
 
-        Page::Project::Packages::Index.perform do |index|
-          expect(index).to have_content("Package deleted successfully")
-          expect(index).not_to have_package(package_name)
+        # pulling
+        Resource::Repository::Commit.fabricate_via_api! do |commit|
+          commit.project = client_project
+          commit.commit_message = 'Add .gitlab-ci.yml'
+          commit.add_files([client_gitlab_ci_file, client_build_gradle_file])
+        end
+
+        client_project.visit!
+
+        Flow::Pipeline.visit_latest_pipeline
+
+        Page::Project::Pipeline::Show.perform do |pipeline|
+          pipeline.click_job('build')
+        end
+
+        Page::Project::Job::Show.perform do |job|
+          expect(job).to be_successful(timeout: 800)
         end
       end
     end
