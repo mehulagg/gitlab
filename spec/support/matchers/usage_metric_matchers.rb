08032857bd5cfee427d77abd76acf9e693a1b8dc
Mikolaj Wawrzyniak <mwawrzyniak@gitlab.com>
2021-06-21 18:13:42 +0200

    Scaffold CollectServicePingService class
    We need to filter service ping payload based on instance configuration
    to do so, we are going to use new service, that would perform the task.
    This commit adds new empty class, and its test suite, that describe
    business requirements

---
+++ spec/support/matchers/usage_metric_matchers.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+RSpec::Matchers.define :have_usage_metric do |key_path|
+  match do |payload|
+    payload = payload.deep_stringify_keys
+
+    key_path.split('.').each do |part|
+      break false unless payload&.has_key?(part)
+
+      payload = payload[part]
+    end
+  end
+
+  failure_message do
+    "Payload does not contain metric with  key path: '#{key_path}'"
+  end
+end
