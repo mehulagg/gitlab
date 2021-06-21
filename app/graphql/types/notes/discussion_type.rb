cce219a263d6814085baa3d4ec8a06ba27cc9c47
Felipe Artur <felipefac@gmail.com>
2021-06-21 16:44:34 -0300

    Allow to query discussion design on GraphQL
    Expose noteable on GraphQL discussion.
    
    Changelog: added

--- app/graphql/types/notes/discussion_type.rb
+++ app/graphql/types/notes/discussion_type.rb
@@ -19,12 +19,24 @@ module Types
             description: "Timestamp of the discussion's creation."
       field :notes, Types::Notes::NoteType.connection_type, null: false,
             description: 'All notes in the discussion.'
+      field :noteable, Types::NoteableType, null: true, # null is true for because NoteableType union does not implement all types yet.
+            description: 'Object which the discussion belongs to.'
 
       # DiscussionID.coerce_result is suitable here, but will always mark this
       # as being a 'Discussion'. Using `GlobalId.build` guarantees that we get
       # the correct class, and that it matches `id`.
       def reply_id
         ::Gitlab::GlobalId.build(object, id: object.reply_id)
+      end
+
+      def noteable
+        # Todo: maybe use resolver or allow batch loading
+        noteable = object.noteable
+
+        # This should never happen, if user can read discussion it can read noteable, but it feels safer to have it here
+        return unless Ability.allowed?(context[:current_user], :"read_#{noteable.to_ability_name}", noteable)
+
+        noteable
       end
     end
   end
