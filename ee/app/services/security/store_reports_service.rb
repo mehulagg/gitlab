bbe30807485c72e2f7a0c62fdb7d75e66f67e79a
Cameron Swords <cswords@gitlab.com>
2021-06-22 14:50:24 +1000

    Track secure scan event

--- ee/app/services/security/store_reports_service.rb
+++ ee/app/services/security/store_reports_service.rb
@@ -27,6 +27,8 @@ module Security
       pipeline.security_reports.reports.each do |report_type, report|
         result = StoreReportService.new(pipeline, report).execute
         errors << result[:message] if result[:status] == :error
+
+        track_scan_event(report_type, report)
       end
     end
 
@@ -40,6 +42,19 @@ module Security
 
     def full_errors
       errors.join(", ")
+    end
+
+    def track_scan_event(report_type, report)
+      ::Gitlab::Tracking.event(
+        'secure::scan',
+        'scan',
+        end_time: report&.scan&.end_time,
+        project: pipeline.project_id,
+        scan_type: report&.scan&.type || report_type,
+        start_time: report&.scan&.start_time,
+        status: report&.scan&.status,
+        triggered_by: pipeline.user_id,
+      )
     end
   end
 end
--- ee/spec/services/security/store_reports_service_spec.rb
+++ ee/spec/services/security/store_reports_service_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe Security::StoreReportsService do
   let_it_be(:user) { create(:user) }
   let_it_be(:group) { create(:group) }
   let_it_be(:project) { create(:project, :public, namespace: group) }
-  let_it_be(:pipeline) { create(:ci_pipeline, project: project) }
+  let_it_be(:pipeline) { create(:ci_pipeline, project: project, user: user) }
 
   describe '#execute' do
     subject(:execute_service_object) { described_class.new(pipeline).execute }
@@ -63,6 +63,68 @@ RSpec.describe Security::StoreReportsService do
           end
 
           is_expected.to eq(error)
+        end
+      end
+
+      context 'event tracking' do
+        let(:reports) { Gitlab::Ci::Reports::Security::Reports.new(pipeline) }
+        let(:dast_artifact) { create(:ee_ci_job_artifact, :dast) }
+        let(:success) { { status: :success } }
+        let(:scan) { nil }
+
+        let(:dast_report) do
+          report = reports.get_report('dast', dast_artifact)
+          report.scan = scan
+          report
+        end
+
+        before do
+          allow(pipeline).to receive(:security_reports).and_return(reports)
+
+          expect_next_instance_of(Security::StoreReportService, pipeline, dast_report) do |store_service|
+            expect(store_service).to receive(:execute).and_return(success)
+          end
+        end
+
+        context 'report has scan metadata' do
+          let(:scan) do
+            ::Gitlab::Ci::Reports::Security::Scan.new({ type: 'dast',
+                                                        status: 'success',
+                                                        start_time: '2021-06-11T07:26:17',
+                                                        end_time: '2021-06-11T07:27:50' }.stringify_keys)
+          end
+
+          it 'tracks the scan event', :snowplow do
+            execute_service_object
+
+            expect_snowplow_event(
+              category: 'secure::scan',
+              action: 'scan',
+              end_time: '2021-06-11T07:27:50',
+              project: project.id,
+              scan_type: 'dast',
+              start_time: '2021-06-11T07:26:17',
+              status: 'success',
+              triggered_by: user.id
+            )
+          end
+        end
+
+        context 'report has no scan metadata' do
+          it 'tracks the scan event', :snowplow do
+            execute_service_object
+
+            expect_snowplow_event(
+              category: 'secure::scan',
+              action: 'scan',
+              end_time: nil,
+              project: project.id,
+              scan_type: 'dast',
+              start_time: nil,
+              status: nil,
+              triggered_by: user.id
+            )
+          end
         end
       end
     end
