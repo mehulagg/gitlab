7aac486286a1438793dca79f51835f9a50873c99
George Koltsov <gkoltsov@gitlab.com>
2021-06-22 15:37:44 +0000

    Update capybara.rb

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -211,7 +211,7 @@ RSpec.configure do |config|
       execute_script("localStorage.clear();")
     end
 
-    puts page.driver.browser.manage.logs.get(:driver)
+    
 
     # capybara/rspec already calls Capybara.reset_sessions! in an `after` hook,
     # but `block_and_wait_for_requests_complete` is called before it so by
@@ -219,6 +219,9 @@ RSpec.configure do |config|
     # See https://github.com/teamcapybara/capybara/blob/ffb41cfad620de1961bb49b1562a9fa9b28c0903/lib/capybara/rspec.rb#L20-L25
     # We don't reset the session when the example failed, because we need capybara-screenshot to have access to it.
     Capybara.reset_sessions! unless example.exception
+
+    puts page.driver.browser.manage.logs.get(:driver)
+    
     block_and_wait_for_requests_complete
   end
 end
