01632f6433171749197dd505d5f13a078472e9ce
Chad Woolley <cwoolley@gitlab.com>
2021-06-21 18:33:05 -0700

    Migrate Axios Captcha Interceptor to use headers
    - See https://gitlab.com/gitlab-org/gitlab/-/merge_requests/64463

--- app/assets/javascripts/captcha/captcha_modal_axios_interceptor.js
+++ app/assets/javascripts/captcha/captcha_modal_axios_interceptor.js
@@ -13,22 +13,28 @@ export function registerCaptchaModalInterceptor(axios) {
         const { data } = err.response;
         const captchaSiteKey = data.captcha_site_key;
         const spamLogId = data.spam_log_id;
-        // eslint-disable-next-line promise/no-promise-in-callback
-        return import('~/captcha/wait_for_captcha_to_be_solved')
-          .then(({ waitForCaptchaToBeSolved }) => waitForCaptchaToBeSolved(captchaSiteKey))
-          .then((captchaResponse) => {
-            const errConfig = err.config;
-            const originalData = JSON.parse(errConfig.data);
-            return axios({
-              method: errConfig.method,
-              url: errConfig.url,
-              data: {
-                ...originalData,
-                captcha_response: captchaResponse,
-                spam_log_id: spamLogId,
-              },
-            });
-          });
+        return (
+          // eslint-disable-next-line promise/no-promise-in-callback
+          import('~/captcha/wait_for_captcha_to_be_solved')
+            // show the CAPTCHA modal and wait for it to be solved or closed
+            .then(({ waitForCaptchaToBeSolved }) => waitForCaptchaToBeSolved(captchaSiteKey))
+            // resubmit the original request with the captcha_response and spam_log_id in the headers
+            .then((captchaResponse) => {
+              const errConfig = err.config;
+              const originalData = JSON.parse(errConfig.data);
+              return axios({
+                method: errConfig.method,
+                url: errConfig.url,
+                headers: {
+                  'X-GitLab-Captcha-Response': captchaResponse,
+                  'X-GitLab-Spam-Log-Id': spamLogId,
+                },
+                data: {
+                  ...originalData,
+                },
+              });
+            })
+        );
       }
 
       return Promise.reject(err);
