a0b1e8ce1ec1f55f539ae2d4e0458d91e5fe6b5a
Serena Fang <sfang@gitlab.com>
2021-06-23 14:04:03 -0700

    Fix failing spec

--- spec/models/users/banned_user_spec.rb
+++ spec/models/users/banned_user_spec.rb
@@ -6,12 +6,11 @@ RSpec.describe Users::BannedUser do
   it { is_expected.to belong_to(:user) }
 
   it do
+    subject.user = create(:user)
     is_expected.to validate_uniqueness_of(:user_id).with_message("banned user already exists")
   end
 
-  context 'creates a new banned user with is_banned state' do
-    let(:user) { create(:user) }
-
+  context 'creates a new banned user' do
     it 'creates a new banned user object with is_banned ban state' do
       expect { described_class.new(user: user, ban_state: 3).save! }.to change { described_class.count }.by(1)
       expect(described_class.last.ban_state).to eq("is_banned")
