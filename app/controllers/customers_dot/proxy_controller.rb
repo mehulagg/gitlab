cf26138cd91768809092e8a241c5292445d30338
Ryan Cobb <rcobb@gitlab.com>
2021-06-22 17:30:44 -0700

    Attach logged in user id customers proxy

--- app/controllers/customers_dot/proxy_controller.rb
+++ app/controllers/customers_dot/proxy_controller.rb
@@ -7,15 +7,44 @@ module CustomersDot
 
     feature_category :purchase
 
-    BASE_URL = Gitlab::SubscriptionPortal::SUBSCRIPTIONS_URL
+    CUSTOMERS_GRAPHQL_URL = "#{Gitlab::SubscriptionPortal::SUBSCRIPTIONS_URL}/graphql"
 
     def graphql
-      response = Gitlab::HTTP.post("#{BASE_URL}/graphql",
-        body: request.raw_post,
-        headers: { 'Content-Type' => 'application/json' }
+      response = Gitlab::HTTP.post(CUSTOMERS_GRAPHQL_URL,
+        body: forward_body.to_json,
+        headers: forward_headers
       )
 
       render json: response.body, status: response.code
+    end
+
+    private
+
+    def forward_body
+      return params_for_proxy unless current_user
+
+      params_for_proxy.merge(gitlab_user_id: current_user.id)
+    end
+
+    def forward_headers
+      return base_headers unless current_user
+
+      base_headers.merge(admin_headers)
+    end
+
+    def params_for_proxy
+      params.require(:proxy).permit(:query, :variables)
+    end
+
+    def base_headers
+      { 'Content-Type' => 'application/json' }
+    end
+
+    def admin_headers
+      {
+        'X-Admin-Email' => EE::SUBSCRIPTION_PORTAL_ADMIN_EMAIL,
+        'X-Admin-Token' => EE::SUBSCRIPTION_PORTAL_ADMIN_TOKEN
+      }
     end
   end
 end
--- spec/requests/customers_dot/proxy_controller_spec.rb
+++ spec/requests/customers_dot/proxy_controller_spec.rb
@@ -4,34 +4,100 @@ require 'spec_helper'
 
 RSpec.describe CustomersDot::ProxyController, type: :request do
   describe 'POST graphql' do
-    let_it_be(:customers_dot) { "#{Gitlab::SubscriptionPortal::SUBSCRIPTIONS_URL}/graphql" }
+    let_it_be(:customers_dot) { described_class::CUSTOMERS_GRAPHQL_URL }
+    let_it_be(:base_headers)  { { 'Content-Type' => 'application/json' } }
 
-    it 'forwards request body to customers dot' do
-      request_params = '{ "foo" => "bar" }'
+    let_it_be(:request_params) do
+      {
+        "query" => "foo",
+        "variables" => "bar"
+      }
+    end
 
+    before do
       stub_request(:post, customers_dot)
-
-      post customers_dot_proxy_graphql_path, params: request_params
-
-      expect(WebMock).to have_requested(:post, customers_dot).with(body: request_params)
     end
 
-    it 'responds with customers dot status' do
-      stub_request(:post, customers_dot).to_return(status: 500)
+    shared_examples 'customersdot proxy' do
+      it 'responds with customers dot status' do
+        stub_request(:post, customers_dot).to_return(status: 500)
 
-      post customers_dot_proxy_graphql_path
+        post_proxy(request_params)
 
-      expect(response).to have_gitlab_http_status(:internal_server_error)
+        expect(response).to have_gitlab_http_status(:internal_server_error)
+      end
+
+      it 'responds with customers dot response body' do
+        customers_dot_response = 'foo'
+
+        stub_request(:post, customers_dot).to_return(body: customers_dot_response)
+
+        post_proxy(request_params)
+
+        expect(response.body).to eq(customers_dot_response)
+      end
     end
 
-    it 'responds with customers dot response body' do
-      customers_dot_response = 'foo'
+    context 'with logged in gitlab user' do
+      let_it_be(:user) { create(:user) }
+      let_it_be(:expected_customersdot_params) do
+        request_params.merge( { gitlab_user_id: user.id } )
+      end
 
-      stub_request(:post, customers_dot).to_return(body: customers_dot_response)
+      before do
+        login_as(user)
+      end
 
-      post customers_dot_proxy_graphql_path
+      it 'forwards request with user id' do
+        post_proxy(request_params)
 
-      expect(response.body).to eq(customers_dot_response)
+        expect(WebMock).to have_requested(:post, customers_dot).with(body: expected_customersdot_params)
+      end
+
+      it 'adds admin headers' do
+        expected_headers = base_headers.merge(
+          'X-Admin-Email' => EE::SUBSCRIPTION_PORTAL_ADMIN_EMAIL,
+          'X-Admin-Token' => EE::SUBSCRIPTION_PORTAL_ADMIN_TOKEN
+        )
+
+        post_proxy(request_params)
+
+        expect(WebMock).to have_requested(:post, customers_dot).with(headers: expected_headers)
+      end
+
+      it 'ignores passed in gitlab_user_id' do
+        post_proxy(request_params.merge(gitlab_user_id: 'foo'))
+
+        expect(WebMock).to have_requested(:post, customers_dot).with(body: expected_customersdot_params)
+      end
+
+      it_behaves_like 'customersdot proxy'
     end
+
+    context 'with no gitlab user logged in' do
+      it 'forwards request to customers dot' do
+        post_proxy(request_params)
+
+        expect(WebMock).to have_requested(:post, customers_dot).with(body: request_params)
+      end
+
+      it 'does not attach admin headers' do
+        post_proxy(request_params)
+
+        expect(WebMock).to have_requested(:post, customers_dot).with(headers: base_headers)
+      end
+
+      it 'ignores passed in gitlab_user_id' do
+        post_proxy(request_params.merge(gitlab_user_id: 'foo'))
+
+        expect(WebMock).to have_requested(:post, customers_dot).with(body: request_params)
+      end
+
+      it_behaves_like 'customersdot proxy'
+    end
+  end
+
+  def post_proxy(params)
+    post customers_dot_proxy_graphql_path, params: params, as: :json
   end
 end
