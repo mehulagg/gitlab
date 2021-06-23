5fb628e852910be9dfd442ff565d266d056b1682
Dmitry Gruzd <dgruzd@gitlab.com>
2021-06-23 10:57:17 +0300

    Add upvotes field to issues index
    EE: true
    Changelog: changed

--- ee/lib/elastic/latest/issue_instance_proxy.rb
+++ ee/lib/elastic/latest/issue_instance_proxy.rb
@@ -8,7 +8,7 @@ module Elastic
 
         # We don't use as_json(only: ...) because it calls all virtual and serialized attributes
         # https://gitlab.com/gitlab-org/gitlab/issues/349
-        [:id, :iid, :title, :description, :created_at, :updated_at, :state, :project_id, :author_id, :confidential].each do |attr|
+        [:id, :iid, :title, :description, :created_at, :updated_at, :state, :project_id, :author_id, :confidential, :upvotes].each do |attr|
           data[attr.to_s] = safely_read_attribute_for_elasticsearch(attr)
         end
 
