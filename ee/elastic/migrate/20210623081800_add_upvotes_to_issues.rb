4df5148bde709d6e2cb0b6841db76d731ec963f6
Dmitry Gruzd <dgruzd@gitlab.com>
2021-06-24 10:40:21 +0300

    fixup! Add upvotes field to issues index

--- ee/elastic/migrate/20210623081800_add_upvotes_to_issues.rb
+++ ee/elastic/migrate/20210623081800_add_upvotes_to_issues.rb
@@ -1,10 +1,12 @@
 # frozen_string_literal: true
 
 class AddUpvotesToIssues < Elastic::Migration
-  DOCUMENT_TYPE = Issue
-
   batched!
   throttle_delay 3.minutes
+
+  DOCUMENT_TYPE = Issue
+  QUERY_BATCH_SIZE = 5000
+  UPDATE_BATCH_SIZE = 100
 
   def migrate
     client.indices.put_mapping index: index_name, body: {
@@ -18,7 +20,35 @@ class AddUpvotesToIssues < Elastic::Migration
       return
     end
 
-    true
+    log "Adding upvotes field to issues documents for batch of #{QUERY_BATCH_SIZE} documents"
+
+    query = {
+      size: QUERY_BATCH_SIZE,
+      query: {
+        bool: {
+          filter: issues_missing_upvotes_field
+        }
+      }
+    }
+
+    results = client.search(index: index_name, body: query)
+    hits = results.dig('hits', 'hits') || []
+
+    document_references = hits.map do |hit|
+      id = hit.dig('_source', 'id')
+      es_id = hit.dig('_id')
+      es_parent = "project_#{hit.dig('_source', 'project_id')}"
+
+      # ensure that any issues missing from the database will be removed from Elasticsearch
+      # as the data is back-filled
+      Gitlab::Elastic::DocumentReference.new(Issue, id, es_id, es_parent)
+    end
+
+    document_references.each_slice(UPDATE_BATCH_SIZE) do |refs|
+      Elastic::ProcessInitialBookkeepingService.track!(*refs)
+    end
+
+    log "Adding upvotes field to issues documents is completed for batch of #{document_references.size} documents"
   end
 
   def completed?
@@ -26,15 +56,7 @@ class AddUpvotesToIssues < Elastic::Migration
       size: 0,
       aggs: {
         issues: {
-          filter: {
-            bool: {
-              must_not: {
-                exists: {
-                  field: 'upvotes'
-                }
-              }
-            }
-          }
+          filter: issues_missing_upvotes_field
         }
       }
     }
@@ -46,7 +68,19 @@ class AddUpvotesToIssues < Elastic::Migration
 
   private
 
+  def issues_missing_upvotes_field
+    {
+      bool: {
+        must_not: {
+          exists: {
+            field: 'upvotes'
+          }
+        }
+      }
+    }
+  end
+
   def index_name
-    helper.standalone_indices_proxies(target_classes: [DOCUMENT_TYPE]).map(&:index_name).first
+    DOCUMENT_TYPE.__elasticsearch__.index_name
   end
 end
