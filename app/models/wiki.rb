c4111649916fc379990328847bfd74568426dfe9
Francisco Javier López <fjlopez@gitlab.com>
2021-06-28 10:35:32 +0200

    More changes

--- app/models/wiki.rb
+++ app/models/wiki.rb
@@ -174,6 +174,7 @@ class Wiki
     commit = commit_details(:created, message, title)
 
     wiki.write_page(title, format.to_sym, content, commit)
+    repository.expire_status_cache
     after_wiki_activity
 
     true
@@ -322,9 +323,9 @@ class Wiki
   end
 
   def change_head_to_default_branch
-    repository.before_change_head
+    # repository.before_change_head
     repository.raw_repository.write_ref('HEAD', "refs/heads/#{default_branch}")
-    repository.after_change_head
+    # repository.after_change_head
   end
 end
 
