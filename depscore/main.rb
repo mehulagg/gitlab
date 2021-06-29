22cb69ffb5fc617f262eae3c17293ba1e2d468f0
Nikhil George <ngeorge@gitlab.com>
2021-06-29 18:51:28 +0530

    summary report

--- depscore/main.rb
+++ depscore/main.rb
@@ -6,6 +6,7 @@ require_relative "rubytoolbox"
 require "csv"
 require "date"
 require "optparse"
+require 'json'
 
 gem_hash = {}
 
@@ -20,7 +21,7 @@ end
 
 def dreport_csvprep()
   headers = ["Name", "Type", "tot_downloads", "reverse_dep_count", "latest_vesion_age(in Months)",
-             "latest_release_on", "rel_freq_last_4quater" "score"]
+             "latest_release_on", "rel_freq_last_4quater", "score"]
   write_csv(headers)
 
   all_gems = LibData.all_instances
@@ -30,6 +31,72 @@ def dreport_csvprep()
                gem.signals[:score]])
   end
 end
+
+
+def dreport_summary()
+  summary = ""
+  all_gems = LibData.all_instances
+  all_gems.each do |gem|
+    if gem.signals[:score].to_i == 0
+      summary = summary + gem.name + ","
+    end
+  end
+
+  foo = {
+    "version": "14.0.0",
+    "vulnerabilities": [
+      {
+        "id": "038e8c173096a16af7zzzzzzzzzc9a4fa164e9da701046ce95",
+        "category": "dependency_scanning",
+        "name": "Unreliable libraries",
+        "message": "Libraries that are not frequently mantained detected",
+        "description": "Based on the score calculated from the librariy metadata the below libraries seems to have less frequent maintance",
+        "cve": "yarn.lock:execa:gemnasium:05cfa2e8-2d0c-42c1-8894-638e2f12ff3d",
+        "severity": "Critical",
+        "solution": "Upgrade to version 2.0.0 or above.",
+        "scanner": {
+          "id": "depscore",
+          "name": "depscore",
+        },
+        "location": {
+          "file": "Gem.lock",
+          "dependency": {
+            "iid": 333,
+            "package": {
+              "name": summary,
+            },
+            "version": "1.0.0",
+          },
+        },
+        "identifiers": [],
+        "links": [],
+      },
+    ],
+    "remediations": [],
+    "dependency_files": [],
+    "scan": {
+      "scanner": {
+        "id": "depscore",
+        "name": "Depscore",
+        "url": "https://gitlab.com/gitlab-org/tbd/analyzers/tbd",
+        "vendor": {
+          "name": "GitLab",
+        },
+        "version": "2.29.7",
+      },
+      "type": "dependency_scanning",
+      "start_time": "2021-06-16T06:58:17",
+      "end_time": "2021-06-16T06:58:18",
+      "status": "success",
+    },
+  }
+
+  summary_json = JSON(foo)
+  File.open("gl-depscore.json", "w") do |f|
+    f.write(summary_json)
+  end
+end
+
 
 def dreport_readgems(dependencies)
   puts "[*] Reading all gems"
@@ -70,6 +137,7 @@ def dreport_read(dreport_path)
       # utils = Utils.new
       # utils.debug_libdata
       dreport_csvprep()
+      dreport_summary()
     end
   end
 end
