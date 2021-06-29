6b6759351bc38cfb0fb3b50f498e736699ba3e10
Simon Tomlinson <stomlinson@gitlab.com>
2021-06-29 10:32:41 -0500

    Partition detach worker

---
+++ app/workers/partition_detach_worker.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+class PartitionDetachWorker
+  include ApplicationWorker
+
+  sidekiq_options retry: 3
+  include CronjobQueue # rubocop:disable Scalability/CronWorkerContext
+
+  feature_category :database
+  idempotent!
+
+  def perform
+    Gitlab::Database::Partitioning::PartitionManager.new.detach_partitions
+  ensure
+    Gitlab::Database::Partitioning::PartitionMonitoring.new.report_metrics
+  end
+end
