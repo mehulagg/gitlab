77b371fe23d38931e109817884ec46400aeaa778
Sashi Kumar Kumaresan <skumar@gitlab.com>
2021-07-01 14:20:32 +0000

    Revert "Merge branch 'sk/329422-protected-branch-check' into 'master'"
    This reverts merge request !65133

--- ee/app/services/security/security_orchestration_policies/project_create_service.rb
+++ ee/app/services/security/security_orchestration_policies/project_create_service.rb
@@ -13,7 +13,7 @@ module Security
         project.create_security_orchestration_policy_configuration! do |p|
           p.security_policy_management_project_id = policy_project.id
         end
-        create_or_update_protected_branch(policy_project)
+        create_protected_branch(policy_project)
 
         members = add_members(policy_project)
         errors = members.flat_map { |member| member.errors.full_messages }
@@ -25,20 +25,12 @@ module Security
 
       private
 
-      def create_or_update_protected_branch(policy_project)
-        protected_branch = policy_project.protected_branches.find_by_name(policy_project.default_branch_or_main)
+      def create_protected_branch(policy_project)
         params = {
           name: policy_project.default_branch_or_main,
           push_access_levels_attributes: [{ access_level: Gitlab::Access::NO_ACCESS }],
           merge_access_levels_attributes: [{ access_level: Gitlab::Access::DEVELOPER }]
         }
-
-        if protected_branch.present?
-          ProtectedBranch::UpdateService
-            .new(policy_project, current_user, params)
-            .execute(protected_branch)
-          return
-        end
 
         ProtectedBranches::CreateService
           .new(policy_project, current_user, params)
