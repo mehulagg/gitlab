dedeaff9eb32223589be749ec2c28f30135b378f
Rémy Coutable <remy@rymai.me>
2021-07-01 16:00:11 +0200

    Debug wrong modified files in Danger
    Signed-off-by: Rémy Coutable <remy@rymai.me>

--- tooling/danger/feature_flag.rb
+++ tooling/danger/feature_flag.rb
@@ -11,6 +11,14 @@ module Tooling
       #   - :deleted
       def feature_flag_files(change_type:)
         files = git.public_send("#{change_type}_files") # rubocop:disable GitlabSecurity/PublicSend
+        puts "#{change_type} files: #{files.to_a}"
+
+        p project_helper.changes.added
+        p project_helper.changes.modified
+        p project_helper.changes.deleted
+        p project_helper.changes.renamed_before
+        p project_helper.changes.renamed_after
+
         files.select { |path| path =~ %r{\A(ee/)?config/feature_flags/} }.map { |path| Found.new(path) }
       end
 
