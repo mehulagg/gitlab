2997e9e3babbfae5e2b2ce8967cc2a765096239a
anna_vovchenko <avovchenko@gitlab.com>
2021-07-01 18:25:46 +0300

    Add alert for GitLab Agent empty state
    Add notification message if no agent configuration files are available in the repository.
    
    Changelog: added
    EE: true

--- ee/spec/frontend/clusters_list/components/agent_empty_state_spec.js
+++ ee/spec/frontend/clusters_list/components/agent_empty_state_spec.js
@@ -1,16 +1,24 @@
-import { GlEmptyState, GlSprintf } from '@gitlab/ui';
-import { shallowMount } from '@vue/test-utils';
+import { GlAlert, GlEmptyState, GlSprintf } from '@gitlab/ui';
 import AgentEmptyState from 'ee/clusters_list/components/agent_empty_state.vue';
+import { shallowMountExtended } from 'helpers/vue_test_utils_helper';
 
 describe('AgentEmptyStateComponent', () => {
   let wrapper;
 
   const propsData = {
     image: '/image/path',
+    projectPath: 'path/to/project',
+    hasConfigurations: false,
   };
 
+  const findConfigurationsAlert = () => wrapper.find(GlAlert);
+  const findIntegrationButton = () => wrapper.findByTestId('integration-primary-button');
+
   beforeEach(() => {
-    wrapper = shallowMount(AgentEmptyState, { propsData, stubs: { GlEmptyState, GlSprintf } });
+    wrapper = shallowMountExtended(AgentEmptyState, {
+      propsData,
+      stubs: { GlEmptyState, GlSprintf },
+    });
   });
 
   afterEach(() => {
@@ -20,8 +28,24 @@ describe('AgentEmptyStateComponent', () => {
     }
   });
 
-  it('should render content', () => {
-    expect(wrapper.find(GlEmptyState).exists()).toBe(true);
-    expect(wrapper.text()).toContain('Integrate with the GitLab Agent');
+  describe('when there are no agent configurations in repository', () => {
+    it('should render notification message box', () => {
+      expect(findConfigurationsAlert().exists()).toBe(true);
+    });
+
+    it('should disable integration button', () => {
+      expect(findIntegrationButton().attributes('disabled')).toBe('true');
+    });
+  });
+
+  describe('when there is a list of agent configurations', () => {
+    it('should render content without notification message box', () => {
+      wrapper.setProps({ hasConfigurations: true });
+      wrapper.vm.$nextTick(() => {
+        expect(wrapper.find(GlEmptyState).exists()).toBe(true);
+        expect(findConfigurationsAlert().exists()).toBe(false);
+        expect(findIntegrationButton().attributes('disabled')).toBeUndefined();
+      });
+    });
   });
 });
