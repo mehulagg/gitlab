10b9f8e806e218b7e63de0b14de7a8954c0ce5a8
Yorick Peterse <yorick@yorickpeterse.com>
2021-07-01 16:28:47 +0200

    Refactor Gitlab::Database to support multiple DBs
    This refactors the module Gitlab::Database so it supports multiple
    databases. Logic tied to a connection is now scoped to instances of
    Gitlab::Dababase::Database. For every database, an instance of this
    class is created and stored in Gitlab::Database::DATABASES.
    
    See https://gitlab.com/gitlab-org/gitlab/-/issues/331776 for more
    information.

---
+++ ee/lib/ee/gitlab/database/database.rb
@@ -0,0 +1,34 @@
+# frozen_string_literal: true
+
+module EE
+  module Gitlab
+    module Database
+      module Database
+        extend ActiveSupport::Concern
+        extend ::Gitlab::Utils::Override
+
+        override :read_only?
+        def read_only?
+          ::Gitlab::Geo.secondary? || ::Gitlab.maintenance_mode?
+        end
+
+        def healthy?
+          # TODO: make aware of multiple databases
+          !Postgresql::ReplicationSlot.lag_too_great?
+        end
+
+        def geo_uncached_queries(&block)
+          raise 'No block given' unless block_given?
+
+          scope.uncached do
+            if ::Gitlab::Geo.secondary?
+              Geo::TrackingBase.uncached(&block)
+            else
+              yield
+            end
+          end
+        end
+      end
+    end
+  end
+end
