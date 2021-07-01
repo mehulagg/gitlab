2c971fe9e8104b8946f97a7bddbee05d99b7eed9
Imre Farkas <ifarkas@gitlab.com>
2021-07-01 15:56:03 +0200

    Avoid filtering on subclass in Namespace recursive queries

--- app/models/namespaces/traversal/recursive.rb
+++ app/models/namespaces/traversal/recursive.rb
@@ -20,16 +20,16 @@ module Namespaces
 
       # Returns all ancestors, self, and descendants of the current namespace.
       def self_and_hierarchy
-        object_hierarchy(self.class.where(id: id))
+        object_hierarchy(Namespace.where(id: id))
           .all_objects
       end
       alias_method :recursive_self_and_hierarchy, :self_and_hierarchy
 
       # Returns all the ancestors of the current namespaces.
       def ancestors(hierarchy_order: nil)
-        return self.class.none unless parent_id
+        return Namespace.none unless parent_id
 
-        object_hierarchy(self.class.where(id: parent_id))
+        object_hierarchy(Namespace.where(id: parent_id))
           .base_and_ancestors(hierarchy_order: hierarchy_order)
       end
       alias_method :recursive_ancestors, :ancestors
@@ -42,14 +42,14 @@ module Namespaces
       # returns all ancestors upto but excluding the given namespace
       # when no namespace is given, all ancestors upto the top are returned
       def ancestors_upto(top = nil, hierarchy_order: nil)
-        object_hierarchy(self.class.where(id: id))
+        object_hierarchy(Namespace.where(id: id))
           .ancestors(upto: top, hierarchy_order: hierarchy_order)
       end
 
       def self_and_ancestors(hierarchy_order: nil)
-        return self.class.where(id: id) unless parent_id
+        return Namespace.where(id: id) unless parent_id
 
-        object_hierarchy(self.class.where(id: id))
+        object_hierarchy(Namespace.where(id: id))
           .base_and_ancestors(hierarchy_order: hierarchy_order)
       end
       alias_method :recursive_self_and_ancestors, :self_and_ancestors
@@ -61,13 +61,13 @@ module Namespaces
 
       # Returns all the descendants of the current namespace.
       def descendants
-        object_hierarchy(self.class.where(parent_id: id))
+        object_hierarchy(Namespace.where(parent_id: id))
           .base_and_descendants
       end
       alias_method :recursive_descendants, :descendants
 
       def self_and_descendants
-        object_hierarchy(self.class.where(id: id))
+        object_hierarchy(Namespace.where(id: id))
           .base_and_descendants
       end
       alias_method :recursive_self_and_descendants, :self_and_descendants
