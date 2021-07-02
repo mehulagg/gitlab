08c83dd6b1f5a46289cc90b404c6819f355f9966
Paul Slaughter <pslaughter@gitlab.com>
2021-07-02 10:38:57 -0500

    Remove combined_menu feature flag
    - Aslo removes some partial views that are no
      longer referenced
    - Also removes .with-deprecated-styles

--- spec/features/dashboard/group_dashboard_with_external_authorization_service_spec.rb
+++ spec/features/dashboard/group_dashboard_with_external_authorization_service_spec.rb
@@ -8,60 +8,38 @@ RSpec.describe 'The group dashboard' do
 
   let(:user) { create(:user) }
 
-  shared_examples 'combined_menu: feature flag examples' do
-    before do
-      sign_in user
-    end
-
-    describe 'The top navigation' do
-      it 'has all the expected links' do
-        visit dashboard_groups_path
-
-        open_top_nav
-
-        within_top_nav do
-          expect(page).to have_button('Projects')
-          expect(page).to have_button('Groups')
-          expect(page).to have_link('Activity')
-          expect(page).to have_link('Milestones')
-          expect(page).to have_link('Snippets')
-        end
-      end
-
-      it 'hides some links when an external authorization service is enabled' do
-        enable_external_authorization_service_check
-        visit dashboard_groups_path
-
-        open_top_nav
-
-        within_top_nav do
-          expect(page).to have_button('Projects')
-          expect(page).to have_button('Groups')
-          expect(page).not_to have_link('Activity')
-          expect(page).not_to have_link('Milestones')
-          expect(page).to have_link('Snippets')
-        end
-      end
-    end
+  before do
+    sign_in user
   end
 
-  context 'with combined_menu feature flag on', :js do
-    let(:needs_rewrite_for_combined_menu_flag_on) { true }
+  describe 'The top navigation' do
+    it 'has all the expected links' do
+      visit dashboard_groups_path
 
-    before do
-      stub_feature_flags(combined_menu: true)
+      open_top_nav
+
+      within_top_nav do
+        expect(page).to have_button('Projects')
+        expect(page).to have_button('Groups')
+        expect(page).to have_link('Activity')
+        expect(page).to have_link('Milestones')
+        expect(page).to have_link('Snippets')
+      end
     end
 
-    it_behaves_like 'combined_menu: feature flag examples'
-  end
+    it 'hides some links when an external authorization service is enabled' do
+      enable_external_authorization_service_check
+      visit dashboard_groups_path
 
-  context 'with combined_menu feature flag off' do
-    let(:needs_rewrite_for_combined_menu_flag_on) { false }
+      open_top_nav
 
-    before do
-      stub_feature_flags(combined_menu: false)
+      within_top_nav do
+        expect(page).to have_button('Projects')
+        expect(page).to have_button('Groups')
+        expect(page).not_to have_link('Activity')
+        expect(page).not_to have_link('Milestones')
+        expect(page).to have_link('Snippets')
+      end
     end
-
-    it_behaves_like 'combined_menu: feature flag examples'
   end
 end
