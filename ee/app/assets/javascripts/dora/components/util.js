9b1424ebefbc536b3e8fc91aa769b6d8a9eab715
Nathan Friend <nathan@gitlab.com>
2021-07-06 12:26:36 -0400

    Update dot style for DORA lead time graphs
    This commit updates the data point style on lead time graphs (both
    project and group-level) to always render a blue dot. This makes it
    easier to see isolated data points on the graphs.
    
    Changelog: changed
    EE: true

--- ee/app/assets/javascripts/dora/components/util.js
+++ ee/app/assets/javascripts/dora/components/util.js
@@ -116,6 +116,9 @@ export const buildNullSeriesForLeadTimeChart = (seriesData) => {
   });
 
   merge(nonNullSeries, {
+    showSymbol: true,
+    showAllSymbol: true,
+    symbolSize: 12,
     lineStyle: {
       color: dataVizBlue500,
     },
