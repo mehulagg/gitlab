3093a702ccd1c351c94cd35258831c9c8b42f8c7
Reuben Pereira <rpereira@gitlab.com>
2021-07-06 17:04:08 +0530

    Remove the read_container_registry_access_level feature flag

--- app/models/project.rb
+++ app/models/project.rb
@@ -406,8 +406,9 @@ class Project < ApplicationRecord
     :wiki_access_level, :snippets_access_level, :builds_access_level,
     :repository_access_level, :pages_access_level, :metrics_dashboard_access_level, :analytics_access_level,
     :operations_enabled?, :operations_access_level, :security_and_compliance_access_level,
-    :container_registry_access_level,
+    :container_registry_access_level, :container_registry_enabled?,
     to: :project_feature, allow_nil: true
+  alias_method :container_registry_enabled, :container_registry_enabled?
   delegate :show_default_award_emojis, :show_default_award_emojis=,
     :show_default_award_emojis?,
     to: :project_setting, allow_nil: true
@@ -2620,15 +2621,6 @@ class Project < ApplicationRecord
   def merge_requests_author_approval
     !!read_attribute(:merge_requests_author_approval)
   end
-
-  def container_registry_enabled
-    if Feature.enabled?(:read_container_registry_access_level, self.namespace, default_enabled: :yaml)
-      project_feature.container_registry_enabled?
-    else
-      read_attribute(:container_registry_enabled)
-    end
-  end
-  alias_method :container_registry_enabled?, :container_registry_enabled
 
   def ci_forward_deployment_enabled?
     return false unless ci_cd_settings
