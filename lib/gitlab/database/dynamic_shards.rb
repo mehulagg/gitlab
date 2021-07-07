8c52e453c701d7af0ae8cc2f8052564dc3ccab02
Thong Kuah <tkuah@gitlab.com>
2021-07-07 21:20:19 +1200

    Remove schema_search_path from database.yml.postgresql
    Drive everything from dynamic_shards.rb for now

--- lib/gitlab/database/dynamic_shards.rb
+++ lib/gitlab/database/dynamic_shards.rb
@@ -10,9 +10,9 @@ module DynamicShards
 
       configs.each do |config_name, config|
         if config_name == 'main'
-          config["schema_search_paths"] ||= "public,ci"
+          config["schema_search_paths"] ||= "public,gitlab_ci"
         elsif config_name == 'ci'
-          config["schema_search_paths"] ||= "ci"
+          #config["schema_search_paths"] ||= "gitlab_ci"
           config["migrations_paths"] ||= "db/ci_migrate"
         end
 
