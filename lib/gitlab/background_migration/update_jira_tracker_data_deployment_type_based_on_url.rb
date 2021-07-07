f9f153bcfda64d520a8d4b18fae54de6fe603db7
Mehmet Emin INAC <minac@gitlab.com>
2021-07-07 19:01:20 +0300

    Remove application logic dependency from migrations

--- lib/gitlab/background_migration/update_jira_tracker_data_deployment_type_based_on_url.rb
+++ lib/gitlab/background_migration/update_jira_tracker_data_deployment_type_based_on_url.rb
@@ -7,7 +7,15 @@ class Gitlab::BackgroundMigration::UpdateJiraTrackerDataDeploymentTypeBasedOnUrl
     self.table_name = "jira_tracker_data"
     self.inheritance_column = :_type_disabled
 
-    include ::Integrations::BaseDataFields
+    def self.encryption_options
+      {
+        key: Settings.attr_encrypted_db_key_base_32,
+        encode: true,
+        mode: :per_attribute_iv,
+        algorithm: 'aes-256-gcm'
+      }
+    end
+
     attr_encrypted :url, encryption_options
     attr_encrypted :api_url, encryption_options
 
