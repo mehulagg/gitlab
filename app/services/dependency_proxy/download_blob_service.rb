40f303a0b7ffbcca92e0ae4c1953b96b77612827
Steve Abrams <sabrams@gitlab.com>
2021-07-07 12:01:00 -0600

    Unlink dependency proxy temp files
    Update the manifest and blob services
    to unlink the Tempfiles immediately.
    
    Refactor the DownloadBlobService to have
    a similar pattern as the PullManifestService.

--- app/services/dependency_proxy/download_blob_service.rb
+++ app/services/dependency_proxy/download_blob_service.rb
@@ -11,6 +11,8 @@ module DependencyProxy
 
     def execute
       File.open(@temp_file.path, "wb") do |file|
+        file.unlink
+
         Gitlab::HTTP.get(blob_url, headers: auth_headers, stream_body: true) do |fragment|
           if [301, 302, 307].include?(fragment.code)
             # do nothing
@@ -23,6 +25,33 @@ module DependencyProxy
       end
 
       success(file: @temp_file)
+    rescue DownloadError => exception
+      error(exception.message, exception.http_status)
+    rescue Timeout::Error => exception
+      error(exception.message, 599)
+    end
+
+    def execute_with_blob
+      raise ArgumentError, 'Block must be provided' unless block_given?
+
+      file = Tempfile.new('dependency_proxy_blob:')
+      file.unlink
+      begin
+        Gitlab::HTTP.get(blob_url, headers: auth_headers, stream_body: true) do |fragment|
+          if [301, 302, 307].include?(fragment.code)
+            # do nothing
+          elsif fragment.code == 200
+            file.write(fragment)
+          else
+            raise DownloadError.new('Non-success response code on downloading blob fragment', fragment.code)
+          end
+        end
+
+        file.flush
+        yield(success(file: @temp_file, content_type: response.headers['content-type']))
+      ensure
+        file.close
+      end
     rescue DownloadError => exception
       error(exception.message, exception.http_status)
     rescue Timeout::Error => exception
