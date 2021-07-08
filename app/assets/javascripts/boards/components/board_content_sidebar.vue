a06d4119a515284fcc76dc4b6334aebf045574de
Florie Guibert <fguibert@gitlab.com>
2021-07-08 15:52:20 +1000

    Migrate Add To Do button to widget
    Use Add To Do widget on Issue and MR sidebars
    Remove To Do label for consistency with boards sidebar
    
    Changelog: changed

--- app/assets/javascripts/boards/components/board_content_sidebar.vue
+++ app/assets/javascripts/boards/components/board_content_sidebar.vue
@@ -11,6 +11,7 @@ import SidebarAssigneesWidget from '~/sidebar/components/assignees/sidebar_assig
 import SidebarConfidentialityWidget from '~/sidebar/components/confidential/sidebar_confidentiality_widget.vue';
 import SidebarDateWidget from '~/sidebar/components/date/sidebar_date_widget.vue';
 import SidebarSubscriptionsWidget from '~/sidebar/components/subscriptions/sidebar_subscriptions_widget.vue';
+import SidebarTodoWidget from '~/sidebar/components/todo_toggle/sidebar_todo_widget.vue';
 import glFeatureFlagMixin from '~/vue_shared/mixins/gl_feature_flags_mixin';
 
 export default {
@@ -24,6 +25,7 @@ export default {
     BoardSidebarLabelsSelect,
     SidebarSubscriptionsWidget,
     SidebarDropdownWidget,
+    SidebarTodoWidget,
     MountingPortal,
     SidebarWeightWidget: () =>
       import('ee_component/sidebar/components/weight/sidebar_weight_widget.vue'),
@@ -88,7 +90,13 @@ export default {
       @close="handleClose"
     >
       <template #header>
-        <h2 class="gl-my-0 gl-font-size-h2 gl-line-height-24">{{ __('Issue details') }}</h2>
+        <h2 class="gl-mt-0 gl-mb-3 gl-font-size-h2 gl-line-height-24">{{ __('Issue details') }}</h2>
+        <sidebar-todo-widget
+          :issuable-id="activeBoardItem.fullId"
+          :issuable-iid="activeBoardItem.iid"
+          :full-path="fullPath"
+          :issuable-type="issuableType"
+        />
       </template>
       <template #default>
         <board-sidebar-title />
--- app/assets/javascripts/sidebar/components/todo_toggle/sidebar_todo_widget.vue
+++ app/assets/javascripts/sidebar/components/todo_toggle/sidebar_todo_widget.vue
@@ -1,5 +1,5 @@
 <script>
-import { GlTooltipDirective } from '@gitlab/ui';
+import { GlIcon, GlTooltipDirective } from '@gitlab/ui';
 import { produce } from 'immer';
 import createFlash from '~/flash';
 import { __, sprintf } from '~/locale';
@@ -8,10 +8,16 @@ import TodoButton from '~/vue_shared/components/sidebar/todo_button.vue';
 
 export default {
   components: {
+    GlIcon,
     TodoButton,
   },
   directives: {
     GlTooltip: GlTooltipDirective,
+  },
+  inject: {
+    isClassicSidebar: {
+      default: false,
+    },
   },
   props: {
     issuableId: {
@@ -86,6 +92,9 @@ export default {
       }
       return TodoMutationTypes.Create;
     },
+    collapsedButtonIcon() {
+      return this.hasTodo ? 'todo-done' : 'todo-add';
+    },
   },
   methods: {
     toggleTodo() {
@@ -158,7 +167,21 @@ export default {
       :is-todo="hasTodo"
       :loading="isLoading"
       size="small"
+      class="hide-collapsed"
       @click.stop.prevent="toggleTodo"
     />
+    <div
+      v-if="isClassicSidebar"
+      v-gl-tooltip
+      class="sidebar-collapsed-icon sidebar-collapsed-container"
+    >
+      <gl-icon
+        :size="16"
+        :class="{ 'todo-undone': hasTodo }"
+        :name="collapsedButtonIcon"
+        :aria-label="collapsedButtonIcon"
+        @click.stop.prevent="toggleTodo"
+      />
+    </div>
   </div>
 </template>
