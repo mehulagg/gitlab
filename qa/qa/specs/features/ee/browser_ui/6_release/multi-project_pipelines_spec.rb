100bdbfcda19d609f63134d50d37939addd2034a
Nailia Iskhakova <niskhakova@gitlab.com>
2021-07-08 18:54:01 +0300

    Simplify expectation per review
    Signed-off-by: Nailia Iskhakova <niskhakova@gitlab.com>

--- qa/qa/specs/features/ee/browser_ui/6_release/multi-project_pipelines_spec.rb
+++ qa/qa/specs/features/ee/browser_ui/6_release/multi-project_pipelines_spec.rb
@@ -77,8 +77,7 @@ module QA
 
       it 'creates a multi-project pipeline', testcase: 'https://gitlab.com/gitlab-org/quality/testcases/-/issues/560' do
         Page::MergeRequest::Show.perform do |show|
-          pipeline_passed = show.has_pipeline_status?('passed')
-          expect(pipeline_passed).to be_truthy, "The pipeline did not pass."
+          expect(show.has_pipeline_status?('passed')).to be_truthy, "The pipeline did not pass."
 
           show.click_pipeline_link
         end
