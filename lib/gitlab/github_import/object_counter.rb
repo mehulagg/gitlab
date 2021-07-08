e60e465306a32063c2ebe1589ea21abced25a40f
Kassio Borges <kborges@gitlab.com>
2021-07-08 04:21:25 +0100

    GithubImporter: Count fetched/import objects globally and by project
    Counts fetched and imported Github objects by project and globally. This
    is done using the `Gitlab::GithubImport::ObjectCounter`.
    
    These counts will provide information to follow the stability of the
    Github Importer on Grafana.
    
    Related to: https://gitlab.com/gitlab-org/gitlab/-/issues/335199
    
    MR: https://gitlab.com/gitlab-org/gitlab/-/merge_requests/65700
    Changelog: changed

--- lib/gitlab/github_import/object_counter.rb
+++ lib/gitlab/github_import/object_counter.rb
@@ -6,19 +6,20 @@ module Gitlab
   module GithubImport
     class ObjectCounter
       OPERATIONS = %w[fetched imported].freeze
-      COUNTER_LIST_KEY = 'github-importer/object-counters-list/%{project}/%{operation}'
-      COUNTER_KEY = 'github-importer/object-counter/%{project}/%{operation}/%{object_type}'
+      PROJECT_COUNTER_LIST_KEY = 'github-importer/object-counters-list/%{project}/%{operation}'
+      PROJECT_COUNTER_KEY = 'github-importer/object-counter/%{project}/%{operation}/%{object_type}'
+
+      GLOBAL_COUNTER_KEY = 'github_importer_%{operation}_%{object_type}'
+      GLOBAL_COUNTER_DESCRIPTION = 'The number of %{operation} Github %{object_type}'
+
       CACHING = Gitlab::Cache::Import::Caching
 
       class << self
         def increment(project, object_type, operation)
           validate_operation!(operation)
 
-          counter_key = COUNTER_KEY % { project: project.id, operation: operation, object_type: object_type }
-
-          add_counter_to_list(project, operation, counter_key)
-
-          CACHING.increment(counter_key)
+          increment_project_counter(project, object_type, operation)
+          increment_global_counter(object_type, operation)
         end
 
         def summary(project)
@@ -37,12 +38,33 @@ module Gitlab
 
         private
 
+        def increment_global_counter(object_type, operation)
+          key = GLOBAL_COUNTER_KEY % {
+            operation: operation,
+            object_type: object_type
+          }
+          description = GLOBAL_COUNTER_DESCRIPTION % {
+            operation: operation,
+            object_type: object_type.to_s.humanize
+          }
+
+          Gitlab::Metrics.counter(key, description).increment
+        end
+
+        def increment_project_counter(project, object_type, operation)
+          counter_key = PROJECT_COUNTER_KEY % { project: project.id, operation: operation, object_type: object_type }
+
+          add_counter_to_list(project, operation, counter_key)
+
+          CACHING.increment(counter_key)
+        end
+
         def add_counter_to_list(project, operation, key)
           CACHING.set_add(counter_list_key(project, operation), key)
         end
 
         def counter_list_key(project, operation)
-          COUNTER_LIST_KEY % { project: project.id, operation: operation }
+          PROJECT_COUNTER_LIST_KEY % { project: project.id, operation: operation }
         end
 
         def validate_operation!(operation)
