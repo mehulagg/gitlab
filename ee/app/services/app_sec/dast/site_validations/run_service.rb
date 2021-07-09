49329d7536ef81f2e56be49ced399d8faef5d66a
Philip Cunningham <pcunningham@gitlab.com>
2021-07-09 17:00:05 +1000

    wip

--- ee/app/services/app_sec/dast/site_validations/run_service.rb
+++ ee/app/services/app_sec/dast/site_validations/run_service.rb
@@ -23,19 +23,19 @@ module AppSec
 
         def ci_configuration
           {
-            stages: ['dast'],
-            validation: {
-              stage: 'dast',
-              image: '192.168.99.103:5001/root/dast-validation-runner:latest',
-              script: ['~/validate.sh'],
-              variables: {
-                GIT_STRATEGY: 'none',
-                DAST_SITE_VALIDATION_ID: dast_site_validation.id,
-                DAST_SITE_VALIDATION_HEADER: ::DastSiteValidation::HEADER,
-                DAST_SITE_VALIDATION_STRATEGY: dast_site_validation.validation_strategy,
-                DAST_SITE_VALIDATION_TOKEN: dast_site_validation.dast_site_token.token,
-                DAST_SITE_VALIDATION_URL: dast_site_validation.validation_url
-              }
+            'stages' => ['dast'],
+            'validation' => {
+              'stage' => 'dast',
+              'image' => '192.168.99.103:5001/root/dast-validation-runner:latest',
+              'variables' => {
+                'GIT_STRATEGY' => 'none',
+                'DAST_SITE_VALIDATION_ID' => dast_site_validation.id,
+                'DAST_SITE_VALIDATION_HEADER' => ::DastSiteValidation::HEADER,
+                'DAST_SITE_VALIDATION_STRATEGY' => dast_site_validation.validation_strategy,
+                'DAST_SITE_VALIDATION_TOKEN' => dast_site_validation.dast_site_token.token,
+                'DAST_SITE_VALIDATION_URL' => dast_site_validation.validation_url
+              },
+              'script' => ['~/validate.sh']
             }
           }
         end
