43b9c5a2720b8b0621444b1e6d0f1e163c66225d
Nikhil George <ngeorge@gitlab.com>
2021-07-09 12:26:39 +0530

    vul for each gem

--- depscore/main.rb
+++ depscore/main.rb
@@ -7,6 +7,8 @@ require "csv"
 require "date"
 require "optparse"
 require "json"
+require 'securerandom'
+
 
 gem_hash = {}
 
@@ -32,59 +34,44 @@ def dreport_csvprep()
 end
 
 def dreport_summary()
+  report = {
+ version:"14.0.0",
+ vulnerabilities: []
+  }
   summary = ""
   all_gems = LibData.all_instances
   all_gems.each do |gem|
     if gem.signals[:score].to_i == 0
-      summary = summary + gem.name + ","
-    end
+       summary = summary + gem.name 
+        + "Total download: " + (gem.signals[:tot_downloads]|| "").to_s
+        + " .Number of reverse dependency: "+  (gem.signals[:reverse_dep_count]|| "").to_s
+        + " .Latest version age: "+ (gem.signals[:latest_vesion_age] || "").to_s
+        + " .Latest release on: "+ (gem.signals[:latest_release_on] || "")
+        + " .Number of releases last year: "+ (gem.signals[:rel_freq_last_4quater]|| "").to_s
+        + " .Score: "+gem.signals[:score].to_s
+
+        vul_id = SecureRandom.uuid
+        vul = {
+          id: vul_id,
+          category: "dependency_scanning",
+          name: "Use of Unmaintained Third Party Components",
+          message: "Third party component which is not frequently mantained detected",
+          description: summary,
+          cve: vul_id,
+          severity: "Info",
+          solution: "Find an alternate third party library.",
+          scanner:{id: "depscore", name: "depscore"},
+          location: {file:"Gemfile",dependency:{package:{name:gem.name},version:gem.signals[:version]}},
+          identifiers: [{type:"cwe",name:"CWE-1104",value: "Use of Unmaintained Third Party Components",url: "https://cwe.mitre.org/data/definitions/1104.html" }],
+          links: [{url: "https://cwe.mitre.org/data/definitions/1104.html"}]
+        }
+        report[:vulnerabilities].append(vul)
+        summary = ""
+    end #if score = 0
   end
-
-  foo = '{"version":"14.0.0",
-  "vulnerabilities":[{
-         "id":"038e8c173096a16af7zzzzzzzzzc9a4fa164e9da701046ce95",
-         "category":"dependency_scanning",
-         "name":"Unreliable libraries",
-         "message":"Libraries that are not frequently mantained detected",
-         "description":"Based on the score calculated from the librariy metadata the below libraries seems to have less frequent maintance",
-         "cve":"yarn.lock:execa:gemnasium:05cfa2e8-2d0c-42c1-8894-638e2f12ff3d",
-         "severity":"Info",
-         "solution":"Upgrade to version 2.0.0 or above.",
-         "scanner":{
-            "id":"depscore",
-            "name":"depscore"
-         },
-         "location":{
-            "file":"client/package.json",
-            "dependency":{
-               "package":{
-                  "name":"handlebars"
-               },
-               "version":"4.0.11"
-            }
-         },
-         "identifiers":[
-            {
-               "type":"gemnasium",
-               "name":"Gemnasium-4f3402a7-97dd-45fc-9ed5-f49e707020c3",
-               "value":"4f3402a7-97dd-45fc-9ed5-f49e707020c3",
-               "url":"https://gitlab.com/gitlab-org/security-products/gemnasium-db/-/blob/master/npm/postcss/CVE-2021-23382.yml"
-            },
-            {
-               "type":"cve",
-               "name":"CVE-2021-23382",
-               "value":"CVE-2021-23382",
-               "url":"https://cve.mitre.org/cgi-bin/cvename.cgi?name=CVE-2021-23382"
-            }
-         ]
-      }
-   ]
-}
-'
-
-  summary_json = JSON(foo)
+  # summary_json = JSON(vul)
   File.open("gl-depscore-report.json", "w") do |f|
-    f.write(summary_json.to_json)
+    f.write(report.to_json)
   end
 end
 
