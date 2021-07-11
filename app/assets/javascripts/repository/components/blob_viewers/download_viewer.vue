c9d24fdb00561c9fb7c827321bb98e304907853d
Samantha Ming <sming@gitlab.com>
2021-07-11 05:44:37 -0700

    Create download blob viewer
    Issue: https://gitlab.com/gitlab-org/gitlab/-/issues/324320

---
+++ app/assets/javascripts/repository/components/blob_viewers/download_viewer.vue
@@ -0,0 +1,50 @@
+<script>
+import { GlIcon } from '@gitlab/ui';
+import { numberToHumanSize } from '~/lib/utils/number_utils';
+import { sprintf, __ } from '~/locale';
+
+export default {
+  components: {
+    GlIcon,
+  },
+  props: {
+    fileName: {
+      type: String,
+      required: true,
+    },
+    filePath: {
+      type: String,
+      required: true,
+    },
+    fileSize: {
+      type: Number,
+      required: false,
+      default: 0,
+    },
+  },
+  computed: {
+    downloadFileSize() {
+      return numberToHumanSize(this.fileSize);
+    },
+    downloadText() {
+      if (this.fileSize > 0) {
+        return sprintf(__('Download (%{fileSizeReadable})'), {
+          fileSizeReadable: this.downloadFileSize,
+        });
+      }
+      return __('Download');
+    },
+  },
+};
+</script>
+
+<template>
+  <div class="gl-text-center gl-py-13 gl-bg-gray-50">
+    <a :href="filePath" rel="nofollow" :download="fileName" target="_blank">
+      <div>
+        <gl-icon :size="16" name="download" class="gl-text-gray-900" />
+      </div>
+      <h4>{{ downloadText }}</h4>
+    </a>
+  </div>
+</template>
