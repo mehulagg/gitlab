43323ad8e50cd5bacacf7ba5cb41847783e2aeb8
Jarek Ostrowski <jostrowski@gitlab.com>
2021-07-13 15:43:13 -0400

    Sort by last accessed, add header

--- app/assets/javascripts/vue_shared/components/new_project_item_select.vue
+++ app/assets/javascripts/vue_shared/components/new_project_item_select.vue
@@ -37,31 +37,44 @@ export default {
   data() {
     return {
       searchTerm: '',
-    }
+    };
   },
   computed: {
     text() {
-      return `Create ${this.label.toLowerCase()} in...`
+      return `Create ${this.label.toLowerCase()}`;
     },
     filteredProjectNames() {
-      return this.frequentProjects.filter(project => project.name.toLowerCase().includes(this.searchTerm.toLowerCase())).sort((a, b) => b.lastAccessedOn - a.lastAccessedOn);
+      return this.frequentProjects
+        .filter((project) => project.namespace.toLowerCase().includes(this.searchTerm.toLowerCase()))
+        .sort((a, b) => b.lastAccessedOn - a.lastAccessedOn);
     },
     filteredProjectNamesLength() {
       return this.filteredProjectNames.length === 0;
     },
   },
   mounted() {
-    return console.log(this.filteredProjectNames, 'filtered names')
-  }
+    return console.log(this.filteredProjectNames, 'filtered names');
+  },
 };
 </script>
 
 <template>
-  <gl-dropdown :text="text">
-    <gl-search-box-by-type v-model.trim="searchTerm" />
-    <gl-dropdown-item v-for="project in filteredProjectNames" :key="project.id" >
-      {{ project.name }}
+  <gl-dropdown 
+    variant="confirm"  
+    :text="text" 
+    header-text="Recent projects" 
+    right
+  >
+    <template #header>
+      <gl-search-box-by-type v-model.trim="searchTerm" />
+    </template>
+    <gl-dropdown-item
+      v-for="project in filteredProjectNames"
+      :key="project.id"
+      :href="`${project.webUrl}/-/${type}/new`"
+    >
+      {{ project.namespace }}
     </gl-dropdown-item>
-    <div v-show="filteredProjectNamesLength" class="text-secondary p-2">Nothing found…</div>
+    <div v-show="filteredProjectNamesLength" class="text-secondary p-2">No projects found…</div>
   </gl-dropdown>
 </template>
