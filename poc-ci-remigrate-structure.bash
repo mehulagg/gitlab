c170e80e465ef6da1988bac4286135a4084adb6d
Kamil Trzciński <ayufan@ayufan.eu>
2021-07-13 19:02:06 +0200

    Fix usage of many databases

--- poc-ci-remigrate-structure.bash
+++ poc-ci-remigrate-structure.bash
@@ -3,7 +3,8 @@
 set -xeo pipefail
 
 export RAILS_ENV=test
-export ALL_SCHEMAS=true
+export FORCE_SINGLE_DB=true
+export ENABLE_SPRING=0
 
 echo Reverting schema...
 rm -rf db/schema_migrations/ db/migrate/20211201000001_drop_ci_foreign_keys.rb
@@ -16,3 +17,7 @@ bin/rake db:drop db:create db:schema:load db:migrate
 echo Recreate FKs drop...
 bin/rails runner poc-ci-validate_all_fks.rb
 bin/rake db:migrate
+
+echo Recreate all DBs...
+unset FORCE_SINGLE_DB
+bin/rake db:drop db:create db:schema:load db:migrate
