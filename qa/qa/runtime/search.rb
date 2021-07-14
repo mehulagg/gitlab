f7c6ec418a2797e38ecb936adb957cc317a60aaf
Erick Banks <ebanks@gitlab.com>
2021-07-14 17:25:47 +0000

    Update search.rb

--- qa/qa/runtime/search.rb
+++ qa/qa/runtime/search.rb
@@ -9,7 +9,7 @@ module QA
       extend Support::Api
 
       RETRY_MAX_ITERATION = 10
-      RETRY_SLEEP_INTERVAL = 30
+      RETRY_SLEEP_INTERVAL = 60
       INSERT_RECALL_THRESHOLD = RETRY_MAX_ITERATION * RETRY_SLEEP_INTERVAL
 
       ElasticSearchServerError = Class.new(RuntimeError)
