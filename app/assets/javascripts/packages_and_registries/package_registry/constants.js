8e17ff3a8a663cfd481e25c8141386cea836667c
Nicolò Maria Mezzopera <nicolo.mezzopera@gmail.com>
2021-07-14 11:15:10 +0200

    Update package helper to return package id
    - helper
    - views
    
    Add apollo-graphql bootstrap
    
    - basic query
    - init files
    - add to app
    
    Move constants file from original folder
    
    Add fetch error constants
    
    - constants
    - regenerate pot file
    
    Add used package utilities
    
    - source
    - tests
    
    Add mock data file
    
    Move and refactor package title component
    
    - source
    - tests
    - snapshots
    
    Use snake case in ruby methods
    
    Fix mock data format
    
    Add apollo and basic query to details app
    
    - source
    - tests
    
    Adjust linting errors and use extendedWrapper
    
    - app
    - package_title
    - tests

---
+++ app/assets/javascripts/packages_and_registries/package_registry/constants.js
@@ -0,0 +1,52 @@
+import { __, s__ } from '~/locale';
+
+export const PackageType = {
+  CONAN: 'CONAN',
+  MAVEN: 'MAVEN',
+  NPM: 'NPM',
+  NUGET: 'NUGET',
+  PYPI: 'PYPI',
+  COMPOSER: 'COMPOSER',
+  RUBYGEMS: 'RUBYGEMS',
+  GENERIC: 'GENERIC',
+  DEBIAN: 'DEBIAN',
+  HELM: 'HELM',
+};
+
+// we want this separated from the main dictionary to avoid it being pulled in the search of package
+export const TERRAFORM_PACKAGE_TYPE = 'terraform_module';
+
+export const TrackingActions = {
+  DELETE_PACKAGE: 'delete_package',
+  REQUEST_DELETE_PACKAGE: 'request_delete_package',
+  CANCEL_DELETE_PACKAGE: 'cancel_delete_package',
+  PULL_PACKAGE: 'pull_package',
+  DELETE_PACKAGE_FILE: 'delete_package_file',
+  REQUEST_DELETE_PACKAGE_FILE: 'request_delete_package_file',
+  CANCEL_DELETE_PACKAGE_FILE: 'cancel_delete_package_file',
+};
+
+export const TrackingCategories = {
+  [PackageType.MAVEN]: 'MavenPackages',
+  [PackageType.NPM]: 'NpmPackages',
+  [PackageType.CONAN]: 'ConanPackages',
+};
+
+export const SHOW_DELETE_SUCCESS_ALERT = 'showSuccessDeleteAlert';
+export const DELETE_PACKAGE_ERROR_MESSAGE = s__(
+  'PackageRegistry|Something went wrong while deleting the package.',
+);
+export const DELETE_PACKAGE_FILE_ERROR_MESSAGE = s__(
+  __('PackageRegistry|Something went wrong while deleting the package file.'),
+);
+export const DELETE_PACKAGE_FILE_SUCCESS_MESSAGE = s__(
+  'PackageRegistry|Package file deleted successfully',
+);
+export const FETCH_PACKAGE_DETAILS_ERROR_MESSAGE = s__(
+  'PackageRegistry|Failed to load the package data',
+);
+
+export const PACKAGE_ERROR_STATUS = 'error';
+export const PACKAGE_DEFAULT_STATUS = 'default';
+export const PACKAGE_HIDDEN_STATUS = 'hidden';
+export const PACKAGE_PROCESSING_STATUS = 'processing';
