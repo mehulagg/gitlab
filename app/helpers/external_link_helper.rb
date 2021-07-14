76cf9902c8314afa2d164f4d59a1c991a5a6b020
Fernando <farias@gitlab.com>
2021-07-14 03:34:41 +0200

    First attempt

--- app/helpers/external_link_helper.rb
+++ app/helpers/external_link_helper.rb
@@ -1,9 +1,11 @@
 # frozen_string_literal: true
 
 module ExternalLinkHelper
+  include ActionView::Helpers::TextHelper
+
   def external_link(body, url, options = {})
     link_to url, { target: '_blank', rel: 'noopener noreferrer' }.merge(options) do
-      "#{body}#{sprite_icon('external-link', css_class: 'gl-ml-1')}".html_safe
+      "#{sanitize(body)}#{sprite_icon('external-link', css_class: 'gl-ml-1')}"
     end
   end
 end
--- spec/helpers/external_link_helper_spec.rb
+++ spec/helpers/external_link_helper_spec.rb
@@ -4,6 +4,7 @@ require 'spec_helper'
 
 RSpec.describe ExternalLinkHelper do
   include IconsHelper
+  include ActionView::Helpers::TextHelper
 
   it 'returns external link with icon' do
     link = external_link('https://gitlab.com', 'https://gitlab.com').to_s
@@ -16,5 +17,12 @@ RSpec.describe ExternalLinkHelper do
 
     expect(link).to start_with('<a target="_blank" rel="noopener noreferrer" data-foo="bar" class="externalLink" href="https://gitlab.com">https://gitlab.com')
     expect(link).to include('data-testid="external-link-icon"')
+  end
+
+  it 'sanitizes and returns external link with icon' do
+    link = external_link('https://gitlab.com/users?return_to=javascript:alert()', 'https://gitlab.com/users?return_to=javascript:alert()').to_s
+    expect('https://gitlab.com/users?return_to=javascript:alert()').to start_with('foo');
+    #expect(link).to start_with('<a target="_blank" rel="noopener noreferrer" href="https://gitlab.com/users">https://gitlab.com/users')
+    #expect(link).to include('data-testid="external-link-icon"')
   end
 end
