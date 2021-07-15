cbe52592b1075ee56e303f36cd16083f1f3a57ac
Kassio Borges <kborges@gitlab.com>
2021-07-15 15:16:02 +0100

    WIP

--- lib/api/bulk_imports.rb
+++ lib/api/bulk_imports.rb
@@ -33,7 +33,7 @@ module API
       params do
         use :pagination
         optional :status, type: String, values: BulkImport.all_human_statuses,
-                 desc: 'Return GitLab Migrations with specified status'
+          desc: 'Return GitLab Migrations with specified status'
       end
       get do
         present paginate(bulk_imports), with: Entities::BulkImport
@@ -45,7 +45,7 @@ module API
       params do
         use :pagination
         optional :status, type: String, values: ::BulkImports::Entity.all_human_statuses,
-                 desc: "Return all GitLab Migrations' entities with specified status"
+          desc: "Return all GitLab Migrations' entities with specified status"
       end
       get :entities do
         entities = ::BulkImports::EntitiesFinder.new(user: current_user, status: params[:status]).execute
@@ -69,7 +69,7 @@ module API
       params do
         requires :import_id, type: Integer, desc: "The ID of user's GitLab Migration"
         optional :status, type: String, values: ::BulkImports::Entity.all_human_statuses,
-                 desc: 'Return import entities with specified status'
+          desc: 'Return import entities with specified status'
         use :pagination
       end
       get ':import_id/entities' do
@@ -85,6 +85,44 @@ module API
       end
       get ':import_id/entities/:entity_id' do
         present bulk_import_entity, with: Entities::BulkImports::Entity
+      end
+
+      desc 'Start a new group migration' do
+        detail 'This feature was introduced in GitLab 14.2.'
+      end
+      params do
+        requires :source_type, String
+        requires :url, type: String, desc: 'The source GitLab url'
+        requires :access_token, type: String, desc: 'The access token to the source GitLab'
+        requires :source_full_path, type: String, desc: 'The full path of the group to be imported'
+        requires :destination_name, type: String, desc: 'Destination name for the group'
+        optional :destination_namespace, type: String, desc: 'Destination namespace for the group'
+      end
+      post 'migrate/:source_type' do
+        bulk_import_parms = {
+          bulk_import:
+          {
+            source_type: :group,
+            source_full_path: params[:source_full_path],
+            destination_name: params[:destination_name],
+            destination_namespace: params[:destination_namespace]
+          }
+        }
+
+        service = BulkImportService.new(
+          current_user,
+          bulk_import_params,
+          url: params[:url],
+          access_token: params[:access_token]
+        )
+
+        service.execute
+
+        if response.success?
+          render json: response.payload.to_json(only: [:id])
+        else
+          render json: { error: response.message }, status: response.http_status
+        end
       end
     end
   end
