167ff95c58718265c35351aa4d3530fa9c8850c9
Doug Stull <dstull@gitlab.com>
2021-07-16 21:07:59 +0000

    Revert "Merge branch..."
    This reverts merge request !65078

--- db/migrate/20210630144339_add_invite_email_success_to_members.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-class AddInviteEmailSuccessToMembers < ActiveRecord::Migration[6.1]
-  def change
-    add_column :members, :invite_email_success, :boolean, null: false, default: true
-  end
-end
