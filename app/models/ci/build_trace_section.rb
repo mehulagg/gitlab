0d14a0357502e0ec1047394e88ef55b11eab795e
pbair <pbair@gitlab.com>
2021-07-19 15:06:35 -0400

    Inherit CI models from Ci::ApplicationRecord
    Add a new Ci::ApplicationRecord that all CI models should inherit from.
    This also replaces the usage of the previous Gitlab::Ci::Model.

--- app/models/ci/build_trace_section.rb
+++ app/models/ci/build_trace_section.rb
@@ -1,9 +1,8 @@
 # frozen_string_literal: true
 
 module Ci
-  class BuildTraceSection < ApplicationRecord
+  class BuildTraceSection < Ci::ApplicationRecord
     extend SuppressCompositePrimaryKeyWarning
-    extend Gitlab::Ci::Model
     include IgnorableColumns
 
     belongs_to :build, class_name: 'Ci::Build'
