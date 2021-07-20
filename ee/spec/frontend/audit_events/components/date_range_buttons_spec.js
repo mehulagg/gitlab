468b02f4d3ff994a1a25a8e763c66703225f00f0
Robert Hunt <rhunt@gitlab.com>
2021-07-20 12:45:54 +0100

    Adds snowplow to audit events date range buttons
    This adds the tracking data attributes to the audit events date range
    buttons. This will be picked up by snowplow and used to determine if the
    buttons are actually used

--- ee/spec/frontend/audit_events/components/date_range_buttons_spec.js
+++ ee/spec/frontend/audit_events/components/date_range_buttons_spec.js
@@ -1,6 +1,5 @@
 import { GlButtonGroup, GlButton } from '@gitlab/ui';
 import { shallowMount } from '@vue/test-utils';
-
 import DateRangeButtons from 'ee/audit_events/components/date_range_buttons.vue';
 import { CURRENT_DATE } from 'ee/audit_events/constants';
 import { getDateInPast } from '~/lib/utils/datetime_utility';
@@ -14,9 +13,22 @@ describe('DateRangeButtons component', () => {
     });
   };
 
+  const findButtonGroup = () => wrapper.findComponent(GlButtonGroup);
+  const findSelectedButton = () => findButtonGroup().find('[selected="true"]');
+
   afterEach(() => {
     wrapper.destroy();
-    wrapper = null;
+  });
+
+  it('sets the tracking data on the button', () => {
+    createComponent({
+      dateRange: { startDate: getDateInPast(CURRENT_DATE, 7), endDate: CURRENT_DATE },
+    });
+
+    expect(findSelectedButton().attributes()).toMatchObject({
+      'data-track-action': 'click_date_range_button',
+      'data-track-label': 'date_range_button_last_7_days',
+    });
   });
 
   it('shows the selected the option that matches the provided dateRange property', () => {
@@ -24,7 +36,7 @@ describe('DateRangeButtons component', () => {
       dateRange: { startDate: getDateInPast(CURRENT_DATE, 7), endDate: CURRENT_DATE },
     });
 
-    expect(wrapper.find(GlButtonGroup).find('[selected="true"]').text()).toBe('Last 7 days');
+    expect(findSelectedButton().text()).toBe('Last 7 days');
   });
 
   it('shows no date range as selected when the dateRange property does not match any option', () => {
@@ -35,14 +47,14 @@ describe('DateRangeButtons component', () => {
       },
     });
 
-    expect(wrapper.find(GlButtonGroup).find('[selected="true"]').exists()).toBe(false);
+    expect(findSelectedButton().exists()).toBe(false);
   });
 
   it('emits an "input" event with the dateRange when a new date range is selected', async () => {
     createComponent({
       dateRange: { startDate: getDateInPast(CURRENT_DATE, 1), endDate: CURRENT_DATE },
     });
-    wrapper.find(GlButtonGroup).find(GlButton).vm.$emit('click');
+    findButtonGroup().findComponent(GlButton).vm.$emit('click');
 
     await wrapper.vm.$nextTick();
     expect(wrapper.emitted().input[0]).toEqual([
